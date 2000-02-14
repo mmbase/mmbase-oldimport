7068bcf1e148c9a6e08fb4e9f65f5fc980633f98
wwwtech <wwwtech@91430c5f-d668-0410-9e7d-dd663366dcfd>
2000-02-14 23:59:02 +0000

    new annot en video builder
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@21 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/builders/AnnotRel.java
+++ src/org/mmbase/module/builders/AnnotRel.java
@@ -49,6 +49,7 @@ public class AnnotRel extends InsRel {
 	public void setDefaults(MMObjectNode node) {
 		// Set the default value for pos and length to 0 (0:0:0.0)
 		node.setValue("pos",0);
+		node.setValue("end",0);
 		node.setValue("length",0);
 		// All time values are default stored in milliseconds.
 		node.setValue("type",MILLIS);
@@ -71,12 +72,13 @@ public class AnnotRel extends InsRel {
     public String getGUIIndicator(String field,MMObjectNode node) {
 		if (field.equals("pos")){
 			int time = node.getIntValue("pos");
-			return RelativeTime.convertIntToTime(time);
-
-		} else if (field.equals("length")){
+			return (RelativeTime.convertIntToTime(time));
+		} else if (field.equals("end")){
+			int time = node.getIntValue("end");
+			return (RelativeTime.convertIntToTime(time));
+		} else if (field.equals("length")) {
 			int time = node.getIntValue("length");
-			return RelativeTime.convertIntToTime(time);
-
+			return(RelativeTime.convertIntToTime(time));
 		} else if (field.equals("type")) {
             int val=node.getIntValue("type");
             if (val==HOURS) {
@@ -108,9 +110,6 @@ public class AnnotRel extends InsRel {
         return(null);
     }
 
-//NOTE : getList, replace process, getEditRelativeTimeField en setEditRelativeTimeField can all be 
-//removed, since we now use INFO.java for relativetime manipulation.
-
 	/**
     * getList all for frontend code
     */
@@ -131,92 +130,33 @@ public class AnnotRel extends InsRel {
     * replace all for frontend code
     */
 	public String replace(scanpage sp, StringTokenizer command) {
-
-        if (command.hasMoreTokens()) {
-            String token=command.nextToken();
-			System.out.println("AnnotRel::replace: The nextToken = "+token);
-			//RELTIME means RelativeTime.
-			if (token.equals("GETFIELDRELTIME")) {
-				// System.out.println("AnnotRel::replace: This method isn't implemented yet.");
-        		// return("GETFIELDRELTIME not implemented yet, says the AnnotRel builder.");
-
-                return (getEditRelativeTimeField(command.nextToken(), command.nextToken()));
-
-            } else if (token.equals("SETFIELDRELTIME")) {
-				// System.out.println("AnnotRel::replace: This method isn't implemented yet.");
-        		// return("SETFIELDRELTIME not implemented yet, says the AnnotRel builder.");
-
-                return (setEditRelativeTimeField(command));
-            } 
-        }
-        return("No command defined, says the AnnotRel builder.");
+		System.out.println("AnnotRel::replace: This method isn't implemented yet.");
+        return("");
     }		
 
-	/**
-	 *	This method retrieves the RelativeTimeField value that was filled in before by extracting it from
-	 * 	the editnode, and returns it as a string to the user. 
-	 * 	@param what The time field that's requested for.
-	 * 	@param fieldname The fieldname of the annotrel node that's currently being edited.
-	 * 	@returns The fieldname value as a String.
-	 */
- 	String getEditRelativeTimeField (String what, String fieldname) {
-		String res = new String ();
-		int timeValue = 0;
-
-		System.out.println("AnnotRel::getEditRelativeTimeField: fieldname = "+fieldname);
-
-		if (what.equals ("HOURS")) {
-			res += RelativeTime.getHours(timeValue);
-			System.out.println("AnnotRel::getEditRelativeTimeField::getHours() = "+res);
-			// return ("HOURS not implemented yet.");
-		} else if (what.equals ("MINUTES")) {
-		 	res += RelativeTime.getMinutes(timeValue);
-			System.out.println("AnnotRel::getEditRelativeTimeField::getHours() = "+res);
-			// return ("MINUTES not implemented yet.");
-		} else if (what.equals ("SECONDS")) {
-			res += RelativeTime.getSeconds(timeValue);
-			System.out.println("AnnotRel::getEditRelativeTimeField::getHours() = "+res);
-			// return ("SECONDS not implemented yet.");
-		} else if (what.equals ("MILLIS")) {
-			res += RelativeTime.getMillis(timeValue);
-			System.out.println("AnnotRel::getEditRelativeTimeField::getHours() = "+res);
-			// return ("MILLIS not implemented yet.");
-		} else {
-			return ("No timeAttribute OR Invalid timeAttribute provided!"); 
-		}
-
-		// return ("getEditRelativeTimeField Not Implemented yet");
-		return (res);
-    }
 
 	/**
-	 *	This method sets the RelativeTime value by using the timefield values h,m,s and ms.  
-	 * 	@param commands A StringTokenizer object containing the remainder of the $MOD String.
-	 * 	@returns The calculated timeValue in milliseconds as a stringValue.
-	 */
- 	String setEditRelativeTimeField(StringTokenizer command) {
+	* called then a local field is changed
+	*/
 
-		String hours, minutes, seconds, millis, value;
-		int time = 0;
-
-        if (command.hasMoreTokens() && (command.countTokens() == 4) ) {
-
-			hours   = command.nextToken();
-			minutes = command.nextToken();
-			seconds = command.nextToken();
-			millis  = command.nextToken();
-			value   = hours + ":" + minutes + ":" + seconds + "." + millis;
-
-			time = RelativeTime.convertTimeToInt(value);
-			System.out.println ("AnnotRel::setEditRelativeTimeField -> Storing time: " +time);
-
-			return (""+time);
-
-		} else {
-			String error = "Annotrel::setEditRelativeTimeField: Error, Amount of timeValues is != 4 (h,m,s,ms)"; 
-			System.out.println(error); 
-			return error;
+	public boolean setValue(MMObjectNode node,String field) {
+		System.out.println("PRE FIELD="+field);
+		if (field.equals("end")) {
+			int pos=node.getIntValue("pos");
+			int end=node.getIntValue("end");
+			if (end!=-1) node.setValue("length",(end-pos));
+		} else if (field.equals("pos")) {
+			int pos=node.getIntValue("pos");
+			int end=node.getIntValue("end");
+			if (end!=-1) node.setValue("length",(end-pos));
+		} else if (field.equals("length")) {
+			// extra check needed to make sure we don't create a loop !
+			int pos=node.getIntValue("pos");
+			int end=node.getIntValue("end");
+			int len=node.getIntValue("length");
+			System.out.println("P="+pos+" E="+end+" L="+len);
+			if ((end-pos)!=len) node.setValue("end",pos+len);
 		}
+		return(true);
 	}
-
 }
