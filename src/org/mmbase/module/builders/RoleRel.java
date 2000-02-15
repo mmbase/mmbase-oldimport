9615d4b42c01a5d1bf7bd4e9a4118bcf4ccb9acd
wwwtech <wwwtech@91430c5f-d668-0410-9e7d-dd663366dcfd>
2000-02-15 23:40:15 +0000

    New rel type
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@26 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/module/builders/RoleRel.java
@@ -0,0 +1,48 @@
+/*
+
+VPRO (C)
+
+This source file is part of mmbase and is (c) by VPRO until it is being
+placed under opensource. This is a private copy ONLY to be used by the
+MMBase partners.
+
+*/
+package org.mmbase.module.builders;
+
+import java.util.*;
+import java.sql.*;
+
+import org.mmbase.module.database.*;
+import org.mmbase.module.corebuilders.InsRel;
+import org.mmbase.module.corebuilders.RelDef;
+import org.mmbase.module.core.*;
+import org.mmbase.util.*;
+
+/**
+ * @author Rico Jansen
+ */
+public class RoleRel extends InsRel {
+
+	public RoleRel() {
+	}
+	
+	/**
+	* setDefaults for a node
+	*/
+	int relnumber=-1;
+
+	public void setDefaults(MMObjectNode node) {
+		if (relnumber==-1) {
+			RelDef bul=(RelDef)mmb.getMMObject("reldef");
+			if (bul!=null) {
+				relnumber=bul.getGuessedByName(tableName);
+				if (relnumber==-1) System.out.println("RoleRel-> Can not guess name");
+			} else {
+				System.out.println("RoleRel-> Can not reach RelDef");
+			}
+		}
+		node.setValue("rnumber",relnumber);
+	}
+
+
+}
