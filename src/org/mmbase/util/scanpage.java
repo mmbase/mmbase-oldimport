14ec228f0ae9cdd682e22c7a4f7606240dd29d60
wwwtech <wwwtech@91430c5f-d668-0410-9e7d-dd663366dcfd>
2000-02-25 14:54:10 +0000

    Rico: added several info methods
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@88 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/util/scanpage.java
+++ src/org/mmbase/util/scanpage.java
@@ -25,6 +25,7 @@ import org.mmbase.module.*;
  * because we want extend the model of offline page generation.
  *
  * @author Daniel Ockeloen
+ * @version $Id: scanpage.java,v 1.4 2000-02-25 14:54:10 wwwtech Exp $
  */
 public class scanpage {
 	public ProcessorInterface processor;
@@ -120,5 +121,164 @@ public class scanpage {
 	public String getSessionName() {
 		return(sname);
 	}
-}
 
+//  ------------------------------------------------------------------------------------------------------------
+
+	/**
+	* return page URL and parameters
+	*/
+	public String getUrl()
+	{
+		return req.getRequestURI()+"?"+req.getQueryString();
+	}
+
+
+//  ------------------------------------------------------------------------------------------------------------
+
+    private static  boolean     isForVPRO        = true;                // is this class used by vpro or others
+    private static  String      VPRODomain       = "145.58";            // well not quite, but does the trick :)
+    private static  String      VPROProxyName    = "vpro6d.vpro.nl";    // name of proxyserver
+    private static  String      VPROProxyAddress = "145.58.172.6";      // address of proxyserver
+
+    // methods
+    // -------
+
+    /**
+    *
+    * uses      : VPROProxyName, VPROProxyAddress, VPRODomain
+    */
+    public boolean isInternalVPROAddress()
+    {
+        boolean intern  = false;
+
+           String  ip      = req.getRemoteAddr();
+
+            // computers within vpro domain, use *.vpro.nl as server, instead *.omroep.nl
+            // --------------------------------------------------------------------------
+
+            if( ip != null && !ip.equals(""))
+            {
+                // is address from proxy?
+                // ----------------------
+                if( ip.indexOf( VPROProxyName )!= -1  || ip.indexOf( VPROProxyAddress )!= -1 )
+                {
+                    // positive on proxy, get real ip
+                    // ------------------------------
+                    ip = req.getHeader("X-Forwarded-For");
+
+                    // come from internal host?
+                    // ------------------------
+                    if( ip != null && !ip.equals("") && ip.indexOf( VPRODomain ) != -1 )
+                        intern = true;
+                }
+                else
+                    // no proxy, this is the real thing
+                    // --------------------------------
+                    if( ip.indexOf("145.58") != -1 )
+                        intern = true;
+            }
+
+        return intern;
+    }
+
+    /**
+    * Extract hostname from scanpage, get address and determine the proxies between it.
+    * Needed to determine if user comes from internal or external host, because
+    * we use two streaming servers, one for external users and one for internal users.
+    *
+    * input     : scanpage sp, contains hostname as ipaddress
+    * output    : String "clientproxy.clientside.com->dialin07.clientside.com"
+    *
+    * uses      : VPROProxyName, VPROProxyAddress, VPRODomain
+    */
+    public String getAddress()
+    {
+        String  result      = null;
+        boolean fromProxy   = false;
+        String  addr        = req.getRemoteHost();
+
+        if( addr != null && !addr.equals("") )
+        {
+                // from proxy ?
+                // ------------
+
+                if( addr.indexOf( VPROProxyName ) != -1 || addr.indexOf( VPROProxyAddress ) != -1 )
+                {
+                    // get real address
+                    // ----------------
+
+                    fromProxy = true;
+                    addr = req.getHeader("X-Forwarded-For");
+                    if( addr != null && !addr.equals("") )
+                        result = addr;
+                }
+                else
+                    result = addr;
+            }
+
+            result = getHostNames( addr );
+            if( fromProxy )
+                result = "zen.vpro.nl->" + result;
+
+        return result;
+    }
+
+    /**
+    *
+    */
+    private String getHostNames( String host )
+    {
+        String result   = null;
+        String hn       = null;
+
+        // comes client from his own proxy?
+        // --------------------------------
+        if( host.indexOf(",") != -1 )
+        {
+            int pos;
+
+            // filter and display the clientproxies
+            // ------------------------------------
+            while( (pos = host.indexOf(",")) != -1 )
+            {
+                hn = host.substring( 0, pos );
+                host = host.substring( pos + 2 );
+                if( result == null )
+                    result  = getHostName( hn );
+                else
+                    result += "->" + getHostName( hn );
+            }
+            // which results in "proxy.clientside.com->dailin07.clientside.com"
+            // ----------------------------------------------------------------
+        }
+        else
+            result = getHostName( host );
+
+        return result;
+    }
+
+    /**
+    *
+    */
+    private String getHostName( String hostname )
+    {
+        // if hostname == ipaddress, try to get a hostname for it
+        // ------------------------------------------------------
+
+        String hn = null;
+        if( hostname != null && !hostname.equals(""))
+        {
+            try
+            {
+                hn = InetAddress.getByName( hostname ).getHostName();
+            }
+            catch( UnknownHostException e )
+            {
+                hn = hostname;
+            }
+        }
+        else
+            hn = hostname;
+        return hn;
+    }
+}
