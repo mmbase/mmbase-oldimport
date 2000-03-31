441613fcb0f7c79f3473661da16e180223bb21db
wwwtech <wwwtech@91430c5f-d668-0410-9e7d-dd663366dcfd>
2000-03-31 09:33:45 +0000

    Wilbert Add exception class for syntax errors during parse of multilevel
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@221 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ applications/scan/src/org/mmbase/module/gui/html/MultiLevelParseException.java
@@ -0,0 +1,16 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.module.gui.html;
+
+/**
+ * This exception gets thrown when a Syntax Error occures in a Multi Level Listing.
+ */
+public class MultiLevelParseException extends ParseException {		public MultiLevelParseException(String msg) {		super(msg);	}
+}
