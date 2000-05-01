2ed61140b47046cb8f6f014e968c171a07911b9f
wwwtech <wwwtech@91430c5f-d668-0410-9e7d-dd663366dcfd>
2000-05-01 21:00:23 +0000

    daniel: new Module.java with XML install xerces.jar
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@309 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/Module.java
+++ src/org/mmbase/module/Module.java
@@ -20,13 +20,17 @@ import javax.servlet.http.*;
 import org.mmbase.util.*;
 import org.mmbase.module.core.*;
 
+//XercesParser
+import org.apache.xerces.parsers.*;
+import org.xml.sax.*;
+
 /**
  * Module , the wrapper for the modules.
  *
  * @author Rico Jansen
  * @author Rob Vermeulen (securitypart)
  *
- * @version $Revision: 1.8 $ $Date: 2000-05-01 20:59:04 $
+ * @version $Revision: 1.9 $ $Date: 2000-05-01 20:59:39 $
  */
 public abstract class Module {
 
@@ -254,24 +258,65 @@ public abstract class Module {
 
 	public static synchronized final Hashtable loadModulesFromDisk() {
 		Class newclass;
+		XMLProperties xmlReader=null;
+		SAXParser parser=null;
+		
+		// SAXParser, if this is gonna used by more classes 
+		// it must not be created here but somewhere else (higher?)
+		// and it must be possible to use another parser
+		try {
+	        	parser = new SAXParser();
+			// Telling the parser it must not use some features
+			// we're not using right now as dtd-validation and namespaces
+			try {
+			    parser.setFeature("http://xml.org/sax/features/validation",false);
+			    parser.setFeature("http://xml.org/sax/features/namespaces",false);
+			} catch (SAXNotSupportedException ex) {
+       	             debug("loadModulesFromDisk(): failed because parser didn't support feature");
+			    ex.printStackTrace();
+			} catch (SAXNotRecognizedException ex) {
+       	             debug("loadModulesFromDisk(): failed because parser didn't recognized feature");
+			    ex.printStackTrace();
+			}
+			//create new ContentHandler and let the parser use it
+			xmlReader = new XMLProperties();
+			parser.setContentHandler(xmlReader);
+		} catch(Exception e) {
+		}
 
-		debug("loadModulesFromDisk(): mmbase.config="+System.getProperty("mmbase.config"));
+		if (debug) debug("loadModulesFromDisk(): mmbase.config="+System.getProperty("mmbase.config"));
 		mmbaseconfig=System.getProperty("mmbase.config");
 		MMBaseContext.setConfigPath(mmbaseconfig);
-
+		
 		// the container for the started modules
 		Hashtable results=new Hashtable();
-	
-		// get us a propertie reader	
+
+		
+		// get us a (normal) propertie reader	
 		ExtendedProperties Reader=new ExtendedProperties();
+        Hashtable mods = null;		
 
 		// load the properties file of this server
-		String filename=mmbaseconfig+"/modules.properties";
+		String filename=mmbaseconfig+"/modules";
 		filename=filename.replace('/',(System.getProperty("file.separator")).charAt(0));
 		filename=filename.replace('\\',(System.getProperty("file.separator")).charAt(0));
 
+        //check if there's a xml-configuration file
+		if (parser!=null && (new File(filename+".xml")).exists()) {
+		    filename = filename + ".xml";
+			//parse the configuration file
+			try {   
+			    parser.parse(new InputSource(filename));
+			    mods = xmlReader.getProperties();
+            } catch(Exception f) {
+				f.printStackTrace();
+			}
+		} else {
+		    filename = filename + ".properties";
+		    mods = Reader.readProperties(filename);
+		}
 
-		Hashtable mods = Reader.readProperties(filename);
+debug("mods =" + mods.toString());
 
 		// oke try loading all these modules and start em up
 		for (Enumeration e=mods.keys();e.hasMoreElements();) {
@@ -286,22 +331,33 @@ public abstract class Module {
 				Object mod = newclass.newInstance();
 				if (mod!=null) {
 					results.put(key,mod);
-	
+					Hashtable modprops = null;
+
 					// try to load the properties that are defined for this module	
-					filename=mmbaseconfig+"/modules/"+key+".properties";
+					filename=mmbaseconfig+"/modules/"+key;
 					filename=filename.replace('/',(System.getProperty("file.separator")).charAt(0));
 					filename=filename.replace('\\',(System.getProperty("file.separator")).charAt(0));
 
 					// extra check to load propertie files from weird places (security reasons for example)
 					String tmp=System.getProperty("mmbase.mod_"+key);
 					if (tmp!=null) {
-						debug("Reading "+key+" mod file from : "+tmp);
+						if (debug) debug("Reading "+key+" mod file from : "+tmp);
 						filename=tmp;
 					}	
-					Hashtable modprops = Reader.readProperties(filename);
+
+					//check if there's a xml-configuration file
+					if (parser!=null && (new File(filename+".xml")).exists()) {
+						filename = filename + ".xml";
+						//parse the configuration file   
+						parser.parse(new InputSource(filename));
+						modprops = xmlReader.getProperties();
+					 } else {
+						filename = filename + ".properties";
+					    modprops = Reader.readProperties(filename);
+					 }
 					//debug("loadModulesFromDisk(): MOD "+key+" "+modprops);
 					if (modprops!=null) {
-						((Module)mod).properties=modprops;	
+						((Module)mod).properties=modprops;
 					}
 				}
 			} catch(Exception f) {
@@ -340,6 +396,8 @@ public abstract class Module {
 	}
 
 	public static final Object getModule(String name) {
+		// bring request to lowercase
+		name=name.toLowerCase();
 		// are the modules loaded yet ? if not load them
 		if (modules==null) {
 			debug("getModule("+name+"): Modules not loaded, loading them..");
