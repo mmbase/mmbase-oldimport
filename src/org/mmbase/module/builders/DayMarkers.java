0f84490f3958a79f36ede15afab80e44ee5aea05
wwwtech <wwwtech@91430c5f-d668-0410-9e7d-dd663366dcfd>
2000-05-29 16:56:48 +0000

    Rico: fixed some bugs and added extra functions
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@389 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/builders/DayMarkers.java
+++ src/org/mmbase/module/builders/DayMarkers.java
@@ -95,30 +95,123 @@ public class DayMarkers extends MMObjectBuilder {
 
 	public int getDayCountAge(int daysold) {
 		int wday=day-daysold;
+		return(getDayCount(wday));
+	}
+
+	public int getDayCount(int wday) {
 		Integer result=(Integer)daycache.get(new Integer(wday));
 		if (result!=null) {
 			return(result.intValue());
 		}
 		if (mmb==null) return(-1);
-		try {
-			MultiConnection con=mmb.getConnection();
-			if (con==null) return(-1);
-			Statement stmt=con.createStatement();
-			ResultSet rs=stmt.executeQuery("select mark from "+mmb.baseName+"_daymarks where daycount="+(wday));
-			if (rs.next()) {
-				int tmp=rs.getInt(1);
-				daycache.put(new Integer(wday),new Integer(tmp));
+		if (wday<=day) {
+			try {
+				MultiConnection con=mmb.getConnection();
+				if (con==null) return(-1);
+				Statement stmt=con.createStatement();
+				ResultSet rs=stmt.executeQuery("select mark from "+mmb.baseName+"_daymarks where daycount="+(wday));
+				if (rs.next()) {
+					int tmp=rs.getInt(1);
+					daycache.put(new Integer(wday),new Integer(tmp));
+					stmt.close();
+					con.close();
+					return(tmp);
+				}
 				stmt.close();
 				con.close();
-				return(tmp);
+			} catch(Exception e) {
+				daycache.put(new Integer(wday),new Integer(1));
+				return(1);
 			}
-			stmt.close();
-			con.close();
-		} catch(Exception e) {
-			daycache.put(new Integer(wday),new Integer(1));
-			return(1);
+		} else {
+			return(Integer.MAX_VALUE);
 		}
 		daycache.put(new Integer(wday),new Integer(1));
 		return(1);
 	}
+
+
+    public String replace(scanpage sp, StringTokenizer command) {
+		String rtn="";
+		int ival;
+		if (command.hasMoreTokens()) {
+            String token=command.nextToken();
+            if (token.equals("COUNT")) {
+				ival=fetchIntValue(command);
+				rtn=""+getDayCount(ival);
+			} else if (token.equals("COUNTAGE")) {
+				ival=fetchIntValue(command);
+				rtn=""+getDayCountAge(ival);
+			} else if (token.equals("COUNTMONTH")) {
+				ival=fetchIntValue(command);
+				rtn=""+getDayCount(getDayCountMonth(ival));
+			} else if (token.equals("COUNTNEXTMONTH")) {
+				ival=fetchIntValue(command);
+				rtn=""+getDayCount(getDayCountNextMonth(ival));
+			} else if (token.equals("COUNTPREVMONTH")) {
+				ival=fetchIntValue(command);
+				rtn=""+getDayCount(getDayCountPreviousMonth(ival));
+			} else if (token.equals("COUNTPREVDELTAMONTH")) {
+				ival=fetchIntValue(command);
+				int delta=0-fetchIntValue(command);
+				rtn=""+getDayCount(getDayCountDeltaMonth(ival,delta));
+			} else if (token.equals("COUNTNEXTDELTAMONTH")) {
+				ival=fetchIntValue(command);
+				int delta=fetchIntValue(command);
+				rtn=""+getDayCount(getDayCountDeltaMonth(ival,delta));
+			} else {
+				rtn="UnknownCommand";
+			}
+        }
+		return(rtn);
+    }
+
+	private int fetchIntValue(StringTokenizer command) {
+		String val;
+		int ival;
+		if (command.hasMoreTokens()) {
+			val=command.nextToken();
+		} else {
+			val="0";
+		}
+		try {
+			ival=Integer.parseInt(val);
+		} catch (NumberFormatException e) {
+			ival=0;
+		}
+		return(ival);
+	}
+
+	private Calendar getCalendarMonths(int months) {
+		int year,month;
+		year=months/12;
+		month=months%12;
+		GregorianCalendar cal=new GregorianCalendar();
+		cal.set(year+1970,month,1,0,0,0);
+		return(cal);
+	}
+
+	private int getDayCountMonth(int months) {
+		Calendar cal=getCalendarMonths(months);
+		return((int)((cal.getTime().getTime())/(24*3600*1000)));
+	}
+
+	private int getDayCountPreviousMonth(int months) {
+		Calendar cal=getCalendarMonths(months);
+		cal.add(Calendar.MONTH,-1);
+		return((int)((cal.getTime().getTime())/(24*3600*1000)));
+	}
+
+	private int getDayCountNextMonth(int months) {
+		Calendar cal=getCalendarMonths(months);
+		cal.add(Calendar.MONTH,1);
+		return((int)((cal.getTime().getTime())/(24*3600*1000)));
+	}
+
+	private int getDayCountDeltaMonth(int months,int delta) {
+		Calendar cal=getCalendarMonths(months);
+		cal.add(Calendar.MONTH,delta);
+		return((int)((cal.getTime().getTime())/(24*3600*1000)));
+	}
+
 }
