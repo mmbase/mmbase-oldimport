97537cc3d23dfcc734ba2a768c1b15c0973b4aab
wwwtech <wwwtech@91430c5f-d668-0410-9e7d-dd663366dcfd>
2000-06-15 15:54:34 +0000

    Rico: added support files for XML
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@441 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/module/database/support/dTypeInfo.java
@@ -0,0 +1,12 @@
+package org.mmbase.module.database.support;
+
+public class dTypeInfo {
+public String mmbaseType;
+public String dbType;
+public int minSize=-1;
+public int maxSize=-1;
+
+	public dTypeInfo() {
+	}
+
+}
---
+++ src/org/mmbase/module/database/support/dTypeInfos.java
@@ -0,0 +1,10 @@
+package org.mmbase.module.database.support;
+
+import java.util.Vector;
+
+public class dTypeInfos {
+public Vector maps=new Vector();
+
+	public dTypeInfos() {
+	}
+}
