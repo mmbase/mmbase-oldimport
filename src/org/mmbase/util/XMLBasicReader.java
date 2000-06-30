f9b5036366323e5a2a9b6252c59f43fc691f1ed3
case <case@91430c5f-d668-0410-9e7d-dd663366dcfd>
2000-06-30 05:17:58 +0000

    Base class for XML Readers, main utility function allows one to get an XML element by passing the tags of an element tree as a path.
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@551 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/util/XMLBasicReader.java
@@ -0,0 +1,152 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.util;
+
+import java.io.*;
+import java.util.*;
+
+import org.xml.sax.*;
+import org.apache.xerces.parsers.*;
+import org.w3c.dom.*;
+import org.w3c.dom.traversal.*;
+
+import org.mmbase.module.corebuilders.*;
+
+/**
+ * @author cjr@dds.nl
+ *
+ */
+public class XMLBasicReader  {
+
+    Document document;
+    DOMParser parser;
+
+    Hashtable languageList; // Hashtable from languagecode to Hashtables with dictionaries
+
+    String languagecode;  // code for language, e.g. 'nl'
+    Hashtable dictionary; // dictionary of mmbase term identifiers to translations in language
+
+
+    public XMLBasicReader(String filename) {
+
+        try {
+            parser = new DOMParser();
+            parser.setFeature("http://apache.org/xml/features/dom/defer-node-expansion", true);
+            parser.setFeature("http://apache.org/xml/features/continue-after-fatal-error", true);
+            parser.parse(filename);
+            document = parser.getDocument();
+
+	} catch(Exception e) {
+	    e.printStackTrace();
+	}
+    }
+    
+    /**
+     * @param path Dot-separated list of tags describing path from root element to requested element
+     * @return Leaf element of the path
+     */
+    public Element getElementByPath(String path) {
+	StringTokenizer st = new StringTokenizer(path,".");
+	Element e = document.getDocumentElement();
+	if (!st.hasMoreTokens()) {
+	    // faulty path
+	    System.err.println("no tokens in path");
+	} else {
+	    String root = st.nextToken();
+	    if (!e.getNodeName().equals(root)) {
+		// path should start with document root element
+		System.err.println("path ["+path+" doesn't start with root element");
+	    } else {
+		while (st.hasMoreTokens()) {
+		    String tag = st.nextToken();
+		    NodeList nl = e.getElementsByTagName(tag);
+		    if (nl.getLength()>0) {
+			e = (Element)nl.item(0);
+		    } else {
+			// Handle error!
+			System.err.println("No subelements found in "+e.getTagName());
+			return null;
+		    }
+		}
+	    }
+	    return e;
+	}
+	System.err.println("failed miserably");
+	return null;
+    }
+
+    /**
+     * @param e Element
+     * @return Text value of element
+     */
+    public String getElementValue(Element e) {
+	if (e == null) {
+	    return "";
+	} else {
+	    NodeList nl = e.getChildNodes();
+	    for (int i=0;i<nl.getLength();i++) {
+		Node n = nl.item(i);
+		if (n.getNodeType() == n.TEXT_NODE) {
+		    return n.getNodeValue();
+		}
+	    }
+	    return "";
+	}
+    }
+    
+    /**
+     * @param e Element
+     * @return Tag name of the element
+     */
+    public String getElementName(Element e) {
+	return e.getTagName();
+    }
+
+    /**
+     * @param e Element
+     * @param attr Attribute name
+     * @return Value of attribute
+     */
+    public String getElementAttributeValue(Element e, String attr) {
+	Node n = e.getAttributes().getNamedItem(attr);
+	// XXX Add errorchecking
+	return n.getNodeValue();
+    }
+
+    /**
+     * @param e Element
+     * @return Enumeration of child elements
+     */
+    public Enumeration getChildElements(Element e) {
+	Vector v = new Vector();
+	NodeList nl = e.getChildNodes();
+	for (int i=0;i<nl.getLength();i++) {
+	    Node n = nl.item(i);
+	    if (n.getNodeType() == n.ELEMENT_NODE) {
+		v.addElement(n);
+	    }
+	}
+	return v.elements();
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
