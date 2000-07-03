f6beaac04aa9a2d1aa8b794561d9eff4475d0b22
daniel <daniel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2000-07-03 08:46:03 +0000

    Daniel.. (see Application project on mmbase.org soon)
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@558 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/util/XMLApplicationReader.java
@@ -0,0 +1,265 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.util;
+
+import java.io.*;
+import java.util.*;
+
+import org.xml.sax.*;
+import org.apache.xerces.parsers.*;
+import org.w3c.dom.*;
+import org.w3c.dom.traversal.*;
+
+import org.mmbase.module.corebuilders.*;
+import org.mmbase.module.database.support.*;
+
+/**
+*/
+public class XMLApplicationReader  {
+
+    Document document;
+    DOMParser parser;
+
+
+    public XMLApplicationReader(String filename) {
+        try {
+            parser = new DOMParser();
+            parser.setFeature("http://apache.org/xml/features/dom/defer-node-expansion", true);
+            parser.setFeature("http://apache.org/xml/features/continue-after-fatal-error", true);
+            //Errors errors = new Errors();
+            //parser.setErrorHandler(errors);
+            parser.parse(filename);
+            document = parser.getDocument();
+
+	    System.out.println("*** START XML APPLICATION READER FOR : "+filename);	
+	    System.out.println("Application name="+getApplicationName());	
+	    System.out.println("Application version="+getApplicationVersion());	
+	    System.out.println("Needed builders="+getNeededBuilders());	
+	    System.out.println("Needed reldefs="+getNeededRelDefs());	
+	    System.out.println("Allowed relations="+getAllowedRelations());	
+	    System.out.println("DataSources="+getDataSources());	
+	    System.out.println("RelationSources="+getRelationSources());	
+	    System.out.println("*** END XML APPLICATION READER FOR : "+filename);	
+	} catch(Exception e) {
+	    e.printStackTrace();
+	}
+    }
+
+    /**
+    * get the name of this application
+    */
+    public String getApplicationName() {
+	Node n1=document.getFirstChild();
+	if (n1!=null) {
+		NamedNodeMap nm=n1.getAttributes();
+		if (nm!=null) {
+			Node n2=nm.getNamedItem("name");
+			String name=n2.getNodeValue();
+			return(name);
+		}
+	}
+	return(null);
+    }
+
+
+    /**
+    * get the version of this application
+    */
+    public int getApplicationVersion() {
+	Node n1=document.getFirstChild();
+	if (n1!=null) {
+		NamedNodeMap nm=n1.getAttributes();
+		if (nm!=null) {
+			Node n2=nm.getNamedItem("version");
+			String tmp=n2.getNodeValue();
+			try {
+				int version=Integer.parseInt(tmp);
+				return(version);
+			} catch (Exception e) {
+				return(-1);
+			}
+		}
+	}
+	return(-1);
+    }
+
+
+
+
+    /**
+    * getNeededBuilders for this application
+    */
+    public Vector getNeededBuilders() {
+	Vector results=new Vector();
+	Node n1=document.getFirstChild();
+	if (n1!=null) {
+		Node n2=n1.getFirstChild();
+		while (n2!=null) {
+			if (n2.getNodeName().equals("neededbuilderlist")) {
+				Node n3=n2.getFirstChild();
+				while (n3!=null) {
+					if (n3.getNodeName().equals("builder")) {
+						Hashtable bset=new Hashtable();	
+						Node n5=n3.getFirstChild();
+						bset.put("name",n5.getNodeValue());
+						NamedNodeMap nm=n1.getAttributes();
+						if (nm!=null) {
+							Node n4=nm.getNamedItem("maintainer");
+							if (n4!=null) bset.put("maintainer",n4.getNodeValue());
+							n4=nm.getNamedItem("version");
+							if (n4!=null) bset.put("version",n4.getNodeValue());
+						}
+						results.addElement(bset);
+					}
+					n3=n3.getNextSibling();
+				}
+			}
+			n2=n2.getNextSibling();
+		}
+	}
+	return(results);
+    }
+
+
+    /**
+    * getNeededRelDefs for this application
+    */
+    public Vector getNeededRelDefs() {
+	Vector results=new Vector();
+	Node n1=document.getFirstChild();
+	if (n1!=null) {
+		Node n2=n1.getFirstChild();
+		while (n2!=null) {
+			if (n2.getNodeName().equals("neededreldeflist")) {
+				Node n3=n2.getFirstChild();
+				while (n3!=null) {
+					if (n3.getNodeName().equals("reldef")) {
+						Hashtable bset=new Hashtable();	
+						NamedNodeMap nm=n3.getAttributes();
+						if (nm!=null) {
+							Node n4=nm.getNamedItem("source");
+							if (n4!=null) bset.put("source",n4.getNodeValue());
+							n4=nm.getNamedItem("target");
+							if (n4!=null) bset.put("target",n4.getNodeValue());
+							n4=nm.getNamedItem("direction");
+							if (n4!=null) bset.put("direction",n4.getNodeValue());
+							n4=nm.getNamedItem("guisourcename");
+							if (n4!=null) bset.put("guisourcename",n4.getNodeValue());
+							n4=nm.getNamedItem("guitargetname");
+							if (n4!=null) bset.put("guitargetname",n4.getNodeValue());
+						}
+						results.addElement(bset);
+					}
+					n3=n3.getNextSibling();
+				}
+			}
+			n2=n2.getNextSibling();
+		}
+	}
+	return(results);
+    }
+
+
+    /**
+    * getNeededBuilders for this application
+    */
+    public Vector getAllowedRelations() {
+	Vector results=new Vector();
+	Node n1=document.getFirstChild();
+	if (n1!=null) {
+		Node n2=n1.getFirstChild();
+		while (n2!=null) {
+			if (n2.getNodeName().equals("allowedrelationlist")) {
+				Node n3=n2.getFirstChild();
+				while (n3!=null) {
+					if (n3.getNodeName().equals("relation")) {
+						Hashtable bset=new Hashtable();	
+						NamedNodeMap nm=n3.getAttributes();
+						if (nm!=null) {
+							Node n4=nm.getNamedItem("from");
+							if (n4!=null) bset.put("from",n4.getNodeValue());
+							n4=nm.getNamedItem("to");
+							if (n4!=null) bset.put("to",n4.getNodeValue());
+							n4=nm.getNamedItem("type");
+							if (n4!=null) bset.put("type",n4.getNodeValue());
+						}
+						results.addElement(bset);
+					}
+					n3=n3.getNextSibling();
+				}
+			}
+			n2=n2.getNextSibling();
+		}
+	}
+	return(results);
+    }
+
+
+    /**
+    * datasources attached to this application
+    */
+    public Vector getDataSources() {
+	Vector results=new Vector();
+	Node n1=document.getFirstChild();
+	if (n1!=null) {
+		Node n2=n1.getFirstChild();
+		while (n2!=null) {
+			if (n2.getNodeName().equals("datasourcelist")) {
+				Node n3=n2.getFirstChild();
+				while (n3!=null) {
+					if (n3.getNodeName().equals("datasource")) {
+						Hashtable bset=new Hashtable();	
+						NamedNodeMap nm=n3.getAttributes();
+						if (nm!=null) {
+							Node n4=nm.getNamedItem("path");
+							if (n4!=null) bset.put("path",n4.getNodeValue());
+						}
+						results.addElement(bset);
+					}
+					n3=n3.getNextSibling();
+				}
+			}
+			n2=n2.getNextSibling();
+		}
+	}
+	return(results);
+    }
+
+
+    /**
+    * datasources attached to this application
+    */
+    public Vector getRelationSources() {
+	Vector results=new Vector();
+	Node n1=document.getFirstChild();
+	if (n1!=null) {
+		Node n2=n1.getFirstChild();
+		while (n2!=null) {
+			if (n2.getNodeName().equals("relationsourcelist")) {
+				Node n3=n2.getFirstChild();
+				while (n3!=null) {
+					if (n3.getNodeName().equals("relationsource")) {
+						Hashtable bset=new Hashtable();	
+						NamedNodeMap nm=n3.getAttributes();
+						if (nm!=null) {
+							Node n4=nm.getNamedItem("path");
+							if (n4!=null) bset.put("path",n4.getNodeValue());
+						}
+						results.addElement(bset);
+					}
+					n3=n3.getNextSibling();
+				}
+			}
+			n2=n2.getNextSibling();
+		}
+	}
+	return(results);
+    }
+}
---
+++ src/org/mmbase/util/XMLNodeReader.java
@@ -0,0 +1,149 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.util;
+
+import java.io.*;
+import java.util.*;
+
+import org.xml.sax.*;
+import org.apache.xerces.parsers.*;
+import org.w3c.dom.*;
+import org.w3c.dom.traversal.*;
+
+import org.mmbase.module.core.*;
+import org.mmbase.module.corebuilders.*;
+import org.mmbase.module.database.support.*;
+
+/**
+*/
+public class XMLNodeReader  {
+
+    Document document;
+    DOMParser parser;
+
+
+    public XMLNodeReader(String filename,MMBase mmbase) {
+        try {
+            parser = new DOMParser();
+            parser.setFeature("http://apache.org/xml/features/dom/defer-node-expansion", true);
+            parser.setFeature("http://apache.org/xml/features/continue-after-fatal-error", true);
+            //Errors errors = new Errors();
+            //parser.setErrorHandler(errors);
+            parser.parse(filename);
+            document = parser.getDocument();
+
+		/*
+	    System.out.println("*** START XML APPLICATION READER FOR : "+filename);	
+	    System.out.println("ExportSource="+getExportSource());	
+	    System.out.println("TimeStamp="+getTimeStamp());	
+	    System.out.println("Nodes nodes="+getNodes(mmbase));	
+	    System.out.println("*** END XML APPLICATION READER FOR : "+filename);	
+		*/
+	} catch(Exception e) {
+	    e.printStackTrace();
+	}
+    }
+
+
+    /**
+    * get the name of this application
+    */
+    public String getExportSource() {
+	Vector nodes=new Vector();
+	Node n1=document.getFirstChild();
+	while (n1!=null) {
+		NamedNodeMap nm=n1.getAttributes();
+		if (nm!=null) {
+			Node n2=nm.getNamedItem("exportsource");
+			return(n2.getNodeValue());
+		}
+	}
+	return(null);
+    }
+
+
+    /**
+    * get the name of this application
+    */
+    public int getTimeStamp() {
+	Vector nodes=new Vector();
+	Node n1=document.getFirstChild();
+	while (n1!=null) {
+		NamedNodeMap nm=n1.getAttributes();
+		if (nm!=null) {
+			Node n2=nm.getNamedItem("timestamp");
+			int times=DateSupport.parsedatetime(n2.getNodeValue());
+			return(times);
+		}
+	}
+	return(-1);
+    }
+
+    /**
+    * get the name of this application
+    */
+    public Vector getNodes(MMBase mmbase) {
+	Vector nodes=new Vector();
+	Node n1=document.getFirstChild();
+	while (n1!=null) {
+		MMObjectBuilder bul=mmbase.getMMObject(n1.getNodeName());
+		if (bul!=null) {
+			Node n2=n1.getFirstChild();
+			while (n2!=null) {
+				if (n2.getNodeName().equals("node")) {
+					NamedNodeMap nm=n2.getAttributes();
+					if (nm!=null) {
+						Node n4=nm.getNamedItem("owner");
+						MMObjectNode newnode=bul.getNewNode(n4.getNodeValue());
+						n4=nm.getNamedItem("number");
+						try {
+							int num=Integer.parseInt(n4.getNodeValue());
+			
+							newnode.setValue("number",num);
+						} catch(Exception e) {}
+						Node n5=n2.getFirstChild();
+						while (n5!=null) {
+							String key=n5.getNodeName();
+							if (!key.equals("#text")) {
+								Node n6=n5.getFirstChild();
+								String value=n6.getNodeValue();
+								String type=bul.getDBType(key);
+								if (type!=null) {
+									if (type.equals("varchar")) {
+										newnode.setValue(key,value);
+									} else if (type.equals("integer")) {
+										try { 
+											newnode.setValue(key,Integer.parseInt(value));
+										} catch(Exception e) {
+											newnode.setValue(key,-1);
+										}
+									} else { 
+										System.out.println("XMLNodeReader node error : "+key+" "+value+" "+type);
+									}
+								}
+							}
+							n5=n5.getNextSibling();
+						}
+						nodes.addElement(newnode);
+					}
+				}
+				n2=n2.getNextSibling();
+			}
+		} else {
+			System.out.println("XMLNodeReader can't access builder : "+bul);
+		}
+		n1=n1.getNextSibling();
+	}
+	return(nodes);
+    }
+
+
+
+}
