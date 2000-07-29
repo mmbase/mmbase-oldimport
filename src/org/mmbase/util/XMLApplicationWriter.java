4e79c95b35b94ea69ed01c64d78235c6bfbd5012
daniel <daniel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2000-07-29 21:12:45 +0000

    version 0.2
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@742 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/util/XMLApplicationWriter.java
+++ src/org/mmbase/util/XMLApplicationWriter.java
@@ -31,19 +31,34 @@ public class XMLApplicationWriter  {
 	boolean deploy=app.getApplicationAutoDeploy();
 	
 	String body="<application name=\""+name+"\" maintainer=\""+maintainer+"\" version=\""+version+"\" auto-deploy=\""+deploy+"\">\n";
-	// status
-	body+="\t<neededbuilderlist>\n";
-	body+=getNeededBuilders(app);
-	body+="\t</neededbuilderlist>\n\n";
 
-	System.out.println("BODY="+body);
+	// write the needed builders
+	body+=getNeededBuilders(app);
+	
+	// write the needed reldefs
+	body+=getNeededRelDefs(app);
+
+	// write the allowed relations
+	body+=getAllowedRelations(app);
+
+	// write the datasources
+	body+=getDataSources(app);
+
+	// write the datasources
+	body+=getDataSources(app);
+
+	// write the relationsources
+	body+=getRelationSources(app);
+
+	// close the application file
+	body+="</application>\n";
+	saveFile(targetpath+"/"+app.getApplicationName()+".xml",body);
 	return(true);
     }
 
     static String getNeededBuilders(XMLApplicationReader app) {
-	String body="";
+	String body="\t<neededbuilderlist>\n";
 	Vector builders=app.getNeededBuilders();
-	System.out.println("builders="+builders.size());
 	for (Enumeration e=builders.elements();e.hasMoreElements();) {
 		Hashtable bset=(Hashtable)e.nextElement();
 		String name=(String)bset.get("name");
@@ -51,6 +66,66 @@ public class XMLApplicationWriter  {
 		String version=(String)bset.get("version");
 		body+="\t\t<builder maintainer=\""+maintainer+"\" version=\""+version+"\">"+name+"</builder>\n";
 	}
+	body+="\t</neededbuilderlist>\n\n";
+	return(body);	
+    }
+
+
+    static String getNeededRelDefs(XMLApplicationReader app) {
+	String body="\t<neededreldeflist>\n";
+	Vector builders=app.getNeededRelDefs();
+	for (Enumeration e=builders.elements();e.hasMoreElements();) {
+		Hashtable bset=(Hashtable)e.nextElement();
+		String source=(String)bset.get("source");
+		String target=(String)bset.get("target");
+		String dir=(String)bset.get("direction");
+		String guisourcename=(String)bset.get("guisourcename");
+		String guitargetname=(String)bset.get("guitargetname");
+		body+="\t\t<reldef source=\""+source+"\" target=\""+target+"\" direction=\""+dir+"\" guisourcename=\""+guisourcename+"\" guitargetname=\""+guitargetname+"\" />\n";
+	}
+	body+="\t</neededreldeflist>\n\n";
+	return(body);	
+    }
+
+
+    static String getAllowedRelations(XMLApplicationReader app) {
+	String body="\t<allowedrelationlist>\n";
+	Vector builders=app.getAllowedRelations();
+	for (Enumeration e=builders.elements();e.hasMoreElements();) {
+		Hashtable bset=(Hashtable)e.nextElement();
+		String from=(String)bset.get("from");
+		String to=(String)bset.get("to");
+		String type=(String)bset.get("type");
+		body+="\t\t<relation from=\""+from+"\" to=\""+to+"\" type=\""+type+"\" />\n";
+	}
+	body+="\t</allowedrelationlist>\n\n";
+	return(body);	
+    }
+
+
+    static String getDataSources(XMLApplicationReader app) {
+	String body="\t<datasourcelist>\n";
+	Vector builders=app.getDataSources();
+	for (Enumeration e=builders.elements();e.hasMoreElements();) {
+		Hashtable bset=(Hashtable)e.nextElement();
+		String path=(String)bset.get("path");
+		body+="\t\t<datasource path=\""+path+"\" />\n";
+	}
+	body+="\t</datasourcelist>\n\n";
+
+	return(body);	
+    }
+
+
+    static String getRelationSources(XMLApplicationReader app) {
+	String body="\t<relationsourcelist>\n";
+	Vector builders=app.getRelationSources();
+	for (Enumeration e=builders.elements();e.hasMoreElements();) {
+		Hashtable bset=(Hashtable)e.nextElement();
+		String path=(String)bset.get("path");
+		body+="\t\t<relationsource path=\""+path+"\" />\n";
+	}
+	body+="\t</relationsourcelist>\n\n";
 	return(body);	
     }
 
