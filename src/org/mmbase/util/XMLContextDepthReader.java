2e6ee3cbac00980c6878f189d5d141e03afb54a3
daniel <daniel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2000-07-30 15:48:17 +0000

    first context reader
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@745 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/util/XMLContextDepthReader.java
@@ -0,0 +1,129 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.util;
+
+import java.io.*;
+import java.util.*;
+
+import org.xml.sax.*;
+import org.apache.xerces.parsers.*;
+import org.w3c.dom.*;
+import org.w3c.dom.traversal.*;
+
+import org.mmbase.module.core.*;
+import org.mmbase.module.corebuilders.*;
+import org.mmbase.module.database.support.*;
+
+/**
+*/
+public class XMLContextDepthReader  {
+
+    Document document;
+    DOMParser parser;
+
+
+    public XMLContextDepthReader(String filename) {
+        try {
+            parser = new DOMParser();
+            parser.setFeature("http://apache.org/xml/features/dom/defer-node-expansion", true);
+            parser.setFeature("http://apache.org/xml/features/continue-after-fatal-error", true);
+            //Errors errors = new Errors();
+            //parser.setErrorHandler(errors);
+            parser.parse(filename);
+            document = parser.getDocument();
+
+		/*
+	    System.out.println("*** START XML APPLICATION READER FOR : "+filename);	
+	    System.out.println("ExportSource="+getExportSource());	
+	    System.out.println("TimeStamp="+getTimeStamp());	
+	    System.out.println("Nodes nodes="+getNodes(mmbase));	
+	    System.out.println("*** END XML APPLICATION READER FOR : "+filename);	
+		*/
+	} catch(Exception e) {
+	    e.printStackTrace();
+	}
+    }
+
+
+
+    /**
+    */
+    public int getDepth() {
+	Node n1=document.getFirstChild();
+	if (n1!=null) {
+		Node n2=n1.getFirstChild();
+		while (n2!=null) {
+			if (n2.getNodeName().equals("depth")) {
+				Node n3=n2.getFirstChild();
+		    		String tmp=n3.getNodeValue();
+				try {
+			    		return(Integer.parseInt(tmp));
+				} catch(Exception e) {
+					return(-1);
+				}
+			
+			}
+			n2=n2.getNextSibling();
+		}
+	}
+	return(-1);
+    }
+
+
+
+    /**
+    */
+    public String getStartBuilder() {
+	Hashtable table=new Hashtable();
+	Node n1=document.getFirstChild();
+	if (n1!=null) {
+		Node n2=n1.getFirstChild();
+		while (n2!=null) {
+			if (n2.getNodeName().equals("startnode")) {
+				Node n3=n2.getFirstChild();
+				while (n3!=null) {
+					if (n3.getNodeName().equals("builder")) {
+						Node n4=n3.getFirstChild();
+		    				return(n4.getNodeValue());
+					}
+					n3=n3.getNextSibling();
+				}
+			}
+			n2=n2.getNextSibling();
+		}
+	}
+	return(null);
+    }
+
+
+    /**
+    */
+    public String getStartWhere() {
+	Hashtable table=new Hashtable();
+	Node n1=document.getFirstChild();
+	if (n1!=null) {
+		Node n2=n1.getFirstChild();
+		while (n2!=null) {
+			if (n2.getNodeName().equals("startnode")) {
+				Node n3=n2.getFirstChild();
+				while (n3!=null) {
+					if (n3.getNodeName().equals("where")) {
+						Node n4=n3.getFirstChild();
+		    				return(n4.getNodeValue());
+					}
+					n3=n3.getNextSibling();
+				}
+			}
+			n2=n2.getNextSibling();
+		}
+	}
+	return(null);
+    }
+}
