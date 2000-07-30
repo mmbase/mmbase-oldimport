b1c556e3e5c5d02cd09b70b8785159b4e35f7cd4
daniel <daniel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2000-07-30 15:47:40 +0000

    first context writer
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@744 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/util/XMLContextDepthWriter.java
@@ -0,0 +1,224 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.util;
+
+import java.io.*;
+import java.util.*;
+import org.mmbase.module.core.*;
+
+import org.mmbase.module.corebuilders.*;
+
+/**
+*/
+public class XMLContextDepthWriter  {
+
+    public static boolean writeContext(XMLApplicationReader app,XMLContextDepthReader capp,String targetpath,MMBase mmb) {
+
+	// first get the startnode we need to walk x depth
+	int startnode=getStartNode(capp,mmb);
+
+	// get depth
+	int depth=capp.getDepth();
+
+	// get valid builders to filter
+	Vector fb=getFilterBuilders(app.getNeededBuilders(),mmb.getTypeDef());	
+
+	// the trick is to get all nodes until depth x and filter them
+	Vector nodes=getSubNodes(startnode,0,depth,fb,new Vector(),mmb.getTypeDef());
+	System.out.println("Nodes size="+nodes.size());
+
+	// write DataSources
+	writeDataSources(app,nodes,targetpath,mmb);
+
+
+	// write relationSources
+	writeRelationSources(app,nodes,targetpath,mmb);
+	return(true);
+    }
+
+    static void writeDataSources(XMLApplicationReader app, Vector nodes, String targetpath,MMBase mmb) {
+	Enumeration res=app.getNeededBuilders().elements();
+	while (res.hasMoreElements()) {
+		Hashtable bset=(Hashtable)res.nextElement();
+		String name=(String)bset.get("name");
+		int type = mmb.getTypeDef().getIntValue(name);
+		MMObjectBuilder bul=mmb.getMMObject(name);
+		
+		// write the header
+		String body="<"+name+" exportsource=\"mmbase://127.0.0.1/install/b1\" timestamp=\"20000602143030\">\n";
+
+		// lets enum the nodes and filter all of this type
+		Enumeration nods=nodes.elements();
+		while (nods.hasMoreElements()) {
+			MMObjectNode node=bul.getNode(((Integer)nods.nextElement()).intValue());
+			if (type==node.getIntValue("otype")) {
+				int number=node.getIntValue("number");
+				String owner=node.getStringValue("owner");
+
+				// start the node
+				body+="\t<node number=\""+number+"\" owner=\""+owner+"\">\n";
+				// write the values of the node
+				Hashtable values=node.getValues();	
+				Enumeration nd=values.keys();
+				while (nd.hasMoreElements()) {
+					String key=(String)nd.nextElement();
+	
+					if (!key.equals("number") && !key.equals("owner") && !key.equals("otype") && !key.equals("CacheCount")) {
+						body+="\t\t<"+key+">"+node.getValue(key)+"</"+key+">\n";
+					}
+				}
+	
+				// end the node
+				body+="\t</node>\n\n";
+			}
+		}
+	
+		// write the footer	
+		body+="</"+name+">\n";
+		String filename=targetpath+"/"+app.getApplicationName()+"/"+name+".xml";
+		System.out.println("Writing DataSource="+filename);
+		saveFile(filename,body);
+	}
+   }	
+
+
+    static void writeRelationSources(XMLApplicationReader app, Vector nodes, String targetpath,MMBase mmb) {
+//	Enumeration res=app.getNeededBuilders().elements();
+//	while (res.hasMoreElements()) {
+//		Hashtable bset=(Hashtable)res.nextElement();
+
+
+
+		String name="insrel";
+		int type = mmb.getTypeDef().getIntValue(name);
+		MMObjectBuilder bul=mmb.getMMObject(name);
+		
+		// write the header
+		String body="<"+name+" exportsource=\"mmbase://127.0.0.1/install/b1\" timestamp=\"20000602143030\">\n";
+
+		// lets enum the nodes and filter all of this type
+		Enumeration nods=nodes.elements();
+		while (nods.hasMoreElements()) {
+			MMObjectNode node=bul.getNode(((Integer)nods.nextElement()).intValue());
+			if (type==node.getIntValue("otype")) {
+				int number=node.getIntValue("number");
+				String owner=node.getStringValue("owner");
+				int snumber=node.getIntValue("snumber");
+				int dnumber=node.getIntValue("dnumber");
+
+				// start the node
+				body+="\t<node number=\""+number+"\" owner=\""+owner+"\" snumber=\""+snumber+"\" dnumber=\""+dnumber+"\" rtype=\"related\">\n";
+				// write the values of the node
+				Hashtable values=node.getValues();	
+				Enumeration nd=values.keys();
+				while (nd.hasMoreElements()) {
+					String key=(String)nd.nextElement();
+	
+					if (!key.equals("number") && !key.equals("owner") && !key.equals("otype") && !key.equals("CacheCount") && !key.equals("snumber") && !key.equals("dnumber") && !key.equals("rnumber")) {
+						body+="\t\t<"+key+">"+node.getValue(key)+"</"+key+">\n";
+					}
+				}
+	
+				// end the node
+				body+="\t</node>\n\n";
+			}
+		}
+	
+		// write the footer	
+		body+="</"+name+">\n";
+		String filename=targetpath+"/"+app.getApplicationName()+"/"+name+".xml";
+		System.out.println("Writing RelationSource="+filename);
+		saveFile(filename,body);
+	//}
+   }	
+
+
+    static Vector getSubNodes(int startnode,int curdepth,int maxdepth,Vector fb,Vector nodes,MMObjectBuilder bul ) {
+	MMObjectNode node=bul.getNode(startnode);
+	if (node!=null) {
+		Integer number=new Integer(node.getIntValue("number"));
+		Integer type=new Integer(node.getIntValue("otype"));
+		if (fb.contains(type) && !nodes.contains(number)) nodes.addElement(number);
+		Enumeration rel=node.getRelations();
+		while (rel.hasMoreElements()) {
+			MMObjectNode node2=(MMObjectNode)rel.nextElement();
+			Integer number2=new Integer(node2.getIntValue("number"));
+			int snumber=node2.getIntValue("snumber");
+			int dnumber=node2.getIntValue("dnumber");
+
+			if (curdepth!=maxdepth) {
+				if (snumber==number.intValue()) {
+					// this is kinda tricky we need
+					// to get the related node to
+					// and check its type to be sure if
+					// we should add the relation
+					MMObjectNode node3=bul.getNode(dnumber);
+					type=new Integer(node3.getIntValue("otype"));
+					if (fb.contains(type) && !nodes.contains(number2)) nodes.addElement(number2);
+					nodes=getSubNodes(dnumber,curdepth+1,maxdepth,fb,nodes,bul);
+				} else {
+					MMObjectNode node3=bul.getNode(snumber);
+					type=new Integer(node3.getIntValue("otype"));
+					if (fb.contains(type) && !nodes.contains(number2)) nodes.addElement(number2);
+					nodes=getSubNodes(snumber,curdepth+1,maxdepth,fb,nodes,bul);
+				}
+			}
+		}
+	}
+	return(nodes);
+    }
+
+    static Vector getFilterBuilders(Vector filter,TypeDef bul) {
+	Vector results=new Vector();
+	Enumeration res=filter.elements();
+	while (res.hasMoreElements()) {
+		Hashtable bset=(Hashtable)res.nextElement();
+		String name=(String)bset.get("name");
+		int value=bul.getIntValue(name);
+		if (value!=-1) {
+			results.addElement(new Integer(value));
+		} else {
+			System.out.println("XMLContextDepthWriter -> can't get intvalue for : "+name);
+		}
+	}
+	return(results);
+    }
+
+
+    static int getStartNode(XMLContextDepthReader capp, MMBase mmb) {
+	String builder=capp.getStartBuilder();
+	String where=capp.getStartWhere();
+	MMObjectBuilder bul=mmb.getMMObject(builder);
+	if (bul!=null) {
+		Enumeration results=bul.search(where);	
+		if (results.hasMoreElements()) {
+			MMObjectNode node=(MMObjectNode)results.nextElement();
+			return(node.getIntValue("number"));
+		}
+	} else {
+		System.out.println("ContextDepthWriter-> can't find builder ("+builder+")");
+	}
+	return(-1);
+    }
+
+	static boolean saveFile(String filename,String value) {
+		File sfile = new File(filename);
+		try {
+			DataOutputStream scan = new DataOutputStream(new FileOutputStream(sfile));
+			scan.writeBytes(value);
+			scan.flush();
+			scan.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return(true);
+	}
+
+}
