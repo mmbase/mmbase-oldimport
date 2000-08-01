77140d82c0ad429d280741d397b52b367bcc7a5b
gerard <gerard@91430c5f-d668-0410-9e7d-dd663366dcfd>
2000-08-01 13:31:57 +0000

    gerard: added support classes for mmusers
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@766 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/util/PasswordGeneratorInterface.java
@@ -0,0 +1,8 @@
+//package nl.vpro.mmbase.util;
+package org.mmbase.util;
+
+
+public interface PasswordGeneratorInterface {
+	public String getPassword();
+	public String getPassword(String template);
+}
