0ac31cbebb96ebd715ba3c55e13abf0ae2681709
install <install@91430c5f-d668-0410-9e7d-dd663366dcfd>
2000-08-02 16:47:05 +0000

    Rob almost finished
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@779 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/IrcModule.java
+++ src/org/mmbase/module/IrcModule.java
@@ -24,8 +24,40 @@ public class IrcModule extends ProcessorModule implements CommunicationUserInter
 	public IrcModule() {}
 
 	public void receive( String msg ) {
+		StringBuffer sb = new StringBuffer(msg);
+		//debug( classname +":"+ msg );
+		msg=sb.substring(msg.indexOf(']')+1,msg.length());
 		debug( classname +":"+ msg );
 		if(msg.toLowerCase().indexOf("mmbeest")!=-1) com.sendPublic("Ahum, my name is MMBase not MMBeest");
+
+		try {
+		StringTokenizer st = new StringTokenizer(msg);
+		String name = st.nextToken(); 
+		if(name.equals("mmbase")) {
+			String number = st.nextToken();
+			String antwoord = "";
+			while (st.hasMoreTokens()) {
+				antwoord+=st.nextToken()+" ";
+			}
+			MMObjectNode answer = answers.getNewNode("irc");
+			answer.setValue("body",antwoord);
+			answer.insert("irc");
+			number2answer.put(number,answer);
+
+			//create relations
+			int rnumber = mmbase.InsRel.getGuessedNumber("related");
+			debug(""+new Integer(rnumber));
+			MMObjectNode question = (MMObjectNode)number2question.get(number);
+			mmbase.InsRel.insert("irc",answer.getIntValue(number),question.getIntValue(number),rnumber);
+			debug(""+new Integer(answer.getIntValue(number)));
+			debug(""+new Integer(question.getIntValue(number)));
+			
+		}
+		} catch (Exception e) {
+			System.out.println(e);
+ 			e.printStackTrace();
+
+		}
 	}
 
 	private void debug( String msg )
@@ -57,19 +89,59 @@ public class IrcModule extends ProcessorModule implements CommunicationUserInter
 	public boolean process(scanpage sp, Hashtable cmds,Hashtable vars) {
 		System.out.println("CMDS="+cmds);
 		System.out.println("VARS="+vars);
-		System.out.println("vraag="+vars.get("vraag"));
-		com.sendPublic((String)vars.get("vraag"));
-		MMObjectNode vraag = questions.getNewNode("irc");
-		vraag.setValue("body",vars.get("vraag"));
-		vraag.insert("irc");
-		String koekie = sp.session.getCookie();
-		String n = ""+new Integer(number++);
-		user2number.put(koekie,n);
-		number2question.put(n,vraag);
+	
+		if(cmds.containsKey("QUESTION")) {
+			System.out.println("vraag="+vars.get("vraag"));
+			MMObjectNode vraag = questions.getNewNode("irc");
+			vraag.setValue("body",vars.get("vraag"));
+			vraag.insert("irc");
+			String koekie = sp.session.getCookie();
+			String n = ""+new Integer(number++);
+			user2number.put(koekie,n);
+			number2question.put(n,vraag);
+			com.sendPublic(n+": " +vars.get("vraag"));
+		}
 		return(false);
 	}
 
 	public String replace(scanpage sp, String cmds) {
+		System.out.println("replace "+cmds);
+		if(cmds.equals("ANSWER")) {
+			try {
+				String number = (String)user2number.get(sp.session.getCookie());
+				MMObjectNode mmon = (MMObjectNode)number2answer.get(number);
+				String body = null;
+				body = (String)mmon.getValue("body");
+				return body;
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+		if(cmds.equals("QUESTION")) {
+			try {
+				String number = (String)user2number.get(sp.session.getCookie());
+				MMObjectNode mmon = (MMObjectNode)number2question.get(number);
+				String body = null;
+				body = (String)mmon.getValue("body");
+				return body;
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+		if(cmds.equals("REFRESH")) {
+			try {
+				String number = (String)user2number.get(sp.session.getCookie());
+				if(number2question.containsKey(number)) {
+					if(number2answer.containsKey(number)) {
+						return "";
+					}
+					return "<META HTTP-EQUIV=\"Refresh\" CONTENT=\"3; URL=http://www.mmbase.org:8080/from.shtml\">";
+				}
+				return "";
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
 		return "";
 	}
 
