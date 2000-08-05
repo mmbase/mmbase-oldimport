cf53aea77a60ae3d6aec9332270ecc9fa5676d77
daniel <daniel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2000-08-05 22:17:16 +0000

    need it for testing mmdemo
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@810 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/module/builders/NrRel.java
@@ -0,0 +1,47 @@
+/*
+
+VPRO (C)
+
+This source file is part of mmbase and is (c) by VPRO until it is being
+placed under opensource. This is a private copy ONLY to be used by the
+MMBase partners.
+
+*/
+package org.mmbase.module.builders;
+
+import java.util.*;
+import java.sql.*;
+
+import org.mmbase.module.database.*;
+import org.mmbase.module.corebuilders.InsRel;
+import org.mmbase.module.corebuilders.RelDef;
+import org.mmbase.module.core.*;
+import org.mmbase.util.*;
+
+/**
+ * @author Rico Jansen
+ */
+public class NrRel extends InsRel {
+
+	int relnumber=-1;
+
+	public NrRel() {
+	}
+
+	/**
+	* setDefaults for a node
+	*/
+	public void setDefaults(MMObjectNode node) {
+		if (relnumber==-1) {
+			RelDef bul=(RelDef)mmb.getMMObject("reldef");
+			if (bul!=null) {
+				relnumber=bul.getGuessedByName(tableName);
+				if (relnumber==-1) System.out.println("NrRel-> Can not guess name");
+			} else {
+				System.out.println("NrRel-> Can not reach RelDef");
+			}
+		}
+		node.setValue("rnumber",relnumber);
+	}
+
+}
