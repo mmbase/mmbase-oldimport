b53a532f9c2aaab9336cc5e0d4a67b97c5a51ccf
daniel <daniel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2000-08-06 15:33:19 +0000

    removed 1 DataScource writer seems silly to do it 2 times :)
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@825 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/util/XMLApplicationWriter.java
+++ src/org/mmbase/util/XMLApplicationWriter.java
@@ -44,9 +44,6 @@ public class XMLApplicationWriter  {
 	// write the datasources
 	body+=getDataSources(app);
 
-	// write the datasources
-	body+=getDataSources(app);
-
 	// write the relationsources
 	body+=getRelationSources(app);
 
