180c560e3c8fa6360057881a98d9e67bfa4c23f2
case <case@91430c5f-d668-0410-9e7d-dd663366dcfd>
2000-08-20 10:59:09 +0000

    cjr: fixed race condition: mmlanguage could be initialized before mmbase      module whereby the default languagePrefix would be set, rather than the      one defined in mmbaseroot.xml
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@901 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/gui/html/MMLanguage.java
+++ src/org/mmbase/module/gui/html/MMLanguage.java
@@ -1,11 +1,11 @@
 /*
-
+ 
 This software is OSI Certified Open Source Software.
 OSI Certified is a certification mark of the Open Source Initiative.
-
+ 
 The license (Mozilla version 1.0) can be read at the MMBase site.
 See http://www.MMBase.org/license
-
+ 
 */
 package org.mmbase.module.gui.html;
 
@@ -30,48 +30,62 @@ import org.mmbase.module.database.support.*;
  */
 public class MMLanguage extends ProcessorModule {
 
-	MMBase mmb=null;
-	String languagePrefix;
+    MMBase mmb=null;
+    String languagePrefix;
 
-	public void init() {
-		mmb=(MMBase)getModule("MMBASEROOT");		
-		languagePrefix=mmb.getLanguage();
-	}
-
-
-	/**
-	 */
-	public MMLanguage() {
-	}
-
-	/**
-	*	Handle a $MOD command
-	*/
-	public String replace(scanpage sp, String cmds) {
-		StringTokenizer tok = new StringTokenizer(cmds,"-\n\r");
-		if (tok.hasMoreTokens()) {
-			String cmd=tok.nextToken();	
-			if (cmd.equals("GET")) { 
-				if (tok.hasMoreTokens()) {
-					return(getFromCoreEnglish(tok.nextToken()));
-				} else {
-					return("missing core term");
-				}
-			} else if (cmd.equals("LANGUAGE")) {
-				return(languagePrefix);
-			}
-		}
-		return("No command defined");
-	}
+    public void init() {
+        // As the modules are loaded in a hashtable, mmlanguage can be initialized *before*
+        // MMBase whereby MMLanguage gets the *default* language value, rather than the
+        // set value in mmbaseroot.xml. Hence delay of setting language until first
+        // translation call.
+        /*
+        mmb=(MMBase)getModule("MMBASEROOT");		
+        languagePrefix=mmb.getLanguage();
+        */
+        languagePrefix = null;
+        //System.out.println("MMLanguage: language prefix = "+languagePrefix);
+    }
 
 
-	private String getFromCoreEnglish(String term) {
-		String translated=getInitParameter(languagePrefix+"_"+term);
-		if (translated==null || translated.equals("")) {
-			System.out.println("MMLanguage -> could not convert : "+term+" into : "+languagePrefix);
-			return(term);
-		} else {
-			return(translated);
-		}
-	}
+    /**
+     */
+    public MMLanguage() {}
+
+    /**
+    *	Handle a $MOD command
+    */
+    public String replace(scanpage sp, String cmds) {
+        StringTokenizer tok = new StringTokenizer(cmds,"-\n\r");
+        if (tok.hasMoreTokens()) {
+            String cmd=tok.nextToken();
+            if (cmd.equals("GET")) {
+                if (tok.hasMoreTokens()) {
+                    return(getFromCoreEnglish(tok.nextToken()));
+                } else {
+                    return("missing core term");
+                }
+            } else if (cmd.equals("LANGUAGE")) {
+                return(languagePrefix);
+            }
+        }
+        return("No command defined");
+    }
+
+
+    private String getFromCoreEnglish(String term) {
+        // Set languagePrefix if not set yet. It isn't set at initialization time because
+        // we can't be sure the MMBase module has already been initialized.
+        if (languagePrefix == null) {
+            mmb=(MMBase)getModule("MMBASEROOT");
+            languagePrefix=mmb.getLanguage();
+        }
+
+        String translated=getInitParameter(languagePrefix+"_"+term);
+        if (translated==null || translated.equals("")) {
+            System.out.println("MMLanguage -> could not convert : "+term+" into : "+languagePrefix);
+            return(term);
+        } else {
+            return(translated);
+        }
+    }
 }
