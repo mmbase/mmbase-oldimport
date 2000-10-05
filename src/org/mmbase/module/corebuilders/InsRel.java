061e96b52fef36564de57f39f13a63b9036ba14b
vpro <vpro@91430c5f-d668-0410-9e7d-dd663366dcfd>
2000-10-05 12:12:51 +0000

    Rico: removed unnecessary constructor
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@980 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/corebuilders/InsRel.java
+++ src/org/mmbase/module/corebuilders/InsRel.java
@@ -44,19 +44,6 @@ public class InsRel extends MMObjectBuilder {
 	}
 
 	/**
-	* Construct the InsRel object that gives access to insrels
-	*/
-	/*
-	public InsRel(MMBase m) {
-		this.mmb=m;
-		this.tableName="insrel";
-		init();
-		m.mmobjs.put(tableName,this);
-	}
-	*/
-
-	
-	/**
 	* Fix a reldef node
 	*/
 	public MMObjectNode alignRelNode(MMObjectNode node) {
