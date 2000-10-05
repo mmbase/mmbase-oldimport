f2ca7271a7ae8d025255012a30729bfae644442a
vpro <vpro@91430c5f-d668-0410-9e7d-dd663366dcfd>
2000-10-05 12:01:50 +0000

    Rico: removed create method
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@977 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/builders/RAStatistics.java
+++ src/org/mmbase/module/builders/RAStatistics.java
@@ -30,29 +30,6 @@ public class RAStatistics extends MMObjectBuilder {
 		init();
 	}
 
-	
-	/**
-	* insert a new object, normally not used (only subtables are used)
-	*/
-	public boolean create() {
-		// create the main object table
-		try {
-			Statement stmt=mmb.getConnection().createStatement();
-			stmt.executeUpdate("create table "+mmb.baseName+"_rastatistics  ("
-								 	+"	ip text not null "
-									+", startTime int"
-									+", raName text"
-									+", bytesSent int"
-									+", timeSent int"
-									+") under "+mmb.baseName+"_object");
-			return(true);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return(false);
-	}
-
-
 	public String getGUIIndicator(MMObjectNode node) {
 		String str=node.getStringValue("title");
 		if (str.length()>15) {
