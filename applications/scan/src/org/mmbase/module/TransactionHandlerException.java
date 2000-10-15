9550ffcf133995a0ebe6189f57419cb6b0be043e
gerard <gerard@91430c5f-d668-0410-9e7d-dd663366dcfd>
2000-10-15 22:55:28 +0000

    gerard: added dummy exception....cvs wouldn't compile without it
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@1026 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ applications/scan/src/org/mmbase/module/TransactionHandlerException.java
@@ -0,0 +1,21 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.module;
+
+/**
+ * Dummy ExceptionHandler Exception
+ *
+ */
+ 
+public class TransactionHandlerException extends java.lang.Exception{
+
+    public TransactionHandlerException (String error) {}
+
+}
