50b0751a497698fc5da42eb20ab5b046a87ba058
install <install@91430c5f-d668-0410-9e7d-dd663366dcfd>
2000-10-25 14:49:36 +0000

    John Balder changed getObject method
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@1079 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/core/TemporaryNodeManagerInterface.java
+++ src/org/mmbase/module/core/TemporaryNodeManagerInterface.java
@@ -13,9 +13,12 @@ import java.util.*;
 
 import org.mmbase.util.*;
 /*
-	$Id: TemporaryNodeManagerInterface.java,v 1.2 2000-10-13 09:39:54 vpro Exp $
+	$Id: TemporaryNodeManagerInterface.java,v 1.3 2000-10-25 14:49:36 install Exp $
 
 	$Log: not supported by cvs2svn $
+	Revision 1.2  2000/10/13 09:39:54  vpro
+	Rico: added a method
+	
 	Revision 1.1  2000/08/14 19:19:06  rico
 	Rico: added the temporary node and transaction support.
 	      note that this is rather untested but based on previously
@@ -25,13 +28,13 @@ import org.mmbase.util.*;
 
 /**
  * @author Rico Jansen
- * @version $Id: TemporaryNodeManagerInterface.java,v 1.2 2000-10-13 09:39:54 vpro Exp $
+ * @version $Id: TemporaryNodeManagerInterface.java,v 1.3 2000-10-25 14:49:36 install Exp $
  */
 public interface TemporaryNodeManagerInterface {
 	public String createTmpNode(String type,String owner,String key);
 	public String deleteTmpNode(String key);
 	public MMObjectNode getNode(String key);
-	public String getObject(String key);
+	public String getObject(String key, String owner);
 	public String setObjectField(String key,String field,Object value);
 	public String getObjectFieldAsString(String key,String field);
 	public Object getObjectField(String key,String field);
