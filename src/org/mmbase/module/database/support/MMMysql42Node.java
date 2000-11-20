ec136b6872185fadb711937a96a85952cceff4f5
install <install@91430c5f-d668-0410-9e7d-dd663366dcfd>
2000-11-20 14:20:51 +0000

    Rob: enable lock/unlock function
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@1195 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/database/support/MMMysql42Node.java
+++ src/org/mmbase/module/database/support/MMMysql42Node.java
@@ -28,10 +28,34 @@ import org.mmbase.util.*;
 *
 * @author Daniel Ockeloen
 * @version 12 Mar 1997
-* @$Revision: 1.16 $ $Date: 2000-06-24 23:18:45 $
+* @$Revision: 1.17 $ $Date: 2000-11-20 14:20:51 $
 */
 public class MMMysql42Node extends MMSQL92Node implements MMJdbc2NodeInterface {
 
-
-
+	/**
+     * gives an unique number
+     * this method will work with multiple mmbases
+     * @return unique number
+     */
+    public synchronized int getDBKey() {
+        int number =-1;
+        try {
+            MultiConnection con=mmb.getConnection();
+            Statement stmt=con.createStatement();
+            stmt.executeUpdate("lock tables "+mmb.baseName+"_numberTable WRITE;");
+            stmt.executeUpdate("update "+mmb.baseName+"_numberTable set number = number+1");
+            ResultSet rs=stmt.executeQuery("select number from "+mmb.baseName+"_numberTable;");
+            while(rs.next()) {
+                        number=rs.getInt(1);
+            }
+            stmt.executeUpdate("unlock tables;");
+            stmt.close();
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("MMSQL92NODE -> SERIOUS ERROR, Problem with retrieving DBNumber from databse");
+        }
+        if (debug) System.out.println("MMSQL92NODE -> retrieving number "+number+" from the database");
+        return (number);
+    }
 }
