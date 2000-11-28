d8f77d6b5a335cd0cd61f92c391799d62b6db2b7
vpro <vpro@91430c5f-d668-0410-9e7d-dd663366dcfd>
2000-11-28 16:44:38 +0000

    davzev: Added some method comments and debug to figure out what goes on...
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@1255 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/remote/RemoteBuilder.java
+++ src/org/mmbase/remote/RemoteBuilder.java
@@ -9,6 +9,9 @@ See http://www.MMBase.org/license
 */
 /*
 $Log: not supported by cvs2svn $
+Revision 1.7  2000/11/27 12:33:11  vpro
+davzev: Changed debug and classname var to public
+
 Revision 1.6  2000/11/22 14:22:25  vpro
 davzev: Added cvs logging comments.
 
@@ -22,7 +25,7 @@ import java.util.*;
 
 
 /**
- * @version $Revision: 1.7 $ $Date: 2000-11-27 12:33:11 $  
+ * @version $Revision: 1.8 $ $Date: 2000-11-28 16:44:38 $  
  * @author Daniel Ockeloen
  */
 public class RemoteBuilder {
@@ -39,25 +42,32 @@ public class RemoteBuilder {
 
 	int lease=-1;
 
+
 	public void init(MMProtocolDriver con,String servicefile) {
 		this.con=con;
-		debug("init("+con+","+servicefile+")");
-
+		if (debug) debug("init("+con+","+servicefile+"): Reading props from servicefile in props hashtable.");
 		ExtendedProperties Reader=new ExtendedProperties();
 		props = Reader.readProperties(servicefile);
 		buildername=(String)props.get("buildername");
 		nodename=(String)props.get("nodename");
 
+		
+		if (debug) debug("init("+con+","+servicefile+"): Calling con.addListener("+buildername+","+nodename+","+this+")");
 		con.addListener(buildername,nodename,this);
-
+		if (debug) debug("init("+con+","+servicefile+"): Calling getNode()");
 		getNode();
 	}
 
+	/**
+	 * Calls con.getNode with nodename and buildername, and waits 8 seconds
+	 */
 	public synchronized void getNode() {
+		int MULTICASTWAIT=8000;
+		debug("getNode(): Calling con.getNode("+nodename+","+buildername+")");
 		con.getNode(nodename,buildername);
 		if (con.getProtocol().equals("multicast")) {
 			try {
-				wait(8000);
+				wait(MULTICASTWAIT);
 			} catch(Exception e) {
 				e.printStackTrace();
 			}
@@ -79,26 +89,24 @@ public class RemoteBuilder {
 	 * @ctype the node changetype.
 	 */
 	public void nodeRemoteChanged(String nodename,String buildername,String ctype) {		
-		if( debug ) debug("nodeRemoteChanged("+nodename+","+buildername+","+ctype+")");
+		if( debug ) debug("nodeRemoteChanged("+nodename+","+buildername+","+ctype+"): Do nothing");
 	}
 
 	public void nodeLocalChanged(String nodename,String buildername,String ctype) {		
-		if( debug ) debug("nodeLocalChanged("+nodename+","+buildername+","+ctype+")");
+		if( debug ) debug("nodeLocalChanged("+nodename+","+buildername+","+ctype+"): Do noting");
 	}
 
-
-
+	/**
+	 * XML Parses the body parameter retrieving .
+	 * @param body a String with information in xml format.
+	 */
 	public synchronized void gotXMLValues(String body) {
-
-	
 		StringTokenizer tok = new StringTokenizer(body,"\n\r");
 		String xmlline=tok.nextToken();
 		String docline=tok.nextToken();
-		
 	
 		String builderline=tok.nextToken();
 		String endtoken="</"+builderline.substring(1);
-		
 
 		String nodedata=body.substring(body.indexOf(builderline)+builderline.length());
 		nodedata=nodedata.substring(0,nodedata.indexOf(endtoken));
@@ -113,6 +121,7 @@ public class RemoteBuilder {
 			String value=nodedata.substring(nodedata.indexOf(begintoken)+begintoken.length());
 			value=value.substring(0,value.indexOf(endtoken));
 
+			if (debug) debug("gotXMLValues("+body+"): Storing in hashtable key:"+key+", value:"+value);
 			values.put(key,value);
 
 			nodedata=nodedata.substring(nodedata.indexOf(endtoken)+endtoken.length());
@@ -141,12 +150,20 @@ public class RemoteBuilder {
 		values.put(key,""+value);
 	}
 
+	/**
+	 * Gets the node in XML value Calls con.commitNode with it. 
+	 */
 	public void commit() {
+		if (debug) debug("commit(): Calling con.commitNode("+nodename+","+buildername+","+toXML());
 		con.commitNode(nodename,buildername,toXML());
 	}
 
-
+	/**
+	 * Gets the node contents from hashtable and return it in XML format.
+	 * @return a String with XML contents.
+	 */
 	public String toXML() {
+		if (debug) debug("toXML(): Read out the hashtable(=node) and write it in XML.");
 		String body="<?xml version=\"1.0\"?>\n";
 		// body+="<!DOCTYPE mmnode."+buildername+" SYSTEM \"http://openbox.vpro.nl/mmnode/"+buildername+".dtd\">\n";
 		body+="<!DOCTYPE mmnode."+buildername+" SYSTEM \"http://openbox.vpro.nl/mmnode/"+buildername+".dtd\">\n";
@@ -157,38 +174,44 @@ public class RemoteBuilder {
 				body+="<"+key+">"+value+"</"+key+">\n";
 		}
 		body+="</"+buildername+">\n";
+		if (debug) debug("toXML(): Returning body: "+body);
 		return(body);
 	}
 
-
 	public boolean maintainance() {
+		if (debug) debug("maintenance: lease="+lease+", state="+getStringValue("state"));
 		if (lease!=-1 && getStringValue("state").equals("claimed")) {
 			if (lease<1) {
+				if (debug) debug("maintenance: Setting state to waiting and emptying info field.");
 				setValue("state","waiting");			
 				setValue("info","");
 				commit();
 				System.out.println("C=0 released !");
 			} else {
+				if (debug) debug("maintenance: lease="+lease+", decrementing lease value");
 				System.out.println("C="+lease);
 				lease--;
 			}
 		} else {
+			if (debug) debug("maintenance: Setting lease to -1");
 			lease=-1;
 		}
 
+		if (debug) debug("maintenance: Calling getNode() and checking state");
 		getNode();
 		String state=getStringValue("state");
 		// System.out.println("state ("+nodename+")="+state);
 		// does the server think im down ?
 		if (state.equals("down")) {
+			if (debug) debug("maintenance: State is down, resetting to waiting");
 			setValue("state","waiting");
 			commit();
 		}
 		return(true);		
 	}
 
-
 	public void setClaimed() {
+		if (debug) debug("maintenance: Getting lease value from info field");
 		String cmds=getStringValue("info");
 		StringTagger tagger=new StringTagger(cmds);
 		String tmp=tagger.Value("lease");
