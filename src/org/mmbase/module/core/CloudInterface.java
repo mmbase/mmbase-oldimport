8d2aed83106a00daebe1f207b728f08c6565f12d
install <install@91430c5f-d668-0410-9e7d-dd663366dcfd>
2000-12-11 14:04:13 +0000

    Rob:
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@1284 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/module/core/CloudInterface.java
@@ -0,0 +1,51 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+
+package org.mmbase.module.core;
+
+/**
+ *
+ * @author Rob Vermeulen
+ */
+public interface CloudInterface {
+
+	/**
+	 * give the Node 
+	 * @param the number of the Node
+	 * @return Node
+	 */
+	public NodeInterface getNode(int nodenumber);
+
+	/**
+	 * give the Node with given aliasname
+	 * @param the aliasname of the Node
+	 * @return Node with given aliasname
+	 */
+	public NodeInterface getNode(String aliasname);
+
+	/**
+	 * gets all Builders
+	 * @return a enumeration of all Builders
+	 */
+	public Enumeration getBuilders();
+	
+	/**
+	 * gets a builder
+	 * @return the builder with given buildername 
+	 */
+	public BuilderInterface getBuilder(String buildername);
+
+	/**
+	 * creates a node of a specific type
+	 * @param name of the nodetype
+	 * @return the builder with given buildername 
+	 */
+	public NodeInterface createNode(String buildername);
+ }
