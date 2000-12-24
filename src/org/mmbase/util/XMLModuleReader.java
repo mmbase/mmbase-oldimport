84f7d8c2b87e9e2af2ed713498e63e1d526275ba
daniel <daniel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2000-12-24 23:09:32 +0000

    new module config xml reader
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@1340 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/util/XMLModuleReader.java
@@ -0,0 +1,73 @@
+/*
+ 
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+ 
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+ 
+*/
+package org.mmbase.util;
+
+import java.io.*;
+import java.util.*;
+
+import org.xml.sax.*;
+import org.apache.xerces.parsers.*;
+import org.w3c.dom.*;
+import org.w3c.dom.traversal.*;
+
+import org.mmbase.module.corebuilders.*;
+
+/**
+ */
+public class XMLModuleReader extends XMLBasicReader {
+
+    Document document;
+    DOMParser parser;
+
+
+    public XMLModuleReader(String filename) {
+        super(filename);
+    }
+
+    /**
+    * get the status of this builder
+    */
+    public String getStatus() {
+        Element e = getElementByPath("module.status");
+        return getElementValue(e);
+    }
+
+    /**
+    * get the classfile of this builder
+    */
+    public String getClassFile() {
+        Element e = getElementByPath("module.classfile");
+        return getElementValue(e);
+    }
+
+
+    /**
+    * Get the properties of this builder
+    */
+    public Hashtable getProperties() {
+        Hashtable hash=new Hashtable();
+        Element e = getElementByPath("module.properties");
+	if (e!=null) {
+	    Enumeration enum = getChildElements(e,"property");
+	    Element p;
+	    String name, value;
+	    while (enum.hasMoreElements()) {
+		p = (Element)enum.nextElement();
+		name = getElementAttributeValue(p,"name");
+		value = getElementValue(p);
+		hash.put(name,value);
+	    }
+	}
+        return(hash);
+    }
+
+
+}
+
