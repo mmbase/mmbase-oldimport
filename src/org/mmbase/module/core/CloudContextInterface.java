51eff851b739eb90c40661956bc07ba9748a7b3c
install <install@91430c5f-d668-0410-9e7d-dd663366dcfd>
2001-01-04 13:17:06 +0000

    Rob made methods static
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@1377 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/core/CloudContextInterface.java
+++ src/org/mmbase/module/core/CloudContextInterface.java
@@ -23,7 +23,7 @@ public interface CloudContextInterface {
 	 * Retrieves all the modules available in this context
 	 * @return all available modules
 	 */
-	public Iterator getModules();
+	public static Iterator getModules();
 
 	
 	/**
@@ -31,25 +31,25 @@ public interface CloudContextInterface {
 	 * @param modulename name of the module
 	 * @return the requested module
 	 */
-	public ModuleInterface getModule(String modulename);
+	public static ModuleInterface getModule(String modulename);
 
 	/**
 	 * Retrieves all clouds within this context
 	 * @return all Clouds within this context
 	 */
-	public Iterator getClouds();
+	public static Iterator getClouds();
 
 	/**
 	 * Retrieves a Cloud
 	 * @param cloudname name of the Cloud
 	 * @return all Clouds
 	 */
-	public CloudInterface getCloud(String cloudname);
+	public static CloudInterface getCloud(String cloudname);
 
 	/** 
 	 * Retrieve a remote CloudContext
 	 * @param cloudcontextUrl place of the remote CloudContext
 	 * @return remote CloudContext
-	 * public CloudContext getCloudContext(String cloudcontextUrl);
+	 * public static CloudContext getCloudContext(String cloudcontextUrl);
 	 */
 }
