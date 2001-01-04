ee37567898ef53ee8c2656a39b38eb2aff7cc7e1
install <install@91430c5f-d668-0410-9e7d-dd663366dcfd>
2001-01-04 14:32:48 +0000

    Rob
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@1380 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/core/CloudInterface.java
+++ src/org/mmbase/module/core/CloudInterface.java
@@ -9,45 +9,101 @@ See http://www.MMBase.org/license
 */
 
 package org.mmbase.module.core;
-import java.util.Enumeration;
+import java.util.Iterator;
 import org.mmbase.module.core.NodeInterface; 
 
 /**
+ * A Cloud is a collection of Nodes (and relations that are also nodes).
+ * A Cloud is tied to one or more CLoudContexts (which reside on various VMs).
  *
  * @author Rob Vermeulen
+ * @author Pierre van Rooden
  */
 public interface CloudInterface {
 
 	/**
-	 * give the Node 
-	 * @param the number of the Node
-	 * @return Node
+	 * Retrieve the node from the cloud
+	 * @param nodenumber the number of the node
+	 * @return the requested node
 	 */
 	public NodeInterface getNode(int nodenumber);
 
 	/**
-	 * give the Node with given aliasname
-	 * @param the aliasname of the Node
-	 * @return Node with given aliasname
+	 * Retrieves the node with given aliasname
+	 * @param aliasname the aliasname of the node
+	 * @return the requested node
 	 */
 	public NodeInterface getNode(String aliasname);
 
-	/**
-	 * gets all Builders
-	 * @return a enumeration of all Builders
-	 */
-	public Enumeration getNodeTypes();
-	
-	/**
-	 * gets a builder
-	 * @return the builder with given buildername 
-	 */
-	public NodeTypeInterface getNodeType(String nodetype);
+ 	/**
+     * Retrieves all node types (aka builders) available in this cloud
+     * @return an <code>Iterator</code> containing all node types
+     */
+    public Iterator getNodeTypes();
 
 	/**
-	 * creates a node of a specific type
-	 * @param name of the nodetype
-	 * @return the builder with given buildername 
-	 */
-	public NodeInterface createNode(String buildername);
- }
+     * Retrieves a node type (aka builder)
+     * @param nodeTypeName name of the node type to retrieve
+     * @return the requested node type
+     */
+    public NodeTypeInterface getNodeType(String nodeTypeName);
+
+ 	/**
+     * Retrieves a node type (aka builder)
+     * @param nodeTypeId Number of the node type to retrieve
+     * @return the requested node type
+     */
+    public NodeTypeInterface getNodeType(int nodeTypeId);
+
+	/**
+     * Creates a node of a specific type
+     * @param nodeTypeName name of the node type defining the node structure
+     * @return the newly created (but not yet committed) node
+     */
+    public NodeInterface createNode(String nodeTypeName);
+
+	/**
+     * Creates a node of a specific type
+     * @param nodeTypeId number of the node type defining the node structure
+     * @return the newly created (but not yet committed) node
+     */
+    public NodeInterface createNode(int nodeTypeId);
+ 
+	/**
+	 * Creates a node of a specific type
+     * @param nodeType the node type defining the node structure
+     * @return the newly created (but not yet committed) node
+     */
+    public NodeInterface createNode(NodeTypeInterface nodeType);
+
+	/**
+     * Retrieves the context for this cloud
+     * @return the cloud's context
+     */
+    public CloudContextInterface getCloudContext();
+
+  	/**
+     * Logs the current user on under a given logon name.
+     * @param account the useraccount to log on to
+     * @param password the user's password
+     * @return <code>true</code> if logon was succesful, <code>false</code> otherwise.
+     * public boolean logOn(String account,String password);
+     */
+
+    /**
+     * Logs the current user off.
+     * public void logOff();
+     */
+
+  	/**
+     * Retrieves the cloud's name (this is an unique identifier).
+     * @return the cloud's name
+     */
+    public String getName();
+
+    /**
+     * Retrieves the description of the cloud
+	 * @return return a description of the cloud
+     */
+    public String getDescription();
+}
