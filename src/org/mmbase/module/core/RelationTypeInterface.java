05702884ef9947d5b8f070be5c9b1a3015ef2cc7
install <install@91430c5f-d668-0410-9e7d-dd663366dcfd>
2001-01-05 13:23:54 +0000

    Rob
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@1387 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/core/RelationTypeInterface.java
+++ src/org/mmbase/module/core/RelationTypeInterface.java
@@ -12,10 +12,30 @@ package org.mmbase.module.core;
 import java.util.Enumeration;
 
 /**
+ * This interface represents a relation constraint (or contex, if you like).
+ * More specifically, it represents a relation type (itself a node type) as it applies between two
+ * other node types.
+ * Some of the information here is retrieved from the node type used to build the catual relation node
+ * (the data as described in the xml builder config file). This node type is also referred to as the parent type.
+ * Other data is retrieved from a special (hidden) object that decsribes what relations apply between two nodes.
+ * (formerly known as the TypeRel builder).
+ * This includes direction and cardinality, and the type of nodes itself. These fields are the only ones that can be changed
+ * (node type data cannot be changed except through the use of an administration module).
+ * This interface is therefor not a real mmbase 'object' in itself - it exists of two objects joined together.
  *
  * @author Rob Vermeulen
+ * @author Pierre van Rooden
  */
 public interface RelationTypeInterface extends NodeTypeInterface {
+	/** 
+	 * Directionality constant : uni-directional
+     */
+    public final static int UNIDIRECTIONAL = 1;
+
+    /** 
+	 * Directionality constant : bi-directional
+     */
+    public final static int BIDIRECTIONAL = 2;
 
 	/**
 	 * gets the role of the source to the destination
@@ -28,4 +48,22 @@ public interface RelationTypeInterface extends NodeTypeInterface {
 	 * @return the role
 	 */
 	public String getReciprocalRole();
+
+ 	/**
+     * Retrieves the directionality for this type (the default assigned to a new relation).
+     * @return one of the directionality constants
+     */
+    public int getDirectionality();
+
+    /**
+     * Retrieves the type of node that can act as the source of a relation of this type.
+     * @return the source type
+     */
+    public NodeTypeInterface getSourceType();
+
+	/**
+     * Retrieves the type of node that can act as the destination of a relation of this type.
+     * @return the destination type
+     */
+    public NodeTypeInterface getDestinationType();
 }
