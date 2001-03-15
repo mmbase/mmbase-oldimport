3bff60c63553d02ce5f7769a79931eebbba14ecc
eduard <eduard@91430c5f-d668-0410-9e7d-dd663366dcfd>
2001-03-15 09:36:28 +0000

    eduard: added new class UserContext, which should contain user information
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@1727 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/security/UserContext.java
@@ -0,0 +1,9 @@
+package org.mmbase.security;
+
+/**
+ *  This UserContext class provides a storage for the authentication
+ *  and authorization, so that information can be shared.
+ *  This class is NOT a container class for client related stuff.
+ */
+public final class UserContext {
+}
