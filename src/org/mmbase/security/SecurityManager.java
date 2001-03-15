abef32b52e84d59fe740fd55bcddc7e346695369
eduard <eduard@91430c5f-d668-0410-9e7d-dd663366dcfd>
2001-03-15 09:34:18 +0000

    eduard: added SecurityManager class, which is our base class
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@1725 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/security/SecurityManager.java
@@ -0,0 +1,148 @@
+package org.mmbase.security;
+
+import org.mmbase.util.logging.Logger;
+import org.mmbase.util.logging.Logging;
+
+/** 
+ *  This class is the main class of the security system. It loads the authentication
+ *  and authorization classes if needed, and they can be requested from this manager.
+ */        
+public final class SecurityManager {
+    private static Logger log=Logging.getLoggerInstance(SecurityManager.class.getName()); 
+    
+    /** our current authentication class */
+    private Authentication authentication;
+    
+    /** our current authorization class */    
+    private Authorization authorization;
+    
+    /** if the securitymanager is configured to functionate */        
+    private boolean active = false;
+
+    /** 
+     *	The constructor, will load the classes for authorization and authentication
+     *	with their config files, as specied in the xml from configUrl
+     *	@exception  java.io.IOException When reading the file failed
+     *	@exception  java.lang.NoSuchMethodException When a tag was not specified
+     *	@exception  org.mmbase.security.SecurityException When the class could not
+     *	    be loaded
+     */        
+    public SecurityManager(String configUrl)
+      throws java.io.IOException, java.lang.NoSuchMethodException, SecurityException
+    {
+    	SecurityXmlReader reader = new SecurityXmlReader(configUrl);
+
+      	// are we active ?
+      	String sActive = reader.getAttribute("/security", "active");
+      	if(sActive.equalsIgnoreCase("true")) {
+	    log.debug("SecurityManager will be active");
+	    active = true;
+	}
+      	else if(sActive.equalsIgnoreCase("false")) {
+	    log.debug("SecurityManager will NOT be active");	
+	    active = false;
+	}
+      	else {
+	    log.error("security attibure active must have value of true or false("+configUrl+")");		
+	    throw new SecurityException("security attibure active must have value of true or false");
+	}
+
+      	// load our authentication...
+      	String authClass = reader.getAttribute("/security/authentication", "class");
+      	if(authClass == null) {
+	    log.error("attribute class could not be found in authentication("+configUrl+")");		
+    	    throw new java.util.NoSuchElementException("class in authentication");
+	}
+      	String authUrl = reader.getAttribute("/security/authentication", "url");
+      	if(authUrl == null) {
+	    log.error("attribute url could not be found in authentication("+configUrl+")");			
+	    throw new java.util.NoSuchElementException("url in authentication");
+	}
+      	loadAuthentication(authClass, authUrl);
+
+      	// load our authorization...
+      	String auteClass = reader.getAttribute("/security/authorization", "class");
+      	if(auteClass == null) {
+	    log.error("attribute class could not be found in auhotization("+configUrl+")");				
+	    throw new java.util.NoSuchElementException("class in authorization");
+	}
+      	String auteUrl = reader.getAttribute("/security/authorization", "url");
+      	if(auteUrl == null) {
+	    log.error("attribute url could not be found in auhotization("+configUrl+")");					
+	    throw new java.util.NoSuchElementException("url in authorization");
+	}
+      	loadAuthorization(auteClass, auteUrl);
+    }
+
+    /** 
+     *	Returns the authentication class, which should be used.
+     *	@return The authentication class which should be used.
+     */        
+    public Authentication getAuthentication() {
+    	return authentication;
+    }
+
+    /** 
+     *	Returns the authorization class, which should be used.
+     *	@return The authorization class which should be used.
+     */        
+    public Authorization getAuthorization() {
+    	return authorization;
+    }
+
+    /** 
+     *	Returns the authorization class, which should be used(for optimizations)
+     *	@return <code>true</code>When the SecurityManager should
+     *	    be used.
+     *	    	<code>false</code>When not.
+     */        
+    public boolean getActive() {
+    	return active;
+    }
+
+    private void loadAuthentication(String className, String configUrl) throws SecurityException {
+    	log.debug("Using class:"+className+" with config:"+configUrl+" for Authentication");
+      	try {
+            Class classType = Class.forName(className);
+            Object o = classType.newInstance();
+            authentication = (Authentication) o;
+            authentication.load(this, configUrl);
+      	}
+      	catch(java.lang.ClassNotFoundException cnfe) {
+            cnfe.printStackTrace();
+            throw new SecurityException(cnfe.toString());
+      	}
+      	catch(java.lang.IllegalAccessException iae) {
+            iae.printStackTrace();
+            throw new SecurityException(iae.toString());
+      	}
+      	catch(java.lang.InstantiationException ie) {
+            ie.printStackTrace();
+            throw new SecurityException(ie.toString());
+      	}
+      	log.debug("Authentication loaded");
+    }
+
+    private void loadAuthorization(String className, String configUrl) throws SecurityException {
+    	log.debug("Using class:"+className+" with config:"+configUrl+" for Authorization");
+      	try {
+            Class classType = Class.forName(className);
+            Object o = classType.newInstance();
+            authorization = (Authorization) o;
+            authorization.load(this, configUrl);
+      	}
+      	catch(java.lang.ClassNotFoundException cnfe) {
+            cnfe.printStackTrace();
+            throw new SecurityException(cnfe.toString());
+      	}
+      	catch(java.lang.IllegalAccessException iae) {
+            iae.printStackTrace();
+            throw new SecurityException(iae.toString());
+      	}
+      	catch(java.lang.InstantiationException ie) {
+            ie.printStackTrace();
+            throw new SecurityException(ie.toString());
+      	}
+      	log.debug("Authorization loaded");
+    }
+}
