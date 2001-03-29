86e2fe730e0e8ee6f365eebb81becb2c507ec5ba
install <install@91430c5f-d668-0410-9e7d-dd663366dcfd>
2001-03-29 12:17:10 +0000

    Rob: not needed
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@1788 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/remote/run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/local/bin/bash
-	# java org.mmbase.remote.startMMService /tmp/test.properties 2>&1 | cat >>service.log
-	java org.mmbase.remote.startMMService runner1 5282 service/mmcounter.properties service/mmcounter2.properties
