55eb8d6010f9c3d575e14f0385f8acca837fde09
daniel <daniel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2001-03-29 22:32:25 +0000

    CRC error on page checks
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@1795 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ applications/scan/src/org/mmbase/util/PageCRCException.java
@@ -0,0 +1,25 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.util;
+
+import javax.servlet.*;
+import java.lang.Exception;
+
+/**
+ */
+public class PageCRCException extends ServletException {
+
+	/**
+	 * Create the exception
+ 	 */
+	public PageCRCException (String s) {
+		super(s);
+	}
+}
