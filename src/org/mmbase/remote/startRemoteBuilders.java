fefb77bb1ffc1b58e0b2e416364db669642c0ee5
install <install@91430c5f-d668-0410-9e7d-dd663366dcfd>
2001-03-29 13:02:54 +0000

    Rob: added a shared secret mechanism to authenticate our own remoteBuilders
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@1790 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/remote/startRemoteBuilders.java
+++ src/org/mmbase/remote/startRemoteBuilders.java
@@ -8,9 +8,12 @@ See http://www.MMBase.org/license
 
 */
 /*
-$Id: startRemoteBuilders.java,v 1.9 2000-12-22 13:57:19 vpro Exp $
+$Id: startRemoteBuilders.java,v 1.10 2001-03-29 13:02:54 install Exp $
 
 $Log: not supported by cvs2svn $
+Revision 1.9  2000/12/22 13:57:19  vpro
+Davzev: Changed debug method initialization for application
+
 Revision 1.8  2000/12/20 16:31:45  vpro
 Davzev: added changed some debug stuff
 
@@ -33,7 +36,7 @@ import java.util.*;
  * example : java org.mmbase.startRemoteBuilders /tmp/cdrom1.cfg
  * </PRE>
  *
- * @version $Revision: 1.9 $ $Date: 2000-12-22 13:57:19 $
+ * @version $Revision: 1.10 $ $Date: 2001-03-29 13:02:54 $
  * @author Daniel Ockeloen
  */
 public class startRemoteBuilders {
@@ -41,6 +44,7 @@ public class startRemoteBuilders {
 	private static String classname = startRemoteBuilders.class.getName();
 	private static boolean debug = true;
 	private static void debug(String msg) {System.out.println(classname +":"+ msg );}
+	private static String sharedSecret = "";
 
 	static Vector runningServices=new Vector();
 
@@ -67,6 +71,12 @@ public class startRemoteBuilders {
 		} else {
 			ExtendedProperties ServiceReader=new ExtendedProperties();
 			Hashtable servprops = ServiceReader.readProperties(args[0]);
+
+			// Set the shared Secret
+			// This key is shared by remoteBuilders and the MMBase system 
+			// to be able to authenticate each other.
+			sharedSecret=(String)servprops.get("sharedsecret");
+
 			// decode protocol url
 			String tmp=(String)servprops.get("connection");
 		
@@ -88,9 +98,10 @@ public class startRemoteBuilders {
 			debug("main(): Prot="+protocol);
 			debug("main(): Host="+host);
 			debug("main(): Port="+port);
+			debug("main(): Key ="+sharedSecret);
 			
 			String name=(String)servprops.get("name");
-			if (protocol.equals("ulticast")) {	
+			if (protocol.equals("multicast")) {	
 				debug("main(): starting multicast sender/receiver");
 				con=(MMProtocolDriver)new MMRemoteMultiCast(name,host,port);
 			} else if (protocol.equals("http")) {	
@@ -199,5 +210,28 @@ public class startRemoteBuilders {
 			debug("postXML(): Error connecting to object host : "+e);
 			e.printStackTrace();
 		}	
+	}
+
+	/**
+	 * checks if the received shared secret is equals to your own shared secret
+	 * @param receive shared secret
+	 * @return true if received shared secret equals your own shared secret 
+	 * @return false if received shared secret not equals your own shared secret 
+	 */ 
+	public static boolean checkSharedSecret(String key) {
+		if(sharedSecret.equals(key)) {
+			return true;
+		} else {
+			debug("ERROR, the shared "+sharedSecret+"!="+key+" secrets don't match.");	
+			return false;
+		}
+	}
+
+	/**
+	 * get the shared Secret
+	 * @return the shared Secret
+	 */
+	public static String getSharedSecret() {
+		return sharedSecret;
 	}
 }
