08af87347e0604a23faa78fea4b1f36fa3cda359
eduard <eduard@91430c5f-d668-0410-9e7d-dd663366dcfd>
2001-04-02 14:08:14 +0000

    eduard: added methods for a shared Secret, this will work for the moment, have to decide how to do this in the future. reason: remote builders[rob]
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@1804 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/security/SecurityManager.java
+++ src/org/mmbase/security/SecurityManager.java
@@ -3,30 +3,32 @@ package org.mmbase.security;
 import org.mmbase.util.logging.Logger;
 import org.mmbase.util.logging.Logging;
 
-/** 
+/**
  *  This class is the main class of the security system. It loads the authentication
  *  and authorization classes if needed, and they can be requested from this manager.
- */        
+ */
 public final class SecurityManager {
-    private static Logger log=Logging.getLoggerInstance(SecurityManager.class.getName()); 
-    
+    private static Logger log=Logging.getLoggerInstance(SecurityManager.class.getName());
+
     /** our current authentication class */
     private Authentication authentication;
-    
-    /** our current authorization class */    
+
+    /** our current authorization class */
     private Authorization authorization;
-    
-    /** if the securitymanager is configured to functionate */        
+
+    /** if the securitymanager is configured to functionate */
     private boolean active = false;
 
-    /** 
+    private String sharedSecret;
+
+    /**
      *	The constructor, will load the classes for authorization and authentication
      *	with their config files, as specied in the xml from configUrl
      *	@exception  java.io.IOException When reading the file failed
      *	@exception  java.lang.NoSuchMethodException When a tag was not specified
      *	@exception  org.mmbase.security.SecurityException When the class could not
      *	    be loaded
-     */        
+     */
     public SecurityManager(String configUrl)
       throws java.io.IOException, java.lang.NoSuchMethodException, SecurityException
     {
@@ -39,23 +41,23 @@ public final class SecurityManager {
 	    active = true;
 	}
       	else if(sActive.equalsIgnoreCase("false")) {
-	    log.debug("SecurityManager will NOT be active");	
+	    log.debug("SecurityManager will NOT be active");
 	    active = false;
 	}
       	else {
-	    log.error("security attibure active must have value of true or false("+configUrl+")");		
+	    log.error("security attibure active must have value of true or false("+configUrl+")");
 	    throw new SecurityException("security attibure active must have value of true or false");
 	}
 
       	// load our authentication...
       	String authClass = reader.getAttribute("/security/authentication", "class");
       	if(authClass == null) {
-	    log.error("attribute class could not be found in authentication("+configUrl+")");		
+	    log.error("attribute class could not be found in authentication("+configUrl+")");
     	    throw new java.util.NoSuchElementException("class in authentication");
 	}
       	String authUrl = reader.getAttribute("/security/authentication", "url");
       	if(authUrl == null) {
-	    log.error("attribute url could not be found in authentication("+configUrl+")");			
+	    log.error("attribute url could not be found in authentication("+configUrl+")");
 	    throw new java.util.NoSuchElementException("url in authentication");
 	}
       	loadAuthentication(authClass, authUrl);
@@ -63,41 +65,71 @@ public final class SecurityManager {
       	// load our authorization...
       	String auteClass = reader.getAttribute("/security/authorization", "class");
       	if(auteClass == null) {
-	    log.error("attribute class could not be found in auhotization("+configUrl+")");				
+	    log.error("attribute class could not be found in auhotization("+configUrl+")");
 	    throw new java.util.NoSuchElementException("class in authorization");
 	}
       	String auteUrl = reader.getAttribute("/security/authorization", "url");
       	if(auteUrl == null) {
-	    log.error("attribute url could not be found in auhotization("+configUrl+")");					
+	    log.error("attribute url could not be found in auhotization("+configUrl+")");
 	    throw new java.util.NoSuchElementException("url in authorization");
 	}
       	loadAuthorization(auteClass, auteUrl);
+
+        // load the sharedSecret
+      	String sharedSecret = reader.getValue("/security/sharedsecret");
+      	if(sharedSecret == null) {
+	    log.error("sharedsecret could not be found in security("+configUrl+")");
+	    throw new java.util.NoSuchElementException("sharedsecret in security");
+	}
     }
 
-    /** 
+    /**
      *	Returns the authentication class, which should be used.
      *	@return The authentication class which should be used.
-     */        
+     */
     public Authentication getAuthentication() {
     	return authentication;
     }
 
-    /** 
+    /**
      *	Returns the authorization class, which should be used.
      *	@return The authorization class which should be used.
-     */        
+     */
     public Authorization getAuthorization() {
     	return authorization;
     }
 
-    /** 
+    /**
      *	Returns the authorization class, which should be used(for optimizations)
      *	@return <code>true</code>When the SecurityManager should
      *	    be used.
      *	    	<code>false</code>When not.
-     */        
+     */
     public boolean getActive() {
     	return active;
+    }
+
+    /**
+     * checks if the received shared secret is equals to your own shared secret
+     * @param receive shared secret
+     * @return true if received shared secret equals your own shared secret
+     * @return false if received shared secret not equals your own shared secret
+     */
+    public boolean checkSharedSecret(String key) {
+        if(sharedSecret.equals(key)) {
+            return true;
+        } else {
+            log.error("the shared "+sharedSecret+"!="+key+" secrets don't match.");
+            return false;
+        }
+    }
+
+    /**
+     * get the shared Secret
+     * @return the shared Secret
+     */
+    public String getSharedSecret() {
+        return sharedSecret;
     }
 
     private void loadAuthentication(String className, String configUrl) throws SecurityException {
