b58a2d9b7a08126fa4fe9b70682f4c691deb6537
install <install@91430c5f-d668-0410-9e7d-dd663366dcfd>
2001-04-05 12:35:19 +0000

    Rob changed logging system
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@1830 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/builders/g2encoders.java
+++ src/org/mmbase/module/builders/g2encoders.java
@@ -7,28 +7,6 @@ The license (Mozilla version 1.0) can be read at the MMBase site.
 See http://www.MMBase.org/license
 
 */
-/*
-$Id: g2encoders.java,v 1.7 2001-02-15 13:35:10 vpro Exp $
-
-$Log: not supported by cvs2svn $
-Revision 1.6  2000/11/09 12:03:57  vpro
-Rico: removed setTableName() as its function is superseded by init() in servicebuilder
-
-Revision 1.5  2000/03/30 13:11:35  wwwtech
-Rico: added license
-
-Revision 1.4  2000/03/29 10:59:26  wwwtech
-Rob: Licenses changed
-
-Revision 1.3  2000/03/21 15:39:18  wwwtech
-- (marcel) Removed debug (globally declared in MMOBjectNode)
-
-Revision 1.2  2000/02/24 15:07:16  wwwtech
-Davzev added debug() methods and calls to all methods.
-
-
-*/
-
 package org.mmbase.module.builders;
 
 import java.util.*;
@@ -38,42 +16,41 @@ import java.io.*;
 import org.mmbase.module.database.*;
 import org.mmbase.module.core.*;
 import org.mmbase.util.*;
+import org.mmbase.util.logging.*;
 
 /**
  * @author Daniel Ockeloen
- * @$Revision: 1.7 $ $Date: 2001-02-15 13:35:10 $
+ * @$Revision: 1.8 $ $Date: 2001-04-05 12:35:19 $
  */
 public class g2encoders extends ServiceBuilder implements MMBaseObserver {
 
-	private String classname = getClass().getName();
-	private boolean debug = true;
-	//  private void debug(String msg){System.out.println(classname+":"+msg);}
+    private static Logger log = Logging.getLoggerInstance(g2encoders.class.getName());
 
 	public boolean nodeRemoteChanged(String number,String builder,String ctype) {
 		boolean result = false;
-		if (debug) debug("nodeRemoteChanged("+number+","+builder+","+ctype+"): Calling super");
+		log.debug("nodeRemoteChanged("+number+","+builder+","+ctype+"): Calling super");
 
 		super.nodeRemoteChanged(number,builder,ctype);
 		result = nodeChanged(number,builder,ctype);
 
-		if (debug) debug("nodeRemoteChanged("+number+","+builder+","+ctype+"): return("+result+")");
+		log.debug("nodeRemoteChanged("+number+","+builder+","+ctype+"): return("+result+")");
 		return result;
 	}
 
 	public boolean nodeLocalChanged(String number,String builder,String ctype) {
 		boolean result = false;
-		if (debug) debug("nodeLocalChanged("+number+","+builder+","+ctype+"): Calling super");
+		log.debug("nodeLocalChanged("+number+","+builder+","+ctype+"): Calling super");
 
 		super.nodeLocalChanged(number,builder,ctype);
 		result = nodeChanged(number,builder,ctype);
 
-		if (debug) debug("nodeLocalChanged("+number+","+builder+","+ctype+"): returning("+result+")");
+		log.debug("nodeLocalChanged("+number+","+builder+","+ctype+"): returning("+result+")");
 		return result;
 	}
 
 	public boolean nodeChanged(String number,String builder,String ctype) {
 		boolean result = true;
-		if (debug) debug("nodeChanged("+number+","+builder+","+ctype+"), do nothing, return("+result+")");
+		log.debug("nodeChanged("+number+","+builder+","+ctype+"), do nothing, return("+result+")");
 		return result;
 	}
 }
