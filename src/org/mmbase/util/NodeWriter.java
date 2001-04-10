f680ffc4faddc1b6f08dbb41a01ee2f3e4952d1c
daniel <daniel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2001-04-10 22:28:00 +0000

    added xml headeri on writing for correct char encoding
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@1860 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/util/NodeWriter.java
+++ src/org/mmbase/util/NodeWriter.java
@@ -64,6 +64,9 @@ public class NodeWriter{
             resultsmsgs.addElement("Failed opening file " + file);
         }
         // Write the header
+
+	write("<?xml version=\"1.0\" encoding=\"iso-8859-1\"?>\n");
+	write("<!DOCTYPE builder PUBLIC \"//MMBase - data//\" \"http://www.mmbase.org/dtd/data.dtd\">\n");
         write("<" + builderName + " "
               + "exportsource=\"mmbase://127.0.0.1/install/b1\" "
               + "timestamp=\"20000602143030\">\n");
