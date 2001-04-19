a44cc361d08b4b3b34c61f2ef1be72aaf60a408c
pierre <pierre@91430c5f-d668-0410-9e7d-dd663366dcfd>
2001-04-19 11:12:47 +0000

    added logging, javadocs, indent
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@1921 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/util/URLEscape.java
+++ src/org/mmbase/util/URLEscape.java
@@ -8,91 +8,128 @@ See http://www.MMBase.org/license
 
 */
 package org.mmbase.util;
+import org.mmbase.util.logging.*;
+
 /**
-* Escapes and Unescapes undesirable characters using % (URLEncoding)
-*/
+ * Escapes and Unescapes undesirable characters using % (URLEncoding)
+ */
 public class URLEscape {
-	
-	static boolean isacceptable[] =
-	{
-		false, false, false, false, false, false, false, false,	//  !"#$%&' 
-		false, false, true, true, true, true, true, false,		// ()*+,-./
-		true, true, true, true, true, true, true, true,			// 01234567
-		true, true, true, false, false, false, false, false,	// 89:;<=>? 
-		true, true, true, true, true, true, true, true,			// @ABCDEFG 
-		true, true, true, true, true, true, true, true,			// HIJKLMNO 
-		true, true, true, true, true, true, true, true,			// PQRSTUVW 
-		true, true, true, false, false, false, false, true,		// XYZ[\]^_ 
-		false, true, true, true, true, true, true, true,		// `abcdefg 
-		true, true, true, true, true, true, true, true,			// hijklmno 
-		true, true, true, true, true, true, true, true,			// pqrstuvw 
-		true, true, true, false, false, false, false, false		// xyz{|}~  
-	};
-	static char hex[] = {
-		'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'
-	};
-	
-	static char HEX_ESCAPE='%';
-	
-	public static String escapeurl(String str) {
-		byte buf[];
-		int i,a;
-		StringBuffer esc=new StringBuffer();
-	
-		buf=new byte[str.length()];
-		str.getBytes(0,str.length(),buf,0);
 
-		for (i = 0; i<str.length();i++) {
-			a = (int)buf[i] & 0xff;
-			if (a>=32 && a<128 && isacceptable[a-32]) {
-				esc.append((char)a);
-			} else {
-				esc.append(HEX_ESCAPE);
-				esc.append(hex[a >> 4]);
-				esc.append(hex[a & 15]);
-			}
-		}
-		return(esc.toString());
-	}
-	
-	private static char from_hex(char c)
-	{
-		return (char)(c >= '0' && c <= '9' ? c - '0'
-			: c >= 'A' && c <= 'F' ? c - 'A' + 10
-			: c - 'a' + 10);			/* accept small letters just in case */
-	}
-	
-	public static String unescapeurl(String str)
-	{
-		int i;
-		char j,t;
-		StringBuffer esc=new StringBuffer();
+    // logger
+    private static Logger log = Logging.getLoggerInstance(URLEscape.class.getName());
 
-		if (str!=null) {
-			for (i=0;i<str.length();i++) {
-				t=str.charAt(i);
-				if (t==HEX_ESCAPE) {
-					t=str.charAt(++i);
-					j=(char)(from_hex(t)*16);
-					t=str.charAt(++i);
-					j+=from_hex(t);
-					esc.append(j);
-				} else {
-					esc.append(t);
-				}
-			}	
-		} else {
-			System.out.println("Unescapeurl -> Bogus parameter");
-		}
-		return (esc.toString());
-	}
+    /**
+     * List for all ASCII characters whether it can be part of an
+     * URL line.
+     */
+    static boolean isacceptable[] =
+    {
+        false, false, false, false, false, false, false, false,    //  !"#$%&'
+        false, false, true, true, true, true, true, false,         // ()*+,-./
+        true, true, true, true, true, true, true, true,            // 01234567
+        true, true, true, false, false, false, false, false,       // 89:;<=>?
+        true, true, true, true, true, true, true, true,            // @ABCDEFG
+        true, true, true, true, true, true, true, true,            // HIJKLMNO
+        true, true, true, true, true, true, true, true,            // PQRSTUVW
+        true, true, true, false, false, false, false, true,        // XYZ[\]^_
+        false, true, true, true, true, true, true, true,           // `abcdefg
+        true, true, true, true, true, true, true, true,            // hijklmno
+        true, true, true, true, true, true, true, true,            // pqrstuvw
+        true, true, true, false, false, false, false, false        // xyz{|}~
+    };
 
-	public static void main(String args[]) {
-		for (int i=0;i<args.length;i++) {
-			System.out.println("Original : '"+args[i]+"'");
-			System.out.println("Escaped : '"+escapeurl(args[i])+"'");
-			System.out.println("Unescaped again : '"+unescapeurl(escapeurl(args[i]))+"'");
-		}
+    /**
+     * Hex characters
+     */
+    static char hex[] = {
+        '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'
+    };
 
-	}
+    /**
+     * Character to use for escaping invalid characters
+     */
+    static char HEX_ESCAPE='%';
+
+    /**
+     * Escape a url.
+     * Replaces 'invalid characters' with their Escaped code, i.e.
+     * the questionmark (?) is escaped with %3F.
+     * @param url the urls to escape
+     * @return the escaped url.
+     */
+    public static String escapeurl(String str) {
+        byte buf[];
+        int i,a;
+        StringBuffer esc=new StringBuffer();
+
+        buf=new byte[str.length()];
+        str.getBytes(0,str.length(),buf,0);
+
+        for (i = 0; i<str.length();i++) {
+            a = (int)buf[i] & 0xff;
+            if (a>=32 && a<128 && isacceptable[a-32]) {
+                esc.append((char)a);
+            } else {
+                esc.append(HEX_ESCAPE);
+                esc.append(hex[a >> 4]);
+                esc.append(hex[a & 15]);
+            }
+        }
+        return esc.toString();
+    }
+
+    /**
+     * converts a HEX-character to its approprtiate byte value.
+     * i.e. 'A' is returned as '/011'
+     * @param c teh Hex character
+     * @return the byte value as a <code>char</code>
+     */
+    private static char from_hex(char c) {
+        return (char)(c >= '0' && c <= '9' ? c - '0'
+            : c >= 'A' && c <= 'F' ? c - 'A' + 10
+            : c - 'a' + 10);            /* accept small letters just in case */
+    }
+
+    /**
+     * Unescape a url.
+     * Replaces escapesequenced with the actual character.
+     * i.e %3F is replaced with the the questionmark (?).
+     * @param url the urls to unescape
+     * @return the unescaped url.
+     */
+    public static String unescapeurl(String str) {
+        int i;
+        char j,t;
+        StringBuffer esc=new StringBuffer();
+
+        if (str!=null) {
+            for (i=0;i<str.length();i++) {
+                t=str.charAt(i);
+                if (t==HEX_ESCAPE) {
+                    t=str.charAt(++i);
+                    j=(char)(from_hex(t)*16);
+                    t=str.charAt(++i);
+                    j+=from_hex(t);
+                    esc.append(j);
+                } else {
+                    esc.append(t);
+                }
+            }
+        } else {
+            log.warn("Unescapeurl -> Bogus parameter");
+        }
+        return esc.toString();
+    }
+
+    /**
+     * Method for testing this class from the command line
+     */
+    public static void main(String args[]) {
+        for (int i=0;i<args.length;i++) {
+            log.info("Original : '"+args[i]+"'");
+            log.info("Escaped : '"+escapeurl(args[i])+"'");
+            log.info("Unescaped again : '"+unescapeurl(escapeurl(args[i]))+"'");
+        }
+
+    }
 }
