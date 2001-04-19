2ba73aaca9d8748987a4f0b994e28b25b64f18da
pierre <pierre@91430c5f-d668-0410-9e7d-dd663366dcfd>
2001-04-19 11:26:41 +0000

    added javadoc, logging, indentation
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@1922 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/scan/src/org/mmbase/util/XFileCompare.java
+++ applications/scan/src/org/mmbase/util/XFileCompare.java
@@ -9,40 +9,55 @@ See http://www.MMBase.org/license
 */
 package org.mmbase.util;
 
-
 /**
  * Class to compare XFile object depending on their comparefield.
- * @see vpro.james.util.SortedVector
- * @see vpro.james.util.CompareInterface
- * @see vpro.james.util.XFile
- * 
+ * @see SortedVector
+ * @see CompareInterface
+ * @see XFile
+ *
  * @author David V van Zeventer
  * @version 16 November 1998
  */
 public class XFileCompare implements CompareInterface {
-	String compareField;
 
-	public XFileCompare(String compareField) {
-		this.compareField = compareField;
-	}
+    /**
+     * Attribute to compare on ('filepath' or 'modtime')
+     */
+    String compareField;
 
-	public int compare(Object thisOne, Object other) {
-		int result = 0;
-		long longresult=0;
-		XFile xfileobj1 = (XFile)thisOne;
-		XFile xfileobj2 = (XFile)other;
-		
-		if (compareField.equals("filepath")){		//Compare objects using their filepathname.
-			result = (xfileobj1.getFilePath()).compareTo(xfileobj2.getFilePath());
-			//System.out.println("XFileCompare: Filepath compare result= "+result);
+    /**
+     * Create a XFuileCompare.
+     * @param compareField Attribute to compare on ('filepath' or 'modtime')
+     */
+    public XFileCompare(String compareField) {
+        this.compareField = compareField;
+    }
 
-		}else {
-			if (compareField.equals("modtime")){	//Compare objects using their modification time.
-				longresult =  xfileobj1.getModTime() - xfileobj2.getModTime();
-				result = (int)(longresult/1000);
-				//System.out.println("XFileCompare: Modificationtime compare result= "+result);
-			}
-		}
-		return (result);
-	}
-} 
+    /**
+     * Make the comparison.
+     * The result is a negative value if the comparefield of the first file is 'smaller' than the second,
+     * a positive value if it is 'larger', and 0 if they are 'equal'.
+     * @param thisOne the first object to compare. should be a <code>XFile</code>.
+     * @param other the second object to compare. should be a <code>XFile</code>.
+     * @return the result of the comparison
+     */
+    public int compare(Object thisOne, Object other) {
+        int result = 0;
+        long longresult=0;
+        XFile xfileobj1 = (XFile)thisOne;
+        XFile xfileobj2 = (XFile)other;
+
+        if (compareField.equals("filepath")) {       //Compare objects using their filepathname.
+            result = (xfileobj1.getFilePath()).compareTo(xfileobj2.getFilePath());
+            //log.debug("XFileCompare: Filepath compare result= "+result);
+
+        } else {
+            if (compareField.equals("modtime")) {   //Compare objects using their modification time.
+                longresult =  xfileobj1.getModTime() - xfileobj2.getModTime();
+                result = (int)(longresult/1000);
+                //log.debug("XFileCompare: Modificationtime compare result= "+result);
+            }
+        }
+        return result;
+    }
+}
--- src/org/mmbase/util/XFile.java
+++ src/org/mmbase/util/XFile.java
@@ -12,40 +12,59 @@ package org.mmbase.util;
 import java.util.*;
 import java.io.*;
 
-
 /**
+ * Retrieves the file properties from a File object and stores them as fields.
+ * Eg: the files' modification time will be stored as a moddate long variabele.
  *
  * @author David V van Zeventer
  * @version: 13 nov 1998
  */
+public class XFile {
 
-/*
- * class XFile : Will retrieve the file properties from a File object and stores them as fields.
- *		 Eg: the files' modification time will be stored as a moddate long variabele. 
- */
-public class XFile{
+    /**
+     * The File itself
+     */
+    private File file=null;
+    /**
+     * The file path of the file
+     */
+    private String filepath=null;
+    /**
+     * The modification time of the file
+     */
+    private long modtime=0;
 
-	private File file=null;
-	private String filepath=null;
-	private long modtime=0;
+    /**
+     * Creates a XFile object
+     * @param f the File to store the values of.
+     */
+    public XFile(File f) {
+        filepath = f.getPath();        // Get filepath.
+        modtime = f.lastModified();    // Get modificationtime.
+    }
 
-	public XFile(File f){
-		filepath = f.getPath();		//Get filepath.
-		modtime = f.lastModified();	//Get modificationtime.	
-	}
+    /**
+     * Creates a XFile object
+     * @param filepath the path of the file to store the values of.
+     */
+    public XFile(String filepath) {
+        file = new File(filepath);        // Create fileobject.
+        this.filepath = file.getPath();   // Get filepath.
+        modtime = file.lastModified();    // Get modificationtime.
+    }
 
-	public XFile(String filepath){
-		file = new File(filepath);	//Create fileobject.
-		this.filepath = file.getPath();	//Get filepath.
-		modtime = file.lastModified();	//Get modificationtime.	
-	}
-		
-		
-	public String getFilePath(){	//Method for retrieving filepath value.
-		return filepath;
-	}
-	public long getModTime(){	//Method for retrieveing modtime value.
-		return modtime;
-	}
 
+    /**
+     * Retrieves the file path.
+     */
+    public String getFilePath() {
+        return filepath;
+    }
+
+    /**
+     * Retrieves the modification time
+     */
+    public long getModTime() {
+        return modtime;
+    }
 }
