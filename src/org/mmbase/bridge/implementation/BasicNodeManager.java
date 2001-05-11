14a32ebba0c007406d286e872c818add1e30d88c
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2001-05-11 18:56:42 +0000

    michiel: Made it 'Comparable'. Handy for sorting a list of them. Gets really handy if BasicNodeManagerList becomes an ArrayList (as proposed)    NodeManagerList l = cloud.getNodeManagers();    Collections.sort(l);
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@2057 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/bridge/implementation/BasicNodeManager.java
+++ src/org/mmbase/bridge/implementation/BasicNodeManager.java
@@ -28,7 +28,7 @@ import org.mmbase.module.corebuilders.*;
  * @author Rob Vermeulen
  * @author Pierre van Rooden
  */
-public class BasicNodeManager implements NodeManager {
+public class BasicNodeManager implements NodeManager, Comparable {
 
     // Cloud for this nodetype
     protected BasicCloud cloud=null;
@@ -52,11 +52,11 @@ public class BasicNodeManager implements NodeManager {
         this.cloud=(BasicCloud)cloud;
         this.builder=builder;
         if (!builder.tableName.equals("multirelations")) {
-    	    for(Iterator i=builder.getFields().iterator(); i.hasNext();){
-	            FieldDefs f=(FieldDefs)i.next();
-	            Field ft= new BasicField(f,this);
-	            fieldTypes.put(ft.getName(),ft);
-    	    }
+            for(Iterator i=builder.getFields().iterator(); i.hasNext();){
+                FieldDefs f=(FieldDefs)i.next();
+                Field ft= new BasicField(f,this);
+                fieldTypes.put(ft.getName(),ft);
+            }
         }
     }
 
@@ -89,7 +89,7 @@ public class BasicNodeManager implements NodeManager {
     }
 
     public String getGUIName() {
-	    Hashtable singularNames=builder.getSingularNames();
+        Hashtable singularNames=builder.getSingularNames();
         if (singularNames!=null) {
             String lang=cloud.language;
             String tmp=(String)singularNames.get(lang);
@@ -97,11 +97,11 @@ public class BasicNodeManager implements NodeManager {
                 return tmp;
             }
         }
-	    return builder.getTableName();
-	}
+        return builder.getTableName();
+    }
 
-	public String getDescription() {
-	    Hashtable descriptions=builder.getDescriptions();
+    public String getDescription() {
+        Hashtable descriptions=builder.getDescriptions();
         if (descriptions!=null) {
             String lang=cloud.language;
             String tmp=(String)descriptions.get(lang);
@@ -109,105 +109,105 @@ public class BasicNodeManager implements NodeManager {
                 return tmp;
             }
         }
-	    return builder.getDescription();
-	}
+        return builder.getDescription();
+    }
 
-	public FieldList getFields() {
-	    return new BasicFieldList(fieldTypes.values(),cloud,this);
-	}
+    public FieldList getFields() {
+        return new BasicFieldList(fieldTypes.values(),cloud,this);
+    }
 
-	public FieldList getFields(int order) {
-	    if (order == ORDER_EDIT) {
-	        return new BasicFieldList(builder.getSortedFields(),cloud,this);
-	    } else if (order == ORDER_LIST) {
-	        return new BasicFieldList(builder.getSortedListFields(),cloud,this);
-	    } else if (order == ORDER_SEARCH) {
-	        return new BasicFieldList(builder.getEditFields(),cloud,this);
-	    } else {
-	        return getFields() ;
-	    }
-	}
+    public FieldList getFields(int order) {
+        if (order == ORDER_EDIT) {
+            return new BasicFieldList(builder.getSortedFields(),cloud,this);
+        } else if (order == ORDER_LIST) {
+            return new BasicFieldList(builder.getSortedListFields(),cloud,this);
+        } else if (order == ORDER_SEARCH) {
+            return new BasicFieldList(builder.getEditFields(),cloud,this);
+        } else {
+            return getFields() ;
+        }
+    }
 
-	public Field getField(String fieldName) {
-	    Field f= (Field)fieldTypes.get(fieldName);
-	    return f;
-	}
+    public Field getField(String fieldName) {
+        Field f= (Field)fieldTypes.get(fieldName);
+        return f;
+    }
 
     public NodeList getList(String constraints, String orderby,
             String directions) {
         String where = null;
-  		if ((constraints != null) && (!constraints.trim().equals(""))) {
-		    where = "WHERE " + constraints;
-  		}
-  		Vector v;
-  		if (orderby != null) {
+          if ((constraints != null) && (!constraints.trim().equals(""))) {
+            where = "WHERE " + constraints;
+          }
+          Vector v;
+          if (orderby != null) {
             v = builder.searchVector(where, orderby, directions);
-	    } else {
-		    v = builder.searchVector(where);
-	    }
-	    // remove all nodes that cannot be accessed
-	    for(int i=(v.size()-1); i>-1; i--) {
-	        if (!cloud.check(Operation.READ, ((MMObjectNode)v.get(i)).getIntValue("number"))) {
-	            v.remove(i);
-	        }
-	    }
-	    return new BasicNodeList(v,cloud,this);
+        } else {
+            v = builder.searchVector(where);
+        }
+        // remove all nodes that cannot be accessed
+        for(int i=(v.size()-1); i>-1; i--) {
+            if (!cloud.check(Operation.READ, ((MMObjectNode)v.get(i)).getIntValue("number"))) {
+                v.remove(i);
+            }
+        }
+        return new BasicNodeList(v,cloud,this);
     }
 
-	public String getInfo(String command) {
-	    return getInfo(command, null,null);
-	}
+    public String getInfo(String command) {
+        return getInfo(command, null,null);
+    }
 
-	public String getInfo(String command, ServletRequest req,  ServletResponse resp){
-	    StringTokenizer tokens= new StringTokenizer(command,"-");
-	    return builder.replace(BasicCloudContext.getScanPage(req, resp),tokens);
-	}
+    public String getInfo(String command, ServletRequest req,  ServletResponse resp){
+        StringTokenizer tokens= new StringTokenizer(command,"-");
+        return builder.replace(BasicCloudContext.getScanPage(req, resp),tokens);
+    }
 
-	public NodeList getList(String command, Hashtable parameters){
-	    return getList(command,parameters,null,null);
-	}
+    public NodeList getList(String command, Hashtable parameters){
+        return getList(command,parameters,null,null);
+    }
 
-	public NodeList getList(String command, Hashtable parameters, ServletRequest req, ServletResponse resp){
-	    StringTagger params= new StringTagger("");
-	    if (parameters!=null) {
-	        for (Enumeration keys=parameters.keys(); keys.hasMoreElements(); ) {
-	            String key=(String)keys.nextElement();
-	            Object o = parameters.get(key);
-	            if (o instanceof Vector) {
-	                params.setValues(key,(Vector)o);
-    	        } else {
-	                params.setValue(key,""+o);
-	            }
-	        }
-	    }
-	    try {
-	        StringTokenizer tokens= new StringTokenizer(command,"-");
-    	    Vector v=builder.getList(BasicCloudContext.getScanPage(req, resp),params,tokens);
-    	    if (v==null) { v=new Vector(); }
+    public NodeList getList(String command, Hashtable parameters, ServletRequest req, ServletResponse resp){
+        StringTagger params= new StringTagger("");
+        if (parameters!=null) {
+            for (Enumeration keys=parameters.keys(); keys.hasMoreElements(); ) {
+                String key=(String)keys.nextElement();
+                Object o = parameters.get(key);
+                if (o instanceof Vector) {
+                    params.setValues(key,(Vector)o);
+                } else {
+                    params.setValue(key,""+o);
+                }
+            }
+        }
+        try {
+            StringTokenizer tokens= new StringTokenizer(command,"-");
+            Vector v=builder.getList(BasicCloudContext.getScanPage(req, resp),params,tokens);
+            if (v==null) { v=new Vector(); }
             int items=1;
-    	    try { items=Integer.parseInt(params.Value("ITEMS")); } catch (Exception e) {}
-	        Vector fieldlist=params.Values("FIELDS");
-	        Vector res=new Vector(v.size() / items);
-    	    for(int i= 0; i<v.size(); i+=items) {
-    	        MMObjectNode node = new MMObjectNode(builder);
-    	        for(int j= 0; (j<items) && (j<v.size()); j++) {
-    	            if ((fieldlist!=null) && (j<fieldlist.size())) {
-        	            node.setValue((String)fieldlist.get(j),v.get(i+j));
-    	            } else {
-        	            node.setValue("item"+(j+1),v.get(i+j));
-        	        }
-    	        }
-    	        res.add(node);
-    	    }
-   		    NodeManager tempNodeManager = null;
-   		    if (res.size()>0) {
-  		        tempNodeManager = new VirtualNodeManager((MMObjectNode)res.get(0),cloud);
+            try { items=Integer.parseInt(params.Value("ITEMS")); } catch (Exception e) {}
+            Vector fieldlist=params.Values("FIELDS");
+            Vector res=new Vector(v.size() / items);
+            for(int i= 0; i<v.size(); i+=items) {
+                MMObjectNode node = new MMObjectNode(builder);
+                for(int j= 0; (j<items) && (j<v.size()); j++) {
+                    if ((fieldlist!=null) && (j<fieldlist.size())) {
+                        node.setValue((String)fieldlist.get(j),v.get(i+j));
+                    } else {
+                        node.setValue("item"+(j+1),v.get(i+j));
+                    }
+                }
+                res.add(node);
+            }
+               NodeManager tempNodeManager = null;
+               if (res.size()>0) {
+                  tempNodeManager = new VirtualNodeManager((MMObjectNode)res.get(0),cloud);
             }
             return new BasicNodeList(res,cloud,tempNodeManager);
-    	} catch (Exception e) {
-    	    throw new BasicBridgeException(e);
-    	}
-	}
+        } catch (Exception e) {
+            throw new BasicBridgeException(e);
+        }
+    }
 
 
     /**
@@ -218,6 +218,18 @@ public class BasicNodeManager implements NodeManager {
     public boolean equals(Object o) {
         return (o instanceof NodeManager) && (o.hashCode()==hashCode());
     };
+
+    /**
+     * Compares two nodemanagers for sorting. It uses the GUIName for
+     * sorting, which makes the sorting alphabetic. This is what you
+     * need when you e.g. try to make an generic editor.
+     *
+     * @param o the object to compare with 
+     */
+    public int compareTo(Object o) {
+	if (! (o instanceof NodeManager)) { return -1; }
+	return getGUIName().compareToIgnoreCase(((NodeManager)o).getGUIName());
+    }
 
     /**
      * Returns the nodemanager's hashCode.
