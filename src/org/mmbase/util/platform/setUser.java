77a6f822a3b3c57d7e67cbcdd8badb680d56e47e
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2001-05-21 11:42:26 +0000

    michiel: logging system
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@2131 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/util/platform/setUser.java
+++ src/org/mmbase/util/platform/setUser.java
@@ -9,37 +9,44 @@ See http://www.MMBase.org/license
 */
 package org.mmbase.util.platform;
 
+import org.mmbase.util.logging.Logging;
+import org.mmbase.util.logging.Logger;
+
+
 public class setUser {
-boolean needSetUser;
-String osname;
+    
+    static private Logger log = Logging.getLoggerInstance(setUser.class.getName()); 
 
-	public setUser() {
-		//System.out.println("java.library.path="+System.getProperty("java.library.path"));
-		needSetUser=needSetUser();
-		if (needSetUser) {
-			System.loadLibrary("setUser");
-		}
-	}
+    boolean needSetUser;
+    String osname;
 
-	private boolean needSetUser() {
-		boolean rtn=true;
-		/* Figure out if need setuid/setgid */
-		osname=System.getProperty("os.name");
-		System.out.println("OS "+osname);
-		if (osname.equals("Windows 95") || osname.equals("Windows NT")) {
-			rtn=false;
-		}
-		return(rtn);
-	}
+    public setUser() {
+        //log.debug("java.library.path="+System.getProperty("java.library.path"));
+        needSetUser=needSetUser();
+        if (needSetUser) {
+            System.loadLibrary("setUser");
+        }
+    }
 
-	public boolean setUserGroup(String user,String group) {
-		if (needSetUser) {
-			return(setUserGroupNative(user,group));
-		} else {
-			return(true);
-		}
-	}
+    private boolean needSetUser() {
+        boolean rtn=true;
+        /* Figure out if need setuid/setgid */
+        osname=System.getProperty("os.name");
+        log.debug("OS "+osname);
+        if (osname.equals("Windows 95") || osname.equals("Windows NT")) {
+            rtn=false;
+        }
+        return(rtn);
+    }
 
-	private native boolean setUserGroupNative(String user,String group);
+    public boolean setUserGroup(String user,String group) {
+        if (needSetUser) {
+            return(setUserGroupNative(user,group));
+        } else {
+            return(true);
+        }
+    }
+
+    private native boolean setUserGroupNative(String user,String group);
 
 }
--- src/org/mmbase/util/platform/setUserTest.java
+++ src/org/mmbase/util/platform/setUserTest.java
@@ -11,18 +11,23 @@ package org.mmbase.util.platform;
 
 import java.util.Vector;
 
-public class setUserTest {
-setUser setit=new setUser();
-boolean b=false;
+import org.mmbase.util.logging.Logging;
+import org.mmbase.util.logging.Logger;
 
-	public setUserTest() {
-		b=setit.setUserGroup("wwwtech","www");
-		System.out.println("setUser : "+b);
-	}
+public class setUserTest {
+
+    static private Logger log = Logging.getLoggerInstance(setUserTest.class.getName()); 
+    setUser setit=new setUser();
+    boolean b=false;
+    
+    public setUserTest() {
+        b=setit.setUserGroup("wwwtech","www");
+        log.debug("setUser : "+b);
+    }
 
     public static void main(String args[]) {
-		setUserTest srv;
-		srv=new setUserTest();
+        setUserTest srv;
+        srv=new setUserTest();
 		System.exit(0);
     }
 }
