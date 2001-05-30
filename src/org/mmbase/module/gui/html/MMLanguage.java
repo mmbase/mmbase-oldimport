7ef8d6bb4a0c76cadd1b1419480e6ba57e74a184
pierre <pierre@91430c5f-d668-0410-9e7d-dd663366dcfd>
2001-05-30 08:45:46 +0000

    pierre: made getFromCoreEnglish() public
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@2161 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/gui/html/MMLanguage.java
+++ src/org/mmbase/module/gui/html/MMLanguage.java
@@ -1,40 +1,41 @@
 /*
- 
+
 This software is OSI Certified Open Source Software.
 OSI Certified is a certification mark of the Open Source Initiative.
- 
+
 The license (Mozilla version 1.0) can be read at the MMBase site.
 See http://www.MMBase.org/license
- 
+
 */
 package org.mmbase.module.gui.html;
 
-import java.lang.*;
-import java.net.*;
 import java.util.*;
-import java.util.Date;
-import java.io.*;
-import java.sql.*;
 
-import org.mmbase.util.*;
 import org.mmbase.module.*;
 import org.mmbase.module.core.*;
-import org.mmbase.module.builders.*;
-import org.mmbase.module.corebuilders.*;
-import org.mmbase.module.database.*;
-import org.mmbase.module.database.support.*;
-
-import org.mmbase.util.logging.Logger;
-import org.mmbase.util.logging.Logging;
+import org.mmbase.util.*;
+import org.mmbase.util.logging.*;
 
 /**
+ * Module for multi-language support.
+ * This module reads (english) terms and their localized value(s) from a
+ * configuration file.
+ * It can then return the localized terms when needed.
+ *
  * @author Daniel Ockeloen
  */
 public class MMLanguage extends ProcessorModule {
 
-    private static Logger log = Logging.getLoggerInstance(MMLanguage.class.getName()); 
+    // logger
+    private static Logger log = Logging.getLoggerInstance(MMLanguage.class.getName());
 
+    /**
+     * Reference to the MMbase module.
+     */
     MMBase mmb=null;
+    /**
+     * The language currently in use.
+     */
     String languagePrefix;
 
     public void init() {
@@ -42,41 +43,45 @@ public class MMLanguage extends ProcessorModule {
         // MMBase whereby MMLanguage gets the *default* language value, rather than the
         // set value in mmbaseroot.xml. Hence delay of setting language until first
         // translation call.
-        /*
-        mmb=(MMBase)getModule("MMBASEROOT");		
-        languagePrefix=mmb.getLanguage();
-        */
         languagePrefix = null;
-        //log.debug("MMLanguage: language prefix = "+languagePrefix);
     }
 
 
     /**
+     * Basic constructor
      */
     public MMLanguage() {}
 
     /**
-    *	Handle a $MOD command
-    */
+     * Handles the $MOD-MMLANGUAGE-commands.
+     * Commands handled by this method are:
+     * <ul>
+     * <li> GET-term : translates 'term' to the current language, if possible
+     *      (otherwise returns the term unchanged).</li>
+     * <li> LANGUAGE: returns the language prefix currently in use.</li>
+     * </ul>
+     * @param scanpage the current page context
+     * @param tok the tokenized command
+     * @return the result of the command as a String
+     */
     public String replace(scanpage sp, String cmds) {
         StringTokenizer tok = new StringTokenizer(cmds,"-\n\r");
         if (tok.hasMoreTokens()) {
             String cmd=tok.nextToken();
             if (cmd.equals("GET")) {
                 if (tok.hasMoreTokens()) {
-                    return(getFromCoreEnglish(tok.nextToken()));
+                    return getFromCoreEnglish(tok.nextToken());
                 } else {
-                    return("missing core term");
+                    return "missing core term";
                 }
             } else if (cmd.equals("LANGUAGE")) {
-                return(languagePrefix);
+                return languagePrefix;
             }
         }
-        return("No command defined");
+        return "No command defined";
     }
 
-
-    private String getFromCoreEnglish(String term) {
+    public String getFromCoreEnglish(String term) {
         // Set languagePrefix if not set yet. It isn't set at initialization time because
         // we can't be sure the MMBase module has already been initialized.
         if (languagePrefix == null) {
@@ -87,9 +92,9 @@ public class MMLanguage extends ProcessorModule {
         String translated=getInitParameter(languagePrefix+"_"+term);
         if (translated==null || translated.equals("")) {
             log.warn("MMLanguage -> could not convert : "+term+" into : "+languagePrefix);
-            return(term);
+            return term;
         } else {
-            return(translated);
+            return translated;
         }
     }
 }
