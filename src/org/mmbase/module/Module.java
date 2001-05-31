4610285d8de1db761efb2122bf3bf4f456dbb322
pierre <pierre@91430c5f-d668-0410-9e7d-dd663366dcfd>
2001-05-31 09:20:23 +0000

    pierre: fixed indentation, code conventions
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@2163 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/Module.java
+++ src/org/mmbase/module/Module.java
@@ -1,4 +1,4 @@
-/* -*- tab-width: 4 -*- 
+/*
 
 This software is OSI Certified Open Source Software.
 OSI Certified is a certification mark of the Open Source Initiative.
@@ -9,13 +9,10 @@ See http://www.MMBase.org/license
 */
 package org.mmbase.module;
 
-import java.lang.*;
-import java.net.*;
 import java.util.*;
 import java.io.*;
 
 import javax.servlet.*;
-import javax.servlet.http.*;
 
 import org.mmbase.util.*;
 import org.mmbase.module.core.*;
@@ -29,238 +26,236 @@ import org.mmbase.util.logging.Logger;
  * @author Rico Jansen
  * @author Rob Vermeulen (securitypart)
  *
- * @version $Revision: 1.30 $ $Date: 2001-05-07 09:11:31 $
+ * @version $Revision: 1.31 $ $Date: 2001-05-31 09:20:23 $
  */
 public abstract class Module {
 
-    static Logger log = Logging.getLoggerInstance(Module.class.getName()); 
+    static private Logger log = Logging.getLoggerInstance(Module.class.getName());
+    static Hashtable modules;
+    static String mmbaseconfig;
+    static ModuleProbe mprobe;
 
-	Object SecurityObj;
-	String moduleName=null;
-	//protected final Hashtable state=new Hashtable();
-	Hashtable state=new Hashtable();
-	// org.mmbase private UsersInterface users;
-	Hashtable mimetypes;
+    Object SecurityObj;
+    String moduleName=null;
+    //protected final Hashtable state=new Hashtable();
+    Hashtable state=new Hashtable();
+    // org.mmbase private UsersInterface users;
+    Hashtable mimetypes;
+    Hashtable properties;
+    String maintainer;
+    int    version;
 
-	static Hashtable modules;
-	static String mmbaseconfig;
-	
-	Hashtable properties;
-	static ModuleProbe mprobe;
-	private String className;
-	String maintainer;
-	int    version;	
-		
-	/**
- 	 * variable to synchronize SecurityObj
- 	 */
-	private Object synobj=new Object();
-
-	public Module() {
-		String StartedAt=(new Date(DateSupport.currentTimeMillis())).toString();
-		//String StartedAt=(new Date()).toString();
-		state.put("Start Time",StartedAt);
-		// org.mmbase mimetypes=Environment.getProperties(this,"mimetypes");
-
-	}
-	
-	public final void setName(String name) {
-		if (moduleName==null) {
-			moduleName=name;
-		}
-	}
-
-	/**
-	 * Inits the module (startup final step 2).
-	 * This is called second on startup, the module is expected
-	 * to read the environment variables it needs. Startup threads,
-	 * open connections etc.
-	 */
-	public abstract void init();
-
-	public abstract void onload();
-
-	/**
-	 * state, returns the state hashtable that is/can be used to debug. Should
-	 * be overridden when live state should be done.
-	 */
-	 public Hashtable state() {
-		return(state);
-	 }
-
-    	public void setInitParameter(String key,String value) {
-		if (properties!=null) {
-			properties.put(key,value);
-		}
-	}
-	/**
-	 * Gets his init-parameters
- 	 */
-    	public String getInitParameter(String key) {
-		if (properties!=null) {
-			String value=(String)properties.get(key);
-			if (value==null) {
-				key=key.toLowerCase();
-				value=(String)properties.get(key);
-			}
-			return(value);
-		} else {
-            log.error("getInitParameters(" + key + "): No properties found, called before they where loaded");
-		}
-		return(null);
-	}
-
-
-	/**
-	 * Returns the properties to the subclass. 
-	 */
-   	protected Hashtable getProperties(String propertytable) {
-		//String filename="/usr/local/vpro/james/adminopen/modules/";
-		//return(results);		
-		return(null);
-		//return(Environment.getProperties(this,propertytable));
-	}
-	
-	/**
-	 * Returns one propertyvalue to the subclass.
-	 */
-	/* daniel, org.mmbase needs fix
-	*/
-	protected String getProperty(String name, String var) {
-		//return(Environment.getProperty(this,name,var));
-		return("");
-	} 
-
-	/* daniel, org.mmbase
-   	protected Object removeProperty(String propertytable, String key) {
-		return(Environment.removeProperty(this,propertytable,key));
-	}
-	*/
-	
-	/**
-	 * Adds a property to the propertytabel 
-	 */
-	/* daniel, org.mmbase
-	protected String putProperty(String propertytable, String key, String value) {
-		return(String)(Environment.putProperty(this,propertytable,key,value));
-	}
-	*/
- 
-	/**
-	 * Adds a property to the propertytabel 
-	 */
-	/* daniel, org.mmbase
-	protected String putInitProperty(String key, String value) {
-		return(String)(Environment.putProperty(this,"module/"+moduleName,key,value));
-	} 
-	*/
-
-	/**
-	 * Gets own modules properties
-	 */
-   	public Hashtable getInitParameters() {
-		return(properties);
-	}
-
-
-
-	/**
- 	 * Returns an iterator of all the modules that are currently active.
- 	 * This function <code>null</code> if no attempt has the modules have (not) yet been to loaded.
- 	 * Unlike {@link #getModule}, this method does not automatically load modules if this hadn't occurred yet.
-	 * @return an <code>Iterator</code> with all active modules
- 	 */
-	public static final Iterator getModules() {
-	    if (modules==null) {
-	        return null;
-	    } else {
-	        return modules.values().iterator();
-	    }
-	}
-
-
-	/**
-	 *	Get user Module property
-	 */
-	/* daniel, org.mmbase
-	protected final String getUserModuleProperty(String userName,String name,int type) {
-		if (users!=null && userName!=null) {
-			return(users.getModuleProperty(moduleName,userName,name,type));
-		} else {
-			return(null);
-		}
-	}
-	*/
-
-	/**
-	 *	Get user Module property
-	 */
-	/* daniel, org.mmbase
-	protected final String getUserModuleProperty(String userName,String name) {
-		return(getUserModuleProperty(userName,name,1));
-	}
-	*/
-
-	/**
-	 *	Get user Module property
-	 */
-	/* daniel, org.mmbase
-	protected final boolean setUserModuleProperty(String userName,String name,String value, int type) {
-		if (users!=null && userName!=null) {
-			return(users.setModuleProperty(moduleName,userName,name,value,type));
-		} else {
-			return(false);
-		}
-	}
-	*/
-
-	/**
-	 *	Get user Module property
-	 */
-	/* daniel, org.mmbase
-	protected final boolean setUserModuleProperty(String userName,String name,String value) {
-		return(setUserModuleProperty(userName,name,value,1));
-	}
-	*/
-	
-	/** 
-	 * getName
- 	 */
-	/* daniel, org.mmbase
-	protected final String getName(Object asker) {
-		return Environment.getName(asker);
-	}
-	*/
-
-
-	/**
-	 *  Returns the name of the module
-	 * @return the module name
-	 */
-	public final String getName() {
-		return moduleName; // org.mmbase
-	}
-
-	/** 
-	 * provide some info on the module
- 	 */
-	 public String getModuleInfo() {
-		return("No module info provided");
-	 }
-
-	/**
-	* maintainance call called by the admin module every x seconds.
-	*/
-	public void maintainance() {
-	}
+    private String className;
 
     /**
-    * getMimeType: Returns the mimetype using ServletContext.getServletContext which returns the servlet context
-    * which is set when servscan is loaded.
-    * Fixed on 22 December 1999 by daniel & davzev.
-    * @param ext A String containing the extension.
-    * @return The mimetype.
+     * variable to synchronize SecurityObj
+     */
+    private Object synobj=new Object();
+
+    public Module() {
+        String StartedAt=(new Date(DateSupport.currentTimeMillis())).toString();
+        //String StartedAt=(new Date()).toString();
+        state.put("Start Time",StartedAt);
+        // org.mmbase mimetypes=Environment.getProperties(this,"mimetypes");
+
+    }
+
+    public final void setName(String name) {
+        if (moduleName==null) {
+            moduleName=name;
+        }
+    }
+
+    /**
+     * Inits the module (startup final step 2).
+     * This is called second on startup, the module is expected
+     * to read the environment variables it needs. Startup threads,
+     * open connections etc.
+     */
+    public abstract void init();
+
+    public abstract void onload();
+
+    /**
+     * state, returns the state hashtable that is/can be used to debug. Should
+     * be overridden when live state should be done.
+     */
+    public Hashtable state() {
+        return state;
+    }
+
+    public void setInitParameter(String key,String value) {
+        if (properties!=null) {
+            properties.put(key,value);
+        }
+    }
+
+    /**
+     * Gets his init-parameters
+     */
+    public String getInitParameter(String key) {
+        if (properties!=null) {
+            String value=(String)properties.get(key);
+            if (value==null) {
+                key=key.toLowerCase();
+                value=(String)properties.get(key);
+            }
+            return value;
+        } else {
+            log.error("getInitParameters(" + key + "): No properties found, called before they where loaded");
+        }
+        return null;
+    }
+
+
+    /**
+     * Returns the properties to the subclass.
+     */
+    protected Hashtable getProperties(String propertytable) {
+        //String filename="/usr/local/vpro/james/adminopen/modules/";
+        //return results;
+        return null;
+        //return Environment.getProperties(this,propertytable);
+    }
+
+    /**
+     * Returns one propertyvalue to the subclass.
+     */
+    /* daniel, org.mmbase needs fix
     */
+    protected String getProperty(String name, String var) {
+        //return Environment.getProperty(this,name,var);
+        return "";
+    }
+
+    /* daniel, org.mmbase
+       protected Object removeProperty(String propertytable, String key) {
+        return Environment.removeProperty(this,propertytable,key);
+    }
+    */
+
+    /**
+     * Adds a property to the propertytabel
+     */
+    /* daniel, org.mmbase
+    protected String putProperty(String propertytable, String key, String value) {
+        return (String)Environment.putProperty(this,propertytable,key,value);
+    }
+    */
+
+    /**
+     * Adds a property to the propertytabel
+     */
+    /* daniel, org.mmbase
+    protected String putInitProperty(String key, String value) {
+        return (String)Environment.putProperty(this,"module/"+moduleName,key,value);
+    }
+    */
+
+    /**
+     * Gets own modules properties
+     */
+       public Hashtable getInitParameters() {
+        return properties;
+    }
+
+    /**
+     * Returns an iterator of all the modules that are currently active.
+     * This function <code>null</code> if no attempt has the modules have (not) yet been to loaded.
+     * Unlike {@link #getModule}, this method does not automatically load modules if this hadn't occurred yet.
+     * @return an <code>Iterator</code> with all active modules
+     */
+    public static final Iterator getModules() {
+        if (modules==null) {
+            return null;
+        } else {
+            return modules.values().iterator();
+        }
+    }
+
+
+    /**
+     *    Get user Module property
+     */
+    /* daniel, org.mmbase
+    protected final String getUserModuleProperty(String userName,String name,int type) {
+        if (users!=null && userName!=null) {
+            return users.getModuleProperty(moduleName,userName,name,type);
+        } else {
+            return null;
+        }
+    }
+    */
+
+    /**
+     *    Get user Module property
+     */
+    /* daniel, org.mmbase
+    protected final String getUserModuleProperty(String userName,String name) {
+        return getUserModuleProperty(userName,name,1);
+    }
+    */
+
+    /**
+     *    Get user Module property
+     */
+    /* daniel, org.mmbase
+    protected final boolean setUserModuleProperty(String userName,String name,String value, int type) {
+        if (users!=null && userName!=null) {
+            return users.setModuleProperty(moduleName,userName,name,value,type);
+        } else {
+            return false;
+        }
+    }
+    */
+
+    /**
+     *    Get user Module property
+     */
+    /* daniel, org.mmbase
+    protected final boolean setUserModuleProperty(String userName,String name,String value) {
+        return setUserModuleProperty(userName,name,value,1);
+    }
+    */
+
+    /**
+     * getName
+     */
+    /* daniel, org.mmbase
+    protected final String getName(Object asker) {
+        return Environment.getName(asker);
+    }
+    */
+
+
+    /**
+     *  Returns the name of the module
+     * @return the module name
+     */
+    public final String getName() {
+        return moduleName; // org.mmbase
+    }
+
+    /**
+     * provide some info on the module
+     */
+    public String getModuleInfo() {
+        return "No module info provided";
+     }
+
+    /**
+     * maintainance call called by the admin module every x seconds.
+     */
+    public void maintainance() {
+    }
+
+    /**
+     * getMimeType: Returns the mimetype using ServletContext.getServletContext which returns the servlet context
+     * which is set when servscan is loaded.
+     * Fixed on 22 December 1999 by daniel & davzev.
+     * @param ext A String containing the extension.
+     * @return The mimetype.
+     */
     public String getMimeType(String ext) {
         return getMimeTypeFile("dummy."+ext);
     }
@@ -272,101 +267,99 @@ public abstract class Module {
             log.warn("getMimeType(" + filename + "): Can't find mimetype retval=null -> setting mimetype to default text/html");
             mimetype="text/html";
         }
-        return(mimetype);
+        return mimetype;
     }
 
 
 
-	public static synchronized final void startModules() {
-		// call the onload to get properties
-		if( log.isDebugEnabled()) {
+    public static synchronized final void startModules() {
+        // call the onload to get properties
+        if( log.isDebugEnabled()) {
             log.debug("startModules(): onloading modules(" + modules + ")");
         }
-		for (Enumeration e=modules.elements();e.hasMoreElements();) {
-			Module mod=(Module)e.nextElement();
-			if( log.isDebugEnabled() ) {
+        for (Enumeration e=modules.elements();e.hasMoreElements();) {
+            Module mod=(Module)e.nextElement();
+            if( log.isDebugEnabled() ) {
                 log.debug("startModules(): modules.onload(" + mod + ")");
             }
-			try {
-				mod.onload();		
-			} catch (Exception f) {
-				log.warn("startModules(): modules(" + mod + ") not found to 'onload'!");
-				f.printStackTrace();
-			}
-		}
-		// so now really give em their init
-		if (log.isDebugEnabled()) {
+            try {
+                mod.onload();
+            } catch (Exception f) {
+                log.warn("startModules(): modules(" + mod + ") not found to 'onload'!");
+                f.printStackTrace();
+            }
+        }
+        // so now really give em their init
+        if (log.isDebugEnabled()) {
             log.debug("startModules(): init the modules(" + modules + ")");
         }
-		for (Enumeration e=modules.elements();e.hasMoreElements();) {
-			Module mod=(Module)e.nextElement();
-			if ( log.isDebugEnabled()) {
+        for (Enumeration e=modules.elements();e.hasMoreElements();) {
+            Module mod=(Module)e.nextElement();
+            if ( log.isDebugEnabled()) {
                 log.debug("startModules(): mod.init(" + mod + ")");
             }
-			try {
-				mod.init();		
-			} catch (Exception f) {
-				log.error("startModules(): module(" + mod + ") not found to 'init'!");
-				f.printStackTrace();
-			}
-		}
-	}
+            try {
+                mod.init();
+            } catch (Exception f) {
+                log.error("startModules(): module(" + mod + ") not found to 'init'!");
+                f.printStackTrace();
+            }
+        }
+    }
 
-	public static Object getModule(String name) {
-		// are the modules loaded yet ? if not load them
-
-
-		if (modules==null) {
-				modules=loadModulesFromDisk();
-			if (log.isDebugEnabled()) {
+    public static Object getModule(String name) {
+        // are the modules loaded yet ? if not load them
+        if (modules==null) {
+            modules=loadModulesFromDisk();
+            if (log.isDebugEnabled()) {
                 log.debug("getModule(" + name + "): Modules not loaded, loading them..");
             }
-			startModules();
-			// also start the maintaince thread that calls all modules every x seconds
-			mprobe = new ModuleProbe(modules);
-		}
-		String orgname=name;
-		name=name.toLowerCase();
+            startModules();
+            // also start the maintaince thread that calls all modules every x seconds
+            mprobe = new ModuleProbe(modules);
+        }
+        String orgname=name;
+        name=name.toLowerCase();
 
 
-		// try to obtain the ref to the wanted module
-		Object obj=modules.get(name);	
-		if (obj==null) obj=modules.get(orgname);
-	
-		if (obj!=null) {
-			return(obj);
-		} else {
-			return(null);
-		}
-	}	
+        // try to obtain the ref to the wanted module
+        Object obj=modules.get(name);
+        if (obj==null) obj=modules.get(orgname);
 
-	public String getMaintainer() {
-		return(maintainer);
-	}
+        if (obj!=null) {
+            return obj;
+        } else {
+            return null;
+        }
+    }
 
-	public void setMaintainer(String m) {
-		maintainer=m;
-	}
+    public String getMaintainer() {
+        return maintainer;
+    }
 
-	public void setVersion(int v) {
-		version=v;
-	}
+    public void setMaintainer(String m) {
+        maintainer=m;
+    }
 
-	public int getVersion() {
-		return(version);
-	}
+    public void setVersion(int v) {
+        version=v;
+    }
+
+    public int getVersion() {
+        return version;
+    }
 
 
     /**
-    * set classname of the builder
-    */
+     * set classname of the builder
+     */
     public void setClassName(String d) {
         this.className=d;
     }
 
     /**
-    * return classname of this builder
-    */
+     * return classname of this builder
+     */
     public String getClassName() {
         return className;
     }
@@ -374,7 +367,7 @@ public abstract class Module {
 
     public static synchronized Hashtable loadModulesFromDisk() {
         Hashtable results=new Hashtable();
-        
+
         String dtmp=System.getProperty("mmbase.mode");
         if (dtmp!=null && dtmp.equals("demo")) {
             String curdir=System.getProperty("user.dir");
@@ -408,7 +401,7 @@ public abstract class Module {
                 if (bname.endsWith(".xml")) {
                     bname=bname.substring(0,bname.length()-4);
                     XMLModuleReader parser=new XMLModuleReader(dirname+bname+".xml");
-                    if (parser!=null) {		
+                    if (parser!=null) {
                         if (parser.getStatus().equals("active")) {
                             String cname=parser.getClassFile();
                             // try starting the module and give it its properties
@@ -427,10 +420,10 @@ public abstract class Module {
                                     ((Module)mod).setMaintainer(parser.getModuleMaintainer());
                                     ((Module)mod).setVersion(parser.getModuleVersion());
                                     ((Module)mod).setClassName(parser.getClassFile());
-                                    
+
                                 }
                             } catch (java.lang.ClassNotFoundException cnfe) {
-                                log.error("Could not load class with name '" + cname + "', " + 
+                                log.error("Could not load class with name '" + cname + "', " +
                                           "which was specified in the module:'" + dirname + bname + ".xml'(" + cnfe + ")" );
                             } catch (Exception e) {
                                 log.error("Error while loading module class" + Logging.stackTrace(e));
@@ -440,7 +433,7 @@ public abstract class Module {
                 }
             }
         }
-        return(results);
+        return results;
     }
 
 }
