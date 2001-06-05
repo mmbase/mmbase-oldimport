fece7a8b7e3dbab326bee0e9db62debf5f1f860f
pierre <pierre@91430c5f-d668-0410-9e7d-dd663366dcfd>
2001-06-05 07:54:25 +0000

    deprecated, use NodeBreaker instead (can be removed)
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@2197 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/community/RelationBreaker.java
+++ src/org/mmbase/module/community/RelationBreaker.java
@@ -1,11 +1,11 @@
-/* -*- tab-width: 4; -*-
- 
+/*
+
 This software is OSI Certified Open Source Software.
 OSI Certified is a certification mark of the Open Source Initiative.
- 
+
 The license (Mozilla version 1.0) can be read at the MMBase site.
 See http://www.MMBase.org/license
- 
+
 */
 
 package org.mmbase.module.community;
@@ -21,115 +21,176 @@ import org.mmbase.util.logging.Logger;
 import org.mmbase.util.logging.Logging;
 
 /**
- * @author Dirk-Jan Hoekstra
- * @version 5 Jan 2001
- *
  * RelationBreaker stores relation numbers with an expiretime.
  * After the expiretime has expired the relation is removed.
+ * Unfortunately, this class doesn't work. See docs for more info.
+ *
+s * @deprecated use NodeBreaker instead
+ *
+ * @author Dirk-Jan Hoekstra
+ * @version 5 Jan 2001
  */
 
-public class RelationBreaker extends Thread
-{
+public class RelationBreaker extends Thread {
 
-    private static Logger log = Logging.getLoggerInstance(RelationBreaker.class.getName()); 
-	private Vector relations = new Vector();
-	private long checkInterval = 10 * 60 * 1000;
-	private MMBase mmb;
-	private boolean shouldRun = false;
-	private TemporaryNodeManager tmpNodeManager;
+    // logger
+    private static Logger log = Logging.getLoggerInstance(RelationBreaker.class.getName());
+    // List of RelationHolder objects, which reference relations in the
+    // temporary node manager
+    private Vector relations = new Vector();
+    // The interval at which the relation breaker checks for expired relations
+    private long checkInterval = 10 * 60 * 1000;
+    // Reference to MMBase
+    private MMBase mmb;
+    // boolean to make sure the breaker is started when called for the first time,
+    // and to stop the thread later on.
+    private boolean shouldRun = false;
+    // The temporary node manager that holds the relations.
+    private TemporaryNodeManager tmpNodeManager;
 
 
-	public RelationBreaker(MMBase mmb, long checkInterval, TemporaryNodeManager tmpNodeManager)
-	{	this.mmb = mmb;
-		this.checkInterval = checkInterval;
-		this.tmpNodeManager = tmpNodeManager;
-	}
+    /**
+     * Creates a new relation breaker.
+     * Used by the Channel builder.
+     * @param mmb reference to MMBase
+     * @param checkinterval the interval at which the relation breaker checks for expired relations
+     * @param tmpNodeManager the temporary node manager that holds the relations.
+     */
+    public RelationBreaker(MMBase mmb, long checkInterval, TemporaryNodeManager tmpNodeManager) {
+        this.mmb = mmb;
+        this.checkInterval = checkInterval;
+        this.tmpNodeManager = tmpNodeManager;
+    }
 
-	public synchronized void add(String id, long expireTime)
-	{	relations.add(new RelationHolder(id, expireTime));
-		log.debug("add");
-		if (!shouldRun)
-		{	shouldRun = true;
-			start();
-		}
-	}
+    /**
+     * Adds a relation to be watched.
+     * This method starts the breaker if it hasn't been done already.
+     * @param id the id of the referred relation
+     * @param expireTime expiration time of the relation
+     */
+    public synchronized void add(String id, long expireTime) {
+        relations.add(new RelationHolder(id, expireTime));
+        log.debug("add");
+        if (!shouldRun) {
+            shouldRun = true;
+            start();
+        }
+    }
 
-	public synchronized boolean update(String id, long expireTime)
-	{	RelationHolder relationHolder = (RelationHolder)relations.elementAt(relations.indexOf(id));
-		if (relationHolder != null)
-		{	relationHolder.setExpireTime(expireTime);
-			return true;
-		}
-		return false;
-	}
+    /**
+     * Updates a relation to be watched, preventing it from being removed prematurely.
+     * @param id the id of the referred relation
+     * @param expireTime the new expiration time of the relation
+     */
+    public synchronized boolean update(String id, long expireTime) {
+        RelationHolder relationHolder = (RelationHolder)relations.elementAt(relations.indexOf(id));
+        if (relationHolder != null)
+        {    relationHolder.setExpireTime(expireTime);
+            return true;
+        }
+        return false;
+    }
 
-	public synchronized void remove(String id)
-	{	String owner = id.substring(0, id.indexOf("_"));
-		String key = id.substring(id.indexOf("_") + 1);
-		int i = relations.indexOf(id);
-		if (i > 0) relations.remove(i);
-		tmpNodeManager.deleteTmpNode(owner, key);
-	}
+    /**
+     * Removes a relation.
+     * This also removes the relation from the temporary node manager cache.
+     * XXX: doesn't work
+     * @param id the id of the referred relation
+     */
+    public synchronized void remove(String id) {
+        String owner = id.substring(0, id.indexOf("_"));
+        String key = id.substring(id.indexOf("_") + 1);
+//      searching on id doesn't work!
+        int i = relations.indexOf(id);
+        if (i > 0) relations.remove(i);
+        tmpNodeManager.deleteTmpNode(owner, key);
+    }
 
-	public synchronized void remove(RelationHolder relationHolder, int i)
-	{	//relations.remove(i);
-		log.debug(relationHolder.id);
-		String owner = relationHolder.id.substring(0, relationHolder.id.indexOf("_"));
-		String key = relationHolder.id.substring(relationHolder.id.indexOf("_") + 1);					
-		tmpNodeManager.deleteTmpNode(owner, key);
-	}
+    /**
+     * Removes a relation.
+     * This also removes the relation from the temporary node manager cache.
+     * XXX: doesn't work
+     * @param relationHolder the RelationHolder of the referred relation
+     * @param i index of the holder in the internal list
+     */
+    public synchronized void remove(RelationHolder relationHolder, int i) {
+        //relations.remove(i);
+        log.debug(relationHolder.id);
+        String owner = relationHolder.id.substring(0, relationHolder.id.indexOf("_"));
+        String key = relationHolder.id.substring(relationHolder.id.indexOf("_") + 1);
+        tmpNodeManager.deleteTmpNode(owner, key);
+    }
 
-	public void run()
-	{
-		InsRel insrel = mmb.getInsRel();
-		long currentTime;
+    /**
+     * Runs the thread that checks for expired relations.
+     */
+    public void run() {
+        InsRel insrel = mmb.getInsRel();
+        long currentTime;
 
-		while (shouldRun)
-		{
-			try
-			{	sleep(checkInterval);
-			}
-			catch(Exception e)
-			{	
+        while (shouldRun) {
+            try {
+                sleep(checkInterval);
+            } catch(Exception e) {
                 log.error("run(): can't sleep.");
-				shouldRun = false;
-				return;
-			}
+                shouldRun = false;
+                return;
+            }
 
-			currentTime = System.currentTimeMillis();
+            currentTime = System.currentTimeMillis();
 
-			log.debug("search for expired");
-			int i = 0;
-			while (i < relations.size())
-			{	RelationHolder relationHolder = (RelationHolder)relations.elementAt(i);
-				if (relationHolder.getExpireTime() < currentTime)
-					remove(relationHolder, i);
-				
-				i++;
-			}
-		}		
-	}
+            log.debug("search for expired");
+            int i = 0;
+            while (i < relations.size()) {
+                RelationHolder relationHolder = (RelationHolder)relations.elementAt(i);
+                if (relationHolder.getExpireTime() < currentTime)
+                    remove(relationHolder, i);
+
+                i++;
+            }
+        }
+    }
 }
 
 
-class RelationHolder
-{	public String id;
-	private long expireTime;
+/**
+ * Holds a reference to a relation.
+ */
+class RelationHolder {
+    // The id of the referred relation.
+    public String id;
+    // expiration time of the relation
+    private long expireTime;
 
-	public RelationHolder(String id, long expireTime)
-	{	this.id = id;
-		this.expireTime = expireTime;
-	}
+    /**
+     * Creates a relation reference.
+     * @param id the id of the referred relation
+     * @param expireTime expiration time of the relation
+     */
+    public RelationHolder(String id, long expireTime) {
+        this.id = id;
+        this.expireTime = expireTime;
+    }
 
-	public synchronized boolean equals(Object anObject)
-	{	return (id.equals((String)anObject));
-	}
+    /**
+     * Compares the relation references to another object.
+     * In this specific case, the object should be equal to the id.
+     */
+    public synchronized boolean equals(Object anObject) {
+        return (id.equals((String)anObject));
+    }
 
-	public synchronized void setExpireTime(long expireTime)
-	{	this.expireTime = expireTime;
-	}
+    /**
+     * Sets the expiration time of the relation.
+     */
+    public synchronized void setExpireTime(long expireTime) {
+        this.expireTime = expireTime;
+    }
 
-	public synchronized long getExpireTime()
-	{	return expireTime;
-	}
+    /**
+     * Retrieves the expiration time of the relation
+     */
+    public synchronized long getExpireTime() {
+        return expireTime;
+    }
 }
