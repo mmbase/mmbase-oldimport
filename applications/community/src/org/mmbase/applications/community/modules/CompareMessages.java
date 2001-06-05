5e74064c96f47bba660676945edbc704c8aa2365
pierre <pierre@91430c5f-d668-0410-9e7d-dd663366dcfd>
2001-06-05 07:40:36 +0000

    depreacted, use java.util.NodeComparator instead
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@2193 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/community/src/org/mmbase/applications/community/modules/CompareMessages.java
+++ applications/community/src/org/mmbase/applications/community/modules/CompareMessages.java
@@ -1,67 +1,84 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+
 package org.mmbase.module.community;
+
 import org.mmbase.util.*;
 import org.mmbase.module.core.*;
-import org.mmbase.module.corebuilders.*;
 import java.util.Vector;
 
 /**
  * @author Dirk-Jan Hoekstra
  * @version 5 Jan 2001
  *
- * CompareMessages implemtnes the CompareInterface used by SortedVector.
- * At forhand you've to specifie on which fields the message nodes should be compared,
+ * CompareMessages implements the CompareInterface used by SortedVector.
+ * At forhand you've to specify on which fields the message nodes should be compared,
  * these fields may not have a null value.
+ * @deprecated use mmbase.util.NodeComparator instead
  */
-public class CompareMessages implements CompareInterface
-{ 
-	private Vector fields;
-	private Vector sortDirs;
+public class CompareMessages implements CompareInterface {
 
-	public CompareMessages(Vector fields)
-	{	this.fields = fields;
-		sortDirs = new Vector(fields.size());
-		for (int i = 0; i < fields.size(); i++)	sortDirs.add((String)"UP");
-	}
+    public final static String UP = "UP";
+    public final static String DOWN = "DOWN";
 
-	/**
-	 * Fields are the fields on which the message nodes get compared.
-	 * Use UP and DOWN in the sortDirs vector to specifie the sort directions.
-	 */
-	public CompareMessages(Vector fields, Vector sortDirs)
-	{	this.fields = fields;
-		this.sortDirs = sortDirs;
-		for (int i = sortDirs.size(); i < fields.size(); i++) sortDirs.add((String)"UP");
-	}
+    private Vector fields;
+    private Vector sortDirs;
 
-	/**
-	 * The two message nodes will be compared using the compare function of the values out of the fields.
-	 * Only String and Integer values can be used, in other cases it's assumed that the values are equal.
-	 */
-	public int compare(Object thisone, Object other)
-	{	MMObjectNode n1 = (MMObjectNode)thisone;
-		MMObjectNode n2 = (MMObjectNode)other;
-		Object o1, o2;
-		int result;
-		int fieldnr = 0;
-		do
-		{	o1 = n1.getValue((String)fields.elementAt(fieldnr));
-			o2 = n2.getValue((String)fields.elementAt(fieldnr));
-			if (o1 instanceof Integer)
-			{	if (((String)sortDirs.elementAt(fieldnr)).equals("UP"))
-					result = (((Integer)o1).compareTo((Integer)o2));
-				else
-					result = -(((Integer)o1).compareTo((Integer)o2));
-			} else
-			if (o1 instanceof String)
-			{	if (((String)sortDirs.elementAt(fieldnr)).equals("UP"))
-					result = (((String)o1).compareToIgnoreCase((String)o2));
-				 else
-					result = -(((String)o1).compareToIgnoreCase((String)o2));
-			}
-			else
-				result = 0;
-			fieldnr++;
-		} while ((result == 0) && (fieldnr < fields.size()));
-		return result;
-	}
+    public CompareMessages(Vector fields) {
+        this.fields = fields;
+        sortDirs = new Vector(fields.size());
+        for (int i = 0; i < fields.size(); i++) {
+            sortDirs.add(UP);
+        }
+    }
+
+    /**
+     * Fields are the fields on which the message nodes get compared.
+     * Use UP and DOWN in the sortDirs vector to specifie the sort directions.
+     */
+    public CompareMessages(Vector fields, Vector sortDirs) {
+        this.fields = fields;
+        this.sortDirs = sortDirs;
+        for (int i = sortDirs.size(); i < fields.size(); i++) {
+            sortDirs.add(UP);
+        }
+    }
+
+    /**
+     * The two message nodes will be compared using the compare function of the values out of the fields.
+     * Only String and Integer values can be used, in other cases it's assumed that the values are equal.
+     */
+    public int compare(Object thisone, Object other) {
+        MMObjectNode n1 = (MMObjectNode)thisone;
+        MMObjectNode n2 = (MMObjectNode)other;
+        Object o1, o2;
+        int result;
+        int fieldnr = 0;
+        do {
+            o1 = n1.getValue((String)fields.elementAt(fieldnr));
+            o2 = n2.getValue((String)fields.elementAt(fieldnr));
+            if (o1 instanceof Integer) {
+                if (((String)sortDirs.elementAt(fieldnr)).equals(UP))
+                    result = (((Integer)o1).compareTo((Integer)o2));
+                else
+                    result = -(((Integer)o1).compareTo((Integer)o2));
+            } else if (o1 instanceof String) {
+                if (((String)sortDirs.elementAt(fieldnr)).equals(UP))
+                    result = (((String)o1).compareToIgnoreCase((String)o2));
+                else
+                    result = -(((String)o1).compareToIgnoreCase((String)o2));
+            } else {
+                result = 0;
+            }
+            fieldnr++;
+        } while ((result == 0) && (fieldnr < fields.size()));
+        return result;
+    }
 }
