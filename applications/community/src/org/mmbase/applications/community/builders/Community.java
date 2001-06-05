eea59fbb8d83463471c96d6ceb32ea52b9118ce5
pierre <pierre@91430c5f-d668-0410-9e7d-dd663366dcfd>
2001-06-05 07:43:09 +0000

    made getDefaultURL deprecated as it only serves SCAN
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@2196 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/community/src/org/mmbase/applications/community/builders/Community.java
+++ applications/community/src/org/mmbase/applications/community/builders/Community.java
@@ -1,106 +1,149 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+
 package org.mmbase.module.builders;
 
 import java.util.*;
-import java.util.Date;
-import java.sql.*;
-import java.io.*;
-import java.lang.reflect.Field;
-import java.lang.Integer;
-import java.awt.Point;
 
-import org.mmbase.module.database.*;
-import org.mmbase.module.core.*;
-import org.mmbase.util.*;
-import org.mmbase.module.core.MMObjectNode;
-import org.mmbase.module.corebuilders.InsRel;
-import org.mmbase.module.corebuilders.RelDef;
 import org.mmbase.module.community.*;
+import org.mmbase.module.core.*;
+import org.mmbase.module.corebuilders.*;
+import org.mmbase.util.*;
 import org.mmbase.util.logging.*;
 
 /**
+ * This builder implements additional functionality and methods to handle
+ * community objects.
+ * Added functionality involve opening and closing all channels related to
+ * this community, and expanding a URL obtained form the 'maps' builder with
+ * a community number (SCAN only).
+ *
  * @author Dirk-Jan Hoekstra
- * @version 31 Jan 2001
+ * @author Pierre van Rooden
+ * @version 28 May 2001
  */
 
 public class Community extends MMObjectBuilder {
+
+    /** Community type : chatbox */
+    public static final String STR_CHATBOX = "chatbox";
+    /** Community type : forum */
+    public static final String STR_FORUM = "forum";
+
+    //logger
     private static Logger log = Logging.getLoggerInstance(Community.class.getName());
-	private Channel channelBuilder;
-	private int relationNumberParentChild;
-	private int relationNumberCreator;
-	private static int otypeMsg = -1;
-	private static int otypeChannel = -1;
 
-	public Community() {
-	}
+    private Channel channelBuilder;
+    private MMObjectBuilder mapBuilder;
 
-	public boolean init() {
-		boolean result = super.init();
-		RelDef reldef = ((RelDef)mmb.getMMObject("reldef"));
-		relationNumberParentChild = reldef.getRelNrByName("parent", "child");
-		relationNumberCreator = reldef.getRelNrByName("creator", "subject");		
-		return result;
-	}
+    /**
+     * Constructor
+     */
+    public Community() {
+    }
 
-	/**
-	 * Opens all the channels of the community.
-	 *
-	 * @param community The community node of which to open all the channels.
-	 */
-	public void openAllChannels(MMObjectNode community) {
-		if (channelBuilder == null) channelBuilder = (Channel)mmb.getMMObject("channel");
-		Enumeration relatedChannels = mmb.getInsRel().getRelated(community.getIntValue("number"), otypeMsg);
-		while (relatedChannels.hasMoreElements()) channelBuilder.open((MMObjectNode)relatedChannels.nextElement());
-	}
+    /**
+     * Initializes the builder.
+     * Retrieves associated builders.
+     * @return Always true.
+     */
+    public boolean init() {
+        boolean result = super.init();
+        mapBuilder = (Channel)mmb.getMMObject("maps");
+        channelBuilder = (Channel)mmb.getMMObject("channel");
+        return result;
+    }
 
-	/**
-	 * Closes all the channels of the community.
-	 *
-	 * @param community The community of which to close all the channels.
-	 */
-	public void closeAllChannels(MMObjectNode community) {
-		if (channelBuilder == null) channelBuilder = (Channel)mmb.getMMObject("channel");
-		Enumeration relatedChannels = mmb.getInsRel().getRelated(community.getIntValue("number"), otypeMsg);
-		while (relatedChannels.hasMoreElements()) channelBuilder.close((MMObjectNode)relatedChannels.nextElement());
-	}
+    /**
+     * Opens all the channels that are connected to this community
+     *
+     * @param community The community node of which to open all the channels.
+     */
+    public void openAllChannels(MMObjectNode community) {
+        if (channelBuilder == null) {
+            log.error("No channel builder");
+            return;
+        }
+        Enumeration relatedChannels = mmb.getInsRel().getRelated(community.getNumber(), channelBuilder.oType);
+        while (relatedChannels.hasMoreElements()) {
+            channelBuilder.open((MMObjectNode)relatedChannels.nextElement());
+        }
+    }
 
-	/**
-	 * Handles the $MOD-MMBASE-BUILDER-community- commands.
-	 */
-	public String replace(scanpage sp, StringTokenizer tok) {
-		/* The first thing we expect is a community number.
-		 */
-		if (!tok.hasMoreElements()) {
-			log.error("replace(): community number expected after $MOD-BUILDER-community-.");
-			return "";
-		}		
-		MMObjectNode community = getNode(tok.nextToken());
-		
-		if (tok.hasMoreElements()) {
-			String cmd = tok.nextToken();
-			if (cmd.equals("OPEN")) openAllChannels(community);
-			if (cmd.equals("CLOSE")) closeAllChannels(community);
-		}
-		return "";
-	}
-	
-	/**
-	 * Ask URL from related Map and append the community number to the URL
-	 *
-	 * @param src The number of the community MMObjectNode.
-	 */
-	public String getDefaultUrl(int src) {
-		int otypeMap = mmb.TypeDef.getIntValue("maps");
-		Enumeration e= mmb.getInsRel().getRelated(src, otypeMap);
-		if (!e.hasMoreElements()) {
-			log.debug("GetDefaultURL Could not find related map for community node " + src);
-			return(null);
-		}
-		
-		MMObjectNode mapNode = (MMObjectNode)e.nextElement();
-		String URL = mapNode.parent.getDefaultUrl(mapNode.getIntValue("number"));
-		if (URL!=null) {
-			URL += "+" + src;
-		}
-		return URL;
-	}
+    /**
+     * Closes all the channels of the community.
+     *
+     * @param community The community of which to close all the channels.
+     */
+    public void closeAllChannels(MMObjectNode community) {
+        if (channelBuilder == null) {
+            log.error("No channel builder");
+            return;
+        }
+        Enumeration relatedChannels = mmb.getInsRel().getRelated(community.getNumber(), channelBuilder.oType);
+        while (relatedChannels.hasMoreElements()) {
+            channelBuilder.close((MMObjectNode)relatedChannels.nextElement());
+        }
+    }
+
+    /**
+     * Handles the $MOD-MMBASE-BUILDER-community-commands.
+     * Commands handled by this command are:
+     * <ul>
+     * <li> communitynr-OPEN : opens all channels that are connected to this community</li>
+     * <li> communitynr-CLOSE: closes all channels that are connected to this community</li>
+     * </ul>
+     * @param scanpage the current page context
+     * @param tok the tokenized command
+     * @return the empty string
+     */
+    public String replace(scanpage sp, StringTokenizer tok) {
+        // The first thing we expect is a community number.
+        if (!tok.hasMoreElements()) {
+            log.error("replace(): community number expected after $MOD-BUILDER-community-.");
+            return "";
+        }
+        MMObjectNode community = getNode(tok.nextToken());
+
+        if (tok.hasMoreElements()) {
+            String cmd = tok.nextToken();
+            if (cmd.equals("OPEN")) openAllChannels(community);
+            if (cmd.equals("CLOSE")) closeAllChannels(community);
+        }
+        return "";
+    }
+
+    /**
+     * Retrieves a URL from a related Map object, and append the community number
+     * to the URL.
+     * This requires the presence of a 'maps' builder, which should have a
+     * functional 'getDefaultURL' method.
+     *
+     * @param src The number of the community MMObjectNode.
+     * @return the resulting URL, or <code>null</code> if not map-node was
+     *         associated with this community.
+     * @deprecated There is no maps definition available in cvs.
+     *    In addition, this method only produces SCAN-format urls.
+     */
+    public String getDefaultUrl(int src) {
+        if (mapBuilder==null) return null;
+        Enumeration e= mmb.getInsRel().getRelated(src, mapBuilder.oType);
+        if (!e.hasMoreElements()) {
+            log.debug("GetDefaultURL Could not find related map for community node " + src);
+            return null;
+        }
+        MMObjectNode mapNode = (MMObjectNode)e.nextElement();
+        String URL = mapBuilder.getDefaultUrl(mapNode.getNumber());
+        if (URL!=null) {
+            URL += "+" + src;
+        }
+        return URL;
+    }
 }
