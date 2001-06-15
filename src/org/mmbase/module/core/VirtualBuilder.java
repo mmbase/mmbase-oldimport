d7ae18287e4a65fbe9909e77749b9b2f54a37b62
pierre <pierre@91430c5f-d668-0410-9e7d-dd663366dcfd>
2001-06-15 09:40:22 +0000

    pierre: new VirtualNode and VirtualBuilder classes. These are basic classes that override some methods in MMObjectNode/MMObjectBuilder, and implement default behavior for non-persistent nodes.
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@2244 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/module/core/VirtualBuilder.java
@@ -0,0 +1,175 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.module.core;
+
+import java.util.*;
+import java.sql.*;
+import org.mmbase.module.*;
+import org.mmbase.module.core.*;
+import org.mmbase.module.corebuilders.*;
+import org.mmbase.module.corebuilders.InsRel;
+import org.mmbase.module.database.*;
+import org.mmbase.util.*;
+
+import org.mmbase.util.logging.Logger;
+import org.mmbase.util.logging.Logging;
+
+/**
+ * VirtualBuilder is a builder which creates 'virtual' nodes.
+ * This class is intended to facilitate practical creation of virtual
+ * builders by capturing events that migth otherwise lead to unexpected or
+ * faulty behavior.
+ *
+ * @author Pierre van Rooden
+ * @version 11 May 2001
+ */
+public class VirtualBuilder extends MMObjectBuilder {
+
+    // logging variable
+    private static Logger log = Logging.getLoggerInstance(VirtualBuilder.class.getName());
+
+    /**
+     * Creates an instance of a Virtual builder and registers it in MMBase.
+     * @param m the MMbase cloud creating the node
+     * @param tableName the name of the builder as known in the MMbase system
+     */
+    protected VirtualBuilder(MMBase m, String tableName) {
+        this.mmb=m;
+        this.tableName=tableName;
+        this.description="";
+        m.mmobjs.put(tableName,this);
+        virtual=true;
+    }
+
+    /**
+     * Creates an instance of a Virtual builder.
+     * A builder instantiated with this constrcutor is not registered in MMBase
+     * and should only be used as a temporary parent for virtual nodes which
+     * do not have a long life span.
+     * @param m the MMbase cloud creating the node
+     */
+    public VirtualBuilder(MMBase m) {
+        this.mmb=m;
+        this.tableName="virtualnodes_"+System.currentTimeMillis();
+        this.description="";
+        virtual=true;
+    }
+
+    /**
+     * Initializes this builder.
+     * No specifici cation is performed.
+     * This method overrides the default emthod in MMObhjectBuilder, which
+     * would otherwise attempt to access the database.
+     * @return Always true.
+     * @see create
+     */
+    public boolean init() {
+        return true;
+    }
+
+    /**
+     * Creates a new builder table in the current database.
+     * This method does not perform any action in a virtual builder, as there is
+     * no actual table associated with it.
+     */
+    public boolean create() {
+        return true;
+    }
+
+    /**
+     * Insert a new object (content provided) in the cloud, including an entry for the object alias (if provided).
+     * This method does not perform any action in a virtual builder.
+     * @param owner The administrator creating the node
+     * @param node The object to insert
+     * @return -1 (the insert failed)
+     */
+    public int insert(String owner,MMObjectNode node) {
+        // no insert allowed on this builder, so signal -1
+        return -1;
+    }
+
+    /**
+     * Get a new node, using this builder as its parent.
+     * The new node is a virtual node.
+     * @param owner The administrator creating the new node.
+     * @return A newly initialized <code>VirtualNode</code>.
+     */
+    public MMObjectNode getNewNode(String owner) {
+        VirtualNode node=new VirtualNode(this);
+        node.setValue("number",-1);
+        node.setValue("owner",owner);
+        node.setValue("otype",oType);
+        setDefaults(node);
+        return node;
+    }
+
+   /**
+     * What should a GUI display for this node.
+     * The default behavior of a virtual node is to display the content of
+     * the 'name' field (if present).
+     * XXX: should be changed to something better
+     * @param node The node to display
+     * @return either the name field of the node or "no info"
+     */
+     public String getGUIIndicator(MMObjectNode node) {
+        String s= node.getStringValue("name");
+        if (s!=null) {
+            return s;
+        } else {
+            return GUIIndicator;
+        }
+    }
+
+    /**
+     * Return a field's database type.
+     * The default behavior for a virtual node is to return <code>DBTYPE_UNKNOWN</code>.
+     * @param the requested field's name
+     * @return the field's type (<code>DBTYPE_UNKNOWN</code>).
+     */
+    public int getDBType(String fieldName) {
+        return FieldDefs.TYPE_UNKNOWN;
+    }
+
+    /**
+     * Return a field's database state.
+     * The default behavior for a virtual node is to return <code>DBSTATE_VIRTUAL</code>.
+     * @param the requested field's name
+     * @return <code>DBSTATE_VIRTUAL</code>
+     */
+    public int getDBState(String fieldName) {
+        return FieldDefs.DBSTATE_VIRTUAL;
+    }
+
+    /**
+     * Get text from a blob field from a database.
+     * Since virtual builders are generally not associated with a database,
+     * this method returns null.
+     * @param fieldname name of the field
+     * @param number number of the object in the table
+     * @return <code>null</code>
+     */
+    public String getShortedText(String fieldname,int number) {
+        return null;
+    }
+
+
+    /**
+     * Get binary data of a blob field from a database.
+     * Since virtual builders are generally not associated with a database,
+     * this method returns null.
+     * @param fieldname name of the field
+     * @param number number of the object in the table
+     * @return <code>null</code>
+     */
+    public byte[] getShortedByte(String fieldname,int number) {
+        return null;
+    }
+
+}
---
+++ src/org/mmbase/module/core/VirtualNode.java
@@ -0,0 +1,107 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.module.core;
+
+import java.util.*;
+
+import org.mmbase.util.logging.*;
+
+/**
+ * VirtualNode is a representation of a virtual objectnode.
+ * Virtual Object nodes are nodes that are not stored in a databasetable.
+ * Note that a temporary node is not virtual.
+ * This class captures a number of methods that would normally require datbase
+ * access, such as obtaining relations or determining age of a node.
+ *
+ * @author Pierre van Rooden
+ * @version 26 Apr 2001
+ */
+public class VirtualNode extends MMObjectNode {
+    /**
+     * Logger routine
+     */
+    private static Logger log = Logging.getLoggerInstance(VirtualNode.class.getName());
+
+    /**
+     * Main contructor.
+     * @param parent the node's parent, generally (but not necessarily) an instance
+     * of a virtual builder.
+     */
+    public VirtualNode(MMObjectBuilder parent) {
+        super(parent);
+        virtual=true;
+    }
+
+     /**
+      * commit : commits the node to the database or other storage system.
+      * Generally, commiting a virtual node has no effect, so the basic
+      * implementation returns false.
+      * @return <code>false</code>
+      */
+    public boolean commit() {
+      return false;
+    }
+
+    /**
+     *  Insert is not implemented on a virtual node.
+     *  @return nothing, throws an exception
+     *  @throw UnsupportedOperationException
+     */
+    public int insert(String userName) {
+        throw new UnsupportedOperationException("Method insert is not implemented on a virtual node.");
+    }
+
+    /**
+     * Returns whether this node has relations.
+     * A virtual node never has relations.
+     * @return <code>false</code>
+     */
+    public boolean hasRelations() {
+        return false;
+    }
+
+    /**
+     * Return the relations of this node.
+     * A virtual node never has relations.
+     * @return <code>null</code>
+     */
+    public Enumeration getRelations() {
+        return null;
+    }
+
+    /**
+     * Returns the number of relations of this node.
+     * A virtual node never has relations.
+     * @return 0
+     */
+    public int getRelationCount() {
+        return 0;
+    }
+
+    /**
+     * Return the number of relations of this node, filtered on a specified type.
+     * A virtual node never has relations.
+     * @param wantedtype the 'type' of related nodes (NOT the relations!).
+     * @return An <code>int</code> indicating the number of nodes found
+     */
+    public int getRelationCount(String wantedtype) {
+        return 0;
+    }
+
+    /**
+     * Returns the node's age
+     * A virtual node is always new (0)
+     * @return the age in days (0)
+     */
+    public int getAge() {
+        return 0;
+    }
+
+}
