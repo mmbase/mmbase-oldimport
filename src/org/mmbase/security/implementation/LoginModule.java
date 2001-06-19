e15b1ea330690d82386bce5cfed21d4893581a66
eduard <eduard@91430c5f-d668-0410-9e7d-dd663366dcfd>
2001-06-19 17:03:31 +0000

    eduard : still restructuring..
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@2270 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/security/implementation/LoginModule.java
+++
@@ -1,8 +0,0 @@
-package org.mmbase.security.implementation;
-
-import java.util.HashMap;
-
-public interface LoginModule {
-    public void load(HashMap properties);
-    public boolean login(NameContext user, HashMap loginInfo,  Object[] parameters);
-}
