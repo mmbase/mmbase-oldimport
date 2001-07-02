45a3396a0b4337e9f4528a72e7bd3c14347e853d
pierre <pierre@91430c5f-d668-0410-9e7d-dd663366dcfd>
2001-07-02 10:36:56 +0000

    pierre: added getNodeList() method for calling ProcessorModule from the MMCI. This method returns a vector of MMObjectnodes. Default method calles normal getList(), but allows for optimization.
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@2374 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/ProcessorInterface.java
+++ src/org/mmbase/module/ProcessorInterface.java
@@ -11,34 +11,59 @@ package org.mmbase.module;
 
 import java.util.*;
 import java.lang.*;
+import org.mmbase.module.core.*;
 import org.mmbase.util.*;
 
 /**
  */
 public interface  ProcessorInterface  {
 
-	/**
-	 * Generate a list of values from a command to the processor
-	 */
-	abstract public Vector getList(scanpage sp,StringTagger tagger, String value) throws ParseException;
+    /**
+     * Returns a virtual builder used to create node lists from the results
+     * returned by getList().
+     * The default method does not associate the builder with a cloud (mmbase module),
+     * so processormodules that need this association need to override this method.
+     * Note that different lists may return different builders.
+     * @param command the LIST command for which to retrieve the builder
+     * @param params contains the attributes for the list
+     */
+    public MMObjectBuilder getListBuilder(String command,Map params);
 
-	/**
-	 * Execute the commands provided in the form values
-	 */
-	abstract public boolean process(scanpage sp, Hashtable cmds,Hashtable vars);
+    /**
+     * Generate a list of values from a command to the processor.
+     * The values are grouped into nodes.
+     * @param context the context of the page or calling application (currently, this should be a scanpage object)
+     * @param command the list command to execute.
+     * @param params contains the attributes for the list
+     * @return a <code>Vector</code> that contains the list values contained in MMObjectNode objects
+     */
+    public Vector getNodeList(Object context,String command, Map params) throws ParseException;
 
-	/**
-	*	Replace a command by a string 
-	*/
-	abstract public String replace (scanpage sp, String command);
+    /**
+     * Generate a list of values from a command to the processor
+     * @param sp the page context
+     * @param tagger contains the attributes for the list
+     * @param command the list command to execute.
+     */
+    abstract public Vector getList(scanpage sp,StringTagger tagger, String value) throws ParseException;
 
-	/**
-	*	Replace a command by a string 
-	*/
-	abstract public String replace (scanpage sp, StringTagger command);
+    /**
+     * Execute the commands provided in the form values
+     */
+    abstract public boolean process(scanpage sp, Hashtable cmds,Hashtable vars);
 
-	/**
-	* Do a cache check (304) for this request
-	*/
-	abstract public boolean cacheCheck(scanpage sp,String cmd);
+    /**
+     *    Replace a command by a string
+     */
+    abstract public String replace (scanpage sp, String command);
+
+    /**
+     *    Replace a command by a string
+     */
+    abstract public String replace (scanpage sp, StringTagger command);
+
+    /**
+     * Do a cache check (304) for this request
+     */
+    abstract public boolean cacheCheck(scanpage sp,String cmd);
 }
--- src/org/mmbase/module/ProcessorModule.java
+++ src/org/mmbase/module/ProcessorModule.java
@@ -17,6 +17,7 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+import org.mmbase.module.core.*;
 import org.mmbase.util.*;
 import org.mmbase.util.logging.Logger;
 import org.mmbase.util.logging.Logging;
@@ -30,56 +31,122 @@ import org.mmbase.util.logging.Logging;
  */
 public class ProcessorModule extends Module implements ProcessorInterface {
 
-	/**
-	 * Generate a list of values from a command to the processor
-	 */
-	public Vector  getList(scanpage sp,StringTagger tagger, String value) throws ParseException {
-		throw new ParseException("Module " + this.getClass().getName() + " does not implement LIST");
-	}
+    /**
+     * Returns a virtual builder used to create node lists from the results
+     * returned by getList().
+     * The default method does not associate the builder with a cloud (mmbase module),
+     * so processormodules that need this association need to override this method.
+     * Note that different lists may return different builders.
+     * @param command the LIST command for which to retrieve the builder
+     * @param params contains the attributes for the list
+     */
+    public MMObjectBuilder getListBuilder(String command,Map params) {
+        return new VirtualBuilder(null);
+    }
 
-	/**
-	 * Execute the commands provided in the form values
-	 */
-	public boolean process(scanpage sp, Hashtable cmds,Hashtable vars) {
-		return(false);
-	}
+    /**
+     * Generate a list of values from a command to the processor.
+     * The values are grouped into nodes.
+     * @param context the context of the page or calling application (currently, this should be a scanpage object)
+     * @param command the list command to execute.
+     * @param params contains the attributes for the list
+     * @return a <code>Vector</code> that contains the list values contained in MMObjectNode objects
+     */
+    public Vector getNodeList(Object context, String command, Map params) throws ParseException {
+        StringTagger tagger=null;
+        if (params instanceof StringTagger) {
+            tagger= (StringTagger)params;
+        } else {
+            tagger= new StringTagger("");
+            if (params!=null) {
+                for (Iterator keys=params.keySet().iterator(); keys.hasNext(); ) {
+                    String key=(String)keys.next();
+                    Object o = params.get(key);
+                    if (o instanceof Vector) {
+                        tagger.setValues(key,(Vector)o);
+                    } else {
+                        tagger.setValue(key,""+o);
+                    }
+                }
+            }
+        }
+        scanpage sp=null;
+        if (context instanceof scanpage) {
+            sp = (scanpage)context;
+        }
+        Vector v=getList(sp,tagger,command);
+        int items=1;
+        try { items=Integer.parseInt(tagger.Value("ITEMS")); } catch (NumberFormatException e) {}
+        Vector fieldlist=tagger.Values("FIELDS");
+        Vector res=new Vector(v.size() / items);
+        MMObjectBuilder bul= getListBuilder(command,params);
+        for(int i= 0; i<v.size(); i+=items) {
+            MMObjectNode node = new MMObjectNode(bul);
+            for(int j= 0; (j<items) && (j<v.size()); j++) {
+                if ((fieldlist!=null) && (j<fieldlist.size())) {
+                    node.setValue((String)fieldlist.get(j),v.get(i+j));
+                } else {
+                    node.setValue("item"+(j+1),v.get(i+j));
+                }
+            }
+            res.add(node);
+        }
+        return res;
+    }
 
-	/**
-	*	Replace a command by a string 
-	*/
-	public String replace (scanpage sp, String command) {
-		return("This module doesn't implement this processor call");
-	}
+    /**
+     * Generate a list of values from a command to the processor
+     * @param sp the page context
+     * @param params contains the attributes for the list
+     * @param command the list command to execute.
+     */
+    public Vector  getList(scanpage sp,StringTagger params, String command) throws ParseException {
+        throw new ParseException("Module " + this.getClass().getName() + " does not implement LIST");
+    }
 
-	/**
-	*	Replace a command by a string 
-	* who the hell uses this (daniel)
-	*/
-	public String replace (scanpage sp, StringTagger command) {
-		return("This module doesn't implement this processor call");
-	}
+    /**
+     * Execute the commands provided in the form values
+     */
+    public boolean process(scanpage sp, Hashtable cmds,Hashtable vars) {
+        return(false);
+    }
 
-	/**
-	* Do a cache check (304) for this request
-	*/
-	public boolean cacheCheck(scanpage sp,String cmd) {
-		return(false);
-	}
+    /**
+    *    Replace a command by a string
+    */
+    public String replace (scanpage sp, String command) {
+        return("This module doesn't implement this processor call");
+    }
+
+    /**
+    *    Replace a command by a string
+    * who the hell uses this (daniel)
+    */
+    public String replace (scanpage sp, StringTagger command) {
+        return("This module doesn't implement this processor call");
+    }
+
+    /**
+    * Do a cache check (304) for this request
+    */
+    public boolean cacheCheck(scanpage sp,String cmd) {
+        return(false);
+    }
 
 
-	public void init() {
-	}
+    public void init() {
+    }
 
-	public void reload() {
-	}
+    public void reload() {
+    }
 
-	public void onload() {
-	}
+    public void onload() {
+    }
 
-	public void unload() {
-	}
+    public void unload() {
+    }
 
-	public void shutdown() {
-	}
+    public void shutdown() {
+    }
 
 }
