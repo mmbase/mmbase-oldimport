cc15f7407f1249c9ed38f9f116ee6aa099015cb8
vpro <vpro@91430c5f-d668-0410-9e7d-dd663366dcfd>
2001-07-02 12:08:39 +0000

    Davzev: Changed path to the remote property files
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@2386 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/remote/startRemoteBuilders.sh
+++ src/org/mmbase/remote/startRemoteBuilders.sh
@@ -3,7 +3,7 @@
 CLASSPATH=
 export CLASSPATH
 
-cd /usr/local/webserver/nl/vpro/mmbase/config/plongrunner
+cd /PathToTheRemotePropertyFiles
 
 while [ 1=1 ]; do
 /usr/local/java/bin/java org.mmbase.remote.startRemoteBuilders server.properties g2encoder.properties cdplayer.properties 2>&1 | cat >>remoteBuilder.log
