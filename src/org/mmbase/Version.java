32ed174f8f2e04140db077e9f4493b351f2a813a
daniel <daniel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2001-07-24 16:25:33 +0000

    added a help file
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@2558 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/Version.java
+++ src/org/mmbase/Version.java
@@ -24,29 +24,56 @@ public class Version {
 
 
     public static void main(String args[]) {
-	System.out.println("Version report tool 0.1");
-	String cmd=args[0];
-	if (cmd!=null) {
+	System.out.println("\n\n Version report tool 0.2 ");
+	System.out.println("=========================\n");
+	if (args.length>0) {
+		String cmd=args[0];
 		if (cmd.equals("list")) {
 			performList(args[1]);
+		} else if (cmd.equals("help")) {
+			showHelp();
 		} else if (cmd.equals("check")) {
 			performCheck(args[1],args[2]);
 		} else if (cmd.equals("checklist")) {
-			performCheckList(args[1],args[2]);
+			performCheckList(args[1],args[2],false);
+		} else if (cmd.equals("checknew")) {
+			performCheckList(args[1],args[2],true);
 		} else if (cmd.equals("create")) {
 			createManifest(args[1],args[2],args[3]);
 		}
 	} else {
-		System.out.println("No command found");
+		System.out.println("No command found, showing help\n");
+		showHelp();
 	}
     }
 
+    public static void showHelp() {
+	System.out.println("usage   : [command] [param1] [param2] [...]\n");
+	System.out.println("format  : check localjar remote-manifest");
+	System.out.println("info    : compare mmbase.jar version against a manifest file");
+	System.out.println("example : check /usr/mmrunner/lib/mmbase.jar http://www.mmbase.org/cvs/MANIFEST.MF\n");
+	System.out.println("format  : checklist localjar remote-manifest");
+	System.out.println("info    : compare mmbase.jar version per classfile");
+	System.out.println("example : checklist /usr/mmrunner/lib/mmbase http://www.mmbase.org/cvs/MANIFEST.MF\n");
+	System.out.println("format  : create sourcepath/cvsfile manifest-file versionfile");
+	System.out.println("info    : create manifest file from source or cvs version file");
+	System.out.println("example : create /tmp/cvsversions.log /tmp/MANIFEST.MF /tmp/version.txt");
+	System.out.println("remark  : normally only used by release maintainer/admin\n");
+    }
+
     public static void createManifest(String path,String targetfile,String versionfile) {
-	System.out.println("scanning : "+path+"/* for target manifest : "+targetfile);
 	String body="Manifest-Version: 1.0\n";
 		
 	body+=getMainMMBaseVersion(versionfile);
-	body+=getManifestFile(path);
+
+	// check if input is a cvs log or a src dir
+	File fl=new File(path);
+	if (fl.isDirectory()) {
+		body+=getManifestFile(path);
+	} else {
+		body+=getManifestCVS(path);
+	}
+	System.out.println("writing new manifest file : "+targetfile);
 	saveFile(targetfile,body);
     }
 
@@ -79,16 +106,56 @@ public class Version {
 	String body="Name: MMBase Content Management System\n";
 	body+="Created-By: "+maintainer+"\n";
 	body+="Implementation-Vendor: mmbase organisation\n";
-	body+="Implementation-Version: "+major+"."+minor+"."+(build+1)+"\n\n";
+	body+="Implementation-Version: "+major+"."+minor+"."+(build+1)+"\n";
+	body+="Implementation-Date: "+new Date().toGMTString()+"\n\n";
 	String newbody="maintainer="+maintainer+"\n";
 	newbody+="major="+major+"\n";
 	newbody+="minor="+minor+"\n";
 	newbody+="build="+(build+1)+"\n";
+	System.out.println("updating version file : "+versionfile+" to : "+major+"."+minor+"."+(build+1));
 	saveFile(versionfile,newbody);
 	return(body);
     }
 
+
+    public static String getManifestCVS(String filename) {
+	System.out.println("reading : "+filename+" for target manifest");
+	String body="";
+	String cvslog=loadFile(filename);
+	StringTokenizer tok=new StringTokenizer(cvslog,"\n\r");
+	while (tok.hasMoreTokens()) {
+		// read 9 lines
+		String l1=tok.nextToken();
+		String fileline=tok.nextToken();
+		String wline=tok.nextToken();
+		String rline=tok.nextToken();
+		String l6=tok.nextToken();	
+		String l7=tok.nextToken();	
+		String l8=tok.nextToken();	
+		// convert the rline to a valid package name
+		if (rline.endsWith(".java,v")) {
+			int pos=rline.indexOf("/cvs/");
+			if (pos!=-1) {
+				String p="org."+rline.substring(pos+5,rline.length()-7);
+				p=p.replace('/','.');
+				String v=rline.substring(24);
+				int pos2=v.indexOf("\t");
+				if (pos2!=-1) {
+					v=v.substring(0,pos2);
+				}
+				if (!p.equals("org.mmbase.Version")) {
+					body+="Name: "+p+"\n";
+					body+="Created-By: www.mmbase.org\n";
+					body+="Implementation-Version: "+v+"\n\n";
+				}
+			}
+		}
+	}
+	return(body);
+    }
+
     public static String getManifestFile(String path) {
+	System.out.println("scanning : "+path+"/* for target manifest");
 	String body="";
 	File fl=new File(path);
 	if (fl.isDirectory()) {
@@ -133,14 +200,15 @@ public class Version {
     public static void performCheck(String file,String checkurl) {
 	System.out.println("checking : "+file+" against public manifest : "+checkurl);
 	try {
+		JarFile jf=new JarFile(file);
+		Manifest mf=jf.getManifest();
+		System.out.println("local manifest : loaded");
+
 		URL ur=new URL(checkurl);
 		InputStream in=ur.openStream();	
 		Manifest remotemf=new Manifest(in);
-		System.out.println("remote manifest : "+remotemf);
+		System.out.println("remote manifest : loaded");
 
-		JarFile jf=new JarFile(file);
-		Manifest mf=jf.getManifest();
-		System.out.println("local manifest : "+mf);
 
 		if (mf.equals(remotemf)) {
 			System.out.println("mmbase.jar up to date");
@@ -154,17 +222,17 @@ public class Version {
     }
 
 
-    public static void performCheckList(String file,String checkurl) {
+    public static void performCheckList(String file,String checkurl,boolean newonly) {
 	System.out.println("checking : "+file+" against public manifest : "+checkurl);
 	try {
 		URL ur=new URL(checkurl);
 		InputStream in=ur.openStream();	
 		Manifest remotemf=new Manifest(in);
-		System.out.println("remote manifest : "+remotemf);
+		System.out.println("remote manifest : loaded");
 
 		JarFile jf=new JarFile(file);
 		Manifest mf=jf.getManifest();
-		System.out.println("local manifest : "+mf);
+		System.out.println("local manifest : loaded");
 
 		Iterator r=remotemf.getEntries().keySet().iterator();
 		while (r.hasNext()) {
@@ -175,11 +243,17 @@ public class Version {
 			Attributes remoteat=remotemf.getAttributes(key);
 			String remoteversion=remoteat.getValue("Implementation-Version");
 			String remotecreatedby=remoteat.getValue("Created-By");
+			/*
+			System.out.println("version="+version);
+			System.out.println("createdby="+createdby);
+			*/
 
 			if (version.equals(remoteversion)) {
+				if (!newonly) {
 				System.out.println("valid : "+key+" "+version+" "+createdby);
+				}
 			} else {
-				System.out.println("problem : "+key+" "+version+" "+createdby+" (remote version : "+remoteversion+")");
+				System.out.println("newer version : "+key+" "+version+" "+createdby+" (remote version : "+remoteversion+")");
 			}
 		
 		}
