3793970c304841e8a86db1b16c673511d39e3fb8
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2001-09-21 12:04:04 +0000

    michiel: escaping for SQL queries
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@2754 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/util/transformers/Sql.java
@@ -0,0 +1,50 @@
+package org.mmbase.util.transformers;
+
+import java.io.Reader;
+import java.io.Writer;
+import java.util.HashMap;
+
+import org.mmbase.util.Escape;
+
+/**
+ * Encodings related to Sql. It can escape quotes, by replacing them by double quotes, as is 
+ * needed in SQL statements.
+ *
+ * @author Michiel Meeuwissen 
+ */
+
+public class Sql extends AbstractTransformer implements CharTransformer {
+    
+    private final static int ESCAPE_QUOTES    = 1;     
+
+    /**
+     * Used when registering this class as a possible Transformer
+     */
+
+    public HashMap transformers() {
+        HashMap h = new HashMap();
+        h.put("escape_single_quote".toUpperCase(), new Config(Sql.class, ESCAPE_QUOTES, "Escape single quotes for SQL statements"));
+        return h;
+    }
+
+    public Writer transform(Reader r) {
+        throw new UnsupportedOperationException("transform(Reader) is not yet supported");
+    }
+    public Writer transformBack(Reader r) {
+        throw new UnsupportedOperationException("transformBack(Reader) is not yet supported");
+    } 
+
+    public String transform(String r) {
+        switch(to){
+        case ESCAPE_QUOTES:           return Escape.singlequote(r);
+        default: throw new UnsupportedOperationException("Cannot transform");
+        }
+    }
+    public String transformBack(String r) {
+        switch(to){
+        case ESCAPE_QUOTES:           throw new UnsupportedOperationException("Not needed to revert this at anytime it tinks");
+        default: throw new UnsupportedOperationException("Cannot transform");
+        }
+    } 
+
+}
