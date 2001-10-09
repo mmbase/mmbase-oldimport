221277d7525ddd4273c4105f96a1c7cda0ee7a8e
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2001-10-09 09:58:40 +0000

    michiel: made same arangement as in Images.java for the GUI-indcators. Reindented
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@2827 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/builders/ImageCaches.java
+++ src/org/mmbase/module/builders/ImageCaches.java
@@ -24,84 +24,98 @@ import org.mmbase.util.logging.*;
 public class ImageCaches extends MMObjectBuilder {
 
     private static Logger log = Logging.getLoggerInstance(ImageCaches.class.getName());
-	private LRUHashtable handlecache=new LRUHashtable(128);
-
-	public String getGUIIndicator(MMObjectNode node) {
-		int num=node.getIntValue("id");
-		if (num!=-1) {
-			return("<IMG SRC=\"/img.db?"+node.getIntValue("id")+"+s(100x60)\">");
-		}
-		return(null);
-		/*
-		String str=node.getStringValue("title");
-		if (str.length()>15) {
-			return(str.substring(0,12)+"...");
-		} else {
-			return(str);
-		}
-		*/
-	}
-
-	public String getGUIIndicator(String field,MMObjectNode node) {
-		if (field.equals("handle")) {
-			int num=node.getIntValue("id");
-			if (num!=-1) {
-				return("<IMG SRC=\"/img.db?"+num+"+s(100x60)\">");
-			}
-		}
-		return(null);
-	}
-
-	public String getDefaultUrl(int src) {
-		MMObjectNode node=getNode(src);
-		String url=node.getStringValue("url");
-		return(url);
-	}
-
-	public synchronized byte[] getCkeyNode(String ckey) {
-		ByteArray b;
-		byte[] rtn=null;
+    private LRUHashtable handlecache=new LRUHashtable(128);
+    
+    public String getGUIIndicator(MMObjectNode node) {
+        // code copied from Images.java
+        int num = node.getIntValue("id");
+        if (num == -1 ) {   // img.db cannot handle uncommited images..
+            return null; // ObjectBuilder itself will handle this case.
+        }
+        // NOTE that this has to be configurable instead of static like this
+        String servlet    = MMBaseContext.getHtmlRootUrlPath() + "img.db";
+        String image      = servlet + "?" + num;
+        String imagethumb = image   + "+s(100x60)";
+        String title      = node.getStringValue("title");
+        return ("<a href=\"" + image + "\" target=\"_new\"><img src=\"" + imagethumb + "\" border=\"0\" alt=\"" + title + "\" /></a>");
+        /*
+          String str=node.getStringValue("title");
+          if (str.length()>15) {
+          return(str.substring(0,12)+"...");
+          } else {
+          return(str);
+          }
+        */
+    }
+    
+    public String getGUIIndicator(String field,MMObjectNode node) {
+        // code was copied from Images.java, yeach...
+        if (field.equals("handle")) { 
+            int num = node.getIntValue("id");
+            if(num == -1) { // img.db cannot handle uncommited images..
+                return null;
+            }
+            // NOTE that this has to be configurable instead of static like this
+            String servlet    = MMBaseContext.getHtmlRootUrlPath() + "img.db";
+            String image      = servlet + "?" + num;
+            String imagethumb = servlet + "?" + num + "+s(100x60)";
+            
+            return("<a href=\"" + image + "\" target=\"_new\"><img src=\"" + imagethumb + "\" border=\"0\" alt=\"*\" /></a>");
+        }
+        // other fields can be handled by the gui function...
+        return null;
+    }
+    
+    public String getDefaultUrl(int src) {
+        MMObjectNode node=getNode(src);
+        String url=node.getStringValue("url");
+        return(url);
+    }
+    
+    public synchronized byte[] getCkeyNode(String ckey) {
+        ByteArray b;
+        byte[] rtn=null;
 		
-		b=(ByteArray)handlecache.get(ckey);
-		if (b==null) {
-			try {
-				MultiConnection con=mmb.getConnection();
-				Statement stmt2=con.createStatement();
+        b=(ByteArray)handlecache.get(ckey);
+        if (b==null) {
+            try {
+                MultiConnection con=mmb.getConnection();
+                Statement stmt2=con.createStatement();
+                
+                
 
+                ResultSet rs=stmt2.executeQuery("SELECT "+mmb.getDatabase().getAllowedField("number")+" FROM "+mmb.baseName+"_icaches WHERE ckey='"+ckey+"'");
+                if (rs.next()) {
+                    int number=rs.getInt(1);
+                    MMObjectNode n2=getNode(number);
+                    byte[] bytes=n2.getByteValue("handle");	
+                    rtn=bytes;
+                    if (rtn!=null && bytes.length<(100*1024)) handlecache.put(ckey,new ByteArray(rtn));
+                }
+                stmt2.close();
+                con.close();
+            } catch (Exception e) {
+                log.error("getCkeyNode error "+ckey+":"+toHexString(ckey));
+                e.printStackTrace();
+            }
+        } else {
+            rtn=b.getBytes();
+        }
+        if (debug && rtn==null) {
+            log.debug("getCkeyNode: empty array returned for ckey "+ckey);
+        }
+        return(rtn);
+    }
 
-
-				ResultSet rs=stmt2.executeQuery("SELECT "+mmb.getDatabase().getAllowedField("number")+" FROM "+mmb.baseName+"_icaches WHERE ckey='"+ckey+"'");
-				if (rs.next()) {
-					int number=rs.getInt(1);
-					MMObjectNode n2=getNode(number);
-					byte[] bytes=n2.getByteValue("handle");	
-					rtn=bytes;
-					if (rtn!=null && bytes.length<(100*1024)) handlecache.put(ckey,new ByteArray(rtn));
-				}
-				stmt2.close();
-				con.close();
-			} catch (Exception e) {
-				log.error("getCkeyNode error "+ckey+":"+toHexString(ckey));
-				e.printStackTrace();
-			}
-		} else {
-			rtn=b.getBytes();
-		}
-		if (debug && rtn==null) {
-			log.debug("getCkeyNode: empty array returned for ckey "+ckey);
-		}
-		return(rtn);
-	}
-
-	private String toHexString(String str) {
-		StringBuffer b=new StringBuffer();
-		char[] chb;
-		chb=str.toCharArray();
-		for (int i=0;i<chb.length;i++) {
-			b.append(Integer.toString((int)chb[i],16)+",");
-		}
-		return(b.toString());
-	}	
+    private String toHexString(String str) {
+        StringBuffer b=new StringBuffer();
+        char[] chb;
+        chb=str.toCharArray();
+        for (int i=0;i<chb.length;i++) {
+            b.append(Integer.toString((int)chb[i],16)+",");
+        }
+        return(b.toString());
+    }	
 
     /**
      * Invalidate the Image Cache for a specific Node
@@ -135,13 +149,13 @@ public class ImageCaches extends MMObjectBuilder {
 }
 
 class ByteArray {
-	byte[] bytes;
+    byte[] bytes;
 
-	ByteArray(byte[] b) {
-		bytes=b;
-	}
+    ByteArray(byte[] b) {
+        bytes=b;
+    }
 
-	public byte[] getBytes() {
-		return(bytes);
-	}
+    public byte[] getBytes() {
+        return(bytes);
+    }
 }
