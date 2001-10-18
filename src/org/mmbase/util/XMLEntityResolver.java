c525f1e1d9137050b01112d58c3aea21f5b16247
pierre <pierre@91430c5f-d668-0410-9e7d-dd663366dcfd>
2001-10-18 11:05:08 +0000

    pierre: XMLEntityResolver now returns an empty DTD, rather than null, when used outside the MMBase context. This avoids crashes when validating XML documents outside the server context.
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@2873 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/util/XMLEntityResolver.java
+++ src/org/mmbase/util/XMLEntityResolver.java
@@ -17,18 +17,17 @@ import org.mmbase.module.core.MMBaseContext;
 import org.mmbase.util.logging.*;
 
 /**
- * Take the systemId and converts it into a local file
- *
+ * Take the systemId and converts it into a local file, using the MMBase config path
  *
  * @author Gerard van Enk
- * @version $Revision: 1.10 $ $Date: 2001-07-16 10:08:16 $
+ * @version $Revision: 1.11 $ $Date: 2001-10-18 11:05:08 $
  */
 public class XMLEntityResolver implements EntityResolver {
 
     private static Logger log = Logging.getLoggerInstance(XMLEntityResolver.class.getName());
 
-    String dtdpath;
-    boolean hasDTD; // tells whether or not a DTD is set - if not, no validition can take place
+    private String dtdpath;
+    private boolean hasDTD; // tells whether or not a DTD is set - if not, no validition can take place
 
     /**
      * empty constructor
@@ -41,31 +40,44 @@ public class XMLEntityResolver implements EntityResolver {
     /**
      * takes the systemId and returns the local location of the dtd
      */
-    public InputSource resolveEntity(String publicId, String systemId)
-    throws SAXException, IOException {
+    public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
 
-        if (systemId != null) {
+        //does systemId contain a mmbase-dtd
+        if ((systemId == null) || (systemId.indexOf("http://www.mmbase.org/") == -1)) {
+            // it's a systemId we can't do anything with,
+            // so let the parser decide what to do
+            return null;
+        } else if (!canResolve()) {
+            // cannot determine the dtd - create an empty dtd stream instead
+            return new InputSource(new StringReader(""));
+        } else {
             hasDTD = true;
-            //does systemId contain a mmbase-dtd
-            if (systemId.indexOf("http://www.mmbase.org/") != -1) {
-                int i = systemId.indexOf("/dtd/");
-                String dtdName = systemId.substring(i+5);
-                String configpath = MMBaseContext.getConfigPath();
-                String dtdLocation = configpath + File.separator + "dtd"
-		                     + File.separator + dtdName;
-                log.debug("dtdLocation = "+dtdLocation);
-                InputStreamReader dtdInputStreamReader =
-                    new InputStreamReader(new FileInputStream(dtdLocation));
-                InputSource dtdInputSource = new InputSource();
-                dtdInputSource.setCharacterStream(dtdInputStreamReader);
-                dtdpath = dtdLocation;
-                return dtdInputSource;
-            }
+            int i = systemId.indexOf("/dtd/");
+            String dtdName = systemId.substring(i+5);
+            String configpath = MMBaseContext.getConfigPath();
+            if (configpath==null) return null;
+            String dtdLocation = configpath+File.separator+"dtd"+File.separator+dtdName;
+            log.debug("dtdLocation = "+dtdLocation);
+            InputStreamReader dtdInputStreamReader =
+                new InputStreamReader(new FileInputStream(dtdLocation));
+            InputSource dtdInputSource = new InputSource();
+            dtdInputSource.setCharacterStream(dtdInputStreamReader);
+            dtdpath = dtdLocation;
+            return dtdInputSource;
         }
-        // it's a systemId we can't do anything with,
-        // so let the parser decide what to do
-        return null;
     }
+
+    /**
+     * Returns whether the resolver has enough environmental information to resolve the DTD.
+     * @return whether the resolver can resolve DTDs
+     */
+    public boolean canResolve() {
+        return MMBaseContext.isInitialized();
+    }
+
+    /**
+     * @return whether the resolver has determiend a DTD
+     */
     public boolean hasDTD() {
         return hasDTD;
     }
