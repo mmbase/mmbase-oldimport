e1c71a4c23317c83329d765bb7cda2676ca39d17
pierre <pierre@91430c5f-d668-0410-9e7d-dd663366dcfd>
2002-01-23 15:05:42 +0000

    pierre: tagged code
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@3141 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/CALC.java
+++ src/org/mmbase/module/CALC.java
@@ -18,57 +18,73 @@ import org.mmbase.util.*;
 import org.mmbase.util.logging.*;
 
 /**
+ * @application SCAN
+ * @javadoc
+ * @rename Calc.java
  * @author Daniel Ockeloen
+ * @version $Id: CALC.java,v 1.8 2002-01-23 15:05:42 pierre Exp $
  */
 public class CALC extends ProcessorModule {
+    // logging
     private static Logger log = Logging.getLoggerInstance(CALC.class.getName());
- 
-	/**
-	 * Generate a list of values from a command to the processor
-	 */
-	 public Vector  getList(scanpage sp,StringTagger tagger, String value) throws ParseException {
-    	String line = Strip.DoubleQuote(value,Strip.BOTH);
-		StringTokenizer tok = new StringTokenizer(line,"-\n\r");
-		if (tok.hasMoreTokens()) {
-			String cmd=tok.nextToken();	
-		}
-		return(null);
-	}
 
-	/**
-	 * Execute the commands provided in the form values
-	 */
-	public boolean process(scanpage sp, Hashtable cmds,Hashtable vars) {
-		log.debug("CMDS="+cmds);
-		log.debug("VARS="+vars);
-		return(false);
-	}
+    /**
+     * Generate a list of values from a command to the processor
+     * @javadoc
+     * @deprecated-now doesn't add any functionality
+     */
+     public Vector  getList(scanpage sp,StringTagger tagger, String value) throws ParseException {
+        String line = Strip.DoubleQuote(value,Strip.BOTH);
+        StringTokenizer tok = new StringTokenizer(line,"-\n\r");
+        if (tok.hasMoreTokens()) {
+            String cmd=tok.nextToken();
+        }
+        return null;
+    }
 
-	/**
-	*	Handle a $MOD command
-	*/
-	public String replace(scanpage sp, String cmds) {
-		StringTokenizer tok = new StringTokenizer(cmds,"-\n\r");
-		if (tok.hasMoreTokens()) {
-			String cmd=tok.nextToken();	
-			while (tok.hasMoreTokens()) {
-				cmd+="-"+tok.nextToken();
-			}
-			return(doCalc(cmd));
-		}
-		log.warn("Calc replace was involed with no decent command");
-		return("No command defined");
-	}
-	
-	String doCalc(String cmd) {
-		log.service("Calc module calculates "+cmd);
-		ExprCalc cl=new ExprCalc(cmd);
-		// marcel: annoying, upgraded this log to debug
-		log.debug("Calc converts number Natural number");
-		return(""+(int)(cl.getResult()+0.5));
-	}
+    /**
+     * Execute the commands provided in the form values
+     * @javadoc
+     * @deprecated-now doesn't add any functionality
+     */
+    public boolean process(scanpage sp, Hashtable cmds,Hashtable vars) {
+        log.debug("CMDS="+cmds);
+        log.debug("VARS="+vars);
+        return false;
+    }
 
-	public String getModuleInfo() {
-		return("Support routines simple calc, Daniel Ockeloen");
-	}
+    /**
+     * Handle a $MOD command
+     * @javadoc
+     */
+    public String replace(scanpage sp, String cmds) {
+        StringTokenizer tok = new StringTokenizer(cmds,"-\n\r");
+        if (tok.hasMoreTokens()) {
+            String cmd=tok.nextToken();
+            while (tok.hasMoreTokens()) {
+                cmd+="-"+tok.nextToken();
+            }
+            return doCalc(cmd);
+        }
+        log.warn("Calc replace was involed with no decent command");
+        return "No command defined";
+    }
+
+    /**
+     * @javadoc
+     */
+    String doCalc(String cmd) {
+        log.service("Calc module calculates "+cmd);
+        ExprCalc cl=new ExprCalc(cmd);
+        // marcel: annoying, upgraded this log to debug
+        log.debug("Calc converts number Natural number");
+        return ""+(int)(cl.getResult()+0.5);
+    }
+
+    /**
+     * @javadoc
+     */
+    public String getModuleInfo() {
+        return "Support routines simple calc, Daniel Ockeloen";
+    }
 }
