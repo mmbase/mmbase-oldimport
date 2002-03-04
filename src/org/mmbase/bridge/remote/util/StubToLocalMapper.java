59d71c64f73ec531d8228590112de578ba2d7324
kees <kees@91430c5f-d668-0410-9e7d-dd663366dcfd>
2002-03-04 16:30:27 +0000

    keesj:added more debug info(map size and types of object in debug mode)
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@3373 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/bridge/remote/util/StubToLocalMapper.java
+++ src/org/mmbase/bridge/remote/util/StubToLocalMapper.java
@@ -11,16 +11,19 @@ package org.mmbase.bridge.remote.util;
 
 import java.util.*;
 
+import org.mmbase.bridge.*;
 import org.mmbase.util.logging.Logging;
 import org.mmbase.util.logging.Logger;
+
 /**
  * StubToLocalMapper is a utitity class that helps
  * a Stub to find it's Local implementation
  * @author Kees Jongenburger
- * @version $Id: StubToLocalMapper.java,v 1.3 2002-02-07 13:12:22 kees Exp $
+ * @version $Id: StubToLocalMapper.java,v 1.4 2002-03-04 16:30:27 kees Exp $
  */
 public class StubToLocalMapper{
     static private Logger log = Logging.getLoggerInstance(StubToLocalMapper.class.getName());
+
     /**
      * private data member to keep track of mapperCode/object combinations
      */
@@ -35,7 +38,13 @@ public class StubToLocalMapper{
     public static String add(Object object){
         if (object != null){
             String mapperCode = "" + object.hashCode();
-            log.debug("adding an object to the StubToLocalMapper with mapperCode=("+ mapperCode +")");
+	    if (log.isDebugEnabled()){
+	    	StringBuffer sb = new StringBuffer();
+		sb.append("StubToLocalMapper.add{mapperCode=("+ mapperCode +")}");
+		sb.append(getObjectInfo(object));
+		sb.append(" SIZE=" + hash.size());
+            	log.debug(sb.toString());
+	    }
             hash.put(mapperCode,object);
             return mapperCode;
         }
@@ -53,12 +62,47 @@ public class StubToLocalMapper{
     }
     
     /**
+     * @param object the object to acquire information about
+     * @return a String containing information about the 
+     **/
+    private static String getObjectInfo(Object object){
+	if (object instanceof Node){
+	    Node node = (Node)object;
+	    return "node (type,number)=("+node.getNodeManager().getName() +","+ node.getNumber() +")";
+	} else if (object instanceof NodeManager){
+	    NodeManager nodeManager = (NodeManager)object;
+	    return "nodemanager (name)=("+nodeManager.getName() +")";
+	} else if (object instanceof NodeList){
+	    NodeList nodeList = (NodeList)object;
+	    return "nodelist (size)=("+nodeList.size() +")";
+	} else if (object instanceof Cloud){
+	    Cloud cloud = (Cloud)object;
+	    return "cloud (name)=("+cloud.getName() +")";
+	} else if (object instanceof CloudContext){
+	    CloudContext cloudContext = (CloudContext)object;
+	    return "cloudcontext";
+	} else if (object instanceof Module){
+	    Module module = (Module)object;
+	    return "module (name)("+ module.getName() +")";
+	} else {
+	    return object.getClass().getName();
+	}
+    }
+
+    /**
      * remove an entry in the StubToLocal mapper
      * @param mapperCode the MapperCode of the object to be removed
      */
     public static void remove(String mapperCode){
         if (mapperCode != null && ! mapperCode.equals("")){
-            log.debug("removing an object of StubToLocalMapper with mapperCode=("+ mapperCode +")");
+	    if (log.isDebugEnabled()){
+	        Object object = hash.get(mapperCode);
+	    	StringBuffer sb = new StringBuffer();
+		sb.append("StubToLocalMapper.remove{mapperCode=("+ mapperCode +")}");
+		sb.append(getObjectInfo(object));
+		sb.append(" SIZE=" + hash.size());
+            	log.debug(sb.toString());
+	    }
             hash.remove(mapperCode);
         }
     }
