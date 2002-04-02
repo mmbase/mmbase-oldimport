39927799340893c8409627b0d5b58bb58b18dd2c
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2002-04-02 13:34:25 +0000

    michiel: this class can be used as a Xalan extension to access the executefunctions of MMObjectBuilders in XSLt's
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@3608 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/bridge/util/xml/NodeFunction.java
@@ -0,0 +1,87 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+
+package org.mmbase.bridge.util.xml;
+
+import org.mmbase.bridge.*;
+import org.mmbase.util.logging.*;
+
+import org.apache.xpath.XPathAPI;
+
+/**
+ * Nodes of the bridge can have `virtual fields', which are in fact
+ * functions on the node. An example of such a function is "gui()". If
+ * you want to use these functions in an XSL transformation it is
+ * necessary to use this Xalan extension. The XSLT looks like this then:
+ *
+ <pre>
+  &lt;xsl:stylesheet  version = "1.0" 
+    xmlns:xsl ="http://www.w3.org/1999/XSL/Transform"
+    xmlns:node ="org.mmbase.bridge.util.xml.NodeFunction" 
+  &gt;
+  ..
+  </pre
+  You can then use a function like this if in the current template is a DOM Node with a 'field' subnode.
+  <pre>
+  ..
+  &lt;img src="{$formatter_imgdb}{node:function(., 'cache(s(180x180))')}" /&gt;
+  <pre>
+  And otherwise you can also feed it a number:
+  <pre>
+  ..
+  &lt;img src="{$formatter_imgdb}{node:function(string(@number), 'cache(s(180x180))')}" /&gt;
+  <pre>
+  Possibly even with the name of MMBase:
+  <pre>
+  ..
+  &lt;img src="{$formatter_imgdb}{node:function('mmbase', string(@number), 'cache(s(180x180))')}" /&gt;
+  <pre>
+ *
+ *
+ * @author  Michiel Meeuwissen
+ * @version $Id: NodeFunction.java,v 1.1 2002-04-02 13:34:25 michiel Exp $
+ * @since   MMBase-1.6
+ */
+public  class NodeFunction {
+    private static Logger log = Logging.getLoggerInstance(NodeFunction.class.getName());
+
+    /**
+     * Supposes the default cloud 'mmbase'.
+     * @see function
+     */
+
+    public static String function(String node, String function) {
+        log.debug("calling with string " + node);
+        return function("mmbase", node, function);
+    }
+    /**
+     * @param  cloudName The name of the Cloud.
+     * @param  number  The number (or alias) of the Node
+     * @param  function The function (with arguments).
+     * @return The result of the function (as a String)
+     */
+    public static String function(String cloudName, String number, String function) {
+        log.debug("calling base");
+        Cloud cloud = LocalContext.getCloudContext().getCloud(cloudName);        
+        Node node = cloud.getNode(number);
+        return node.getStringValue(function);
+    }
+    /**
+     * It can be handy to supply a whole node, it will search for the field 'number' itself.
+     */
+
+    public static String function(org.w3c.dom.Node node, String function) throws javax.xml.transform.TransformerException {
+        log.debug("calling with dom node");
+        String number = XPathAPI.eval(node, "./field[@name='number']").toString();
+        return function(number, function);
+    }
+
+
+}
