1bac9b5b8d3b580789983102a4abf12821bd0e96
robmaris <robmaris@91430c5f-d668-0410-9e7d-dd663366dcfd>
2002-05-30 09:30:57 +0000

    Fixed loadBuilder(). It assumed that the "builders" directory is in directory "config", which is not required. Also, a file separator had to be appended to the end of the constructed filepath.
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@4269 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/corebuilders/ObjectTypes.java
+++ src/org/mmbase/module/corebuilders/ObjectTypes.java
@@ -22,7 +22,7 @@ import org.mmbase.util.logging.*;
  * node.
  * TODO: update/merging code, and futher testing..
  * @author Eduard Witteveen
- * @version $Id: ObjectTypes.java,v 1.12 2002-05-29 14:41:42 michiel Exp $
+ * @version $Id: ObjectTypes.java,v 1.13 2002-05-30 09:30:57 robmaris Exp $
  */
 public class ObjectTypes extends TypeDef {
     private static Logger log = Logging.getLoggerInstance(ObjectTypes.class.getName());
@@ -320,16 +320,16 @@ public class ObjectTypes extends TypeDef {
     protected  MMObjectBuilder loadBuilder(MMObjectNode node) {
         log.debug("[load builder '" + node.getStringValue("name") + "' ( #"+node.getNumber()+")]");
         String path = getBuilderPath(node);
-        // remove everything till last 'config/builders/' 
+        // remove everything till last '/builders/' 
         // TODO: find a better way for whole file location stuff
-        String search = "config" + java.io.File.separator + "builders";
-        int pos = path.indexOf(search);
+        String search = java.io.File.separator + "builders";
+        int pos = path.lastIndexOf(search);
         if(pos == -1) {
             String msg = "could not retrieve the path to store the file..(path: " + path + " search: " + search + ")";
             log.fatal(msg);
             throw new RuntimeException(msg);
         }       
-        path = path.substring(pos + search.length());
+        path = path.substring(pos + search.length()) + java.io.File.separator;
         MMObjectBuilder builder = mmb.loadBuilderFromXML(node.getStringValue("name"), path);
         if(builder == null) {
             // inactive builder?
