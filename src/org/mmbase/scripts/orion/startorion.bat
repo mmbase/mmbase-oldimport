528662674ac162833e7d682702105b3fd4f5fdca
gerard <gerard@91430c5f-d668-0410-9e7d-dd663366dcfd>
2002-06-01 15:42:05 +0000

    gerard: removed a lot of very old scripts
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@4292 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/scripts/orion/startorion.bat
+++
@@ -1,9 +0,0 @@
-# set the orion directory
-set basepath=d:\ego\orion
-
-# jump to the correct harddisk
-d:
-
-cd %basepath%
-
-java -Dmmbase.config=%basepath%\org\mmbase\config\default -Dmmbase.htmlroot=%basepath%\default-web-app\ -jar orion.jar
