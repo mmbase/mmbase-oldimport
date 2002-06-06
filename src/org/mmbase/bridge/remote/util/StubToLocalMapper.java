58e7b2c323b7b20f199b60fdc29b452f9de4d9f7
kees <kees@91430c5f-d668-0410-9e7d-dd663366dcfd>
2002-06-06 08:39:05 +0000

    keesj:bugfix for http://www.mmbase.org/bugtracker/fullview.shtml?8564
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@4317 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/bridge/remote/util/StubToLocalMapper.java
+++ src/org/mmbase/bridge/remote/util/StubToLocalMapper.java
@@ -6,7 +6,7 @@ OSI Certified is a certification mark of the Open Source Initiative.
 The license (Mozilla version 1.0) can be read at the MMBase site.
 See http://www.MMBase.org/license
  
- */
+*/
 package org.mmbase.bridge.remote.util;
 
 import java.util.*;
@@ -18,7 +18,7 @@ import org.mmbase.util.logging.Logger;
  * StubToLocalMapper is a utitity class that helps
  * a Stub to find it's Local implementation
  * @author Kees Jongenburger
- * @version $Id: StubToLocalMapper.java,v 1.5 2002-04-10 07:05:31 kees Exp $
+ * @version $Id: StubToLocalMapper.java,v 1.6 2002-06-06 08:39:05 kees Exp $
  */
 public class StubToLocalMapper{
     static private Logger log = Logging.getLoggerInstance(StubToLocalMapper.class.getName());
@@ -39,56 +39,79 @@ public class StubToLocalMapper{
             String mapperCode = null;
 	    if (object instanceof Node){
 	    	Node node = (Node)object;
-	        mapperCode = "node:" + node.getNodeManager().getName() + "-" + node.getNumber();
+		mapperCode = "node:" + node.getNodeManager().getName() + "->" + node.getNumber();
+	    } else if (object instanceof Cloud){
+		Cloud cloud = (Cloud)object;
+		mapperCode = "cloud:" + cloud.getName() ;
 	    } else if (object instanceof Relation){
-	    	Relation rel = (Relation)object;
-	        mapperCode = "relation:" + rel.getNodeManager().getName() + "-" + rel.getNumber();
+		Relation rel = (Relation)object;
+		mapperCode = "relation:" + rel.getNodeManager().getName() + "->" + rel.getNumber();
+	    } else if (object instanceof RelationManager){
+		RelationManager relationManager = (RelationManager)object;
+		mapperCode = "relationmanager:" + relationManager.getName() + "->" + relationManager.hashCode();
 	    } else if (object instanceof NodeManager){
-	    	NodeManager nodeManager = (NodeManager)object;
-	        mapperCode = "nodemanager:" + nodeManager.getName() + "-" + nodeManager.hashCode();
+		NodeManager nodeManager = (NodeManager)object;
+		mapperCode = "nodemanager:" + nodeManager.getName() + "->" + nodeManager.hashCode();
 	    } else {
-	    	mapperCode = "" + object;
+		mapperCode = "" + object;
 	    }
+            //code neede to support transactions
+            //the generated key (like node:nodemanagername->nodeNumber) is currently wrong
+            //since multiple different instances of the node might exist
+            //the real fix is to add the BasicCloud.getAccount() to the key
+            //but this requires a different code for every type of object(to find the key)
+            //so first we will as a temp fix try to not also check if the object is in the hash
+            //is so and they are not the same instance we create a new hash entry
+            if (hash.get(mapperCode) != null){
+		//if there is a hash entry but the object are not equal
+		if (hash.get(mapperCode) != object){
+		    int counter=1;
+		    while(hash.get(mapperCode + counter) != null && hash.get(mapperCode + counter) != object){
+			counter++;
+		    }
+		    mapperCode += "{" + counter + "}";
+		}
+            }
 	    
-            log.debug("add=("+ mapperCode +")");
+	    log.debug("add=("+ mapperCode +")");
 	    int rcount = increaseRefCount(mapperCode);
 	    if (rcount ==1){
-               hash.put(mapperCode,object);
-               log.debug("add=("+ mapperCode +")");
+		hash.put(mapperCode,object);
+		log.debug("add=("+ mapperCode +")");
 	    } else {
-               log.debug("increace=("+ mapperCode +")("+ rcount +")");
+		log.debug("increace=("+ mapperCode +")("+ rcount +")");
 	    }
-            return mapperCode;
-        }
-        return "";
+	    return mapperCode;
+	}
+	return "";
     }
 
     private static int increaseRefCount(String mapperCode){
-    	Integer count = (Integer)refcount.get(mapperCode);
+	Integer count = (Integer)refcount.get(mapperCode);
 	if (count == null){
-		refcount.put(mapperCode,new Integer(1));
-		return 1;
+	    refcount.put(mapperCode,new Integer(1));
+	    return 1;
 	} else {
-		refcount.put(mapperCode,new Integer(count.intValue() + 1));
-		return count.intValue() + 1;
+	    refcount.put(mapperCode,new Integer(count.intValue() + 1));
+	    return count.intValue() + 1;
 	}
 
     }
 
     private static int decreaseRefCount(String mapperCode){
-    	Integer count = (Integer)refcount.get(mapperCode);
+	Integer count = (Integer)refcount.get(mapperCode);
 	if (count == null){
-		log.warn("refcount entry not found for(" + mapperCode + ")");
-		return 0;
+	    log.warn("refcount entry not found for(" + mapperCode + ")");
+	    return 0;
 	}
 	int c = count.intValue();
 
 	if (c ==1 ){
-		refcount.remove(mapperCode);
-		return 0;
+	    refcount.remove(mapperCode);
+	    return 0;
 	} else {
-		refcount.put(mapperCode,new Integer(c  - 1));
-		return c -1;
+	    refcount.put(mapperCode,new Integer(c  - 1));
+	    return c -1;
 	}
     }
     
@@ -98,13 +121,13 @@ public class StubToLocalMapper{
      * @return the required object or null if there was no such object
      */
     public static Object get(String mapperCode){
-        log.debug("access=("+ mapperCode +")");
+	log.debug("access=("+ mapperCode +")");
 
-        Object o = hash.get(mapperCode);
+	Object o = hash.get(mapperCode);
 	if (o ==null){
-		log.warn("hash.get("  + mapperCode +") has null value");
+	    log.warn("hash.get("  + mapperCode +") has null value");
 	}
-        return o;
+	return o;
     }
     
     /**
@@ -113,17 +136,17 @@ public class StubToLocalMapper{
      * @return if the entry was removed because refcount was 0
      */
     public static boolean remove(String mapperCode){
-        if (mapperCode != null && ! mapperCode.equals("")){
+	if (mapperCode != null && ! mapperCode.equals("")){
 	    int rcount = decreaseRefCount(mapperCode);
 	    if (rcount ==0){
-		    log.debug("remove=("+ mapperCode +")");
-		    hash.remove(mapperCode);
-		    return true;
+		log.debug("remove=("+ mapperCode +")");
+		hash.remove(mapperCode);
+		return true;
 	    } else {
-		    log.debug("keep=("+ mapperCode +") refcount=("+ rcount +")");
-		    return false;
+		log.debug("keep=("+ mapperCode +") refcount=("+ rcount +")");
+		return false;
 	    }
-        }
+	}
 	return false;
     }
 }
