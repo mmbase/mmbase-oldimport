23ab15af2e08062d2f632ecf33817fff3cacdba5
kees <kees@91430c5f-d668-0410-9e7d-dd663366dcfd>
2002-06-28 16:20:19 +0000

    bugtracker communicates with the mmbase cloud
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@4581 91430c5f-d668-0410-9e7d-dd663366dcfd

--- speeltuin/bugtracker/src/org/mmbase/applications/bugtracker/BugCategory.java
+++ speeltuin/bugtracker/src/org/mmbase/applications/bugtracker/BugCategory.java
@@ -1,11 +1,8 @@
-/*
- * BugCategory.java
- *
- * Created on June 7, 2002, 9:01 AM
- */
 
 package org.mmbase.applications.bugtracker;
 
+import org.mmbase.bridge.*;
+import java.util.*;
 /**
  *
  * @author  mmbase
@@ -22,13 +19,92 @@ package org.mmbase.applications.bugtracker;
  * @mmbase-relationmanager-destination bugcategory
  * @mmbase-relationmanager-nodemanager catrel
  */
-public class BugCategory {
-    
+public class BugCategory implements Comparable{
+    Relation parentRelation = null;
+    Node bugCategoryNode = null;
     /** Creates a new instance of BugCategory */
-    public BugCategory() {
+    public BugCategory(Relation parentRelation,Node bugCategoryNode) {
+        this.parentRelation = parentRelation;
+        this.bugCategoryNode = bugCategoryNode;
+    }
+    
+    public int getPos(){
+        if (parentRelation != null){
+            return parentRelation.getIntValue("pos");
+        } else {
+            return -1;
+        }
+    }
+    
+    public void setPos(int pos){
+        if (parentRelation != null){
+            parentRelation.setIntValue("pos",pos);
+            parentRelation.commit();
+        }
+    }
+    
+    public BugCategories getBugSubCategories(){
+        BugCategories retval = new BugCategories();
+        RelationIterator ri  =bugCategoryNode.getRelations("subcategoryrel","bugcategory").relationIterator();
+        while(ri.hasNext()){
+            Relation rel = ri.nextRelation();
+            Node destNode = rel.getDestination();
+            retval.add( new BugCategory(rel,destNode));
+        }
+        //sort on pos
+        Collections.sort(retval);
+        
+        return retval;
+    }
+    
+    public void deleteBugCategory(){
+        if (parentRelation!= null){
+            //get the parent category if avaiable
+            Node parentNode = parentRelation.getSource();
+            if (parentNode.getNodeManager().getName().equals("bugcategory")){
+                BugCategory parentCategory = new BugCategory(null,parentNode);
+                
+                //delete the current relation of the parent category
+                parentRelation.delete();
+                
+                //get a list of sub categories
+                BugCategories cats = getBugSubCategories();
+                
+                //and add them to the parent category
+                for (int x =0 ; x < cats.size(); x++){
+                    BugCategory currentSubCat = cats.getBugCategory(x);
+                    parentCategory.addBugSubCategory(currentSubCat.getPos(),currentSubCat);
+                }
+            }
+        }
+        bugCategoryNode.delete(true);
+    }
+    
+    public String getName(){
+        return bugCategoryNode.getStringValue("name");
+    }
+    
+    public void setName(String name){
+        bugCategoryNode.setStringValue("name",name);
+        bugCategoryNode.commit();
     }
     
     public BugCategories getSubCategories(){
         return new BugCategories();
+    }
+    
+    public Node getBugCategoryNode(){
+        return bugCategoryNode;
+    }
+    public int compareTo(Object obj) {
+        BugCategory otherCat = (BugCategory)obj;
+        return getPos() - otherCat.getPos();
+    }
+    
+    public void addBugSubCategory(int pos,BugCategory bugCategory){
+        RelationManager relman = bugCategoryNode.getCloud().getRelationManager("bugcategory","bugcategory","subcategoryrel");
+        Relation rel =relman.createRelation(bugCategoryNode,bugCategory.getBugCategoryNode());
+        rel.setIntValue("pos",pos);
+        rel.commit();
     }
 }
--- speeltuin/bugtracker/src/org/mmbase/applications/bugtracker/Bugtracker.java
+++ speeltuin/bugtracker/src/org/mmbase/applications/bugtracker/Bugtracker.java
@@ -6,6 +6,10 @@
 
 package org.mmbase.applications.bugtracker;
 
+import org.mmbase.bridge.*;
+import java.util.*;
+
+
 /**
  * Bugtracker is the mail class for the bugtracker
  * it contains the root bugtracker category and information
@@ -27,18 +31,69 @@ package org.mmbase.applications.bugtracker;
  */
 public class Bugtracker {
     
+    
+    private Cloud cloud;
+    private Node bugtrackerNode = null;
     /** Creates a new instance of BugReport */
-    public Bugtracker() {
+    public Bugtracker(Cloud cloud) {
+        this.cloud = cloud;
+        checkSetup();
+        bugtrackerNode = cloud.getNode("bugtracker.start");
+        
     }
     
+    private void checkSetup(){
+        try {
+            Node node = cloud.getNode("bugtracker.start");
+        } catch (BridgeException e){
+            Node node = cloud.getNodeManager("bugtracker").createNode();
+            node.setStringValue("name","MMBase bugtracker");
+            node.commit();
+            node.createAlias("bugtracker.start");
+            System.out.println("created a bugtracker node with alias bugtracker.start");
+        }
+    }
+    public String getName(){
+	    return bugtrackerNode.getStringValue("name");
+    }
+    
+    public void setName(String name){
+            bugtrackerNode.setStringValue("name",name);
+            bugtrackerNode.commit();
+    }
     /**
      * bugreports are stored in a hirachical scruture
      * this method returns an empty root category with the
      * containing the sub categories
      **/
-    public BugCategory getRootCategory(){
-        return new BugCategory();
+    public BugCategories getRootBugCategories(){
+        BugCategories retval = new BugCategories();
+        RelationIterator ri  =bugtrackerNode.getRelations("subcategoryrel","bugcategory").relationIterator();
+        while(ri.hasNext()){
+            Relation rel = ri.nextRelation();
+            Node destNode = rel.getDestination();
+            retval.add( new BugCategory(rel,destNode));
+        }
+        //sort on pos
+        Collections.sort(retval);
+    
+        return retval;
     }
+    
+    public BugCategory createBugCategory(String name){
+        Node node = cloud.getNodeManager("bugcategory").createNode();
+        node.setStringValue("name",name);
+        node.commit();
+        return new BugCategory(null,node);
+    }
+    
+    public void addRootBugCategory(int pos,BugCategory bugCategory){
+        RelationManager relman = cloud.getRelationManager("bugtracker","bugcategory","subcategoryrel");
+        Relation rel =relman.createRelation(bugtrackerNode,bugCategory.getBugCategoryNode());
+        rel.setIntValue("pos",pos);
+        rel.commit();
+    }
+    
     
     /**
      * @return the list of maintainers of the bugtracker
@@ -48,6 +103,25 @@ public class Bugtracker {
     }
     
     public String getVersion(){
-        return "$Id: Bugtracker.java,v 1.4 2002-06-27 19:20:29 kees Exp $";
+        return "$Id: Bugtracker.java,v 1.5 2002-06-28 16:20:19 kees Exp $";
+    }
+    
+    public static void main(String argv[]) throws Exception{
+        CloudContext  cloudContext = ContextProvider.getCloudContext("rmi://24.132.250.86:1111/remotecontext");
+        HashMap user = new HashMap();
+        user.put("username", "admin");
+        user.put("password", "admin2k");
+        Cloud cloud = cloudContext.getCloud("mmbase","name/password",user);
+        Bugtracker bugtracker = new Bugtracker(cloud);
+        BugCategories rootCats = bugtracker.getRootBugCategories();
+        for (int x =0 ; x < rootCats.size(); x++){
+            BugCategory cat = rootCats.getBugCategory(x);
+            System.err.println(cat.getName());
+        }
+        //bugtracker.addRootBugCategory(10,bugtracker.createBugCategory("org"));
+        //bugtracker.addRootBugCategory(20,bugtracker.createBugCategory("nl"));
+        //bugtracker.addRootBugCategory(5,bugtracker.createBugCategory("com"));
+        
+        
     }
 }
