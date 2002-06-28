0415294e76cd533c0403d442542dd01661003b55
kees <kees@91430c5f-d668-0410-9e7d-dd663366dcfd>
2002-06-28 20:12:58 +0000

    keesj:added javadoc about the MMBasedoclet
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@4582 91430c5f-d668-0410-9e7d-dd663366dcfd

--- speeltuin/bugtracker/src/org/mmbase/applications/mmbasedoclet/MMBaseDoclet.java
+++ speeltuin/bugtracker/src/org/mmbase/applications/mmbasedoclet/MMBaseDoclet.java
@@ -3,7 +3,91 @@ package org.mmbase.applications.mmbasedoclet;
 import com.sun.javadoc.*;
 import java.util.*;
 import org.mmbase.applications.config.*;
-
+/**
+ * MMBaseDoclet is a javadoc doclet to create MMBase an mmbase object model
+ * based on javadoc comments in files in a package. The doclet introduces
+ * a tags to the standard doclet. defined tags are:<BR>
+ *<table BORDER="1" CELLPADDING="3" CELLSPACING="0" WIDTH="100%">
+ *<tr BGCOLOR="#CCCCFF" CLASS="TableHeadingColor">
+ *<td COLSPAN=3><font SIZE="+2">
+ *<b>Application tags</b></font>that give information about the MMBase application</td>
+ *</tr>
+ *<tr BGCOLOR="white" CLASS="TableRowColor">
+ *<td ALIGN="right" VALIGN="top" WIDTH="1%"><font SIZE="-1">
+ *<code>mmbase-application-name</code></font></td>
+ *<td>defines the name of the application</td>
+ *<td>required</td>
+ *</tr>
+ *
+ *<tr BGCOLOR="#CCCCFF" CLASS="TableHeadingColor">
+ *<td COLSPAN=3><font SIZE="+2">
+ *<b>NodeManager tags</b></font> that define node managers</td>
+ *</tr>
+ *<tr BGCOLOR="white" CLASS="TableRowColor">
+ *<td ALIGN="right" VALIGN="top" WIDTH="1%"><font SIZE="-1">
+ *<code>mmbase-nodemanager-name</code></font></td>
+ *<td>defines the name of a node manager this should always be the first tag to use  when you are trying to define a nodemanager</td>
+ *<td>required and unique</td>
+ *</tr>
+ *<tr BGCOLOR="white" CLASS="TableRowColor">
+ *<td ALIGN="right" VALIGN="top" WIDTH="1%"><font SIZE="-1">
+ *<code>mmbase-nodemanager-extends</code></font></td>
+ *<td>defines what configuration the nodemanager extends values van be <b>object</b> and <b>insrel</b></td>
+ *<td>default = "object"</td>
+ *</tr>
+ *<tr BGCOLOR="white" CLASS="TableRowColor">
+ *<td ALIGN="right" VALIGN="top" WIDTH="1%"><font SIZE="-1">
+ *<code>mmbase-nodemanager-classfile</code></font></td>
+ *<td>defines what java class file the node manager should use. if you haven't writen a special MMObjectBuilder is is best not to define this tag, the class file will be taken from
+ * the node manager configuration this object extends</td>
+ *<td>not required</td>
+ *</tr>
+ *<tr BGCOLOR="white" CLASS="TableRowColor">
+ *<td ALIGN="right" VALIGN="top" WIDTH="1%"><font SIZE="-1">
+ *<code>mmbase-nodemanager-version</code></font></td>
+ *<td>integer value giving information about the version of the nodemanager</td>
+ *<td>default ="1"</td>
+ *</tr>
+ *<tr BGCOLOR="white" CLASS="TableRowColor">
+ *<td ALIGN="right" VALIGN="top" WIDTH="1%"><font SIZE="-1">
+ *<code>mmbase-nodemanager-maintainer</code></font></td>
+ *<td>autho/organisation maintaining the node manager</td>
+ *<td>default ="mmbase.org"</td>
+ *</tr>
+ *<tr BGCOLOR="white" CLASS="TableRowColor">
+ *<td ALIGN="right" VALIGN="top" WIDTH="1%"><font SIZE="-1">
+ *<code>mmbase-nodemanager-searchage</code></font></td>
+ *<td>default value in days for wich nodes from this nodemanager should not been show in the editor</td>
+ *<td>default ="360"</td>
+ *</tr>
+ *<tr BGCOLOR="white" CLASS="TableRowColor">
+ *<td ALIGN="right" VALIGN="top" WIDTH="1%"><font SIZE="-1">
+ *<code>mmbase-nodemanager-field</code></font></td>
+ *<td>defines a field for the node manager. this value main contain 3 strings.
+ *<ul>
+ * <li>the name of the field</li>
+ * <li>the type of the field (MMBase types like STRING/INTEGER) default = STRING</li>
+ * <li>the size of the field (default is 127 if applicable)
+ *</ul>
+ * </td>
+ *<td>not required</td>
+ *</tr>
+ *
+ *<tr BGCOLOR="#CCCCFF" CLASS="TableHeadingColor">
+ *<td COLSPAN=3><font SIZE="+2">
+ *<b>Relation manager tags</b></font> that give information about possible relation between nodemanagers</td>
+ *</tr>
+ *<tr BGCOLOR="white" CLASS="TableRowColor">
+ *<td ALIGN="right" VALIGN="top" WIDTH="1%"><font SIZE="-1">
+ *<code>mmbase-relationmanager-name</code></font></td>
+ *<td>defines the name of a node manager this should always be the first tag to use  when you are trying to define a nodemanager</td>
+ *<td>required but not unique</td>
+ *</tr>
+ *</table>
+ *
+ * @author Kees Jongenburger
+ * @version $Id: MMBaseDoclet.java,v 1.6 2002-06-28 20:12:58 kees Exp $
+ **/
 public class MMBaseDoclet{
     
     public static boolean start(RootDoc root){
@@ -25,8 +109,8 @@ public class MMBaseDoclet{
                 String tagContent = tags[tagcount].text();
                 
                 if (tagName.startsWith("@mmbase-application-name")) {
-			appconfig.setName(tagContent);
-	        }
+                    appconfig.setName(tagContent);
+                }
                 if (tagName.startsWith("@mmbase-nodemanager-name")) {
                     int startIndex = tagcount;
                     while(
@@ -51,7 +135,7 @@ public class MMBaseDoclet{
                 }
             }
         }
-	ConfigurationXMLWriter.writeApplication(appconfig);
+        ConfigurationXMLWriter.writeApplication(appconfig);
         
     }
     
@@ -87,12 +171,12 @@ public class MMBaseDoclet{
         
         DocletRelationManagerConfiguration relationManagerConfig = new DocletRelationManagerConfiguration();
         
-	//@mmbase-relationmanager-name: maintainer
-	//@mmbase-relationmanager-nodemanager: insrel
-	//@mmbase-relationmanager-source: bugtracker
-	//@mmbase-relationmanager-destination: bugtrackeruser
-   	//@mmbase-relationmanager-directionality: unidirectional
-
+        //@mmbase-relationmanager-name: maintainer
+        //@mmbase-relationmanager-nodemanager: insrel
+        //@mmbase-relationmanager-source: bugtracker
+        //@mmbase-relationmanager-destination: bugtrackeruser
+        //@mmbase-relationmanager-directionality: unidirectional
+        
         for (int tagcount = startIndex ; tagcount <= endIndex;tagcount++){
             String name = tags[tagcount].name().substring("mmbase-relationmanager-".length() + 1);
             String text = tags[tagcount].text();
