01051c0567764fa8710c7b6e13b4801de6b61e94
kees <kees@91430c5f-d668-0410-9e7d-dd663366dcfd>
2002-06-29 15:52:11 +0000

    keesj:added javadocs
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@4604 91430c5f-d668-0410-9e7d-dd663366dcfd

--- speeltuin/bugtracker/src/org/mmbase/applications/config/BasicApplicationConfiguration.java
+++ speeltuin/bugtracker/src/org/mmbase/applications/config/BasicApplicationConfiguration.java
@@ -1,7 +1,7 @@
 package org.mmbase.applications.config;
 
 /**
- *
+ * implementation of ApplicationConfiguration
  * @author Kees Jongenburger
  */
 public class BasicApplicationConfiguration implements org.mmbase.applications.config.ApplicationConfiguration {
@@ -13,17 +13,32 @@ public class BasicApplicationConfiguration implements org.mmbase.applications.co
         nodeManagerConfigurations = new NodeManagerConfigurations();
         relationManagerConfigurations = new RelationManagerConfigurations();
     }
+    
+    /**
+     * set the name of the application
+     * @param name the name of the application
+     **/
     public void setName(String name){
 	    this.name = name;
     }
+    
     public String getName(){
 	    return name;
     }
     
+    /**
+     * add a nodemanager configuration to the application
+     * @param nodeManagerConfiguration the nodemanagerconfiguration to add
+     **/
     public void addNodeManagerConfiguration(NodeManagerConfiguration nodeManagerConfiguration){
         nodeManagerConfigurations.add(nodeManagerConfiguration);
     }
     
+    /**
+     * add a relation manager configuration to the application
+     * @param relationManagerConfiguration the relation manager configuration to add
+     *
+     **/
     public void addRelationManagerConfiguration(RelationManagerConfiguration relationManagerConfiguration){
         relationManagerConfigurations.add(relationManagerConfiguration);
     }
