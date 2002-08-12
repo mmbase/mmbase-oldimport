5e32e455e75adfbe37e217e753025d6e272c425e
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2002-08-12 16:15:12 +0000

    michiel: Caches are 'Sizeable' now, meaning that smart things can be tried to determin (approximately) how many bytes they take. Entries of the Cache should also be made 'Sizeable', but I think that at least MMObjectNodes are detrermined quite accurately as they are already
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@4948 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/cache/Cache.java
+++ src/org/mmbase/cache/Cache.java
@@ -10,28 +10,32 @@ See http://www.MMBase.org/license
 package org.mmbase.cache;
 
 import java.util.*;
+import java.lang.reflect.*; // necessary for SizeOf
+
 import java.io.File;
 import org.mmbase.util.LRUHashtable;
-import org.mmbase.module.core.MMBaseContext;
+import org.mmbase.module.core.*; // MMBaseContext;
 
 import org.mmbase.util.XMLBasicReader;
 import org.w3c.dom.Element;
 
 import org.mmbase.util.FileWatcher;
+import org.mmbase.util.Sizeable;
+import org.mmbase.util.SizeOf;
 import org.mmbase.util.logging.Logger;
 import org.mmbase.util.logging.Logging;
 
-import java.lang.reflect.*; // necessary for SizeOf
 
 /**
  * A base class for all Caches. Extend this class for other caches.  
  *
  * @author Michiel Meeuwissen
- * @version $Id: Cache.java,v 1.8 2002-08-09 21:00:53 michiel Exp $
+ * @version $Id: Cache.java,v 1.9 2002-08-12 16:15:12 michiel Exp $
  */
-abstract public class Cache extends LRUHashtable {
+abstract public class Cache extends LRUHashtable implements Sizeable  {
 
     private static Logger log = Logging.getLoggerInstance(Cache.class.getName());
+
     private static Map caches = new Hashtable();
    
     /**
@@ -208,149 +212,46 @@ abstract public class Cache extends LRUHashtable {
         return active;
     }
 
-    /**
-     * Returns the (approximate) bytesize of this cache
-     * EXPERIMENTAL
-     */
-
-    public int getByteSize() { 
-        return SizeOf.sizeof(this);
+    public int getByteSize() {
+        return getByteSize(new SizeOf());
     }
 
-    /**
-     * Implementation of sizeof
-     */
+    public int getByteSize(SizeOf sizeof) {
+        Iterator i = entrySet().iterator();
+        int len = 0;
+        while (i.hasNext()) {
+            Map.Entry entry = (Map.Entry) i.next();
+            len += sizeof.sizeof(entry.getKey());
+            len += sizeof.sizeof(entry.getValue());
+        }
+        return len;
+    }
 
-    static private class SizeOf {
-        
-        private static final int SZ_REF = 4;               
-        private static int size_prim(Class t) { 
-            if      (t == Boolean.TYPE)   return 1;
-            else if (t == Byte.TYPE)      return 1;
-            else if (t == Character.TYPE) return 2;
-            else if (t == Short.TYPE)     return 2;
-            else if (t == Integer.TYPE)   return 4;
-            else if (t == Long.TYPE)      return 8;
-            else if (t == Float.TYPE)     return 4;
-            else if (t == Double.TYPE)    return 8;
-            else if (t == Void.TYPE)      return 0;
-            else return SZ_REF;
-        }
-        
-        public static int sizeof(boolean b) { return 1; }        
-        public static int sizeof(byte b)    { return 1; }        
-        public static int sizeof(char c)    { return 2; }
-        public static int sizeof(short s)   { return 2; }        
-        public static int sizeof(int i)     { return 4; }
-        public static int sizeof(long l)    { return 8; }        
-        public static int sizeof(float f)   { return 4; }        
-        public static int sizeof(double d)  { return 8; }
-        public static int sizeof(Object obj) {
-            System.out.println("sizeof object");
-            if (obj == null) {
-                return 0;
-            }
-            
-            Class c = obj.getClass();
-            
-            if (c.isArray()) {
-                System.out.println("an array");
-                return size_arr(obj, c);
-            } else {
-                System.out.println("an object");
-                if (obj instanceof Map)  return sizeof((Map) obj);
-                return size_inst(obj, c);
-            }
-        }
-        
-        public static int sizeof(Map m) {
-            System.out.println("sizeof Map");
-            int len = size_inst(m, m.getClass());
-            Iterator i = m.entrySet().iterator();
-            while (i.hasNext()) {
-                Map.Entry entry = (Map.Entry) i.next();
-                len += sizeof(entry.getKey());
-                len += sizeof(entry.getValue());
-            }
-            return len;
-        }
-        
-
-        private static int size_inst(Object obj, Class c) {
-            Field flds[] = c.getDeclaredFields();
-            int sz = 0;
-            
-            for (int i = 0; i < flds.length; i++) {
-                Field f = flds[i];
-                if (!c.isInterface() &&  (f.getModifiers() & Modifier.STATIC) != 0) {
-                    continue;
-                }
-                sz += size_prim(f.getType());
-                try {
-                    System.out.println("found a field " + f);
-                    sz += sizeof(f.get(obj)); // recursion
-                } catch (java.lang.IllegalAccessException e) {
-                    // well...
-                    System.out.println(e);
-
-                }
-            }
-            
-            if (c.getSuperclass() != null) {
-                sz += size_inst(obj, c.getSuperclass());
-            }
-            
-            Class cv[] = c.getInterfaces();
-            for (int i = 0; i < cv.length; i++) {
-                sz += size_inst(obj, cv[i]);
-            }
-            
-            return sz;
-        }
-        
-        private static int size_arr(Object obj, Class c) {
-            Class ct = c.getComponentType();
-            int len = Array.getLength(obj);
-            
-            if (ct.isPrimitive()) {
-                return len * size_prim(ct);
-            }
-            else {
-                int sz = 0;
-                for (int i = 0; i < len; i++) {
-                    sz += SZ_REF;
-                    Object obj2 = Array.get(obj, i);
-                    if (obj2 == null)
-                        continue;
-                    Class c2 = obj2.getClass();
-                    if (!c2.isArray())
-                        continue;
-                    sz += size_arr(obj2, c2);
-                }
-                return sz;
-            }
-        }
        
-    }
-    public static void main(String args[]) {
-        Cache mycache = new Cache(20) {  
-                public String getName()        { return "test cache"; }
-                public String getDescription() { return ""; }
-            };
-        System.out.println("putting some strings in cache");
-        mycache.put("aaa", "AAA"); // 6 bytes
-        mycache.put("bbb", "BBB"); // 6 bytes
+public static void main(String args[]) {
+    Cache mycache = new Cache(20) {  
+            public String getName()        { return "test cache"; }
+            public String getDescription() { return ""; }
+        };
+    /*
+    System.out.println("putting some strings in cache");
+    mycache.put("aaa", "AAA"); // 6 bytes
+    mycache.put("bbb", "BBB"); // 6 bytes
 
-        System.out.println("putting an hashmap in cache");
-        Map m = new HashMap();
-        m.put("ccc", "CCC");      
-        m.put("ddd", "DDD"); 
-        
-        mycache.put("eee", m);   // 24 bytes
+    System.out.println("putting an hashmap in cache");
+    Map m = new HashMap();
+    m.put("ccc", "CCCCCC");      
+    m.put("ddd", "DDD");            
+    m.put("abc", "EEE");
+    mycache.put("eee", m);   
+    */
 
-        mycache.put("fff", mycache); 
+    MMObjectNode node = new MMObjectNode(new MMObjectBuilder());
+    node.setValue("hoi", "hoi");
+    mycache.put("node", node);
 
-        System.out.println("size of cache: " + mycache.getByteSize());
+    System.out.println("size of cache: " + mycache.getByteSize());
 
-    }
+}
+
 }
