752b4a8517d2d202ee25e61a23eb81b9102c352a
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2002-08-14 10:23:35 +0000

    michiel: bugfix, all security exceptions are mmbase specific
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@4979 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/security/implementation/context/ContextAuthentication.java
+++ src/org/mmbase/security/implementation/context/ContextAuthentication.java
@@ -10,6 +10,7 @@ See http://www.MMBase.org/license
 package org.mmbase.security.implementation.context;
 
 import org.mmbase.security.*;
+import org.mmbase.security.SecurityException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -30,7 +31,7 @@ import org.mmbase.util.logging.Logging;
  * @javadoc
  *
  * @author Eduard Witteveen
- * @version $Id: ContextAuthentication.java,v 1.9 2002-06-25 12:30:45 michiel Exp $
+ * @version $Id: ContextAuthentication.java,v 1.10 2002-08-14 10:23:35 michiel Exp $
  */
 public class ContextAuthentication extends Authentication {
     private static Logger log=Logging.getLoggerInstance(ContextAuthentication.class.getName());
@@ -55,11 +56,11 @@ public class ContextAuthentication extends Authentication {
             String message = "error loading configfile :'" + configFile + "'("+se + "->"+se.getMessage()+"("+se.getMessage()+"))";
             log.error(message);
             log.error(Logging.stackTrace(se));
-            throw new org.mmbase.security.SecurityException(message);
+            throw new SecurityException(message);
         } catch(java.io.IOException ioe) {
             log.error("error parsing file :"+configFile);
             log.error(Logging.stackTrace(ioe));
-            throw new org.mmbase.security.SecurityException("error loading configfile :'"+configFile+"'("+ioe+")" );
+            throw new SecurityException("error loading configfile :'"+configFile+"'("+ioe+")" );
         }
         if (log.isDebugEnabled()) {
             log.debug("loaded: '" +  configFile + "' as config file for authentication");
@@ -75,7 +76,7 @@ public class ContextAuthentication extends Authentication {
         } catch(javax.xml.transform.TransformerException te) {
             log.error("error executing query: '" + xpath + "' ");
             log.error( Logging.stackTrace(te));
-            throw new java.lang.SecurityException("error executing query: '"+xpath+"' ");
+            throw new SecurityException("error executing query: '"+xpath+"' ");
         }
         // we now have a list of login modules.. process them all, and load them...
         for(Node contains = found.nextNode(); contains != null; contains = found.nextNode()) {
@@ -92,7 +93,7 @@ public class ContextAuthentication extends Authentication {
                 String msg = "could not load module with the name:"+moduleName+ " with class:" + className;
                 log.error(msg);
                 log.error( Logging.stackTrace(e));
-                throw new java.lang.SecurityException(msg);
+                throw new SecurityException(msg);
             }
             module.load(document, validKey, moduleName, manager);
             log.info("loaded module with the name:"+moduleName+ " with class:" + className);
@@ -103,12 +104,12 @@ public class ContextAuthentication extends Authentication {
     }
 
 
-    public UserContext login(String moduleName, Map loginInfo, Object[] parameters) throws org.mmbase.security.SecurityException {
+    public UserContext login(String moduleName, Map loginInfo, Object[] parameters) throws SecurityException {
         // look if we can find our login module...
         if(!loginModules.containsKey(moduleName)) {
             String msg = "could not load module with name:" +  moduleName;
             log.error(msg);
-            throw new java.lang.SecurityException(msg);
+            throw new SecurityException(msg);
         }
         ContextLoginModule module = (ContextLoginModule) loginModules.get(moduleName);
         // and we do the login...
@@ -124,7 +125,7 @@ public class ContextAuthentication extends Authentication {
     /**
      * this method does nothing..
      */
-    public boolean isValid(UserContext usercontext) throws org.mmbase.security.SecurityException {
+    public boolean isValid(UserContext usercontext) throws SecurityException {
         return validKey == ((ContextUserContext)usercontext).getKey();
     }
 }
