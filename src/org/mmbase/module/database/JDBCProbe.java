d28897f4659bcfa7062d4db5df52e8b5128b6e30
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2002-10-10 18:08:43 +0000

    michiel: cleaned, made probe time configurable
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@5365 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/database/JDBCProbe.java
+++ src/org/mmbase/module/database/JDBCProbe.java
@@ -23,64 +23,72 @@ import org.mmbase.util.logging.Logging;
  * JDBCProbe checks all JDBC connection every X seconds to find and
  * remove bad connections works using a callback into JDBC.
  *
- * @version 27 Mar 1997
+ *
+ * @version $Id: JDBCProbe.java,v 1.5 2002-10-10 18:08:43 michiel Exp $
  * @author Daniel Ockeloen
+
  */
 public class JDBCProbe implements Runnable {
     private static Logger log = Logging.getLoggerInstance(JDBCProbe.class.getName());
-
-	Thread kicker = null;
-	JDBC parent=null;
-	String name;
-	String input;
-	int len;
-
-	public JDBCProbe(JDBC parent) {
-		this.parent=parent;
-		init();
-	}
-
-	public void init() {
-		this.start();	
-	}
+    
+    private Thread kicker = null;
+    private JDBC parent=null;
+    private String name;
+    private String input;
+    private int len;
+    private long checkTime;
 
 
-	/**
-	 * Starts the admin Thread.
-	 */
-	public void start() {
-		/* Start up the main thread */
-		if (kicker == null) {
-			kicker = new Thread(this,"JDBCProbe");
-			kicker.start();
-		}
-	}
-	
-	/**
-	 * Stops the admin Thread.
-	 */
-	public void stop() {
-		/* Stop thread */
-		kicker.setPriority(Thread.MIN_PRIORITY);  
-		kicker.suspend();
-		kicker.stop();
-		kicker = null;
-	}
-
-	/**
-	 * admin probe, try's to make a call to all the maintainance calls.
-	 */
-	public void run () {
-		log.info("JDBC probe starting");
-		while (kicker!=null) {
-			try{
-				Thread.sleep(30000);
-			} catch(InterruptedException e) {
-			}
-			try {
-				parent.checkTime();
-			} catch (Exception e) {
-			}
-		}
-	}
+    public JDBCProbe(JDBC parent) {
+        this(parent, 30);
+    }
+    public JDBCProbe(JDBC parent, int ct) {
+        this.parent=parent;
+        checkTime = ct * 1000;
+        init();
+    }
+    
+    public void init() {
+        this.start();	
+    }
+    
+    
+    /**
+     * Starts the admin Thread.
+     */
+    public void start() {
+        /* Start up the main thread */
+        if (kicker == null) {
+            kicker = new Thread(this, "JDBCProbe");
+            kicker.start();
+        }
+    }
+    
+    /**
+     * Stops the admin Thread.
+     */
+    public void stop() {
+        /* Stop thread */
+        kicker.setPriority(Thread.MIN_PRIORITY);  
+        kicker.suspend();
+        kicker.stop();
+        kicker = null;
+    }
+    
+    /**
+     * admin probe, try's to make a call to all the maintainance calls.
+     */
+    public void run () {
+        log.info("JDBC probe starting");
+        while (kicker != null) {
+            try{
+                Thread.sleep(checkTime);
+            } catch(InterruptedException e) {
+            }
+            try {
+                parent.checkTime();
+            } catch (Exception e) {
+            }
+        }
+    }
 }
