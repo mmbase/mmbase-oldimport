02166b71ddc25747da1b6f58e6bc5e8ed4c634ee
pierre <pierre@91430c5f-d668-0410-9e7d-dd663366dcfd>
2002-10-17 12:13:44 +0000

    pierre bugfix: _status field is required by Dove, add it in MMObjectBuilder init
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@5417 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/core/MMObjectBuilder.java
+++ src/org/mmbase/module/core/MMObjectBuilder.java
@@ -48,7 +48,7 @@ import org.mmbase.util.logging.*;
  * @author Pierre van Rooden
  * @author Eduard Witteveen
  * @author Johan Verelst
- * @version $Id: MMObjectBuilder.java,v 1.172 2002-10-17 09:43:31 eduard Exp $
+ * @version $Id: MMObjectBuilder.java,v 1.173 2002-10-17 12:13:44 pierre Exp $
  */
 public class MMObjectBuilder extends MMTable {
 
@@ -329,6 +329,7 @@ public class MMObjectBuilder extends MMTable {
         // add temporary fields
         checkAddTmpField("_number");
         checkAddTmpField("_exists");
+        checkAddTmpField("_status");
 
 	// get property dof maximum number of queries..
 	String property = getInitParameter(MAX_NODES_FROM_QUERY_PROPERY);
