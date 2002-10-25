2e0361763ed95a3f407f7c0a979d9163939155db
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2002-10-25 12:53:13 +0000

    michiel: hack drop logj 1.1 support
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@5466 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/util/logging/log4j/Log4jLevel.java
+++ src/org/mmbase/util/logging/log4j/Log4jLevel.java
@@ -17,69 +17,70 @@ See http://www.MMBase.org/license
 
 */
 
-package org.mmbase.util.logging.log4j;
+package org.mmbase.util.logging.log4j2;
+import  org.apache.log4j.Level;
 import  org.apache.log4j.Priority;
 
 /** 
- *    LoggerPriority The new Priority class for Log4jImpl. It extends
- *    the log4j Priority with 2 extra priorities, namely `SERVICE' and
+ *    LoggerLevel The new Level class for Log4jImpl. It extends
+ *    the log4j Level with 2 extra levels, namely `SERVICE' and
  *    `TRACE'.
  *
  * @author Michiel Meeuwissen
  **/
 
-public class Log4jPriority extends Priority {
+public class Log4jLevel extends Level {
 
 
     final static int SERVICE_INT   = 15000;
     final static int TRACE_INT     = 5000;
 
-    // FATAL          (from log4j.Priority)
+    // FATAL          (from log4j.Level)
     // ERROR
     // WARN
     // INFO
-    public static final Log4jPriority SERVICE = new Log4jPriority(SERVICE_INT, "SERVICE", 5);
+    public static final Log4jLevel SERVICE = new Log4jLevel(SERVICE_INT, "SERVICE", 5);
     // DEBUG    
-    public static final Log4jPriority TRACE   = new Log4jPriority(TRACE_INT,   "TRACE",   7);
+    public static final Log4jLevel TRACE   = new Log4jLevel(TRACE_INT,   "TRACE",   7);
 
 
-    protected  Log4jPriority(int level, String strLevel, int syslogEquiv) {
+    protected  Log4jLevel(int level, String strLevel, int syslogEquiv) {
         super(level, strLevel, syslogEquiv);
     }
 
-    public static  Priority toPriority(String sArg) {
+    public static  Level toLevel(String sArg) {
         if(sArg == null)
-            return Log4jPriority.TRACE;
+            return Log4jLevel.TRACE;
 
         String stringVal = sArg.toUpperCase();
 
-        if(stringVal.equals("TRACE"))   return Log4jPriority.TRACE;
-        if(stringVal.equals("SERVICE")) return Log4jPriority.SERVICE;
-        return Priority.toPriority(sArg);
+        if(stringVal.equals("TRACE"))   return Log4jLevel.TRACE;
+        if(stringVal.equals("SERVICE")) return Log4jLevel.SERVICE;
+        return Level.toLevel(sArg);
     }
         
-    public static Priority toPriority(int i) throws  IllegalArgumentException {
+    public static Level toLevel(int i) throws  IllegalArgumentException {
         switch(i) {
-        case TRACE_INT:   return Log4jPriority.TRACE;
-        case SERVICE_INT: return Log4jPriority.SERVICE;
+        case TRACE_INT:   return Log4jLevel.TRACE;
+        case SERVICE_INT: return Log4jLevel.SERVICE;
         }
-        return Priority.toPriority(i);
+        return Level.toLevel(i);
     }
     
     public static Priority[] getAllPossibleLog4jPriorities() {
         return new Priority[] {FATAL, ERROR, WARN, INFO, SERVICE, DEBUG, TRACE};
     }
    
-    public static Priority toLog4jPriority(String sArg) { // needed?
-        Priority result;
-        result = Priority.toPriority(sArg, null);
+    public static Level toLog4jLevel(String sArg) { // needed?
+        Level result;
+        result = (Level) Level.toLevel(sArg, null);
         if (result != null) {
             return result;
         }
         String s = sArg.toUpperCase();
         if (s.equals("SERVICE")) return SERVICE;
         if (s.equals("TRACE"))   return TRACE;
-        return DEBUG;
+        return (Level) DEBUG;
     }
 
 }
