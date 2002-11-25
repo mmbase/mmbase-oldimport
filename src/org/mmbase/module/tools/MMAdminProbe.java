9302e4f9e575c73cddf81d8c22f59677bdf6c563
kees <kees@91430c5f-d668-0410-9e7d-dd663366dcfd>
2002-11-25 19:39:19 +0000

    keesj:made the code look more like a java class and less like and applet
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@5941 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/tools/MMAdminProbe.java
+++ src/org/mmbase/module/tools/MMAdminProbe.java
@@ -1,4 +1,4 @@
-/* -*- tab-width: 4; -*-
+/* 
 
 This software is OSI Certified Open Source Software.
 OSI Certified is a certification mark of the Open Source Initiative.
@@ -7,25 +7,8 @@ The license (Mozilla version 1.0) can be read at the MMBase site.
 See http://www.MMBase.org/license
 
 */
-/*
-
-$Id: MMAdminProbe.java,v 1.3 2001-04-11 14:22:07 michiel Exp $
-
-$Log: not supported by cvs2svn $
-Revision 1.2  2000/08/27 21:00:41  daniel
-Small change to allow server restart
-
-Revision 1.1  2000/08/27 19:04:46  daniel
-changed from examples to admin module
-
-*/
-
 package org.mmbase.module.tools;
 
-import java.lang.*;
-import java.net.*;
-import java.util.*;
-import java.io.*;
 
 import org.mmbase.module.database.*;
 import org.mmbase.module.core.*;
@@ -35,98 +18,68 @@ import org.mmbase.util.logging.Logger;
 import org.mmbase.util.logging.Logging;
 
 /**
+ * Bootstrap class that wait's in a thead for MMBase to have a running state. Once the state is running
+ * it calls MMAdmin.probeCall() and finishes.
  * @author Daniel Ockeloen
- * @version0 $Revision: 1.3 $ $Date: 2001-04-11 14:22:07 $ 
+ * @version  $Revision: 1.4 $ $Date: 2002-11-25 19:39:19 $ 
  */
 public class MMAdminProbe implements Runnable {
 
     private static Logger log = Logging.getLoggerInstance(MMAdminProbe.class.getName()); 
 
-	Thread kicker = null;
-	MMAdmin parent=null;
-	long sleeptime=0;
-	long startdelay=2000;
+	private Thread kicker = null;
 
-	public MMAdminProbe() {
-	}
+	/**
+	 *  DEFAULT_SLEEP_TIME = 0 ms
+	 **/
+	public final static long DEFAULT_SLEEP_TIME = 0;
+
+	long sleeptime = DEFAULT_SLEEP_TIME;
+
+	MMAdmin parent=null;
+
+	/**
+	 * DEFAULT_START_DELAY = 2000; ms
+	 **/
+	public final static long DEFAULT_START_DELAY = 2000;
+
+	long startdelay=DEFAULT_START_DELAY;
 
 	public MMAdminProbe(MMAdmin parent) {
 		this.parent=parent;
-		startdelay=2000;
 		init();
 	}
 
 	public MMAdminProbe(MMAdmin parent,long sleeptime) {
-		this.sleeptime=sleeptime;
 		this.parent=parent;
+
+		this.sleeptime=sleeptime;
+
 		startdelay=0;
 		init();
 	}
 
 	public void init() {
-		this.start();	
-	}
-
-
-	/**
-	 * Starts the main Thread.
-	 */
-	public void start() {
-		/* Start up the main thread */
-		if (kicker == null) {
-			kicker = new Thread(this,"mmexamplesprobe");
+		if (kicker ==null){
+			kicker = new Thread(this,"MMAdminProbe");
 			kicker.start();
+		} else {
+			log.error("MMAdminProbe thread was already running");
 		}
 	}
-	
-	/**
-	 * Stops the main Thread.
-	 */
-	public void stop() {
-		/* Stop thread */
-		kicker.setPriority(Thread.MIN_PRIORITY);  
-		kicker.suspend();
-		kicker.stop();
-		kicker = null;
-	}
 
-	/**
-	 * Main loop, exception protected
-	 */
+
 	public void run () {
-		kicker.setPriority(Thread.MIN_PRIORITY+1);  
 		try {
-			doWork();
+			while (parent.mmb.getState()==false) {
+				try {Thread.sleep(startdelay);} catch (InterruptedException e){}
+			}
+			
+			try { Thread.sleep(sleeptime); } catch (InterruptedException e){ }
+			parent.probeCall();
 		} catch(Exception e) {
-			log.error("run(): Exception in mmexamples thread!");
+			log.error(e.getMessage());
 			log.error(Logging.stackTrace(e));
 		}
 	}
-
-	/**
-	 * Main work loop
-	 */
-	public void doWork() {
-		kicker.setPriority(Thread.MIN_PRIORITY+1);  
-		MMObjectNode node,node2;
-		boolean needbreak=false;
-		int id;
-
-		// ugly pre up polling
-		while (parent.mmb.getState()==false) {
-			try {
-				Thread.sleep(startdelay);
-			} catch (InterruptedException e){
-			}
-		}
-
-		try {
-			Thread.sleep(sleeptime);
-		} catch (InterruptedException e){
-		}
-
-		parent.probeCall();
-	}
-
-
 }
