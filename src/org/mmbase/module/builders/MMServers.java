d5fa3a8bfdb15c3163ee1066d4f338a499f605fb
rob <rob@91430c5f-d668-0410-9e7d-dd663366dcfd>
2003-01-14 10:53:19 +0000

    Rob: removed code in relation to unused protocol drivers.
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@6283 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/builders/MMServers.java
+++ src/org/mmbase/module/builders/MMServers.java
@@ -14,13 +14,13 @@ import java.util.*;
 
 import org.mmbase.module.core.*;
 import org.mmbase.util.*;
-import org.mmbase.module.builders.protocoldrivers.*;
+//import org.mmbase.module.builders.protocoldrivers.*;
 import org.mmbase.util.logging.*;
 
 /**
  * @javadoc
- * @author  $Author: vpro $
- * @version $Revision: 1.17 $ $Date: 2002-05-06 14:39:42 $
+ * @author  $Author: rob $
+ * @version $Revision: 1.18 $ $Date: 2003-01-14 10:53:19 $
  */
 public class MMServers extends MMObjectBuilder implements MMBaseObserver {
 
@@ -31,10 +31,8 @@ public class MMServers extends MMObjectBuilder implements MMBaseObserver {
     private String host;
     private Vector possibleServices=new Vector();
 
-    /** @scope private */
-    Hashtable name2driver;
-    /** @scope private */
-    Hashtable url2driver;
+    //private Hashtable name2driver;
+    //private Hashtable url2driver;
     private int starttime;
 
     /**
@@ -131,7 +129,7 @@ public class MMServers extends MMObjectBuilder implements MMBaseObserver {
         if (imoke==false) {
             createMySelf(machineName);
         }
-        if (name2driver==null) startProtocolDrivers();
+        //if (name2driver==null) startProtocolDrivers();
     }
 
     /**
@@ -242,7 +240,8 @@ public class MMServers extends MMObjectBuilder implements MMBaseObserver {
     /**
      * @javadoc
      */
-    public void startProtocolDrivers() {
+    /*
+    private void startProtocolDrivers() {
         name2driver=new Hashtable();
         url2driver=new Hashtable();
         Enumeration e=search("");
@@ -292,13 +291,7 @@ public class MMServers extends MMObjectBuilder implements MMBaseObserver {
             }
         }
     }
-
-    /**
-     * @javadoc
-     */
-    public ProtocolDriver getDriverByName(String name) {
-        return (ProtocolDriver)name2driver.get(name);
-    }
+    */
 
     /**
      * @javadoc
