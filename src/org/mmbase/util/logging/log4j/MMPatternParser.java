fe41ab4db73b222aef67ce141a16d7c0e3a14142
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2003-02-18 12:04:00 +0000

    because of the memory troubles, I added a '%k' formatter, which logs the memory uses in kb, also reindented
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@6539 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/util/logging/log4j/MMPatternParser.java
+++ src/org/mmbase/util/logging/log4j/MMPatternParser.java
@@ -1,39 +1,54 @@
-package org.mmbase.util.logging.log4j;
+ package org.mmbase.util.logging.log4j;
 
+import org.apache.log4j.*;
 import org.apache.log4j.helpers.FormattingInfo;
 import org.apache.log4j.helpers.PatternConverter;
 import org.apache.log4j.helpers.PatternParser;
 import org.apache.log4j.spi.LoggingEvent;
 
 /**
- * Adds the  conversion pattern 'q' which returns a shorted level (to 3 chars) (so it is like 'p').
+ * Adds the  conversion pattern 'q' which returns a truncated level (from the _end_, not from the beginning as log4j itself would do) . To 3 chars. So it is like 'p'. Also add 'k' which give the currently memory in use (in kb).
  * @author Michiel Meeuwissen
  * @since  MMBase-1.6
- * @version $Id: MMPatternParser.java,v 1.3 2003-02-10 23:44:44 nico Exp $
+ * @version $Id: MMPatternParser.java,v 1.4 2003-02-18 12:04:00 michiel Exp $
  */
 public class MMPatternParser extends PatternParser {
 
 
-  public MMPatternParser(String pattern) {
-    super(pattern);
-  }
+    public MMPatternParser(String pattern) {
+        super(pattern);
+    }
     
-  public void finalizeConverter(char c) {
-    if (c == 'q') {
-      addConverter(new UserDirPatternConverter(formattingInfo));
-      currentLiteral.setLength(0);
-    } else {
-      super.finalizeConverter(c);
+    public void finalizeConverter(char c) {
+        if (c == 'q') {
+            addConverter(new TruncatedLevelPatternConverter(formattingInfo));
+            currentLiteral.setLength(0);
+        } else if (c == 'k') {
+            addConverter(new MemoryPatternConverter(formattingInfo));
+            currentLiteral.setLength(0);
+        } else {
+            super.finalizeConverter(c);
+        }
     }
-  }
-  
-  private class UserDirPatternConverter extends PatternConverter {
-    UserDirPatternConverter(FormattingInfo formattingInfo) {
-      super(formattingInfo);
-    }
-
-    public String convert(LoggingEvent event) {
-        return event.level.toString().substring(0, 3);
-    }
-  }  
+    
+    private class TruncatedLevelPatternConverter extends PatternConverter {
+        TruncatedLevelPatternConverter(FormattingInfo formattingInfo) {
+            super(formattingInfo);
+        }
+        
+        public String convert(LoggingEvent event) {
+            return event.level.toString().substring(0, 3);
+        }
+    }  
+    
+    private class MemoryPatternConverter extends PatternConverter {        
+        MemoryPatternConverter(FormattingInfo formattingInfo) {
+            super(formattingInfo);
+        }
+        
+        public String convert(LoggingEvent event) {
+            Runtime rt = Runtime.getRuntime();
+            return  "" + (rt.totalMemory() - rt.freeMemory()) / 1024;
+        }
+    }  
 }
