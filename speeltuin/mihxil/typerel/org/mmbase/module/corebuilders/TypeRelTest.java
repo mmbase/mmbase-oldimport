8976a58edac452a3d415a1cd2f13b89c6ef7c5be
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2003-02-27 14:16:26 +0000

    more test-case, still not working because remote-impl
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@6672 91430c5f-d668-0410-9e7d-dd663366dcfd

--- speeltuin/mihxil/typerel/org/mmbase/module/corebuilders/TypeRelTest.java
+++ speeltuin/mihxil/typerel/org/mmbase/module/corebuilders/TypeRelTest.java
@@ -8,19 +8,22 @@ import java.util.*;
  * JUnit tests for TypeRel
  *
  * @author  Michiel Meeuwissen 
- * @version $Revision: 1.2 $
+ * @version $Id: TypeRelTest.java,v 1.3 2003-02-27 14:16:26 michiel Exp $
  */
 public class TypeRelTest extends TestCase {
 
     static protected String UNIDIRROLE = "unidirectionalrelation";
     static protected String  BIDIRROLE = "bidirectionalrelation";
+    static protected String  INHROLE   = "inheritancerelation";
 
-    protected Cloud cloud;
-    protected NodeManager relDefManager;
-    protected NodeManager typeRelManager;
-    protected NodeManager insRelManager; 
-    protected NodeManager newsManager; 
-    protected NodeManager urlsManager; 
+    static protected Cloud cloud = null;
+    static protected NodeManager relDefManager;
+    static protected NodeManager typeRelManager;
+    static protected NodeManager insRelManager; 
+    static protected NodeManager newsManager; 
+    static protected NodeManager urlsManager; 
+    static protected NodeManager objectManager; 
+    static protected NodeList    createdNodes;
 
     public TypeRelTest(String testName) {
         super(testName);
@@ -36,12 +39,7 @@ public class TypeRelTest extends TestCase {
         }
         
     }
-    
-
-    /**
-     * Create bidirection relation type, and check if relationmanager in both directions can be found.
-     */
-    public void testBidirectionRelation() {               
+    protected Node createRelDefNode(String role, int dir) {
         // create a new relation-definition
         Node reldef = relDefManager.createNode();
         reldef.setValue("sname", BIDIRROLE);
@@ -51,26 +49,29 @@ public class TypeRelTest extends TestCase {
         reldef.setIntValue("dir", 2);
         reldef.setNodeValue("builder", insRelManager);
         reldef.commit();
+        createdNodes.add(reldef);
+        return reldef;
+    }
 
+    /**
+     * Create bidirection relation type, and check if relationmanager in both directions can be found.
+     */
+    public void testBidirectionRelation() {               
+        Node reldef = createRelDefNode(BIDIRROLE, 2);
         Node typerel = typeRelManager.createNode();
         typerel.setNodeValue("snumber", newsManager);
         typerel.setNodeValue("dnumber", urlsManager);
         typerel.setNodeValue("rnumber", reldef);
         typerel.commit();
+        createdNodes.add(typerel);
 
         // now this relation must exist.
-        try {
-            RelationManagerList rm1 = cloud.getRelationManagers(newsManager, urlsManager, BIDIRROLE);
-            assertTrue(rm1.size() > 0);
-        } catch (NotFoundException e) {
-            fail(e.toString());
-        }
-        try {
-            RelationManagerList rm2 = cloud.getRelationManagers(urlsManager, newsManager, BIDIRROLE);
-            assertTrue(rm2.size() > 0);
-        } catch (NotFoundException e) {
-            fail(e.toString());
-        }
+
+        RelationManagerList rm1 = cloud.getRelationManagers(newsManager, urlsManager, BIDIRROLE);
+        assertTrue(rm1.size() > 0);
+
+        RelationManagerList rm2 = cloud.getRelationManagers(urlsManager, newsManager, BIDIRROLE);
+        assertTrue(rm2.size() > 0);
 
                     
     }
@@ -78,18 +79,8 @@ public class TypeRelTest extends TestCase {
      * Create unidirection relation type, and check if relationmanager in only one direction can be found.
      */
 
-    public void testUnidirectionalRelation() {               
-        // create a new relation-definition
-        Node reldef = relDefManager.createNode();
-        reldef.setValue("sname", UNIDIRROLE);
-        reldef.setValue("dname", UNIDIRROLE);
-        reldef.setValue("sguiname", UNIDIRROLE);
-        reldef.setValue("dguiname", UNIDIRROLE);
-
-        reldef.setIntValue("dir", 1);
-        reldef.setNodeValue("builder", insRelManager);
-
-        reldef.commit();
+    public void testUnidirectionalRelation() {
+        Node reldef = createRelDefNode(UNIDIRROLE, 1);
 
         Node typerel = typeRelManager.createNode();
         typerel.setNodeValue("snumber", newsManager);
@@ -97,39 +88,71 @@ public class TypeRelTest extends TestCase {
 
         typerel.setNodeValue("rnumber", reldef);
         typerel.commit();
+        createdNodes.add(typerel);
+
 
         // now this relation must exist.
-        try {
-            RelationManagerList rm1 = cloud.getRelationManagers(newsManager, urlsManager, UNIDIRROLE);
-            assertTrue(rm1.size() > 0);
-        } catch (NotFoundException e) {
-            fail(e.toString());
-        }
-        try {
-            RelationManagerList rm2 = cloud.getRelationManagers(urlsManager, newsManager, UNIDIRROLE);
-            assertTrue("Found the relations also the other way around, but it is unidirectional", rm2.size() == 0);
-        } catch (NotFoundException e) {
-            fail(e.toString());
-        }
+        RelationManagerList rm1 = cloud.getRelationManagers(newsManager, urlsManager, UNIDIRROLE);
+        assertTrue(rm1.size() > 0);
 
-                    
+        RelationManagerList rm2 = cloud.getRelationManagers(urlsManager, newsManager, UNIDIRROLE);
+        assertTrue("Found the relations also the other way around, but it is unidirectional", rm2.size() == 0);
     }
 
+    public void testInheritanceRelations() {
+        Node reldef = createRelDefNode(INHROLE, 2);
+
+        Node typerel = typeRelManager.createNode();
+        typerel.setNodeValue("snumber", objectManager);
+        typerel.setNodeValue("dnumber", urlsManager);
+        typerel.setNodeValue("rnumber", reldef);
+        typerel.commit();
+        createdNodes.add(typerel);
+
+        // now this relation must exist.
+        RelationManagerList rm1 = cloud.getRelationManagers(objectManager, urlsManager, INHROLE);
+        assertTrue(rm1.size() > 0);
+
+        RelationManagerList rm2 = cloud.getRelationManagers(newsManager, urlsManager, INHROLE);
+        assertTrue(rm2.size() > 0);       
+    }
+
+    public void testClearUpMess() {
+        System.out.println("Clearing up the mess");
+        NodeIterator i = createdNodes.nodeIterator();
+        while (i.hasNext()) {
+            Node node = i.nextNode();
+            System.out.println("Deleting " + node);
+            node.delete();
+        }
+    }
 
     /**
      * Sets up before each test.
      */
     public void setUp() throws Exception {
-        CloudContext cloudContext= ContextProvider.getCloudContext("rmi://127.0.0.1:1111/remotecontext");
-        HashMap user = new HashMap();
-        user.put("username", "admin");
-        user.put("password", "admin5k");
-        cloud = cloudContext.getCloud("mmbase","name/password",user);
-        relDefManager =  cloud.getNodeManager("reldef");
-        typeRelManager = cloud.getNodeManager("typerel");
-        insRelManager  = cloud.getNodeManager("insrel");
-        newsManager  = cloud.getNodeManager("news");
-        urlsManager  = cloud.getNodeManager("urls");
+        if (cloud == null) {
+            CloudContext cloudContext= ContextProvider.getCloudContext("rmi://127.0.0.1:1111/remotecontext");
+            HashMap user = new HashMap();
+            user.put("username", "admin");
+            user.put("password", "admin5k");
+            cloud = cloudContext.getCloud("mmbase","name/password",user);
+            
+            // needed builders for this test.
+            try {
+                relDefManager  =  cloud.getNodeManager("reldef");
+                typeRelManager = cloud.getNodeManager("typerel");
+                insRelManager  = cloud.getNodeManager("insrel");
+                newsManager    = cloud.getNodeManager("news");
+                urlsManager    = cloud.getNodeManager("urls");
+                objectManager  = cloud.getNodeManager("object");
+            } catch (NotFoundException e) {
+                throw new Exception("Test cases cannot be performed because " + e.getMessage() + " Please arrange this in your cloud before running this TestCase.");
+            }
+
+            createdNodes = cloudContext.createNodeList();
+            assertNotNull(createdNodes);
+        }
     }
     
 }
