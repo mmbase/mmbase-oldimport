71dd0f823213f403497430f05de8d6680c89b903
robmaris <robmaris@91430c5f-d668-0410-9e7d-dd663366dcfd>
2003-03-03 22:49:25 +0000

    RvM - database search queries project: Removed BasicFieldValueBetweenConstraintTest as this file is accidentally lost.
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@6729 91430c5f-d668-0410-9e7d-dd663366dcfd

--- speeltuin/dbsq/test/src/org/mmbase/storage/search/implementation/ImplementationSuite.java
+++ speeltuin/dbsq/test/src/org/mmbase/storage/search/implementation/ImplementationSuite.java
@@ -6,7 +6,7 @@ import junit.framework.*;
  * JUnit tests.
  *
  * @author Rob van Maris
- * @version $Revision: 1.8 $
+ * @version $Revision: 1.9 $
  */
 public class ImplementationSuite extends TestCase {
     
@@ -41,7 +41,7 @@ public class ImplementationSuite extends TestCase {
         suite.addTest(org.mmbase.storage.search.implementation.BasicFieldCompareConstraintTest.suite());
         suite.addTest(org.mmbase.storage.search.implementation.BasicFieldConstraintTest.suite());
         suite.addTest(org.mmbase.storage.search.implementation.BasicFieldNullConstraintTest.suite());
-        suite.addTest(org.mmbase.storage.search.implementation.BasicFieldValueBetweenConstraintTest.suite());
+//        suite.addTest(org.mmbase.storage.search.implementation.BasicFieldValueBetweenConstraintTest.suite());
         suite.addTest(org.mmbase.storage.search.implementation.BasicFieldValueConstraintTest.suite());
         suite.addTest(org.mmbase.storage.search.implementation.BasicFieldValueInConstraintTest.suite());
         suite.addTest(org.mmbase.storage.search.implementation.BasicLegacyConstraintTest.suite());
