11db89b828c158b320e27ddd39aedf847f49e1d3
pierre <pierre@91430c5f-d668-0410-9e7d-dd663366dcfd>
2003-03-05 08:14:11 +0000

    pierre: added version tag
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@6752 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/corebuilders/RelDef.java
+++ src/org/mmbase/module/corebuilders/RelDef.java
@@ -41,7 +41,7 @@ import org.mmbase.util.logging.Logging;
  * @todo Fix cache so it will be updated using multicast.
  * @author Daniel Ockeloen
  * @author Pierre van Rooden
- * @version 3 jan 2001
+ * @version $Id: RelDef.java,v 1.27 2003-03-05 08:14:11 pierre Exp $
  */
 
 public class RelDef extends MMObjectBuilder {
