71b6651c8a981b71651daee29b537fa796c70f5e
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2003-03-20 13:42:08 +0000

    moved/cleaned util-reader
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@6900 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/util/xml/UtilReader.java
+++ src/org/mmbase/util/xml/UtilReader.java
@@ -7,46 +7,46 @@ The license (Mozilla version 1.0) can be read at the MMBase site.
 See http://www.MMBase.org/license
  
 */
-package org.mmbase.util;
+package org.mmbase.util.xml;
 
 import org.w3c.dom.Element;
-import java.util.Hashtable;
-import java.util.Enumeration;
-import org.mmbase.module.corebuilders.*;
-import org.mmbase.module.core.*;
+import java.util.*;
 import java.io.File;
+import org.mmbase.module.core.MMBaseContext;
 
 /**
  * This class reads configuration files for utilities, that are 
  * placed in /config/utils/.
  *
  */
-public class XMLUtilReader extends XMLBasicReader {
+public class UtilReader extends org.mmbase.util.XMLBasicReader {
 
+    public static final String CONFIG_UTILS = "utils";
     /**
      * @param filename The name of the property file (e.g. httppost.xml).
      */
-    public XMLUtilReader(String filename) {
-        super(MMBaseContext.getConfigPath()+File.separator+"utils"+File.separator+filename, XMLUtilReader.class);
+    public UtilReader(String filename) {
+        super(MMBaseContext.getConfigPath() + File.separator + CONFIG_UTILS + File.separator + filename, 
+              UtilReader.class);
     }
 
     /**
-     * Get the properties of this builder
+     * Get the properties of this utility.
      */
-    public Hashtable getProperties() {
-        Hashtable hash=new Hashtable();
+    public Map getProperties() {
+        Map map = new HashMap();
         Element e = getElementByPath("util.properties");
-	if (e!=null) {
-	    Enumeration enum = getChildElements(e,"property");
+	if (e != null) {
+	    Enumeration enum = getChildElements(e, "property");
 	    Element p;
 	    String name, value;
 	    while (enum.hasMoreElements()) {
 		p = (Element)enum.nextElement();
-		name = getElementAttributeValue(p,"name");
+		name = getElementAttributeValue(p, "name");
 		value = getElementValue(p);
-		hash.put(name,value);
+		map.put(name,value);
 	    }
 	}
-        return(hash);
+        return Collections.unmodifiableMap(map);
     }
 }
