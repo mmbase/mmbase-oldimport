6e7b5b82573ec38dd56e8ad91d750bb3f302c8ee
kees <kees@91430c5f-d668-0410-9e7d-dd663366dcfd>
2003-03-21 12:22:48 +0000

    keesj:added some spaces
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@6911 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/ProcessorModule.java
+++ src/org/mmbase/module/ProcessorModule.java
@@ -1,4 +1,4 @@
-/* -*- tab-width: 4; -*-
+/*
 
 This software is OSI Certified Open Source Software.
 OSI Certified is a certification mark of the Open Source Initiative.
@@ -31,7 +31,7 @@ public class ProcessorModule extends Module implements ProcessorInterface {
      * Note that different lists may return different builders.
      * @param command the LIST command for which to retrieve the builder
      * @param params contains the attributes for the list
-     */
+     **/
     public MMObjectBuilder getListBuilder(String command,Map params) {
         return new VirtualBuilder(null);
     }
@@ -43,7 +43,7 @@ public class ProcessorModule extends Module implements ProcessorInterface {
      * @param command the list command to execute.
      * @param params contains the attributes for the list
      * @return a <code>Vector</code> that contains the list values contained in MMObjectNode objects
-     */
+     **/
     public Vector getNodeList(Object context, String command, Map params) throws ParseException {
         StringTagger tagger=null;
         if (params instanceof StringTagger) {
@@ -91,7 +91,7 @@ public class ProcessorModule extends Module implements ProcessorInterface {
      * @param sp the page context
      * @param params contains the attributes for the list
      * @param command the list command to execute.
-     */
+     **/
     public Vector  getList(scanpage sp,StringTagger params, String command) throws ParseException {
         throw new ParseException("Module " + this.getClass().getName() + " does not implement LIST");
     }
@@ -104,31 +104,31 @@ public class ProcessorModule extends Module implements ProcessorInterface {
     }
 
     /**
-    *    Replace a command by a string
-    */
+	 * Replace a command by a string
+	 **/
     public String replace (scanpage sp, String command) {
         return("This module doesn't implement this processor call");
     }
 
     /**
-    *    Replace a command by a string
-    * who the hell uses this (daniel)
-    */
+	 * Replace a command by a string
+	 * who the hell uses this (daniel)
+	 **/
     public String replace (scanpage sp, StringTagger command) {
         return("This module doesn't implement this processor call");
     }
 
     /**
-    * Do a cache check (304) for this request
-    */
+	 * Do a cache check (304) for this request
+	 */
     public boolean cacheCheck(scanpage sp,String cmd) {
         return(false);
     }
-
-
+	
+	
     public void init() {
     }
-
+	
     public void reload() {
     }
 
@@ -137,6 +137,4 @@ public class ProcessorModule extends Module implements ProcessorInterface {
 
     public void unload() {
     }
-
-
 }
