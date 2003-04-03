ae96bd5c2e2d39d6aba6956f0559a68b7f9b8f14
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2003-04-03 16:18:39 +0000

    reindented, added @javadoc tags
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@7035 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/core/MMBaseObserver.java
+++ src/org/mmbase/module/core/MMBaseObserver.java
@@ -15,9 +15,16 @@ package org.mmbase.module.core;
  * itself as a change listener on Builder to recieve signals if nodes change.
  *
  * @author Daniel Ockeloen
- * @version $Id: MMBaseObserver.java,v 1.6 2003-03-10 11:50:30 pierre Exp $
+ * @version $Id: MMBaseObserver.java,v 1.7 2003-04-03 16:18:39 michiel Exp $
  */
 public interface MMBaseObserver {
-	public boolean nodeRemoteChanged(String machine,String number,String builder,String ctype);
-	public boolean nodeLocalChanged(String machine,String number,String builder,String ctype);
+    /**
+     * @javadoc
+     */
+    public boolean nodeRemoteChanged(String machine, String number, String builder, String ctype);
+
+    /**
+     * @javadoc
+     */
+    public boolean nodeLocalChanged(String machine, String number, String builder, String ctype);
 }
