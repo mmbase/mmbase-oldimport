4206e20558dfc01e24ded7b2404a0f6aa0522d0b
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2003-04-07 19:18:56 +0000

    Made the 'properties' static and watched so no parsing on every new instance of HttpPost
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@7069 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/util/xml/UtilReader.java
+++ src/org/mmbase/util/xml/UtilReader.java
@@ -13,40 +13,77 @@ import org.w3c.dom.Element;
 import java.util.*;
 import java.io.File;
 import org.mmbase.module.core.MMBaseContext;
+import org.mmbase.util.FileWatcher;
+import org.mmbase.util.XMLBasicReader;
 
+import org.mmbase.util.logging.Logger;
+import org.mmbase.util.logging.Logging;
 /**
  * This class reads configuration files for utilities, that are 
  * placed in /config/utils/.
  *
  */
-public class UtilReader extends org.mmbase.util.XMLBasicReader {
+public class UtilReader {
 
+    private static Logger log = Logging.getLoggerInstance(UtilReader.class.getName());
     public static final String CONFIG_UTILS = "utils";
+
+    private class UtilFileWatcher extends FileWatcher {
+
+        public UtilFileWatcher() {
+            super(true); // true: keep reading.
+        }
+        
+        public void onChange(File file) {
+            readProperties(file);
+        }
+    }
+
+
+    private Map properties;
+    private FileWatcher watcher;
+
     /**
      * @param filename The name of the property file (e.g. httppost.xml).
      */
-    public UtilReader(String filename) {
-        super(MMBaseContext.getConfigPath() + File.separator + CONFIG_UTILS + File.separator + filename, 
-              UtilReader.class);
+    public UtilReader(String filename) {        
+        File file = new File(MMBaseContext.getConfigPath() + File.separator + CONFIG_UTILS + File.separator + filename);
+        readProperties(file);
+        if (file.exists()) {
+            watcher = new UtilFileWatcher();
+            watcher.add(file);
+            watcher.start();
+        } 
     }
 
     /**
      * Get the properties of this utility.
      */
     public Map getProperties() {
-        Map map = new HashMap();
-        Element e = getElementByPath("util.properties");
-	if (e != null) {
-	    Enumeration enum = getChildElements(e, "property");
-	    Element p;
-	    String name, value;
-	    while (enum.hasMoreElements()) {
-		p = (Element)enum.nextElement();
-		name = getElementAttributeValue(p, "name");
-		value = getElementValue(p);
-		map.put(name,value);
-	    }
-	}
-        return Collections.unmodifiableMap(map);
+        return Collections.unmodifiableMap(properties);
+    }
+    protected void readProperties(File f) {
+        if (properties == null) {
+            properties = new HashMap();
+        } else {
+            properties.clear();
+        }
+        if (f.exists()) {
+            XMLBasicReader reader = new XMLBasicReader(f.toString(), UtilReader.class);
+            Element e = reader.getElementByPath("util.properties");
+            if (e != null) {
+                Enumeration enum = reader.getChildElements(e, "property");
+                Element p;
+                String name, value;
+                while (enum.hasMoreElements()) {
+                    p = (Element)enum.nextElement();
+                    name = reader.getElementAttributeValue(p, "name");
+                    value = reader.getElementValue(p);
+                    properties.put(name,value);
+                }
+            }
+        } else {
+            log.error("File " + f + " does not exist");
+        }
     }
 }
