a08941f25261237c5111ed3c76548fd46ebdf53f
pierre <pierre@91430c5f-d668-0410-9e7d-dd663366dcfd>
2003-04-09 14:31:24 +0000

    Pierre: Fix bug #6003: FileWatchers cannot watch file that does not exist. Creating a file or removing it is now concidered a change (Filewatcher no longer throws exceptions when a file does not exist, but waits for it to be created). Code called by the FileWatcher should check this condition (and take appropriate measures) though.
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@7105 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/util/FileWatcher.java
+++ src/org/mmbase/util/FileWatcher.java
@@ -7,12 +7,6 @@ The license (Mozilla version 1.0) can be read at the MMBase site.
 See http://www.MMBase.org/license
 
 */
-/* 
-CREDITS:
-    Contributors:  Mathias Bogaert
-    @author Ceki G&uuml;lc&uuml;    
-    changed licence from apache 1.1 to current licence    
-*/
 
 package org.mmbase.util;
 
@@ -23,43 +17,83 @@ import java.util.Iterator;
 import org.mmbase.util.logging.*;
 
 /**
- *  This will run as a thread afer it has been started, it will check every interval if one
- *  of it's files has been changed.
- *  When one of them has been changed, the OnChange method will be called, with the file witch 
+ *  This will run as a thread after it has been started.
+ *  It will check every interval if one of it's files has been changed.
+ *  When one of them has been changed, the OnChange method will be called, with the file that
  *  was changed. After that the thread will stop.
  *  To stop a running thread, call the method exit();
  *
  *  Example:
-    <code>
-    	class FooFileWatcher extends FileWatcher {
-
-            public FooFileWatcher() {
-                super(true); // true: keep reading.
-            }
-
-	    public void onChange(File file) {
-		System.out.println(file.getAbsolutePath());
-	    }
-    	}
-	    
-	// create new instance
-    	FooFileWatcher watcher = new FooFileWatcher();
-	// set inteval
-	watcher.setDelay(10 * 1000);
-	watcher.add(new File("/tmp/foo.txt"));
-	watcher.start();
-	watcher.add(new File("/tmp/foo.txt"));
-	wait(100*1000);
-	watcher.exit();
-    </code>
+ *   <code style="white-space:pre;">
+ *   	class FooFileWatcher extends FileWatcher {
  *
- * @author Ceki G&uuml;lc&uuml;    
+ *           public FooFileWatcher() {
+ *               super(true); // true: keep reading.
+ *           }
+ *
+ *	    public void onChange(File file) {
+ *		System.out.println(file.getAbsolutePath());
+ *	    }
+ *   	}
+ *
+ *	// create new instance
+ *    	FooFileWatcher watcher = new FooFileWatcher();
+ *	// set inteval
+ *	watcher.setDelay(10 * 1000);
+ *	watcher.add(new File("/tmp/foo.txt"));
+ *	watcher.start();
+ *	watcher.add(new File("/tmp/foo.txt"));
+ *	wait(100*1000);
+ *	watcher.exit();
+ *    </code>
+ *
+ * Thanks to contributions by Mathias Bogaert.
+ * Licence was changed from apache 1.1 to Mozilla.
+ *
+ * @author Ceki G&uuml;lc&uuml;
  * @author Eduard Witteveen
  * @author Michiel Meeuwissen
  * @since  MMBase-1.4
+ * @version $Id: FileWatcher.java,v 1.15 2003-04-09 14:31:24 pierre Exp $
  */
-public abstract class FileWatcher  { //extends Thread {
-    static Logger log = Logging.getLoggerInstance(FileWatcher.class.getName());
+public abstract class FileWatcher  {
+    private static Logger log = Logging.getLoggerInstance(FileWatcher.class.getName());
+
+    private static FileWatcherRunner fileWatchers = new FileWatcherRunner();
+    static {
+        fileWatchers.start();
+    }
+
+    /**
+     *	The default delay between every file modification check, set to 60
+     *  seconds.
+     */
+    static final public long DEFAULT_DELAY = 60000;
+
+    /**
+     * The one thread doing al the work also needs a delay.
+     */
+    static final public long THREAD_DELAY = 1000;
+
+    /**
+     * The delay to observe between every check. By default set {@link
+     * #DEFAULT_DELAY}.
+     */
+    private long delay = DEFAULT_DELAY;
+    private Set files       = new HashSet();
+    private Set removeFiles = new HashSet();
+    private boolean stop = false;
+    private boolean continueAfterChange = false;
+    private long lastCheck = 0;
+
+    protected FileWatcher() {
+        this(true);
+    }
+
+    protected FileWatcher(boolean c) {
+        // make it end when parent treath ends..
+        continueAfterChange = c;
+    }
 
     /**
      * This class is used by init of logging system.
@@ -68,93 +102,6 @@ public abstract class FileWatcher  { //extends Thread {
     public static void reinitLogger() {
         log = Logging.getLoggerInstance(FileWatcher.class.getName());
     }
-
-
-    private class FileEntry {
-    	// static final Logger log = Logging.getLoggerInstance(FileWatcher.class.getName());
-    	private long lastModified; 
-    	private File file;
-        	
-	public FileEntry(File file) {
-	    if(file == null) {
-	    	String msg = "file was null";
-	    	// log.error(msg);
-	    	throw new RuntimeException(msg);	    
-	    }
-	    
-    	    if(!file.exists()) {
-	    	String msg = "file :"  + file.getAbsolutePath() + " did not exist";
-	    	// log.error(msg);
-	    	throw new RuntimeException(msg);	    
-	    }
-	    this.file = file;
-	    lastModified = file.lastModified();
-	}
-	
-	public boolean changed() {
-	    return (lastModified < file.lastModified());
-	}
-
-        /**
-         * Call if changes were treated.
-         */
-        public void updated() {
-            lastModified = file.lastModified();
-        }
-
-    	public File getFile() {
-	    return file;
-	}	
-        public String toString() {
-            return file.toString() + ":" + lastModified;
-        }
-        public boolean equals(Object o) {
-            if (o instanceof FileEntry) {
-                FileEntry fe = (FileEntry) o;
-                return file.equals(fe.file);
-            } else if (o instanceof File) {
-                return file.equals((File) o);
-            }
-            return false;
-        }
-        public int hashCode() {
-            return file.hashCode();
-        }
-        
-    }
-
-    private Set files       = new HashSet();
-    private Set removeFiles = new HashSet();
-
-    /**
-     *	The default delay between every file modification check, set to 60
-     *  seconds.  
-     */
-    static final public long DEFAULT_DELAY = 60000; 
-
-    /**
-     * The one thread doing al the work also needs a delay.
-     */
-    static final public long THREAD_DELAY = 1000; 
-    
-    /**
-     * The delay to observe between every check. By default set {@link
-     * #DEFAULT_DELAY}. 
-     */
-    private long delay = DEFAULT_DELAY; 
-    private boolean stop = false;
-    private boolean continueAfterChange = false;
-    private long lastCheck = 0;
-  
-    protected FileWatcher() {
-        this(true);
-    }
-
-    protected FileWatcher(boolean c) {
-    	// make it end when parent treath ends..
-        continueAfterChange = c;
-    }
-
 
     public void start() {
         fileWatchers.add(this);
@@ -170,20 +117,20 @@ public abstract class FileWatcher  { //extends Thread {
      * Set the delay to observe between each check of the file changes.
      */
     public void setDelay(long delay) {
-    	this.delay = delay;
+        this.delay = delay;
     }
-    
+
     /**
      * Add's a file to be checked...
      * @param file The file which has to be monitored..
      * @throws RuntimeException If file is null or does not exist.
      */
     public void add(File file) {
-    	FileEntry fe = new FileEntry(file);
-    	synchronized(this) {
-	    files.add(fe);
-            if (removeFiles.remove(fe)) log.service("Canceling removal from filewatcher " + fe); 
-	}
+        FileEntry fe = new FileEntry(file);
+        synchronized(this) {
+            files.add(fe);
+            if (removeFiles.remove(fe)) log.service("Canceling removal from filewatcher " + fe);
+        }
     }
 
     /**
@@ -207,9 +154,9 @@ public abstract class FileWatcher  { //extends Thread {
     /**
      */
     public void exit() {
-    	synchronized(this) {
-    	    stop = true;
-	}
+        synchronized(this) {
+            stop = true;
+        }
     }
 
     /**
@@ -218,7 +165,7 @@ public abstract class FileWatcher  { //extends Thread {
     public String toString() {
         return files.toString();
     }
-    
+
     /**
      * Looks if a file has changed. If it is, the 'onChance' for this file is called.
      *
@@ -227,34 +174,34 @@ public abstract class FileWatcher  { //extends Thread {
      *
      */
     private boolean changed() {
-    	synchronized(this) {
-	    Iterator i = files.iterator();
-	    while(i.hasNext()) {
-	    	FileEntry fe = (FileEntry) i.next();       
-		if(fe.changed()) {
-		    log.service("the file :" + fe.getFile().getAbsolutePath() + " has changed.");
+        synchronized(this) {
+            Iterator i = files.iterator();
+            while(i.hasNext()) {
+                FileEntry fe = (FileEntry) i.next();
+                if(fe.changed()) {
+                    log.service("the file :" + fe.getFile().getAbsolutePath() + " has changed.");
                     try {
                         onChange(fe.getFile());
                     } catch (Throwable e) {
                         log.warn("onChange of " + fe.getFile().getName() + " lead to exception:");
                         log.warn(Logging.stackTrace(e));
                     }
-                    if (continueAfterChange) {                        
+                    if (continueAfterChange) {
                         fe.updated(); // onChange was called now, it can be marked up-to-date again
-                    } else { // 
+                    } else { //
                         return true; // stop watching
                     }
-		}
-	    }
-	}
-	return false;
+                }
+            }
+        }
+        return false;
     }
-    
+
     private void removeFiles() {
         synchronized(this) {
             // remove files if necessary
             Iterator ri = removeFiles.iterator();
-            while (ri.hasNext()) {    
+            while (ri.hasNext()) {
                 File f = (File) ri.next();
                 FileEntry found = null;
 
@@ -263,9 +210,9 @@ public abstract class FileWatcher  { //extends Thread {
                 while (i.hasNext()) {
                     FileEntry fe = (FileEntry) i.next();
                     if (fe.getFile().equals(f)) {
-			if (log.isDebugEnabled()){
-				log.debug("removing file["+ fe.getFile().getName() +"]");
-			}
+                        if (log.isDebugEnabled()){
+                                log.debug("removing file["+ fe.getFile().getName() +"]");
+                        }
                         found = fe;
                         break;
                     }
@@ -283,9 +230,9 @@ public abstract class FileWatcher  { //extends Thread {
      * Looks if we have to stop
      */
     private boolean mustStop(){
-    	synchronized(this) {
-	    return stop;
-	}
+        synchronized(this) {
+            return stop;
+        }
     }
 
     public boolean equals(Object o) {
@@ -294,73 +241,11 @@ public abstract class FileWatcher  { //extends Thread {
         return this.getClass().equals(f.getClass()) && this.files.equals(f.files);
     }
 
-    private static FileWatcherRunner fileWatchers = new FileWatcherRunner();
-    static {
-        fileWatchers.start();
-    }
-
-    private static class FileWatcherRunner extends Thread {
-
-        private Set watchers = new HashSet();
-        FileWatcherRunner() {
-            setPriority(MIN_PRIORITY);
-            setDaemon(true);
-        }
-
-        void add(FileWatcher f) {
-            watchers.add(f);
-        }
-
-        /**
-         *  Main loop, will repeat every amount of time.
-         *	It will stop, when either a file has been changed, or exit() has been called
-         */
-        public void run() {
-            do {
-                try {
-                    Iterator i = watchers.iterator();
-                    long now = System.currentTimeMillis();
-                    while (i.hasNext()) {
-                        FileWatcher f = (FileWatcher) i.next();
-                        if (now - f.lastCheck > f.delay) {
-                            if(log.isDebugEnabled()) { 
-                                log.trace("Filewatcher will sleep for : " + f.delay / 1000 + " s. " + 
-                                          "Currently watching: " + f.toString());
-                            }
-                            // System.out.print(".");
-                            f.removeFiles();
-                            if (f.changed() || f.mustStop()) watchers.remove(f); 
-                        }
-                    }
-                    Thread.currentThread().sleep(THREAD_DELAY);
-                } catch(InterruptedException e) {
-                    System.out.println("interrupted");
-                    // no interruption expected
-                }
-                // when we found a change, we exit..
-            } while (true);
-        }
-                
-    }
-
-
     /**
-     * Performance test
+     * @javadoc
      */
-
-    private static class TestFileWatcher extends FileWatcher {
-        int i = 0;
-        protected void onChange(java.io.File f) {
-            // do something..
-            i++;
-        }
-        protected void finalize() {
-            System.out.println(this.toString() + ":" + i);
-        }
-    }
-
     public static void main(String[] args) {
-        
+
         // start some filewatchers
         for (int i = 0; i < 100; i++) {
             FileWatcher w = new TestFileWatcher();
@@ -387,4 +272,154 @@ public abstract class FileWatcher  { //extends Thread {
         }
         System.out.println("\ntook " + (System.currentTimeMillis() - start) + " ms");
     }
+
+    /**
+     * @javadoc
+     */
+    private static class FileWatcherRunner extends Thread {
+
+        private Set watchers = new HashSet();
+        FileWatcherRunner() {
+            setPriority(MIN_PRIORITY);
+            setDaemon(true);
+        }
+
+        void add(FileWatcher f) {
+            watchers.add(f);
+        }
+
+        /**
+         *  Main loop, will repeat every amount of time.
+         *	It will stop, when either a file has been changed, or exit() has been called
+         */
+        public void run() {
+            do {
+                try {
+                    Iterator i = watchers.iterator();
+                    long now = System.currentTimeMillis();
+                    while (i.hasNext()) {
+                        FileWatcher f = (FileWatcher) i.next();
+                        if (now - f.lastCheck > f.delay) {
+                            if(log.isDebugEnabled()) {
+                                log.trace("Filewatcher will sleep for : " + f.delay / 1000 + " s. " +
+                                          "Currently watching: " + f.toString());
+                            }
+                            // System.out.print(".");
+                            f.removeFiles();
+                            if (f.changed() || f.mustStop()) watchers.remove(f);
+                        }
+                    }
+                    Thread.currentThread().sleep(THREAD_DELAY);
+                } catch(InterruptedException e) {
+                    System.out.println("interrupted");
+                    // no interruption expected
+                }
+                // when we found a change, we exit..
+            } while (true);
+        }
+    }
+
+    /**
+     * Performance test
+     */
+    private static class TestFileWatcher extends FileWatcher {
+        int i = 0;
+        protected void onChange(java.io.File f) {
+            // do something..
+            i++;
+        }
+        protected void finalize() {
+            System.out.println(this.toString() + ":" + i);
+        }
+    }
+
+    /**
+     * @javadoc
+     */
+    private class FileEntry {
+        // static final Logger log = Logging.getLoggerInstance(FileWatcher.class.getName());
+        private long lastModified=-1;
+        private boolean exists=false;
+        private File file;
+
+        public FileEntry(File file) {
+            if(file == null) {
+                String msg = "file was null";
+                // log.error(msg);
+                throw new RuntimeException(msg);
+            }
+            exists=file.exists();
+            if(!exists) {
+                // file does not exist. A change will be triggered
+                // once the file comes into existence
+log.info("file :"  + file.getAbsolutePath() + " did not exist (yet)");
+                log.debug("file :"  + file.getAbsolutePath() + " did not exist (yet)");
+                lastModified=-1;
+            } else {
+                lastModified = file.lastModified();
+            }
+            this.file = file;
+        }
+
+        /**
+         * Signal a change.
+         * Returns true if the file was modified, added, or removed.
+         * @return <code>true</code> if the file was changed
+         */
+        public boolean changed() {
+            if (file.exists()) {
+                if (!exists) {
+log.info("file :"  + file.getAbsolutePath() + " added");
+                    return true;
+                } else {
+                    boolean result=lastModified < file.lastModified();
+                    if (result) {
+log.info("file :"  + file.getAbsolutePath() + " changed");
+                    }
+                    return result;
+                }
+            } else {
+                if (exists) {
+log.info("file :"  + file.getAbsolutePath() + " removed");
+                }
+                return exists;
+            }
+        }
+
+        /**
+         * Call if changes were treated.
+         */
+        public void updated() {
+            exists=file.exists();
+            if (exists) {
+                lastModified = file.lastModified();
+            } else {
+                lastModified = -1;
+            }
+        }
+
+        public File getFile() {
+            return file;
+        }
+
+        public String toString() {
+            return file.toString() + ":" + lastModified;
+        }
+
+        public boolean equals(Object o) {
+            if (o instanceof FileEntry) {
+                FileEntry fe = (FileEntry) o;
+                return file.equals(fe.file);
+            } else if (o instanceof File) {
+                return file.equals((File) o);
+            }
+            return false;
+        }
+
+        public int hashCode() {
+            return file.hashCode();
+        }
+
+    }
+
 }
