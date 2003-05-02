e766bb7f958bec0b37cbf91f5dd59fc9070cb180
kees <kees@91430c5f-d668-0410-9e7d-dd663366dcfd>
2003-05-02 11:31:20 +0000

    keesj:indenting
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@7293 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/scan/src/org/mmbase/module/Upload.java
+++ applications/scan/src/org/mmbase/module/Upload.java
@@ -24,112 +24,112 @@ import org.mmbase.util.logging.*;
  * nog wat uitleg over hoe je de .shtml maakt
  */
 public class Upload extends ProcessorModule {
-
+    
     private static Logger log = Logging.getLoggerInstance(Upload.class.getName());
-	private Hashtable FilesInMemory = new Hashtable();
-	private String fileUploadDirectory = null;
-
-	public void init() {
-		fileUploadDirectory=getInitParameter("fileUploadDirectory");
-    	log.debug("Upload module, fileUploadDirectory = "+fileUploadDirectory);
-	}
-
-	/**
-	 * handle the uploaded bytestream.	
-	 */
+    private Hashtable FilesInMemory = new Hashtable();
+    private String fileUploadDirectory = null;
+    
+    public void init() {
+        fileUploadDirectory=getInitParameter("fileUploadDirectory");
+        log.debug("Upload module, fileUploadDirectory = "+fileUploadDirectory);
+    }
+    
+    /**
+     * handle the uploaded bytestream.
+     */
     public boolean process(scanpage sp, Hashtable cmds, Hashtable vars) {
-
-		// Get the place where to store the file
-		// Currently implemented places are: mem:// and file://
-		String filename = (String)cmds.get("file");
-
-		// Get the posted bytearray
-		byte[] bytes = null;
-		try {
-			bytes = sp.poster.getPostParameterBytes("file");
-		} catch (Exception e) {
-			log.error("Upload module postValue to large");
-			return false;
-		}
-
-		if (sp.poster.getPostParameter("file_name") == null) {
-	    	log.error("no filename ");
-			return false;
-		}
-
-		log.debug("Upload module is storing "+filename);
-		// Store in memory
-		if(filename.indexOf("mem://")!=-1) {
-
-			// Create file object in memory		
-			FileInfo fi = new FileInfo();
-   	     	fi.bytes= bytes;
-       	    fi.name = sp.poster.getPostParameter("file_name");
-           	fi.type = sp.poster.getPostParameter("file_type");
-           	fi.size = sp.poster.getPostParameter("file_size");
-			FilesInMemory.put(filename,fi);
-			log.debug("Upload module saves in memory: "+fi.toString());
-			return true;
-		}
-		// Store at filesystem
-		if(filename.indexOf("file://")!=-1) {
-			String fname = filename.substring(7);
-			// If no filename is given the real filename will be used.
-			if (fname.equals("")) { 
-				fname = sp.poster.getPostParameter("file_name");
-			}
-			if (fname.indexOf("..")<0) {
-				saveFile(fileUploadDirectory+fname,bytes);
-				log.debug("Upload module saved to disk: "+filename);
-				return true;
-			} else {
-				log.error("Upload Filename may not contain '..'");
-				return false;
-			}
-		}
-		return false;
-	}
-
-	/**
-	 * deletes an uploaded file.
-	 * @param filename the name of the file, e.g. mem://filename
-	 */
-	public void deleteFile(String filename) {
-		// Is file located in memory?
-		if(filename.indexOf("mem://")!=-1) {
-			if(FilesInMemory.containsKey(filename)) {
-				FilesInMemory.remove(filename);
-			}
-		}
-	}
-
-	/** 
-	 * gets the bytearray of an uploaded file.
-	 * @param filename the name of the file, e.g. mem://filename
-	 */
-	public byte[] getFile(String filename) {
-		log.debug("Upload module is getting "+filename);
-		// Is file located in memory?
-		if(filename.indexOf("mem://")!=-1) {
-			if(FilesInMemory.containsKey(filename)) {
-				FileInfo fi = (FileInfo)FilesInMemory.get(filename);
-				return fi.bytes;
-			}
-		}
-		if(filename.indexOf("http://")!=-1) {
-			return getHttp(filename);
-		}
-		return null;
-	}
-
-
-
-	/**
-	 * save bytearray to filesystem
-	 * @param filename name of the file
-	 * @param value the actual bytes you want to save
-	 */
- 	private boolean saveFile(String filename,byte[] value) {
+        
+        // Get the place where to store the file
+        // Currently implemented places are: mem:// and file://
+        String filename = (String)cmds.get("file");
+        
+        // Get the posted bytearray
+        byte[] bytes = null;
+        try {
+            bytes = sp.poster.getPostParameterBytes("file");
+        } catch (Exception e) {
+            log.error("Upload module postValue to large");
+            return false;
+        }
+        
+        if (sp.poster.getPostParameter("file_name") == null) {
+            log.error("no filename ");
+            return false;
+        }
+        
+        log.debug("Upload module is storing "+filename);
+        // Store in memory
+        if(filename.indexOf("mem://")!=-1) {
+            
+            // Create file object in memory
+            FileInfo fi = new FileInfo();
+            fi.bytes= bytes;
+            fi.name = sp.poster.getPostParameter("file_name");
+            fi.type = sp.poster.getPostParameter("file_type");
+            fi.size = sp.poster.getPostParameter("file_size");
+            FilesInMemory.put(filename,fi);
+            log.debug("Upload module saves in memory: "+fi.toString());
+            return true;
+        }
+        // Store at filesystem
+        if(filename.indexOf("file://")!=-1) {
+            String fname = filename.substring(7);
+            // If no filename is given the real filename will be used.
+            if (fname.equals("")) {
+                fname = sp.poster.getPostParameter("file_name");
+            }
+            if (fname.indexOf("..")<0) {
+                saveFile(fileUploadDirectory+fname,bytes);
+                log.debug("Upload module saved to disk: "+filename);
+                return true;
+            } else {
+                log.error("Upload Filename may not contain '..'");
+                return false;
+            }
+        }
+        return false;
+    }
+    
+    /**
+     * deletes an uploaded file.
+     * @param filename the name of the file, e.g. mem://filename
+     */
+    public void deleteFile(String filename) {
+        // Is file located in memory?
+        if(filename.indexOf("mem://")!=-1) {
+            if(FilesInMemory.containsKey(filename)) {
+                FilesInMemory.remove(filename);
+            }
+        }
+    }
+    
+    /**
+     * gets the bytearray of an uploaded file.
+     * @param filename the name of the file, e.g. mem://filename
+     */
+    public byte[] getFile(String filename) {
+        log.debug("Upload module is getting "+filename);
+        // Is file located in memory?
+        if(filename.indexOf("mem://")!=-1) {
+            if(FilesInMemory.containsKey(filename)) {
+                FileInfo fi = (FileInfo)FilesInMemory.get(filename);
+                return fi.bytes;
+            }
+        }
+        if(filename.indexOf("http://")!=-1) {
+            return getHttp(filename);
+        }
+        return null;
+    }
+    
+    
+    
+    /**
+     * save bytearray to filesystem
+     * @param filename name of the file
+     * @param value the actual bytes you want to save
+     */
+    private boolean saveFile(String filename,byte[] value) {
         File file = new File(filename);
         try {
             FileOutputStream outputstream = new FileOutputStream(file);
@@ -142,15 +142,15 @@ public class Upload extends ProcessorModule {
         }
         return(true);
     }
-
-
-	/** 
-	 * this method gets the requested http page
-	 */ 
-	private byte[] getHttp(String page) {
+    
+    
+    /**
+     * this method gets the requested http page
+     */
+    private byte[] getHttp(String page) {
         URL pageToGrab=null;
         DataInputStream dis=null;
-
+        
         try {
             pageToGrab = new URL(page);
             dis = new DataInputStream(pageToGrab.openStream());
@@ -171,19 +171,19 @@ public class Upload extends ProcessorModule {
         }
         return tekst.toString().getBytes();
     }
-
-
-	/*
-	 * a class to store an uploaded file into memory
-	 */	
-	class FileInfo {
-  		byte[] bytes= null;
+    
+    
+        /*
+         * a class to store an uploaded file into memory
+         */
+    class FileInfo {
+        byte[] bytes= null;
         String name = null;
         String type = null;
         String size = null;
-
-		public String toString () {
-			return "name="+name+" type="+type+" size="+size;
-		}
-	}
+        
+        public String toString() {
+            return "name="+name+" type="+type+" size="+size;
+        }
+    }
 }
