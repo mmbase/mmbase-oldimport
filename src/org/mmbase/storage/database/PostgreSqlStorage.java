731ebe16f71e89197146dff9e680a945b4fbabf2
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2003-05-02 20:18:33 +0000

    moved a bunch of function which are not postgresql specific to AbstractDatabaseStroage
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@7311 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/storage/database/PostgreSqlStorage.java
+++ src/org/mmbase/storage/database/PostgreSqlStorage.java
@@ -22,110 +22,54 @@ import org.mmbase.util.logging.*;
  *
  * @author Pierre van Rooden
  * @since MMBase-1.7
- * @version $Id: PostgreSqlStorage.java,v 1.1 2003-05-02 14:23:05 michiel Exp $
+ * @version $Id: PostgreSqlStorage.java,v 1.2 2003-05-02 20:18:33 michiel Exp $
  */
 public class PostgreSqlStorage extends OODatabaseStorage implements DatabaseStorage {
-
-    /**
-     * Logging instance
-     */
     private static Logger log = Logging.getLoggerInstance(PostgreSqlStorage.class.getName());
 
-    /*
-     * Constructs the AbstractDatabaseSupport database layer support class
-     */
     public PostgreSqlStorage() {
+        super();
     }
 
-    /**
-     * Get text from blob
-     * @javadoc
-     */
-    protected String getText(String tableName,String fieldname,int number) {
-        throw new UnsupportedOperationException("getText");
+    // javadoc inherited
+    protected String getText(String tableName, String fieldName, int number) {
+        throw new UnsupportedOperationException("getText"); // why?
     }
 
-    /**
-     * Get bytes from blob
-     * @javadoc
-     */
-    public byte[] getBytes(String tableName,String fieldname,int number) {
-        byte[] result=null;
-        String sqlselect=selectSQL(tableName,fieldname,number);
-        DatabaseTransaction trans=null;
-        try {
-            trans=createDatabaseTransaction();
-            ResultSet rs=trans.executeQuery(sqlselect);
-            if ((rs!=null) && rs.next()) {
-                result=getDBByte(rs,1);
-            }
-            trans.commit();
-        } catch (Exception e) {
-            if (trans!=null) trans.rollback();
-        }
-        return result;
-    }
 
+
+    // javadoc inherited
     protected void prepare() {
         createSequence();
     }
 
+    /**
+     * Instead of the numberTable in postgresql a 'sequence' is used, which is created by this function.
+     */
     private boolean createSequence() {
-        boolean result=created(getFullTableName("autoincrement"));
+        boolean result = created(getFullTableName("autoincrement"));
         if (!result) {
-            DatabaseTransaction trans=null;
+            DatabaseTransaction trans = null;
             try {
-                trans=createDatabaseTransaction();
-                trans.executeUpdate("CREATE SEQUENCE "+getFullTableName("autoincrement")+" INCREMENT 1 START 1");
+                trans = createDatabaseTransaction();
+                trans.executeUpdate("CREATE SEQUENCE " + getFullTableName("autoincrement") + " INCREMENT 1 START 1");
                 trans.commit();
-                result=true;
+                result = true;
             } catch (StorageException e) {
-                if (trans!=null) trans.rollback();
+                if (trans != null) trans.rollback();
                 log.error(e.getMessage());
             }
         }
         return result;
     }
 
-    /**
-     * Gives an unique number for a node to be inserted.
-     * This method will work with multiple mmbases
-     * @param trans the transaction to use for obtaining the key
-     * @return unique number
-     * @throws StorageException if an error occurred while obtaining the key
-     */
+    // javadoc inherited
     public synchronized int createKey(Transaction trans) throws StorageException {
         DatabaseTransaction dbtrans = (DatabaseTransaction)trans;
-        String sqlselect="SELECT NEXTVAL ('"+  getFullTableName("autoincrement") + "')";
+        String sqlselect = "SELECT NEXTVAL ('"+  getFullTableName("autoincrement") + "')";
         dbtrans.executeQuery(sqlselect);
         return dbtrans.getIntegerResult();
     }
-
-    /** is next function nessecary? */
-    public byte[] getDBByte(ResultSet rs,int idx) {
-        byte[] bytes=null;
-        try {
-            Blob b = rs.getBlob(idx);
-            return b.getBytes(0, (int)b.length());
-        } catch (Exception e) {
-            log.error("MMObjectBuilder -> MMMysql byte  exception "+e);
-            log.error(Logging.stackTrace(e));
-        }
-        return bytes;
-    }
-
-    /** is next function nessecary? */
-    public void setDBByte(int i, PreparedStatement stmt, byte[] bytes) {
-        try {
-            java.io.InputStream stream=new java.io.ByteArrayInputStream(bytes);
-            stmt.setBinaryStream(i, stream, bytes.length);
-            stream.close();
-        } catch (Exception e) {
-            log.error("Can't set byte stream");
-            log.error(Logging.stackTrace(e));
-        }
-    }
-
 
 }
 
