1960a75a42e6e7675cdf861a5f7ecb2eb9dd6843
kees <kees@91430c5f-d668-0410-9e7d-dd663366dcfd>
2003-05-07 13:46:57 +0000

    keesj:removed unsued import + made function final
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@7359 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/storage/database/OODatabaseStorage.java
+++ src/org/mmbase/storage/database/OODatabaseStorage.java
@@ -9,11 +9,6 @@ See http://www.MMBase.org/license
 */
 package org.mmbase.storage.database;
 
-import java.sql.*;
-
-import org.mmbase.storage.*;
-import org.mmbase.util.logging.*;
-
 /**
  * OODatabaseStorage implements the DatabaseStorage interface and the MMJdbc2NodeInterface for
  * an objectoriented database.
@@ -23,12 +18,12 @@ import org.mmbase.util.logging.*;
  * @author Pierre van Rooden
  * @author Michiel Meeuwissen
  * @since MMBase-1.6
- * @version $Id: OODatabaseStorage.java,v 1.5 2003-05-02 20:20:06 michiel Exp $
+ * @version $Id: OODatabaseStorage.java,v 1.6 2003-05-07 13:46:57 kees Exp $
  */
 public abstract class OODatabaseStorage extends SQL92DatabaseStorage implements DatabaseStorage {
 
     // javadoc inherited
-    public boolean supportsExtendedTables() {
+    public final boolean supportsExtendedTables() {
         return true;
     }
 
