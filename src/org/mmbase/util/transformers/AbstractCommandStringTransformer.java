4c39c0ba54e0a8b3751592065923e060e655b1d5
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2003-05-12 21:09:52 +0000

    Yet some other very wild transformers which you might like to try as 'postprocessor' for the content-tag...
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@7470 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/util/transformers/AbstractCommandStringTransformer.java
@@ -0,0 +1,50 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.util.transformers;
+
+import java.io.*;
+
+import org.mmbase.util.externalprocess.CommandLauncher;
+import org.mmbase.util.logging.*;
+
+/**
+ * If you want to transform a Reader stream by the use of an external command, than you can extend
+ * this class. Implement the 'getCommand' function.
+ *
+ * @author Michiel Meeuwissen 
+ * @since MMBase-1.7
+ */
+
+public abstract class AbstractCommandStringTransformer extends StringTransformer 
+    implements CharTransformer {
+    private static Logger log = Logging.getLoggerInstance(AbstractCommandStringTransformer.class);
+
+
+    protected abstract String[] getCommand();
+
+    // javadoc inherited
+    public final String transform(String s) {
+        try {
+            String encoding = System.getProperty("file.encoding");
+            CommandLauncher launcher = new CommandLauncher("Transformer");
+            ByteArrayOutputStream errorStream = new ByteArrayOutputStream();
+            InputStream inputStream = new ByteArrayInputStream(s.getBytes(encoding));
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            launcher.execute(getCommand());
+            launcher.waitAndWrite(inputStream, outputStream, errorStream);        
+            return new String(outputStream.toByteArray(), encoding) + new String(errorStream.toByteArray(), encoding);
+        } catch (UnsupportedEncodingException uee) {
+            log.error(uee.toString());
+        } catch (org.mmbase.util.externalprocess.ProcessException pe) {
+            log.error(pe.toString());
+        }
+        return s;
+    }
+}
---
+++ src/org/mmbase/util/transformers/Figlet.java
@@ -0,0 +1,27 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.util.transformers;
+/**
+ * An escaper based on the amusing 'figlet' tool, which is available on some system. (Make sure it
+ * is in the 'PATH'.
+ *
+ * Figlet output is only fit for text/plain output, or perhaps for <pre> tags...
+ *
+ * @author Michiel Meeuwissen 
+ * @since MMBase-1.7
+ * @version $Id: Figlet.java,v 1.1 2003-05-12 21:09:52 michiel Exp $
+ * @todo   no way to specify commandline arguments on the fly.
+ */
+
+public class Figlet extends AbstractCommandStringTransformer implements CharTransformer {
+    protected String[] getCommand() {
+        return new String[] {"figlet", "-w", "-160"};
+    }
+}
