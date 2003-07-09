e02b5ad00ad2e5850bfe1dacc436c0b8a277c2d4
pierre <pierre@91430c5f-d668-0410-9e7d-dd663366dcfd>
2003-07-09 08:47:15 +0000

    pierre: use cache. apply code-conventions, add javadoc
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@8020 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/scan/src/org/mmbase/module/builders/People.java
+++ applications/scan/src/org/mmbase/module/builders/People.java
@@ -11,29 +11,46 @@ package org.mmbase.module.builders;
 
 import java.util.*;
 
+import org.mmbase.cache.Cache;
 import org.mmbase.module.core.*;
 import org.mmbase.util.*;
 import org.mmbase.util.logging.*;
 
-
 /**
+ * People builder, adds a replace() methods which retrieves a people objectnumber based on the current user's (SCAN) session.
+ *
  * @application Basic [builder]
- * @javadoc
- * @code-conventions
  * @author Daniel Ockeloen
- * @version $Id: People.java,v 1.5 2003-03-10 11:50:20 pierre Exp $
+ * @$Id: People.java,v 1.6 2003-07-09 08:47:15 pierre Exp $
  */
 public class People extends MMObjectBuilder {
 
     private static Logger log = Logging.getLoggerInstance(People.class.getName());
 
-    // cache the 100 most active people, enh. is to allow
-    // people to set it in people.xml
-    /** @scope private */
-    LRUHashtable cache = new LRUHashtable(100);
+    /**
+     * Cache for the most active people
+     */
+    private Cache peopleCache = new Cache(100) {
+        public String getName()        { return "PeopleCache"; }
+        public String getDescription() { return "Cache for most active people"; }
+        };
 
     /**
-     * replace call, when called in format MMBASE-BUILDER-users-xxxxx
+     * Obtains a string value by performing the provided command.
+     * The command can be called:
+     * <ul>
+     *   <li>by SCAN : $MOD-MMBASE-BUILDER-people-[command]</li>
+     *   <li>in jsp : cloud.getNodeManager("people").getInfo(command);</li>
+     * </lu>
+     * The command recognized by the people builder : 
+     * <ul>
+     *   <li>number : Get the object number for the current user's people object, 
+     *                based on the current user/cookie as defined by the key.
+     *                Only works in SCAN, as it uses sessiondata from the scanpage.</li>
+     * </lu>
+     * @param sp The scanpage (containing http and user info) that calls the function
+     * @param tok a list of strings that describe the (sub)command to execute
+     * @return the result value as a <code>String</code>
      */
     public String replace(scanpage sp, StringTokenizer tok) {
         if (tok.hasMoreTokens()) {
@@ -41,53 +58,44 @@ public class People extends MMObjectBuilder {
             if (cmd.equals("number")) {
                 int i=getNumber(sp.getSessionName());
                 if (i!=-1) {
-                    return(""+i);
-                } else {
-                    return("");
+                    return ""+i;
                 }
             }
         }
-            return("");
-        }
+        return "";
+    }
 
     /**
-     * get the object number of this user based on the
-     * current user/cookie as defined by the key
-     * @duplicate check whether the base for this code should be put in MMObjectBuilder
+     * Get the object number for the current user's people object, based on the current user/cookie as defined by the key.
+     * Only works in SCAN, when used with the users builder.
      * @dependency users
-     * @performance reference to users should eb set during init() method.
+     * @performance reference to users should be set during init() method.
      */
     public int getNumber(String key) {
-
-        Integer n=(Integer)cache.get(key);
+        Integer n=(Integer)peopleCache.get(key);
         if (n!=null) {
             log.debug("People - "+key+" people found in cache.");
-            return(n.intValue());
+            return n.intValue();
         }
-
-        // its not in cache so lets check since the current
-        // way is only by users ask the users builder
-        // in the future more ways can be added here
+        // obtain a number from users
         Users bul=(Users)mmb.getMMObject("users");
         if (bul!=null) {
             int i=bul.getNumber(key);
-
             if (i!=-1) {
-                // lets find a related people
+                // lets find a related people object
                 MMObjectNode node=getNode(i);
                 Enumeration e=node.getRelatedNodes("people").elements();
                 if (e.hasMoreElements()) {
                     MMObjectNode node2=(MMObjectNode)e.nextElement();
                     if (node2!=null) {
-                        int number=node2.getIntValue("number");
-                        cache.put(key,new Integer(number));
-                        log.debug("People - people positive");
-                        return(number);
+                        int number=node2.getNumber();
+                        peopleCache.put(key,new Integer(number));
+                        return number;
                     }
                 }
             }
         }
-        return(-1);
+        return -1;
     }
 
     /**
@@ -95,6 +103,6 @@ public class People extends MMObjectBuilder {
      * also signals the session module
      */
     public void flushCache(String key) {
-        cache.remove(key);
+        peopleCache.remove(key);
     }
 }
