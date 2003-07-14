436b2ac057098ab9d1f8a72710536b2e0d683b65
vpro <vpro@91430c5f-d668-0410-9e7d-dd663366dcfd>
2003-07-14 08:13:53 +0000

    Rob: minor changes, fixed layout.
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@8058 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/media/src/org/mmbase/applications/media/cache/URLCache.java
+++ applications/media/src/org/mmbase/applications/media/cache/URLCache.java
@@ -1,12 +1,12 @@
 /*
-
+ 
 This software is OSI Certified Open Source Software.
 OSI Certified is a certification mark of the Open Source Initiative.
-
+ 
 The license (Mozilla version 1.0) can be read at the MMBase site.
 See http://www.MMBase.org/license
-
-*/
+ 
+ */
 package org.mmbase.applications.media.cache;
 
 import org.mmbase.cache.Cache;
@@ -18,7 +18,7 @@ import java.util.*;
 
 /**
  * A cache for URLS requested in the MediaFragment builder.
- * This case can easily be extended in such a way it can cache more requests.
+ * This cache can easily be extended in such a way it can cache more requests.
  *
  * @author Rob Vermeulen (VPRO)
  * @author Michiel Meeuwissen (NOS)
@@ -34,18 +34,20 @@ public class URLCache extends Cache {
     public static URLCache getCache() {
         return cache;
     }
-
+    
     static {
         cache = new URLCache(cacheSize);
         putCache(cache);
     }
-
+    
     /**
      * creates a key based of the media fragment number and the user information
+     * @param mediaFragment fragment to be cached
+     * @param info user information to be cached
      * @return key to be cached
      */
-     static public String toKey(MMObjectNode mediaFragment, Map info) {
-                                                                                                                      
+    static public String toKey(MMObjectNode mediaFragment, Map info) {
+        
         StringBuffer toKey = new StringBuffer("MediaFragmentNumber=").append(mediaFragment.getNumber());
         Iterator infoItems = info.entrySet().iterator();
         while (infoItems.hasNext()) {
@@ -57,136 +59,139 @@ public class URLCache extends Cache {
         }
         return toKey.toString();
     }
-
-
+        
     /**
      * put an entry in the cache
-     * @param objects The objects that can invalidate the cache 
+     * @param key cache key
+     * @param result  cache result
+     * @param objects the objects that can invalidate the cache
      */
     public synchronized void put(String key, String result, List objects) {
         cache.put(key, result);
         log.debug("Adding to cache, key="+key);
         if(objects!=null) {
-            cacheExpire.put(objects,key);	
+            cacheExpire.put(objects,key);
         } else {
             log.debug("No objects are specified to expire the cache entries");
         }
     }
-
+    
     /**
-     * signal if a node change to see if cache entries have to be invalidated.
-     * @param objects The object that can invalidate the cache
+     * Invalidates cache entries based on the node that changes
+     * @param nodeNumber number of node that changes
      */
     public synchronized void nodeChange(String nodeNumber) {
         log.debug("Node changed, number="+nodeNumber);
-	    cacheExpire.remove(nodeNumber);
-    } 
-
+        cacheExpire.remove(nodeNumber);
+    }
+    
     public String getName() {
         return "MediaURLS";
     }
     public String getDescription() {
         return "This cache contains the evaluated urls of media fragments (with user information)";
     }
-
+    
     /**
      * Creates the Cache
      */
     private URLCache(int size) {
         super(size);
     }
-
-	/**	
-	 * Contains information about which objects are used to create a certain cache entry.
-	 * If an object changes it is a good idea to assume that the cache entry is invalid.
-	 */
-	class CacheExpire {
-		private Hashtable objectNumber2Keys = new Hashtable(10000);
-	
-		/**
-	 	 * add objects that were needed for the creation of a cache entry
-		 * @param obj A vector with object numbers (Strings).
-		 * @param key The key of the cache entry to invalidate if an object changes.
-		 */	
-		private void put(List obj, String key) {
-			for(Iterator objects = obj.iterator();objects.hasNext();) {
-				put((MMObjectNode)objects.next(), key);
-			}
-		}
-
-		/**
-	 	 * add object that was needed for the creation of a cache entry
-		 * @param obj A object number.
-		 * @param key The key of the cache entry to invalidate if an object changes.
-		 */	
-		private void put(MMObjectNode node, String key) {
-			Vector keyList = null;
-            		String objectNumber = ""+node.getNumber();
-			if(objectNumber2Keys.containsKey(objectNumber)) {
-				keyList = (Vector)objectNumber2Keys.get(objectNumber);
-			} else {
-				keyList = new Vector(20);
-				objectNumber2Keys.put(objectNumber,new Vector(20));
-			}
-			keyList.add(key);
-			observer.put(node);
-		}
-
-		/**
-		 * remove all entries form the cache that are invalidated by the change of the object.
-		 * @param object the object that changes
-		 */
-		private void remove(String nodeNumber) {
-			if(objectNumber2Keys.contains(nodeNumber)) {
-				Vector keyList = (Vector)objectNumber2Keys.get(nodeNumber);
-				for(Iterator items = keyList.iterator(); items.hasNext();) {
-					String key = (String)items.next();
-					cache.remove(key);
-					log.debug("Flusing key from cache, key="+key);
-				}
-			} 
-		}
-	}
-
+    
+    /**
+     * Contains information about which objects are used to create a certain cache entry.
+     * If an object changes it is a good idea to assume that the cache entry is invalid.
+     */
+    class CacheExpire {
+        private Hashtable objectNumber2Keys = new Hashtable(10000);
+        
+        /**
+         * add objects that were needed for the creation of a cache entry
+         * @param obj A vector with object numbers (Strings).
+         * @param key The key of the cache entry to invalidate if an object changes.
+         */
+        private void put(List obj, String key) {
+            for(Iterator objects = obj.iterator();objects.hasNext();) {
+                put((MMObjectNode)objects.next(), key);
+            }
+        }
+        
+        /**
+         * add object that was needed for the creation of a cache entry
+         * @param obj A object number.
+         * @param key The key of the cache entry to invalidate if an object changes.
+         */
+        private void put(MMObjectNode node, String key) {
+            Vector keyList = null;
+            String objectNumber = ""+node.getNumber();
+            if(objectNumber2Keys.containsKey(objectNumber)) {
+                keyList = (Vector)objectNumber2Keys.get(objectNumber);
+            } else {
+                keyList = new Vector(20);
+                objectNumber2Keys.put(objectNumber,new Vector(20));
+            }
+            keyList.add(key);
+            observer.put(node);
+        }
+        
+        /**
+         * remove all entries form the cache that are invalidated by the change of the object.
+         * @param object the object that changes
+         */
+        private void remove(String nodeNumber) {
+            if(objectNumber2Keys.contains(nodeNumber)) {
+                Vector keyList = (Vector)objectNumber2Keys.get(nodeNumber);
+                for(Iterator items = keyList.iterator(); items.hasNext();) {
+                    String key = (String)items.next();
+                    cache.remove(key);
+                    log.debug("Flusing key from cache, key="+key);
+                }
+            }
+        }
+    }
+    
+    /**
+     */
     private class Observer implements MMBaseObserver  {
         private Set observingBuilders = new HashSet(); // the builders in which 'this' was registered already.
-                
+        
         Observer() {
         }
         
         private void addToObservingBuilder(MMObjectBuilder bul) {
-	    log.debug("Adding observer for builder = "+bul.getTableName());
+            log.debug("Adding observer for builder = "+bul.getTableName());
             bul.addLocalObserver(this);
             bul.addRemoteObserver(this);
             observingBuilders.add(bul.getTableName());
         }
         
         /**
-         */       
+         */
         synchronized void put(MMObjectNode object) {
             MMObjectBuilder bul = object.parent;
             if (!observingBuilders.contains(bul.getTableName())) {
                 addToObservingBuilder(bul);
             }
         }
-
+        
         /**
          * If something changes this function is called, and the observer multilevel cache entries are removed.
          */
         protected boolean nodeChanged(String machine, String number, String builder, String ctype) {
             // d=delete, c=create
             if (ctype.equals("d") || ctype.equals("c")) {
-		log.debug("Recieved a change, object number = "+number);
+                log.debug("Recieved a change, object number = "+number);
                 nodeChange(number);
             }
             return true;
         }
-
+        
         // javadoc inherited (from MMBaseObserver)
         public boolean nodeRemoteChanged(String machine, String number,String builder,String ctype) {
             return nodeChanged(machine, number, builder, ctype);
         }
-
+        
         // javadoc inherited (from MMBaseObserver)
         public boolean nodeLocalChanged(String machine, String number, String builder, String ctype) {
             return nodeChanged(machine, number, builder, ctype);
