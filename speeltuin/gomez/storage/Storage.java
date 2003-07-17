d7ccb8ba4359b768756521dc91eddb97d580c4f7
pierre <pierre@91430c5f-d668-0410-9e7d-dd663366dcfd>
2003-07-17 17:13:10 +0000

    update Storage, add exceptions
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@8128 91430c5f-d668-0410-9e7d-dd663366dcfd

--- speeltuin/gomez/storage/Storage.java
+++ speeltuin/gomez/storage/Storage.java
@@ -12,43 +12,59 @@ package org.mmbase.storage;
 import org.mmbase.module.core.MMBase;
 
 /**
- * The Storage class is used to instantiate the StorageManagerFactory for a 
+ * The Storage class is used to instantiate the StorageManagerFactory for a
  * MMBase system.
  *
  * @author Pierre van Rooden
  * @since MMBase-1.7
- * @version $Id: Storage.java,v 1.2 2003-07-17 13:05:55 pierre Exp $
+ * @version $Id: Storage.java,v 1.3 2003-07-17 17:13:09 pierre Exp $
  */
 public final class Storage {
 
     /**
      * The default storage factory class.
-     * For backward copatibility with classes that do not provide the factory classname in the
+     * For backward compatibility with classes that do not provide the factory classname in the
      * mmbaseroot configuration file.
      */
     static private final String DEFAULT_FACTORY_CLASS = "org.mmbase.storage.database.DatabaseStorageManagerFactory";
-    
+
     /**
      * Obtain the storage manager factory belonging to the indicated MMBase module.
      * @param mmbase The MMBase module for which to retrieve the storagefactory
-     * @return The StoragemanagerFactory
+     * @return The StorageManagerFactory
+     * @throws StorageFactoryException if the StorageManagerFactory class cannot be located, accessed, or instantiated,
+     *         or when something went wrong during configuration of the factory
+     * @throws StorageInaccessibleException when the storage cannot be accessed
      */
-    static public StorageManagerFactory getStorageManagerFactory(MMBase mmbase) {
-        // get teh class name for the factory
-        String factoryClassName = mmbaseRoot.getProperty("storagemanagerfactory");
+    static public StorageManagerFactory getStorageManagerFactory(MMBase mmbase)
+                  throws StorageFactoryException, StorageInaccessibleException {
+        // get the class name for the factory to instantiate
+        String factoryClassName = mmbase.getInitParameter("storagemanagerfactory");
         if (factoryClassName == null) factoryClassName = DEFAULT_FACTORY_CLASS;
         // instantiate and initialize the class
-        Class factoryClass = Class.forName(factoryClassName);
-        StorageManagerFactory factory= (StorageManagerFactory)factoryClass.newInstance();
-        factory.init(mmbase);
-        return factory;
+        try {
+            Class factoryClass = Class.forName(factoryClassName);
+            StorageManagerFactory factory= (StorageManagerFactory)factoryClass.newInstance();
+            factory.init(mmbase);
+            return factory;
+        } catch (ClassNotFoundException cnfe) {
+            throw new StorageFactoryException(cnfe);
+        } catch (IllegalAccessException iae) {
+            throw new StorageFactoryException(iae);
+        } catch (InstantiationException ie) {
+            throw new StorageFactoryException(ie);
+        }
     }
 
     /**
      * Obtain the storage manager factory belonging to the default MMBase module.
      * @return The StoragemanagerFactory
+     * @throws StorageFactoryException if the StorageManagerFactory class cannot be located, accessed, or instantiated,
+     *         or when something went wrong during configuration of the factory
+     * @throws StorageInaccessibleException when the storage cannot be accessed
      */
-    static public StorageManagerFactory getStorageManagerFactory() {
+    static public StorageManagerFactory getStorageManagerFactory()
+                  throws StorageFactoryException, StorageInaccessibleException {
         // determine the mmbase module.
         return getStorageManagerFactory(MMBase.getMMBase());
     }
--- speeltuin/gomez/storage/StorageManagerFactory.java
+++ speeltuin/gomez/storage/StorageManagerFactory.java
@@ -9,33 +9,37 @@ See http://www.MMBase.org/license
 */
 package org.mmbase.storage;
 
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.Map;
-
 import org.mmbase.module.core.MMBase;
 
-
 /**
- * This interface contains functionality for retrieving StorageManager instances, which give access to the storage device. 
+ * This interface contains functionality for retrieving StorageManager instances, which give access to the storage device.
  * It also provides functionality for setting and retrieving configuration data.
  *
  * @author Pierre van Rooden
  * @since MMBase-1.7
- * @version $Id: StorageManagerFactory.java,v 1.1 2003-07-17 13:05:56 pierre Exp $
+ * @version $Id: StorageManagerFactory.java,v 1.2 2003-07-17 17:13:10 pierre Exp $
  */
 public interface StorageManagerFactory {
 
-	public boolean init(MMbase mmbase);
+    /**
+     * Initialize the StorageManagerFactory.
+     * This method is called after instantiation of the class.
+     * This normally happens underwater by calling Storage.getStorageManagerFactory().
+     * @param mmbase the MMBase instance to which this factory belongs
+     * @throws StorageConfigurationException when something went wrong during configuration of the factory
+     * @throws StorageInaccessibleException when the storage cannot be accessed
+     */
+    public void init(MMBase mmbase) throws StorageConfigurationException, StorageInaccessibleException;
 
-	public StorageManager getStorageManager();
-	
+    public StorageManager getStorageManager();
+
     public Map getAttributes();
-	
-	public void setAttributes(Map attributes);
-	
-	public Object getAttribute(Object key);
-	
-	public void setAttribute(Object key, Object value);
+
+    public void setAttributes(Map attributes);
+
+    public Object getAttribute(Object key);
+
+    public void setAttribute(Object key, Object value);
 
 }
