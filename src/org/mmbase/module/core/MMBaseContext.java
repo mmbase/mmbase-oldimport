ceb07b468c47f00567af360044022fdbb4c193ba
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2003-07-18 17:43:02 +0000

    made an exception for orion: don't try to determin rooturlpath automaticly, because it cant (even orion 2.0 can't) (crashing completely)
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@8140 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/core/MMBaseContext.java
+++ src/org/mmbase/module/core/MMBaseContext.java
@@ -23,10 +23,10 @@ import org.mmbase.util.logging.Logging;
  * @author Daniel Ockeloen
  * @author David van Zeventer
  * @author Jaco de Groot
- * @version $Id: MMBaseContext.java,v 1.36 2003-07-01 15:09:42 keesj Exp $
+ * @version $Id: MMBaseContext.java,v 1.37 2003-07-18 17:43:02 michiel Exp $
  */
 public class MMBaseContext {
-    private static Logger log;
+    private static final Logger log = Logging.getLoggerInstance(MMBaseContext.class);
     private static boolean initialized = false;
     private static boolean htmlRootInitialized = false;
     private static ServletContext sx;
@@ -97,7 +97,7 @@ public class MMBaseContext {
      *                    config files.
      *
      */
-    public synchronized static void init(String configPath, boolean initlogging) throws Exception {
+    public synchronized static void init(String configPath, boolean initLogging) throws Exception {
         if (!initialized) {
             // Get the current directory using the user.dir property.
             userDir = System.getProperty("user.dir");
@@ -108,7 +108,7 @@ public class MMBaseContext {
             // Init configpath.
             initConfigpath(configPath);
             // Init logging.
-            if (initlogging) {
+            if (initLogging) {
                 initLogging();
             }
             initialized = true;
@@ -246,9 +246,7 @@ public class MMBaseContext {
 
     private static void initLogging() {
         // Starting the logger
-        Logging.configure(configPath + "/log/log.xml");
-        // Initializing log here because log4j has to be initialized first.
-        log = Logging.getLoggerInstance(MMBaseContext.class.getName());
+        Logging.configure(configPath + File.separator + "log" + File.separator + "log.xml");
         log.info("===========================");
         log.info("MMBase logging initialized.");
         log.info("===========================");
@@ -277,8 +275,7 @@ public class MMBaseContext {
      */
     public synchronized static void initHtmlRoot() throws ServletException {
         if (!initialized || sx == null) {
-            String message = "The init(ServletContext) method should be called"
-                                       + " first.";
+            String message = "The init(ServletContext) method should be called first.";
             System.err.println(message);
             throw new RuntimeException(message);
         }
@@ -422,13 +419,12 @@ public class MMBaseContext {
         try {
             java.net.URL transformed = servletContext.getResource(url);
             if(transformed == null){
-                servletContext.log("no resource is mapped to the pathname: '"+url+"'");
+                log.error("no resource is mapped to the pathname: '"+url+"'");
                 return null;
             }
             return transformed.toString();
-        }
-        catch (java.net.MalformedURLException e) {
-            servletContext.log("could not convert the url: '" + e + "'(error converting)", e);
+        } catch (java.net.MalformedURLException e) {
+            log.error("could not convert the url: '" + e + "'(error converting)");
         }
         return null;
     }
@@ -444,26 +440,37 @@ public class MMBaseContext {
      */
     public synchronized static String getHtmlRootUrlPath() {
         if (! htmlRootUrlPathInitialized) {
+            log.info("Finding root url");
             if (! initialized) {
                 String message = "The init method should be called first.";
                 System.err.println(message);
                 throw new RuntimeException(message);
             }
-            if (sx == null) {
+            if (sx == null) { // no serlvetContext -> not htmlRootUrlPath
                 htmlRootUrlPathInitialized = true;
                 return htmlRootUrlPath;
             }
             String initPath = sx.getInitParameter("mmbase.htmlrooturlpath");
             if (initPath != null) {
+                log.debug("Found mmbase.htmlrooturlpath  explicitely configured");
                 htmlRootUrlPath = initPath;
             } else {
                 // init the htmlRootUrlPath
-                // fetch resource path for the current serletcontext root...
-                String contextUrl = convertResourceUrl(sx, "/");
+                log.debug("Autodetecting htmlrooturlpath ");
 
-                // fetch resource path for the root serletcontext root...
-                ServletContext rootContext = sx.getContext("/");
-                String rootContextUrl = convertResourceUrl(rootContext, "/");
+                // fetch resource path for the root servletcontext root...
+                ServletContext rootContext = null;
+                String rootContextUrl = null;
+                String contextUrl = null;
+            
+
+                if (! sx.getClass().getName().startsWith("com.evermind")) { // Orion horribly fails
+                    contextUrl = convertResourceUrl(sx, "/");
+                    rootContext = sx.getContext("/"); // Orion fails here
+                    rootContextUrl = convertResourceUrl(rootContext, "/");
+                } else {
+                    log.info("For Orion: Use the parameter mmbase.htmlrooturlpath (in web.xml) if your app is not running on '/' (Cannot detect now)");
+                }
 
                 if(contextUrl != null && rootContextUrl != null) {
                     // the beginning of contextUrl is the same as the string rootContextUrl,
@@ -471,28 +478,22 @@ public class MMBaseContext {
                     if(contextUrl.startsWith(rootContextUrl)) {
                         // htmlUrl is gonna be filled
                         htmlRootUrlPath = "/" + contextUrl.substring(rootContextUrl.length(), contextUrl.length());
-                    }
-                    else {
+                    } else {
                         log.warn("the current context:" + contextUrl + " did not begin with the root context :"+rootContextUrl);
                     }
-                }
-                // This works on my tomcat (4.03),.. this is supposed to be the reference implementation?
-                // so what should be the code?
-                else if (rootContextUrl == null && contextUrl != null && contextUrl.startsWith(CONTEXT_URL_IDENTIFIER)) {
+                } else if (rootContextUrl == null && contextUrl != null && contextUrl.startsWith(CONTEXT_URL_IDENTIFIER)) {
+                    // This works on my tomcat (4.03),.. this is supposed to be the reference implementation?
+                    // so what should be the code?
                     // the String will be typically something like "jndi:/hostname/contextname/", so we are looking for the first '/' after the hostname..
                     int contextStart = contextUrl.substring(CONTEXT_URL_IDENTIFIER.length()).indexOf('/');
                     if(contextStart != -1) {
                         htmlRootUrlPath = contextUrl.substring(CONTEXT_URL_IDENTIFIER.length() + contextStart);
-                    }
-                    else {
+                    } else {
                         log.warn("Could not determine htmlRootUrlPath. Using default " + htmlRootUrlPath
                                  + "\nbut  contextUrl     : '" + contextUrl + "' did start with: '"+CONTEXT_URL_IDENTIFIER + "'");
                     }
-                }
-                else {
-                    log.warn("Could not determine htmlRootUrlPath. Using default " + htmlRootUrlPath
-                             + "\n  contextUrl     :" + contextUrl
-                             + "\n  rootContextUrl :" + rootContextUrl );
+                } else {
+                    log.warn("Could not determine htmlRootUrlPath. Using default " + htmlRootUrlPath + "(contextUrl     :" + contextUrl + "  rootContextUrl :" + rootContextUrl + ")");
                 }
             }
             htmlRootUrlPathInitialized = true;
