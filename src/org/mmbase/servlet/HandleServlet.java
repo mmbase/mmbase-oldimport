4f2e44be05540307a7b57ea23499e2d2b20d55f8
keesj <keesj@91430c5f-d668-0410-9e7d-dd663366dcfd>
2003-11-10 13:11:55 +0000

    added util class to filter out photoshop 7 headers that crashes IE
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@8904 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/servlet/HandleServlet.java
+++ src/org/mmbase/servlet/HandleServlet.java
@@ -9,28 +9,22 @@ See http://www.MMBase.org/license
 */
 package org.mmbase.servlet;
 
+import java.io.*;
+import java.util.Map;
+
 import javax.servlet.ServletException;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import java.io.IOException;
-import java.io.BufferedOutputStream;
+import javax.servlet.http.*;
 
 import org.mmbase.bridge.Node;
-
-import org.mmbase.util.logging.Logger;
-import org.mmbase.util.logging.Logging;
-
-import java.util.*;
-
+import org.mmbase.util.IECompatibleJpegInputStream;
+import org.mmbase.util.logging.*;
 
 /**
  * Base servlet for nodes with a 'handle' field. It serves as a basic implementation for more
  * specialized servlets. The mime-type is always application/x-binary, forcing the browser to
  * download.
  *
- * @version $Id: HandleServlet.java,v 1.9 2003-07-09 20:51:20 michiel Exp $
+ * @version $Id: HandleServlet.java,v 1.10 2003-11-10 13:11:55 keesj Exp $
  * @author Michiel Meeuwissen
  * @since  MMBase-1.6
  * @see ImageServlet
@@ -41,13 +35,12 @@ public class HandleServlet extends BridgeServlet {
 
     private long expires; // expires so many milliseconds after serving
 
-
     protected Map getAssociations() {
         Map a = super.getAssociations();
         // Can do the following:
         a.put("attachments", new Integer(0));
-        a.put("downloads",   new Integer(20)); // good at this (because it does not determin the mime-type)
-        a.put("images",      new Integer(-10)); // bad in images (no mime-type, no awareness of icaches)
+        a.put("downloads", new Integer(20)); // good at this (because it does not determin the mime-type)
+        a.put("images", new Integer(-10)); // bad in images (no mime-type, no awareness of icaches)
         return a;
     }
 
@@ -64,7 +57,6 @@ public class HandleServlet extends BridgeServlet {
         }
     }
 
-
     protected String getMimeType(Node node) {
         return "application/x-binary";
     }
@@ -78,9 +70,9 @@ public class HandleServlet extends BridgeServlet {
         String fileName = node.getStringValue("filename");
         if (fileName == null || fileName.equals("")) {
             fileName = node.getStringValue("title");
-            if (fileName == null || fileName.equals("")) {  // give it up
+            if (fileName == null || fileName.equals("")) { // give it up
                 fileName = "mmbase-attachment";
-            } 
+            }
             // try to add an extension. 
             String format = node.getFunctionValue("format", null).toString();
             if (format != null && !format.equals("")) {
@@ -101,14 +93,13 @@ public class HandleServlet extends BridgeServlet {
             // cached images never expire, they cannot change without receiving a new number, thus changing the URL.
             long never = System.currentTimeMillis() + (long) (365.25 * 24 * 60 * 60 * 1000);
             // one year in future, this is considered to be sufficiently 'never'.
-            res.setDateHeader("Expires", never);           
+            res.setDateHeader("Expires", never);
         } else {
             long later = System.currentTimeMillis() + expires;
             res.setDateHeader("Expires", later);
         }
         return true;
     }
-
 
     /**
      * Sets cache-controlling headers. Only nodes which are to be served to 'anonymous' might be
@@ -119,7 +110,7 @@ public class HandleServlet extends BridgeServlet {
      * @since MMBase-1.7
      */
     protected boolean setCacheControl(HttpServletResponse res, Node node) {
-        if (! node.getCloud().getUser().getRank().equals(org.mmbase.security.Rank.ANONYMOUS.toString())) {            
+        if (!node.getCloud().getUser().getRank().equals(org.mmbase.security.Rank.ANONYMOUS.toString())) {
             res.setHeader("Cache-Control", "private,no-store");
             res.setHeader("Pragma", "no-cache"); // for http 1.0
             return true;
@@ -133,30 +124,44 @@ public class HandleServlet extends BridgeServlet {
      * Serves a node with a byte[] handle field as an attachment.
      */
 
-    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {    
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         Node node = getNode(req, res);
-        if (node == null) { 
+        if (node == null) {
             log.debug("No node found, returning");
             return;
         }
-        if (! node.getNodeManager().hasField("handle")) {
+        if (!node.getNodeManager().hasField("handle")) {
             res.sendError(HttpServletResponse.SC_NOT_FOUND, "No handle found in node " + node.getNumber());
             return;
         }
-        byte[] bytes = node.getByteValue("handle"); 
+        byte[] bytes = node.getByteValue("handle");
 
         // fill the headers
-        res.setDateHeader("Date", System.currentTimeMillis());                    
+        res.setDateHeader("Date", System.currentTimeMillis());
 
         String mimeType = getMimeType(node);
         res.setContentType(mimeType);
 
-        if (!setContent(res, node, mimeType)) return;
+        /*
+         *  remove additional information left by PhotoShop 7 in jpegs
+         * , this information may crash Internet Exploder. that's why you need to remove it.
+         * With PS 7, Adobe decided by default to embed XML-encoded "preview" data into JPEG files, 
+         * using a feature of the JPEG format that permits embedding of arbitrarily-named "profiles". 
+         * In theory, these files are valid according to the JPEG specifications. 
+         * However they break many applications, including Quark and, significantly, 
+         * various versions of Internet Explorer on various platforms. 
+         * http://www.photo.net/bboard/q-and-a-fetch-msg?msg_id=003j8d
+         */
+        if (mimeType.equals("image/jpeg") || mimeType.equals("image/jpg")) {
+        	bytes = IECompatibleJpegInputStream.process(bytes);
+        }
+
+        if (!setContent(res, node, mimeType))
+            return;
         setExpires(res, node);
         setCacheControl(res, node);
         sendBytes(res, bytes);
     }
-
 
     /**
      * Utility function to send bytes at the end of doGet implementation.
@@ -175,6 +180,5 @@ public class HandleServlet extends BridgeServlet {
         out.flush();
         out.close();
     }
-
 
 }
