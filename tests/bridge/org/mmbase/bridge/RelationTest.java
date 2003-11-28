77d3aba2814726f30d47adf7809c26628e411775
keesj <keesj@91430c5f-d668-0410-9e7d-dd663366dcfd>
2003-11-28 12:04:30 +0000

    tweaked RelationTest so that it does not fail any more
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@9082 91430c5f-d668-0410-9e7d-dd663366dcfd

--- tests/bridge/org/mmbase/bridge/RelationTest.java
+++ tests/bridge/org/mmbase/bridge/RelationTest.java
@@ -13,18 +13,18 @@ import org.mmbase.tests.*;
 /**
  * Test cases to test the creation of relations and the retrieval of them
  * @author Kees Jongenburger
- * @version $Id: RelationTest.java,v 1.3 2003-11-19 10:01:28 keesj Exp $
+ * @version $Id: RelationTest.java,v 1.4 2003-11-28 12:04:30 keesj Exp $
  */
 public class RelationTest extends BridgeTest {
-    
-    public RelationTest(String name){
-    	super(name);
+
+    public RelationTest(String name) {
+        super(name);
     }
-    
+
     public void testCreateRelations() {
         //first create 3 nodes
         Cloud cloud = getCloud();
-        
+
         Node aaFirstNode = cloud.getNodeManager("aa").createNode();
         aaFirstNode.setStringValue("stringfield", "aaFirstNode");
         aaFirstNode.commit();
@@ -36,37 +36,72 @@ public class RelationTest extends BridgeTest {
         Node aaSecondNode = cloud.getNodeManager("aa").createNode();
         aaSecondNode.setStringValue("stringfield", "aaSecondNode");
         aaSecondNode.commit();
-        
+
         RelationManager relatedRelationManager = cloud.getRelationManager("related");
-        
-        
-        
+
         //create a relation from aaFirstNode to bbNode
-        Relation related1 =aaFirstNode.createRelation(bbNode,relatedRelationManager);
+        Relation related1 = aaFirstNode.createRelation(bbNode, relatedRelationManager);
         related1.commit();
-        
+
         //create a relation from bbNode to aaSecondNode
-        Relation related2 = bbNode.createRelation(aaSecondNode,relatedRelationManager);
+        Relation related2 = bbNode.createRelation(aaSecondNode, relatedRelationManager);
         related2.commit();
-        
+
         //we now have 3 node
         //in the BridgeTest application the folowing is defined
-		//<relation from="aa" to="bb" type="related" />
-		//<relation from="bb" to="aa" type="related" />
+        //<relation from="aa" to="bb" type="related" />
+        //<relation from="bb" to="aa" type="related" />
 
-		//and we have created the folowing structure
+        //and we have created the folowing structure
         //aaFirstNode -> related1 -> bbNode -> related2 -> aaSecondNode
+
+        //check back if we can get the nodes
+        {
+            //ask for the relation in the "right" direction
+            NodeList aaRelatedList = aaFirstNode.getRelatedNodes("bb", "related", "destination");
+            assertTrue("related list has size " + aaRelatedList.size() + " but should be 1", aaRelatedList.size() == 1);
+            if (aaRelatedList.size() > 0) {
+                Node theOtherNode = aaRelatedList.getNode(0);
+                assertTrue("expected the bb node but got " + theOtherNode, theOtherNode.getNumber() == bbNode.getNumber());
+            }
+        }
         
-        //check back if we can get the direction
+        {
+            //folow the relation in  wrong direction
+            NodeList aaRelatedList = aaFirstNode.getRelatedNodes("bb", "related", "source");
+            assertTrue("related list has size " + aaRelatedList.size() + " but should be 0", aaRelatedList.size() == 0);
+        }
+
+        {
+            //try to get the first aa node from the bb node
+            NodeList bbRelatedListSource = bbNode.getRelatedNodes("aa", "related", "source");
+            assertTrue("relation count should be 1 but is " + bbRelatedListSource.size() + " mmbase relations dont' work", bbRelatedListSource.size() == 1);
+            if (bbRelatedListSource.size() > 0) {
+                Node sourceNode = bbRelatedListSource.getNode(0);
+                assertTrue("expected the first aa node but got " + sourceNode, sourceNode.getNumber() == aaFirstNode.getNumber());
+            }
+        }
+
+        {
+            //try to get the second aa node from the bb node
+            NodeList bbRelatedListSource = bbNode.getRelatedNodes("aa", "related", "destination");
+            assertTrue("relation count should be 1 but is " + bbRelatedListSource.size() + " mmbase relations dont' work", bbRelatedListSource.size() == 1);
+            if (bbRelatedListSource.size() > 0) {
+                Node sourceNode = bbRelatedListSource.getNode(0);
+                assertTrue("expected the first aa node but got " + sourceNode, sourceNode.getNumber() == aaSecondNode.getNumber());
+            }
+        }
+
+        {
+            //try to get both the aa nodes using "both"
+            NodeList bbRelatedListSource = bbNode.getRelatedNodes("aa", "related", "both");
+            assertTrue("relation count should be 2 but is " + bbRelatedListSource.size() + " mmbase relatednodes(BOTH) does not work", bbRelatedListSource.size() == 2);
+        }
         
-        NodeList aaRelatedList = aaFirstNode.getRelatedNodes("bb","related" ,"source");
-        assertTrue("related list has size " + aaRelatedList.size() + " but should be 1", aaRelatedList.size() == 1);
-        
-        NodeList bbRelatedListSource = bbNode.getRelatedNodes("aa","related","source");
-        assertTrue("relation count should be 1 but is "+ bbRelatedListSource.size() +" mmbase relations dont' work",bbRelatedListSource.size() == 1);
-        
-        NodeList bbRelatedListDestination = bbNode.getRelatedNodes("aa","related","destination");
-        assertTrue("relation count should be 1 but is "+bbRelatedListDestination.size() +" mmbase relations dont' work",bbRelatedListDestination.size() ==1);
-		
+        {
+            //try to get both the aa node from bb using "null"
+            NodeList bbRelatedListSource = bbNode.getRelatedNodes("aa", "related", null);
+            assertTrue("relation count should be 2 but is " + bbRelatedListSource.size() + " mmbase relatednodes() does not work", bbRelatedListSource.size() == 2);
+        }
     }
 }
