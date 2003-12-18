c63672554c0529a61ad39221d6e9edd0ee79d48d
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2003-12-18 23:12:20 +0000

    function-hack
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@9294 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/util/functions/Function.java
@@ -0,0 +1,98 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.util.functions;
+
+
+//import org.mmbase.util.logging.*;
+
+
+/**
+ * An abstract representation of a piece of functionality (a 'function'). A function as a name, a
+ * return type, and an parameter-definition (a Parameter array).
+ *
+ * @author Daniel Ockeloen
+ * @author Michiel Meeuwissen
+ * @version $Id: Function.java,v 1.1 2003-12-18 23:12:20 michiel Exp $
+ * @since MMBase-1.7
+ * @see Parameter
+ * @see Parameters
+ */
+abstract public class Function {
+
+    //private static final Logger log = Logging.getLoggerInstance(Function.class);
+    
+    protected String      name;
+    protected ReturnType  returnType;
+
+    private Parameter[] parameterDefinition;
+    private String     description;
+
+
+    protected Function(String name, Parameter[] def, ReturnType returnType) {
+        this.name = name;
+        this.parameterDefinition = def;
+        this.returnType = returnType;
+    }
+    /**
+     * Creates an empty 'Parameters'  object for you, which you have to fill and feed back to getFunctionValue
+     * @see #getFunctionValue
+     */
+
+    public Parameters getNewParameters() {
+        if (parameterDefinition == null) {
+            throw new IllegalStateException("Definition is not set yet");
+        }
+        return new Parameters(parameterDefinition);
+    }
+
+    /**
+     * Executes the defined function supplying the given arguments.
+     * @see #getNewParameters
+     */
+
+    abstract public Object getFunctionValue(Parameters arguments); 
+    /*
+    {
+        throw new UnsupportedOperationException("This is only an abstract representation of a function with name and cannot be actually executed. Use an extension of this class if you want that.");
+    }
+    */
+
+    public void setDescription(String description)   { 
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public Parameter[] getParameterDefinition() {
+        return parameterDefinition;
+    }
+    public void setParameterDefinition(Parameter[] params) {
+        if (parameterDefinition != null) {
+            throw new IllegalStateException("Definition is set already");
+        }
+        parameterDefinition = params;
+    }
+    public ReturnType getReturnType() {
+        return returnType;
+    }
+    public void setReturnType(ReturnType type) {
+        if (returnType != null) {
+            throw new IllegalStateException("Returntype is set already");
+        }
+        returnType = type;
+    }
+    
+
+}
---
+++ src/org/mmbase/util/functions/ReturnType.java
@@ -0,0 +1,70 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.util.functions;
+
+import java.util.*;
+import org.mmbase.util.logging.*;
+import org.mmbase.bridge.*;
+
+/**
+ * Description of the return type of certain function. This wraps a Class object but it has some extra 
+ * members.
+ *
+ * @author Daniel Ockeloen
+ * @author Michiel Meeuwissen
+
+ * @version $Id: ReturnType.java,v 1.1 2003-12-18 23:12:20 michiel Exp $
+ * @since MMBase-1.7
+ */
+public class ReturnType {
+
+    public static final ReturnType VOID = new ReturnType(null, "Does not return anything");
+    public static final Object     VOID_VALUE = new Object();
+
+    private Class type;
+    private String description;
+    private Map   typeStruct = new HashMap(); // key -> ReturnType
+
+    public  ReturnType(Class type, String description) {
+        this.type = type;
+        this.description = description;
+    }  
+
+    /**
+     * @return The 'Class' object which this object is wrapping.
+     */
+    public Class getType() {
+        return type;
+    }
+    
+    /**
+     * @return A description of the return value. For documentation purposes.
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * If the return type is like a map or struct (key-values pairs), then you might want to describe the 
+     * types of the values seperately too.
+     */
+
+    ReturnType addSubType(String name,  ReturnType type) {
+        return (ReturnType) typeStruct.put(name, type); 
+    }
+
+    public Map getSubTypes() {
+        return Collections.unmodifiableMap(typeStruct);
+    }
+
+
+
+
+}
