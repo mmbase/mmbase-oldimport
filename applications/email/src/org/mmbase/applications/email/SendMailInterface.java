03d985be558ef7a5084e38f0e0b68f389e6e1ee5
daniel <daniel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2004-01-07 12:07:28 +0000

    prepared move from core
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@9439 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ applications/email/src/org/mmbase/applications/email/SendMailInterface.java
@@ -0,0 +1,25 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.applications.email;
+
+import java.util.*;
+import org.mmbase.util.*;
+
+
+public interface SendMailInterface {
+    public boolean sendMail(String from, String to, String data);
+    public boolean sendMail(String from, String to, String data, Map headers);
+    public boolean sendMail(Mail mail);
+    /**
+     * checks the e-mail address
+     */
+    public String verify(String name);
+    public List   expand(String name);
+}
