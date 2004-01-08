baac8389fbc5dd9758be9eaca8e1aaba60e4724c
robmaris <robmaris@91430c5f-d668-0410-9e7d-dd663366dcfd>
2004-01-08 14:19:20 +0000

    RvM - Added note for tests yet to be implemented.
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@9515 91430c5f-d668-0410-9e7d-dd663366dcfd

--- tests/dbsq/org/mmbase/storage/search/implementation/ImplementationSuite.java
+++ tests/dbsq/org/mmbase/storage/search/implementation/ImplementationSuite.java
@@ -6,7 +6,7 @@ import junit.framework.*;
  * JUnit tests.
  *
  * @author Rob van Maris
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class ImplementationSuite extends TestCase {
     
@@ -41,7 +41,8 @@ public class ImplementationSuite extends TestCase {
         suite.addTest(org.mmbase.storage.search.implementation.BasicFieldCompareConstraintTest.suite());
         suite.addTest(org.mmbase.storage.search.implementation.BasicFieldConstraintTest.suite());
         suite.addTest(org.mmbase.storage.search.implementation.BasicFieldNullConstraintTest.suite());
-//        suite.addTest(org.mmbase.storage.search.implementation.BasicFieldValueBetweenConstraintTest.suite());
+//        yet to be implemented:
+//        suite.addTest(org.mmbase.storage.search.implementation.BasicFieldValueBetweenConstraintTest.suite()); 
         suite.addTest(org.mmbase.storage.search.implementation.BasicFieldValueConstraintTest.suite());
         suite.addTest(org.mmbase.storage.search.implementation.BasicFieldValueInConstraintTest.suite());
         suite.addTest(org.mmbase.storage.search.implementation.BasicLegacyConstraintTest.suite());
