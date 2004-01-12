709a18ddf274666069aa63bbc2617042a3ebbc59
nico <nico@91430c5f-d668-0410-9e7d-dd663366dcfd>
2004-01-12 22:10:29 +0000

    Fixed save javascript
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@9630 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/editwizard/examples/finalist/javascript/override.js
+++ applications/editwizard/examples/finalist/javascript/override.js
@@ -26,12 +26,16 @@ function updateButtons(allvalid) {
     
    if(savebut!= null) {
       if (allvalid) {
+         setSaveInactive("false");
+
          savebut.src = savebut.getAttribute("enabledsrc");
          savebut.className = "bottombutton";
          savebut.disabled = false;
          var usetext = getToolTipValue(savebut,"titlesave", "Stores all changes.");
          savebut.title = usetext;
       } else {
+         setSaveInactive("true");
+
          savebut.src = savebut.getAttribute("disabledsrc");
          savebut.className = "bottombutton-disabled";
          savebut.disabled = true;
@@ -39,4 +43,21 @@ function updateButtons(allvalid) {
          savebut.title = usetext;
       }
    }
+}
+
+function setFacusOnFirstInput() {
+    var form = document.forms["form"];
+    for (var i=0; i < form.elements.length; i++) {
+        var elem = form.elements[i];
+        // find first editable field
+        var hidden = elem.getAttribute("type"); //.toLowerCase();
+        if (hidden != "hidden") {
+            // It is very annoying when you want to scroll with a wheel mouse
+            // when you open a wizard and the selectbox is the first field.
+            if (elem.getAttribute('ftype') != 'enum') {
+                elem.focus();
+                break;
+            }
+        }
+    }
 }
