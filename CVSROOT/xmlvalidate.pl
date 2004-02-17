933cc4a71769424804a6c3a087f6f4cff397de57
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2004-02-17 15:53:13 +0000

    proposal, whch we might use soon (thanks to Gullie)
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@10000 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ CVSROOT/xmlvalidate.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+
+# Michiel: not yet used, but we propose to do it soon.
+# 
+# <Gullie> commitinfo:^eo/docs   $CVSROOT/CVSROOT/xmlvalidate.xml
+
+$module_dir = shift @ARGV;
+
+foreach $file (@ARGV) {
+  if ($file =~ m/xml$/ && $file !~ m/build.xml/) {
+    if (open FIL, "<$file") {
+      close FIL;
+      $files .= " $file";
+    } else {
+      print "Ignoring file '$file', cannot open it\n";
+    }
+  }
+}
+
+print "Testing for XML DTD compliance:$files\n";
+if ($files ne "") {
+  @output = `SGML_CATALOG_FILES=/usr/share/sgml/docbook/xmlcatalog:/usr/share/sgml/docbook/dtd/xml/4.1.2/catalog.xml /usr/bin/xmllint --catalogs --valid --noout $files 2>&1`;
+  for $line (@output) {
+    print STDERR $line;
+  }
+  if ($? != 0) {
+    exit(-1);
+  }
+  if ($#output > 0) {
+    exit(-1);
+  }
+  exit(0);
+}
