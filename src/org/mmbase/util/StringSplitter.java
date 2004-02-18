231be0f96965ca26b7a9ae8ebf9d9a850429fc6c
pierre <pierre@91430c5f-d668-0410-9e7d-dd663366dcfd>
2004-02-18 12:29:26 +0000

    bugfix #6342  mm:import vartype="List" ignores empty elements Now accepts empty elements
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@10020 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/util/StringSplitter.java
+++ src/org/mmbase/util/StringSplitter.java
@@ -10,34 +10,40 @@ See http://www.MMBase.org/license
 package org.mmbase.util;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.StringTokenizer;
 
 /**
- * Simple util methods to split strings.
- *
- **/
-
+ * Utility class for splitting delimited values.
+ * @author Pierre van Rooden
+ * @author Kees Jongenburger
+ * @version $Id: StringSplitter.java,v 1.2 2004-02-18 12:29:26 pierre Exp $
+ */
 public class StringSplitter {
 
     /**
-     * Simple util method to split comma separated values
-     * to a list. Useful for attributes.
+     * Simple util method to split delimited values to a list. Useful for attributes.
+     * Similar to <code>String.split()</code>, but returns a List instead of an array, and trims the values.
      * @param string the string to split
      * @param delimiter
-     * @return a List containing the elements, the elements are also trimed     
+     * @return a List containing the elements
      */
-
     static public List split(String attribute, String delimiter) {
-        List retval = new ArrayList();
-        if (attribute == null) return retval;
-        StringTokenizer st = new StringTokenizer(attribute, delimiter);
-        while(st.hasMoreTokens()){
-            retval.add(st.nextToken().trim());
+        if (attribute == null) return new ArrayList();
+        String[] values = attribute.split(delimiter);
+        for (int i = 0; i < values.length; i++) {
+            values[i] = values[i].trim();
         }
-        return retval;
+        return Arrays.asList(values);
     }
 
+    /**
+     * Simple util method to split comma separated values.
+     * @see #split(String, String)
+     * @param string the string to split
+     * @param delimiter
+     * @return a List containing the elements
+     */
     static public List split(String string) {
         return split(string, ",");
     }
