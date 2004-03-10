5c1d54a1b61995c34565de604d98200b2b33c464
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2004-03-10 15:02:42 +0000

    fixed some typo's, added javadoc for constructor
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@10235 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/core/ByteFieldContainer.java
+++ src/org/mmbase/module/core/ByteFieldContainer.java
@@ -11,21 +11,27 @@ package org.mmbase.module.core;
 
 /**
  * MMObjectNodes can contain Binary data. These BYTE fields can be
- * retrieved, byt teh node does not sstore them internally - nodes are cached, and
+ * retrieved, byt the node does not store them internally - nodes are cached, and
  * caching large amounts of binary data is bad for performance.
- * However, some classes keep a sepearta e cacahe for binaries (i.e.Images).
+ * However, some classes keep a separate  cache for binaries (i.e. Images).
  * This class is meant to hold byte arrays while simultaneously keep data as to
  * which object it is part of. This allows for caching and passing binary values
  * between functions without loosing track of which image it is part of.
  *
  * @author Pierre van Rooden
- * @version $Id: ByteFieldContainer.java,v 1.2 2004-03-10 14:51:35 michiel Exp $
+ * @version $Id: ByteFieldContainer.java,v 1.3 2004-03-10 15:02:42 michiel Exp $
  * @since MMBase-1.7
  */
 public class ByteFieldContainer {
     public int number = -1;
     public byte[] value = null;
 
+
+    /**
+     * Constructor of this container class.
+     * @param number The node number of the node where the byte[] is belonging to, or -1 if the byte array is not yet associated with a node.
+     * @param value  The byte array which this container is wrapping.
+     */
     public ByteFieldContainer(int number, byte[] value) {
         this.number = number;
         this.value = value;
