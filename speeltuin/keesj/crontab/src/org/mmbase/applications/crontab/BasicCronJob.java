7f3a1ff5f922cea2ab82d0269829e20397961189
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2004-03-25 18:26:03 +0000

    also helpful
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@10371 91430c5f-d668-0410-9e7d-dd663366dcfd

--- speeltuin/keesj/crontab/src/org/mmbase/applications/crontab/BasicCronJob.java
+++ speeltuin/keesj/crontab/src/org/mmbase/applications/crontab/BasicCronJob.java
@@ -7,7 +7,12 @@ public abstract class BasicCronJob implements JCronJob {
     protected JCronEntry jCronEntry;
     public void init(JCronEntry j) {
         jCronEntry = j;
+        init();
     }
+
+    protected init() {
+    }
+
     public void stop() {
     }
     public abstract void run();
