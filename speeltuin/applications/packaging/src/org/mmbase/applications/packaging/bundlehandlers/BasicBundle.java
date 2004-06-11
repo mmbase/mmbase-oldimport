59b62bbf23dda6436372fce2faf21392ceecff7e
daniel <daniel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2004-06-11 23:24:32 +0000

    bundle handers
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@10885 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ speeltuin/applications/packaging/src/org/mmbase/applications/packaging/bundlehandlers/BasicBundle.java
@@ -0,0 +1,577 @@
+/*
+ 
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+ 
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+ 
+ */
+
+package org.mmbase.applications.packaging.bundlehandlers;
+
+import org.mmbase.bridge.*;
+import org.mmbase.module.core.*;
+import org.mmbase.util.logging.*;
+import org.mmbase.util.*;
+import org.mmbase.module.builders.Versions;
+import org.mmbase.applications.packaging.*;
+import org.mmbase.applications.packaging.packagehandlers.*;
+import org.mmbase.applications.packaging.providerhandlers.*;
+import org.mmbase.applications.packaging.installhandlers.*;
+import org.mmbase.applications.packaging.sharehandlers.*;
+
+import java.io.*;
+import java.util.*;
+import java.util.jar.*;
+
+import org.w3c.dom.*;
+
+/**
+ * BasicBundle, base class for bundles
+ *
+ * @author Daniel Ockeloen (MMBased)
+ */
+public class BasicBundle implements BundleInterface {
+    private static Logger log = Logging.getLoggerInstance(BasicBundle.class.getName());
+    
+    private String name;
+    private String id;
+    private String maintainer;
+    private String version;
+    private String date;
+    private String type="unknown/unknown";
+    private String state="not installed";
+    private String path;
+    private String description="";
+    private String releasenotes="";
+    private String installationnotes="";
+    private String licensename="";
+    private String licensetype="";
+    private String licenseversion="";
+    private String licensebody="";
+    private ProviderInterface provider;
+    private ShareInfo shareinfo;
+    private Hashtable neededpackages = new Hashtable();
+    private ArrayList initiators,supporters,contacts,developers;
+
+
+    // the install manager keeps track of what happend during a install
+    // of a package or bundle. These are called steps because they not
+    // only can provide log info but also possible fixed, feedback, stats
+    // etc etc. Each step in itself can have steps again providing for things
+    // like three style logging and feedback
+    private Vector installsteps;
+
+    private long lastupdated;
+
+    public BasicBundle() {
+    }
+
+    public BasicBundle(org.w3c.dom.Node n,ProviderInterface provider,String name,String type,String maintainer,String version, String date,String path) {
+	this.name=name;
+	this.version=version;
+	this.date=date;
+	this.maintainer=maintainer;
+	this.provider=provider;
+	this.type=type;
+	this.id=name+"@"+maintainer+"_"+type;
+	this.id=this.id.replace(' ','_');
+	this.id=this.id.replace('/','_');
+	this.path=path;
+
+	decodeNeededPackages(n);
+        if (n!=null) {
+                addMetaInfo(n);
+        }
+    }
+
+    public String getId() {
+	if (id==null) return "";
+	return id;
+    }
+
+    public String getName() {
+	if (name==null) return"";
+	return name;
+    }
+
+    public Enumeration getNeededPackages() {
+	return neededpackages.elements();
+    }
+    
+    public String getVersion() {
+	if (version==null) return("");
+	return version;
+    }
+
+    public String getCreationDate() {
+	if (date==null) return "";
+	return date;
+    }
+
+    public String getMaintainer() {
+	if (maintainer==null) return "";
+	return maintainer;
+    }
+
+    public String getState() {
+	if (InstallManager.isActive()) {
+		if (this==InstallManager.getInstallingBundle()) {
+			return "installing";
+		}	
+	}
+
+	if (BundleManager.isInstalledVersion(this)) {
+		return "installed";
+	}
+
+	if (state==null) return"";
+	return state;
+    }
+
+    public String getType() {
+	if (type==null) return "";
+	return type;
+    }
+
+    public ProviderInterface getProvider() {
+	return provider;
+    }
+
+    public boolean setState(String state) {
+	this.state=state;
+	return true;
+    }
+
+
+    public boolean install() {
+	// step1
+	installStep step=getNextInstallStep();
+	step.setUserFeedBack("bundle/basic installer started");
+
+	JarFile jf=getJarFile();
+
+	boolean changed=true; // signals something was installed
+	while (changed) {
+	Enumeration e=getNeededPackages();
+	changed=false;
+	while (e.hasMoreElements()) {
+			Hashtable np=(Hashtable)e.nextElement();
+			PackageInterface pkg=PackageManager.getPackage((String)np.get("id"));
+			if (pkg!=null) {
+				String state=pkg.getState();
+				String name=pkg.getName();
+				if (!state.equals("installed")) {
+					step=getNextInstallStep();
+					step.setUserFeedBack("calling package installer "+name+"..");
+					boolean ins=pkg.install(step);
+					if (ins) {
+						step.setUserFeedBack("calling package installer "+name+"...done");
+						changed=true;
+					} else {
+						if (pkg.getDependsFailed()) {
+							removeInstallStep(step);
+							// step.setUserFeedBack("calling package installer "+name+"...skipped (depends on other package maybe in bundle)");
+						} else {
+							step.setUserFeedBack("calling package installer "+name+"...failed");
+							return false;
+						}
+					}
+				}
+			} else {
+				log.error("Missing package on +"+np.get("id"));
+			}
+	}			
+        }
+
+        step=getNextInstallStep();
+        step.setUserFeedBack("updating mmbase registry ..");
+        updateRegistryInstalled();
+        step.setUserFeedBack("updating mmbase registry ... done");
+
+	step=getNextInstallStep();
+	step.setUserFeedBack("bundle/basic installer ended");
+	return true;
+    }
+
+    public boolean uninstall() {
+	try {
+		// step1
+		installStep step=getNextInstallStep();
+		step.setUserFeedBack("bundle/basic uninstaller started");
+
+		Enumeration e=getNeededPackages();
+		while (e.hasMoreElements()) {
+			Hashtable np=(Hashtable)e.nextElement();
+			PackageInterface pkg=PackageManager.getPackage((String)np.get("id"));
+			if (pkg!=null) {
+				String state=pkg.getState();
+				String name=pkg.getName();
+				step=getNextInstallStep();
+				step.setUserFeedBack("calling package uninstaller "+name+"..");
+				if (pkg.uninstall(step)) {
+					step.setUserFeedBack("calling package uninstaller "+name+"...done");
+				} else {
+					step.setUserFeedBack("calling package uninstaller "+name+"...failed");
+				}
+			} else {
+				log.error("Missing package on +"+np.get("id"));
+
+			}
+		}			
+
+		// step 3
+		step=getNextInstallStep();
+		step.setUserFeedBack("updating mmbase registry ..");
+		updateRegistryUninstalled();
+		step.setUserFeedBack("updating mmbase registry ... done");
+
+		// step 4
+		step=getNextInstallStep();
+		step.setUserFeedBack("bundle/basic installer ended");
+
+	} catch (Exception e) {
+		log.error("install crash on : "+this);
+		return false;
+	}
+	return true;
+    }
+
+    public installStep getNextInstallStep() {
+	// create new step
+	installStep step=new installStep();
+	if (installsteps==null) {
+		installsteps=new Vector();
+		installsteps.addElement(step);
+		return step;
+	} else {
+		installsteps.addElement(step);
+		return step;
+	}
+    }
+
+
+    public boolean removeInstallStep(installStep step) {
+	if (installsteps.contains(step)) {
+		installsteps.removeElement(step);
+		return true;
+	} else {
+		return false;
+	}
+    }
+
+    public Enumeration getInstallSteps() {
+        if (installsteps!=null) {
+                return installsteps.elements();
+        } else {
+                return null;
+        }
+    }
+
+
+	/*
+    public Enumeration getInstallSteps(int logid) {
+    	Enumeration e=getInstallSteps();
+	while (e.hasMoreElements()) {
+		installStep step=(installStep)e.nextElement();
+		if (step.getId()==logid) {
+			return step.getInstallSteps();
+		}
+	}
+	return null;
+    }
+	*/
+
+
+    public Enumeration getInstallSteps(int logid) {
+	// well maybe its one of my subs ?
+    	Enumeration e=getInstallSteps();
+	while (e.hasMoreElements()) {
+		installStep step=(installStep)e.nextElement();
+		Object o=step.getInstallSteps(logid);
+		if (o!=null) {
+			return (Enumeration)o;
+		}
+	}
+	return null;
+    }
+
+    public void clearInstallSteps() {
+	installsteps=null;
+    }
+
+    public JarFile getJarFile() {
+	if (provider!=null) {
+		return provider.getJarFile(getPath(),getId(),getVersion());
+	}
+	return null;	
+    }
+
+    public JarFile getIncludedPackageJarFile(String packageid,String packageversion) {
+	if (provider!=null) {
+		return provider.getIncludedPackageJarFile(getPath(),getId(),getVersion(),packageid,packageversion);
+	}
+	return null;	
+    }
+
+
+    public BufferedInputStream getJarStream() {
+	if (provider!=null) {
+		return provider.getJarStream(getPath());
+	}
+	return null;	
+    }
+
+    public String getPath() {
+	return path;
+    }
+
+
+    public boolean updateRegistryInstalled() {
+	return BundleManager.updateRegistryInstalled(this);
+    }
+
+    public boolean updateRegistryUninstalled() {
+	return BundleManager.updateRegistryUninstalled(this);
+    }
+
+    public void signalUpdate() {
+	lastupdated=System.currentTimeMillis();
+    }
+
+    public long lastSeen() {
+	return lastupdated;
+    }
+
+    private void decodeNeededPackages(org.w3c.dom.Node n) {
+	org.w3c.dom.Node n2=n.getFirstChild();
+        while (n2!=null) {
+		String name=n2.getNodeName();
+		// this should me one way defined (remote or local)
+		if (name.equals("neededpackages")) {
+			org.w3c.dom.Node n3=n2.getFirstChild();
+               	 	while (n3!=null) {
+				name=n3.getNodeName();
+   		               	NamedNodeMap nm=n3.getAttributes();
+                    		if (nm!=null) {
+					String maintainer=null;
+					String type=null;
+					String version=null;
+					String included=null;
+				
+					// decode name
+                        		org.w3c.dom.Node n5=nm.getNamedItem("name");
+                        		if (n5!=null) {
+						name=n5.getNodeValue();
+					}
+
+					// decode the type
+                        		n5=nm.getNamedItem("type");
+                        		if (n5!=null) {
+						type=n5.getNodeValue();
+					}
+
+					// decode the maintainer 
+                        		n5=nm.getNamedItem("maintainer");
+                        		if (n5!=null) {
+						maintainer=n5.getNodeValue();
+					}
+
+					// decode the version
+                        		n5=nm.getNamedItem("version");
+                        		if (n5!=null) {
+						version=n5.getNodeValue();
+					}
+
+
+					String wid=name+"@"+maintainer+"_"+type;
+					wid=wid.replace(' ','_');
+					wid=wid.replace('/','_');
+	
+					Hashtable h=new Hashtable();
+					h.put("name",name);
+					h.put("id",wid);
+					h.put("type",type);
+					h.put("maintainer",maintainer);
+					h.put("version",version);
+					neededpackages.put(wid,h);
+				}
+				n3=n3.getNextSibling();
+			}
+		}
+		// if in a remote file
+		if (name.equals("includedpackages")) {
+			org.w3c.dom.Node n3=n2.getFirstChild();
+               	 	while (n3!=null) {
+				name=n3.getNodeName();
+   		               	NamedNodeMap nm=n3.getAttributes();
+                    		if (nm!=null) {
+					String maintainer=null;
+					String type=null;
+					String version=null;
+					boolean packed=false;
+				
+					// decode name
+                        		org.w3c.dom.Node n5=nm.getNamedItem("name");
+                        		if (n5!=null) {
+						name=n5.getNodeValue();
+					}
+
+					// decode the type
+                        		n5=nm.getNamedItem("type");
+                        		if (n5!=null) {
+						type=n5.getNodeValue();
+					}
+
+					// decode the maintainer 
+                        		n5=nm.getNamedItem("maintainer");
+                        		if (n5!=null) {
+						maintainer=n5.getNodeValue();
+					}
+
+					// decode the version
+                        		n5=nm.getNamedItem("version");
+                        		if (n5!=null) {
+						version=n5.getNodeValue();
+					}
+
+					// decode the packed
+                        		n5=nm.getNamedItem("packed");
+                        		if (n5!=null) {
+						if (n5.getNodeValue().equals("true")) packed=true;
+					}
+
+					String wid=name+"@"+maintainer+"_"+type;
+					wid=wid.replace(' ','_');
+					wid=wid.replace('/','_');
+	
+					Hashtable h=new Hashtable();
+					h.put("name",name);
+					h.put("id",wid);
+					h.put("type",type);
+					h.put("maintainer",maintainer);
+					h.put("version",version);
+					neededpackages.put(wid,h);
+	
+					// if its included add it to available packages
+				}
+				n3=n3.getNextSibling();
+			}
+		}
+		n2=n2.getNextSibling();
+	}
+    }
+
+
+   private void addMetaInfo(org.w3c.dom.Node n) {
+       org.w3c.dom.Node n2=n.getFirstChild();
+       while (n2!=null) {
+         if (n2.getNodeName().equals("description")) {
+              org.w3c.dom.Node n3=n2.getFirstChild();
+	      description=n3.getNodeValue();
+         } else if (n2.getNodeName().equals("releasenotes")) {
+              org.w3c.dom.Node n3=n2.getFirstChild();
+	      releasenotes=n3.getNodeValue();
+         } else if (n2.getNodeName().equals("installationnotes")) {
+              org.w3c.dom.Node n3=n2.getFirstChild();
+	      installationnotes=n3.getNodeValue();
+         } else if (n2.getNodeName().equals("license")) {
+              org.w3c.dom.Node n3=n2.getFirstChild();
+	      if (n3!=null) {
+	      	licensebody=n3.getNodeValue();
+	      }
+              NamedNodeMap nm=n2.getAttributes();
+              if (nm!=null) {
+			// decode name
+                        org.w3c.dom.Node n4=nm.getNamedItem("name");
+                       	if (n4!=null) {
+				licensename=n4.getNodeValue();
+			}
+			// decode type
+                        n4=nm.getNamedItem("type");
+                       	if (n4!=null) {
+				licensetype=n4.getNodeValue();
+			}
+			// decode version
+                        n4=nm.getNamedItem("version");
+                       	if (n4!=null) {
+				licenseversion=n4.getNodeValue();
+			}
+	      }
+         } else if (n2.getNodeName().equals("initiators")) {
+		initiators=decodeRelatedPeople(n2,"initiator");
+         } else if (n2.getNodeName().equals("supporters")) {
+		supporters=decodeRelatedPeople(n2,"supporter");
+         } else if (n2.getNodeName().equals("contacts")) {
+		contacts=decodeRelatedPeople(n2,"contact");
+         } else if (n2.getNodeName().equals("developers")) {
+		developers=decodeRelatedPeople(n2,"developer");
+	 }
+         n2=n2.getNextSibling();
+       }
+   }
+
+   public List getRelatedPeople(String type) {
+	if (type.equals("initiators")) return initiators;
+	if (type.equals("supporters")) return supporters;
+	if (type.equals("developers")) return developers;
+	if (type.equals("contacts")) return contacts;
+	return null;
+   }
+
+
+   private ArrayList decodeRelatedPeople(org.w3c.dom.Node n,String type) {
+	ArrayList list=new ArrayList();
+        org.w3c.dom.Node n2=n.getFirstChild();
+        while (n2!=null) {
+         	if (n2.getNodeName().equals(type)) {
+			Person p=new Person();
+              		NamedNodeMap nm=n2.getAttributes();
+              		if (nm!=null) {
+                        	org.w3c.dom.Node n3=nm.getNamedItem("name");
+                       		if (n3!=null) p.setName(n3.getNodeValue());
+                        	n3=nm.getNamedItem("company");
+                       		if (n3!=null) p.setCompany(n3.getNodeValue());
+                        	n3=nm.getNamedItem("reason");
+                       		if (n3!=null) p.setReason(n3.getNodeValue());
+                        	n3=nm.getNamedItem("mailto");
+                       		if (n3!=null) p.setMailto(n3.getNodeValue());
+			}
+			list.add(p);
+		}
+         	n2=n2.getNextSibling();
+	}
+	return list;
+   }
+
+
+    public String getDescription() {
+	return description;
+    }
+    
+    public String getInstallationNotes() {
+	return installationnotes;
+    }
+    
+    public String getReleaseNotes() {
+	return releasenotes;
+    }
+
+    public String getLicenseType() {
+	return licensetype;
+    }
+
+    public String getLicenseName() {
+	return licensename;
+    }
+
+    public String getLicenseVersion() {
+	return licenseversion;
+    }
+
+    public String getLicenseBody() {
+	return licensebody;
+    }
+
+}
---
+++ speeltuin/applications/packaging/src/org/mmbase/applications/packaging/bundlehandlers/BundleVersionContainer.java
@@ -0,0 +1,110 @@
+/*
+ 
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+ 
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+ 
+ */
+
+package org.mmbase.applications.packaging.bundlehandlers;
+
+import org.mmbase.bridge.*;
+import org.mmbase.module.core.*;
+import org.mmbase.util.logging.*;
+import org.mmbase.util.*;
+import org.mmbase.module.builders.Versions;
+import org.mmbase.applications.packaging.*;
+import org.mmbase.applications.packaging.packagehandlers.*;
+import org.mmbase.applications.packaging.providerhandlers.*;
+import org.mmbase.applications.packaging.sharehandlers.*;
+
+import java.io.File;
+import java.util.*;
+
+import org.w3c.dom.*;
+
+/**
+ * The bundle version container, keeps all track of all the bundles with
+ * the same version (but multiple providers)
+ *
+ * @author Daniel Ockeloen (MMBased)
+ */
+public class BundleVersionContainer  {
+    private static Logger log = Logging.getLoggerInstance(BundleVersionContainer.class.getName());
+
+    private ShareInfo shareinfo;
+
+    private Hashtable bundles=new Hashtable();
+
+    public BundleVersionContainer(BundleInterface b) {
+	bundles.put(b.getProvider(),b);
+    }
+
+    public Object addBundle(BundleInterface b) {
+	Object o=bundles.put(b.getProvider(),b);
+	if (o!=null) {
+		return(o);
+	}
+	return(null);
+    }
+
+
+    public boolean removeBundle(BundleInterface b) {
+	bundles.remove(b.getProvider());
+	return true;
+    }
+
+    public int getBundleCount() {
+	return bundles.size();
+    }
+
+    public Object get(ProviderInterface provider) {
+	Object o=bundles.get(provider);
+	if (o!=null) {
+		return(o);
+	}
+	return(null);
+    } 
+
+   public Enumeration getBundles() {
+	return bundles.elements();
+   }
+
+
+    public boolean contains(ProviderInterface provider) {
+	if (bundles.get(provider)!=null) {
+		return(true);
+	} else {
+		return(false);
+	}
+    }
+   
+    public boolean isShared() {
+	if (shareinfo!=null) {
+		return true;
+	}
+	return false;
+
+    }
+
+    public ShareInfo getShareInfo() {
+	return shareinfo;
+    }
+
+    public BundleInterface getBundleByScore() {
+	BundleInterface winner=null;
+	Enumeration e=bundles.elements();
+	while (e.hasMoreElements()) {
+		BundleInterface b=(BundleInterface)e.nextElement();
+		if (winner==null) {
+			winner=b;
+		} else if (b.getProvider().getBaseScore()>winner.getProvider().getBaseScore()) {
+			winner=b;
+		}
+	}
+	return winner;
+    }
+
+}
