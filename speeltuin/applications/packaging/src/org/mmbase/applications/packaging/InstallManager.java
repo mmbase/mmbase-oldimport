e556512b67e124c5de3ac6929f851bfa5d7e7d12
daniel <daniel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2004-06-11 23:03:39 +0000

    needed Managers for packaging
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@10884 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ speeltuin/applications/packaging/src/org/mmbase/applications/packaging/InstallManager.java
@@ -0,0 +1,171 @@
+/*
+ 
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+ 
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+ 
+ */
+
+package org.mmbase.applications.packaging;
+
+import org.mmbase.bridge.*;
+import org.mmbase.module.core.*;
+import org.mmbase.util.logging.*;
+import org.mmbase.module.builders.Versions;
+import org.mmbase.applications.packaging.packagehandlers.*;
+import org.mmbase.applications.packaging.bundlehandlers.*;
+import org.mmbase.applications.packaging.providerhandlers.*;
+import org.mmbase.applications.packaging.installhandlers.*;
+import org.mmbase.applications.packaging.*;
+
+import java.io.File;
+import java.util.*;
+
+import org.w3c.dom.*;
+
+/**
+ * install manager, keeps track of what is being installed, provides background
+ * threads and logs errors & message to feedback to users.
+ *
+ * @author Daniel Ockeloen (MMBased)
+ */
+public class InstallManager {
+    private static Logger log = Logging.getLoggerInstance(InstallManager.class.getName());
+
+    // is the install manager active (for dependencies reasons)
+    private static boolean active=false;
+
+    // signal if we are installing a bundle or a package
+    private static boolean bundle=false;
+ 
+    // package we are installing
+    private static PackageInterface pkg;
+
+    private static BundleInterface bnd;
+
+    private static installThread runner;
+
+    private static String state;
+
+    private static boolean running=false;
+
+
+
+    public static synchronized void init() {
+		if (!PackageManager.isRunning()) PackageManager.init();
+		if (!BundleManager.isRunning()) BundleManager.init();
+		if (!ProviderManager.isRunning()) ProviderManager.init();
+		if (!ShareManager.isRunning()) ShareManager.init();
+		if (!ProjectManager.isRunning()) ProjectManager.init();
+		running=true;
+    }
+
+    /**
+    * install a package
+    */
+    public static synchronized boolean installPackage(PackageInterface p) {
+	if (!active) {
+		// turn the installManager to active
+		active=true;
+		
+		// signal we are a package only install
+		bundle=false;
+
+		// set the package
+		pkg=p;
+
+		state="installing";
+
+		p.clearInstallSteps();
+
+		runner = new installThread();
+		
+		return true;
+	} else {
+		// returning false _allways_ means we where busy
+		// error feedback is provided by the processsteps
+		return false;	
+	}
+    }
+
+
+    /**
+    * install a bundle
+    */
+    public static synchronized boolean installBundle(BundleInterface b) {
+	if (!active) {
+		// turn the installManager to active
+		active=true;
+		
+		// signal we are a bundle install
+		bundle=true;
+
+		// set the bundle
+		bnd=b;
+
+		state="installing";
+
+		b.clearInstallSteps();
+
+		runner = new installThread();
+		
+		return true;
+	} else {
+		// returning false _allways_ means we where busy
+		// error feedback is provided by the processsteps
+		return false;	
+	}
+    }
+
+
+    /**
+    * called by the install thread class, performs the real install in 
+    * the background and keeps providing feedback using the steps interfaces
+    */
+    public static void performInstall() {
+	try {
+		if (bnd!=null) {
+			bnd.install();
+			state="waiting";
+			active=false;
+		} else if (pkg!=null) {
+			pkg.install();
+			state="waiting";
+			active=false;
+		}
+	} catch(Exception e) {
+		log.error("performInstall problem");
+	}
+    }
+
+    public void setState(String state) {
+	this.state=state;
+    }
+    
+    public String getState() {
+	return state;
+    }
+
+    public static boolean isActive() {
+	return active;
+    }
+
+    public static boolean isRunning() {
+	return running;
+    }
+
+    public static PackageInterface getInstallingPackage() {
+	return pkg;
+    }
+
+    public static BundleInterface getInstallingBundle() {
+	return bnd;
+    }
+
+    public static Enumeration getInstallSteps() {
+	return pkg.getInstallSteps();
+    }
+  
+}
