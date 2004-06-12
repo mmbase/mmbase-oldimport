d692213ad7221f1cd928b83410256548101d492e
eduard <eduard@91430c5f-d668-0410-9e7d-dd663366dcfd>
2004-06-12 11:00:38 +0000

    ew: HACK: Allowing startup of MMBase by a cloud call (sorry for the late checkin, i thought i already commited this code)
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@10895 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/bridge/implementation/BasicCloudContext.java
+++ src/org/mmbase/bridge/implementation/BasicCloudContext.java
@@ -22,7 +22,7 @@ import javax.servlet.http.*;
  *
  * @author Rob Vermeulen
  * @author Pierre van Rooden
- * @version $Id: BasicCloudContext.java,v 1.33 2004-02-24 12:19:35 michiel Exp $
+ * @version $Id: BasicCloudContext.java,v 1.34 2004-06-12 11:00:38 eduard Exp $
  */
 public class BasicCloudContext implements CloudContext {
     private static final Logger log = Logging.getLoggerInstance(BasicCloudContext.class);
@@ -54,29 +54,48 @@ public class BasicCloudContext implements CloudContext {
      */
     protected BasicCloudContext() {
         Iterator i = org.mmbase.module.Module.getModules();
-        if (i != null) {
-            mmb = (MMBase)org.mmbase.module.Module.getModule("MMBASEROOT");
-
-
-            // create transaction manager and temporary node manager
-            tmpObjectManager = new TemporaryNodeManager(mmb);
-            transactionManager = new TransactionManager(mmb, tmpObjectManager);
-
-            // create module list
-            while(i.hasNext()) {
-                Module mod = ModuleHandler.getModule((org.mmbase.module.Module)i.next(),this);
-                localModules.put(mod.getName(),mod);
-            }
-
-            // set all the names of all accessable clouds..
-            localClouds.add("mmbase");
-            
-        } else {
-            // why dont we start mmbase, when there isnt a running instance, just change the check...
-            String message = "MMBase has not been started, and cannot be started by this Class. (" + getClass().getName() + ")";
-            log.error(message);
-            throw new BridgeException(message);
+        // check if MMBase is already running
+        if (i == null) {
+        	// build the error message, since it has very less overhead (only entered once incase of startup)
+        	String message = "MMBase has not been started, and cannot be started by this Class. (" + getClass().getName() + " : no propery mmbase.config found)"; 
+        	// MMBase may only be started from the bridge when the property mmbase.config was provided
+        	if (java.lang.System.getProperty("mmbase.config") == null) {
+        		// when mmbase.config is empty fill it with current working dir + /config
+        		// this way there is no need to provide the info on the commandline
+        		// java.lang.System.setProperty("mmbase.config", java.lang.System.getProperty("user.dir") + java.io.File.separatorChar + "config");
+        		log.error(message);
+        		throw new BridgeException(message); 
+        	} 
+        	// when MMBase is not running, try to start it!
+        	try {
+        		// init the MMBaseContext,... 
+        		org.mmbase.module.core.MMBaseContext.init();
+        		// try to start MMBase now,...
+        		org.mmbase.module.core.MMBase.getMMBase();
+        		// now re-assign the values agina
+        		i = org.mmbase.module.Module.getModules();
+        	}
+        	catch(java.lang.Exception ex) {
+        		log.error("Error while trying to start MMBase from the bridge:" + Logging.stackTrace(ex));
+        	}
+        	// if still null,.. give error!
+        	if(i == null) {
+        		log.error(message);
+        		throw new BridgeException(message);
+        	}
+        } 
+        // get the core module!
+        mmb = org.mmbase.module.core.MMBase.getMMBase();
+        // create transaction manager and temporary node manager
+        tmpObjectManager = new TemporaryNodeManager(mmb);
+        transactionManager = new TransactionManager(mmb,tmpObjectManager);
+        // create module list
+        while(i.hasNext()) {
+        	Module mod = ModuleHandler.getModule((org.mmbase.module.Module)i.next(),this);
+        	localModules.put(mod.getName(),mod);
         }
+        // set all the names of all accessable clouds..
+        localClouds.add("mmbase");        
     }
 
     public ModuleList getModules() {
