508bfdd6a82b36184213e46f021ef40757185147
daniel <daniel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2004-06-13 10:58:12 +0000

    code for the functions
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@10902 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ speeltuin/applications/packaging/src/org/mmbase/applications/packaging/providerhandlers/gui/Controller.java
@@ -0,0 +1,182 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.applications.packaging.providerhandlers.gui;
+
+import java.io.*;
+import java.net.URL;
+import java.text.DateFormat;
+import java.util.*;
+import java.util.jar.*;
+
+import org.mmbase.bridge.*;
+import org.mmbase.bridge.implementation.*;
+import org.mmbase.applications.packaging.*;
+import org.mmbase.applications.packaging.packagehandlers.*;
+import org.mmbase.applications.packaging.bundlehandlers.*;
+import org.mmbase.applications.packaging.installhandlers.*;
+import org.mmbase.applications.packaging.providerhandlers.*;
+import org.mmbase.applications.packaging.sharehandlers.*;
+import org.mmbase.applications.packaging.projects.creators.*;
+import org.mmbase.applications.packaging.projects.*;
+import org.mmbase.util.logging.*;
+import org.mmbase.module.core.*;
+
+
+/**
+ * @author Daniel Ockeloen
+ * @version $Id: guiController.java
+ */
+public class Controller {
+
+	private static Logger log = Logging.getLoggerInstance(Controller.class.getName());
+	private static Cloud cloud;
+       	NodeManager manager;
+	CloudContext context;
+
+
+	public Controller() {
+		cloud=LocalContext.getCloudContext().getCloud("mmbase");
+
+		// hack needs to be solved
+        	manager=cloud.getNodeManager("typedef");
+		if (manager==null) log.error("Can't access builder typedef");
+		context=LocalContext.getCloudContext();
+		if (!InstallManager.isRunning()) InstallManager.init();
+	}
+
+	public List getProviderHandlers() {
+		// get the current provider handlers we have installed
+		Hashtable providerhandlers=ProviderManager.getProviderHandlers();
+
+		// create a result list
+                List list = new ArrayList();
+                VirtualBuilder builder = new VirtualBuilder(MMBase.getMMBase());
+	
+		Enumeration e=providerhandlers.keys();
+		while (e.hasMoreElements()) {
+			String key=(String)e.nextElement();
+			String value=(String)providerhandlers.get(key);
+
+                        MMObjectNode virtual = builder.getNewNode("admin");
+			virtual.setValue("name",key);
+			virtual.setValue("value",value);
+			list.add(virtual);
+		}
+		return list;
+	}
+
+
+	public boolean changeProviderSettings(String name,String maintainer,String account,String password,String path) {
+
+		ProviderInterface provider=ProviderManager.get(name);
+		if (provider!=null) {
+			String method=provider.getMethod();
+			provider.setMaintainer(maintainer);
+			provider.setPath(path);
+			if (method.equals("http")) {
+				((HttpProvider)provider).setAccount(account);
+				((HttpProvider)provider).setPassword(password);
+			}
+		}
+		ProviderManager.writeProviderFile();
+		return true;
+	}
+
+
+	public boolean delProvider(String name) {
+		if (ProviderManager.delete(name)) {
+			// handle ok
+		} else {
+			// handle a problem
+			return false;
+		}
+		ProviderManager.writeProviderFile();
+		return true;
+	}
+
+
+	public List addSubscribeProvider(String url) {
+	        List list = new ArrayList();
+         	VirtualBuilder builder = new VirtualBuilder(MMBase.getMMBase());
+         	MMObjectNode virtual = builder.getNewNode("admin");
+
+		String result=ProviderManager.addSubscribeProvider(url);
+		if (result!=null) {
+			virtual.setValue("feedback",result);
+		}
+		ProviderManager.writeProviderFile();
+	
+		list.add(virtual);
+		return list;
+	}
+
+
+	public List addDiskProvider(String name,String path) {
+	        List list = new ArrayList();
+         	VirtualBuilder builder = new VirtualBuilder(MMBase.getMMBase());
+         	MMObjectNode virtual = builder.getNewNode("admin");
+
+		String result=ProviderManager.addDiskProvider(name,path);
+		if (result!=null) {
+			virtual.setValue("feedback",result);
+		}
+		ProviderManager.writeProviderFile();
+		list.add(virtual);
+		return list;
+	}
+
+
+	public List getProviders() {
+		// get the current providers
+		Enumeration providers=ProviderManager.getProviders();
+
+                List list = new ArrayList();
+                VirtualBuilder builder = new VirtualBuilder(MMBase.getMMBase());
+
+		while (providers.hasMoreElements()) {
+			ProviderInterface  provider=(ProviderInterface)providers.nextElement();
+                        MMObjectNode virtual = builder.getNewNode("admin");
+			virtual.setValue("method",provider.getMethod());
+			virtual.setValue("name",provider.getName());
+			virtual.setValue("maintainer",provider.getMaintainer());
+			virtual.setValue("state",provider.getState());
+			list.add(virtual);
+		}
+		return list;
+	}
+
+
+	public List getProviderInfo(String name) {
+		// get the current providers
+		ProviderInterface provider=ProviderManager.get(name);
+
+                List list = new ArrayList();
+                VirtualBuilder builder = new VirtualBuilder(MMBase.getMMBase());
+
+                MMObjectNode virtual = builder.getNewNode("admin");
+
+		if (provider!=null) {
+			virtual.setValue("method",provider.getMethod());
+			virtual.setValue("name",provider.getName());
+			virtual.setValue("maintainer",provider.getMaintainer());
+			virtual.setValue("state",provider.getState());
+			virtual.setValue("path",provider.getPath());
+			if (provider.getMethod().equals("http")) {
+				virtual.setValue("account",((HttpProvider)provider).getAccount());
+				virtual.setValue("password",((HttpProvider)provider).getPassword());
+			}
+		}
+		list.add(virtual);
+		return list;
+	}
+
+
+
+}
