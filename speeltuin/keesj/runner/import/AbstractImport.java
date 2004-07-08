fc414cbb2928e0c1b352c366d64cc7150bee9d5b
keesj <keesj@91430c5f-d668-0410-9e7d-dd663366dcfd>
2004-07-08 11:49:21 +0000

    keesj:definition of old and new cloud in one place
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@11101 91430c5f-d668-0410-9e7d-dd663366dcfd

--- speeltuin/keesj/runner/import/AbstractImport.java
+++ speeltuin/keesj/runner/import/AbstractImport.java
@@ -3,23 +3,22 @@ import org.mmbase.bridge.*;
 import java.util.*;
 /**
  * @author keesj
- * @version $Id: AbstractImport.java,v 1.1.1.1 2004-07-08 10:32:15 keesj Exp $
+ * @version $Id: AbstractImport.java,v 1.2 2004-07-08 11:49:21 keesj Exp $
  */
 public abstract class AbstractImport{
     public Cloud newCloud ;
     public Cloud oldCloud ;
     public SyncNodes syncNode;
     public static String IMPORT_SOURCE_NAME="oldmmbase";
-    public static String oldCloudRMI = "rmi://127.0.0.1:1120/old";
-    public static String newCloudRMI = "rmi://127.0.0.1:1120/new";
+    public static String oldCloudRMI = "rmi://127.0.0.1:1111/old";
+    public static String newCloudRMI = "rmi://127.0.0.1:1111/new";
 
     public AbstractImport(){
             HashMap user = new HashMap();
                 user.put("username","admin");
-                user.put("password","temp$$42");
-                //user.put("password","nietgebruikenaub");
-                oldCloud = ContextProvider.getCloudContext("rmi://127.0.0.1:1120/old").getCloud("mmbase");
-                newCloud = ContextProvider.getCloudContext("rmi://127.0.0.1:1120/new").getCloud("mmbase","name/password",user);
+                user.put("password","passwd");
+                oldCloud = ContextProvider.getCloudContext(oldCloudRMI).getCloud("mmbase");
+                newCloud = ContextProvider.getCloudContext(newCloudRMI).getCloud("mmbase","name/password",user);
 
 	syncNode = new SyncNodes(newCloud);
     }
