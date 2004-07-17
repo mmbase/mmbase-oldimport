97dd4e5e1e527787500c93115c2036aeecedff4a
daniel <daniel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2004-07-17 22:51:24 +0000

    some cleanup
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@11196 91430c5f-d668-0410-9e7d-dd663366dcfd

--- speeltuin/applications/packaging/src/org/mmbase/applications/packaging/PackageManager.java
+++ speeltuin/applications/packaging/src/org/mmbase/applications/packaging/PackageManager.java
@@ -37,7 +37,7 @@ public class PackageManager {
     private static Hashtable packages = new Hashtable();
 
     // state of this manager
-    private static boolean state=false;
+    private static boolean state = false;
     private static Hashtable packagehandlers;
 
     public static final String DTD_PACKAGEHANDLERS_1_0 = "packagehandlers_1_0.dtd";
@@ -50,18 +50,18 @@ public class PackageManager {
     public static void registerPublicIDs() {
         XMLEntityResolver.registerPublicID(PUBLIC_ID_PACKAGEHANDLERS_1_0, DTD_PACKAGEHANDLERS_1_0, PackageManager.class);
     }
-	/**
-	* init(), starts the package manager mostly start the
-	* package discovery system.
-	*/
-	public static synchronized void init() {
-		readPackageHandlers();
-		state=true;
-	}
+    /**
+    * init(), starts the package manager mostly start the
+    * package discovery system.
+    */
+    public static synchronized void init() {
+        readPackageHandlers();
+        state = true;
+    }
 
-	public static boolean isRunning() {
-		return(state);
-	}
+    public static boolean isRunning() {
+        return state;
+    }
 
     /**
      * return all packages based on the input query
@@ -76,12 +76,11 @@ public class PackageManager {
      * @return all packages
      */
     public static Enumeration getPackageVersions(String id) {
-	Object o=packages.get(id);
-	if (o!=null) {
-		PackageContainer pc=(PackageContainer)o;
-		return pc.getVersions();
-		
-	}
+        Object o = packages.get(id);
+        if (o != null) {
+            PackageContainer pc = (PackageContainer)o;
+            return pc.getVersions();
+        }
         return null;
     }
 
@@ -90,12 +89,11 @@ public class PackageManager {
      * return a list of version numbers of this package
      */
     public static Enumeration getPackageVersionNumbers(String id) {
-	Object o=packages.get(id);
-	if (o!=null) {
-		PackageContainer pc=(PackageContainer)o;
-		return pc.getVersionNumbers();
-		
-	}
+        Object o = packages.get(id);
+        if (o != null) {
+            PackageContainer pc = (PackageContainer)o;
+            return pc.getVersionNumbers();
+        }
         return null;
     }
 
@@ -104,12 +102,12 @@ public class PackageManager {
      * @return all packages
      */
     public static PackageInterface getPackage(String id) {
-	Object o=packages.get(id);
-	if (o!=null) {
-		return (PackageInterface)o;
-	}
-	log.error("package with id = "+id+" not found");
-	return(null);
+        Object o = packages.get(id);
+        if (o != null) {
+            return (PackageInterface)o;
+        }
+        log.error("package with id = "+id+" not found");
+        return null;
     }
 
 
@@ -118,19 +116,19 @@ public class PackageManager {
      * @return all packages
      */
     public static PackageInterface getPackage(String id,String wv,String wp) {
-	Object o=packages.get(id);
-	if (o!=null) {
-		PackageContainer pc=(PackageContainer)o;
-		ProviderInterface provider=(ProviderInterface)ProviderManager.get(wp);
-		if (provider!=null) {
-			PackageInterface p=pc.getVersion(wv,provider);
-			if (p!=null) {
-				return(p);
-			}
-		}
-	}
-	log.error("package with id = "+id+" not found");
-	return(null);
+        Object o = packages.get(id);
+        if (o != null) {
+            PackageContainer pc = (PackageContainer)o;
+            ProviderInterface provider = (ProviderInterface)ProviderManager.get(wp);
+            if (provider != null) {
+                PackageInterface p = pc.getVersion(wv,provider);
+                if (p != null) {
+                    return p;
+                }
+            }
+        }
+        log.error("package with id = "+id+" not found");
+        return null;
     }
 
 
@@ -139,16 +137,16 @@ public class PackageManager {
      * @return all packages
      */
     public static PackageInterface getPackage(String id,String wv) {
-	Object o=packages.get(id);
-	if (o!=null) {
-		PackageContainer pc=(PackageContainer)o;
-		PackageInterface p=pc.getPackageByScore(wv);
-		if (p!=null) {
-			return(p);
-		}
-	}
-	log.error("package with id = "+id+" not found");
-	return(null);
+        Object o = packages.get(id);
+        if (o != null) {
+            PackageContainer pc = (PackageContainer)o;
+            PackageInterface p = pc.getPackageByScore(wv);
+            if (p != null) {
+                return p;
+            }
+        }
+        log.error("package with id = "+id+" not found");
+        return null;
     }
 
     /**
@@ -158,189 +156,186 @@ public class PackageManager {
      */
     public static PackageInterface foundPackage(ProviderInterface provider,org.w3c.dom.Element n,String name,String type,String maintainer,String version,String date,String path) {
 
-	// create its id (name+maintainer)
-	String id=name+"@"+maintainer+"_"+type;
-	id=id.replace(' ','_');
-	id=id.replace('/','_');
-	
-	// check if we allready have a package container for this
-	PackageContainer pc=(PackageContainer)packages.get(id);
+        // create its id (name+maintainer)
+        String id = name+"@"+maintainer+"_"+type;
+        id = id.replace(' ','_');
+        id = id.replace('/','_');
+    
+        // check if we allready have a package container for this
+        PackageContainer pc = (PackageContainer)packages.get(id);
 
-	boolean found=false;
-	if (pc!=null) {
-		// we allready have a container check if we allready
-		// have this one
-		found=pc.contains(version,provider);
-	}
+        boolean found = false;
+        if (pc != null) {
+            // we allready have a container check if we allready
+            // have this one
+            found = pc.contains(version,provider);
+        }
 
-	if (!found) {	
-		// so we don't have this package refernce yet, then
-		// create and store it
+        if (!found) {    
+            // so we don't have this package refernce yet, then
+            // create and store it
 
-		// try to create this handler
-		String classname=(String)packagehandlers.get(type);
-		if (classname!=null) {
-		try {
-                	Class newclass=Class.forName(classname);
-
-             	  	PackageInterface newpackage = (PackageInterface)newclass.newInstance();
-			newpackage.init(n,provider,name,type,maintainer,version,date,path);
-			if (pc==null) {
-				pc=new PackageContainer(newpackage);
-				// since this is a new container store it
-				packages.put(id,pc);
-			} else {
-				pc.addPackage(newpackage);
-			}
-			((BasicPackage)newpackage).signalUpdate();
-			return newpackage;
-		} catch(Exception e) {
-			log.error("Can't create packagehandler : "+classname);
-			e.printStackTrace();
-		}
-		} else {
-			log.error("package type : "+type+" not supported (no handler)");
-		}
-	} else {
-		// get the package to update its available time
-		BasicPackage oldp=(BasicPackage)pc.getVersion(version,provider);
-		if (oldp!=null) oldp.signalUpdate();
-	}
-	return null;
+            // try to create this handler
+            String classname = (String)packagehandlers.get(type);
+            if (classname != null) {
+                try {
+                    Class newclass = Class.forName(classname);
+                    PackageInterface newpackage = (PackageInterface)newclass.newInstance();
+                    newpackage.init(n,provider,name,type,maintainer,version,date,path);
+                    if (pc == null) {
+                        pc = new PackageContainer(newpackage);
+                        // since this is a new container store it
+                        packages.put(id,pc);
+                    } else {
+                        pc.addPackage(newpackage);
+                    }
+                    ((BasicPackage)newpackage).signalUpdate();
+                    return newpackage;
+                } catch(Exception e) {
+                    log.error("Can't create packagehandler : "+classname);
+                    e.printStackTrace();
+                }
+            } else {
+                log.error("package type : "+type+" not supported (no handler)");
+            }
+        } else {
+            // get the package to update its available time
+            BasicPackage oldp = (BasicPackage)pc.getVersion(version,provider);
+            if (oldp != null) oldp.signalUpdate();
+        }
+        return null;
     }
 
 
-    public static int getInstalledVersion(String id) 
-        throws SearchQueryException {
+    public static int getInstalledVersion(String id) throws SearchQueryException {
         // Get the versions builder
         Versions versions = (Versions) MMBase.getMMBase().getMMObject("versions");
-        if(versions==null) {
+        if(versions == null) {
             log.error("Versions builder not installed.");
-	    return -1;
+            return -1;
         } else {
-	    return versions.getInstalledVersion(id,"package");	
-	}
+            return versions.getInstalledVersion(id,"package");    
+        }
     }
 
     public static boolean isInstalledVersion(PackageInterface p) {
-	try {
-		int newversion=Integer.parseInt(p.getVersion());
-		if (getInstalledVersion(p.getId())==newversion) {
-			return true;
-		} 
-	} catch(Exception e) {
-		return false;
-	}
-	return false;
+        try {
+            int newversion = Integer.parseInt(p.getVersion());
+            if (getInstalledVersion(p.getId()) == newversion) {
+                return true;
+            } 
+        } catch(Exception e) {
+            return false;
+        }
+        return false;
     }
 
 
 
     public static boolean updateRegistryInstalled(PackageInterface p) { 
-	try {
-        	Versions versions = (Versions) MMBase.getMMBase().getMMObject("versions");
-        	if(versions==null) {
-            		log.error("Versions builder not installed.");
-	    		return false;
-        	} 
-		int newversion=Integer.parseInt(p.getVersion());
-		int oldversion=getInstalledVersion(p.getId());
-		if (oldversion==-1) {
-			versions.setInstalledVersion(p.getId(),"package",p.getMaintainer(),newversion);
-		} else if (oldversion!=newversion) {
-			versions.updateInstalledVersion(p.getId(),"package",p.getMaintainer(),newversion);
-		}
-		return(true);
-	} catch(Exception e) {
-		return false;
-	}
+        try {
+            Versions versions = (Versions) MMBase.getMMBase().getMMObject("versions");
+            if (versions == null) {
+                log.error("Versions builder not installed.");
+                return false;
+            } 
+            int newversion = Integer.parseInt(p.getVersion());
+            int oldversion = getInstalledVersion(p.getId());
+            if (oldversion == -1) {
+                versions.setInstalledVersion(p.getId(),"package",p.getMaintainer(),newversion);
+            } else if (oldversion != newversion) {
+                versions.updateInstalledVersion(p.getId(),"package",p.getMaintainer(),newversion);
+            }
+            return true;
+        } catch(Exception e) {
+            return false;
+        }
     }
 
 
     public static boolean updateRegistryUninstalled(PackageInterface p) { 
-	try {
-        	Versions versions = (Versions) MMBase.getMMBase().getMMObject("versions");
-        	if(versions==null) {
-            		log.error("Versions builder not installed.");
-	    		return false;
-        	} 
-		versions.updateInstalledVersion(p.getId(),"package",p.getMaintainer(),0);
-		return(true);
-	} catch(Exception e) {
-		return false;
-	}
+        try {
+            Versions versions = (Versions) MMBase.getMMBase().getMMObject("versions");
+            if(versions == null) {
+                log.error("Versions builder not installed.");
+                return false;
+            } 
+            versions.updateInstalledVersion(p.getId(),"package",p.getMaintainer(),0);
+            return true;
+        } catch(Exception e) {
+            return false;
+        }
     }
    
     public static boolean removeOfflinePackages(ProviderInterface wantedprov) {
-	// this checks all the packages if they are still found at their
-	// providers, this is done by checking the last provider update
-	// against the last package update
-	Enumeration e=packages.elements();
-	while (e.hasMoreElements()) {
-		PackageContainer pc=(PackageContainer)e.nextElement();
-		Enumeration e2=pc.getVersions();
-		while (e2.hasMoreElements()) {
-			PackageVersionContainer pvc=(PackageVersionContainer)e2.nextElement();
-			Enumeration e3=pvc.getPackages();
-			while (e3.hasMoreElements()) {
-				BasicPackage p=(BasicPackage)e3.nextElement();
-				ProviderInterface prov=p.getProvider();
-				if (wantedprov==prov) {
-					long providertime=p.getProvider().lastSeen();
-					long packagetime=p.lastSeen();
-					if (providertime>packagetime) {
-						pvc.removePackage(p);
-						if (pvc.getPackageCount()==0) {
-							pc.removePackage(p);
-							if (pc.getPackageCount()==0) {
-								packages.remove(pc.getId());
-							}
-						}
-					}
-				}
-			}
-		}
-	
-	}
-	return true;
+        // this checks all the packages if they are still found at their
+        // providers, this is done by checking the last provider update
+        // against the last package update
+        Enumeration e = packages.elements();
+        while (e.hasMoreElements()) {
+            PackageContainer pc = (PackageContainer)e.nextElement();
+            Enumeration e2 = pc.getVersions();
+            while (e2.hasMoreElements()) {
+                PackageVersionContainer pvc = (PackageVersionContainer)e2.nextElement();
+                Enumeration e3 = pvc.getPackages();
+                while (e3.hasMoreElements()) {
+                    BasicPackage p = (BasicPackage)e3.nextElement();
+                    ProviderInterface prov = p.getProvider();
+                    if (wantedprov == prov) {
+                        long providertime = p.getProvider().lastSeen();
+                        long packagetime = p.lastSeen();
+                        if (providertime > packagetime) {
+                            pvc.removePackage(p);
+                            if (pvc.getPackageCount() == 0) {
+                                pc.removePackage(p);
+                                if (pc.getPackageCount() == 0) {
+                                    packages.remove(pc.getId());
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return true;
     }
 
     public static void readPackageHandlers() {
-       packagehandlers=new Hashtable();
-       String filename = MMBaseContext.getConfigPath()+File.separator+"packaging"+File.separator+"packagehandlers.xml";
+        packagehandlers = new Hashtable();
+        String filename = MMBaseContext.getConfigPath()+File.separator+"packaging"+File.separator+"packagehandlers.xml";
 
         File file = new File(filename);
         if(file.exists()) {
-            	XMLBasicReader reader = new XMLBasicReader(filename,PackageManager.class);
-            	if(reader!=null) {
-            		for(Enumeration ns=reader.getChildElements("packagehandlers","packagehandler");ns.hasMoreElements(); ) {
-                		Element n=(Element)ns.nextElement();
-   		                NamedNodeMap nm=n.getAttributes();
-                    		if (nm!=null) {
-					String type=null;
-					String classname=null;
+            XMLBasicReader reader = new XMLBasicReader(filename,PackageManager.class);
+            if(reader != null) {
+                for(Enumeration ns = reader.getChildElements("packagehandlers","packagehandler");ns.hasMoreElements(); ) {
+                    Element n = (Element)ns.nextElement();
+                    NamedNodeMap nm = n.getAttributes();
+                    if (nm != null) {
+                        String type = null;
+                        String classname = null;
 
-					// decode type
-                        		org.w3c.dom.Node n2=nm.getNamedItem("type");
-                        		if (n2!=null) {
-						type=n2.getNodeValue();
-					}
+                        // decode type
+                        org.w3c.dom.Node n2 = nm.getNamedItem("type");
+                        if (n2 != null) {
+                            type = n2.getNodeValue();
+                        }
 
-					// decode the class
-                        		n2=nm.getNamedItem("class");
-                        		if (n2!=null) {
-						classname=n2.getNodeValue();
-					}
-					packagehandlers.put(type,classname);
-				}
-			}
-		}
-	} else {
+                        // decode the class
+                        n2 = nm.getNamedItem("class");
+                        if (n2 != null) {
+                            classname = n2.getNodeValue();
+                        }
+                        packagehandlers.put(type,classname);
+                    }
+                }
+            }
+        } else {
             log.error("missing packagehandler file : "+filename);
-	}
+        }
     }
 
-    public static Hashtable getPackageHandlers() {
-	return packagehandlers;
+    public static Hashtable getPackageHandlers() { 
+        return packagehandlers;
     }
 }
