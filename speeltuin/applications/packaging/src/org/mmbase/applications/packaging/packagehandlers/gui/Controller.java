ed859edea3fd8e58da14430bf69224437a94c91a
daniel <daniel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2004-07-20 15:40:03 +0000

    cleanup
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@11226 91430c5f-d668-0410-9e7d-dd663366dcfd

--- speeltuin/applications/packaging/src/org/mmbase/applications/packaging/packagehandlers/gui/Controller.java
+++ speeltuin/applications/packaging/src/org/mmbase/applications/packaging/packagehandlers/gui/Controller.java
@@ -1,12 +1,9 @@
 /*
-
-This software is OSI Certified Open Source Software.
-OSI Certified is a certification mark of the Open Source Initiative.
-
-The license (Mozilla version 1.0) can be read at the MMBase site.
-See http://www.MMBase.org/license
-
-*/
+ *  This software is OSI Certified Open Source Software.
+ *  OSI Certified is a certification mark of the Open Source Initiative.
+ *  The license (Mozilla version 1.0) can be read at the MMBase site.
+ *  See http://www.MMBase.org/license
+ */
 package org.mmbase.applications.packaging.packagehandlers.gui;
 
 import java.io.*;
@@ -28,121 +25,152 @@ import org.mmbase.applications.packaging.projects.*;
 import org.mmbase.util.logging.*;
 import org.mmbase.module.core.*;
 
-
 /**
- * @author Daniel Ockeloen
- * @version $Id: guiController.java
+ * @author     Daniel Ockeloen
+ * @created    July 20, 2004
+ * @version    $Id: guiController.java
  */
 public class Controller {
 
-	private static Logger log = Logging.getLoggerInstance(Controller.class);
-	private static Cloud cloud;
-       	NodeManager manager;
-	CloudContext context;
+    private static Logger log = Logging.getLoggerInstance(Controller.class);
+    private static Cloud cloud;
+    NodeManager manager;
+    CloudContext context;
 
 
-	public Controller() {
-		cloud=LocalContext.getCloudContext().getCloud("mmbase");
+    /**
+     *Constructor for the Controller object
+     */
+    public Controller() {
+        cloud = LocalContext.getCloudContext().getCloud("mmbase");
 
-		// hack needs to be solved
-        	manager=cloud.getNodeManager("typedef");
-		if (manager==null) log.error("Can't access builder typedef");
-		context=LocalContext.getCloudContext();
-		if (!InstallManager.isRunning()) InstallManager.init();
-	}
-
-
-
-	public List getPackageHandlers() {
-		// get the current package handlers we have installed
-		Hashtable packagehandlers=PackageManager.getPackageHandlers();
-                List list = new ArrayList();
-                VirtualBuilder builder = new VirtualBuilder(MMBase.getMMBase());
-
-		Enumeration e=packagehandlers.keys();
-		while (e.hasMoreElements()) {
-			String key=(String)e.nextElement();
-			String value=(String)packagehandlers.get(key);
-
-                        MMObjectNode virtual = builder.getNewNode("admin");
-			virtual.setValue("name",key);
-			virtual.setValue("value",value);
-			list.add(virtual);
-		}
-		return list;
-	}
-
-	public List getPackages() {
-		// get the current best packages
-		Enumeration packages=PackageManager.getPackages();
-
-                List list = new ArrayList();
-                VirtualBuilder builder = new VirtualBuilder(MMBase.getMMBase());
-
-		while (packages.hasMoreElements()) {
-			PackageInterface  p=(PackageInterface)packages.nextElement();
-                        MMObjectNode virtual = builder.getNewNode("admin");
-			virtual.setValue("id",p.getId());
-			virtual.setValue("name",p.getName());
-			virtual.setValue("type",p.getType());
-			virtual.setValue("maintainer",p.getMaintainer());
-			virtual.setValue("version",p.getVersion());
-			virtual.setValue("creation-date",p.getCreationDate());
-			virtual.setValue("state",p.getState());
-			list.add(virtual);
-		}
-
-		return list;
-	}
+        // hack needs to be solved
+        manager = cloud.getNodeManager("typedef");
+        if (manager == null) {
+            log.error("Can't access builder typedef");
+        }
+        context = LocalContext.getCloudContext();
+        if (!InstallManager.isRunning()) {
+            InstallManager.init();
+        }
+    }
 
 
 
-	public List getPackageVersions(String id) {
-		// get the packages of one id (all versions)
-		Enumeration packageversions=PackageManager.getPackageVersions(id);
+    /**
+     *  Gets the packageHandlers attribute of the Controller object
+     *
+     * @return    The packageHandlers value
+     */
+    public List getPackageHandlers() {
+        // get the current package handlers we have installed
+        Hashtable packagehandlers = PackageManager.getPackageHandlers();
+        List list = new ArrayList();
+        VirtualBuilder builder = new VirtualBuilder(MMBase.getMMBase());
 
-                List list = new ArrayList();
-                VirtualBuilder builder = new VirtualBuilder(MMBase.getMMBase());
+        Enumeration e = packagehandlers.keys();
+        while (e.hasMoreElements()) {
+            String key = (String) e.nextElement();
+            String value = (String) packagehandlers.get(key);
 
-		while (packageversions.hasMoreElements()) {
-			PackageVersionContainer  pvc=(PackageVersionContainer)packageversions.nextElement();
-
-			Enumeration packages=pvc.getPackages();
-			while (packages.hasMoreElements()) {
-				PackageInterface  p=(PackageInterface)packages.nextElement();
-                        	MMObjectNode virtual = builder.getNewNode("admin");
-				virtual.setValue("id",p.getId());
-				virtual.setValue("name",p.getName());
-				virtual.setValue("type",p.getType());
-				virtual.setValue("maintainer",p.getMaintainer());
-				virtual.setValue("version",p.getVersion());
-				virtual.setValue("state",p.getState());
-				virtual.setValue("creation-date",p.getCreationDate());
-				ProviderInterface provider=p.getProvider();
-				if (provider!=null) {
-					virtual.setValue("provider",provider.getName());
-				}
-				list.add(virtual);
-			}
-		}
-
-		return list;
-	}
+            MMObjectNode virtual = builder.getNewNode("admin");
+            virtual.setValue("name", key);
+            virtual.setValue("value", value);
+            list.add(virtual);
+        }
+        return list;
+    }
 
 
-	public List getPackageVersionNumbers(String id) {
-		// get the packages of one id (all versions)
-		Enumeration verlist=PackageManager.getPackageVersionNumbers(id);
+    /**
+     *  Gets the packages attribute of the Controller object
+     *
+     * @return    The packages value
+     */
+    public List getPackages() {
+        // get the current best packages
+        Enumeration packages = PackageManager.getPackages();
 
-                List list = new ArrayList();
-                VirtualBuilder builder = new VirtualBuilder(MMBase.getMMBase());
-		while (verlist.hasMoreElements()) {
-			String version=(String)verlist.nextElement();
-                       	MMObjectNode virtual = builder.getNewNode("admin");
-			virtual.setValue("version",version);
-			list.add(virtual);
-		}
-		return list;
-	}
+        List list = new ArrayList();
+        VirtualBuilder builder = new VirtualBuilder(MMBase.getMMBase());
+
+        while (packages.hasMoreElements()) {
+            PackageInterface p = (PackageInterface) packages.nextElement();
+            MMObjectNode virtual = builder.getNewNode("admin");
+            virtual.setValue("id", p.getId());
+            virtual.setValue("name", p.getName());
+            virtual.setValue("type", p.getType());
+            virtual.setValue("maintainer", p.getMaintainer());
+            virtual.setValue("version", p.getVersion());
+            virtual.setValue("creation-date", p.getCreationDate());
+            virtual.setValue("state", p.getState());
+            list.add(virtual);
+        }
+
+        return list;
+    }
+
+
+
+    /**
+     *  Gets the packageVersions attribute of the Controller object
+     *
+     * @param  id  Description of the Parameter
+     * @return     The packageVersions value
+     */
+    public List getPackageVersions(String id) {
+        // get the packages of one id (all versions)
+        Enumeration packageversions = PackageManager.getPackageVersions(id);
+
+        List list = new ArrayList();
+        VirtualBuilder builder = new VirtualBuilder(MMBase.getMMBase());
+
+        while (packageversions.hasMoreElements()) {
+            PackageVersionContainer pvc = (PackageVersionContainer) packageversions.nextElement();
+
+            Enumeration packages = pvc.getPackages();
+            while (packages.hasMoreElements()) {
+                PackageInterface p = (PackageInterface) packages.nextElement();
+                MMObjectNode virtual = builder.getNewNode("admin");
+                virtual.setValue("id", p.getId());
+                virtual.setValue("name", p.getName());
+                virtual.setValue("type", p.getType());
+                virtual.setValue("maintainer", p.getMaintainer());
+                virtual.setValue("version", p.getVersion());
+                virtual.setValue("state", p.getState());
+                virtual.setValue("creation-date", p.getCreationDate());
+                ProviderInterface provider = p.getProvider();
+                if (provider != null) {
+                    virtual.setValue("provider", provider.getName());
+                }
+                list.add(virtual);
+            }
+        }
+
+        return list;
+    }
+
+
+    /**
+     *  Gets the packageVersionNumbers attribute of the Controller object
+     *
+     * @param  id  Description of the Parameter
+     * @return     The packageVersionNumbers value
+     */
+    public List getPackageVersionNumbers(String id) {
+        // get the packages of one id (all versions)
+        Enumeration verlist = PackageManager.getPackageVersionNumbers(id);
+
+        List list = new ArrayList();
+        VirtualBuilder builder = new VirtualBuilder(MMBase.getMMBase());
+        while (verlist.hasMoreElements()) {
+            String version = (String) verlist.nextElement();
+            MMObjectNode virtual = builder.getNewNode("admin");
+            virtual.setValue("version", version);
+            list.add(virtual);
+        }
+        return list;
+    }
 
 }
+
