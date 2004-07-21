b999aeb680fc757fd65a3fa24dce1676f4de644d
daniel <daniel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2004-07-21 08:31:26 +0000

    replaced vectors/hashtables
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@11236 91430c5f-d668-0410-9e7d-dd663366dcfd

--- speeltuin/applications/packaging/src/org/mmbase/applications/packaging/installhandlers/uninstallThread.java
+++ speeltuin/applications/packaging/src/org/mmbase/applications/packaging/installhandlers/uninstallThread.java
@@ -1,13 +1,9 @@
 /*
-
-This software is OSI Certified Open Source Software.
-OSI Certified is a certification mark of the Open Source Initiative.
-
-The license (Mozilla version 1.0) can be read at the MMBase site.
-See http://www.MMBase.org/license
-
-*/
-
+ *  This software is OSI Certified Open Source Software.
+ *  OSI Certified is a certification mark of the Open Source Initiative.
+ *  The license (Mozilla version 1.0) can be read at the MMBase site.
+ *  See http://www.MMBase.org/license
+ */
 package org.mmbase.applications.packaging.installhandlers;
 
 import java.lang.*;
@@ -24,32 +20,35 @@ import org.mmbase.util.logging.Logging;
 import org.mmbase.util.logging.Logger;
 
 /**
- * @author Daniel Ockeloen
- * 
+ * @author     Daniel Ockeloen
+ *
  * background hanlder for sending email, a call backthread
  * that is used to send email (one thread per active email
  * node)
+ * @created    July 20, 2004
  */
 public class uninstallThread implements Runnable {
 
     // logger
-    static private Logger log = Logging.getLoggerInstance(uninstallThread.class); 
+    private static Logger log = Logging.getLoggerInstance(uninstallThread.class);
 
     // Thread
     Thread kicker = null;
 
+
     /**
-    * create a background thread with given email node
-    */
+     * create a background thread with given email node
+     */
     public uninstallThread() {
         init();
     }
 
+
     /**
-    * init the thread
-    */
+     * init the thread
+     */
     public void init() {
-        this.start();    
+        this.start();
     }
 
 
@@ -57,30 +56,35 @@ public class uninstallThread implements Runnable {
      * Starts the main Thread.
      */
     public void start() {
-        /* Start up the main thread */
+        /*
+         *  Start up the main thread
+         */
         if (kicker == null) {
-            kicker = new Thread(this,"uninstallthread");
+            kicker = new Thread(this, "uninstallthread");
             kicker.start();
         }
     }
-    
+
+
     /**
      * Main run, exception protected
      */
-    public void run () {
+    public void run() {
         try {
             doWork();
-        } catch(Exception e) {
+        } catch (Exception e) {
             log.error("run(): ERROR: Exception in uninstallThread thread!");
             log.error(Logging.stackTrace(e));
-         }
+        }
     }
+
 
     /**
      * Main work handelr
      */
     public void doWork() {
-	UninstallManager.performUninstall();
+        UninstallManager.performUninstall();
     }
 
 }
+
