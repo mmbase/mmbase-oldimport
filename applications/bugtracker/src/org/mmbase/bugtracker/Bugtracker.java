b7c413c4bf4a51c80f4ac4c0639fc357f627af67
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2004-09-07 09:03:22 +0000

    ant sucks, because the build fails only because no java-files in src-tree of bugtracker. Well, here is one (an empty one), just to keep ant happy :-(
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@11443 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ applications/bugtracker/src/org/mmbase/bugtracker/Bugtracker.java
@@ -0,0 +1,12 @@
+package org.mmbase.bugtracker;
+
+
+/**
+ * This class only exists because the javadoc target of ant needs at least one java-file in the
+ * directory which it processes. The src-dir of bugtracker did only contains some resources.
+ * @author Michiel Meeuwissen
+ * @since  MMBase-1.8
+ * @version $Id: Bugtracker.java,v 1.1 2004-09-07 09:03:22 michiel Exp $
+ */
+public class Bugtracker {
+}
