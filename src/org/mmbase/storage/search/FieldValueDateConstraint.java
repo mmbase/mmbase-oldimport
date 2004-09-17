89d4a0f3b0726d1a8b6d36a247150d3cc59605c3
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2004-09-17 10:09:41 +0000

    field-types: We are going to need this too. (unfinished, but it compiles..)
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@11536 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/storage/search/FieldValueDateConstraint.java
@@ -0,0 +1,39 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.storage.search;
+
+/**
+ * @javadoc
+ * @author Michiel Meeuwissen
+ * @version $Id: FieldValueDateConstraint.java,v 1.1 2004-09-17 10:09:41 michiel Exp $
+ * @since MMBase-1.8
+ */
+public interface FieldValueDateConstraint extends FieldValueConstraint {
+
+    static final int CENTURY      = 0;
+    static final int YEAR         = 1;
+    static final int MONTH        = 2;
+    static final int QUARTER      = 3;
+    static final int WEEK         = 4;
+    static final int DAY_OF_YEAR  = 5;
+    static final int DAY_OF_MONTH = 6;
+    static final int DAY_OF_WEEK  = 7;
+
+    static final int HOUR         = 8;
+    static final int MINUTE       = 9;
+    static final int SECOND       = 10;
+
+
+    /**
+     * Returns the part of the date-field wich is to be compared.
+     */    
+    int getPart();
+
+}
---
+++ src/org/mmbase/storage/search/implementation/BasicFieldValueDateConstraint.java
@@ -0,0 +1,67 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.storage.search.implementation;
+
+import org.mmbase.storage.search.*;
+
+/**
+ * A constraint on a 'part' of a DateTime field. E.g. where extract(YEAR, lastmodified) = 2004.
+ *
+ * @author Michiel Meeuwissen
+ * @version $Id: BasicFieldValueDateConstraint.java,v 1.1 2004-09-17 10:09:41 michiel Exp $
+ * @since MMBase-1.8
+ */
+public class BasicFieldValueDateConstraint extends BasicFieldValueConstraint implements FieldValueDateConstraint {
+
+    /** The date part. */
+    private int part = -1; // unset
+
+    /**
+     * Constructor.
+     * Depending on the field type, the value must be of type
+     * <code>String</code> or <code>Number</code>.
+     *
+     * @param field The associated field.
+     * @param value The non-null property value.
+     * @param part  Which part of the date to compare
+     * @throws IllegalArgumentException when an invalid argument is supplied.
+     */
+    public BasicFieldValueDateConstraint(StepField field, Object value, int part) {
+        super(field, value);
+        if (field.getType() != org.mmbase.module.corebuilders.FieldDefs.TYPE_DATETIME) {
+            throw new IllegalArgumentException("Date value constraints can only be applied to 'DATETIME' type fields");
+        }
+        setValue(value);
+    }
+
+    public int getPart() {
+        return part;
+    }
+
+    public void setPart(int p) {
+        part = p;
+    }
+
+
+    // javadoc is inherited
+    public boolean equals(Object obj) {
+        return super.equals(obj) && ((FieldValueDateConstraint)obj).getPart() == part;
+    }
+
+    // javadoc is inherited
+    public int hashCode() {
+        return super.hashCode() + part * 117;
+    }
+
+    // javadoc is inherited
+    public String toString() {
+        return super.toString() + ", date-part: " + getPart();
+    }
+}
