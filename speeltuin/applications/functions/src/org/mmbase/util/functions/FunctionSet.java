bab985a0f3bacd36d37830c230ffe8b09ff26e99
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2004-09-20 17:13:40 +0000

    only code-convention: identation, spaces, some var-names, some scoping, no real changes
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@11570 91430c5f-d668-0410-9e7d-dd663366dcfd

--- speeltuin/applications/functions/src/org/mmbase/util/functions/FunctionSet.java
+++ speeltuin/applications/functions/src/org/mmbase/util/functions/FunctionSet.java
@@ -28,118 +28,135 @@ import org.mmbase.bridge.implementation.*;
  * @javadoc
  * @author Daniel Ockeloen
  * @version $Id: FunctionSet.java
+ * @since MMBase-1.8
  */
 public class FunctionSet {
+    private static final Logger log = Logging.getLoggerInstance(FunctionSet.class);
 
-	private String name,status,version,description;
-	private static Logger log = Logging.getLoggerInstance(FunctionSet.class.getName());
-	private Hashtable functions=new Hashtable();
-	private String filename;
+    private String name,status,version,description;
 
-	public FunctionSet(String name,String version,String status,String description) {
-		this.name=name;
-		this.version=version;
-		this.status=status;
-		this.description=description;
-	}  
+    private Map functions = new Hashtable();
+    
+    private String fileName;
 
-	public void addFunction(Function fun) {
-		functions.put(fun.getName(),fun);	
-	}
+    public FunctionSet(String name, String version, String status, String description) {
+        this.name        = name;
+        this.version     = version;
+        this.status      = status;
+        this.description = description;
+    }  
+    
+    /** 
+     * @javadoc
+     */
+    public void addFunction(Function fun) {
+        functions.put(fun.getName(), fun);    
+    }
 
-	public Function getFunction(String name) {
-		Object o=functions.get(name);
-		if (o!=null) {
-			return (Function)o;
-		}	
-		return null;
-	}
+    /** 
+     * @javadoc
+     */
+    public Function getFunction(String name) {
+        Object o = functions.get(name);
+        if (o!=null) {
+            return (Function)o;
+        }    
+        return null;
+    }
 
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getVersion() {
-		return version;
-	}
+    public String getVersion() {
+        return version;
+    }
 
-	public String getStatus() {
-		return status;
-	}
+    public String getStatus() {
+        return status;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setStatus(String status) {
-		this.status=status;
-	}
+    public void setStatus(String status) {
+        this.status=status;
+    }
 
-	public void setVersion(String version) {
-		this.version=version;
-	}
+    public void setVersion(String version) {
+        this.version=version;
+    }
 
-	public void setDescription(String description) {
-		this.description=description;
-	}
+    public void setDescription(String description) {
+        this.description=description;
+    }
 
-	public boolean save() {
-		// 1.7temp
-		/*
-		if (filename!=null) {
-			saveFile(filename,createSetXML());
-		}
-		*/
+    /** 
+     * @javadoc
+     */
+    public boolean save() {
+        // 1.7temp
+        /*
+          if (filename!=null) {
+          saveFile(filename,createSetXML());
+        }
+        */
 
-		return true;
-	}
+        return true;
+    }
 
-	public Enumeration getFunctions() {
-		return functions.elements();
-	}
+    /** 
+     * Enumeration??!! Using deprecated stuff to start with is not nice
+     * @javadoc
+     */
+    public Enumeration getFunctions() {
+        return Collections.enumeration(functions.keySet()); 
+    }
+
 
 
     /*
     private String createSetXML() {
-	String body="<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-	body+="<!DOCTYPE functionset PUBLIC \"//MMBase - functionset //\" \"http://www.mmbase.org/dtd/functionset_1_0.dtd\">\n";
+    String body="<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    body+="<!DOCTYPE functionset PUBLIC \"//MMBase - functionset //\" \"http://www.mmbase.org/dtd/functionset_1_0.dtd\">\n";
 
-	body+="<functionset>\n";
-			body+="<status>"+getStatus()+"</status>\n";
-			body+="<version>"+getVersion()+"</version>\n";
-			body+="<description>"+getDescription()+"</description>\n";
-			Enumeration e=functions.elements();
-			while (e.hasMoreElements()) {
-				Function fun=(Function)e.nextElement();
-				body+="\t<function name=\""+fun.getName()+"\">\n";
-				body+="\t\t<description>"+fun.getDescription()+"</description>\n";
-				body+="\t\t<type>"+fun.getType()+"</type>\n";
-				body+="\t\t<class>"+fun.getClassName()+"</class>\n";
-				body+="\t\t<method>"+fun.getMethodName()+"</method>\n";
-				Enumeration e2=fun.getParams().elements();
-				while (e2.hasMoreElements()) {
-					FunctionParam p=(FunctionParam)e2.nextElement();
-					Object o=p.getDefaultValue();
-					if (o==null) {
-						body+="\t\t<param name=\""+p.getName()+"\" type=\""+p.getType()+"\" description=\""+p.getDescription()+"\" />\n";
-					} else {
-						body+="\t\t<param name=\""+p.getName()+"\" type=\""+p.getType()+"\" description=\""+p.getDescription()+"\">"+(String)o+"</param>\n";
-					}
-				}
-				body+="\t\t<return type=\""+fun.getReturnType()+"\"> \n";
-				e2=fun.getReturnValues().elements();
-				while (e2.hasMoreElements()) {
-					FunctionReturnValue r=(FunctionReturnValue)e2.nextElement();
-					body+="\t\t\t<field name=\""+r.getName()+"\" type=\""+r.getType()+"\" description=\""+r.getDescription()+"\" />\n";
-					
-				}
-				body+="\t\t</return> \n";
+    body+="<functionset>\n";
+            body+="<status>"+getStatus()+"</status>\n";
+            body+="<version>"+getVersion()+"</version>\n";
+            body+="<description>"+getDescription()+"</description>\n";
+            Enumeration e=functions.elements();
+            while (e.hasMoreElements()) {
+                Function fun=(Function)e.nextElement();
+                body+="\t<function name=\""+fun.getName()+"\">\n";
+                body+="\t\t<description>"+fun.getDescription()+"</description>\n";
+                body+="\t\t<type>"+fun.getType()+"</type>\n";
+                body+="\t\t<class>"+fun.getClassName()+"</class>\n";
+                body+="\t\t<method>"+fun.getMethodName()+"</method>\n";
+                Enumeration e2=fun.getParams().elements();
+                while (e2.hasMoreElements()) {
+                    FunctionParam p=(FunctionParam)e2.nextElement();
+                    Object o=p.getDefaultValue();
+                    if (o==null) {
+                        body+="\t\t<param name=\""+p.getName()+"\" type=\""+p.getType()+"\" description=\""+p.getDescription()+"\" />\n";
+                    } else {
+                        body+="\t\t<param name=\""+p.getName()+"\" type=\""+p.getType()+"\" description=\""+p.getDescription()+"\">"+(String)o+"</param>\n";
+                    }
+                }
+                body+="\t\t<return type=\""+fun.getReturnType()+"\"> \n";
+                e2=fun.getReturnValues().elements();
+                while (e2.hasMoreElements()) {
+                    FunctionReturnValue r=(FunctionReturnValue)e2.nextElement();
+                    body+="\t\t\t<field name=\""+r.getName()+"\" type=\""+r.getType()+"\" description=\""+r.getDescription()+"\" />\n";
+                    
+                }
+                body+="\t\t</return> \n";
 
-				body+="\t</function>\n\n";
-			}
-	body+="</functionset>\n";
-	return body;
+                body+="\t</function>\n\n";
+            }
+    body+="</functionset>\n";
+    return body;
     }
     */
 
@@ -158,7 +175,7 @@ public class FunctionSet {
     }
     */
 
-    public void setFileName(String filename) {
-	this.filename=filename;
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
     }
 }
--- speeltuin/applications/functions/src/org/mmbase/util/functions/SetFunction.java
+++ speeltuin/applications/functions/src/org/mmbase/util/functions/SetFunction.java
@@ -22,125 +22,136 @@ import org.mmbase.util.logging.*;
  *
  * @author Michiel Meeuwissen
  * @author Daniel Ockeloen
- * @version $Id: SetFunction.java,v 1.1 2004-06-14 12:04:18 daniel Exp $
- * @since MMBase-1.7
+ * @version $Id: SetFunction.java,v 1.2 2004-09-20 17:13:40 michiel Exp $
+ * @since MMBase-1.8
  */
 public class SetFunction extends Function {
-    private String type="unknown";
-    private String classname="unknown";
-    private String methodname="unknown";
-    private String description="unknown";
+    private static final Logger log = Logging.getLoggerInstance(SetFunction.class);
+
+    private String type       = "unknown";
+    private String classname  = "unknown";
+    private String methodname = "unknown";
+    private String description = "unknown";
     // private String returntype="unknown";
     private Class functionClass;
     private Method functionMethod;
     private Object functionInstance; 
-    private Vector params = new Vector();
+    private List params = new Vector();
 
-    private static final Logger log = Logging.getLoggerInstance(SetFunction.class);
 
-    public static Function getFunction(String set, String name) {
-        
+    /**
+     * {@link org.mmbase.util.function.Function#getFunction(String, String)}
+     */
+    public static Function getFunction(String set, String name) {        
 	return FunctionSets.getFunction(set,name);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public Object getFunctionValue(Parameters arguments) {
 	if (functionMethod==null) {
-		if (!initFunction()) {
-			return null;
-		}
+            if (!initFunction()) {
+                return null;
+            }
 	}
 		
 
 	Object arglist[] = new Object[arguments.size()];
 	try {
 
-		for (int i=0;i<arguments.size();i++) {
-			//log.info(arguments.get(i));
-			arglist[i]=arguments.get(i);
-		}	
+            for (int i = 0; i<arguments.size(); i++) {
+                //log.info(arguments.get(i));
+                arglist[i] = arguments.get(i);
+            }	
+            
+            Object retobj = functionMethod.invoke(functionInstance,arglist);
 
-		Object retobj=functionMethod.invoke(functionInstance,arglist);
-
-		return(retobj);
+            return retobj;
 
 			
 	} catch(Exception e) {
-		log.error("function call : "+name);
-		log.error("functionMethod="+functionMethod);
-		log.error("functionInstance="+functionInstance);
-		log.error("arglist="+arglist.toString());
-		e.printStackTrace();
+            log.error("function call : "+name);
+            log.error("functionMethod="+functionMethod);
+            log.error("functionInstance="+functionInstance);
+            log.error("arglist="+arglist.toString());
+            e.printStackTrace();
 	}
 
 	return null;
     }
 
-    public SetFunction(String name, Parameter[] def, ReturnType returnType) {
+    SetFunction(String name, Parameter[] def, ReturnType returnType) {
         super(name, def, returnType);
     }
 
 
-    	public void setType(String type)   { 
-		this.type=type;
-	}
+    void setType(String type)   { 
+        this.type = type;
+    }
 
-	public void setDescription(String description)   { 
-		this.description=description;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    public void setDescription(String description)   { 
+        this.description = description;
+    }
 
-	public void setName(String name)   { 
-		this.name=name;
-	}
+    void setName(String name)   { 
+        this.name = name;
+    }
 
-	public void setClassName(String classname)   { 
-		this.classname=classname;
-	}
+    void setClassName(String classname)   { 
+        this.classname = classname;
+    }
 
-	public void setMethodName(String methodname)   { 
-		this.methodname=methodname;
-	}
+    void setMethodName(String methodname)   { 
+        this.methodname = methodname;
+    }
 
+    /**
+     * @javadoc
+     */
+    private  boolean initFunction() {
+        if (classname != null) {
+            try {
+                functionClass = Class.forName(classname);
+            } catch(Exception e) {
+                log.error("can't create an application function class : "+classname);
+                e.printStackTrace();
+            }
 
-	private  boolean initFunction() {
-		if (classname!=null) {
-			try {
-				functionClass = Class.forName(classname);
-			} catch(Exception e) {
-				log.error("can't create an application function class : "+classname);
-				e.printStackTrace();
-			}
+            try {
+                functionInstance = functionClass.newInstance();
+            } catch(Exception e) {
+                log.error("can't create an function instance : "+classname);
+                e.printStackTrace();
+            }
 
-			try {
-				functionInstance = functionClass.newInstance();
-			} catch(Exception e) {
-				log.error("can't create an function instance : "+classname);
-				e.printStackTrace();
-			}
-
-			Parameter[] pmd= getParameterDefinition();	
-			Class paramtypes[]= new Class[pmd.length];
-			for (int i=0;i<pmd.length;i++) {
-				paramtypes[i]=pmd[i].getType();
-			}
-			try {
-				functionMethod=functionClass.getMethod(methodname,paramtypes);
-			} catch(NoSuchMethodException f) {
-				f.printStackTrace();
-				String paramstring="";
-				/*
-				e=params.elements();
-				while (e.hasMoreElements()) {
-					if (!paramstring.equals("")) paramstring+=",";
-					FunctionParam p=(FunctionParam)e.nextElement();
-					paramstring+=p.getType();
-					paramstring+=" "+p.getName();
+            Parameter[] pmd= getParameterDefinition();	
+            Class paramtypes[]= new Class[pmd.length];
+            for (int i=0;i<pmd.length;i++) {
+                paramtypes[i]=pmd[i].getType();
+            }
+            try {
+                functionMethod = functionClass.getMethod(methodname,paramtypes);
+            } catch(NoSuchMethodException f) {
+               f.printStackTrace();
+                String paramstring="";
+                /*
+                  e=params.elements();
+                  while (e.hasMoreElements()) {
+                  if (!paramstring.equals("")) paramstring+=",";
+                  FunctionParam p=(FunctionParam)e.nextElement();
+                  paramstring+=p.getType();
+                  paramstring+=" "+p.getName();
 					
-				}
-				*/
-				log.error("Function method  not found : "+classname+"."+methodname+"("+paramstring+")");
-				//f.printStackTrace();
-			}
-		}
-		return(true);
-	}
+                  }
+                */
+                log.error("Function method  not found : "+classname+"."+methodname+"("+paramstring+")");
+                //f.printStackTrace();
+            }
+        }
+        return true;
+    }
 }
