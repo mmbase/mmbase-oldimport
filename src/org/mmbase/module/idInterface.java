47e4f67db19d955aa8151c0a8999f570ddab8375
pierre <pierre@91430c5f-d668-0410-9e7d-dd663366dcfd>
2004-10-04 14:58:10 +0000

    tagging
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@11654 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/idInterface.java
+++ src/org/mmbase/module/idInterface.java
@@ -9,9 +9,17 @@ See http://www.MMBase.org/license
 */
 package org.mmbase.module;
 
+/**
+ * @javadoc
+ * @application SCAN
+ * @deprecated no implementation of this interface exists, and the only class that references the interface
+ *             (scanparser) never instantiates it.
+ * @author vpro
+ * @version $Id: idInterface.java,v 1.4 2004-10-04 14:58:10 pierre Exp $
+ */
 public interface idInterface {
-	public String getState(String userName,String name);
-	public String getValue(String userName,String name);
-	public String setValue(String userName,String name, String value);
-	public String setState(String userName,String name, String value);
+    public String getState(String userName, String name);
+    public String getValue(String userName, String name);
+    public String setValue(String userName, String name, String value);
+    public String setState(String userName, String name, String value);
 }
