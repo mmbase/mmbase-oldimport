5084722c35ca2ff6df684840edd0b4e9a6a42b53
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2004-10-15 18:45:41 +0000

    another transformer that might come in handy
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@11788 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/util/transformers/UnicodeEscaper.java
@@ -0,0 +1,83 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.util.transformers;
+
+import java.io.Reader;
+import java.io.Writer;
+
+import org.mmbase.util.logging.*;
+
+/**
+ * To escape from and to 'java' like unicode escaping. That is \\u<4 hex digits>.
+ *
+ * @author Michiel Meeuwissen
+ * @since MMBase-1.8
+ * @version $Id: UnicodeEscaper.java,v 1.1 2004-10-15 18:45:41 michiel Exp $
+ */
+
+public class UnicodeEscaper extends ReaderTransformer implements CharTransformer {
+    private static final Logger log = Logging.getLoggerInstance(UnicodeEscaper.class);
+
+    public Writer transform(Reader r, Writer w) {
+        try {
+            while (true) {
+                int c = r.read();
+                if (c == -1) break;
+                if (c > 127) {
+                    String hex = Integer.toHexString(c);
+                    int i = hex.length();
+                    w.write("\\u");
+                    while (i < 4) {
+                        w.write('0'); i++;
+                    }
+                    w.write(hex); 
+                } else {
+                    w.write(c);
+                }
+            }
+        } catch (Exception e) {
+            log.error(e.toString());
+        }
+        return w;
+    }
+
+    public Writer transformBack(Reader r, Writer w) {
+        try {
+            while (true) {
+                int c = r.read();
+                if (c == -1) break;
+                if (c == '\\') {
+                    c = r.read();
+                    if (c == -1) { w.write('\\'); break; }
+                    if (c == 'u') {
+                        // read 4 hexadecimal digits.
+                        StringBuffer hex = new StringBuffer(4);
+                        while (c != -1 && hex.length() < 4) {
+                            c = r.read();
+                            hex.append((char) c);
+                        }
+                        w.write((char) Integer.parseInt(hex.toString(), 16));
+                    } else {
+                        w.write("\\" + (char) c);
+                    }
+                } else {
+                    w.write((char) c);
+                }
+            }
+        } catch (java.io.IOException e) {
+            log.error(e.toString());
+        }
+        return w;
+    }
+
+    public String toString() {
+        return "Unicode escaper";
+    }
+}
