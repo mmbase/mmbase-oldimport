f0137f31d2c4db4bc727285594f11a1453981df7
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2004-11-11 16:42:07 +0000

    resourceloader
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@12008 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/module/builders/Resources.java
@@ -0,0 +1,69 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.module.builders;
+
+import org.mmbase.module.core.*;
+import org.mmbase.util.logging.*;
+
+/**
+ * The resources builder can be used by {@link org.mmbase.util.ResourceLoader} to load resources from
+ * (configuration files, classes, resourcebundles).
+ *
+ * @author Michiel Meeuwissen
+ * @version $Id: Resources.java,v 1.1 2004-11-11 16:42:07 michiel Exp $
+ * @since   MMBase-1.8
+ */
+public class Resources extends Attachments {
+    private static final Logger log = Logging.getLoggerInstance(Resources.class);
+
+    // these should perhaps be configurable:
+    public static final String    RESOURCENAME_FIELD  = "name";
+    public static final String    TYPE_FIELD          = "type";
+    public static final String    FILENAME_FIELD      = "filename";
+    public static final String    HANDLE_FIELD        = "handle";
+    public static final String    LASTMODIFIED_FIELD  = "lastmodified";
+    public static final String    DEFAULT_CONTEXT     = "admin";
+
+
+    public static final int   TYPE_CONFIG  = 0;
+    public static final int   TYPE_WEB     = 1;
+
+    /**
+     * Registers this builder in the ResourceLoader.
+     * {@inheritDoc}
+     */
+    public boolean init() {
+        boolean res = super.init();
+        if (res) {
+            org.mmbase.util.ResourceLoader.setResourceBuilder(this);
+        } 
+        return res;
+
+    }
+
+    /**
+     * Implements virtual filename field.
+     * {@inheritDoc}
+     */
+    public Object getValue(MMObjectNode node, String field) {
+        if (field.equals(FILENAME_FIELD)) {
+            String s = node.getStringValue(RESOURCENAME_FIELD);
+            int i = s.lastIndexOf("/");
+            if (i > 0) {
+                return s.substring(i + 1);
+            } else {
+                return s;
+            }
+        } else {
+            return super.getValue(node, field);
+        }
+    }
+
+}
