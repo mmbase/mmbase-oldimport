25b9e31b6ecf11bf44cb611817906313e75fbdc3
keesj <keesj@91430c5f-d668-0410-9e7d-dd663366dcfd>
2004-11-18 22:11:39 +0000

    keesj:port to mmbase 1.8
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@12100 91430c5f-d668-0410-9e7d-dd663366dcfd

--- speeltuin/keesj/runner/Start.java
+++ speeltuin/keesj/runner/Start.java
@@ -1,15 +1,15 @@
 import org.mmbase.util.logging.Logging;
 import org.mmbase.module.tools.MMAdmin;
+import org.mmbase.util.ResourceLoader;
+import org.mmbase.module.core.MMBase;
 import java.io.*;
 
 public class Start{
 	public static void main(String[] argv) throws Exception{
-                Logging.configure(System.getProperty("mmbase.config") + File.separator + "log" + File.separator + "log.xml");
-
+		Logging.configure(ResourceLoader.getConfigurationRoot().getChildResourceLoader("log"), "log.xml");
 		org.mmbase.module.core.MMBaseContext.init();
-		org.mmbase.module.core.MMBase.getMMBase();
-		MMAdmin mmadmin = (MMAdmin) org.mmbase.module.core.MMBase.getModule("mmadmin", true);
-	       while (! mmadmin.hasStarted()) {
+		MMBase mmbase = MMBase.getMMBase();
+	        while (! mmbase.hasStarted()) {
        		     Thread.sleep(1000);
         	}
 		System.err.println("started");
