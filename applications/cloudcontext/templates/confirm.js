50cd70f3d49554616c0bc51a80c784bbabb8cfdc
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2004-12-20 15:23:04 +0000

    in tomcat 4, taglib 1.8 did gave an exception (not in tomcat 5 and / or taglib 1.7). This was the first non-compatibility issue I found with nnew taglib
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@12430 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/cloudcontext/templates/confirm.js
+++ applications/cloudcontext/templates/confirm.js
@@ -1,7 +1,7 @@
-<mm:write referid="prompt" jspvar="prompt" vartype="string">
+<mm:write referid="prompt" jspvar="promptt" vartype="string">
 var element =  document.getElementById('object<mm:field name="number" />');
 element.className = '<mm:even>even</mm:even> highlight'; 
-if (confirm('<%=getPrompt(m, prompt)%>')) { 
+if (confirm('<%=getPrompt(m, promptt)%>')) { 
     return true; 
 } else { 
     element.className = '<mm:even>even</mm:even>';
