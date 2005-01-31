5fa6bd7d1873072aa43d82de5c1d228e14122f89
keesj <keesj@91430c5f-d668-0410-9e7d-dd663366dcfd>
2005-01-31 21:13:22 +0000

    keesj:added to tests to test if converting images work
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@12672 91430c5f-d668-0410-9e7d-dd663366dcfd

--- tests/convertimage/org/mmbase/bridge/tests/ConvertImageTest.java
+++ tests/convertimage/org/mmbase/bridge/tests/ConvertImageTest.java
@@ -1,17 +1,60 @@
 package org.mmbase.bridge.tests;
+
 import org.mmbase.bridge.*;
 import java.io.*;
 
+import org.mmbase.util.functions.DataType;
+import org.mmbase.util.functions.Function;
+import org.mmbase.util.functions.Parameters;
+import org.mmbase.util.logging.*;
+
 /**
  * JUnit tests for convertimage-interface implementation.
- 
- * @author  Michiel Meeuwissen 
- * @version $Id: ConvertImageTest.java,v 1.2 2003-06-18 13:50:54 kees Exp $
+ * 
+ * @author Michiel Meeuwissen
+ * @version $Id: ConvertImageTest.java,v 1.3 2005-01-31 21:13:22 keesj Exp $
  */
 public class ConvertImageTest extends org.mmbase.tests.BridgeTest {
+    private final static Logger log = Logging.getLoggerInstance(ConvertImageTest.class);
+
     private final static String JPG_IMAGE_NAME = "testimage.jpg";
 
-    public void testImportJpegImage() {
+    public void testImportedJpegImage() {
+        Cloud cloud = getCloud();
+        Node node = cloud.getNode("jpeg.test.image");
+        byte[] bytes = node.getByteValue("handle");
+        assertTrue("MMBase failed to determine mime-type properly (magicfile problem?)", node.getStringValue("itype").equals("jpeg"));
+        //node.delete();
+    }
+    
+    /**
+     * test if an image can be converted using the getIntValue
+     */
+    public void testGetInvalueCachedImage() {
+        Cloud cloud = getCloud();
+        Node node = cloud.getNode("jpeg.test.image");
+        node.getIntValue("cache(s(30x30))");
+    }
+
+    /**
+     * test if an image can be converted using getFunctionValue
+     *
+     */
+    public void testFunctionValueCachedImage() {
+        Cloud cloud = getCloud();
+        Node node = cloud.getNode("jpeg.test.image");
+        Function f = node.getFunction("cache");
+        Parameters p = f.createParameters();
+        p.set("template","s(30x30)");
+        f.getFunctionValue(p);
+    }
+
+    /**
+     * Sets up before each test.
+     */
+    public void setUp() throws Exception {
+        startMMBase();
+        startLogging();
         Cloud cloud = getCloud();
         NodeManager nodeManager = cloud.getNodeManager("images");
         Node jpegNode = nodeManager.createNode();
@@ -22,30 +65,24 @@ public class ConvertImageTest extends org.mmbase.tests.BridgeTest {
         jpegNode.createAlias("jpeg.test.image");
     }
 
-    public void testImportedJpegImage() {
+
+    /* (non-Javadoc)
+     * @see junit.framework.TestCase#tearDown()
+     */
+    protected void tearDown() throws Exception {
         Cloud cloud = getCloud();
         Node node = cloud.getNode("jpeg.test.image");
-        byte[] bytes = node.getByteValue("handle");
-        assertTrue("MMBase failed to determine mime-type properly (magicfile problem?)", node.getStringValue("itype").equals("jpeg"));
         node.delete();
     }
-
-    /**
-     * Sets up before each test.
-     */
-    public void setUp() throws Exception {
-        startMMBase();
-        startLogging();
-    }
-
     /**
      * read the test image from the file system and return it a byte[]
+     * 
      * @return the byte[] containing the image
      */
     private byte[] getTextImageBytes(String name) {
         try {
-
             InputStream in = this.getClass().getResourceAsStream(name);
+
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
             byte[] buffer = new byte[200];
@@ -54,7 +91,9 @@ public class ConvertImageTest extends org.mmbase.tests.BridgeTest {
                 baos.write(buffer, 0, readSize);
             }
             return baos.toByteArray();
-        } catch (IOException ioe) {}
+        } catch (IOException ioe) {
+            fail("did not find image to load " + this.getClass().getResource(name));
+        }
         return null;
     }
 }
