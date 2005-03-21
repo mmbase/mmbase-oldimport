f10afabf9f8c2473a5f4ee35f66539e46adf3050
pierre <pierre@91430c5f-d668-0410-9e7d-dd663366dcfd>
2005-03-21 13:03:35 +0000

    It is possible that MMBase has not been loaded when the set() on LocalizedString is called. To prevent issues with recursive calls while loading modules, when MMBase is not yet loaded, the default value is stored with the key 'null' instead of the locale from MMBase. It's not nice, but as a proper fix likely requires a total rewrite of Module.java and MMBase.java, this will have to do for the moment.
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@13053 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/util/LocalizedString.java
+++ src/org/mmbase/util/LocalizedString.java
@@ -18,12 +18,12 @@ import java.util.*;
  * this object.
  *
  * @author Michiel Meeuwissen
- * @version $Id: LocalizedString.java,v 1.2 2005-03-21 12:11:23 michiel Exp $
+ * @version $Id: LocalizedString.java,v 1.3 2005-03-21 13:03:35 pierre Exp $
  * @since MMBase-1.8
  */
 public class LocalizedString  {
 
-    private final String key; 
+    private final String key;
     private Map    values = null;
     private String bundle = null;
 
@@ -44,21 +44,35 @@ public class LocalizedString  {
         }
         if (values != null) {
             String result = (String) values.get(locale);
-            
+
             if (result != null) return result;
-            
+
             String variant  = locale.getVariant();
             String country  = locale.getCountry();
             String language = locale.getLanguage();
-            
+
             if (! "".equals(variant)) {
                 result = (String) values.get(new Locale(language, country));
                 if (result != null) return result;
             }
-            
+
             if (! "".equals(country)) {
                 result = (String) values.get(new Locale(language));
                 if (result != null) return result;
+            }
+
+            // Some LocalizedString isntances may have a default value stored with the key 'null'
+            // instead of the locale from MMBase. This is the case for values stored while the
+            // MMBase module was not yet active
+            // This code 'fixes' that reference.
+            // It's not nice, but as a proper fix likely requires a total rewrite of Module.java and
+            // MMBase.java, this will have to do for the moment.
+            if (locale.equals(org.mmbase.module.core.MMBase.getMMBase().getLocale())) {
+                result = (String) values.get(null);
+                if (result != null) {
+                    values.put(locale,result);
+                    return result;
+                }
             }
         }
 
@@ -83,27 +97,34 @@ public class LocalizedString  {
         }
 
         if (locale == null) {
-            locale = org.mmbase.module.core.MMBase.getMMBase().getLocale();
+            // It is possible that MMBase has not been loaded, so only retrieve the locale if all the
+            // modules have been loaded. This prevents issues with recursive calls while loading modules.
+            if (org.mmbase.module.Module.getModules() != null) {
+                locale = org.mmbase.module.core.MMBase.getMMBase().getLocale();
+            }
         }
 
         values.put(locale, value);
-        
-        String variant  = locale.getVariant();
-        String country  = locale.getCountry();
-        String language = locale.getLanguage();
-        if (! "".equals(variant)) {
-            Locale loc = new Locale(language, country);
-            if (values.get(loc) == null) {
-                values.put(loc, value);
+
+        if (locale != null) {
+            String variant  = locale.getVariant();
+            String country  = locale.getCountry();
+            String language = locale.getLanguage();
+            if (! "".equals(variant)) {
+                Locale loc = new Locale(language, country);
+                if (values.get(loc) == null) {
+                    values.put(loc, value);
+                }
             }
-        }
-        if (! "".equals(country)) {
-            Locale loc = new Locale(language);
-            if (values.get(loc) == null) {
-                values.put(loc, value);
+            if (! "".equals(country)) {
+                Locale loc = new Locale(language);
+                if (values.get(loc) == null) {
+                    values.put(loc, value);
+                }
             }
         }
     }
+
     /**
      * A resource-bundle with given name can be associated to this LocalizedString. If no
      * tranlations where explicitely added, it can be used to look up the translation in the bundle,
