15ea876dd8ee5fc4b20560cb41b144c691d55f6f
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2005-05-04 22:19:48 +0000

    I need this all over the place, lets make it a util
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@13446 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/util/Entry.java
@@ -0,0 +1,31 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.util;
+
+/**
+ * Represents a pair of values. Normally a 'key' and a 'value'.
+ * @since MMBase-1.8
+ */
+public class Entry implements java.util.Map.Entry {
+
+    private final Object key;
+    private  Object value;
+    public Entry (Object k, Object v) {
+        key = k ; value = v;
+    }
+    public Object  getKey() { return key; }
+    public Object  getValue() { return value; }
+
+    public Object  setValue(Object v) { Object r = value; value = v;  return r;}
+    
+    public String toString() { return key + "=" + value; }
+   
+    
+}
