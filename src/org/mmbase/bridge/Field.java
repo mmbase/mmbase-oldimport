3436ce916324383669880b57b7935a1b2532f2e2
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2005-05-10 22:49:00 +0000

    moved most things to more general interface
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@13543 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/bridge/Field.java
+++ src/org/mmbase/bridge/Field.java
@@ -17,41 +17,9 @@ import java.util.Locale;
  *
  * @author Pierre van Rooden
  * @author Jaco de Groot
- * @version $Id: Field.java,v 1.18 2005-03-16 16:01:23 michiel Exp $
+ * @version $Id: Field.java,v 1.19 2005-05-10 22:49:00 michiel Exp $
  */
-public interface Field extends DataType {
-
-    public final static int STATE_VIRTUAL    = 0;
-    public final static int STATE_PERSISTENT = 2;
-    public final static int STATE_SYSTEM     = 3;
-    public final static int STATE_UNKNOWN    = -1;
-
-
-    public final static int TYPE_STRING  = 1;
-    public final static int TYPE_INTEGER = 2;
-    public final static int TYPE_BYTE    = 4;
-    public final static int TYPE_FLOAT   = 5;
-    public final static int TYPE_DOUBLE  = 6;
-    public final static int TYPE_LONG    = 7;
-    public final static int TYPE_XML     = 8;
-    public final static int TYPE_NODE    = 9;
-    /**
-     * @since MMBase-1.8
-     */
-    public final static int TYPE_DATETIME  = 10;   
-    /**
-     * @since MMBase-1.8
-     */
-    public final static int TYPE_BOOLEAN   = 11;
-    /**
-     * // not yet working
-     * @since MMBase-1.8
-     */
-    public final static int TYPE_LIST      = 12;
-
-
-    public final static int TYPE_UNKNOWN = -1;
-
+public interface Field extends org.mmbase.core.FieldType {
     /**
      * Returns the node manager this field belongs to.
      *
@@ -59,36 +27,6 @@ public interface Field extends DataType {
      */
     public NodeManager getNodeManager();
 
-    /**
-     * Returns the name of this field.
-     *
-     * @return  the name of this field
-     */
-    public String getName();
-
-
-    /**
-     * Returns the GUI name for the data type this field contains.
-     *
-     * @return  the GUI name for the data type this field contains
-     */
-    public String getGUIType();
-
-    /**
-     * Returns the GUI name for this field.
-     *
-     * @return  the GUI name for this field
-     */
-    public String getGUIName();
-
-   /**
-     * Returns the GUI name for this field in a specified preferred language.
-     *
-     * @param locale the locale that determines the language for the GUI name
-     * @return  the GUI name for this field
-     * @since MMBase-1.7
-     */
-    public String getGUIName(Locale locale);
 
     /**
      * Returns the description for this field.
@@ -97,74 +35,6 @@ public interface Field extends DataType {
      */
     public String getDescription();
 
-    /**
-     * Returns the description for this field in a specified preferred language.
-     *
-     * @param locale the locale that determines the language for the description
-     * @return  the description for this field
-     * @since MMBase-1.7
-     */
-    public String getDescription(Locale locale);
 
-    /**
-     * Returns the identifier for the data type this field contains.
-     *
-     * @return  an <code>int</code> which identifies the type of data this field
-     *          contains
-     */
-    public int getType();
-
-    /**
-     * Returns this field's state identifier (virtual, persistent, system).
-     *
-     * @return  an <code>int</code> which identifies the state of this field
-     */
-    public int getState();
-
-    /**
-     * Returns whether this field is required (should have content).
-     * Note that MMBase does not generally enforce required fields to be filled -
-     * If not provided, a default value (generally an empty string or the integer value -1)
-     * is filled in by the system.
-     * As such, isRequired will mostly be used as an indicator for (generic) editors.
-     *
-     * @return  <code>true</code> if the field is required
-     * @since  MMBase-1.6
-     */
-    public boolean isRequired();
-
-    /**
-     * Returns whether this field is part of a unique key (a set of fields whose combined content should
-     * occur only once).
-     * Note that MMBase lets the storage layer handle this. If your storage implementation or configuration does
-     * not support this the uniqueness may not be enforced.
-     *
-     * @return  <code>true</code> if the field is part of a unique key
-     * @since  MMBase-1.6
-     */
-    public boolean isUnique();
-
-    /**
-     * Returns whether this field is a key field, meaning that the storage layer should define an index for it, allowing
-     * optimization with search and sort actions.
-     * Note that MMBase lets the storage layer decide whether an index is actually defined.
-     * Some implementations or configurations may not do this.
-     * Note: Currently, this method only returns true if the field is the primary key (number field) or a Node field.
-     *
-     * @return  <code>true</code> if the field has a key defined
-     * @since  MMBase-1.7
-     */
-    public boolean hasIndex();
-
-    /**
-     * Returns the maximum length of data this field can contain.
-     * For example if a field contains characters the size indicates the
-     * maximum number of characters it can contain.
-     * If the field is a numeric field (such as an integer), the result is -1.
-     *
-     * @return  the maximum length of data this field can contain
-     */
-
-    public int getMaxLength();
 
 }
