875e9053a751e911b0830d405e0e4a76b824ce24
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2005-05-24 16:54:25 +0000

    also create threadpool for 'jobs'
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@13718 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/util/ThreadPools.java
+++ src/org/mmbase/util/ThreadPools.java
@@ -10,18 +10,45 @@ See http://www.MMBase.org/license
 package org.mmbase.util;
 import edu.emory.mathcs.backport.java.util.concurrent.*;
 import java.io.*;
+import org.mmbase.util.logging.*;
 /**
  * Generic MMBase Thread Pools
  *
  * @since MMBase 1.8
  * @author Michiel Meewissen
- * @version $Id: ThreadPools.java,v 1.1 2005-05-12 15:37:44 michiel Exp $
+ * @version $Id: ThreadPools.java,v 1.2 2005-05-24 16:54:25 michiel Exp $
  */
 public abstract class ThreadPools {
-    
+    private static final Logger log = Logging.getLoggerInstance(ThreadPools.class);
+
     /**
-     * Generic Thread Pools which can be used by 'filters'.
+     * Generic Thread Pools which can be used by 'filters'. 
      */
     public static final Executor filterExecutor = Executors.newCachedThreadPool();
+
+    
+    /**
+     * For jobs there are 'scheduled', and typically happen on larger time-scales.
+     */
+    public static final Executor jobsExecutor = new ThreadPoolExecutor(2, 10, 5, TimeUnit.MINUTES, new  SynchronousQueue(), new ThreadFactory() {
+            
+            public Thread newThread(Runnable r) {
+                Thread t = new Thread(r, "JOBTHREAD") {
+                        /**
+                         * Overrides run of Thread to catch and log all exceptions. Otherwise they go through to app-server.
+                         */
+                        public void run() {
+                            try {
+                                super.run();
+                            } catch (Throwable t) {
+                                log.error("Error during job: " + t.getClass().getName() + " " + t.getMessage() + "\n" + Logging.stackTrace(t));
+                            }
+                        }
+                    };
+                t.setDaemon(true);
+                return t;
+            }
+        });
+                                                                              
 
 }
