0c33e51566c2a3486e42267f99d8cf691d1d0edb
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2005-06-21 19:14:04 +0000

    added a p;roperty 'sessionname' the one from CloudTag will be dropped
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@14066 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/bridge/Cloud.java
+++ src/org/mmbase/bridge/Cloud.java
@@ -21,11 +21,13 @@ import org.mmbase.util.functions.Function;
  * @author Rob Vermeulen
  * @author Pierre van Rooden
  * @author Jaco de Groot
- * @version $Id: Cloud.java,v 1.52 2005-04-25 11:11:54 michiel Exp $
+ * @version $Id: Cloud.java,v 1.53 2005-06-21 19:14:04 michiel Exp $
  */
 public interface Cloud {
 
-    public static final String PROP_XMLMODE = "org.mmbase.xml-mode";
+    public static final String PROP_XMLMODE     = "org.mmbase.xml-mode";
+
+    public static final String PROP_SESSIONNAME = "org.mmbase.cloud.sessionname";
 
     /**
      * Returns the node with the specified number from this cloud. The returned
