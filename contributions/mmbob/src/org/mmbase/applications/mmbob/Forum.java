247c252e4aea5673bc0a2884a5917eeccff02313
gerard <gerard@91430c5f-d668-0410-9e7d-dd663366dcfd>
2005-06-22 14:49:19 +0000

    oops...forgot to merge
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@14085 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/mmbob/src/org/mmbase/applications/mmbob/Forum.java
+++ contributions/mmbob/src/org/mmbase/applications/mmbob/Forum.java
@@ -1271,6 +1271,18 @@ public class Forum {
         return ForumManager.getAvatarsGalleryEnabled();
     }
 
+    public String getAvatarsGalleryEnabled() {
+        if (config != null) {
+            String tmp = config.getAvatarsGalleryEnabled();
+            if (tmp != null) {
+                log.debug("config.getAvatarsGalleryEnabled() on "+getId()+ ": " + tmp);                
+                return tmp;
+                
+            }
+        }
+        return ForumManager.getAvatarsGalleryEnabled();
+    }
+
 
     public String getHeaderPath() {
         if (config != null) {
