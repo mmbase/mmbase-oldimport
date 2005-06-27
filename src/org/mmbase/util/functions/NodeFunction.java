bffd6026896b10630d526dc45b56f17f4230f610
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2005-06-27 12:29:06 +0000

    Added a utitility function to call functions on Node by String
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@14139 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/util/functions/NodeFunction.java
+++ src/org/mmbase/util/functions/NodeFunction.java
@@ -10,7 +10,7 @@ See http://www.MMBase.org/license
 package org.mmbase.util.functions;
 
 import org.mmbase.module.core.MMObjectNode;
-import org.mmbase.bridge.Node;
+import org.mmbase.bridge.*;
 
 import org.mmbase.util.logging.Logger;
 import org.mmbase.util.logging.Logging;
@@ -21,7 +21,7 @@ import org.mmbase.util.logging.Logging;
  * the Parameter array of the constructor.
  *
  * @author Michiel Meeuwissen
- * @version $Id: NodeFunction.java,v 1.6 2005-06-21 19:23:30 michiel Exp $
+ * @version $Id: NodeFunction.java,v 1.7 2005-06-27 12:29:06 michiel Exp $
  * @see org.mmbase.module.core.MMObjectBuilder#executeFunction
  * @see org.mmbase.bridge.Node#getFunctionValue
  * @see org.mmbase.util.function.BeanFunction
@@ -32,7 +32,27 @@ public abstract class NodeFunction extends AbstractFunction {
 
     private static final Logger log = Logging.getLoggerInstance(NodeFunction.class);
 
-    public NodeFunction(String name, org.mmbase.bridge.DataType[] def, org.mmbase.bridge.DataType returnType) {
+
+    /**
+     * Utility function, for easy call of function on node by one string.
+     */
+    public static FieldValue getFunctionValue(Node node, String function) {
+        java.util.List args = null;
+        String functionName = function;
+        int pos1 = function.indexOf('(');
+        if (pos1 != -1) {
+            int pos2 = function.lastIndexOf(')');
+            if (pos2 != -1) {
+                functionName = function.substring(0, pos1);
+                args = org.mmbase.util.StringSplitter.splitFunctions(function.subSequence(pos1 + 1, pos2));
+            }
+        }
+        return node.getFunctionValue(functionName, args);
+    }
+
+
+
+    public NodeFunction(String name, DataType[] def, DataType returnType) {
         super(name, new Parameter[] { new Parameter.Wrapper(def), Parameter.NODE}, returnType);
     }
 
@@ -47,12 +67,13 @@ public abstract class NodeFunction extends AbstractFunction {
      * Implements the function on a certain node. Override this method <em>or</em> it's bridge
      * counter-part {@link #getFunctionValue(org.mmbase.bridge.Node, Parameters)}.  Overriding the
      * bridge version has two advantages. It's easier, and mmbase security will be honoured.
+     * 
      */
     protected Object getFunctionValue(final MMObjectNode coreNode, final Parameters parameters) {
         if (coreNode == null) throw new RuntimeException("No node argument given for " + this + "(" + parameters + ")!");
-        final org.mmbase.bridge.Cloud cloud   = (org.mmbase.bridge.Cloud)  parameters.get(Parameter.CLOUD);
-        if (cloud == null) throw new RuntimeException("No cloud argument given"  + this + "(" + parameters + ")!");
-        final org.mmbase.bridge.Node node     = cloud.getNode(coreNode.getNumber());
+        final Cloud cloud   = (Cloud)  parameters.get(Parameter.CLOUD);
+        if (cloud == null) throw new RuntimeException("No cloud argument given"  + this + "(" + parameters + ")!" + Logging.stackTrace());
+        final Node node     = cloud.getNode(coreNode.getNumber());
         return getFunctionValue(node, parameters);
             
     }
@@ -60,10 +81,12 @@ public abstract class NodeFunction extends AbstractFunction {
     /**
      * This function will never be called, only by the default implemention of {@link
      * #getFunctionValue(MMObjectNode, Parameters)}. So, you must override either that funciton, or
-     * this one.
+     * this one. This will probably not work if you don't call {@link Node#getFunctionValue} (but
+     * something like getStringValue("function(arguments)"), which is deprecated).
+     *
      * @throws UnssupportedOoperationException
      */
-    protected Object getFunctionValue(org.mmbase.bridge.Node node, Parameters parameters) {
+    protected Object getFunctionValue(Node node, Parameters parameters) {
         throw new UnsupportedOperationException("Not supported");
     }
 
