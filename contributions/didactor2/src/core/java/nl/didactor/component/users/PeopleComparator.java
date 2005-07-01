7e223d4d02fb2b7a0568c72625a087ca395e978d
azemskov <azemskov@91430c5f-d668-0410-9e7d-dd663366dcfd>
2005-07-01 18:20:06 +0000

    [new] lastname,firstname comparator for people
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@14214 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ contributions/didactor2/src/core/java/nl/didactor/component/users/PeopleComparator.java
@@ -0,0 +1,26 @@
+package nl.didactor.component.users;
+
+import java.util.Comparator;
+
+public class PeopleComparator implements Comparator
+{
+   public PeopleComparator()
+   {
+   }
+
+   public int compare(Object ob1, Object ob2)
+   {
+      String[] arrstrObj1 = (String[]) ob1;
+      String[] arrstrObj2 = (String[]) ob2;
+
+      int iResult = arrstrObj1[1].compareTo(arrstrObj2[1]);
+      if (iResult != 0)
+      {
+         return iResult;
+      }
+      else
+      {
+         return arrstrObj1[2].compareTo(arrstrObj2[2]);
+      }
+   }
+}
