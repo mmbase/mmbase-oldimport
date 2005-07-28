7a4fa4dbab1cdebe8838b05fc2c3543bb86462d1
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2005-07-28 16:56:58 +0000

    Simplified FunctionSet, dropped some members which are used nowhere
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@14556 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/util/functions/FunctionSet.java
+++ src/org/mmbase/util/functions/FunctionSet.java
@@ -12,82 +12,34 @@ package org.mmbase.util.functions;
 import java.util.*;
 
 /**
- * The implementation of one set ('namespace') of functions. Objects of this type are managed by {@link org.mmbase.util.functions.FunctionSets}.
+ * The implementation of one set ('namespace') of functions. This is actually just a named FunctionProvider.
  *
  * @javadoc
  * @author Daniel Ockeloen
- * @version $Id: FunctionSet.java
+ * @author Michiel Meeuwissen
+ * @version $Id: FunctionSet.java,v 1.7 2005-07-28 16:56:58 michiel Exp $
  * @since MMBase-1.8
  */
-public class FunctionSet {
+public class FunctionSet extends FunctionProvider {
 
-    private String name, status, version, description;
+    private String name;
+    private String description;
 
-    /**
-     * String -> Function
-     */
-    private Map functions = new HashMap();
-
-    public FunctionSet(String name, String version, String status, String description) {
+    public FunctionSet(String name, String description) {
         this.name        = name;
-        this.version     = version;
-        this.status      = status;
         this.description = description;
     }
 
-    /**
-     * Adds a Function to this set.
-     * @param fun The to-be-added Function object
-     */
-    public void addFunction(Function fun) {
-        functions.put(fun.getName(), fun);
-    }
-
-    /**
-     * Gets a Function from this set. Used by {@link FunctionSets} which manages all set-functions.
-     */
-    public Function getFunction(String name) {
-        Object o = functions.get(name);
-        if (o != null) {
-            return (Function)o;
-        }
-        return null;
-    }
-
-
     public String getName() {
         return name;
-    }
-
-    public String getVersion() {
-        return version;
-    }
-
-    public String getStatus() {
-        return status;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public void setVersion(String version) {
-        this.version = version;
-    }
-
     public void setDescription(String description) {
         this.description = description;
-    }
-
-    /**
-     * @return An unmodifiable Map (String -> {@link Function}) containing all functions of this set.
-     */
-    public Map getFunctions() {
-        return Collections.unmodifiableMap(functions);
     }
 
 }
