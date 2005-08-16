6adac968ac83225171cbcba0f3cb16ad77a8c0e4
pierre <pierre@91430c5f-d668-0410-9e7d-dd663366dcfd>
2005-08-16 09:14:25 +0000

    added whitespace property. Whitespace is not used in validation, but modifies data when processed.
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@14648 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/datatypes/StringDataType.java
+++ src/org/mmbase/datatypes/StringDataType.java
@@ -13,6 +13,7 @@ import java.util.*;
 
 import org.mmbase.bridge.Field;
 import org.mmbase.bridge.Cloud;
+import org.mmbase.bridge.Node;
 import org.mmbase.util.Casting;
 import org.mmbase.util.logging.*;
 
@@ -20,17 +21,25 @@ import org.mmbase.util.logging.*;
  * @javadoc
  *
  * @author Pierre van Rooden
- * @version $Id: StringDataType.java,v 1.5 2005-08-15 16:38:20 pierre Exp $
+ * @version $Id: StringDataType.java,v 1.6 2005-08-16 09:14:25 pierre Exp $
  * @since MMBase-1.8
  */
 public class StringDataType extends BigDataType {
 
+    public static final Integer WHITESPACE_PRESERVE = new Integer(0);
+    public static final Integer WHITESPACE_REPLACE = new Integer(1);
+    public static final Integer WHITESPACE_COLLAPSE = new Integer(2);
+
     public static final String PROPERTY_PATTERN = "pattern";
     public static final String PROPERTY_PATTERN_DEFAULT = null;
+
+    public static final String PROPERTY_WHITESPACE = "whiteSpace";
+    public static final Integer PROPERTY_WHITESPACE_DEFAULT = WHITESPACE_PRESERVE;
 
     private static final Logger log = Logging.getLoggerInstance(StringDataType.class);
 
     protected DataType.Property patternProperty;
+    protected DataType.Property whiteSpaceProperty;
 
     /**
      * Constructor for string data type.
@@ -43,6 +52,7 @@ public class StringDataType extends BigDataType {
     public void erase() {
         super.erase();
         patternProperty = createProperty(PROPERTY_PATTERN, PROPERTY_PATTERN_DEFAULT);
+        whiteSpaceProperty = createProperty(PROPERTY_WHITESPACE, PROPERTY_WHITESPACE_DEFAULT);
     }
 
     public void inherit(DataType origin) {
@@ -50,6 +60,7 @@ public class StringDataType extends BigDataType {
         if (origin instanceof StringDataType) {
             StringDataType dataType = (StringDataType)origin;
             patternProperty = (DataType.Property)dataType.getPatternProperty().clone(this);
+            whiteSpaceProperty = (DataType.Property)dataType.getWhiteSpaceProperty().clone(this);
         }
     }
 
@@ -78,6 +89,35 @@ public class StringDataType extends BigDataType {
         return setProperty(getPatternProperty(), value);
     }
 
+    /**
+     * Returns the whitespace value.
+     * @return one of the constants {@link #WHITESPACE_PRESERVE}, {@link #WHITESPACE_REPLACE},  or {@link #WHITESPACE_COLLAPSE}
+     */
+    public Integer getWhiteSpace() {
+        return (Integer) getWhiteSpaceProperty().getValue();
+    }
+
+    /**
+     * Returns the 'whitespace' property, containing the value, errormessages, and fixed status of this attribute.
+     * @return the property as a {@link DataType#Property}
+     */
+    public DataType.Property getWhiteSpaceProperty() {
+        return whiteSpaceProperty;
+    }
+
+    /**
+     * Sets the whitspeace value used to validate values for this datatype.
+     * @param whitespace one of the constants {@link #WHITESPACE_PRESERVE}, {@link #WHITESPACE_REPLACE}, or {@link #WHITESPACE_COLLAPSE}
+     * @throws Class Identifier: java.lang.UnsupportedOperationException if this datatype is read-only (i.e. defined by MBase)
+     */
+    public DataType.Property setWhiteSpace(Integer value) {
+        if (WHITESPACE_COLLAPSE.equals(value) || WHITESPACE_PRESERVE.equals(value) || WHITESPACE_REPLACE.equals(value)) {
+            return setProperty(getWhiteSpaceProperty(), value);
+        } else {
+            throw new IllegalArgumentException("value should be either WHITESPACE_PRESERVE, WHITESPACE_REPLACE or WHITESPACE_COLLAPSE");
+        }
+    }
+
     public void validate(Object value, Field field, Cloud cloud) {
         super.validate(value, field, cloud);
         if (value != null) {
@@ -91,10 +131,33 @@ public class StringDataType extends BigDataType {
         }
     }
 
+    public Object process(int action, Node node, Field field, Object value, int processingType) {
+        value = super.process(action, node, field, value, processingType);
+        if (value instanceof String) {
+            Integer whiteSpace = getWhiteSpace();
+            if (whiteSpace.equals(WHITESPACE_REPLACE)) {
+                // remove all whitespace
+                value = ((String)value).replaceAll("\\s"," ");
+            } else if (whiteSpace.equals(WHITESPACE_COLLAPSE)) {
+                // remove all whitespace
+                value = ((String)value).replaceAll("\\s"," ");
+                value = ((String)value).trim().replaceAll("  "," ");
+            }
+        }
+        return value;
+    }
+
+
     public String toString() {
         StringBuffer buf = new StringBuffer(super.toString());
         if (getPattern() != null) {
             buf.append("pattern:").append(getPattern()).append("\n");
+        }
+        Integer whiteSpace = getWhiteSpace();
+        if (whiteSpace.equals(WHITESPACE_REPLACE)) {
+            buf.append("whitespace: replace\n");
+        } else if (whiteSpace.equals(WHITESPACE_COLLAPSE)) {
+            buf.append("whitespace: collapse\n");
         }
         return buf.toString();
     }
