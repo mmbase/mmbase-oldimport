a4014af5b03403648646b7459c6638f907042629
nbukharev <nbukharev@91430c5f-d668-0410-9e7d-dd663366dcfd>
2005-08-19 14:23:41 +0000

    first version of Versioning
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@14695 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ contributions/didactor2/src/versioning/java/nl/didactor/versioning/builders/ParagraphVersioningBuilder.java
@@ -0,0 +1,41 @@
+package nl.didactor.versioning.builders;
+
+import org.mmbase.module.core.MMObjectBuilder;
+import org.mmbase.module.core.MMObjectNode;
+import org.mmbase.util.logging.Logger;
+import org.mmbase.util.logging.Logging;
+import org.mmbase.bridge.Node;
+import org.mmbase.bridge.NodeList;
+import org.mmbase.bridge.Cloud;
+import nl.didactor.versioning.VersioningController;
+import net.sf.mmapps.modules.cloudprovider.CloudProvider;
+import net.sf.mmapps.modules.cloudprovider.CloudProviderFactory;
+
+public class ParagraphVersioningBuilder extends MMObjectBuilder
+{
+
+  private static Logger log = Logging.getLoggerInstance(ParagraphVersioningBuilder.class.getName());
+  private Cloud cloud;
+
+  public MMObjectNode preCommit(MMObjectNode node) {
+    CloudProvider cloudProvider = CloudProviderFactory.getCloudProvider();
+    cloud = cloudProvider.getAdminCloud();
+    VersioningController vc = new VersioningController(cloud);
+    Node originalNode = cloud.getNode(node.getNumber());
+    NodeList learnobjects = originalNode.getRelatedNodes("learnblocks");
+    learnobjects = originalNode.getRelatedNodes("learnobjects");
+    for(int i=0;i<learnobjects.size();i++) {
+      vc.addLOVersion(learnobjects.getNode(i));
+      log.info("paragraph.precommit : lo : " + learnobjects.getNode(i).getNumber());
+    }
+    super.preCommit(node);
+    log.info("paragraph.precommit is called for node " + node.getNumber());
+    return node;
+  }
+
+  public boolean commit(MMObjectNode node) {
+    log.info("paragraph.commit is called for node " + node.getNumber());
+    boolean bSuperCommit = super.commit(node);
+    return bSuperCommit;
+  }
+}
