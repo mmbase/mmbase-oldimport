a379f00d601ea93924f40d3d0945dc04889955b7
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2005-09-12 22:11:13 +0000

    Drasticly simplified, javadoced and generalized (to WriterLogger), added @since-tag, removed code-duplicatation
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@14970 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/util/logging/AbstractSimpleImpl.java
+++ src/org/mmbase/util/logging/AbstractSimpleImpl.java
@@ -18,7 +18,7 @@ package org.mmbase.util.logging;
  * @since  MMBase-1.7
  */
 
-abstract public class AbstractSimpleImpl  {
+abstract public class AbstractSimpleImpl  implements Logger {
 
     // could of cause be generalized to an abstract 'getLevel()', but this would
     // nog be too good for performance I think.
@@ -39,6 +39,13 @@ abstract public class AbstractSimpleImpl  {
     /**
      * @since MMBase-1.8
      */
+    protected String message(String level, String message) {
+        return level + " " + message;
+    }
+
+    /**
+     * @since MMBase-1.8
+     */
     protected void log (String s, Throwable t) {
         log(s + "\n"  + Logging.stackTrace(t));
     }
@@ -52,84 +59,84 @@ abstract public class AbstractSimpleImpl  {
 
     public void trace (Object m) {
         if (level <= Level.TRACE_INT) {
-            log("TRACE " + m,  Level.TRACE);
+            log(message("TRACE",  m),  Level.TRACE);
         }
     }
 
     public void trace (Object m, Throwable t) {
         if (level <= Level.TRACE_INT) {
-            log("TRACE " + m,  Level.TRACE, t);
+            log(message("TRACE",  m),  Level.TRACE, t);
         }
     }
 
     public void debug (Object m) {
         if (level <= Level.DEBUG_INT) {
-            log("DEBUG " + m, Level.DEBUG);
+            log(message("DEBUG",  m), Level.DEBUG);
         }
     }
     public void debug (Object m, Throwable t) {
         if (level <= Level.DEBUG_INT) {
-            log("DEBUG " + m, Level.DEBUG, t);
+            log(message("DEBUG", m), Level.DEBUG, t);
         }
     }
 
     public void service (Object m) {
         if (level <= Level.SERVICE_INT) {
-            log("SERVICE " + m, Level.SERVICE);
+            log(message("SERVICE", m), Level.SERVICE);
         }
     }
 
     public void service (Object m, Throwable t) {
         if (level <= Level.SERVICE_INT) {
-            log("SERVICE " + m, Level.SERVICE, t);
+            log(message("SERVICE",  m), Level.SERVICE, t);
         }
     }
 
     public void info    (Object m) {
         if (level <= Level.INFO_INT) {
-            log("INFO " + m, Level.INFO);
+            log(message("INFO", m), Level.INFO);
         }
     }
 
     public void info    (Object m, Throwable t) {
         if (level <= Level.INFO_INT) {
-            log("INFO " + m, Level.INFO, t);
+            log(message("INFO", m), Level.INFO, t);
         }
     }
 
     public void warn    (Object m) {
         if (level <= Level.WARN_INT) {
-            log("WARN " + m, Level.WARN);
+            log(message("WARN", m), Level.WARN);
         }
     }
 
     public void warn    (Object m, Throwable t) {
         if (level <= Level.WARN_INT) {
-            log("WARN " + m, Level.WARN, t);
+            log(message("WARN", m), Level.WARN, t);
         }
     }
 
     public void error   (Object m) {
         if (level <= Level.ERROR_INT) {
-            log("ERROR " + m, Level.ERROR);
+            log(message("ERROR", m), Level.ERROR);
         }
     }
 
     public void error   (Object m, Throwable t) {
         if (level <= Level.ERROR_INT) {
-            log("ERROR " + m, Level.ERROR, t);
+            log(message("ERROR", m), Level.ERROR, t);
         }
     }
 
     public void fatal   (Object m) {
         if (level <= Level.FATAL_INT) {
-            log("FATAL " + m, Level.FATAL);
+            log(message("FATAL", m), Level.FATAL);
         }
     }
 
     public void fatal   (Object m, Throwable t) {
         if (level <= Level.FATAL_INT) {
-            log("FATAL " + m, Level.FATAL, t);
+            log(message("FATAL", m), Level.FATAL, t);
         }
     }
 
---
+++ src/org/mmbase/util/logging/WriterLogger.java
@@ -0,0 +1,46 @@
+/*
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.util.logging;
+import java.io.Writer;
+
+/**
+ * A Logger which writes everything logged to it to a given Writer. The Writer can e.g. be a {@link
+ * java.io.StringWriter} if you want to create one String.
+ *
+ * @author  Michiel Meeuwissen
+ * @version $Id: WriterLogger.java,v 1.1 2005-09-12 22:11:13 michiel Exp $
+ * @since   MMBase-1.8
+ */
+
+public class WriterLogger extends AbstractSimpleImpl {
+
+    private Writer writer;
+
+    public WriterLogger(Writer w) {
+        this(w, Level.INFO);
+    }
+
+    public WriterLogger(Writer w, Level level) {
+        this.level = level.toInt();
+        writer = w;
+    }
+
+    protected final void log(String s) {
+        try {
+            writer.write(s); writer.write('\n');
+        } catch (java.io.IOException ioe) {
+            // should not happen
+        }
+    }
+
+    protected String message(String level, String message) {
+        return "" + message;
+    }
+
+}
