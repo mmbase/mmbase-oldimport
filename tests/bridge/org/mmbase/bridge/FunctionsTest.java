319db7120df5346f2099bd8c289194f50ef7cd16
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2005-10-17 17:29:02 +0000

    Will make tests for functions/bridge too
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@15575 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ tests/bridge/org/mmbase/bridge/FunctionsTest.java
@@ -0,0 +1,30 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+
+package org.mmbase.bridge;
+
+import java.util.*;
+import org.mmbase.util.*;
+import org.mmbase.tests.*;
+
+/**
+ *
+ * @author Michiel Meeuwissen
+ * @since MMBase-1.8
+ */
+public class FunctionsTest extends BridgeTest {
+
+
+    public FunctionsTest(String name) {
+        super(name);
+    }
+
+
+}
