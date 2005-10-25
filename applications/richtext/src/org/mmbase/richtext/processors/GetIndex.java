5e889fa41a418f81260c49ee483dc6151cce76bd
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2005-10-25 12:33:18 +0000

    moved richtext specific processors to here
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@15719 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ applications/richtext/src/org/mmbase/richtext/processors/GetIndex.java
@@ -0,0 +1,127 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.datatypes.processors;
+
+import org.mmbase.bridge.*;
+import org.mmbase.bridge.util.*;
+import org.mmbase.module.core.*;
+import org.mmbase.util.logging.*;
+import java.util.*;
+
+
+/**
+ *
+ * @author Michiel Meeuwissen
+ * @version $Id: GetIndex.java,v 1.1 2005-10-25 12:33:18 michiel Exp $
+ * @since MMBase-1.8
+ */
+
+public class GetIndex implements  Processor {
+    private static final Logger log = Logging.getLoggerInstance(GetIndex.class);
+
+    public static final String CLOUDPROP_INDEXROOT = "org.mmbase.index-root";
+
+    private static final int serialVersionUID = 1;
+
+    private Map cache = Collections.synchronizedMap(new HashMap());
+    private boolean cacheValid = true;
+
+    private String indexField = "index";
+    private String role       = "index";
+    private int depth = 5;
+
+    private MMBaseObserver observer = new MMBaseObserver() {
+
+            public boolean nodeRemoteChanged(String machine, String number, String builder, String ctype) {
+                return nodeChanged(number, builder, ctype);
+            }
+            
+            public boolean nodeLocalChanged(String machine, String number, String builder, String ctype) {
+                return nodeChanged(number, builder, ctype);
+            }
+            
+            protected boolean nodeChanged(String number, String builder, String ctype) {
+                cacheValid = false;
+                GetIndex.this.cache.clear();
+                return true;
+            }
+        };
+    
+    private Object getKey(final Node root, final Node node, final Field field) {
+        return "" + root.getNumber() + "/" + node.getNumber() + "/" + field.getName();
+    }
+
+    private String successor(String i) {
+        if (i == null) return "1";
+        return "" + (Integer.parseInt(i) + 1);
+    }
+
+    private Node findRoot(final Node relation) {
+        Object root = relation.getCloud().getProperty(CLOUDPROP_INDEXROOT);
+        if (root != null) {
+            if (root instanceof Node) {
+                return (Node) root;
+            } else {
+                return relation.getCloud().getNode("" + root);
+            }
+        } else {
+            // not specified, search any.
+            Node n = relation.getNodeValue("snumber");
+            NodeList l = n.getRelatedNodes("object", role, "source");
+            while (l.size() > 0) {
+                n = l.getNode(0);
+                l = n.getRelatedNodes("object", role, "source");
+            }
+            return n;
+        }        
+    }
+
+    private  void fillCache(final Node root, final Node node, final Field field) {
+        // make sure the cache is watched.
+        MMBase mmb = MMBase.getMMBase();        
+        mmb.getBuilder(node.getNodeManager().getName()).addRemoteObserver(observer);
+        mmb.getBuilder(node.getNodeManager().getName()).addLocalObserver(observer);
+
+        log.info("Filling cache, initiated by node " + node.getNumber());
+        Node n = root;
+
+        synchronized(cache) {
+            // 'n' now contains the 'topmost' object.
+            // now iterate down again, while determining all indices.
+            String[] index = new String[depth * 2 + 1];                    
+            GrowingTreeList tree = new GrowingTreeList(Queries.createNodeQuery(n), depth, null, role, "destination");
+            TreeIterator iterator = tree.treeIterator();
+            while (iterator.hasNext()) {
+                n = iterator.nextNode();
+                String i = (String) n.getObjectValue(indexField);
+                if (i == null || i.equals("")) i = successor(index[iterator.currentDepth() - 1]);
+                index[iterator.currentDepth()] = i;
+                cache.put(getKey(root, n, field), i);
+            }
+            log.info("Filled cached " + cache);
+            cacheValid = true;
+        }
+    }
+
+    public Object process(Node node, Field field, Object value) {
+        Node root = findRoot(node);
+        log.info("Found root-node " + root.getNumber());
+        Object key = getKey(root, node, field);
+        Object result = cache.get(key);
+        if (result == null) {
+            fillCache(root, node, field);
+            result = cache.get(key);
+        }
+        log.info("Found index for " + key + " " + result);
+        if (result == null) return "";
+        return result;
+    }
+  
+}
---
+++ applications/richtext/src/org/mmbase/richtext/processors/xml/MmxfGetString.java
@@ -0,0 +1,138 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.richtext.processors.xml;
+import org.mmbase.datatypes.processors.xml.Modes;
+import org.mmbase.datatypes.processors.*;
+import org.mmbase.bridge.*;
+import org.mmbase.bridge.util.xml.Generator;
+import org.mmbase.bridge.Node;
+import org.mmbase.util.logging.*;
+import org.mmbase.util.*;
+import org.mmbase.util.xml.XMLWriter;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.parsers.*;
+
+import java.util.*;
+import java.io.*;
+import org.w3c.dom.*;
+
+
+/**
+ * This class implements the `get' for `mmxf' fields.
+ *
+ * @author Michiel Meeuwissen
+ * @version $Id: MmxfGetString.java,v 1.1 2005-10-25 12:33:18 michiel Exp $
+ * @since MMBase-1.8
+ */
+
+public class MmxfGetString implements  Processor {
+    private static final Logger log = Logging.getLoggerInstance(MmxfGetString.class);
+
+    private static final int serialVersionUID = 1;
+    /**
+     * Returns a 'objects' Document containing the node with the mmxf field plus all idrelated objects
+     */
+    public static Document getDocument(Node node, Field field)  {
+        try {
+           DocumentBuilderFactory dfactory = DocumentBuilderFactory.newInstance();
+           dfactory.setNamespaceAware(true);
+           DocumentBuilder documentBuilder = dfactory.newDocumentBuilder();
+           org.xml.sax.ErrorHandler handler = new XMLErrorHandler();
+           documentBuilder.setErrorHandler(handler);
+           documentBuilder.setEntityResolver( new XMLEntityResolver());
+           Generator generator = new Generator(documentBuilder, node.getCloud());
+           generator.setNamespaceAware(true);
+           generator.add(node, field);
+           generator.add(node.getRelatedNodes("object", "idrel", "destination"));
+           generator.add(node.getRelations("idrel", node.getCloud().getNodeManager("object"), "destination"));
+
+           // TODO. With advent of 'blocks' one deeper level must be queried here (see node.body.jspx)
+           return generator.getDocument();
+        } catch (ParserConfigurationException pce) {
+            throw new RuntimeException(pce.getMessage(), pce);
+        }
+    }
+
+    public Object process(Node node, Field field, Object value) {
+        log.debug("Getting " + field + " from " + node + " as a String");
+        
+        try {
+            switch(Modes.getMode("" + node.getCloud().getProperty(Cloud.PROP_XMLMODE))) {
+            case Modes.KUPU: {
+                // this is actually not really used, at the moment is done on node.body.jspx
+                log.debug("Generating kupu-compatible XML for " + value);
+                Document xml = getDocument(node, field);
+                java.net.URL u = ResourceLoader.getConfigurationRoot().getResource("xslt/mmxf2kupu.xslt");
+                StringWriter res = new StringWriter();
+                // TODO: XSL transformation parameter stuff must be generalized (not only cloud, but only e.g. request specific stuff must be dealt with).
+                Map params = new HashMap();
+                params.put("cloud", node.getCloud());
+                XSLTransformer.transform(new DOMSource(xml), u, new StreamResult(res), params);
+                return res.toString();
+            }
+            case Modes.WIKI: {
+                log.debug("Generating 'wiki'  for " + value);
+                Document xml = getDocument(node, field);
+                java.net.URL u = ResourceLoader.getConfigurationRoot().getResource("xslt/2rich.xslt");
+                StringWriter res = new StringWriter();
+                Map params = new HashMap();
+                params.put("cloud", node.getCloud());
+                XSLTransformer.transform(new DOMSource(xml), u, new StreamResult(res), params);
+                return res.toString();
+            }
+            case Modes.FLAT: {
+                log.debug("Generating 'flat' for " + value);
+                Document xml = getDocument(node, field);
+                log.debug("Generating 'flat' for " + XMLWriter.write(xml, true, true));
+                java.net.URL u = ResourceLoader.getConfigurationRoot().getResource("xslt/mmxf2rich.xslt");
+                StringWriter res = new StringWriter();
+                Map params = new HashMap();
+                params.put("cloud", node.getCloud());
+                XSLTransformer.transform(new DOMSource(xml), u, new StreamResult(res), params);
+                return res.toString();
+            }
+            case Modes.PRETTYXML: {
+                // get the XML from this thing....
+                // javax.xml.parsers.DocumentBuilderFactory dfactory = javax.xml.parsers.DocumentBuilderFactory.newInstance();
+                // javax.xml.parsers.DocumentBuilder dBuilder = dfactory.newDocumentBuilder();
+                // org.w3c.dom.Element xml = node.getXMLValue(field.getName(), dBuilder.newDocument());
+                Document xml = node.getXMLValue(field.getName());
+                
+                if(xml != null) {
+                    return XMLWriter.write(xml, true, true);
+                }
+                return "";
+                
+            }                
+            case Modes.DOCBOOK:
+                // not implemented..
+                return value;
+            case Modes.XML:
+            default:
+                Document xml = node.getXMLValue(field.getName());
+                
+                if(xml != null) {
+                    // make a string from the XML
+                    return XMLWriter.write(xml, false, true);
+                }
+                return "";
+            }          
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            return value;
+        }
+    }
+
+    public String toString() {
+        return "get_MMXF";
+    }
+  
+}
