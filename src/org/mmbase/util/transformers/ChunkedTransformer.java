9739a7fd2f99b6147fcb574737def27c2a7f3035
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2005-10-31 07:58:59 +0000

    generilized 'chunk' functionality of regexp replacer (now also used by ResourceBundleReplacer)
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@15849 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/util/transformers/ChunkedTransformer.java
@@ -0,0 +1,294 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.util.transformers;
+
+import java.util.*;
+import java.io.*;
+import java.util.regex.*;
+import org.mmbase.util.ResourceWatcher;
+import org.mmbase.util.xml.UtilReader;
+import org.mmbase.util.Entry;
+
+import org.mmbase.util.logging.*;
+
+
+/**
+ * A chunked transformer is a transformer that transforms on a 'chunk by chunk' base. A chunck is
+ * typically a word or a line or so. The type of the 'chunks' is controled by the 'mode' parameter.
+ *
+ * It can ignored existing XML markup (the 'XMLTEXT' modes), and also avoids trailing dots and
+ * comments and surrounding quotes and parentheses.
+ *
+ * @author Michiel Meeuwissen
+ * @since MMBase-1.8
+ */
+
+public abstract class ChunkedTransformer extends ConfigurableReaderTransformer implements CharTransformer {
+    private static final Logger log = Logging.getLoggerInstance(ChunkedTransformer.class);
+
+    /**
+     * Match word by word, but only in PCDATA of xml elements.
+     */
+    public final static int XMLTEXT_WORDS     = 1;
+
+    /**
+     * Match in PCDATA of xml elements.
+     */
+    public final static int XMLTEXT     = 2;
+
+    /**
+     * Match word by word.
+     */
+    public final static int WORDS    = 3;
+
+    /**
+     * Match line by line.
+     */
+    public final static int LINES    = 4;
+
+
+    /**
+     * Match the entire stream (so, one String must be created).
+     */
+    public final static int ENTIRE    = 5;
+
+
+
+    protected ChunkedTransformer(int i) {
+        super(i);
+    }
+
+    public ChunkedTransformer() {
+        super(WORDS);
+    }
+
+    protected abstract boolean replace(String string, Writer w) throws IOException;
+
+    protected boolean replaceWord(StringBuffer word, Writer writer) throws IOException {
+        int l = word.length();
+        StringBuffer postFix = null;
+        String w;
+        if (l > 0) {
+
+            postFix = new StringBuffer();
+
+            // surrounding quotes might look like &quot; because of earlier escaping, so we take those out of consideration.
+            w = word.toString();
+            while (w.endsWith("&quot;")) {
+                postFix.insert(0, "&quot;");
+                l -= 6;
+                word.setLength(l);
+                w = word.toString();
+            }
+            if (l > 0) {
+
+                // to allow for . , and like in the end, we tear those of.
+                char d = word.charAt(l - 1);
+                while (! Character.isLetterOrDigit(d)) {
+                    postFix.insert(0, d);
+                    word.setLength(--l);
+                    if (l == 0) break;
+                    d = word.charAt(l - 1);
+                }
+            }
+        }
+
+        w = word.toString();
+
+        // stuff in the beginning:
+        while(w.startsWith("&quot;")) {
+            writer.write("&quot;");
+            word.delete(0, 6);
+            l -= 6;
+            w = word.toString();
+        }
+
+        // ready to make the replacements now.
+        boolean result = replace(w, writer);
+
+        if (postFix != null) {
+            writer.write(postFix.toString());
+        }
+        return result;
+    }
+
+    public Writer transformXmlTextWords(Reader r, Writer w)  {
+        int replaced = 0;
+        StringBuffer word = new StringBuffer();  // current word
+        boolean translating = true;
+        try {
+            log.trace("Starting regexp replacing");
+            while (true) {
+                int c = r.read();
+                if (c == -1) break;
+                if (c == '<') {  // don't do it in existing tags and attributes
+                    translating = false;
+                    if (replaceWord(word, w)) replaced++;
+                    w.write(c);
+                } else if (c == '>') {
+                    translating = true;
+                    word.setLength(0);
+                    w.write(c);
+                } else if (! translating) {
+                    w.write(c);
+                } else {
+                    if (Character.isWhitespace((char) c) || c == '\'' || c == '\"' || c == '(' || c == ')' ) {
+                        if (replaceWord(word, w)) replaced++;
+                        word.setLength(0);
+                        w.write(c);
+                    } else {
+                        word.append((char) c);
+                    }
+                }
+            }
+            // write last word
+            if (translating && replaceWord(word, w)) replaced++;
+            log.debug("Finished regexp replacing. Replaced " + replaced + " words");
+        } catch (java.io.IOException e) {
+            log.error(e.toString());
+        }
+        return w;
+    }
+
+    public Writer transformXmlText(Reader r, Writer w)  {
+        int replaced = 0;
+        StringBuffer xmltext = new StringBuffer();  // current word
+        boolean translating = true;
+        try {
+            log.trace("Starting regexp replacing");
+            while (true) {
+                int c = r.read();
+                if (c == -1) break;
+                // perhaps better use SAX to decently detect XML, but then it probably won't work
+                // very well on sloppy XML (like HTML).
+                if (c == '<') {  // don't do it in existing tags and attributes
+                    translating = false;
+                    if (replace(xmltext.toString(), w)) replaced++;
+                    w.write(c);
+                } else if (c == '>') {
+                    translating = true;
+                    xmltext.setLength(0);
+                    w.write(c);
+                } else if (! translating) {
+                    w.write(c);
+                } else {
+                    xmltext.append((char) c);
+                }
+            }
+            // write last word
+            if (translating && replace(xmltext.toString(), w)) replaced++;
+            log.debug("Finished regexp replacing. Replaced " + replaced + " words");
+        } catch (java.io.IOException e) {
+            log.error(e.toString());
+        }
+        return w;
+    }
+    public Writer transformWords(Reader r, Writer w)  {
+        int replaced = 0;
+        StringBuffer word = new StringBuffer();  // current word
+        try {
+            log.trace("Starting regexp replacing");
+            while (true) {
+                int c = r.read();
+                if (c == -1) break;
+                if (Character.isWhitespace((char) c) || c == '\'' || c == '\"' || c == '(' || c == ')' || c == '<' || c == '>' ) {
+                    if (replaceWord(word, w)) replaced++;
+                    word.setLength(0);
+                    w.write(c);
+                } else {
+                    word.append((char) c);
+                }
+            }
+            // write last word
+            if (replaceWord(word, w)) replaced++;
+            log.debug("Finished regexp replacing. Replaced " + replaced + " words");
+        } catch (java.io.IOException e) {
+            log.error(e.toString());
+        }
+        return w;
+    }
+
+
+
+    public Writer transformLines(Reader r, Writer w) {
+        BufferedReader reader = new BufferedReader(r);
+        try {
+            String line = reader.readLine();
+            while (line != null) {
+                replace(line, w);
+                line = reader.readLine();
+            }
+        } catch (java.io.IOException e) {
+            log.error(e.toString());
+        }
+        return w;
+    }
+
+    public Writer transformEntire(Reader r, Writer w) {
+        StringWriter sw = new StringWriter();
+        try {
+            while (true) {
+                int c = r.read();
+                if (c == -1) break;
+                sw.write(c);
+            }
+            replace(sw.toString(), w);
+        } catch (java.io.IOException e) {
+            log.error(e.toString());
+        }
+
+        return w;
+    }
+
+
+    public Writer transform(Reader r, Writer w) {
+        switch(to) {
+        case XMLTEXT_WORDS: return transformXmlTextWords(r, w);
+        case XMLTEXT:       return transformXmlText(r, w);
+        case WORDS:         return transformWords(r, w);
+        case LINES:         return transformLines(r, w);
+        case ENTIRE:        return transformEntire(r, w);
+        default: throw new UnknownCodingException(getClass(), to);
+        }
+    }
+
+    abstract protected String base();
+
+    public String getEncoding() {
+        switch (to) {
+        case XMLTEXT_WORDS:
+            return base() + "_XMLTEXT_WORDS";
+        case XMLTEXT:
+            return base() + "_XMLTEXT";
+        case WORDS:
+            return base() + "_WORDS";
+        case LINES:
+            return base() + "_LINES";
+        case ENTIRE:
+            return base() + "_ENTIRE";
+        default :
+            throw new UnknownCodingException(getClass(), to);
+        }
+    }
+
+    public Map transformers() {
+        Map h = new HashMap();
+        h.put(base() + "_XMLTEXT_WORDS",  new Config(RegexpReplacer.class, XMLTEXT_WORDS,  "Search and replaces regexps word-by-word, only in XML text() blocks."));
+        h.put(base() + "_XMLTEXT",        new Config(RegexpReplacer.class, XMLTEXT,  "Search and replaces regexps, only in XML text() blocks."));
+        h.put(base() + "_WORDS",          new Config(RegexpReplacer.class, WORDS,  "Search and replaces regexps word-by-word"));
+        h.put(base() + "_LINES",          new Config(RegexpReplacer.class, LINES,  "Search and replaces regexps, line-by-line"));
+        h.put(base() + "_ENTIRE",         new Config(RegexpReplacer.class, ENTIRE,  "Search and replaces regexps"));
+
+        return Collections.unmodifiableMap(h);
+    }
+
+
+
+}
--- src/org/mmbase/util/transformers/ResourceBundleReplacerFactory.java
+++ src/org/mmbase/util/transformers/ResourceBundleReplacerFactory.java
@@ -17,13 +17,14 @@ import org.mmbase.util.functions.*;
 
 
 /**
- * Factories new chartransformer base on {@link java.util.ResourceBundler}. Search for words (which
- * must consist of letters and digits) and searches that in the resource-bundle, and if found,
- * replaces with the value.
+ * Factories new chartransformer based on {@link java.util.ResourceBundle}. The result transformers
+ * are based on {@link ChunkedTransformer}, so using the 'mode' argument it can be controlled what
+ * excactly must correspond to re resource bundle keys (e.g. words, lines, or the entire
+ * string). Defaults to words.
  *
  * @author Michiel Meeuwissen
  * @since MMBase-1.8
- * @version $Id: ResourceBundleReplacerFactory.java,v 1.1 2005-10-26 21:58:34 michiel Exp $
+ * @version $Id: ResourceBundleReplacerFactory.java,v 1.2 2005-10-31 07:58:59 michiel Exp $
  */
 
 public class ResourceBundleReplacerFactory implements ParameterizedTransformerFactory {
@@ -32,6 +33,7 @@ public class ResourceBundleReplacerFactory implements ParameterizedTransformerFa
 
     protected static final Parameter[] PARAMS = new Parameter[] {
         new Parameter("basename", String.class, true),
+        new Parameter("mode", String.class),
         Parameter.LOCALE
     };
 
@@ -47,47 +49,31 @@ public class ResourceBundleReplacerFactory implements ParameterizedTransformerFa
         if (log.isDebugEnabled()) {
             log.debug("Creating transformer, with " + parameters);
         }
-        final ResourceBundle bundle = ResourceBundle.getBundle((String) parameters.get("basename"), (Locale) parameters.get(Parameter.LOCALE));
-        return new ReaderTransformer() {
-                public boolean replaceWord(String word, Writer w) throws IOException  {
-                    try {
-                        w.write("" + bundle.getObject(word));
-                        return true;
-                    } catch (MissingResourceException mre) {
-                        w.write(word);
-                        return false;
-                    }
-                    
-                }
-                public Writer transform(Reader r, Writer w)  {
-                    int replaced = 0;
-                    StringBuffer word = new StringBuffer();  // current word
-                    try {
-                        log.trace("Starting regexp replacing");
-                        while (true) {
-                            int c = r.read();
-                            if (c == -1) break;
-                            if (! Character.isLetterOrDigit((char) c)) {
-                                if (replaceWord(word.toString(), w)) replaced++;
-                                word.setLength(0);
-                                w.write(c);
-                            } else {
-                                word.append((char) c);
-                            }
-                        }
-                        // write last word
-                        if (replaceWord(word.toString(), w)) replaced++;
-                        log.debug("Finished regexp replacing. Replaced " + replaced + " words");
-                    } catch (java.io.IOException e) {
-                        log.error(e.toString());
-                    }
-                    return w;
-                }
-            };
+        String baseName = (String) parameters.get("basename");
+        final ResourceBundle bundle = ResourceBundle.getBundle(baseName, (Locale) parameters.get(Parameter.LOCALE));
+        return new ResourceBundleReplacer(1, bundle, baseName);
     }
-    
-    
-
-
 
 }
+
+class ResourceBundleReplacer extends ChunkedTransformer {
+    private final ResourceBundle bundle;
+    private final String name;
+    ResourceBundleReplacer(int mode, ResourceBundle bundle, String name) {
+        super(mode);
+        this.bundle = bundle;
+        this.name = name;
+    }
+    protected boolean replace(String word, Writer w) throws IOException  {
+        try {
+            w.write("" + bundle.getObject(word));
+            return true;
+        } catch (MissingResourceException mre) {
+            w.write(word);
+            return false;
+        }
+    }
+    protected String base() {
+        return name;
+    }
+};
