260647e8c7e2004f804217e8b9336e2649251a17
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2005-11-01 23:41:02 +0000

    adde utility to connect SimpleDateFormat to Calendar constants
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@15915 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/datatypes/DateTimePattern.java
+++ src/org/mmbase/datatypes/DateTimePattern.java
@@ -13,6 +13,8 @@ package org.mmbase.datatypes;
 import java.text.*;
 import java.util.*;
 import org.mmbase.util.LocalizedString;
+import org.mmbase.util.logging.Logging;
+import org.mmbase.util.logging.Logger;
 
 /**
  * This is a bit like SimpleDateFormat, because it accepts the same pattern String.  It can also
@@ -23,11 +25,11 @@ import org.mmbase.util.LocalizedString;
  *
  * @author Michiel Meeuwissen
  * @since  MMBase-1.8
- * @version $Id: DateTimePattern.java,v 1.5 2005-10-25 08:33:58 michiel Exp $
+ * @version $Id: DateTimePattern.java,v 1.6 2005-11-01 23:41:02 michiel Exp $
  */
 
 public class DateTimePattern implements Cloneable, java.io.Serializable {
-
+    private static final Logger log = Logging.getLoggerInstance(DateTimePattern.class);
 
     public static final DateTimePattern DEFAULT = new DateTimePattern("yyyy-MM-dd HH:mm:ss");
     protected  LocalizedString pattern;
@@ -138,6 +140,171 @@ public class DateTimePattern implements Cloneable, java.io.Serializable {
         return parse(p);
     }
 
+    private static final Element HOUR_OF_DAY   = new Element("hour", Calendar.HOUR_OF_DAY, 0, 23);
+    private static final Element HOUR          = new Element("hourinampm", Calendar.HOUR, 0, 11);
+    private static final Element HOUR_OF_DAY_1 = new Element("hour", Calendar.HOUR_OF_DAY, 1, 24, 1);
+    private static final Element HOUR_1        = new Element("hour", Calendar.HOUR, 1, 12, 1);
+    private static final Element MINUTE        = new Element("minute", Calendar.MINUTE, 0, 59);
+    private static final Element SECOND        = new Element("second", Calendar.SECOND, 0, 59);
+    private static final Element DAY_OF_WEEK   = new Element("dayofweek", Calendar.DAY_OF_WEEK, 1, 7) {
+            public String toString(int value, Locale locale, int length) {
+                SimpleDateFormat format = new SimpleDateFormat("EEEEEEEEEEEEE".substring(0, length), locale);
+                Calendar help = Calendar.getInstance();
+                help.set(field, value);
+                return format.format(help.getTime());
+            }
+        };
+    private static final Element WEEK_OF_YEAR  = new Element("weekinyear", Calendar.WEEK_OF_YEAR, 1, 53);
+    private static final Element DAY_OF_YEAR   = new Element("dayinyear", Calendar.DAY_OF_YEAR, 1, 366);
+    private static final Element DAY_OF_WEEK_IN_MONTH  = new Element("dayofweekinmonth", Calendar.DAY_OF_WEEK_IN_MONTH, 1, 5) {
+            public String toString(int value, Locale locale, int length) {
+                if (length > 0) {
+                    ResourceBundle bundle = ResourceBundle.getBundle("org.mmbase.datatypes.resources.ordinals", locale);
+                    return bundle.getString("" + value);
+                } else {
+                    return super.toString(value, locale, length);
+                }
+            }
+        };
+    private static final Element ERA           = new Element("era", Calendar.ERA, 0, //BC
+                                                             1 //AD
+                                                             ) {
+            public String toString(int value, Locale locale, int length) {
+                SimpleDateFormat format = new SimpleDateFormat("GGGGGGGGGGGG".substring(0, length), locale);
+                Calendar help = Calendar.getInstance();
+                help.set(field, value);
+                return format.format(help.getTime());
+            }
+            
+        };
+    private static final Element AM_PM         = new Element("am_pm", Calendar.AM_PM, 0, //AM
+                                                             1 //PM
+                                                             ) {
+            public String toString(int value, Locale locale, int length) {
+                SimpleDateFormat format = new SimpleDateFormat("aaaaaaaaaaaaaa".substring(0, length), locale);
+                Calendar help = Calendar.getInstance();
+                help.set(field, value);
+                return format.format(help.getTime());
+            }
+        };
+    private static final Element MILLISECOND   = new Element("millisecond", Calendar.MILLISECOND, 0, 9999);
+
+    public static Element getElement(char c, Calendar minDate, Calendar maxDate) {
+        switch(c) {
+        case 'y': {
+            int startYear = minDate.get(Calendar.YEAR);
+            int   endYear = maxDate.get(Calendar.YEAR);
+            return new Element("year",  Calendar.YEAR,  startYear, endYear) {
+                    public int getNullValue() {
+                        return Integer.MAX_VALUE;
+                    }
+                };
+        }
+        case 'M': {
+            int startYear = minDate.get(Calendar.YEAR);
+            int   endYear = maxDate.get(Calendar.YEAR);
+            int min, max;
+            if (startYear == endYear) {
+                min = minDate.get(Calendar.MONTH) + 1;
+                max = maxDate.get(Calendar.MONTH) + 1;
+            } else {
+                min = 1;
+                max = 12;
+            }
+            return new Element("month", Calendar.MONTH, min, max, 1) {
+                    public String toString(int value, Locale locale, int length) {
+                        SimpleDateFormat format = new SimpleDateFormat("MMMMMMMMMMMMMMMMMM".substring(0, length), locale);
+                        Calendar help = Calendar.getInstance();
+                        help.set(field, value);
+                        return format.format(help.getTime());
+                    }
+                };
+        }
+        case 'd': {
+            int startYear = minDate.get(Calendar.YEAR);
+            int   endYear = maxDate.get(Calendar.YEAR);
+            int min, max;
+            if (startYear == endYear) {
+                int minMonth = minDate.get(Calendar.MONTH) + 1;
+                int maxMonth = maxDate.get(Calendar.MONTH) + 1;
+                if (minMonth == maxMonth) {
+                    min = minDate.get(Calendar.DAY_OF_MONTH);
+                    max = maxDate.get(Calendar.DAY_OF_MONTH);
+                } else {
+                    min = 1;
+                    max = 31;
+                }
+            } else {
+                min = 1;
+                max = 31;
+            }
+            return new Element("day", Calendar.DAY_OF_MONTH, min, max);            
+        }
+        case 'H': return HOUR_OF_DAY;
+        case 'K': return HOUR;
+        case 'h': return HOUR_OF_DAY_1;
+        case 'k': return HOUR_1;
+        case 'm': return MINUTE;
+        case 's': return SECOND;
+        case 'E': return DAY_OF_WEEK;
+        case 'w': return WEEK_OF_YEAR;
+        case 'D': return DAY_OF_YEAR;
+        case 'F': return DAY_OF_WEEK_IN_MONTH;
+        case 'G': return ERA;
+        case 'a': return AM_PM;
+        case 'S': return MILLISECOND;
+        default: 
+            log.warn("Unknown pattern " + c);
+            return null;
+        }
+    }
+
+    public static class Element {
+        private final String name;
+        final int field;
+        private final int min;
+        private final int max;
+        private final int offset;
+        Element(String n, int field, int min, int max) {
+            this(n, field, min, max, 0);
+        }
+        Element(String n, int field, int min, int max, int offset) {
+            name = n;
+            this.field = field;
+            this.min = min;
+            this.max = max;
+            this.offset = offset;
+        }
+        public final String getName() {
+            return name;
+        }
+        public final int getField() {
+            return field;
+        }
+        public final int getMin() {
+            return min;
+        }
+        public final int getMax() {
+            return max;
+        }
+        public final int getOffset() {
+            return offset;
+        }
+        public String toString(int value, Locale locale, int length) {
+            StringBuffer buf = new StringBuffer("" + value);
+            while(buf.length() < length) {
+                buf.insert(0, "0");
+            }
+            return buf.toString();
+        }
+        /**
+         * The int-value representing <code>null</code>. Some otherwise impossible value for the field.
+         */
+        public int getNullValue() {
+            return -1;
+        }
+    }
+     
 
     public Object clone() {
         try {
