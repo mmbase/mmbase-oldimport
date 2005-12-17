857af677411f62ef684abd61923c8fc0019ebcb1
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2005-12-17 20:22:40 +0000

    password generator as a module (no idea if it is used..)
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@16573 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ applications/scan/src/org/mmbase/util/PasswordGeneratorModule.java
@@ -0,0 +1,73 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.util;
+import org.mmbase.module.Module;
+
+import org.mmbase.util.logging.Logger;
+import org.mmbase.util.logging.Logging;
+
+/**
+
+ */
+public class PasswordGeneratorModule extends Module {
+
+    // logger
+    private static Logger log = Logging.getLoggerInstance(PasswordGeneratorModule.class);
+
+    private final PasswordGenerator pw;
+
+    public PasswordGeneratorModule() {
+        pw = new PasswordGenerator();
+    }
+
+    /**
+     * Called when the module is loaded.
+     * Not used.
+     */
+    public void onload() {
+    }
+
+    /**
+     * Called when the module is reloaded.
+     * Tries to retrieve a default template for a password from the
+     * template property from the module configuration file.
+     * Not used.
+     */
+    public void reload() {
+        pw.defaulttemplate = getInitParameter("template");
+        if (pw.defaulttemplate == null) pw.defaulttemplate = "SSSSSS";
+    }
+
+    /**
+     * Initializes the module.
+     * Tries to retrieve a default template for a password from the
+     * template property from the module configuration file.
+     */
+    public void init() {
+        pw.defaulttemplate = getInitParameter("template");
+        if (pw.defaulttemplate == null) pw.defaulttemplate = "SSSSSS";
+    }
+
+    /**
+     * Called when the module is unloaded.
+     * Not used.
+     */
+    public void unload() {
+    }
+
+    /**
+     * Called when the module is shut down (removed).
+     * Not used.
+     */
+    public void shutdown() {
+    }
+
+
+}
