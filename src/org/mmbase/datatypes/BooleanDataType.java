f564523c797e1381b6a04afcdd1f72e444ae6952
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2005-12-23 12:30:37 +0000

    cast more conservatively than Casting, because other-wise every invalid value is simply considered 'false', which is not very helpful
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@16716 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/datatypes/BooleanDataType.java
+++ src/org/mmbase/datatypes/BooleanDataType.java
@@ -8,16 +8,15 @@ See http://www.MMBase.org/license
 
 */
 package org.mmbase.datatypes;
-
+import org.mmbase.bridge.*;
 /**
  * The DataType associated with a boolean values. 
  *
  * @author Pierre van Rooden
- * @version $Id: BooleanDataType.java,v 1.4 2005-10-27 17:12:19 simon Exp $
+ * @version $Id: BooleanDataType.java,v 1.5 2005-12-23 12:30:37 michiel Exp $
  * @since MMBase-1.8
  */
 public class BooleanDataType extends BasicDataType {
-
     /**
      * Constructor for boolean field that takes a class as argument, this
      * should be the 'true' class: either boolean.class or Boolean.class
@@ -29,4 +28,28 @@ public class BooleanDataType extends BasicDataType {
         super(name, primitive ? Boolean.TYPE : Boolean.class);
     }
 
+
+    /**
+     * Cast a bit more conservativly, because Casting, aggressively casts everthing to boolean,
+     * which would make nearly every value valid.
+     */
+    protected final Object cast(Object value, Cloud cloud, Node node, Field field) {
+        Object preCast = preCast(value, cloud, node, field);
+        if (preCast == null) return null;
+        if (value instanceof Boolean) return value;
+        if (value instanceof String) {
+            if ("".equals(value)) return null;
+            if ("true".equals(value)) return Boolean.TRUE;
+            if ("false".equals(value)) return Boolean.FALSE;
+            throw new RuntimeException("'" + value + "' cannot be casted to boolean");
+        }
+        if (value instanceof Number) {
+            double d = ((Number) value).doubleValue();
+            if (d == 1.0) return Boolean.TRUE;
+            if (d == 0.0) return Boolean.FALSE;
+            throw new RuntimeException("The number '" + value + "' cannot be casted to boolean (boolean is 0 or 1)");
+        } 
+        throw new RuntimeException("'" + value + "' cannot be casted to boolean (boolean is 0 or 1)");
+
+    }
 }
