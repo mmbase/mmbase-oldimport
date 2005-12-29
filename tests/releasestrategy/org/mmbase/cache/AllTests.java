8bfd204cbd3de6f09d42a1574e882edd076a09ce
ernst <ernst@91430c5f-d668-0410-9e7d-dd663366dcfd>
2005-12-29 14:24:26 +0000

    added class for all strategy tests
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@16791 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ tests/releasestrategy/org/mmbase/cache/AllTests.java
@@ -0,0 +1,43 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+
+package org.mmbase.cache;
+
+import org.mmbase.tests.*;
+
+import junit.framework.*;
+
+/**
+ * TestSuite that runs all the release strategy tests.
+ *
+ * @author Ernst Bunders
+ */
+public class AllTests {
+    
+    public static void main(String[] args) {
+        try { 
+            junit.textui.TestRunner.run(suite());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+
+    public static Test suite() throws Exception {
+        //MMBaseTest.startMMBase();
+
+        // Create the test suite
+        TestSuite suite= new TestSuite("Strategy Tests");
+        suite.addTestSuite(ConstraintMatcherTest.class);
+        suite.addTestSuite(ReleaseStrategyTest.class);
+        return suite;
+    }
+	
+}
