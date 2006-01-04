cd2e00f9328770c514f2d7288259c1eb65678149
jverelst <jverelst@91430c5f-d668-0410-9e7d-dd663366dcfd>
2006-01-04 17:02:25 +0000

    implemented a 'preDelete()' handler that cleans up if the user is removed, and a install() method that can create addressbooks for existing users
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@16976 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/didactor2/src/address/java/nl/didactor/component/address/DidactorAddress.java
+++ contributions/didactor2/src/address/java/nl/didactor/component/address/DidactorAddress.java
@@ -5,10 +5,13 @@ package nl.didactor.component.address;
 import nl.didactor.component.Component;
 import nl.didactor.builders.DidactorBuilder;
 import nl.didactor.component.core.*;
-import org.mmbase.bridge.Cloud;
 import org.mmbase.module.core.*;
 import org.mmbase.module.corebuilders.*;
-import java.util.Map;
+import org.mmbase.storage.search.RelationStep;
+import org.mmbase.storage.search.SearchQueryException;
+import org.mmbase.storage.search.implementation.NodeSearchQuery;
+import java.util.List;
+import java.util.Vector;
 
 public class DidactorAddress extends Component {
     /**
@@ -39,15 +42,40 @@ public class DidactorAddress extends Component {
         MMBase mmbase = MMBase.getMMBase();
         DidactorBuilder people = (DidactorBuilder)mmbase.getBuilder("people");
         people.registerPostInsertComponent(this, 10);
+        people.registerPreDeleteComponent(this, 10);
     }
+
+    public void install() {
+        MMBase mmbase = MMBase.getMMBase();
+        DidactorBuilder people = (DidactorBuilder)mmbase.getBuilder("people");
+        try {
+            List nodes = people.getNodes(new NodeSearchQuery(people));
+            for (int i=0; i<nodes.size(); i++) {
+                postInsert((MMObjectNode)nodes.get(i));
+            }
+        } catch (SearchQueryException e) {
+        }
+    }
+
 
     /**
      * This method is called when a new object is added to Didactor. If the component
      * needs to insert objects for this object, it can do so. 
      */
     public boolean postInsert(MMObjectNode node) {
-        if (node.getBuilder().getTableName().equals("people"))
+        if (node.getBuilder().getTableName().equals("people")) {
             return createUser(node);
+        }
+        return true;
+    }
+
+    /**
+     * This method is called just before an object is removed from MMBase.
+     */
+    public boolean preDelete(MMObjectNode node) {
+        if (node.getBuilder().getTableName().equals("people")) {
+            return deleteUser(node);
+        }
         return true;
     }
 
@@ -71,6 +99,29 @@ public class DidactorAddress extends Component {
         relation.setValue("rnumber", related);
         insrel.insert(username, relation);
        
+        return true;
+    }
+
+    /**
+     * Delete the addressbook of the user.
+     */
+    private boolean deleteUser(MMObjectNode user) {
+        Vector addressbooks = user.getRelatedNodes("addressbooks", "related", RelationStep.DIRECTIONS_DESTINATION);
+
+        // Iterate the addressbooks, to remove them all
+        for (int i=0; i<addressbooks.size(); i++) {
+            MMObjectNode book = (MMObjectNode)addressbooks.get(i);
+            Vector contacts = book.getRelatedNodes("contacts", "related", RelationStep.DIRECTIONS_DESTINATION);
+
+            // Iterate the contacts, to remove them all
+            for (int j=0; j<contacts.size(); j++) {
+                MMObjectNode contact = (MMObjectNode)contacts.get(j);
+                contact.getBuilder().removeRelations(contact);
+                contact.getBuilder().removeNode(contact);
+            }
+            book.getBuilder().removeRelations(book);
+            book.getBuilder().removeNode(book);
+        }
         return true;
     }
 }
