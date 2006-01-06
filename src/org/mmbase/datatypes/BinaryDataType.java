6a3a43eaa8edc9e23647d183f0dfcefce6900784
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2006-01-06 14:21:50 +0000

    Added a property which will make it possible to recognize a datatype as e.g. 'an image'.
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@17021 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/datatypes/BinaryDataType.java
+++ src/org/mmbase/datatypes/BinaryDataType.java
@@ -10,17 +10,20 @@ See http://www.MMBase.org/license
 package org.mmbase.datatypes;
 
 import org.mmbase.util.logging.*;
+import java.util.regex.Pattern;
 
 /**
  * The datatype associated with byte arrays ('blobs').
  *
  * @author Pierre van Rooden
- * @version $Id: BinaryDataType.java,v 1.5 2005-10-21 10:20:28 michiel Exp $
+ * @version $Id: BinaryDataType.java,v 1.6 2006-01-06 14:21:50 michiel Exp $
  * @since MMBase-1.8
  */
 public class BinaryDataType extends AbstractLengthDataType {
 
     private static final Logger log = Logging.getLoggerInstance(BinaryDataType.class);
+
+    Pattern validMimeTypes = Pattern.compile(".*");
     /**
      * Constructor for binary field.
      * @param name the name of the data type
@@ -30,7 +33,7 @@ public class BinaryDataType extends AbstractLengthDataType {
     }
 
 
-
+    // 
     public long getLength(Object value) {
         if (! (value instanceof byte[])) {
             throw new RuntimeException("Value " + value + " of " + getName() + " is not a byte array but" + (value == null ? "null" : value.getClass().getName()));
@@ -49,6 +52,16 @@ public class BinaryDataType extends AbstractLengthDataType {
         }
         return bytes.length;
     }
-
+    /**
+     * Returns a regular expression which describes wich mime-types are valid for blobs with this
+     * DataType. This is not yet available as a Restriction, only as a property.
+     */
+    public Pattern getValidMimeTypes() {
+        return validMimeTypes;
+    }
+    
+    public void setValidMimeTypes(Pattern pattern) {
+        validMimeTypes = pattern;
+    }
 
 }
