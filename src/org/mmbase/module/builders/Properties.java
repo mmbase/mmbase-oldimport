393c2ff381cbdfa526d7790281b69c1be5255ab6
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2006-01-24 10:43:26 +0000

    code-conventions, marked that javadoc is missng, because I think it is not evident where this thing good for. At least I don't quite get it.
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@17303 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/builders/Properties.java
+++ src/org/mmbase/module/builders/Properties.java
@@ -14,18 +14,22 @@ import org.mmbase.module.core.*;
 import org.mmbase.util.logging.*;
 
 /**
- * @version $Id: Properties.java,v 1.13 2005-11-23 15:45:13 pierre Exp $
+ * Probably your builder must extend this if 'properties' on its nodes must work decently?
+ *
+ * @javadoc
+ *
+ * @version $Id: Properties.java,v 1.14 2006-01-24 10:43:26 michiel Exp $
  */
 public class Properties extends MMObjectBuilder {
 
-    private static Logger log = Logging.getLoggerInstance(Properties.class.getName());
+    private static final Logger log = Logging.getLoggerInstance(Properties.class);
 
     public String getGUIIndicator(MMObjectNode node) {
-        String str=node.getStringValue("key");
-        if (str.length()>15) {
-            return(str.substring(0,12)+"...");
+        String str = node.getStringValue("key");
+        if (str.length() > 15) {
+            return str.substring(0, 12) + "...";
         } else {
-            return(str);
+            return str;
         }
     }
 
@@ -34,17 +38,19 @@ public class Properties extends MMObjectBuilder {
      */
     public void notify(NodeEvent event) {
         if (event.getBuilderName().equals(this.getTableName())) {
-            log.debug("nodeChanged(): Property change ! "+ event.getMachine() + " " + event.getNodeNumber() +
-                " " + event.getBuilderName() + " "+ NodeEvent.newTypeToOldType(event.getType()));
-                if (event.getType() == NodeEvent.EVENT_TYPE_CHANGED || event.getType() == NodeEvent.EVENT_TYPE_NEW ) {
+            if (log.isDebugEnabled()) {
+                log.debug("nodeChanged(): Property change ! "+ event.getMachine() + " " + event.getNodeNumber() +
+                          " " + event.getBuilderName() + " "+ NodeEvent.newTypeToOldType(event.getType()));
+            }
+            if (event.getType() == NodeEvent.EVENT_TYPE_CHANGED || event.getType() == NodeEvent.EVENT_TYPE_NEW ) {
                 // The passed node number is node of prop node
-
-                    int parent = getNode(event.getNodeNumber()).getIntValue("parent");
-                    if (isNodeCached(new Integer(parent))) {
-                        log.debug("nodeChanged(): Zapping node properties cache for "+parent);
-                        MMObjectNode pnode = getNode(parent);
-                        if (pnode!=null) pnode.delPropertiesCache();
-                    }
+                
+                int parent = getNode(event.getNodeNumber()).getIntValue("parent");
+                if (isNodeCached(new Integer(parent))) {
+                    log.debug("nodeChanged(): Zapping node properties cache for " + parent);
+                    MMObjectNode pnode = getNode(parent);
+                    if (pnode != null) pnode.delPropertiesCache();
+                }
             }
         }
         super.notify(event);
