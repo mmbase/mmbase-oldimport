24bd6b0659dc91d0e9a15d717db0e9a2eca58258
jverelst <jverelst@91430c5f-d668-0410-9e7d-dd663366dcfd>
2006-02-08 13:03:07 +0000

    new interface for security implementations
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@17514 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ contributions/didactor2/src/core/java/nl/didactor/security/AuthenticationComponent.java
@@ -0,0 +1,14 @@
+package nl.didactor.security;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import nl.didactor.security.UserContext;
+
+public interface AuthenticationComponent {
+    public UserContext processLogin(HttpServletRequest request, HttpServletResponse response, String application);
+    public UserContext isLoggedIn(HttpServletRequest request, HttpServletResponse response);
+    public String getLoginPage(HttpServletRequest request, HttpServletResponse response);
+    public String getName();
+    public void logout(HttpServletRequest request, HttpServletResponse respose);
+}
