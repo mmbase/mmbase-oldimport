7fb85a33eeee1dab05c8a60556c93d614b0f4699
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2006-04-08 13:34:24 +0000

    added hashCode and equals impl. on the returned Collections as defined by List (will problably change to return a List once, but didn't yet fully implemneted ListIterator)
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@18304 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/util/LocalizedEntryListFactory.java
+++ src/org/mmbase/util/LocalizedEntryListFactory.java
@@ -36,7 +36,7 @@ import org.mmbase.util.logging.*;
  * partially by explicit values, though this is not recommended.
  *
  * @author Michiel Meeuwissen
- * @version $Id: LocalizedEntryListFactory.java,v 1.31 2006-03-31 19:15:29 michiel Exp $
+ * @version $Id: LocalizedEntryListFactory.java,v 1.32 2006-04-08 13:34:24 michiel Exp $
  * @since MMBase-1.8
  */
 public class LocalizedEntryListFactory implements Serializable, Cloneable {
@@ -70,7 +70,7 @@ public class LocalizedEntryListFactory implements Serializable, Cloneable {
             return "entries:" + entries + "uu:" + unusedKeys;
         }
     }
-    private HashMap localized  = new HashMap();   //Locale -> LocaledEntry
+    private HashMap localized  = new HashMap();   // Locale -> LocaledEntry
     private ArrayList bundles  = new ArrayList(); // contains all Bundles
     private ArrayList fallBack = new ArrayList(); // List of known keys, used as fallback, if nothing defined for a certain locale
 
@@ -211,12 +211,43 @@ public class LocalizedEntryListFactory implements Serializable, Cloneable {
     /**
      * Returns a Collection of Map.Entries for the given Locale. The collection is kind of 'virtual',
      * it only reflects the underlying memory structures.
+     * 
+     * This collection does have a well defined iteration order.
+     *
      * @param locale The locale of <code>null</code> for the default locale.
      * @param cloud  The cloud to use. Can be <code>null</code> if no queries added (see {@link #addQuery}).
      *               If Locale is <code>null</code>, but cloud isn't, the locale of the cloud is used.
      */
     public Collection /* <Map.Entry> */ get(final Locale locale, final Cloud cloud) {
-        return new AbstractCollection() {
+        return new AbstractCollection () {
+            // hashCode according to List interface
+            public int hashCode() {
+                int hashCode = 1;
+                Iterator i = iterator();
+                while (i.hasNext()) {
+                    Object obj = i.next();
+                    hashCode = 31 * hashCode + (obj == null ? 0 : obj.hashCode());
+                }
+                return hashCode;
+            }
+            // equals according to List interface
+            public boolean equals(Object o) {
+                if (o instanceof Collection) {
+                    Collection col = (Collection) o;
+                    if (col.size() != size()) return false;
+                    Iterator i = iterator(); Iterator j = col.iterator();
+                    while (i.hasNext()) {
+                        Object e1 = i.next();
+                        Object e2 = j.next();
+                        if (! (e1 == null ? e2 == null : e1.equals(e2))) return false;
+                    }
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+
             public int size() {
                 return LocalizedEntryListFactory.this.size(cloud);
             }
