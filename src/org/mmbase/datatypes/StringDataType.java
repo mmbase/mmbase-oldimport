db69ef0817cd3c7a11c0181d2e3b6884c3439204
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2006-04-10 15:06:16 +0000

    Fixed some bugs in the patterns used when inheriting from number types. Also implemnted a few which were missing
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@18331 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/datatypes/StringDataType.java
+++ src/org/mmbase/datatypes/StringDataType.java
@@ -22,7 +22,7 @@ import org.mmbase.util.logging.*;
  *
  * @author Pierre van Rooden
  * @author Michiel Meeuwissen
- * @version $Id: StringDataType.java,v 1.27 2006-01-06 17:19:21 michiel Exp $
+ * @version $Id: StringDataType.java,v 1.28 2006-04-10 15:06:16 michiel Exp $
  * @since MMBase-1.8
  */
 public class StringDataType extends ComparableDataType implements LengthDataType {
@@ -50,23 +50,72 @@ public class StringDataType extends ComparableDataType implements LengthDataType
             isPassword = dataType.isPassword();
         }
     }
+
+    public static final Pattern DOUBLE_PATTERN;
+    static {
+        // copied from javadoc of Double: http://java.sun.com/j2se/1.5.0/docs/api/java/lang/Double.html#valueOf(java.lang.String)
+        final String Digits     = "(\\p{Digit}+)";
+        final String HexDigits  = "(\\p{XDigit}+)";
+        // an exponent is 'e' or 'E' followed by an optionally
+        // signed decimal integer.
+        final String Exp        = "[eE][+-]?"+Digits;
+        final String fpRegex    =
+            ("[\\x00-\\x20]*"+  // Optional leading "whitespace"
+             "[+-]?(" + // Optional sign character
+             "NaN|" +           // "NaN" string
+             "Infinity|" +      // "Infinity" string
+
+             // A decimal floating-point string representing a finite positive
+             // number without a leading sign has at most five basic pieces:
+             // Digits . Digits ExponentPart FloatTypeSuffix
+             //
+             // Since this method allows integer-only strings as input
+             // in addition to strings of floating-point literals, the
+             // two sub-patterns below are simplifications of the grammar
+             // productions from the Java Language Specification, 2nd
+             // edition, section 3.10.2.
+
+             // Digits ._opt Digits_opt ExponentPart_opt FloatTypeSuffix_opt
+             "((("+Digits+"(\\.)?("+Digits+"?)("+Exp+")?)|"+
+
+             // . Digits ExponentPart_opt FloatTypeSuffix_opt
+             "(\\.("+Digits+")("+Exp+")?)|"+
+
+             // Hexadecimal strings
+             "((" +
+             // 0[xX] HexDigits ._opt BinaryExponent FloatTypeSuffix_opt
+             "(0[xX]" + HexDigits + "(\\.)?)|" +
+
+             // 0[xX] HexDigits_opt . HexDigits BinaryExponent FloatTypeSuffix_opt
+             "(0[xX]" + HexDigits + "?(\\.)" + HexDigits + ")" +
+
+             ")[pP][+-]?" + Digits + "))" +
+             "[fFdD]?))" +
+             "[\\x00-\\x20]*");// Optional trailing "whitespace"
+
+        DOUBLE_PATTERN = Pattern.compile(fpRegex);
+    }
+    public static final Pattern BOOLEAN_PATTERN = Pattern.compile("\\A(1|0|true|false)\\z");
+    public static final Pattern INTEGER_PATTERN = Pattern.compile("\\A-?[0-9]+\\z");
+    public static final Pattern LONG_PATTERN    = INTEGER_PATTERN;
+
     protected void inheritRestrictions(BasicDataType origin) {
         super.inheritRestrictions(origin);
         if (origin instanceof StringDataType) {
             StringDataType dataType = (StringDataType)origin;
-            patternRestriction.inherit(dataType.patternRestriction);            
+            patternRestriction.inherit(dataType.patternRestriction);
             minLengthRestriction.inherit(dataType.minLengthRestriction);
             maxLengthRestriction.inherit(dataType.maxLengthRestriction);
         } else if (origin instanceof BooleanDataType) {
-            patternRestriction.setValue(Pattern.compile("(1|0|true|false)"));
+            patternRestriction.setValue(BOOLEAN_PATTERN);
         } else if (origin instanceof IntegerDataType) {
-            patternRestriction.setValue(Pattern.compile("-?[0-9]+"));
+            patternRestriction.setValue(INTEGER_PATTERN);
         } else if (origin instanceof LongDataType) {
-            patternRestriction.setValue(Pattern.compile("-?[0-9]+"));
+            patternRestriction.setValue(LONG_PATTERN);
         } else if (origin instanceof FloatDataType) {
-            // TODO..
+            patternRestriction.setValue(DOUBLE_PATTERN);
         } else if (origin instanceof DoubleDataType) {
-            // TODO..
+            patternRestriction.setValue(DOUBLE_PATTERN);
         }
     }
 
@@ -74,7 +123,7 @@ public class StringDataType extends ComparableDataType implements LengthDataType
         super.cloneRestrictions(origin);
         if (origin instanceof StringDataType) {
             StringDataType dataType = (StringDataType)origin;
-            patternRestriction = new PatternRestriction(dataType.patternRestriction);            
+            patternRestriction = new PatternRestriction(dataType.patternRestriction);
             minLengthRestriction = new AbstractLengthDataType.MinRestriction(this, dataType.minLengthRestriction);
             maxLengthRestriction = new AbstractLengthDataType.MaxRestriction(this, dataType.maxLengthRestriction);
         }
@@ -159,11 +208,12 @@ public class StringDataType extends ComparableDataType implements LengthDataType
         return isPassword;
     }
     public void setPassword(boolean pw) {
+        edit();
         isPassword = pw;
     }
 
-    protected Collection validateCastedValue(Collection errors, Object castedValue, Node node, Field field) {
-        errors = super.validateCastedValue(errors, castedValue, node, field);
+    protected Collection validateCastedValue(Collection errors, Object castedValue, Object value, Node node, Field field) {
+        errors = super.validateCastedValue(errors, castedValue, value,  node, field);
         errors = patternRestriction.validate(errors, castedValue, node, field);
         return errors;
     }
@@ -192,8 +242,15 @@ public class StringDataType extends ComparableDataType implements LengthDataType
         }
         protected boolean simpleValid(Object v, Node node, Field field) {
             String s = Casting.toString(v);
-            return value == null ? true : getPattern().matcher(s).matches();
+            boolean res =  value == null ? true : getPattern().matcher(s).matches();
+            //log.info("VALIDATING " + v + " with " + getPattern() + " -> " + res);
+            return res;
         }
+    }
+
+    public static void main(String [] argv) {
+        Pattern p = Pattern.compile(argv[0]);
+        System.out.println(p.matcher(argv[1]).matches());
     }
 
 }
