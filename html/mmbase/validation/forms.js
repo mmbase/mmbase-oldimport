b44033e0496ac3b3b5efc2d9bab5d55e7c4126f2
andre <andre@91430c5f-d668-0410-9e7d-dd663366dcfd>
2006-04-26 08:00:39 +0000

    Translated docs, added 'eu' as domain, added some documentation.
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@18626 91430c5f-d668-0410-9e7d-dd663366dcfd

--- html/mmbase/validation/forms.js
+++ html/mmbase/validation/forms.js
@@ -1,61 +1,67 @@
-/*
-  Validation scripts
-
-
-*/
+/* Generic javascripts to check forms */
 
 /*
- * Return true if the input has had input (a radio was selected, text has been entered etc.)
- * throw in the formitem itself, not it's value
+ * Return true if the input field has had input (a radio was selected, text has been entered etc.)
+ * throw in the formitem itself, not it's value.
+ * TODO: only input type="text", input type="checkbox", input type="radio", select and textarea 
+ * have been implemented so far.
  *
- * !!! Only input type="text",input type="checkbox", input type="radio", select and textarea
- * !!! have been implemented so far.
- *
+ * @param   formfield   the form field to check
+ * @return  true when there is a value, false when not
  */
-function hasInput(formulier) {
-    if (formulier) {
-        if (formulier.type) {
-            if (formulier.type.toUpperCase()=="TEXT" || formulier.type.toUpperCase()=="TEXTAREA") {
-                if (formulier.value&&formulier.value.length>=1) {
-                    return true;
-                }
-            } else if (formulier.nodeName.toUpperCase() == "SELECT") {
-                switch (formulier.selectedIndex) {
-                case -1:
-                    return false;
-                case 0:
-                    if (formulier.options[formulier.selectedIndex].value) {
-                        return true;
-                    }
-                    break;
-                default:
-                    return true;
-                }
-            }
-        } else {
-            return checkRadio(formulier);
+function hasInput(formfield) {
+  if (formfield) {
+    if (formfield.type) {
+      if (formfield.type.toUpperCase()=="TEXT" || formfield.type.toUpperCase()=="TEXTAREA") {
+        if (formfield.value&&formfield.value.length>=1) {
+          return true;
         }
+      } else if (formfield.nodeName.toUpperCase() == "SELECT") {
+        switch (formfield.selectedIndex) {
+          case -1:
+            return false;
+          case 0:
+            if (formfield.options[formfield.selectedIndex].value) {
+              return true;
+            }
+            break;
+          default:
+            return true;            
+        }
+      }
+    } else {
+        return checkRadio(formfield);
     }
-    return false;
+  }
+  return false;
 }
 
+/* 
+ * This one is called by hasInput(), checks a group of radiobuttons for a value.
+ * 
+ * @param   radioGroup  id of group of radiobuttons to check
+ * @return  true when there is value, false when not
+ */
 function checkRadio(radioGroup) {
-    // Return true if one of the radiobuttons is checked
-    if (radioGroup.length>0) {
-        for (i=0;i<radioGroup.length; i++) {
-            if (radioGroup[i].checked) {
-                return true;
-            }
-        }
-    } else {
-        alert("checkRadio: wrong formfield sent to function.");
-    }
-    return false;
+  // Return true if one of the radiobuttons is checked
+  if (radioGroup.length>0) {
+      for (i=0;i<radioGroup.length; i++) {
+          if (radioGroup[i].checked) {
+              return true;
+          }
+      }
+  } else {
+      alert("checkRadio: wrong formfield sent to function.");
+  }
+  return false;
 }
 
 /*
- * Return true als een postcode voldoet aan de
- * Nederlandse samenstelling voor een postcode
+ * Checks a Dutch postcode '1234 AB'.
+ * Allows a space between '1234' and 'AB', checks case-insensitive
+ * 
+ * @param   str String containing a postcode
+ * @return  true when postcode seams OK, false when not
  */
 function checkPostcode(str) {
     reg = /^[0-9]{4} ?[A-Z]{2}$/i;
@@ -63,27 +69,19 @@ function checkPostcode(str) {
 }
 
 /*
- * Controleer of een tekst is opgemaakt volgens
- * het patroon van een e-mail adres
+ * Checks an email address, this method follows these rules:
+ * 
+ * The address should start with at least 1 character before the at sign (@).
+ * A dot (.) and a hyphen (_) can't be next to each other and not next to an at sign.
+ * The address should contain an at sign.
+ * There have to be at least two characters after the at sign.
+ * The address has to end with one of the Top Level Domains (TLD's) menthioned below
+ * and the TLD should be prefixed with a dot.
+ * 
+ * @param   str an email address
+ * @return  true when the address seems valid, false if otherwise
  */
-function emailCheck(str) {
-    /*
-     * Regels voor controle van e-mail adressen
-     *
-     * Het adres moet beginnen met tenminste 1 teken voor de apenstaart (@)
-     *
-     * Een punt (.) en koppelteken (-) mogen niet naast elkaar staan en ook
-     * niet naast de apenstaart (@)
-     *
-     * Er moet 1 apenstaart (@) in het adres staan
-     *
-     * Er moeten tenminste twee tekens na de apenstaart (@) staan
-     *
-     * Het adres moet eindigen op een gekwalificeerde TLD (Top Level Domain)
-     * vooraf gegaan door een punt (.)
-     *
-     */
-
+function emailCheck(str) {  
     var domStr = "";
     // Well known official top level domains
     domStr += "com|edu|gov|int|mil|net|org|arpa|nato|info|aero|biz|coop|museum|name|pro|";
@@ -92,7 +90,7 @@ function emailCheck(str) {
     domStr += "ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|";
     domStr += "ca|cc|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|";
     domStr += "de|dj|dk|dm|do|dz|";
-    domStr += "ec|ee|eg|eh|er|es|et|";
+    domStr += "ec|ee|eg|eh|er|es|et|eu|";
     domStr += "fi|fj|fk|fm|fo|fr|fx|";
     domStr += "ga|gb|gd|ge|gf|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|";
     domStr += "hk|hm|hn|hr|ht|hu|";
@@ -114,9 +112,9 @@ function emailCheck(str) {
     domStr += "ye|yt|yu|";
     domStr += "za|zm|zr|zw";
     // custom domains for testing purposes
-    //domStr += "mad|local";
+    //domStr += "mad|local";        
 
     var re = new RegExp("^[A-Z0-9_]+([\.-]?[A-Z0-9_])*@([A-Z0-9_-]{2,}[\.]{1})+("+ domStr +")$","i");
-
+    
     return re.test(str);
 }
