7c1b9126f3900b3251da92089a9717fba10af5bb
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2006-06-20 20:18:45 +0000

    used specifiazed log method for logging stack trace
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@19195 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/util/ThreadPools.java
+++ src/org/mmbase/util/ThreadPools.java
@@ -15,22 +15,22 @@ import org.mmbase.util.logging.*;
  *
  * @since MMBase 1.8
  * @author Michiel Meewissen
- * @version $Id: ThreadPools.java,v 1.4 2006-01-17 14:45:58 michiel Exp $
+ * @version $Id: ThreadPools.java,v 1.5 2006-06-20 20:18:45 michiel Exp $
  */
 public abstract class ThreadPools {
     private static final Logger log = Logging.getLoggerInstance(ThreadPools.class);
 
     /**
-     * Generic Thread Pools which can be used by 'filters'. 
+     * Generic Thread Pools which can be used by 'filters'.
      */
     public static final Executor filterExecutor = Executors.newCachedThreadPool();
 
-    
+
     /**
      * For jobs there are 'scheduled', and typically happen on larger time-scales.
      */
     public static final Executor jobsExecutor = new ThreadPoolExecutor(2, 10, 5, TimeUnit.MINUTES, new  LinkedBlockingQueue(), new ThreadFactory() {
-            
+
             public Thread newThread(Runnable r) {
                 Thread t = new Thread(r, "JOBTHREAD") {
                         /**
@@ -40,7 +40,7 @@ public abstract class ThreadPools {
                             try {
                                 super.run();
                             } catch (Throwable t) {
-                                log.error("Error during job: " + t.getClass().getName() + " " + t.getMessage() + "\n" + Logging.stackTrace(t));
+                                log.error("Error during job: " + t.getClass().getName() + " " + t.getMessage(), t);
                             }
                         }
                     };
@@ -48,6 +48,6 @@ public abstract class ThreadPools {
                 return t;
             }
         });
-                                                                              
+
 
 }
