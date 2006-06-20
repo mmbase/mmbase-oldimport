a32a61cf9d0db56320b7ed26f432b2d85b965221
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2006-06-20 21:27:28 +0000

    implemented invalidation of node-caches on this much more logical location (used to be in MMObjectBuilder)
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@19209 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/cache/NodeCache.java
+++ src/org/mmbase/cache/NodeCache.java
@@ -8,14 +8,16 @@ See http://www.MMBase.org/license
 
 */
 package org.mmbase.cache;
+import org.mmbase.core.event.*;
+import org.mmbase.module.core.*;
 
 /**
  * A cache for MMObjectNodes. 
  *
  * @author  Michiel Meeuwissen
- * @version $Id: NodeCache.java,v 1.4 2003-07-14 21:02:02 michiel Exp $
+ * @version $Id: NodeCache.java,v 1.5 2006-06-20 21:27:28 michiel Exp $
  */
-public class NodeCache extends Cache {
+public class NodeCache extends Cache implements NodeEventListener, RelationEventListener { 
     private static final int CACHE_SIZE = 4 * 1024;
 
     private static NodeCache cache;
@@ -41,10 +43,32 @@ public class NodeCache extends Cache {
      */
     private NodeCache() {
         super(CACHE_SIZE);
+        // node cache is registered as a Listener in MMBase.java.
     }
+
 
     public Object remove(Object key) {
         RelatedNodesCache.getCache().removeNode((Integer) key);
         return super.remove(key);
+    }
+
+
+    public void notify(NodeEvent event) {
+        int type = event.getType();
+        if(type == Event.TYPE_DELETE || ((! event.isLocal()) && type == Event.TYPE_CHANGE)) {
+            Integer changedNodeNumber = new Integer(event.getNodeNumber());
+            remove(changedNodeNumber);
+        }
+    }
+
+    private void delRelationsCache(Integer nodeNumber) {
+        MMObjectNode node = (MMObjectNode) get(nodeNumber);
+        if (node != null) {
+            node.delRelationsCache();
+        }
+    }
+    public void notify(RelationEvent event) {
+        delRelationsCache(new Integer(event.getRelationSourceNumber()));
+        delRelationsCache(new Integer(event.getRelationDestinationNumber()));
     }
 }
