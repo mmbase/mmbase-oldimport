fc5f8d6aceaf51e75dcf6c1c70c7ab507a2ce262
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2006-07-12 08:33:41 +0000

    simplified a bit, support for century, and era
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@19552 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/util/DynamicDate.java
+++ src/org/mmbase/util/DynamicDate.java
@@ -181,6 +181,24 @@ public class DynamicDate extends Date {
         return date + ": " + evalDate().toString();
     }
 
+    public static String[] getDemo() {
+        return new String[] {
+            "0", "10000", "-10000", "+1000", // just numbers a bit after 1970, a bit before
+            "1973-05-03", "2006-05-09", "-3-12-25", // absolute dates
+            "2000-01-01 16:00", "TZUTC 2001-01-01 16:00","today 12:34:56.789",
+            "now", "today", "tomorrow", "now + 10 minute", "today + 5 day",
+            "now this year", "next august", "today + 6 month next august", "tomonth", "borreltijd", "today + 5 dayish", "yesteryear", "mondayish",
+            "duration + 5 minute", "duration + 100 year",
+            "TZUTC today noon", "TZEurope/Amsterdam today noon", "TZUTC today", "TZEurope/Amsterdam today",
+            "TZ UTC today noon", "TZ Europe/Amsterdam today noon", "TZ UTC today", "TZ Europe/Amsterdam today",
+            "today 6 oclock", "today 23 oclock", "today 43 oclock",
+            "tosecond", "tominute", "tohour", "today", "previous monday", "tomonth", "toyear", "tocentury", "tocentury_pedantic", "toera",
+            "now this second", "now this minute", "now this hour", "now this day", "today previous monday", "now this month", "now this year", "now this century", "now this era",
+            "now - 15 year this century", "now - 20 year this century_pedantic", "today + 2 century", "toera - 1 minute",
+            "this july", "previous july", "next july", "this sunday", "previous sunday", "next sunday"
+        };
+    }
+
 
     public static void main(String argv[]) throws java.text.ParseException, ParseException {
 
@@ -188,20 +206,7 @@ public class DynamicDate extends Date {
         //System.out.println(TimeZone.getDefault());
         java.text.DateFormat formatter = new java.text.SimpleDateFormat("GGGG yyyy-MM-dd HH:mm:ss.SSS zzz");
         if (argv.length == 0) {
-            String[] demo = {
-                "0", "10000", "-10000", "+1000", // just numbers a bit after 1970, a bit before
-                "1973-05-03", "2006-05-09", "-3-12-25", // absolute dates
-                "2000-01-01 16:00", "TZUTC 2001-01-01 16:00","today 12:34:56.789",
-                "now", "today", "tomorrow", "now + 10 minute", "today + 5 day",
-                "now this year", "next august", "today + 6 month next august", "tomonth", "borreltijd", "today + 5 dayish", "yesteryear",
-                "duration + 5 minute", "duration + 100 year",
-                "TZUTC today noon", "TZEurope/Amsterdam today noon", "TZUTC today", "TZEurope/Amsterdam today",
-                "TZ UTC today noon", "TZ Europe/Amsterdam today noon", "TZ UTC today", "TZ Europe/Amsterdam today",
-                "today 6 oclock", "today 23 oclock", "today 43 oclock",
-                "mondayish",
-
-
-            };
+            String[] demo = getDemo();
             for (int i = 0; i < demo.length; i++) {
                 try {
                     Date d1 = getInstance(demo[i]);
