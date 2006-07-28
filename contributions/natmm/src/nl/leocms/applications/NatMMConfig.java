2f39cdb1dae5065c5e32780f5b2c0cc0ba156191
henk <henk@91430c5f-d668-0410-9e7d-dd663366dcfd>
2006-07-28 13:29:17 +0000

    Added hasClosedUserGroup
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@19848 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/natmm/src/nl/leocms/applications/NatMMConfig.java
+++ contributions/natmm/src/nl/leocms/applications/NatMMConfig.java
@@ -28,52 +28,53 @@ public class NatMMConfig {
    };
 
 
-	// there is a difference between layout and style:
-	// - style are the colours and background images set in the css
-	// - layout is the layout of blocks on the template
-	// style set for every rubriek, layout may have value parent_layout
+   // there is a difference between layout and style:
+   // - style are the colours and background images set in the css
+   // - layout is the layout of blocks on the template
+   // style set for every rubriek, layout may have value parent_layout
 
-	public static int PARENT_STYLE = -1;
-	public static int DEFAULT_STYLE = 7;
+   public static int PARENT_STYLE = -1;
+   public static int DEFAULT_STYLE = 7;
 
-	public static int PARENT_LAYOUT = -1;
-	public static int DEFAULT_LAYOUT = 0;
-	public static int SUBSITE1_LAYOUT = 1;
-	public static int SUBSITE2_LAYOUT = 2;
-	public static int SUBSITE3_LAYOUT = 3;
-	public static int DEMO_LAYOUT = 4;
+   public static int PARENT_LAYOUT = -1;
+   public static int DEFAULT_LAYOUT = 0;
+   public static int SUBSITE1_LAYOUT = 1;
+   public static int SUBSITE2_LAYOUT = 2;
+   public static int SUBSITE3_LAYOUT = 3;
+   public static int DEMO_LAYOUT = 4;
 
-	public static String [] layout = {"Natuurmonumenten", "Naardermeer", "ING-Perspectief", "Actiesite", "Demo" };
-	public static String [] style1 = {"vereniging","steun" ,"nieuws","natuurin","natuurgebieden","links" ,"fun"   ,"default","zoeken","winkel","vragen","naardermeer" };
-    public static String [] color1 = {"552500"    ,"990100","4A7934","D71920"  ,"BAC42B"        ,"9C948C","EC008C","1D1E94" ,"00AEEF","F37021","6C6B5C","F37021" }; // color + line leftnavpage
-    public static String [] color2 = {"E4BFA3"    ,"F7D6C3","B0DF9B","FFBDB7"  ,"EEF584"        ,"EDE9E6","FABFE2","96ADD9" ,"B2E7FA","FED9B2","D6D6D1","F9B790" }; // background leftnavpage_high
-    public static String [] color3 = {"050080"    ,"050080","050080","050080"  ,"050080"        ,"050080","050080","FFFFFF" ,"050080","050080","050080","FFFFFF"};  // footer links
+   public static String [] layout = {"Natuurmonumenten", "Naardermeer", "ING-Perspectief", "Actiesite", "Demo" };
+   public static String [] style1 = {"vereniging","steun" ,"nieuws","natuurin","natuurgebieden","links" ,"fun"   ,"default","zoeken","winkel","vragen","naardermeer" };
+   public static String [] color1 = {"552500"    ,"990100","4A7934","D71920"  ,"BAC42B"        ,"9C948C","EC008C","1D1E94" ,"00AEEF","F37021","6C6B5C","F37021" }; // color + line leftnavpage
+   public static String [] color2 = {"E4BFA3"    ,"F7D6C3","B0DF9B","FFBDB7"  ,"EEF584"        ,"EDE9E6","FABFE2","96ADD9" ,"B2E7FA","FED9B2","D6D6D1","F9B790" }; // background leftnavpage_high
+   public static String [] color3 = {"050080"    ,"050080","050080","050080"  ,"050080"        ,"050080","050080","FFFFFF" ,"050080","050080","050080","FFFFFF"};  // footer links
 
-	public static String cssPath = "hoofdsite/themas/";
+   public static String cssPath = "hoofdsite/themas/";
 
-    public static boolean urlConversion = true;
-    public static boolean checkEmailByMailHost = true;
+   public static boolean urlConversion = true;
+   public static boolean checkEmailByMailHost = true;
+   public static boolean hasClosedUserGroup = false;
 
-    public static String fromEmailAddress = "website@natuurmonumenten.nl";
-    public static String fromCADAddress = "denatuurin@natuurmonumenten.nl";
-    public static String infoEmailAddress = "info@ledenservice.nl";
+   public static String fromEmailAddress = "website@natuurmonumenten.nl";
+   public static String fromCADAddress = "denatuurin@natuurmonumenten.nl";
+   public static String infoEmailAddress = "info@ledenservice.nl";
 
-    public static String toEmailAddress = "hangyi@xs4all.nl";
-    public static String [] liveUrl = { "http://www.natuurmonumenten.nl/", "http://www.prod.natuurmm.asp4all.nl/" };
-    public static String tmpMemberId = "9002162";
+   public static String toEmailAddress = "hangyi@xs4all.nl";
+   public static String [] liveUrl = { "http://www.natuurmonumenten.nl/", "http://www.prod.natuurmm.asp4all.nl/" };
+   public static String tmpMemberId = "9002162";
 
-    public static String toSubscribeAddress = "AanmeldingLidmaatschap@Natuurmonumenten.nl";
-    public static String rootDir = "/export/www/natuurmm/jakarta-tomcat/webapps/ROOT/WEB-INF/data/";
-    public static String tempDir = "/export/www/natuurmm/jakarta-tomcat/temp/";
-    public static String incomingDir = "/home/import/incoming/";
+   public static String toSubscribeAddress = "AanmeldingLidmaatschap@Natuurmonumenten.nl";
+   public static String rootDir = "/export/www/natuurmm/jakarta-tomcat/webapps/ROOT/WEB-INF/data/";
+   public static String tempDir = "/export/www/natuurmm/jakarta-tomcat/temp/";
+   public static String incomingDir = "/home/import/incoming/";
 
-    /*
-    public static String toSubscribeAddress = "hangyi@xs4all.nl";
-    public static String rootDir =  "C:/data/natmm/incoming/";
-    public static String tempDir = "C:/temp/";
-    public static String incomingDir =  "C:/data/natmm/incoming/";
-    */
+   /*
+   public static String toSubscribeAddress = "hangyi@xs4all.nl";
+   public static String rootDir =  "C:/data/natmm/incoming/";
+   public static String tempDir = "C:/temp/";
+   public static String incomingDir =  "C:/data/natmm/incoming/";
+   */
 
-    public NatMMConfig() {
-    }
+   public NatMMConfig() {
+   }
 }
