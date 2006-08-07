1ecc1eafadeaf6e068cd5635305d30b3413fe0aa
henk <henk@91430c5f-d668-0410-9e7d-dd663366dcfd>
2006-08-07 22:40:21 +0000

    Changed some defaults
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@19941 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/natmm/src/nl/leocms/applications/NMIntraConfig.java
+++ contributions/natmm/src/nl/leocms/applications/NMIntraConfig.java
@@ -41,37 +41,37 @@ public class NMIntraConfig {
    };
 	
    public static int PARENT_STYLE = -1;
-	public static int DEFAULT_STYLE = 4;
+	 public static int DEFAULT_STYLE = 4;
 	
-	public static String [] style1 = {"rode_zee","groene_zee","blauwe_zee","bibliotheek","blauw_wad","gele_zee","groene_boomrand","geel_strand","oranje_helmgras","oranje_pompoen","mieren" };
-	public static String [] color1 = {"#1E0064", "#2C620C",	"#6585DF",	 "#1E0064",    "#458FC9",	"#1E0064", "#999A01",		  "#A95500",    "#FF8E00",        "#FD7F00",       "#1E0064" };
+	 public static String [] style1 = { "rode_zee","groene_zee","blauwe_zee","bibliotheek","blauw_wad","gele_zee","groene_boomrand","geel_strand","oranje_helmgras","oranje_pompoen","mieren" };
+	 public static String [] color1 = {"#1E0064", "#2C620C",	"#6585DF",	 "#1E0064",    "#458FC9",	"#1E0064", "#999A01",		  "#A95500",    "#FF8E00",        "#FD7F00",       "#1E0064" };
    public static String [] color2 = {"#963A29", "#3F9D20",	"#506BB5",	 "#1E0064",    "#458FC9",	"#E15603", "#6C6D01",		  "#BC9610",    "#CB8631",        "#CC6C0A",       "#3F9D20" };
    public static String [] color3 = {"#EA5C3D", "#6B9F30",	"#6585DF",	 "#81A5DC",    "#80BFF0",	"#EDBD22", "#999A01",		  "#A95500 ",   "#FF8E00",        "#FD7F00",       "#6FAD22" };
    public static String [] color4 = {"#FD8D73", "#6CD230",	"#96A9DE",	 "#81A5DC",    "#ADD3F0",	"#FFD54F", "#B7BE1F",		  "#F5D84A ",   "#F3BF7D",        "#FEDFBF",       "#86C438" };
 
-	public static String cssPath = "css/";
+	 public static String cssPath = "css/";
 	
    public static boolean urlConversion = true;
    
    public static String fromEmailAddress = "intranet@natuurmonumenten.nl";
-   public static String sDocumentsUrl = "/PenO";
+   public static String sDocumentsUrl = "/documents/PenO";
    
    /*    
-   public static String toEmailAddress = "beheerder@natuurmonumenten.nl";
-   public static String rootDir = "D:/Intranet_Input/";
    public static String sDocumentsRoot = "D:/WIN32APP/Tomcat_Intranet/webapps/ROOT/PenO";
+   public static String toEmailAddress = "beheerder@natuurmonumenten.nl";
+   public static String rootDir = "E:/Intranet_Input/";
    public static String sCorporateWebsite = "http://www.natuurmonumenten.nl/";
    public static String sCorporateEditors = "http://www.natuurmonumenten.nl/editors/";
    */
    
+   public static String sDocumentsRoot = "C:/data/nmintra/webapps/documents/PenO";
    public static String toEmailAddress = "hangyi@xs4all.nl";    
-   public static String rootDir =  "C:/data/natmm/webapps/ROOT/WEB-INF/data/";
-   public static String sDocumentsRoot = "C:/data/natmm/webapps/ROOT/PenO";
+   public static String rootDir =  "C:/data/nmintra/webapps/ROOT/WEB-INF/data/";
    public static String sCorporateWebsite = "http://www.acc.natuurmm.asp4all.nl/";
    public static String sCorporateEditors = "http://www.acc.natuurmm.asp4all.nl/editors/";
-   
-	
+
+
    public NMIntraConfig() {
-      
-   }    
+
+   }
 }
