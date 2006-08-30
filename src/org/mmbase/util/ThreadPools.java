f6050f19508e80adc02a99799cb56e0a62897a23
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2006-08-30 21:01:23 +0000

    java 1.5, and some experiments with thread-pooling
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@20192 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/util/ThreadPools.java
+++ src/org/mmbase/util/ThreadPools.java
@@ -8,14 +8,16 @@ See http://www.MMBase.org/license
 
 */
 package org.mmbase.util;
-import edu.emory.mathcs.backport.java.util.concurrent.*;
+import java.util.*;
+import java.util.concurrent.*;
 import org.mmbase.util.logging.*;
+import org.mmbase.util.xml.UtilReader;
 /**
  * Generic MMBase Thread Pools
  *
  * @since MMBase 1.8
  * @author Michiel Meewissen
- * @version $Id: ThreadPools.java,v 1.5 2006-06-20 20:18:45 michiel Exp $
+ * @version $Id: ThreadPools.java,v 1.6 2006-08-30 21:01:23 michiel Exp $
  */
 public abstract class ThreadPools {
     private static final Logger log = Logging.getLoggerInstance(ThreadPools.class);
@@ -29,7 +31,7 @@ public abstract class ThreadPools {
     /**
      * For jobs there are 'scheduled', and typically happen on larger time-scales.
      */
-    public static final Executor jobsExecutor = new ThreadPoolExecutor(2, 10, 5, TimeUnit.MINUTES, new  LinkedBlockingQueue(), new ThreadFactory() {
+    public static final Executor jobsExecutor = new ThreadPoolExecutor(2, 10, 5 * 60 , TimeUnit.SECONDS, new ArrayBlockingQueue(200), new ThreadFactory() {
 
             public Thread newThread(Runnable r) {
                 Thread t = new Thread(r, "JOBTHREAD") {
@@ -49,5 +51,26 @@ public abstract class ThreadPools {
             }
         });
 
+
+
+    private static final UtilReader properties = new UtilReader("threadpools.xml", new Runnable() { public void run() { configure(); }});
+
+    /**
+     * @since MMBase-1.9
+     */
+    public static void configure() {
+
+        Map props = properties.getProperties();
+        String max = (String) props.get("jobs.maxsize");
+        if (max != null) {
+            log.info("Setting max pool size from " + ((ThreadPoolExecutor) jobsExecutor).getMaximumPoolSize() + " to " + max);
+            ((ThreadPoolExecutor) jobsExecutor).setMaximumPoolSize(Integer.parseInt(max));
+        }
+        String core = (String) props.get("jobs.coresize");
+        if (core != null) {
+            log.info("Setting core pool size from " + ((ThreadPoolExecutor) jobsExecutor).getCorePoolSize() + " to " + core);
+            ((ThreadPoolExecutor) jobsExecutor).setCorePoolSize(Integer.parseInt(core));
+        }
+    }
 
 }
