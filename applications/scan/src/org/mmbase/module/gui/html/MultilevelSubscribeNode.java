819aea7e44403ebe54861ba26d9499ccef4ca91c
pierre <pierre@91430c5f-d668-0410-9e7d-dd663366dcfd>
2006-09-11 10:46:04 +0000

    use observer code on the builder isntead of MMBase
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@20347 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/scan/src/org/mmbase/module/gui/html/MultilevelSubscribeNode.java
+++ applications/scan/src/org/mmbase/module/gui/html/MultilevelSubscribeNode.java
@@ -11,6 +11,8 @@ package org.mmbase.module.gui.html;
 
 import java.util.*;
 import org.mmbase.module.core.*;
+import org.mmbase.util.*;
+import org.mmbase.util.logging.*;
 
 /**
  * This object subscribes itself to builder changes
@@ -21,15 +23,22 @@ import org.mmbase.module.core.*;
  */
 public class MultilevelSubscribeNode implements MMBaseObserver {
 
+    private static Logger log = Logging.getLoggerInstance(MultilevelSubscribeNode.class);
+    
     private MMBase mmb;
     String type;
-    Vector queue=new Vector(50);
+    Vector queue = new Vector(50);
 
     public MultilevelSubscribeNode(MMBase mmb,String type) {
-        this.mmb=mmb;
-        this.type=type;
-        mmb.addLocalObserver(type,this);
-        mmb.addRemoteObserver(type,this);
+        this.mmb = mmb;
+        this.type = type;
+        MMObjectBuilder builder = mmb.getBuilder(type);
+        if (builder != null) {
+            builder.addLocalObserver(this);
+            builder.addRemoteObserver(this);
+        } else {
+            log.error("ERROR: Can't find builder : " + type);
+        }
     }
 
     public boolean nodeChanged(String machine,String number,String builder,String ctype) {
