144d5e4a46f2420ffaf7d4c83987a70a72268248
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2006-09-15 14:55:51 +0000

    made MethodFunction recognize @Name parameter annotation, so that you can easier wrap a java method in an mmbase 'function'
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@20445 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/util/functions/MethodFunction.java
+++ src/org/mmbase/util/functions/MethodFunction.java
@@ -11,6 +11,7 @@ package org.mmbase.util.functions;
 
 
 import java.lang.reflect.*;
+import java.lang.annotation.*;
 
 /**
  * A function based on an abritrary method. Since the name of the parameters cannot be found by
@@ -18,7 +19,7 @@ import java.lang.reflect.*;
  * method-function can come in handy on JSP's.
  *
  * @author Michiel Meeuwissen
- * @version $Id: MethodFunction.java,v 1.5 2006-09-08 18:34:12 michiel Exp $
+ * @version $Id: MethodFunction.java,v 1.6 2006-09-15 14:55:51 michiel Exp $
  * @see org.mmbase.module.core.MMObjectBuilder#executeFunction
  * @see org.mmbase.bridge.Node#getFunctionValue
  * @see org.mmbase.util.functions.BeanFunction
@@ -31,18 +32,53 @@ public class MethodFunction extends AbstractFunction {
         return new MethodFunction(method, name); // could be cached...
     }
 
-    private Method method = null;
+    private final Method method;
+    private final Object instance;
+    /**
+     * @since MMBase-1.9
+     */
+    public MethodFunction(Method method) {
+        this(method, method.getName(), null);
+    }
     public MethodFunction(Method method, String name) {
+        this(method, name, null);
+    }
+    /**
+     * @since MMBase-1.9
+     */
+    public MethodFunction(Method method, Object instance) {
+        this(method, method.getName(), instance);
+    }
+    /**
+     * @since MMBase-1.9
+     */
+    public MethodFunction(Method method, String name, Object instance) {
         super(name, null, null);
         this.method = method;
-        if (! Modifier.isStatic(method.getModifiers())) {
-            throw new IllegalArgumentException("The method " + method + " is not static"); // otherwise NPE in getFunctionValue
+        this.instance = instance;
+        if (instance == null) {
+            if (! Modifier.isStatic(method.getModifiers())) {
+                throw new IllegalArgumentException("The method " + method + " is not static"); // otherwise NPE in getFunctionValue
+            }
+        } else {
+            if (! method.getDeclaringClass().isInstance(instance)) {
+                throw new IllegalArgumentException("The object " + instance + " is not an instance of the class of  " + method);
+            }
         }
 
+        Annotation[][] annots = method.getParameterAnnotations();
         Class[] parameters = method.getParameterTypes();
         Parameter[] def = new Parameter[parameters.length];
         for (int i = 0; i < parameters.length; i++) {
-            def[i] = new Parameter("parameter" + (i + 1), parameters[i]); // no way to find the name of the parameter
+            String paramName = null;
+            for (Annotation annot : annots[i]) {
+                if (annot.annotationType().equals(Name.class)) {
+                    paramName = ((Name) annot).value();
+                }
+            }
+            if (paramName == null) paramName = "parameter" + (i + 1);
+
+            def[i] = new Parameter(paramName, parameters[i]); // no way to find the name of the parameter
         }
 
         setParameterDefinition(def);
@@ -54,7 +90,7 @@ public class MethodFunction extends AbstractFunction {
 
     public Object getFunctionValue(Parameters parameters) {
         try {
-            return method.invoke(null, parameters.toArray());
+            return method.invoke(instance, parameters.toArray());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
---
+++ src/org/mmbase/util/functions/Name.java
@@ -0,0 +1,26 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.util.functions;
+import java.lang.annotation.*;
+
+/**
+ * The name of a parameter as a annotation interface. Is used in {@link MethodFunction}. If your
+ * method has with this annotated parameters, the name will be used for the parameter.
+ *
+ * @author Michiel Meeuwissen
+ * @version $Id: Name.java,v 1.1 2006-09-15 14:55:51 michiel Exp $
+ * @since MMBase-1.9
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+public @interface Name {
+    String value();
+
+}
