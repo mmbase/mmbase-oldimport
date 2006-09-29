35135e3284ce55d4271ada0c9c3dc87403049c47
pierre <pierre@91430c5f-d668-0410-9e7d-dd663366dcfd>
2006-09-29 15:47:39 +0000

    deprecation
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@20655 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/editwizard/src/org/mmbase/applications/editwizard/Utils.java
+++ applications/editwizard/src/org/mmbase/applications/editwizard/Utils.java
@@ -27,6 +27,7 @@ import org.mmbase.util.ResourceLoader;
 
 import org.mmbase.cache.xslt.*;
 import org.mmbase.util.xml.URIResolver;
+import org.mmbase.util.xml.DocumentReader;
 import org.mmbase.util.XMLErrorHandler;
 import org.mmbase.util.XMLEntityResolver;
 
@@ -40,7 +41,7 @@ import org.mmbase.util.XMLEntityResolver;
  * @author  Pierre van Rooden
  * @author  Michiel Meeuwissen
  * @since   MMBase-1.6
- * @version $Id: Utils.java,v 1.41 2005-08-24 12:42:42 michiel Exp $
+ * @version $Id: Utils.java,v 1.42 2006-09-29 15:47:39 pierre Exp $
  */
 
 public class Utils {
@@ -52,7 +53,7 @@ public class Utils {
      * @return     a DocumentBuilder.
      */
     public static DocumentBuilder getDocumentBuilder(boolean validate) {
-        return org.mmbase.util.XMLBasicReader.getDocumentBuilder(validate,
+        return org.mmbase.util.xml.DocumentReader.getDocumentBuilder(validate,
             new XMLErrorHandler(validate, XMLErrorHandler.ERROR),
             new XMLEntityResolver(validate, Utils.class));
     }
