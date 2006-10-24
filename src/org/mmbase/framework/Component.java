9e349cb7807114a2d81687994a941e319704fafc
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2006-10-24 17:16:32 +0000

    added some javadoc
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@20979 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/framework/Component.java
+++ src/org/mmbase/framework/Component.java
@@ -16,21 +16,41 @@ import org.mmbase.util.LocalizedString;
  * components, and may be requested several views.
  *
  * @author Michiel Meeuwissen
- * @version $Id: Component.java,v 1.5 2006-10-13 17:22:15 michiel Exp $
+ * @version $Id: Component.java,v 1.6 2006-10-24 17:16:32 michiel Exp $
  * @since MMBase-1.9
  */
 public interface Component {
 
+    /**
+     * Every component has a (universally) unique name
+     */
     String getName();
 
+    /**
+     * The description can contain further information about the component, mainly to be displayed
+     * in pages about components generally.
+     */
     LocalizedString getDescription();
 
-    void configure(org.w3c.dom.Element doc);
+    /**
+     * Configures the component, by XML.
+     * @param element A 'component' element from the 'components' XSD.
+     */
+    void configure(org.w3c.dom.Element element);
 
+    /**
+     * An unmodifiable collection of all blocks associated with the component
+     */
     Collection<Block> getBlocks();
 
+    /**
+     * Gets a specific block
+     */
     Block getBlock(String name);
 
+    /**
+     * Gets the one block that is the 'default' block of this component
+     */
     Block getDefaultBlock();
 
 }
