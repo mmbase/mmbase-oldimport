49e79d82e532160dd9f1fd73c6daf137d05fd730
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2006-10-25 20:41:52 +0000

    super also modifiable lists, added a varars constructor
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@21004 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/util/ChainedList.java
+++ src/org/mmbase/util/ChainedList.java
@@ -13,26 +13,30 @@ package org.mmbase.util;
 import java.util.*;
 
 /**
- * Simple utility to chained several lists into a new one.
+ * Simple utility to chain several lists into a new one.
  *
  * @author	Michiel Meeuwissen
  * @since	MMBase-1.8
- * @version $Id: ChainedList.java,v 1.1 2006-09-27 20:40:19 michiel Exp $
+ * @version $Id: ChainedList.java,v 1.2 2006-10-25 20:41:52 michiel Exp $
  * @see ChainedIterator
  */
 public class ChainedList<E> extends AbstractList<E> {
 
-    private List<List<? extends E>> lists = new ArrayList();
-    private int size = 0;
-    public ChainedList() {
-
+    private final List<List<? extends E>> lists = new ArrayList<List<? extends E>>();
+    public ChainedList(List<? extends E>... ls) {
+        for (List<? extends E> l : ls) {
+            addList(l);
+        }
     }
 
     public void addList(List<? extends E> l) {
-        size += l.size();
         lists.add(l);
     }
     public int size() {
+        int size = 0;
+        for (List<? extends E> l : lists) {
+            size += l.size();
+        }
         return size;
     }
     public E get(int i) {
