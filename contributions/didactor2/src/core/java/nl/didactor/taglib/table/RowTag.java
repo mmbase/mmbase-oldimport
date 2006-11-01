6f96a506ec8a0ff68e9ca319986241c3dfe167dd
mmeeuwissen <mmeeuwissen@91430c5f-d668-0410-9e7d-dd663366dcfd>
2006-11-01 20:24:00 +0000

    didnt work if EVAL_BODY != EVAL_BODY_BUFFERED
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@21140 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/didactor2/src/core/java/nl/didactor/taglib/table/RowTag.java
+++ contributions/didactor2/src/core/java/nl/didactor/taglib/table/RowTag.java
@@ -10,29 +10,37 @@ import org.mmbase.bridge.jsp.taglib.*;
 import org.mmbase.module.core.*;
 
 /**
+ * @javadoc
  * @author Johannes Verelst &lt;johannes.verelst@eo.nl&gt;
  */
 public class RowTag extends CloudReferrerTag { 
     /**
-     * Execute the body of the tag if the current user has the given role.
      */
     public int doStartTag() throws JspTagException {
+        try {
+            TableTag tt = (TableTag)findParentTag(TableTag.class, null, true);
+            pageContext.getOut().print(tt.getLabel("row.start"));
+        } catch (IOException ioe) {
+            throw new TaglibException(ioe);
+        }
         return EVAL_BODY;
     }
 
-    // Code copied from MMBase 'CloudTag' code. Don't know why this is the case
-    // but apparently there is no body written if this code is not included here
-    // if EVAL_BODY == EVAL_BODY_BUFFERED
     public int doAfterBody() throws JspTagException {
+        if (EVAL_BODY == EVAL_BODY_BUFFERED) {
+            try {
+                if (bodyContent != null) {
+                    bodyContent.writeOut(bodyContent.getEnclosingWriter());
+                }
+            } catch (IOException ioe) {
+                throw new TaglibException(ioe);
+            }
+        }
         try {
-            if (bodyContent != null) {
-                TableTag tt = (TableTag)findParentTag(TableTag.class, null, true);
-                getPreviousOut().print(tt.getLabel("row.start"));
-                bodyContent.writeOut(bodyContent.getEnclosingWriter());
-                getPreviousOut().print(tt.getLabel("row.end"));
-            }    
+            TableTag tt = (TableTag)findParentTag(TableTag.class, null, true);
+            pageContext.getOut().print(tt.getLabel("row.end"));
         } catch (IOException ioe) {
-           throw new TaglibException(ioe);
+            throw new TaglibException(ioe);
         }
         return SKIP_BODY;
     }
