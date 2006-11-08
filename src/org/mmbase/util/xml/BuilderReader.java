de79bcb943133aaa67379bb255fa504af0127ee7
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2006-11-08 13:17:56 +0000

    fix for #6908, NPE's
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@21222 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/util/xml/BuilderReader.java
+++ src/org/mmbase/util/xml/BuilderReader.java
@@ -36,7 +36,7 @@ import org.mmbase.util.logging.*;
  * @author Rico Jansen
  * @author Pierre van Rooden
  * @author Michiel Meeuwissen
- * @version $Id: BuilderReader.java,v 1.80 2006-10-13 14:22:27 nklasens Exp $
+ * @version $Id: BuilderReader.java,v 1.81 2006-11-08 13:17:56 michiel Exp $
  */
 public class BuilderReader extends DocumentReader {
 
@@ -685,7 +685,8 @@ public class BuilderReader extends DocumentReader {
                     } else {
                         dataType = collector.getDataTypeInstance(guiType, baseDataType);
                         if (dataType == null) {
-                            log.warn("Could not find data type for " + baseDataType + " / " + guiType + " for builder: '" + builder.getTableName() + "'");
+                            log.warn("Could not find data type for " + baseDataType + " / " + guiType + " for builder: '" + (builder == null ? "NULL" : builder.getTableName()) + "'");
+
                         }
                     }
                 }
@@ -711,7 +712,7 @@ public class BuilderReader extends DocumentReader {
             } else {
                 requestedBaseDataType = collector == null ? null : collector.getDataType(base, true);
                 if (requestedBaseDataType == null) {
-                    log.error("Could not find base datatype for '" + base + "' falling back to " + baseDataType + " in builder '" + builder.getTableName() + "'");
+                    log.error("Could not find base datatype for '" + base + "' falling back to " + baseDataType + " in builder '" + (builder == null ?  "NULL" : builder.getTableName()) + "'");
                     requestedBaseDataType = baseDataType;
                 }
             }
