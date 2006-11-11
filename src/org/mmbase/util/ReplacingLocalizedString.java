8c207d8dc151049ee0ef02e1bb1921fbe7c54863
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2006-11-11 12:56:39 +0000

    split off WrappedLocalizedString from ReplacingLocalizedSTring, and made it possible to define a default locale for these instances itself
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@21247 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/util/ReplacingLocalizedString.java
+++ src/org/mmbase/util/ReplacingLocalizedString.java
@@ -15,25 +15,23 @@ import org.mmbase.util.logging.*;
 /**
  * Extends and wraps LocalizedString. It extends to look like a 'normal' LocalizedString, but it
  * overrides 'get' to do token-replacements first.
- * 
+ *
  * This functionality is not in LocalizedString itself, because now you can have different
  * replacements on the same value set represented by a LocalizedString withouth having to copy
  * everything every time.
  *
  *
  * @author Michiel Meeuwissen
- * @version $Id: ReplacingLocalizedString.java,v 1.4 2005-10-21 16:46:11 michiel Exp $
+ * @version $Id: ReplacingLocalizedString.java,v 1.5 2006-11-11 12:56:39 michiel Exp $
  * @since MMBase-1.8
  */
-public class ReplacingLocalizedString extends LocalizedString {
-    
+public class ReplacingLocalizedString extends WrappedLocalizedString {
+
     private static final Logger log = Logging.getLoggerInstance(ReplacingLocalizedString.class);
 
+    private List<Map.Entry<String, String>> replacements = new ArrayList<Map.Entry<String, String>>();
 
-    private LocalizedString wrapped;
-    private List replacements = new ArrayList();
 
-    
     // just for the contract of Serializable
     protected ReplacingLocalizedString() {
 
@@ -43,19 +41,16 @@ public class ReplacingLocalizedString extends LocalizedString {
      * @param s The wrapped LocalizedString.
      */
     public ReplacingLocalizedString(LocalizedString s) {
-        if (s == null) s = new LocalizedString("NULL");
-        wrapped = s;
+        super(s);
     }
-    
+
     public void replaceAll(String regexp, String replacement) {
         replacements.add(new Entry(regexp, replacement));
     }
 
     protected String replace(String input) {
         String output = input;
-        Iterator i = replacements.iterator();
-        while (i.hasNext()) {
-            Map.Entry entry = (Map.Entry) i.next();
+        for (Map.Entry entry : replacements) {
             try {
                 output = output.replaceAll((String) entry.getKey(), (String) entry.getValue());
             } catch (Throwable t) {
@@ -65,24 +60,9 @@ public class ReplacingLocalizedString extends LocalizedString {
         return output;
     }
 
-    //javadoc inherited
-    public String getKey() {
-        return wrapped.getKey();
-    }
-
-    //javadoc inherited
-    public void setKey(String key) {
-        wrapped.setKey(key);
-    }
-
     // javadoc inherited
     public String get(Locale locale) {
-        return replace(wrapped.get(locale));
-    }
-
-    //javadoc inherited
-    public void set(String value, Locale locale) {
-        wrapped.set(value, locale);
+        return replace(super.get(locale));
     }
 
     /**
@@ -90,52 +70,44 @@ public class ReplacingLocalizedString extends LocalizedString {
      *
      * Also takes into account the replacements in the values (but only 'lazily', when actually requested).
      */
-    public Map asMap() {
-        final Map map = super.asMap();
-        return new AbstractMap() {
-                public Set entrySet() {
-                    return new AbstractSet() {
-                            public int size() {
-                                return map.size();
+    public Map<Locale, String> asMap() {
+        final Map<Locale, String> map = super.asMap();
+        return new AbstractMap<Locale, String>() {
+            public Set<Map.Entry<Locale, String>> entrySet() {
+                return new AbstractSet<Map.Entry<Locale, String>>() {
+                    public int size() {
+                        return map.size();
+                    }
+                    public Iterator<Map.Entry<Locale, String>> iterator() {
+                        final Iterator<Map.Entry<Locale, String>> it = map.entrySet().iterator();
+                        return new Iterator<Map.Entry<Locale, String>>() {
+                            public boolean hasNext() {
+                                return it.hasNext();
                             }
-                            public Iterator iterator() {
-                                final Iterator it = map.entrySet().iterator();
-                                return new Iterator() {
-                                        public boolean hasNext() {
-                                            return it.hasNext();
-                                        }
-                                        public Object next() {
-                                            final Map.Entry value = (Map.Entry) it.next();
-                                            return new Map.Entry() {
-                                                    public Object getKey() {
-                                                        return value.getKey();
-                                                    }
-                                                    public Object getValue() {
-                                                        return replace((String) value.getValue());
-                                                    }
-                                                    public Object setValue(Object v) {
-                                                        throw new UnsupportedOperationException(); // map is umodifiable
-                                                    }
-                                                };
-                                        }
-                                        public void remove() {
-                                            throw new UnsupportedOperationException(); // map is umodifiable
-                                        }
-                                    };
+                            public Map.Entry<Locale, String> next() {
+                                final Map.Entry<Locale, String> value = it.next();
+                                return new Map.Entry<Locale, String>() {
+                                    public Locale getKey() {
+                                        return value.getKey();
+                                    }
+                                    public String  getValue() {
+                                        return replace(value.getValue());
+                                    }
+                                    public String setValue(String v) {
+                                        throw new UnsupportedOperationException(); // map is umodifiable
+                                    }
+                                };
+                            }
+                            public void remove() {
+                                throw new UnsupportedOperationException(); // map is umodifiable
                             }
                         };
-                }
-            };
+                    }
+                };
+            }
+        };
     }
 
-    // javadoc inherited
-    public void setBundle(String b) {
-        wrapped.setBundle(b);
-    }
-
-    public String toString() {
-        return "replacing-" + wrapped.toString();
-    }
 
     public Object clone() {
         ReplacingLocalizedString clone = (ReplacingLocalizedString) super.clone();
@@ -148,7 +120,7 @@ public class ReplacingLocalizedString extends LocalizedString {
      */
     public static String makeLiteral(String s) {
         // sometimes, implementing java looks rather idiotic, but honestely, this is correct!
-        s =  s.replaceAll("\\\\", "\\\\\\\\"); 
+        s =  s.replaceAll("\\\\", "\\\\\\\\");
         return s.replaceAll("\\$", "\\\\\\$");
     }
 
@@ -156,7 +128,7 @@ public class ReplacingLocalizedString extends LocalizedString {
     public static void main(String argv[]) {
         ReplacingLocalizedString s = new ReplacingLocalizedString(new LocalizedString("abcd"));
         s.replaceAll("b", makeLiteral(argv[0]));
-        System.out.println(s.get(null));
+        System.out.println(s.get((Locale) null));
     }
 
 }
---
+++ src/org/mmbase/util/WrappedLocalizedString.java
@@ -0,0 +1,77 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.util;
+
+import java.util.*;
+import org.mmbase.util.logging.*;
+
+/**
+ * Extends and wraps LocalizedString. 
+ *
+ * @author Michiel Meeuwissen
+ * @version $Id: WrappedLocalizedString.java,v 1.1 2006-11-11 12:56:39 michiel Exp $
+ * @since MMBase-1.9
+ */
+public class WrappedLocalizedString extends LocalizedString {
+
+    private static final Logger log = Logging.getLoggerInstance(WrappedLocalizedString.class);
+
+
+    protected LocalizedString wrapped;
+    private Locale defaultLocale = null;
+
+    // just for the contract of Serializable
+    protected WrappedLocalizedString() {
+
+    }
+
+    /**
+     * @param s The wrapped LocalizedString.
+     */
+    public WrappedLocalizedString(LocalizedString s) {
+        if (s == null) s = new LocalizedString("NULL");
+        wrapped = s;
+    }
+
+    public Locale setLocale(Locale loc) {
+        Locale prev = defaultLocale;
+        defaultLocale = loc;
+        return prev;
+    }
+
+    //javadoc inherited
+    public String getKey() {
+        return wrapped.getKey();
+    }
+
+    //javadoc inherited
+    public void setKey(String key) {
+        wrapped.setKey(key);
+    }
+
+    // javadoc inherited
+    public String get(Locale locale) {
+        if (locale == null) locale = defaultLocale;
+        return wrapped.get(locale);
+    }
+
+    //javadoc inherited
+    public void set(String value, Locale locale) {
+        wrapped.set(value, locale);
+    }
+
+
+    // javadoc inherited
+    public void setBundle(String b) {
+        wrapped.setBundle(b);
+    }
+
+
+}
