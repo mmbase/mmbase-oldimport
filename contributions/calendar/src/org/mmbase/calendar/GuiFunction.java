029d23f507f760b2cf2a5db8692ea4880459e499
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2006-11-16 18:37:36 +0000

    gui function
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@21434 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ contributions/calendar/src/org/mmbase/calendar/GuiFunction.java
@@ -0,0 +1,53 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.calendar;
+
+import org.mmbase.util.functions.*;
+
+import org.mmbase.bridge.*;
+import org.mmbase.util.logging.Logger;
+import org.mmbase.util.logging.Logging;
+
+/**
+ * The gui function of calendar items and calendar item types.
+ *
+ * @author Michiel Meeuwissen
+ * @version $Id: GuiFunction.java,v 1.1 2006-11-16 18:37:36 michiel Exp $
+ * @since MMBase-1.9
+ */
+public class GuiFunction extends org.mmbase.util.functions.GuiFunction {
+
+    private static final Logger log = Logging.getLoggerInstance(GuiFunction.class);
+
+    public GuiFunction() {
+        super();
+    }
+
+    protected String getFunctionValue(Node node, Parameters parameters) {       
+        String fieldName = (String) parameters.get(Parameter.FIELD);
+        boolean nodeGui = fieldName == null || "".equals(fieldName);
+        String color = null;
+        if ("type".equals(fieldName) || (nodeGui && node.getNodeManager().hasField("type"))) {
+            Node typeNode = node.getNodeValue("type");
+            if (typeNode != null) {
+                color = typeNode.getStringValue("color");
+            }
+        } else if ("color".equals(fieldName) || (nodeGui && node.getNodeManager().hasField("color"))) {
+            color = node.getStringValue("color");
+        }
+        if (color != null) {
+            return "<span style='background-color: " + color + ";'>" + super.getFunctionValue(node, parameters) + "</span>";
+        } else {
+            return super.getFunctionValue(node, parameters);
+        }
+
+    }
+
+}
