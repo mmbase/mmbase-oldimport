742d20731534546c4319c179c8cfbdafa5f6b71e
nklasens <nklasens@91430c5f-d668-0410-9e7d-dd663366dcfd>
2006-11-17 13:07:05 +0000

    Don't add entry when not started yet.
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@21439 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/crontab/src/org/mmbase/applications/crontab/builders/CronJobs.java
+++ applications/crontab/src/org/mmbase/applications/crontab/builders/CronJobs.java
@@ -84,11 +84,13 @@ public class CronJobs extends MMObjectBuilder implements Runnable {
      */
     public int insert(String owner, MMObjectNode objectNodenode) {
         int number = super.insert(owner, objectNodenode);
-        Node node = getCloud().getNode(number);
         try {
+            if (cronDaemon != null) {
+		        Node node = getCloud().getNode(number);
             cronDaemon.add(createCronEntry(node));
+            }
         } catch (Exception e) {
-            throw new RuntimeException("error while creating cron entry with id " + node.getNumber() + " error " + e.getMessage(), e);
+            throw new RuntimeException("error while creating cron entry with id " + number + " error " + e.getMessage(), e);
         }
         return number;
     }
