ff88dfcd1811225165fae46707f6ba1245c25f37
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2006-11-21 16:10:28 +0000

    added stall-server param
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@21485 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/servlet/MMBaseStartThread.java
+++ src/org/mmbase/servlet/MMBaseStartThread.java
@@ -19,37 +19,45 @@ import javax.servlet.ServletException;
  * Seperate thread to init MMBase. This is because init() of Servlets and Filters must take little
  * time, to not hold other web-apps.  Init of MMBase may take indefinitely if e.g. the database is down.
  *
- * @version $Id: MMBaseStartThread.java,v 1.5 2005-11-30 15:58:04 pierre Exp $
+ * @version $Id: MMBaseStartThread.java,v 1.6 2006-11-21 16:10:28 michiel Exp $
  * @author Michiel Meeuwissen
  * @since MMBase-1.7
  */
 public class MMBaseStartThread extends Thread {
 
     private   static final Logger log = Logging.getLoggerInstance(MMBaseStartThread.class);
-    private MMBaseStarter starter;
     public MMBaseStartThread(MMBaseStarter s) {
-        super("MMBase Start Thread");
-        starter = s;
+        super(new Job(s), "MMBase Start Thread");
         setDaemon(true); // if init never ends, don't hinder destroy
     }
-    public void run() {
-        synchronized(Module.class) {
 
-            if (starter.getMMBase() == null) {
-                try {
-                    MMBase mmb = MMBase.getMMBase();
-                    if (mmb == null) {
-                        throw new Exception("getMMBase gave null");
+
+    /**
+     * @since MMBase-1.9
+     */
+    public static class Job implements Runnable {
+        private final MMBaseStarter starter;
+        public Job(MMBaseStarter s) {
+            starter = s;
+        }
+
+        public void run() {
+            synchronized(Module.class) {
+                if (starter.getMMBase() == null) {
+                    try {
+                        MMBase mmb = MMBase.getMMBase();
+                        if (mmb == null) {
+                            throw new Exception("getMMBase gave null");
+                        }
+                        starter.setInitException(null); // no error.
+                        starter.setMMBase(mmb);
+                    } catch (Throwable e) {
+                        log.fatal("Could not instantiate the MMBase module! " + e.getClass().getName() + " " + e.getMessage());
+                        log.error(e);
+                        starter.setInitException(new ServletException(e));
                     }
-                    starter.setInitException(null); // no error.
-                    starter.setMMBase(mmb);
-                } catch (Throwable e) {
-                    log.fatal("Could not instantiate the MMBase module! " + e.getClass().getName() + " " + e.getMessage());
-                    log.error(Logging.stackTrace(e));
-                    starter.setInitException(new ServletException(e));
                 }
             }
-
         }
     }
 
