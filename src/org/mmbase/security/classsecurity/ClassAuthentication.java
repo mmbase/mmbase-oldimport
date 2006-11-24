43406a0400bb032708f25fa1edd3fcaba6a5aefb
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2006-11-24 15:17:28 +0000

    generics
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@21546 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/security/classsecurity/ClassAuthentication.java
+++ src/org/mmbase/security/classsecurity/ClassAuthentication.java
@@ -29,7 +29,7 @@ import org.xml.sax.InputSource;
  * its configuration file, contains this configuration.
  *
  * @author   Michiel Meeuwissen
- * @version  $Id: ClassAuthentication.java,v 1.14 2006-10-14 14:35:39 nklasens Exp $
+ * @version  $Id: ClassAuthentication.java,v 1.15 2006-11-24 15:17:28 michiel Exp $
  * @see      ClassAuthenticationWrapper
  * @since    MMBase-1.8
  */
@@ -41,7 +41,7 @@ public class ClassAuthentication {
     static {
         XMLEntityResolver.registerPublicID(PUBLIC_ID_CLASSSECURITY_1_0, DTD_CLASSSECURITY_1_0, ClassAuthentication.class);
     }
-    private static List authenticatedClasses = null;
+    private static List<Login> authenticatedClasses = null;
 
 
     static ResourceWatcher watcher = null;
@@ -65,13 +65,13 @@ public class ClassAuthentication {
      * Reads the configuration file and instantiates and loads the wrapped Authentication.
      */
     protected static synchronized void load(String configFile) throws SecurityException {
-        List resourceList = MMBaseCopConfig.securityLoader.getResourceList(configFile);
+        List<URL> resourceList = MMBaseCopConfig.securityLoader.getResourceList(configFile);
         log.info("Loading " + configFile + "( " + resourceList + ")");
         authenticatedClasses = new ArrayList();
-        ListIterator it = resourceList.listIterator();
+        ListIterator<URL> it = resourceList.listIterator();
         while (it.hasNext()) it.next();
         while (it.hasPrevious()) {
-            URL u = (URL) it.previous();
+            URL u = it.previous();
             try {
                 URLConnection con = u.openConnection();
                 if (! con.getDoInput()) continue;
@@ -88,7 +88,7 @@ public class ClassAuthentication {
                     String clazz    = node.getAttributes().getNamedItem("class").getNodeValue();
                     String method   = node.getAttributes().getNamedItem("method").getNodeValue();
                     Node property   = node.getFirstChild();
-                    Map map = new HashMap();
+                    Map<String, String> map = new HashMap();
                     while (property != null) {
                         if (property instanceof Element && property.getNodeName().equals("property")) {
                             String name     = property.getAttributes().getNamedItem("name").getNodeValue();
@@ -105,7 +105,7 @@ public class ClassAuthentication {
         }
 
         { // last fall back, everybody may get the 'anonymous' cloud.
-            Map map = new HashMap();
+            Map<String, String> map = new HashMap();
             map.put("rank", "anonymous");
             authenticatedClasses.add(new Login(Pattern.compile(".*"), "class", Collections.unmodifiableMap(map)));
         }
@@ -148,10 +148,7 @@ public class ClassAuthentication {
         Throwable t = new Throwable();
         StackTraceElement[] stack = t.getStackTrace();
 
-        Iterator i = authenticatedClasses.iterator();
-
-        while(i.hasNext()) {
-            Login n = (Login) i.next();
+        for (Login n : authenticatedClasses) {
             if (application == null || application.equals(n.application)) {
                 Pattern p = n.classPattern;
                 for (int j = 0; j < stack.length; j++) {
@@ -180,14 +177,14 @@ public class ClassAuthentication {
     public static class  Login {
         Pattern classPattern;
         String application;
-        Map    map;
-        Login(Pattern p , String a, Map m) {
+        Map<String, String>    map;
+        Login(Pattern p , String a, Map<String, String> m) {
             classPattern = p;
             application = a;
             map = m;
         }
 
-        public Map getMap() {
+        public Map<String, String> getMap() {
             return map;
         }
         public String toString() {
