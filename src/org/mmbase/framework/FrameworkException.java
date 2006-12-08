d69964555116b9ae135619814af821fa3ebe2ad7
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2006-12-08 16:25:37 +0000

    pierre: let's use this rather then IOException
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@21772 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/framework/FrameworkException.java
@@ -0,0 +1,41 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.framework;
+
+/**
+ * This exception gets thrown when something goes wrong in the Framework,
+ * such as when rendering or processing a component.
+ *
+ * @author Pierre van Rooden
+ * @version $Id: FrameworkException.java,v 1.1 2006-12-08 16:25:37 michiel Exp $
+ */
+public class FrameworkException extends Exception {
+
+    //javadoc is inherited
+    public FrameworkException() {
+        super();
+    }
+
+    //javadoc is inherited
+    public FrameworkException(String message) {
+        super(message);
+    }
+
+    //javadoc is inherited
+    public FrameworkException(Throwable cause) {
+        super(cause);
+    }
+
+    //javadoc is inherited
+    public FrameworkException(String message, Throwable cause) {
+        super(message,cause);
+    }
+
+}
