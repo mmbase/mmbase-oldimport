b4175b4f9e252a3c357670a4ceff5f55ebb140fd
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-01-02 15:40:48 +0000

    added an (optional) 'total' field to poll builder, and a cronjob to periodically update that. Also added some attributes to the vote-tag, (so that you can use it also for completely implicit votes).
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@22071 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/poll/src/org/mmbase/poll/Total.java
+++ contributions/poll/src/org/mmbase/poll/Total.java
@@ -9,7 +9,7 @@ import org.mmbase.util.logging.Logging;
  * A Node-function on the 'poll' builder, which simply calculates the total number of given answers.
  *
  * @author Michiel Meeuwissen
- * @version $Id: Total.java,v 1.1 2006-12-07 17:01:52 michiel Exp $
+ * @version $Id: Total.java,v 1.2 2007-01-02 15:40:48 michiel Exp $
  * @since MMBase-1.8
  */
 public final class Total {
@@ -17,6 +17,7 @@ public final class Total {
     private static final Logger log = Logging.getLoggerInstance(Total.class);
 
     private Node node;
+    private boolean calculate = false;
 
     /**
      * Makes this bean useable as a Node function.
@@ -25,16 +26,27 @@ public final class Total {
         this.node = node;
     }
 
+    public void setCalculate(Boolean b) {
+        calculate = b;
+    }
+    public boolean getCalculate() {
+        return calculate;
+    }
+
 
     public int total() {
         if (node == null) throw new IllegalArgumentException("total is a node-function");
-        int tot = 0;
-        NodeList answers = node.getRelatedNodes("answer");
-        NodeIterator i = answers.nodeIterator();
-        while(i.hasNext()) {
-            int t = i.nextNode().getIntValue("total_answers");
-            if (t > 0) tot += t;
+        if (calculate || ! node.getNodeManager().hasField("total")) {
+            int tot = 0;
+            NodeList answers = node.getRelatedNodes("answer");
+            NodeIterator i = answers.nodeIterator();
+            while(i.hasNext()) {
+                int t = i.nextNode().getIntValue("total_answers");
+                if (t > 0) tot += t;
+            }
+            return tot;
+        } else {
+            return node.getIntValue("total");
         }
-        return tot;
     }
 }
---
+++ contributions/poll/src/org/mmbase/poll/TotalUpdater.java
@@ -0,0 +1,53 @@
+package org.mmbase.poll;
+
+import org.mmbase.bridge.*;
+import org.mmbase.bridge.util.Queries;
+import org.mmbase.storage.search.*;
+import java.util.*;
+import org.mmbase.util.logging.Logger;
+import org.mmbase.util.logging.Logging;
+
+/**
+ * If the mmbase 'crontab' module is active this class can (and defaultly is) scheduled to
+ * periodically run to update the 'total' field of the poll builder (if this field exists).
+ *
+ * @author Michiel Meeuwissen
+ * @version $Id: TotalUpdater.java,v 1.1 2007-01-02 15:40:48 michiel Exp $
+ * @since MMBase-1.8
+ */
+public class TotalUpdater implements Runnable {
+
+    private static final Logger log = Logging.getLoggerInstance(TotalUpdater.class);
+
+
+
+    public void run() {
+        Cloud cloud = ContextProvider.getDefaultCloudContext().getCloud("mmbase", "class", null);
+        NodeManager poll = cloud.getNodeManager("poll");
+        if (poll.hasField("total")) {
+            NodeQuery q = poll.createQuery();
+            Date now = new Date();
+            Constraint cons1 = Queries.createConstraint(q, "begin", FieldCompareConstraint.LESS, now);
+            Constraint cons2 = Queries.createConstraint(q, "end", FieldCompareConstraint.GREATER, now);
+            Queries.addConstraint(q, cons1);
+            Queries.addConstraint(q, cons2);
+            NodeList nodes = poll.getList(q);
+            NodeIterator ne = nodes.nodeIterator();
+            while (ne.hasNext()) {
+                Node p = ne.nextNode();
+                Total total = new Total();
+                total.setNode(p);
+                total.setCalculate(true);
+                int t = total.total();
+                if (t != p.getIntValue("total")) {
+                    log.service("Updated total of poll " + p.getNumber() + " to " + t);
+                    p.setIntValue("total", t);
+                    p.commit();
+                }
+            }
+        } else {
+            log.warn("no 'total' field");
+        }
+
+    }
+}
