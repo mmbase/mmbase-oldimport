8aa0b25534442a0be8445d3a855cbc91ea397f23
mmeeuwissen <mmeeuwissen@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-01-08 09:08:14 +0000

    mimetype property
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@22116 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/didactor2/src/assessment/java/nl/didactor/component/assessment/email_notification/model/Email.java
+++ contributions/didactor2/src/assessment/java/nl/didactor/component/assessment/email_notification/model/Email.java
@@ -1,43 +1,58 @@
 package nl.didactor.component.assessment.email_notification.model;
 
+/**
+ * @javadoc
+ * @version $Id: Email.java,v 1.2 2007-01-08 09:08:14 mmeeuwissen Exp $
+ */
+
 public class Email {
-   private String body;
-   private String from;
-   private String to;
-   private String subject;
+    private String body;
+    private String from;
+    private String to;
+    private String subject;
+    private String mimeType = "text/plain";
 
-   public Email() {
-   }
+    public Email() {
+    }
 
-   public void setBody(String body) {
-      this.body = body;
-   }
+    public void setBody(String body) {
+        this.body = body;
+    }
 
-   public void setFrom(String from) {
+    public void setFrom(String from) {
       this.from = from;
    }
 
-   public void setTo(String to) {
-      this.to = to;
-   }
+    public void setTo(String to) {
+        this.to = to;
+    }
 
-   public void setSubject(String subject) {
-      this.subject = subject;
-   }
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
 
-   public String getBody() {
-      return body;
-   }
+    public void setMimeType(String mimeType) {
+        this.mimeType = mimeType;
+    }
 
-   public String getFrom() {
-      return from;
-   }
+    public String getBody() {
+        return body;
+    }
 
-   public String getTo() {
-      return to;
-   }
+    public String getFrom() {
+        return from;
+    }
 
-   public String getSubject() {
-      return subject;
-   }
+    public String getTo() {
+        return to;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getMimeType() {
+        return mimeType;
+    }
+
 }
