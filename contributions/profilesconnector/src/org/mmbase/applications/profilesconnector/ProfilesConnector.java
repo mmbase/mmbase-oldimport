024aead79f8850a1e757cb028e886c525d41cb42
ernst <ernst@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-02-13 14:38:20 +0000

    improvement. jira: PC-1
    the urls for profielen and profielengs (those are webservices to upate external profiles) are now configured using the new mmbob properties feature.
    There should be twoo global properties 'profielen' and 'profielengs'
    
    
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@22515 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/profilesconnector/src/org/mmbase/applications/profilesconnector/ProfilesConnector.java
+++ contributions/profilesconnector/src/org/mmbase/applications/profilesconnector/ProfilesConnector.java
@@ -13,13 +13,17 @@ import java.io.*;
 public class ProfilesConnector implements ExternalProfileInterface {
 
     static private Logger log = Logging.getLoggerInstance(ProfilesConnector.class);
-    static String PROFIELEN = "http://profielen.test.kennisnet.nl/profielen/services/KennisnetProfielen";
-    static String PROFIELENGS = "http://profielen.test.kennisnet.nl/profielen/services/KennisnetProfielenGS";
 
     String getVersion() {
         return "1.2.3";
     }
 
+    /**
+     * @see org.mmbase.applications.mmbob.ExternalProfileInterface#getValue(java.lang.String, java.lang.String)
+     * either using PROFIELEN or PROFIELENGS webservice to get a profile field. What service to use is determined by the 
+     * target parameter.
+     * When 'target' has type 'GS' the PROFIELENGS service is used, and oterwise the PROFIELEN webservice is used 
+     */
     public String getValue(String entreeid, String target) {
         StringTokenizer tok = new StringTokenizer(target, ":\n\r");
         if (tok.hasMoreTokens()) {
@@ -44,6 +48,12 @@ public class ProfilesConnector implements ExternalProfileInterface {
         return null;
     }
 
+    /**
+     * @see org.mmbase.applications.mmbob.ExternalProfileInterface#setValue(java.lang.String, java.lang.String, java.lang.String)
+     * either using PROFIELEN or PROFIELENGS webservice to set a profile field. What service to use is determined by the 
+     * target parameter.
+     * When 'target' has type 'GS' the PROFIELENGS service is used, and oterwise the PROFIELEN webservice is used 
+     */
     public boolean setValue(String entreeid, String target, String value) {
         StringTokenizer tok = new StringTokenizer(target, ":\n\r");
         if (tok.hasMoreTokens()) {
@@ -70,8 +80,9 @@ public class ProfilesConnector implements ExternalProfileInterface {
 
     // dummy routines
     private String getGSField(int dienstID, String entreeID, int veldID) {
+        String profielenGsUrl = initProfielenGS();
         try {
-            URL url = new URL(PROFIELENGS);
+            URL url = new URL(profielenGsUrl);
             ProfielenService profielenService = new ProfielenServiceLocator();
             Profielen profielen = profielenService.getKennisnetProfielen(url);
             log.info("Client versie=" + ProfielenClient.getVersion());
@@ -91,13 +102,44 @@ public class ProfilesConnector implements ExternalProfileInterface {
         return null;
     }
 
+    /**
+     * read the url's for the webservices from global properties profielen and profielengs.
+     */
+    private String initProfielen() {
+        String profielenUrl = ForumManager.getProperty("profielen");
+        try {
+            new URL(profielenUrl);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("could not create valid url from property 'profielen' with value: '" + profielenUrl + "'");
+        }
+        return profielenUrl;
+    }
+    
+    private String initProfielenGS(){
+        String profielenGsUrl = ForumManager.getProperty("profielengs");
+        try {
+            new URL(profielenGsUrl);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("could not create valid url from property 'profielen' with value: '" + profielenGsUrl + "'");
+        }
+        return profielenGsUrl;
+    }
+
     private boolean setGSField(int dienstID, String entreeID, int veldID, String value) {
         return true;
     }
 
+    /**
+     * Read a profile field from the 'profielen' webservice;
+     * @param dienstID
+     * @param entreeID
+     * @param veldID
+     * @return
+     */
     private String getBSField(int dienstID, String entreeID, String veldID) {
+        String profielenUrl = initProfielen();
         try {
-            URL url = new URL(PROFIELEN);
+            URL url = new URL(profielenUrl);
             ProfielenService profielenService = new ProfielenServiceLocator();
             Profielen profielen = profielenService.getKennisnetProfielen(url);
             log.info("Client versie=" + ProfielenClient.getVersion());
