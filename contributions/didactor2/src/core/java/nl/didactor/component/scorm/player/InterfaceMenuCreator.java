d92f0609f23a51ec536b48cd9c8cef7d0b50e7ef
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-04-24 16:01:04 +0000

    sigh, wtf
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@23266 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/didactor2/src/core/java/nl/didactor/component/scorm/player/InterfaceMenuCreator.java
+++ contributions/didactor2/src/core/java/nl/didactor/component/scorm/player/InterfaceMenuCreator.java
@@ -1,15 +1,16 @@
 package nl.didactor.component.scorm.player;
 
 /**
- * <p>Title: </p>
- * <p>Description: </p>
- * <p>Copyright: Copyright (c) 2005</p>
- * <p>Company: </p>
- * @author not attributable
- * @version 1.0
+ * WTF is this.
+ *
+ * @javadoc
+ * @author 
+ * @version $Id: InterfaceMenuCreator.java,v 1.3 2007-04-24 16:01:04 michiel Exp $
  */
 
-public interface InterfaceMenuCreator
-{
+public interface InterfaceMenuCreator {
+    /**
+     * @javadoc _please!_
+     */
    public String[] parse(boolean useRelativePaths, String sPackageName, String sSubPath);
 }
