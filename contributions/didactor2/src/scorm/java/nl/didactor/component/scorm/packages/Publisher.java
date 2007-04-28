3630f90e3a6c627dce9b569c9ee87c62bb763ff4
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-04-28 09:28:54 +0000

    code conventions
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@23306 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/didactor2/src/scorm/java/nl/didactor/component/scorm/packages/Publisher.java
+++ contributions/didactor2/src/scorm/java/nl/didactor/component/scorm/packages/Publisher.java
@@ -5,54 +5,46 @@ import java.util.Iterator;
 
 import org.mmbase.bridge.*;
 
+/**
+ * @javadoc
+ */
 
+public class Publisher {
+    private final Cloud cloud;
+    
+    public Publisher(Cloud cloud) {
+        this.cloud = cloud;
+    }
+    
+    
+    
 
-
-public class Publisher
-{
-   private Cloud cloud;
-
-   public Publisher(Cloud cloud)
-   {
-      this.cloud = cloud;
-   }
-
-
-
-
-   public void savePackage(Node nodeEducation) throws Exception
-   {
-      NodeList nlRelatedLearnBlocks = nodeEducation.getRelatedNodes("learnblocks", "posrel", "destination");
-      for(Iterator it = nlRelatedLearnBlocks.iterator(); it.hasNext();)
-      {
-         Node nodeLeranBlock = (Node) it.next();
-         this.saveLearnBlock(nodeLeranBlock);
-      }
-   }
-
-
-
-
-   private void saveLearnBlock(Node nodeLearnBlock) throws Exception
-   {
-//      System.out.println("learnblock=" + nodeLearnBlock.getNumber());
-      NodeList nlRelatedLearnBlocks = nodeLearnBlock.getRelatedNodes("learnblocks", "posrel", "destination");
-      for(Iterator it = nlRelatedLearnBlocks.iterator(); it.hasNext();)
-      {
-         Node nodeLeranBlock = (Node) it.next();
-         this.saveLearnBlock(nodeLeranBlock);
-      }
-
-
-      NodeList nlRelatedHtmlPages = nodeLearnBlock.getRelatedNodes("htmlpages");
-      for(Iterator it = nlRelatedHtmlPages.iterator(); it.hasNext();)
-      {
-         Node nodeHtmlPage = (Node) it.next();
-//         System.out.println("htmlpage=" + nodeHtmlPage.getNumber());
-         RandomAccessFile fileHtmlPage = new RandomAccessFile((String) nodeHtmlPage.getValue("path"), "rw");
-         fileHtmlPage.writeBytes((String) nodeHtmlPage.getValue("content"));
-         fileHtmlPage.close();
-      }
-   }
-
+    public void savePackage(Node nodeEducation) throws Exception {
+        NodeList nlRelatedLearnBlocks = nodeEducation.getRelatedNodes("learnblocks", "posrel", "destination");
+        for(Iterator it = nlRelatedLearnBlocks.iterator(); it.hasNext();) {
+            Node nodeLearnBlock = (Node) it.next();
+            this.saveLearnBlock(nodeLearnBlock);
+        }
+    }
+    
+    
+    
+    
+    private void saveLearnBlock(Node nodeLearnBlock) throws Exception {
+        NodeList nlRelatedLearnBlocks = nodeLearnBlock.getRelatedNodes("learnblocks", "posrel", "destination");
+        for(Iterator it = nlRelatedLearnBlocks.iterator(); it.hasNext();) {
+            Node n = (Node) it.next();
+            this.saveLearnBlock(n);
+        }
+        
+        
+        NodeList nlRelatedHtmlPages = nodeLearnBlock.getRelatedNodes("htmlpages");
+        for(Iterator it = nlRelatedHtmlPages.iterator(); it.hasNext();) {
+            Node nodeHtmlPage = (Node) it.next();
+            RandomAccessFile fileHtmlPage = new RandomAccessFile((String) nodeHtmlPage.getValue("path"), "rw");
+            fileHtmlPage.writeBytes((String) nodeHtmlPage.getValue("content"));
+            fileHtmlPage.close();
+        }
+    }
+    
 }
