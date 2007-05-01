f6a0af66874abf27c32c1dbf1bc356de8f35aafa
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-05-01 15:48:48 +0000

    generics, returns Sets now contains actual nodes in stead of only the numbers (as string) (because it is a petty to loose all that information)
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@23348 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/didactor2/src/education/java/nl/didactor/component/education/utils/EducationPeopleConnector.java
+++ contributions/didactor2/src/education/java/nl/didactor/component/education/utils/EducationPeopleConnector.java
@@ -12,7 +12,7 @@ import org.mmbase.module.core.*;
  * somethings wrong here, and making things less clear in JSP's? (because you need to guess what
  * happens in this class, which btw also lacks javadoc).
  *
- * @version $Id: EducationPeopleConnector.java,v 1.5 2006-12-04 19:03:35 mmeeuwissen Exp $
+ * @version $Id: EducationPeopleConnector.java,v 1.6 2007-05-01 15:48:48 michiel Exp $
  */
 public class EducationPeopleConnector {
     final Cloud cloud;
@@ -21,14 +21,11 @@ public class EducationPeopleConnector {
         this.cloud = cloud;
     }
 
-    public Set relatedPersons(String educationNumber) {
-        Set hsetResult = new HashSet();
-
-        Node nodeEducation = cloud.getNode((new Integer(educationNumber)).intValue());
-
+    public Set<Node> relatedPersons(Node nodeEducation) {
+        Set<Node> hsetResult = new HashSet<Node>();
         NodeList nodelistPeople = nodeEducation.getRelatedNodes("people", "classrel", "destination");
         for(NodeIterator it = nodelistPeople.nodeIterator(); it.hasNext(); ) {
-            hsetResult.add("" + it.nextNode().getNumber());
+            hsetResult.add(it.nextNode());
         }
 
         NodeList nodelistClasses = nodeEducation.getRelatedNodes("classes", "classrel", "destination");
@@ -36,20 +33,24 @@ public class EducationPeopleConnector {
             Node nodeClass = it.nextNode();
             nodelistPeople = nodeClass.getRelatedNodes("people", "classrel", "source");
             for(NodeIterator it2 = nodelistPeople.nodeIterator(); it2.hasNext();) {
-                hsetResult.add("" + it2.nextNode().getNumber());
+                hsetResult.add(it2.nextNode());
             }
         }
         return hsetResult;
     }
 
-    public Set relatedEducations(String personNumber) {
+    public Set<Node> relatedPersons(String educationNumber) {
+        Node nodeEducation = cloud.getNode(educationNumber);
+        return relatedPersons(nodeEducation);
 
-        Set hsetResult = new HashSet();
-        Node nodePerson = cloud.getNode((new Integer(personNumber)).intValue());
+    }
+
+    public Set<Node> relatedEducations(Node nodePerson) {
+        Set<Node> hsetResult = new HashSet<Node>();
 
         NodeList nodelistEducations = nodePerson.getRelatedNodes("educations", "classrel", "source");
         for(NodeIterator it = nodelistEducations.nodeIterator(); it.hasNext(); ) {
-            hsetResult.add("" + it.nextNode().getNumber());
+            hsetResult.add(it.nextNode());
         }
 
         NodeList nodelistClasses = nodePerson.getRelatedNodes("classes", "classrel", "destination");
@@ -57,10 +58,15 @@ public class EducationPeopleConnector {
             Node nodeClass = it.nextNode();
             NodeList nodelistPeople = nodeClass.getRelatedNodes("educations", "classrel", "source");
             for(NodeIterator it2 = nodelistPeople.nodeIterator(); it2.hasNext();) {
-                hsetResult.add("" + it2.nextNode().getNumber());
+                hsetResult.add(it2.nextNode());
             }
         }
         return hsetResult;
+    }
+
+    public Set<Node> relatedEducations(String personNumber) {
+        Node nodePerson = cloud.getNode(personNumber);
+        return relatedEducations(nodePerson);
     }
 
 }
