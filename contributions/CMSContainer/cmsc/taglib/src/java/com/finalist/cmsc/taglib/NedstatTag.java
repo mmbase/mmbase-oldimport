850374dccbf3a8a90c57759988ff248c8d3cc7dd
nklasens <nklasens@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-05-21 18:25:37 +0000

    CMSC-437 Taglib features present in customer projects
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@23674 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer/cmsc/taglib/src/java/com/finalist/cmsc/taglib/NedstatTag.java
+++ contributions/CMSContainer/cmsc/taglib/src/java/com/finalist/cmsc/taglib/NedstatTag.java
@@ -11,6 +11,7 @@ package com.finalist.cmsc.taglib;
 
 import java.io.IOException;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.PageContext;
 import javax.servlet.jsp.JspTagException;
@@ -59,7 +60,8 @@ public class NedstatTag extends CmscTag {
     
 	public void doTag() throws JspException, IOException {				
 		/*
-		 *  Only construct nedstat tag if this is the live production environment. The production environment to be added in the context file is 'server/production' set to 'true' or 'false'.
+		 *  Only construct nedstat tag if this is the live production environment. The production environment
+		 *  to be added in the context file is 'server/production' set to 'true' or 'false'.
 		 */					
 		if (ServerUtil.isLive() && Boolean.valueOf(ServerUtil.getEnvironmentVariableValue(PRODUCTIONVARIABLE))) {			
 			String nedstat = "";
@@ -109,42 +111,63 @@ public class NedstatTag extends CmscTag {
 			String type = getType().toLowerCase();
 			if (type.equals(TYPE_NORMAL)){
 				nedstat = "<!-- Begin Sitestat4 code -->\n<script type=\'text/javascript\'>\n";
-				nedstat = nedstat + "<!--\nfunction sitestat(ns_l){ns_l+=\'&amp;ns__t=\'+(new Date()).getTime();ns_pixelUrl=ns_l;\n";
-				nedstat = nedstat + "ns_0=document.referrer;\n";
-				nedstat = nedstat + "ns_0=(ns_0.lastIndexOf(\'/\')==ns_0.length-1)?ns_0.substring(ns_0.lastIndexOf(\'/\'),0):ns_0;\n";
-				nedstat = nedstat + "if(ns_0.length>0)ns_l+='&amp;ns_referrer='+escape(ns_0);\n";
-				nedstat = nedstat + "if(document.images){ns_1=new Image();ns_1.src=ns_l;}else\n";
-				nedstat = nedstat + "document.write(\'<img src=\"\'+ns_l+\'\" width=\"1\" height=\"1\" alt=\"\">\');}";
-				nedstat = nedstat + "sitestat(\"http://" + getLandCode() + ".sitestat.com/" + getCustomerName() + "/" + getSiteName() + "/s?" + getCounterName() + "\");//-->\n</script>"; 			
-				nedstat = nedstat + "<noscript>\n<img src=\"http://" + getLandCode()+ ".sitestat.com/" + getCustomerName() + "/" + getSiteName() + "/s?" + getCounterName() + "\" width=\"1\" height=\"1\" alt=\"\" /></noscript>\n";								
-				nedstat = nedstat + "<!-- End Sitestat4 code -->";
+				nedstat += "<!--\nfunction sitestat(ns_l){ns_l+=\'&amp;ns__t=\'+(new Date()).getTime();ns_pixelUrl=ns_l;\n";
+				nedstat += "ns_0=document.referrer;\n";
+				nedstat += "ns_0=(ns_0.lastIndexOf(\'/\')==ns_0.length-1)?ns_0.substring(ns_0.lastIndexOf(\'/\'),0):ns_0;\n";
+				nedstat += "if(ns_0.length>0)ns_l+='&amp;ns_referrer='+escape(ns_0);\n";
+				nedstat += "if(document.images){ns_1=new Image();ns_1.src=ns_l;}else\n";
+				nedstat += "document.write(\'<img src=\"\'+ns_l+\'\" width=\"1\" height=\"1\" alt=\"\">\');}";
+				nedstat += "sitestat(\"http://" + getLandCode() + ".sitestat.com/" + getCustomerName() + "/" +
+				            getSiteName() + "/s?" + getCounterName() + "\");//-->\n</script>"; 			
+				nedstat += "<noscript>\n" +
+						"<img src=\"http://" + getLandCode()+ ".sitestat.com/" + getCustomerName() + "/" +
+						    getSiteName() + "/s?" + getCounterName() + "\" width=\"1\" height=\"1\" alt=\"\" />" +
+						"</noscript>\n";
+				nedstat += "<!-- End Sitestat4 code -->";
 			}
 			else if (type.equals(TYPE_TECHNICAL)){
-				nedstat = "<!-- Begin Sitestat4 Technical code -->\n<script type=\'text/javascript\' src=\'/sitestat.js\'></script>\n<!-- End Sitestat4 Technical Code -->";			
+				nedstat = "<!-- Begin Sitestat4 Technical code -->\n" +
+						"<script type=\'text/javascript\' src=\'" + getJavascriptFile(ctx) + "\'></script>\n" +
+						"<!-- End Sitestat4 Technical Code -->";			
 			}
 			else if (type.equals(TYPE_LOADTIME1)){
 				// Place just underneath the <head> tag.
-				nedstat = "<!-- Begin Sitestat4 Loadingtime1 code -->\n<script type=\'text/javascript\'>ns_loadingtime1=(new Date()).getTime()</script>\n<!-- End Sitestat4 Loadingtime1 code -->";
+				nedstat = "<!-- Begin Sitestat4 Loadingtime1 code -->\n" +
+						"<script type=\'text/javascript\'>ns_loadingtime1=(new Date()).getTime()</script>\n" +
+						"<!-- End Sitestat4 Loadingtime1 code -->";
 			}
 			else if (type.equals(TYPE_LOADTIME2)){
 				// Place just before the </body> tag, after the technical measurement.
-				nedstat = "<!-- Begin Sitestat4 Loadingtime2 code -->\n<script type=\'text/javascript\' src=\'/sitestat.js\'></script>\n<script type=\'text/javascript\'>ns_loadingtime2=(new Date()).getTime()</script>\n<!-- End Sitestat4 Loadingtime2 code -->";				
+                nedstat = "<!-- Begin Sitestat4 Loadingtime2 code -->\n" +
+                		"<script type=\'text/javascript\' src=\'" + getJavascriptFile(ctx) + "\'></script>\n" +
+                		"<script type=\'text/javascript\'>ns_loadingtime2=(new Date()).getTime()</script>\n" +
+                		"<!-- End Sitestat4 Loadingtime2 code -->";				
 			}
 			else if (type.equals(TYPE_CLICKIN)){
 				if (getExternalUrl() == null) {
 					throw new JspTagException("No external url provided for type " + getType() + ".");
 				}
-				nedstat = "<!-- Begin Sitestat4 Clickin code -->\n<a href=\"http://" + getLandCode() + ".sitestat.com/" + getCustomerName() + "/" + getSiteName() + "/s?" + getCounterName() + "&amp;ns_type=clickin&amp;ns_url=" + getExternalUrl() + "\">" + getUrlText() + "</a>\n<!-- End Sitestat4 Clickin code -->";			
+				nedstat = "<!-- Begin Sitestat4 Clickin code -->\n" +
+						"<a href=\"http://" + getLandCode() + ".sitestat.com/" + getCustomerName() + "/" +
+						getSiteName() + "/s?" + getCounterName() + "&amp;ns_type=clickin&amp;ns_url=" +
+						getExternalUrl() + "\">" + getUrlText() + "</a>\n" +
+						"<!-- End Sitestat4 Clickin code -->";			
 			}
 			else if (type.equals(TYPE_CLICKOUT)){
 				if (getExternalUrl() == null){
 					throw new JspTagException("No external url provided for type " + getType() + ".");
 				}
-				nedstat = "<!-- Begin Sitestat4 Clickout code -->\n<a href=\"http://" + getLandCode() + ".sitestat.com/" + getCustomerName() + "/" + getSiteName() + "/s?" + getCounterName() + "&amp;ns_type=clickout&amp;ns_url=" + getExternalUrl() + "\">" + getUrlText() + "</a>\n<!-- End Sitestat4 Clickout code -->";
+				nedstat = "<!-- Begin Sitestat4 Clickout code -->\n" +
+						"<a href=\"http://" + getLandCode() + ".sitestat.com/" + getCustomerName() + "/" +
+						getSiteName() + "/s?" + getCounterName() + "&amp;ns_type=clickout&amp;ns_url=" +
+						getExternalUrl() + "\">" + getUrlText() + "</a>\n" +
+						"<!-- End Sitestat4 Clickout code -->";
 			}		
 			else if (type.equals(TYPE_BOOKMARK)){
 				// Place in head section.
-				nedstat = "<link rel=\"shortcut icon\" href=\"http://" + getLandCode() + ".sitestat.com/" + getCustomerName() + "/" + getSiteName() + "/s?" + getCounterName() + "&amp;ns_class=bookmark\" />";
+				nedstat = "<link rel=\"shortcut icon\" href=\"http://" + getLandCode() + ".sitestat.com/" +
+				        getCustomerName() + "/" + getSiteName() + "/s?" + getCounterName() +
+				        "&amp;ns_class=bookmark\" />";
 				log.debug("nedstat is: " + nedstat + ".");
 			}
 			else {
@@ -155,6 +178,15 @@ public class NedstatTag extends CmscTag {
 			ctx.getOut().print(nedstat);
 		}
 	}
+    /**
+     * @param ctx
+     * @return
+     */
+    private String getJavascriptFile(PageContext ctx) {
+        HttpServletRequest request = (HttpServletRequest) ctx.getRequest();
+        String javascriptFile = request.getContextPath() + "/js/sitestat.js";
+        return javascriptFile;
+    }
 	public String getCounterName() {
 		return counterName;
 	}
