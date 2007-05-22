cec8c9dab96ffc1c4d24c206773dec4a4bbd523b
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-05-22 09:57:16 +0000

    generic js-utilities
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@23679 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ contributions/didactor2/src/core/templates/core/js/utils.js
@@ -0,0 +1,7 @@
+  function addParameter(href, parameter) {
+    if (href.indexOf('?') > 0) {
+       return  href + '&' + parameter;
+    } else {
+       return  href + '?' + parameter;
+    }
+  }
