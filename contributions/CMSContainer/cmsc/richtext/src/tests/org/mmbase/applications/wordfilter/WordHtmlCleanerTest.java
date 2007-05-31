03ee6e234ad74071528abe5cd3ba51687c319255
fpunt <fpunt@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-05-31 11:19:54 +0000

    CMSC-421 Fixed problem with whitespaces, it now always stays as it is (no automatic removal of whitespace). .
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@23782 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer/cmsc/richtext/src/tests/org/mmbase/applications/wordfilter/WordHtmlCleanerTest.java
+++ contributions/CMSContainer/cmsc/richtext/src/tests/org/mmbase/applications/wordfilter/WordHtmlCleanerTest.java
@@ -9,16 +9,43 @@ import java.util.regex.Pattern;
  */
 public class WordHtmlCleanerTest extends TestCase {
 
-    /**
-     * Test for jira issue NIJ-385. Bold tags with only spaces in it where reduced to a self closing tag, which will
-     * render al following text bold in IE.<br/>
-     * E.g. The following html <code>&lt;b&gt; &lt;/b&gt;</code> would be cleaned as <code>&lt;b/&gt;</code>. 
-     */
-    public void testEmptyTagsCleaner() {
-        String html = "<b> </b><i> </i><u> </u> <B> </B> <U> </U> <I> </I> ";
+   /**
+    * Test for jira issue NIJ-385. Bold tags with only spaces in it where reduced to a self closing tag, which will
+    * render al following text bold in IE.<br/>
+    * E.g. The following html <code>&lt;b&gt; &lt;/b&gt;</code> would be cleaned as <code>&lt;b/&gt;</code>. 
+    */
+   public void testEmptyTagsCleaner() {
+       String html = "<b> </b><i> </i><u> </u> <B> </B> <U> </U> <I> </I> ";
 
-        String cleanedHtml = WordHtmlCleaner.cleanHtml(html);
-        Pattern p = Pattern.compile("<[bBiIuU]\\s*/>");
-        assertFalse("Cleaned html still has some empty tags left!" + cleanedHtml, p.matcher(cleanedHtml).find());
-    }
+       String cleanedHtml = WordHtmlCleaner.cleanHtml(html);
+       Pattern p = Pattern.compile("<[bBiIuU]\\s*/>");
+       assertFalse("Cleaned html still has some empty tags left!" + cleanedHtml, p.matcher(cleanedHtml).find());
+   }
+   
+   /**
+    * Tests for jira issue CMSC-421, test a bunch of cleanups for the whitespace.
+    */
+   public void testRemoveWhiteSpace() {
+       doTestFilter("<p>test</p><p>test</p>", "test<br/><br/>test");
+   }
+   public void testRemoveWhiteSpace2() {
+       doTestFilter("test<br/><br/><br/><br/>test", "test<br/><br/><br/><br/>test");
+   }
+   public void testRemoveWhiteSpace3() {
+       doTestFilter("<p>test</p><p>x</p><p>test</p>", "test<br/><br/>x<br/><br/>test");
+   }
+   public void testRemoveWhiteSpace4() {
+      doTestFilter("<p>test</p><p></p><p>test</p>", "test<br/><br/><br/><br/>test");
+   }
+   public void testRemoveWhiteSpace5() {
+      doTestFilter("<p>test</p><p><p>test</p>", "test<br/><br/>test");
+   }
+   public void testRemoveWhiteSpace6() {
+      doTestFilter("<p>test</p><p>test", "test<br/><br/>test");
+   }
+   
+   private void doTestFilter(String input, String expected) {
+      String cleanedHtml = WordHtmlCleaner.cleanHtml(input);
+      assertEquals(expected, cleanedHtml);
+   }
 }
