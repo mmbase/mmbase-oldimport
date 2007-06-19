1c83c0a7e0bc8d24dcb5d19b275b3a4b097af8fa
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-06-19 14:05:58 +0000

    ah, I now see the ratio for loadFromContext. Never ind though
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@24154 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/ReloadableModule.java
+++ src/org/mmbase/module/ReloadableModule.java
@@ -19,7 +19,7 @@ import org.mmbase.util.logging.*;
  *
  * @author Michiel Meeuwissen
  * @since MMBase-1.8
- * @version $Id: ReloadableModule.java,v 1.14 2007-06-19 13:59:30 michiel Exp $
+ * @version $Id: ReloadableModule.java,v 1.15 2007-06-19 14:05:58 michiel Exp $
  */
 public abstract class ReloadableModule extends Module {
 
@@ -67,7 +67,7 @@ public abstract class ReloadableModule extends Module {
         properties = parser.getProperties();
         parser.getLocalizedDescription(getLocalizedDescription());
         parser.getLocalizedGUIName(getLocalizedGUIName());
-        loadFromContext();
+        loadInitParameters("mmbase/" + getName());
         return true;
     }
 
