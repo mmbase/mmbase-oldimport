fc92adfeb52f5e2b08670b6a1ab927f4968e6b23
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-06-20 14:14:47 +0000

    was referred in utils.xml long time ago..
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@24194 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/util/functions/Utils.java
@@ -0,0 +1,33 @@
+package org.mmbase.util.functions;
+
+import org.mmbase.util.logging.Logger;
+import org.mmbase.util.logging.Logging;
+
+/**
+ * Several utility methods.
+ *
+ * @author Michiel Meeuwissen
+ * @version $Id: Utils.java,v 1.1 2007-06-20 14:14:47 michiel Exp $
+ * @since MMBase-1.9
+ */
+public final class Utils {
+
+    private static final Logger log = Logging.getLoggerInstance(Utils.class);
+
+
+    public static String getFileItemName(@Name("fileName") String fileName) {
+        if (fileName == null) return null;
+        // some browers provide directory information. Take that away.
+        int pos = fileName.lastIndexOf("\\");
+        if (pos > 0) {
+            fileName = fileName.substring(pos + 1);
+        }
+        pos = fileName.lastIndexOf("/");
+        if (pos > 0) {
+            fileName = fileName.substring(pos + 1);
+        }
+        return fileName;
+
+    }
+
+}
