8dba7a930efd601c0822ae8d6ea6a14b21c97485
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-07-02 16:13:41 +0000

    getValue(Node, field) does not work as expected?. Lowered some log
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@24359 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/mynews/src/org/mmbase/mynews/MyNewsBuilder.java
+++ applications/mynews/src/org/mmbase/mynews/MyNewsBuilder.java
@@ -14,12 +14,11 @@ import org.mmbase.util.logging.*;
 public class MyNewsBuilder extends org.mmbase.util.functions.ExampleBuilder {
     private static final Logger log = Logging.getLoggerInstance(MyNewsBuilder.class);
 
-    // adds the virtual field 'magazine'.
-    // Simply the magazine this news is belonging too.
-    
+    // Adds a few 'virtual' fields.
+
     public Object getValue(MMObjectNode node, String field) {
         if ("magazine".equals(field)) {
-            log.info("Gettign magazine virtual field");
+            log.trace("Getting magazine virtual field");
             BasicSearchQuery query = new BasicSearchQuery();
             BasicStep step = query.addStep(this);
             MMObjectBuilder mags = getMMBase().getBuilder("mags");
@@ -29,7 +28,9 @@ public class MyNewsBuilder extends org.mmbase.util.functions.ExampleBuilder {
                 List<MMObjectNode> nodes = getMMBase().getClusterBuilder().getClusterNodes(query);
                 if (nodes.size() > 0) {
                     String magNumber = nodes.get(0).getStringValue("mags.number");
-                    log.info("Returning mag " + magNumber + mags.getNode(magNumber).getValues());
+                    if (log.isDebugEnabled()) {
+                        log.debug("Returning mag " + magNumber + mags.getNode(magNumber).getValues());
+                    }
                     return mags.getNode(magNumber).getValues();
                 } else {
                     log.service("No magazine node found for " + node + " query: " + org.mmbase.util.Casting.toString(query));
@@ -40,7 +41,9 @@ public class MyNewsBuilder extends org.mmbase.util.functions.ExampleBuilder {
                 return null;
             }
         } else if ("utitle".equals(field)) {
-            return ("" + getValue(node, "title")).toUpperCase();
+            String v = ("" + node.getValue("title")).toUpperCase();
+            log.debug("Return " + v + " for utitle of node " + node.getNumber());
+            return v;
         } else {
             return super.getValue(node, field);
         }
