56f241466cb8318e6db8fe0b0017ff89dc699d67
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-07-26 14:37:58 +0000

    updated from head
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@24743 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/didactor2/src/core/java/org/mmbase/servlet/FileServlet.java
+++ contributions/didactor2/src/core/java/org/mmbase/servlet/FileServlet.java
@@ -14,15 +14,19 @@ import org.mmbase.module.core.*;
 import java.util.*;
 import javax.servlet.http.*;
 import javax.servlet.*;
+import java.util.regex.*;
 
 import java.io.*;
+import java.text.*;
 
 import org.mmbase.util.logging.*;
 
 
 /**
+ * Straight-forward filter which can serve files from one directory (the directory 'files' in the
+ * mmbase 'datadir') outside the web application root.
  *
- * @version $Id: FileServlet.java,v 1.1 2007-04-30 16:51:42 michiel Exp $
+ * @version $Id: FileServlet.java,v 1.2 2007-07-26 14:37:58 michiel Exp $
  * @author Michiel Meeuwissen
  * @since  MMBase-1.9
  * @see    AttachmentServlet
@@ -31,10 +35,17 @@ public class FileServlet extends BridgeServlet {
     private static Logger log;
 
     private static File files = null;
+    
+    private Pattern ignore = Pattern.compile("");
+
     //private static final Properties properties= new Properties();
 
     public void init() throws ServletException {
         super.init();
+        String ig = getInitParameter("ignore");
+        if (ig != null && ig.length() > 0) {
+            ignore = Pattern.compile(ig);
+        }
         log = Logging.getLoggerInstance(FileServlet.class);
     }
     public static void init(HttpServletResponse res) {
@@ -42,8 +53,7 @@ public class FileServlet extends BridgeServlet {
             if (log == null) {
                 log = Logging.getLoggerInstance(FileServlet.class);
             }
-            if (mmbase == null) throw new RuntimeException("MMBase is null");
-            String dataDir = mmbase.getInitParameter("datadir");
+            String dataDir = MMBase.getMMBase().getInitParameter("datadir");
             if (dataDir != null && ! "".equals(dataDir)) {
                 File data;
                 if (dataDir.startsWith(".")) {
@@ -71,6 +81,8 @@ public class FileServlet extends BridgeServlet {
         }
         
     }
+
+
     protected boolean checkInited(HttpServletResponse res) throws ServletException, IOException  {
         boolean inited = super.checkInited(res);
         if (inited) {
@@ -94,28 +106,49 @@ public class FileServlet extends BridgeServlet {
         a.put("files",      50);  
         return a;
     }
-    
+
+    public static File getDirectory() {
+        return files;
+    }    
 
     public static File getFile(String pathInfo, ServletResponse res) {
         if (res != null) init((HttpServletResponse) res);
         if (pathInfo == null) {
             return files;
         } else {
-            return new File(files, pathInfo);
+            return new File(files, pathInfo.replace("/", File.separator));
         }
     }
 
+    protected boolean canRead(HttpServletRequest req, File f) {
+        // something with mmbase security ?
+        return f.canRead();
+    }
+
+    protected boolean ignores(String pi) {
+        return pi != null && (ignore.matcher(pi).matches());
+    }
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        File file = getFile(req.getPathInfo(), resp);
+        String pi = req.getPathInfo();
+        if (ignores(pi)) {
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND, "The file '" + pi + "' is not served");
+            return;
+        }
+
+        File file = getFile(pi, resp);
         if (! file.exists()) {
-            resp.sendError(HttpServletResponse.SC_NOT_FOUND, "The file '" + req.getPathInfo() + "' does not exist");
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND, "The file '" + pi + "' does not exist");
             log.debug("" + file + " does not exist");
             return;
         }
-        if (! file.canRead()) {
-            resp.sendError(HttpServletResponse.SC_FORBIDDEN, "The file '" + req.getPathInfo() + "' may not be read");
+        if (! canRead(req, file)) {
+            resp.sendError(HttpServletResponse.SC_FORBIDDEN, "The file '" + pi + "' may not be read");
             log.debug("" + file + " may not be read");
+            return;
+        }
+        if (file.isDirectory()) {
+            listing(req, resp, file);
             return;
         }
         resp.setContentType(getServletContext().getMimeType(file.getName()));
@@ -131,6 +164,64 @@ public class FileServlet extends BridgeServlet {
         in.close();
         out.close();
     }
+
+
+    protected void listing(HttpServletRequest req, HttpServletResponse resp, File directory) throws IOException {
+        if ("true".equals(getInitParameter("listings"))) {
+            resp.setContentType("application/xhtml+xml"); // We hate IE anyways.
+            StringBuilder result = new StringBuilder();
+            result.append("<?xml version='1.0'?>\n");
+            result.append("<html xmlns='http://www.w3.org/1999/xhtml'>");
+            result.append("<head>");
+            String pathInfo = req.getPathInfo();
+            if (pathInfo == null || ! pathInfo.endsWith("/")) {
+                resp.sendRedirect(req.getContextPath() + req.getServletPath() + (pathInfo == null ? "" : pathInfo) + "/");
+                return;
+            }
+            result.append("<title>Directory Listing For " + pathInfo + "</title>");            
+            result.append("<link rel='stylesheet' href='" + req.getContextPath() + "/mmbase/style/css/mmbase.css' type='text/css' />");
+            result.append("</head>");
+            result.append("<body class='filelisting'>");
+            result.append("<h1>Directory Listing For " + pathInfo + "</h1>");
+            result.append("<table>");
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+            result.append("<tr><td class='lastmodified'>" + df.format(new Date(directory.lastModified())) + "</td><td class='filename'><a href='.'>./</a></td></tr>");
+            if (! pathInfo.equals("/")) {
+                result.append("<tr><td class='lastmodified'>" + df.format(new Date(directory.getParentFile().lastModified()))  +"</td><td class='filename'><a href='..'>../</a></td></tr>");
+            }
+            for (File file : directory.listFiles()) {
+                String name = file.getName() + (file.isDirectory() ? "/" : "");
+                if (ignores(pathInfo + name)) continue;
+                
+                result.append("<tr><td class='lastmodified'>" + df.format(new Date(file.lastModified())) + "</td><td class='filename'>");
+                if (canRead(req, file)) {
+                    result.append("<a href='" + name + "'>" + name + "</a>");
+                } else {
+                    result.append(name);
+                }
+                result.append("</td></tr>");
+            }
+            result.append("</table>");
+            result.append("<h3>" + org.mmbase.Version.get() + "</h3>");
+            result.append("</body>");
+            result.append("</html>");
+            try {
+                byte [] bytes = result.toString().getBytes("UTF-8");
+                resp.setContentLength(bytes.length);
+                BufferedOutputStream out = new BufferedOutputStream(resp.getOutputStream());
+                out.write(bytes);
+                out.flush();
+            } catch (java.io.UnsupportedEncodingException uue) {
+                // cannot happen UTF-8 is supported.
+                log.fatal(uue);
+            }
+            return;
+        } else {
+            resp.sendError(HttpServletResponse.SC_FORBIDDEN, "Directory listings are not enabled. (see web.xml)");
+            return;            
+        }
+    }
+
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         File file = getFile(req.getPathInfo(), resp);
         if (file.exists()) {
