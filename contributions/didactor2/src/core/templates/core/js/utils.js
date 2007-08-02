424caf9dbfb159376b81cf393bc85f0b7f580cb0
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-08-02 08:45:10 +0000

    added getElementsByClass, which is sillily missing from DOM
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@24859 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/didactor2/src/core/templates/core/js/utils.js
+++ contributions/didactor2/src/core/templates/core/js/utils.js
@@ -1,7 +1,25 @@
-  function addParameter(href, parameter) {
+function addParameter(href, parameter) {
     if (href.indexOf('?') > 0) {
-       return  href + '&' + parameter;
+        return  href + '&' + parameter;
     } else {
-       return  href + '?' + parameter;
+        return  href + '?' + parameter;
     }
-  }
+}
+
+Document = document.constructor; 
+HTMLElement = document.createElement("x").constructor;
+Document.prototype.getElementsByClass = HTMLElement.prototype.getElementsByClass =
+    function(searchClass, tag) {
+    if (tag == null) tag ="*";
+    var classElements = new Array();
+    var els = this.getElementsByTagName(tag);
+    var elsLen = els.length;
+    var pattern = new RegExp("(^|\\s)" + searchClass + "(\\s|$)");
+    for (i = 0, j = 0; i < elsLen; i++) {
+        if ( pattern.test(els[i].className) ) {
+            classElements[j] = els[i];
+            j++;
+        }
+    }
+    return classElements;
+}
