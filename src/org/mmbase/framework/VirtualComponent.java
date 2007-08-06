f15351835a6a1efdff2ae03a251816265c2ea937
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-08-06 16:55:55 +0000

    There is no real point to extend this from Component
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@24967 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/framework/VirtualComponent.java
+++ src/org/mmbase/framework/VirtualComponent.java
@@ -20,15 +20,15 @@ import org.mmbase.util.logging.*;
  * A Virtual component is a component which is  only mentioned as a dependency of another component.
  *
  * @author Michiel Meeuwissen
- * @version $Id: VirtualComponent.java,v 1.1 2007-07-30 23:01:42 michiel Exp $
+ * @version $Id: VirtualComponent.java,v 1.2 2007-08-06 16:55:55 michiel Exp $
  * @since MMBase-1.9
  */
-public class VirtualComponent implements Component {
+public class VirtualComponent {
     private static final Logger log = Logging.getLoggerInstance(VirtualComponent.class);
 
 
     private final String name;
-    private int version = -1;
+    private final int version;
 
 
     public VirtualComponent(String name, int v) {
@@ -39,56 +39,7 @@ public class VirtualComponent implements Component {
     public String getName() {
         return name;
     }
-    public URI getUri() {
-        return null;
-    }
     public int getVersion() {
         return version;
-    }
-
-
-    public LocalizedString getDescription() {
-        return null;
-    }
-
-    public void configure(Element el) {
-        throw new UnsupportedOperationException();
-    }
-
-    public Collection<Block> getBlocks() {
-        return null;
-    }
-    public Block getBlock(String name) {
-        return null;
-    }
-    public Block getDefaultBlock() {
-        return null;
-    }
-
-    public String toString() {
-        return getName() + ":" + getVersion();
-    }
-
-    public String getBundle() {
-        return null;
-    }
-
-    public Collection<Setting<?>> getSettings() {
-        return null;
-    }
-
-    public Setting<?> getSetting(String name) {
-        return null;
-    }
-
-    public Collection<Component> getDependencies() {
-        return null;
-    }
-
-    public Collection<VirtualComponent> getUnsatisfiedDependencies() {
-        return null;
-    }
-    public void resolve(VirtualComponent vc, Component comp) {
-        throw new UnsupportedOperationException();
     }
 }
