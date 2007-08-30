66e89d4afab4e85b9ad5e4ddd33ab029bb30524e
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-08-30 22:06:36 +0000

    added some features to this, 'messageformat, SI vs non-SI, units etc
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@25229 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/datatypes/processors/FormatFileSize.java
+++ src/org/mmbase/datatypes/processors/FormatFileSize.java
@@ -10,12 +10,13 @@ See http://www.MMBase.org/license
 package org.mmbase.datatypes.processors;
 
 import org.mmbase.bridge.*;
+import java.text.*;
 
 /**
  * A processor that gets a number as a file-size, that is, rounded with kbytes and Mb's and so on.
  *
  * @author Michiel Meeuwissen
- * @version $Id: FormatFileSize.java,v 1.2 2007-08-03 14:49:19 michiel Exp $
+ * @version $Id: FormatFileSize.java,v 1.3 2007-08-30 22:06:36 michiel Exp $
  * @since MMBase-1.8
  */
 
@@ -23,24 +24,96 @@ public class FormatFileSize implements Processor {
 
     private static final long serialVersionUID = 1L;
     private static final int KILO     = 1000;
-    private static final int MEGA     = 1000000;
-    private static final int GIGA     = 1000000000;
-    private static final int KIBIBYTE    = 1024;
-    private static final int MEBIBYTE    = KIBIBYTE * KIBIBYTE;
-    private static final int GIBIBYTE    = KIBIBYTE * KIBIBYTE * KIBIBYTE;
+    private static final int KIBI     = 1024;
+
+
+    private static final String[] SI_BI = {"Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi", "Yi"};
+    private static final String[] SI    = {"k",  "M",  "G",  "T",  "P",  "E",  "Z",  "Y"};
+
+    protected int      k        = KIBI;
+    protected String[] prefixes = SI_BI;
+    protected String unit = "B";
+    protected String format = "{0,number,#} ";
+    protected int limitFactor = 9;
+
+    /**
+     * If 'bi' set, will use binary SI prefixes. So, Ki, Mi, etc. which are multiples of
+     * 1024. Otherwise, use normal SI prefixes (k, M, G), which are multiples of 1000.
+     * @since MMBase-1.9
+     */
+    public void setBI(boolean bi) {
+        if (bi) {
+            prefixes = SI_BI;
+            k = KIBI;
+        } else {
+            prefixes = SI;
+            k = KILO;
+        }
+    }
+    /**
+     * The unix which is prefixes by the prefixes. Defaults to 'B', the SI unit of a 'byte'.
+     * @since MMBase-1.9
+     */
+    public void setUnit(String u) {
+        unit = u;
+    }
+
+    /**
+     * It was commonplace to mix SI prefixes with 'binary' factors.
+     * If this is set to 'true', then SI prefixes are used 'byte' will be the unit, and 1024 the
+     * basic factor (which is basicly incorrect).
+     * @since MMBase-1.9
+     */
+    public void setClassical(boolean c) {
+        if (c) {
+            prefixes = SI;
+            unit = "byte";
+            k = KIBI;
+        } else {
+            prefixes = SI_BI;
+            unit = "B";
+            k = KIBI;
+        }
+    }
+    /**
+     * MessageFormat to format the number. Default to '{0,number,#} '.
+     * @since MMBase-1.9
+     */
+    public void setFormat(String mf) {
+        if (mf == null) throw new IllegalArgumentException();
+        format = mf;
+    }
+    public String getFormat() {
+        return format;
+    }
+
+    /**
+
+     * @since MMBase-1.9
+     */
+    public void setLimit(int l) {
+        limitFactor = l;
+    }
 
 
     public final Object process(Node node, Field field, Object value) {
         int size = node.getIntValue(field.getName());
-        if (size < 9 * KILO) {
-            return "" + size + " B";
-        } else if (size < 9 * MEGA) {
-            return "" + size / KIBIBYTE + " KiB";
-        } else if (size < 9 * GIGA) {
-            return "" + size / MEBIBYTE + " MiB";
-        } else {
-            return "" + size / GIBIBYTE + " GiB";
+        int factor = k;
+        int power  = 0;
+        while (size > factor * limitFactor) {
+            factor *= k;
+            power++;
         }
+        factor /= k;
+
+        StringBuffer buf = new StringBuffer();
+        MessageFormat mf = new MessageFormat(format,  node.getCloud().getLocale());
+        mf.format(new Object[] { (float) size  / factor}, buf, null);
+        if (power > 0) {
+            buf.append(prefixes[power - 1]);
+        }
+        buf.append(unit);
+        return buf.toString();
     }
 
     public String toString() {
