c736a13aa3382ad114242b02ffc0882af3c688ef
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-09-11 17:11:38 +0000

    javadoc, default implemetnation for 'contains
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@25297 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/jumpers/src/org/mmbase/util/jumpers/JumperStrategy.java
+++ applications/jumpers/src/org/mmbase/util/jumpers/JumperStrategy.java
@@ -24,54 +24,55 @@ import org.mmbase.module.core.MMObjectNode;
 import org.mmbase.bridge.*;
 
 /**
-* This is the baseclass for strategies.
-*
-* A strategy has to extend this class and provide its own methods for:
-*   - contains(MMObjectNode)
-*   - calculate(MMObjectNode)
-*
-* The contains(node) checks whether the strategy can/will handle this node.
-* For example, it can check whether the node has a certain type of relation,
-* is of a special type or is just of a type this strategy can handle.
-*
-* The calculate(node) will then try to calculate an url.
-*
-* @see #contains(MMObjectNode)
-* @see #calculate(MMObjectNode)
-*
-* @author Marcel Maatkamp, VPRO Digitaal
-* @version $Id: JumperStrategy.java,v 1.3 2007-09-11 14:24:29 michiel Exp $
-*/
+ * This is the baseclass for strategies.
+ *
+ * A strategy has to extend this class and provide its own methods for:
+ *   - contains(MMObjectNode)
+ *   - calculate(MMObjectNode)
+ *
+ * The contains(node) checks whether the strategy can/will handle this node.
+ * For example, it can check whether the node has a certain type of relation,
+ * is of a special type or is just of a type this strategy can handle.
+ *
+ * The calculate(node) will then try to calculate an url.
+ *
+ * An implementation for a JumperStrategy must be <em>stateless</em> because it is instantiated only once,
+ * and may be accessed by more than one thread. So you <em>may not</em> use result of the
+ * calculation of {@link #contains} in {@link #calculate}. For calculating actual jumper only {@link
+ * #calculate} is called, which can rapidly return <code>null</code> if it cannot calculate for the
+ * given Node.
+ *
+ *
+ * @see #contains(MMObjectNode)
+ * @see #calculate(MMObjectNode)
+ *
+ * @author Marcel Maatkamp, VPRO Digitaal
+ * @version $Id: JumperStrategy.java,v 1.4 2007-09-11 17:11:38 michiel Exp $
+ */
 public abstract class JumperStrategy {
 
     private static final Logger log = Logging.getLoggerInstance(JumperStrategy.class);
 
 
     /**
-    * signals whether this strategy can calculate an url for this node.
-    *
-    * It will query the database and return true if this strategy can procude an url
-    * The factory will walk through its strategies like:
-    *
-    *   for all enables strategies AND url not found {
-    *       if(strategy[x].contains(node))
-    *           return strategy[x].calculate(node)
-    *   }
-    *
-    * @param node node for which an url has to be calculated for
-    * @return url for this node
+     * signals whether this strategy can calculate an url for this node.
+     *
+     * @param node node for which an url has to be calculated for
+     * @return url for this node
     */
-    public abstract boolean contains(MMObjectNode node);
+    public boolean contains(MMObjectNode node) {
+        return calculate(node) != null;
+    }
 
     /**
-    * calculates an url for this node.
-    *
-    * call this method only when the contains(node) returns true.
-    *
-    * @see #contains(MMObjectNode)
-    * @param node node for which an url has to be calculated for
-    * @return the url for this node
-    */
+     * calculates an url for this node.
+     *
+     * call this method only when the contains(node) returns true.
+     *
+     * @see #contains(MMObjectNode)
+     * @param node node for which an url has to be calculated for
+     * @return the url for this node
+     */
     public abstract String  calculate(MMObjectNode node);
 
 }
