c1688c7a1c54aa3930ca5d80bdcd854207fdef36
mguo <mguo@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-10-10 10:30:15 +0000

    init check in.cmsc option module
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@25535 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer_Modules/glossary/src/java/com/finalist/cmsc/module/glossary/Glossary.java
+++ contributions/CMSContainer_Modules/glossary/src/java/com/finalist/cmsc/module/glossary/Glossary.java
@@ -9,60 +9,53 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Glossary
-{
+public class Glossary {
+    public static final String GLOSSARY = "glossary";
+    private final Map<String, String> TERMS = new HashMap<String, String>();
+    private static Glossary glossary = null;
 
-    private Glossary()
-    {
+    private Glossary() {
     }
 
-    void addTerm(String term, String description)
-    {
+    void addTerm(String term, String description) {
         TERMS.put(term, description);
     }
 
-    public String mark(String material)
-    {
-        Iterator i$ = TERMS.keySet().iterator();
-label0:
-        do
-        {
-            if(!i$.hasNext())
-                break;
-            String word = (String)i$.next();
+    public String mark(String material) {
+        Set<String> keyWords = TERMS.keySet();
+
+        for (String word : keyWords) {
+
             Pattern pattern = Pattern.compile((new StringBuilder()).append("\\b").append(word).append("\\b").toString());
+
             Pattern inLinkPattern = Pattern.compile((new StringBuilder()).append("<a[^<]*").append(word).append("[^>]*>").toString());
             Matcher matcher = pattern.matcher(material);
             Matcher linkMatcher = inLinkPattern.matcher(material);
             do
-                if(!matcher.find())
-                    continue label0;
-            while(linkMatcher.find() && matcher.start() > linkMatcher.start() && matcher.end() < linkMatcher.end());
-            String highlight = String.format("<a href=\"#\" title=\"%s\" id=\"_glossary_%s\">%s</a>", new Object[] {
-                TERMS.get(word), word, word
-            });
+                if (!matcher.find())
+                    continue;
+
+            while (linkMatcher.find() && matcher.start() > linkMatcher.start() && matcher.end() < linkMatcher.end());
+
+            String highlight = String.format("<a href=\"#\" title=\"%s\" id=\"_glossary_%s\">%s</a>", TERMS.get(word), word, word);
+
             material = (new StringBuilder()).append(material.substring(0, matcher.start())).append(highlight).append(material.substring(matcher.end(), material.length())).toString();
-        } while(true);
+
+        }
         return material;
+
     }
 
-    public static synchronized Glossary instance()
-    {
-        if(null == glossary)
-        {
+    public static synchronized Glossary instance() {
+        if (null == glossary) {
             Glossary glossary = new Glossary();
             glossary = glossary;
         }
         return glossary;
     }
 
-    public Map getTerms()
-    {
+    public Map getTerms() {
         return TERMS;
     }
-
-    public static final String GLOSSARY = "glossary";
-    private final Map TERMS = new HashMap();
-    private static Glossary glossary = null;
 
 }
