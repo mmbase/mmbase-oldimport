dd41350794d5568471a3b106f3eacf5de4d8adfe
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-10-12 16:36:39 +0000

    fix for   MMB-1525 allows for nicer didactor tags too
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@25578 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/didactor2/src/core/java/nl/didactor/taglib/Functions.java
+++ contributions/didactor2/src/core/java/nl/didactor/taglib/Functions.java
@@ -11,23 +11,25 @@ import org.mmbase.util.logging.Logging;
 
 /**
  * Provide some Didactor specify functionality as EL-functions too.
- * 
+ *
  * @author Michiel Meeuwissen
- * @version $Id: Functions.java,v 1.4 2007-05-11 12:33:33 michiel Exp $
+ * @version $Id: Functions.java,v 1.5 2007-10-12 16:36:39 michiel Exp $
  * @since Didactor-2.3
  */
 public class Functions {
     private static final Logger log = Logging.getLoggerInstance(Functions.class);
 
 
-    public static String translate(PageContext pageContext, String key) {
+    public static String translate(String key) {
+        PageContext pageContext = ContextReferrerTag.getThreadPageContext();
         TranslateTable.init();
         Locale loc = (Locale) pageContext.getAttribute("javax.servlet.jsp.jstl.fmt.locale.request", PageContext.REQUEST_SCOPE);
-        
+
         TranslateTable tt = new TranslateTable(loc);
         return tt.translate(key);
     }
-    public static Object setting(PageContext pageContext, String component, String setting) throws JspTagException {
+    public static Object setting(String component, String setting) throws JspTagException {
+        PageContext pageContext = ContextReferrerTag.getThreadPageContext();
         Component comp = Component.getComponent(component);
         Cloud cloud = (Cloud) pageContext.getAttribute(CloudTag.KEY, CloudTag.SCOPE);
         ContextTag pageContextTag = (ContextTag) pageContext.getAttribute(ContextTag.CONTEXTTAG_KEY);
