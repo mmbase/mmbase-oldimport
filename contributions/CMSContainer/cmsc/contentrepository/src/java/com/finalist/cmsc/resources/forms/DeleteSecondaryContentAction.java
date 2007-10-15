30ed927111259a1b0bff6d68c4cbca9959c16a26
nklasens <nklasens@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-10-15 10:21:34 +0000

    CMSC-583 Delete Secondary Content should remove object from live site
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@25590 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer/cmsc/contentrepository/src/java/com/finalist/cmsc/resources/forms/DeleteSecondaryContentAction.java
+++ contributions/CMSContainer/cmsc/contentrepository/src/java/com/finalist/cmsc/resources/forms/DeleteSecondaryContentAction.java
@@ -6,11 +6,11 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-import org.mmbase.bridge.Cloud;
-import org.mmbase.bridge.NotFoundException;
+import org.mmbase.bridge.*;
 import org.mmbase.util.logging.Logger;
 import org.mmbase.util.logging.Logging;
 
+import com.finalist.cmsc.services.publish.Publish;
 import com.finalist.cmsc.struts.MMBaseAction;
 
 public class DeleteSecondaryContentAction extends MMBaseAction {
@@ -18,29 +18,32 @@ public class DeleteSecondaryContentAction extends MMBaseAction {
     private static transient Logger log = Logging.getLoggerInstance(DeleteSecondaryContentAction.class.getName());
 
     private static final String ACTION_REMOVE = "remove";
-    
     private static final String ACTION_CANCEL = "cancel";
 
-
+    @Override
+    public String getRequiredRankStr() {
+        return ADMINISTRATOR;
+    }
     
+	@Override
 	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response, Cloud cloud) throws Exception {
 		DeleteSecondaryContentForm deleteForm = (DeleteSecondaryContentForm) form;
 		String number = deleteForm.getObjectnumber();
 		String object_type = request.getParameter("object_type");
 		if (isRemoveAction(request)) {
-			if(MMBaseAction.ADMINISTRATOR.equals(cloud.getUser().getRank().toString())) {
 	            try {
 					log.debug("deleting secondary content: "+number);
-					cloud.getNode(number).delete(true);
-	
+					Node objectNode = cloud.getNode(number);
+					
+                    Publish.remove(objectNode);
+                    Publish.unpublish(objectNode);
+                    
+                    objectNode.delete(true);
+
 					return mapping.findForward(object_type);
 	            } catch (NotFoundException nfe) {
 	                log.info("Failed to delete secondaryContent with number " + number + ", node not found");
 	            }
-			}
-			else {
-				log.warn("did not delete secondary content because user was not administrator: "+number+" ("+cloud.getUser()+":"+cloud.getUser().getRank()+")");
-			}
 		}
 		if (isCancelAction(request)) {
 			return mapping.findForward(object_type);
