15e5fedc7018317b7747f860b8e490c498cc7f56
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-10-26 16:02:45 +0000

    experimental (set)processor for phonenumbers
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@25711 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ applications/notifications/src/org/mmbase/sms/NumberRecognizer.java
@@ -0,0 +1,44 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.sms;
+
+import org.mmbase.bridge.*;
+import org.mmbase.datatypes.processors.Processor;
+import java.util.regex.*;
+
+import org.mmbase.util.logging.Logger;
+import org.mmbase.util.logging.Logging;
+
+/**
+ *
+ * @author Michiel Meeuwissen
+ * @version $Id: NumberRecognizer.java,v 1.1 2007-10-26 16:02:45 michiel Exp $
+ **/
+public  class NumberRecognizer implements Processor {
+
+
+    public Pattern DUTCH = Pattern.compile("(00316|06|+316)\\s*[\\-\\s]?\\s*([0-9]{8})");
+
+    public Object process(Node node, Field field, Object value) {
+        String number = "" + value;
+        Matcher dutch = DUTCH.matcher(number);
+        if (dutch.matches()) {
+            return "00316" + dutch.group(1);
+        } else {
+            return value;
+        }
+    }
+
+    public static void main(String[] argv) {
+        System.out.println(new NumberRecognizer().process(null, null, argv[0]));
+    }
+
+
+}
