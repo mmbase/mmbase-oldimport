d274a416e26f508d20b5b02bc0483d18c6480fdf
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-11-14 16:09:42 +0000

    spacingonly
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@25844 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/framework/UrlConverter.java
+++ src/org/mmbase/framework/UrlConverter.java
@@ -13,15 +13,15 @@ import java.util.*;
 import org.mmbase.util.functions.*;
 
 /**
- * Responsible for the proper handling of urls within the framework. 
+ * Responsible for the proper handling of urls within the framework.
  * Called by FrameworkFilter to resolve userfriendly links into technical urls.
  * You should implement UrlConverter if you want to create and resolve your own
- * userfriendly links within your framework. 
+ * userfriendly links within your framework.
  * You can configure several UrlConverters in your framework's 'framework.xml'.
  * They will be chained one after another.
- * 
+
  * @author Michiel Meeuwissen
- * @version $Id: UrlConverter.java,v 1.5 2007-08-07 19:33:27 andre Exp $
+ * @version $Id: UrlConverter.java,v 1.6 2007-11-14 16:09:42 michiel Exp $
  * @since MMBase-1.9
  * @todo Parameters are passed as Collections of Map.Entry. Not sure that is handy/correct. The main
  * reason is that you can create such objects easily from both Parameters as from Maps, and that you
@@ -33,8 +33,8 @@ public interface UrlConverter {
 
     Parameter[] getParameterDefinition();
 
-    /** 
-     * Return a (possibly modified) URL for a given path. 
+    /**
+     * Return a (possibly modified) URL for a given path.
      * This method is called (for example) from within the mm:url tag, and can be exposed to the outside world.
      * I.e. when within a components's head you use<br />
      * &lt;mm:url page="/css/style.css" /&gt;, <br />
@@ -45,23 +45,23 @@ public interface UrlConverter {
      * @param path The path (generally a relative URL) to create an URL for.
      * @param parameters Parameters The parameters to be passed to the page
      * @param frameworkParameters The parameters that are required by the framework
-     * @param escapeAmps <code>true</code> if parameters should be added with an escaped &amp; (&amp;amp;). 
-     *                   You should escape &amp; when a URL is exposed (i.e. in HTML), but not if the url is 
-     *                   for some reason called directly. 
+     * @param escapeAmps <code>true</code> if parameters should be added with an escaped &amp; (&amp;amp;).
+     *                   You should escape &amp; when a URL is exposed (i.e. in HTML), but not if the url is
+     *                   for some reason called directly.
      * @param process    If following the URL must lead to a process rendering. IOW, this URL is
      *                   for form actions.
      * @return An URL relative to the root of this web application (i.e. withouth a context path),
      * or <code>null</code> if this UrlConvert does not know how produce an url for given parameteters.
      */
-    StringBuilder getUrl(String path, 
+    StringBuilder getUrl(String path,
                                 Collection<Map.Entry<String, Object>> parameters,
-                                Parameters frameworkParameters, 
+                                Parameters frameworkParameters,
                                 boolean escapeAmps);
 
 
     /**
      * Generates an URL to a resource to be called and included by a renderer.
-     * Typically, this generates a URL to a jsp, called by a renderer such as the {@link JspRenderer}, 
+     * Typically, this generates a URL to a jsp, called by a renderer such as the {@link JspRenderer},
      * who calls the resource using the RequestDispatcher.
      * This method allows for frameworks to do some filtering on URLs (such as pretty URLs).
      * You should generally not call this method unless you write a Renderer that depends on code or
@@ -74,8 +74,8 @@ public interface UrlConverter {
      *         determined (this would make it possible to 'chain' frameworks).
      * or <code>null</code> if this UrlConvert does not know how produce an url for given parameteters.
      */
-    StringBuilder getInternalUrl(String path, 
-                                        Collection<Map.Entry<String, Object>> params, 
+    StringBuilder getInternalUrl(String path,
+                                        Collection<Map.Entry<String, Object>> params,
                                         Parameters frameworkParameters);
 
 
