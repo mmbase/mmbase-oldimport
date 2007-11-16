e3710793e95730ca0c9ea5bc2c7744d96fb97c21
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-11-16 17:31:57 +0000

    javadoc
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@25892 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/framework/UrlConverter.java
+++ src/org/mmbase/framework/UrlConverter.java
@@ -13,20 +13,17 @@ import java.util.*;
 import org.mmbase.util.functions.*;
 
 /**
- * Responsible for the proper handling of urls within the framework.
- * Called by FrameworkFilter to resolve userfriendly links into technical urls.
+ * Responsible for the proper handling of urls within the basic framework {@link BasicFramework}.
  * You should implement UrlConverter if you want to create and resolve your own
- * userfriendly links within your framework.
- * You can configure several UrlConverters in your framework's 'framework.xml'.
+ * userfriendly links within {@link BasicFramework}
+ *.
+ * You can configure several UrlConverters in 'framework.xml'.
+ *
  * They will be chained one after another.
 
  * @author Michiel Meeuwissen
- * @version $Id: UrlConverter.java,v 1.8 2007-11-16 17:30:33 michiel Exp $
+ * @version $Id: UrlConverter.java,v 1.9 2007-11-16 17:31:57 michiel Exp $
  * @since MMBase-1.9
- * @todo Parameters are passed as Collections of Map.Entry. Not sure that is handy/correct. The main
- * reason is that you can create such objects easily from both Parameters as from Maps, and that you
- * don't loose order, if there is one (it is also easiy to create a map from Parameters, but it has
- * no garanteed order any more then).
  */
 public interface UrlConverter {
 
