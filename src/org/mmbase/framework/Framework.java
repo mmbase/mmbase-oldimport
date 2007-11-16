523566bdd6f3c9b758531a782cf176fb8b31a071
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-11-16 17:30:33 +0000

    Framework and UrlConvertor are distinct things
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@25891 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/framework/Framework.java
+++ src/org/mmbase/framework/Framework.java
@@ -25,10 +25,10 @@ import org.mmbase.util.logging.Logging;
  *
  * @author Johannes Verelst
  * @author Pierre van Rooden
- * @version $Id: Framework.java,v 1.37 2007-11-16 16:06:30 michiel Exp $
+ * @version $Id: Framework.java,v 1.38 2007-11-16 17:30:33 michiel Exp $
  * @since MMBase-1.9
  */
-public abstract class Framework implements UrlConverter {
+public abstract class Framework {
 
     private static final Logger log = Logging.getLoggerInstance(Framework.class);
 
@@ -196,6 +196,53 @@ public abstract class Framework implements UrlConverter {
      * @throws SecurityException If you are not allowed to change the setting.
      */
     public abstract <C> C setSettingValue(Setting<C> setting, Parameters parameters, C value) throws SecurityException;
+
+
+
+    public abstract Parameter[] getParameterDefinition();
+
+    /**
+     * Return a (possibly modified) URL for a given path.
+     * This method is called (for example) from within the mm:url tag, and can be exposed to the outside world.
+     * I.e. when within a components's head you use<br />
+     * &lt;mm:url page="/css/style.css" /&gt;, <br />
+     * this method is called to determine the proper url (i.e., relative to the framework or component base).
+     * If you need treefile/leaffile type of functionality in your framework, you can implement that
+     * here in your code.
+     *
+     * @param path The path (generally a relative URL) to create an URL for.
+     * @param parameters Parameters The parameters to be passed to the page
+     * @param frameworkParameters The parameters that are required by the framework
+     * @param escapeAmps <code>true</code> if parameters should be added with an escaped &amp; (&amp;amp;).
+     *                   You should escape &amp; when a URL is exposed (i.e. in HTML), but not if the url is
+     *                   for some reason called directly.
+     * @param process    If following the URL must lead to a process rendering. IOW, this URL is
+     *                   for form actions.
+     * @return An URL relative to the root of this web application (i.e. withouth a context path),
+     */
+    public abstract StringBuilder getUrl(String path,
+                         Map<String, Object> parameters,
+                         Parameters frameworkParameters,
+                         boolean escapeAmps);
+
+
+    /**
+     * Generates an URL to a resource to be called and included by a renderer.
+     * Typically, this generates a URL to a jsp, called by a renderer such as the {@link JspRenderer},
+     * who calls the resource using the RequestDispatcher.
+     * This method allows for frameworks to do some filtering on URLs (such as pretty URLs).
+     * You should generally not call this method unless you write a Renderer that depends on code or
+     * data from external resources.
+     * @param path The page (e.g. image/css) provided by the component to create an URL for
+     * @param params Extra parameters for that path
+     * @param frameworkParameters The parameters that are required by the framework, such as the
+     *                            'request' and 'cloud' objects
+     * @return A valid interal URL, or <code>null</code> if nothing framework specific could be
+     *         determined (this would make it possible to 'chain' frameworks).
+     */
+    public abstract StringBuilder getInternalUrl(String path,
+                                                 Map<String, Object> params,
+                                                 Parameters frameworkParameters);
 
 
 }
--- src/org/mmbase/framework/UrlConverter.java
+++ src/org/mmbase/framework/UrlConverter.java
@@ -21,7 +21,7 @@ import org.mmbase.util.functions.*;
  * They will be chained one after another.
 
  * @author Michiel Meeuwissen
- * @version $Id: UrlConverter.java,v 1.7 2007-11-16 11:40:08 michiel Exp $
+ * @version $Id: UrlConverter.java,v 1.8 2007-11-16 17:30:33 michiel Exp $
  * @since MMBase-1.9
  * @todo Parameters are passed as Collections of Map.Entry. Not sure that is handy/correct. The main
  * reason is that you can create such objects easily from both Parameters as from Maps, and that you
@@ -34,24 +34,8 @@ public interface UrlConverter {
     Parameter[] getParameterDefinition();
 
     /**
-     * Return a (possibly modified) URL for a given path.
-     * This method is called (for example) from within the mm:url tag, and can be exposed to the outside world.
-     * I.e. when within a components's head you use<br />
-     * &lt;mm:url page="/css/style.css" /&gt;, <br />
-     * this method is called to determine the proper url (i.e., relative to the framework or component base).
-     * If you need treefile/leaffile type of functionality in your framework, you can implement that
-     * here in your code.
-     *
-     * @param path The path (generally a relative URL) to create an URL for.
-     * @param parameters Parameters The parameters to be passed to the page
-     * @param frameworkParameters The parameters that are required by the framework
-     * @param escapeAmps <code>true</code> if parameters should be added with an escaped &amp; (&amp;amp;).
-     *                   You should escape &amp; when a URL is exposed (i.e. in HTML), but not if the url is
-     *                   for some reason called directly.
-     * @param process    If following the URL must lead to a process rendering. IOW, this URL is
-     *                   for form actions.
-     * @return An URL relative to the root of this web application (i.e. withouth a context path),
-     * or <code>null</code> if this UrlConvert does not know how produce an url for given parameteters.
+     * See {@link Framework#getUrl(String, Map, Parameters,boolean)}.
+     * But it can also return <code>null</code> which mean, 'I don't know.'
      */
     StringBuilder getUrl(String path,
                          Map<String, Object> parameters,
@@ -60,19 +44,8 @@ public interface UrlConverter {
 
 
     /**
-     * Generates an URL to a resource to be called and included by a renderer.
-     * Typically, this generates a URL to a jsp, called by a renderer such as the {@link JspRenderer},
-     * who calls the resource using the RequestDispatcher.
-     * This method allows for frameworks to do some filtering on URLs (such as pretty URLs).
-     * You should generally not call this method unless you write a Renderer that depends on code or
-     * data from external resources.
-     * @param path The page (e.g. image/css) provided by the component to create an URL for
-     * @param params Extra parameters for that path
-     * @param frameworkParameters The parameters that are required by the framework, such as the
-     *                            'request' and 'cloud' objects
-     * @return A valid interal URL, or <code>null</code> if nothing framework specific could be
-     *         determined (this would make it possible to 'chain' frameworks).
-     * or <code>null</code> if this UrlConvert does not know how produce an url for given parameteters.
+     * See {@link Framework#geInternaltUrl(String, Map, Parameters)}.
+     * But it can also return <code>null</code> which mean, 'I don't know'.
      */
     StringBuilder getInternalUrl(String path,
                                  Map<String, Object> params,
