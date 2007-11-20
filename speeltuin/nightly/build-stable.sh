a1e07b5f976b4a591c3a30cba102beb964f39c71
nightly <nightly@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-11-20 13:23:54 +0000

    fixed 1.8 build
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@25940 91430c5f-d668-0410-9e7d-dd663366dcfd

--- speeltuin/nightly/build-stable.sh
+++ speeltuin/nightly/build-stable.sh
@@ -6,14 +6,22 @@
 
 echo setting PATH, JAVA HOME and ANT HOME
 export PATH=/bin:/usr/bin:/usr/local/bin:/usr/local/sbin:/usr/ccs/bin:/home/nightly/bin:/usr/local/ant/bin
-export JAVA_HOME=/usr/java/jdk
+
+## java 1.4
+#export JAVA_HOME=/usr/java/jdk
+
+#$ java 1.5
+## We build with java 1.5 nowadays.
+export JAVA_HOME=/home/nightly/jdk
+export JAVAC=${JAVA_HOME}/bin/javac
+
 export ANT_HOME=/usr/ant
 export CVS="/usr/bin/cvs -d :pserver:guest@cvs.mmbase.org:/var/cvs"
 export FILTER="/home/nightly/filterlog"
 export BUILD_HOME="/home/nightly"
 
 # settings
-antcommand="/usr/bin/ant -buildfile nightly-build.xml"
+antcommand="/usr/bin/ant"
 downloaddir="/home/nightly/download"
 optdir="/home/nightly/optional-libs"
 
@@ -21,6 +29,7 @@ echo generating version, and some directories
 
 #nightly build:
 version=`date '+%Y-%m-%d'`
+cvsversion="${version} `date '+%H:%M:%S'`"
 revision=MMBase-1_8
 
 #release:
@@ -36,52 +45,56 @@ echo ${antcommand}
 #${antcommand} clean > ${builddir}/messages.log 2> ${builddir}/errors.log
 
 cd ${BUILD_HOME}/stable/nightly-build/cvs/mmbase
-echo update cvs to `pwd`
+echo update cvs to `pwd`  >  ${builddir}/messages.log 2> ${builddir}/errors.log
 
-for i in '.' 'applications' 'contributions' ; do
-    echo updating `pwd`/$i 
-    ${CVS} -q update -d -P -l -D "${version}" -r "${revision}" $i >>  ${builddir}/messages.log 2>> ${builddir}/errors.log
-done
-for i in 'src' 'documentation' 'tests' 'config' 'html' \
-    'applications/taglib' 'applications/editwizard' 'applications/dove' 'applications/crontab' 'applications/cloudcontext' \
-    'applications/rmmci' 'applications/vwms' 'applications/scan' 'applications/clustering' 'applications/oscache-cache' \
-    'applications/largeobjects' 'applications/packaging' \
-    'contributations/aselect' 'contributions/mmbar' 'contributions/thememanager' 'contributions/multilanguagegui' \
-    ; do 
-    echo updating `pwd`/$i 
-   ${CVS} -q update -d -P -D "${version}" -r "${revision}" $i >>  ${builddir}/messages.log 2>> ${builddir}/errors.log
-done
-echo "==========UPDATING TO HEAD========" >> ${builddir}/messages.log
-for i in 'applications/email' 'contributions/lucene' 'contributions/mmbob' 'contributions/didactor2' \
-    ; do
-    echo updating to HEAD `pwd`/$i 
-    echo updating to HEAD `pwd`/$i  >> ${builddir}/messages.log    
-    ${CVS} -q update -d -P -D "${version}" -A $i >>  ${builddir}/messages.log 2>> ${builddir}/errors.log
-done
+if ( false ) ; then 
+    for i in '.' 'applications' 'contributions' ; do
+	echo updating `pwd`/$i 
+	${CVS} -q update -d -P -l -D "${cvsversion}" -r "${revision}" $i >>  ${builddir}/messages.log 2>> ${builddir}/errors.log
+    done
+    for i in 'src' 'documentation' 'tests' 'config' 'html' \
+	'applications/taglib' 'applications/editwizard' 'applications/dove' 'applications/crontab' 'applications/cloudcontext' \
+	'applications/rmmci' 'applications/vwms' 'applications/scan' 'applications/clustering' 'applications/oscache-cache' \
+	'applications/largeobjects' 'applications/packaging' \
+	'contributions/aselect' 'contributions/mmbar' 'contributions/thememanager' 'contributions/multilanguagegui' \
+	; do 
+      echo updating `pwd`/$i 
+      ${CVS} -q update -d -P -D "${cvsversion}" -r "${revision}" $i >>  ${builddir}/messages.log 2>> ${builddir}/errors.log
+    done
+    echo "==========UPDATING TO HEAD========" >> ${builddir}/messages.log
+    for i in 'applications/email' 'contributions/lucene' 'contributions/mmbob' 'contributions/didactor2' 'applications/richtext' \
+	; do
+      echo updating to HEAD `pwd`/$i 
+      echo updating to HEAD `pwd`/$i  >> ${builddir}/messages.log    
+      ${CVS} -q update -d -P -D "${cvsversion}" -A $i >>  ${builddir}/messages.log 2>> ${builddir}/errors.log
+    done
 
-stableoptions="-Dbuild.documentation=true -Ddestination.dir=${builddir} -Ddownload.dir=${downloaddir}"
+fi
+stableoptions="-Doptional.lib.dir=${optdir} -Dbuild.documentation=false -Ddestination.dir=${builddir} -Ddownload.dir=${downloaddir}"
 echo "options : ${stableoptions}"
 
-echo "Ant Command: ${antcommand} ${stableoptions}"
-echo Starting nightly build
-${antcommand} ${stableoptions} >> ${builddir}/messages.log 2>> ${builddir}/errors.log
+echo "Ant Command: ${antcommand} ${stableoptions}  " 
 
+echo cleaning
+find . -name build | xargs rm -r
+
+echo "Starting nightly build" + `pwd`
+${antcommand} bindist ${stableoptions} >> ${builddir}/messages.log 2>> ${builddir}/errors.log
+cd applications
+${antcommand} all ${stableoptions} >> ${builddir}/messages.log 2>> ${builddir}/errors.log
+cd ../contributions
+${antcommand} all ${stableoptions} >> ${builddir}/messages.log 2>> ${builddir}/errors.log
+cd ..
 
 ${CVS} log -rMMBase-1_8 -N -d"last week<now" 2> /dev/null | ${FILTER} > ${builddir}/RECENTCHANGES.txt
 
+for i in `find . -name *.zip` ; do
+    cp  -a $i ${builddir}
+done
+
 echo Creating sym for last build
-rm /home/nightly/builds/stable/latest
+latest=/home/nightly/builds/stable/latest
+rm -f ${latest}
 
-ln -s ${version} latest
-
-
-
-
-
-
-
-
-
-
-
+ln -s ${version} ${latest}
 
