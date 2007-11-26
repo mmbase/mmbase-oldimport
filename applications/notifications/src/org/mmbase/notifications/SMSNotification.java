8eb0b1a6dc52af775503259ee38d7f14c36db6b1
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-11-26 16:15:48 +0000

    Don't even offer sms's which are not working because no valid phone number yet
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@26029 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/notifications/src/org/mmbase/notifications/SMSNotification.java
+++ applications/notifications/src/org/mmbase/notifications/SMSNotification.java
@@ -8,21 +8,29 @@ See http://www.MMBase.org/license
 
 */
 package org.mmbase.notifications;
-import org.mmbase.sms.Sender;
+import org.mmbase.sms.*;
 import java.util.*;
 import org.mmbase.bridge.*;
+import org.mmbase.util.logging.Logger;
+import org.mmbase.util.logging.Logging;
 
 /**
  * A notification implementation which uses SMS.
  *
  * @author Michiel Meeuwissen
- * @version $Id: SMSNotification.java,v 1.1 2007-11-12 17:44:06 michiel Exp $
+ * @version $Id: SMSNotification.java,v 1.2 2007-11-26 16:15:48 michiel Exp $
  **/
 public  class SMSNotification extends Notification {
 
+    private static final Logger log = Logging.getLoggerInstance(SMSNotification.class);
 
     public void send(Node recipient, Node notifyable, Date date) {
-        Sender.getInstance().offer(new NotificationSMS(recipient, notifyable, date));
+        SMS sms = new NotificationSMS(recipient, notifyable, date);
+        if (sms.getOperator() < 0) {
+            log.service("Ignoring sms " + sms + " because recipient's phone number not yet validated");
+        } else {
+            Sender.getInstance().offer(sms);
+        }
     }
 
 
