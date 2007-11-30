9bc8d88aa135460b38c055a8973f0a2b21c6a0a4
nklasens <nklasens@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-11-30 11:22:58 +0000

    CMSC-341 Rewritten navigation optional items
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@26095 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer_Modules/publish-remote/src/java/com/finalist/cmsc/publish/ChannelPublisher.java
+++ contributions/CMSContainer_Modules/publish-remote/src/java/com/finalist/cmsc/publish/ChannelPublisher.java
@@ -3,53 +3,32 @@ package com.finalist.cmsc.publish;
 import java.util.*;
 
 import org.mmbase.bridge.*;
-import org.mmbase.remotepublishing.PublishManager;
-import org.mmbase.remotepublishing.util.PublishUtil;
 import com.finalist.cmsc.repository.RepositoryUtil;
 
 /**
  * @author Jeoffrey Bakker, Finalist IT Group
  */
-public class ChannelPublisher extends Publisher {
+public class ChannelPublisher extends Publisher{
 
    public ChannelPublisher(Cloud cloud) {
       super(cloud);
    }
-
 
    @Override
    public boolean isPublishable(Node node) {
       return RepositoryUtil.isContentChannel(node) || RepositoryUtil.isCollectionChannel(node);
    }
 
-
-   @Override
-   public void publish(Node node) {
-      PublishUtil.publishOrUpdateNode(cloud, node.getNumber());
-   }
-
-
    @Override
    public void publish(Node channel, NodeList contentnodes) {
-      List<Integer> relatedNodes = new ArrayList<Integer>();
-      for (Iterator<Node> iterator = contentnodes.iterator(); iterator.hasNext();) {
-         Node content = iterator.next();
-         if (PublishManager.isPublished(content)) {
-            relatedNodes.add(content.getNumber());
-         }
-      }
-      PublishUtil.publishOrUpdateRelations(cloud, channel.getNumber(), relatedNodes);
+       List<Integer> relatedNodes = new ArrayList<Integer>();
+       for (Iterator<Node> iterator = contentnodes.iterator(); iterator.hasNext();) {
+           Node content = iterator.next();
+           if (isPublished(content)) {
+               relatedNodes.add(content.getNumber());
+           }
+       }
+       publishNode(channel, relatedNodes);
    }
 
-
-   @Override
-   public void remove(Node node) {
-      PublishUtil.removeFromQueue(node);
-   }
-
-
-   @Override
-   public void unpublish(Node node) {
-      PublishUtil.removeNode(cloud, node.getNumber());
-   }
 }
--- contributions/CMSContainer_Modules/publish-remote/src/java/com/finalist/cmsc/publish/Publisher.java
+++ contributions/CMSContainer_Modules/publish-remote/src/java/com/finalist/cmsc/publish/Publisher.java
@@ -6,7 +6,7 @@ OSI Certified is a certification mark of the Open Source Initiative.
 The license (Mozilla version 1.0) can be read at the MMBase site.
 See http://www.MMBase.org/license
 
- */
+*/
 package com.finalist.cmsc.publish;
 
 import java.util.*;
@@ -20,88 +20,111 @@ import com.finalist.cmsc.repository.ContentElementUtil;
 import com.finalist.cmsc.repository.RepositoryUtil;
 import com.finalist.cmsc.services.workflow.Workflow;
 
+
 public abstract class Publisher {
+    
+    protected static final String SOURCE = "SOURCE";
+    protected static final String DESTINATION = "DESTINATION";
 
-   protected static final String SOURCE = "SOURCE";
-   protected static final String DESTINATION = "DESTINATION";
+    protected Cloud cloud;
 
-   protected Cloud cloud;
+    public Publisher(Cloud cloud) {
+        this.cloud = cloud;
+    }
 
+    public abstract boolean isPublishable(Node node);
 
-   public Publisher(Cloud cloud) {
-      this.cloud = cloud;
-   }
+    public void publish(Node node) {
+        PublishUtil.publishOrUpdateNode(cloud, node.getNumber());
+    }
 
-
-   public abstract boolean isPublishable(Node node);
-
-
-   public abstract void publish(Node node);
-
-
-   public void publish(Node node, NodeList nodes) {
-      publish(node);
-
-      for (Iterator<Node> iterator = nodes.iterator(); iterator.hasNext();) {
-         Node pnode = iterator.next();
-         Date publishDate;
-         if (node.getNodeManager().hasField("publishdate")) {
-            publishDate = node.getDateValue("publishdate");
-         }
-         else {
-            publishDate = new Date();
-         }
-         PublishUtil.publishOrUpdateNode(cloud, pnode.getNumber(), publishDate);
-      }
-   }
-
-
-   public abstract void remove(Node node);
-
-
-   public abstract void unpublish(Node node);
-
-
-   public static List<Node> findContentBlockNodes(Node node) {
-      List<Node> nodes = new ArrayList<Node>();
-      findContentBlockNodes(node, nodes);
-      return nodes;
-   }
-
-
-   private static void findContentBlockNodes(Node node, List<Node> nodes) {
-      if (nodes.contains(node) || TypeUtil.isSystemType(node.getNodeManager().getName())) {
-         return;
-      }
-
-      nodes.add(node);
-      RelationManagerList rml = node.getNodeManager().getAllowedRelations((NodeManager) null, null, DESTINATION);
-      if (!rml.isEmpty()) {
-         NodeIterator childs = node.getRelatedNodes("object", null, DESTINATION).nodeIterator();
-         while (childs.hasNext()) {
-            Node childNode = childs.nextNode();
-            if (ContentElementUtil.isContentElement(childNode)) {
-               if (!RepositoryUtil.hasContentChannel(childNode)) {
-                  findContentBlockNodes(childNode, nodes);
-               }
+    public void publish(Node node, NodeList nodes) {
+        publish(node);
+        
+        for (Iterator<Node> iterator = nodes.iterator(); iterator.hasNext();) {
+            Node pnode = iterator.next();
+            Date publishDate;
+            if (node.getNodeManager().hasField("publishdate")) {
+                publishDate = node.getDateValue("publishdate");
             }
             else {
-               if (!RepositoryUtil.isContentChannel(childNode) && !Workflow.isWorkflowElement(childNode)) {
-                  findContentBlockNodes(childNode, nodes);
+                publishDate = new Date();
+            }
+            PublishUtil.publishOrUpdateNode(cloud, pnode.getNumber(), publishDate);
+        }
+    }
+    
+    public void remove(Node node) {
+        PublishUtil.removeFromQueue(node);
+    }
+
+    public final void unpublish(Node node) {
+        PublishUtil.removeNode(cloud, node.getNumber());
+    }
+
+    
+    public static List<Node> findContentBlockNodes(Node node) {
+        List<Node> nodes = new ArrayList<Node>();
+        findContentBlockNodes(node, nodes);
+        return nodes;
+    }
+    
+    private static void findContentBlockNodes(Node node, List<Node> nodes) {
+        if (nodes.contains(node) || TypeUtil.isSystemType(node.getNodeManager().getName())) {
+            return;
+        }
+
+        nodes.add(node);
+        RelationManagerList rml = node.getNodeManager().getAllowedRelations((NodeManager) null, null, DESTINATION);        
+        if (!rml.isEmpty()) {
+            NodeIterator childs = node.getRelatedNodes("object", null, DESTINATION).nodeIterator();
+            while (childs.hasNext()) {
+               Node childNode = childs.nextNode();
+               if (ContentElementUtil.isContentElement(childNode)) {
+                   if (!RepositoryUtil.hasContentChannel(childNode)) {
+                       findContentBlockNodes(childNode, nodes);
+                   }
+               }
+               else {
+                   if (!RepositoryUtil.isContentChannel(childNode) &&
+                           !Workflow.isWorkflowElement(childNode)) {
+                      findContentBlockNodes(childNode, nodes);
+                   }
                }
             }
-         }
-      }
-   }
+        }
+    }
 
+    public int getLiveNumber(Node node) {
+        Map<Integer,Integer> numbers = PublishManager.getPublishedNodeNumbers(node);
+        Iterator<Integer> iter = numbers.values().iterator();
+        if (iter.hasNext()) {
+            return iter.next();
+        }
+        return -1;
+    }
 
-   public int getLiveNumber(Node node) {
-      Map<Integer, Integer> numbers = PublishManager.getPublishedNodeNumbers(node);
-      Iterator<Integer> iter = numbers.values().iterator();
-      if (iter.hasNext()) {
-         return iter.next();
-      }
-      return -1;
-   }
+    protected boolean isPublished(Node node) {
+        return PublishManager.isPublished(node);
+    }
+    
+
+    protected void publishNodes(Map<Node, Date> nodes) {
+        for (Map.Entry<Node, Date> entry : nodes.entrySet()) {
+            Node pnode = entry.getKey();
+            Date publish = entry.getValue();
+            PublishUtil.publishOrUpdateNode(cloud, pnode.getNumber(), publish);
+        }
+    }
+
+    protected void publishNode(Node parent, List<Integer> relatedNodes) {
+        PublishUtil.publishOrUpdateRelations(cloud, parent.getNumber(), relatedNodes);
+    }
+    
+    protected void removeNodes(Collection<Node> removeNodes) {
+        for (Node pnode : removeNodes) {
+            PublishUtil.removeFromQueue(pnode);
+        }
+    }
 
 }
