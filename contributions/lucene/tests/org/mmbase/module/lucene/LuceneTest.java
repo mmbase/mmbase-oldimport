7dcd3f018ce535dbdbef12a25f1b3bc73f0835ce
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-01-04 16:54:21 +0000

    more tests
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@26458 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/lucene/tests/org/mmbase/module/lucene/LuceneTest.java
+++ contributions/lucene/tests/org/mmbase/module/lucene/LuceneTest.java
@@ -17,103 +17,112 @@ import junit.framework.TestCase;
 /**
  *
  * @author Michiel Meeuwissen
- * @verion $Id: LuceneTest.java,v 1.1 2007-12-27 09:44:52 michiel Exp $
+ * @verion $Id: LuceneTest.java,v 1.2 2008-01-04 16:54:21 michiel Exp $
  */
 public class LuceneTest extends BridgeTest {
 
     private static Lucene lucene;
-    private static Node article;
+    private static int articleNumber;
     private static Cloud cloud;
 
     public void setUp() throws Exception {
-        System.out.println("Starting mmbase");
-        startMMBase(true);
-        lucene = org.mmbase.module.Module.getModule(Lucene.class);
-        Thread.sleep(15000);
-        System.out.println("" + lucene.listFunction.getFunctionValue());
-        System.out.println("NOW DOING FULL INDICES");
-        lucene.fullIndexFunction.getFunctionValue("mynews_magazine");
-        lucene.fullIndexFunction.getFunctionValue("mynews_news");
-        Thread.sleep(5000);
-        //assertIndicesUpToDate();
-
+        if (cloud == null) {
+            System.out.println("Starting mmbase");
+            startMMBase(true);
+            lucene = org.mmbase.module.Module.getModule(Lucene.class);
+            Thread.sleep(15000); // make sure mynew is deployed. (should perhaps implemented some wait
+            // functionality too).
+            System.out.println("" + lucene.listFunction.getFunctionValue());
+            System.out.println("NOW DOING FULL INDICES");
+            lucene.fullIndexFunction.getFunctionValue("mynews_magazine");
+            lucene.fullIndexFunction.getFunctionValue("mynews_news");
+            cloud = getCloud();
+            System.out.println("Found cloud " + cloud + " and lucene " + lucene);
+        }
     }
 
-    private void testSize(String index, String value, int expected) {
+    private Node getArticle() {
+        return cloud.getNode(articleNumber);
+    }
+
+    private void testSize(String index, String value, int expected) throws Exception {
         int searchSize = lucene.getSearcher(index).searchSize(cloud, value);
         assertTrue ("Search size is not " + expected + " but " + searchSize, searchSize == expected);
     }
-    private void assertIndicesUpToDate() throws Exception {
-        // perhaps something faster then this could be come up with.
+    private void waitForIndicesUpToDate() throws Exception {
         lucene.waitFunction.getFunctionValue();
-        //Thread.sleep(14000);
     }
     /**
      */
     public void testIndex() throws Exception {
-        cloud = getCloud();
-        System.out.println("Found cloud " + cloud + " and lucene " + lucene);
+        waitForIndicesUpToDate();
+
+        testSize("mynews_news", null, 5);
+        testSize("mynews_news", "xml", 3);
 
         testSize("mynews_magazine", null, 5);
         testSize("mynews_magazine", "xml", 3);
-        testSize("mynews_news", null, 5);
-        testSize("mynews_news", "xml", 3);
     }
 
     public void testChange() throws Exception {
-        article = cloud.getNode("anewsarticle");
+        Node article = cloud.getNode("anewsarticle");
         article.setStringValue("title", "something else");
         article.setStringValue("subtitle", "something else");
         article.setStringValue("intro", "something else");
         article.setStringValue("body", "something else");
         article.commit();
-        assertIndicesUpToDate();
-        Thread.sleep(10000);
+        waitForIndicesUpToDate();
         testSize("mynews_magazine", "xml", 2);
         testSize("mynews_news", "xml", 2);
     }
 
     public void testChangeStillInCondition() throws Exception {
-        article = cloud.getNode("anewsarticle");
+        Node article = cloud.getNode("anewsarticle");
         article.setStringValue("title", "Again with XML");
         article.commit();
-        assertIndicesUpToDate();
+        waitForIndicesUpToDate();
         testSize("mynews_magazine", "xml", 3);
         testSize("mynews_news", "xml", 3);
     }
 
     public void testCreateInConditionNotInIndex() throws Exception {
-        article = cloud.getNodeManager("news").createNode();
+        Node article = cloud.getNodeManager("news").createNode();
         article.setStringValue("title", "Also with XML");
         article.commit();
-        assertIndicesUpToDate();
+        articleNumber = article.getNumber();
+        waitForIndicesUpToDate();
         testSize("mynews_magazine", "xml", 3);
         testSize("mynews_news", "xml", 4);
     }
+    public static int rel1Number;
     public void testRelateIntoIndex() throws Exception {
         Node mag = cloud.getNode("default.mags");
         RelationManager rm = cloud.getRelationManager("mags", "news", "posrel");
-        Relation r = rm.createRelation(mag, article);
+        Relation r = rm.createRelation(mag, getArticle());
         r.commit();
-        assertIndicesUpToDate();
+        rel1Number = r.getNumber();
+        waitForIndicesUpToDate();
         testSize("mynews_magazine", "xml", 4);
     }
     public void testChangeRemainInIndex() throws Exception {
+        Node article = getArticle();
         article.setStringValue("title", "Still contains XML");
         article.commit();
-        assertIndicesUpToDate();
+        waitForIndicesUpToDate();
         testSize("mynews_magazine", "xml", 4);
     }
     public void testRemoveFromCondition() throws Exception {
+        Node article = getArticle();
         article.setStringValue("title", "XLM is not ok");
         article.commit();
-        assertIndicesUpToDate();
+        waitForIndicesUpToDate();
         testSize("mynews_magazine", "xml", 3);
     }
     public void testPutBackIntoCondition() throws Exception {
+        Node article = getArticle();
         article.setStringValue("title", "XML is ok");
         article.commit();
-        assertIndicesUpToDate();
+        waitForIndicesUpToDate();
         testSize("mynews_magazine", "xml", 4);
     }
 
@@ -122,9 +131,47 @@ public class LuceneTest extends BridgeTest {
         mag.setStringValue("subtitle", "test test");
         mag.commit();
         // changing startnodes should have no influence of size of indices.
-        assertIndicesUpToDate();
+        waitForIndicesUpToDate();
         testSize("mynews_magazine", "xml", 4);
         testSize("mynews_news", "xml", 4);
+    }
+
+    public static int rel2Number;
+    public void testRelateIntoIndexTwice() throws Exception {
+        Node mag = cloud.getNode("nondefault.mags");
+        RelationManager rm = cloud.getRelationManager("mags", "news", "posrel");
+        Relation r = rm.createRelation(mag, getArticle());
+        r.commit();
+        rel2Number = r.getNumber();
+        waitForIndicesUpToDate();
+        testSize("mynews_magazine", "xml", 4);
+    }
+    public void testRemoveFromConditionRelatedTwice() throws Exception {
+        Node article = getArticle();
+        article.setStringValue("title", "XLM is not ok");
+        article.commit();
+        waitForIndicesUpToDate();
+        testSize("mynews_magazine", "xml", 3);
+    }
+    public void testPutBackIntoConditionRelatedTwice() throws Exception {
+        Node article = getArticle();
+        article.setStringValue("title", "XML is ok");
+        article.commit();
+        waitForIndicesUpToDate();
+        testSize("mynews_magazine", "xml", 4);
+    }
+
+    public void testUnRelateFromStartNodeButStilInIndex() throws Exception {
+        Node r = cloud.getNode(rel1Number);
+        r.delete();
+        waitForIndicesUpToDate();
+        testSize("mynews_magazine", "xml", 4);
+    }
+    public void testUnRelateFromLastStartNode() throws Exception {
+        Node r = cloud.getNode(rel2Number);
+        r.delete();
+        waitForIndicesUpToDate();
+        testSize("mynews_magazine", "xml", 3);
     }
 
 
