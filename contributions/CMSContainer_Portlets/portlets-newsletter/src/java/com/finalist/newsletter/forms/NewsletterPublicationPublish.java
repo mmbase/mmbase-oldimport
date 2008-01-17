385a2b44709e1ba33fd3790047d6000e56e6ab3f
jstroomer <jstroomer@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-01-17 13:44:03 +0000

    CMSC-698 Added a confirmation page after the user clicks the Send Publication option. Also had to moddify the struts config and action handler
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@26580 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer_Portlets/portlets-newsletter/src/java/com/finalist/newsletter/forms/NewsletterPublicationPublish.java
+++ contributions/CMSContainer_Portlets/portlets-newsletter/src/java/com/finalist/newsletter/forms/NewsletterPublicationPublish.java
@@ -14,21 +14,58 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.mmbase.bridge.Cloud;
+import org.mmbase.bridge.Node;
 
+import com.finalist.cmsc.navigation.NavigationUtil;
+import com.finalist.cmsc.security.SecurityUtil;
+import com.finalist.cmsc.security.UserRole;
 import com.finalist.cmsc.struts.MMBaseFormlessAction;
 import com.finalist.newsletter.publisher.NewsletterPublisher;
 
 public class NewsletterPublicationPublish extends MMBaseFormlessAction {
 
+   /** name of submit button in jsp to confirm removal */
+   private static final String ACTION_REMOVE = "remove";
+
+   /** name of submit button in jsp to cancel removal */
+   private static final String ACTION_CANCEL = "cancel";
+
    @Override
    public ActionForward execute(ActionMapping mapping, HttpServletRequest request, Cloud cloud) throws Exception {
 
-      String number = request.getParameter("number");
-      if (number != null) {
+      int number = Integer.parseInt(getParameter(request, "number", true));
+
+      if (isSendAction(request)) {
+         Node newsletterPublicationNode = cloud.getNode(number);
+
+         UserRole role = NavigationUtil.getRole(newsletterPublicationNode.getCloud(), newsletterPublicationNode, false);
+         boolean isWebMaster = (role != null && SecurityUtil.isWebmaster(role));
+
+         if (NavigationUtil.getChildCount(newsletterPublicationNode) > 0 && !isWebMaster) {
+            return mapping.findForward("confirmationpage");
+         }
          Thread publisher = new NewsletterPublisher(number);
          publisher.start();
+
+         return mapping.findForward(SUCCESS);
       }
-      ActionForward ret = mapping.findForward("success");
-      return ret;
+
+      if (isCancelAction(request)) {
+         String forwardPath = mapping.findForward("cancel").getPath();
+         forwardPath = forwardPath.concat("?showpage=" + number);
+         return new ActionForward(forwardPath);
+      }
+
+      // neither remove or cancel, show confirmation page
+      return mapping.findForward("confirmationpage");
    }
+
+   private boolean isCancelAction(HttpServletRequest request) {
+      return getParameter(request, ACTION_CANCEL) != null;
+   }
+
+   private boolean isSendAction(HttpServletRequest request) {
+      return getParameter(request, ACTION_REMOVE) != null;
+   }
+
 }
