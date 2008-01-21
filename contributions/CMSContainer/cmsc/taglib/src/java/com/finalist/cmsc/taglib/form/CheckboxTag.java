b4ecfc0659e071b2d8983bdab1405f602d0bf923
jstroomer <jstroomer@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-01-21 12:29:02 +0000

    CMSC-632 Was broken, not anymore :-)
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@26636 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer/cmsc/taglib/src/java/com/finalist/cmsc/taglib/form/CheckboxTag.java
+++ contributions/CMSContainer/cmsc/taglib/src/java/com/finalist/cmsc/taglib/form/CheckboxTag.java
@@ -22,71 +22,87 @@ import javax.servlet.jsp.tagext.SimpleTagSupport;
 
 public class CheckboxTag extends SimpleTagSupport {
 
-   public String var;
-   public String value;
-   public Object selected;
+	public String var;
+	public String value;
+	public Object selected;
+	public boolean checked;
 
-   @Override
-   public void doTag() throws JspException, IOException {
-      PageContext ctx = (PageContext) getJspContext();
+	@Override
+	public void doTag() throws JspException, IOException {
+		PageContext ctx = (PageContext) getJspContext();
 
-      ctx.getOut().print("<input type=\"checkbox\" class=\"checkbox\" name=\"" + var + "\" value=\"" + value + "\" ");
-      if (isSelected(ctx.getRequest()) == true) {
-         ctx.getOut().print("checked=\"checked\"");
-      }
-      ctx.getOut().print(">");
-      JspFragment frag = getJspBody();
-      if (frag != null) {
-         frag.invoke(null);
-      }
-   }
+		ctx.getOut().print("<input type=\"checkbox\" class=\"checkbox\" style=\"width:15px;\" name=\"" + var + "\" value=\"" + value + "\" ");
+		if (isSelected(ctx.getRequest()) == true || this.checked == true) {
+			ctx.getOut().print("checked=\"checked\"");
+		}
+		ctx.getOut().print(">");
+		JspFragment frag = getJspBody();
+		if (frag != null) {
+			frag.invoke(null);
+		}
+	}
 
-   private boolean isSelected(ServletRequest request) {
-      Object selectedValues = request.getAttribute(var);
-      if (selectedValues != null) {
-         if (selectedValues instanceof String) {
-            return ((String) selectedValues).equals(value);
-         } else if (selectedValues instanceof String[]) {
-            String[] selected = (String[]) selectedValues;
-            List<String> selectedItems = Arrays.asList(selected);
-            if (selectedItems.contains(value)) {
-               return true;
-            }
-         } else if (selectedValues instanceof Integer) {
-            return ((Integer) selectedValues).equals(value);
-         } else if (selectedValues instanceof Integer[]) {
-            Integer[] selected = (Integer[]) selectedValues;
-            List<Integer> selectedItems = new ArrayList<Integer>();
-            for (int i = 0; i < selected.length; i++) {
-               selectedItems.add(Integer.valueOf(selected[i]));
-            }
-            if (selectedItems.contains(value)) {
-               return true;
-            }
-         } else if (selectedValues instanceof List) {
-            List<String> selectedItems = (ArrayList<String>) selectedValues;
-            if (selectedItems.contains(value)) {
-               return true;
-            }
-         }
-      }
-      return false;
-   }
+	private boolean isSelected(ServletRequest request) {
+		Object selectedValues = request.getAttribute(var);
+		if (selectedValues != null) {
+			if (selectedValues instanceof String) {
+				return ((String) selectedValues).equals(value);
+			} else if (selectedValues instanceof String[]) {
+				String[] selected = (String[]) selectedValues;
+				List<String> selectedItems = Arrays.asList(selected);
+				if (selectedItems.contains(value)) {
+					return true;
+				}
+			} else if (selectedValues instanceof Integer) {
+				return ((Integer) selectedValues).equals(value);
+			} else if (selectedValues instanceof Integer[]) {
+				Integer[] selected = (Integer[]) selectedValues;
+				List<Integer> selectedItems = new ArrayList<Integer>();
+				for (int i = 0; i < selected.length; i++) {
+					selectedItems.add(Integer.valueOf(selected[i]));
+				}
+				if (selectedItems.contains(value)) {
+					return true;
+				}
+			} else if (selectedValues instanceof List) {
+				List selectedItems = (ArrayList) selectedValues;
+				if (selectedItems != null && selectedItems.size() > 0) {
+					for (int i = 0; i < selectedItems.size(); i++) {
+						String item = String.valueOf(selectedItems.get(i));
+						if (item.equals(value)) {
+							return true;
+						}
+					}
 
-   public void setVar(String var) {
-      this.var = var;
-   }
+				}
+			}
+		}
+		return false;
+	}
 
-   public String getValue() {
-      return value;
-   }
+	public void setVar(String var) {
+		this.var = var;
+	}
 
-   public void setValue(String value) {
-      this.value = value;
-   }
 
-   public void setSelected(Object selected) {
-      this.selected = selected;
-   }
+	public void setSelected(Object selected) {
+		this.selected = selected;
+	}
+
+	public boolean isChecked() {
+		return checked;
+	}
+
+	public void setChecked(boolean checked) {
+		this.checked = checked;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
 
 }
