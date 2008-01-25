da4d09374e4889ce291d50d643af77b384fdd317
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-01-25 15:07:24 +0000

    didn't compile agains mmbase 1.8
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@26781 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/didactor2/src/core/java/nl/didactor/builders/DidactorBuilder.java
+++ contributions/didactor2/src/core/java/nl/didactor/builders/DidactorBuilder.java
@@ -5,6 +5,7 @@ import org.mmbase.module.core.MMObjectNode;
 import org.mmbase.module.core.MMBase;
 import org.mmbase.module.corebuilders.FieldDefs;
 import org.mmbase.module.corebuilders.InsRel;
+import org.mmbase.core.CoreField;
 import org.mmbase.util.logging.Logger;
 import org.mmbase.util.logging.Logging;
 import java.util.*;
@@ -63,7 +64,7 @@ public class DidactorBuilder extends MMObjectBuilder {
     }
 
     /**
-     * Overridden 'insert' from MMObjectBuilder. It will call the 'preInsert()' 
+     * Overridden 'insert' from MMObjectBuilder. It will call the 'preInsert()'
      * method for all registered components just before inserting the node. It
      * calls the 'postInsert()' for all registered components after inserting the node.
      */
@@ -78,13 +79,16 @@ public class DidactorBuilder extends MMObjectBuilder {
         }
         int res = super.insert(owner, node);
 
-        for (FieldDefs fd : (Collection<FieldDefs>) getFields()) {
+        for (CoreField fd : (Collection<CoreField>) getFields()) {
+            /*
             if (fd.getDBState() == FieldDefs.DBSTATE_VIRTUAL && fd.getDBPos() == 300) {
+            // WTF?
                 if (log.isDebugEnabled()) {
-                    log.debug("Have to process set on field [" + fd.getDBName() + "] with value [" + node.getValues().get(fd.getDBName()) + "]");
+                    log.debug("Have to process set on field [" + fd.getName() + "] with value [" + node.getValues().get(fd.getName()) + "]");
                 }
-                setFieldValue(owner, node, fd.getDBName());
+                setFieldValue(owner, node, fd.getName());
             }
+            */
         }
 
         for (EventInstance e : postInsertComponents) {
@@ -99,7 +103,7 @@ public class DidactorBuilder extends MMObjectBuilder {
 
 
     /**
-     * Overridden 'preCommit' from MMObjectBuilder. It will call the 'preCommit()' 
+     * Overridden 'preCommit' from MMObjectBuilder. It will call the 'preCommit()'
      * method for all registered components.
      */
     public MMObjectNode preCommit(MMObjectNode node) {
@@ -127,11 +131,14 @@ public class DidactorBuilder extends MMObjectBuilder {
     public boolean commit(MMObjectNode node) {
         boolean bSuperCommit = super.commit(node);
 
-        for (FieldDefs fd : (Collection<FieldDefs>) getFields()) {
+        for (CoreField fd : (Collection<CoreField>) getFields()) {
+            /*
+              WTF ?
             if (fd.getDBState() == FieldDefs.DBSTATE_VIRTUAL && fd.getDBPos() == 300) {
                 log.debug("Have to process set on field [" + fd.getDBName() + "] with value [" + node.getValues().get(fd.getDBName()) + "]");
                 setFieldValue(node.getStringValue("owner"), node, fd.getDBName());
             }
+            */
         }
 
         for (EventInstance e : postCommitComponents) {
@@ -141,7 +148,7 @@ public class DidactorBuilder extends MMObjectBuilder {
         }
         return bSuperCommit;
     }
-    
+
     /**
      * This method does NOT override any methods from MMObjectBuilder, but is triggered
      * by the authorization class. This is a rather ugly hack, which might not be supported
@@ -156,7 +163,7 @@ public class DidactorBuilder extends MMObjectBuilder {
         }
         return true;
     }
-    
+
     /**
      * Return the value for a field of the node. This method
      * is overridden from MMObjectBuilder, and will return a value
@@ -210,7 +217,7 @@ public class DidactorBuilder extends MMObjectBuilder {
         InsRel insrel = (InsRel)MMBase.getMMBase().getBuilder("authrel");
         insrel.insert(owner, node.getNumber(), fieldId, authrel);
     }
-        
+
     /**
      * This small innerclass represents an event-instance. It mainly is just a wrapper
      * around the component, but also has a priority that allows the components to be
@@ -219,7 +226,7 @@ public class DidactorBuilder extends MMObjectBuilder {
     private class EventInstance implements Comparable {
         protected Component component;
         protected int priority;
-      
+
         /**
          * Public constructor
          */
