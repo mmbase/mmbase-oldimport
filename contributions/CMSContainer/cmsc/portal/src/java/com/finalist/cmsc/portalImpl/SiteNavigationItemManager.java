7404dec5e6bcfe7f0d1055664c81e5d8a12e86b4
nklasens <nklasens@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-01-28 22:31:16 +0000

    CMSC-690 SiteTree: 'Make new page' twice in menu Used wrong treemanager for site
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@26810 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer/cmsc/portal/src/java/com/finalist/cmsc/portalImpl/SiteNavigationItemManager.java
+++ contributions/CMSContainer/cmsc/portal/src/java/com/finalist/cmsc/portalImpl/SiteNavigationItemManager.java
@@ -3,11 +3,11 @@ package com.finalist.cmsc.portalImpl;
 import com.finalist.cmsc.beans.om.*;
 import com.finalist.cmsc.navigation.NavigationTreeItemRenderer;
 import com.finalist.cmsc.navigation.SiteUtil;
-import com.finalist.cmsc.navigation.tree.PageTreeItemRenderer;
+import com.finalist.cmsc.navigation.tree.SiteTreeItemRenderer;
 
 public class SiteNavigationItemManager extends PageNavigationItemManager {
     
-	private NavigationTreeItemRenderer treeRenderer = new PageTreeItemRenderer();
+	private NavigationTreeItemRenderer treeRenderer = new SiteTreeItemRenderer();
 	
     @Override
 	public String getTreeManager() {
