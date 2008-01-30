3967b39ac4d9448898b7b996115c4c24b0b471cb
wheijke <wheijke@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-01-30 14:06:55 +0000

    works
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@26843 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer_Modules/community/src/java/com/finalist/cmsc/community/taglib/ListUsersTag.java
+++ contributions/CMSContainer_Modules/community/src/java/com/finalist/cmsc/community/taglib/ListUsersTag.java
@@ -1,51 +1,41 @@
 package com.finalist.cmsc.community.taglib;
 
 import java.io.IOException;
-import java.util.Iterator;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.JspException;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import javax.servlet.jsp.PageContext;
 
 import com.finalist.cmsc.services.community.security.Authentication;
 import com.finalist.cmsc.services.community.security.AuthenticationService;
 
 public class ListUsersTag extends CommunityTagSupport {
 
-	private static Log log = LogFactory.getLog(CommunityTagSupport.class);
-	
 	private String var;
 	
 	@Override
 	protected void doTagLogic() throws JspException, IOException {
-	
+		PageContext ctx = (PageContext) getJspContext();
+		HttpServletRequest req = (HttpServletRequest) ctx.getRequest();
 		
 		AuthenticationService as = getAuthenticationService();
 		List<Authentication> list = as.findAuthentications();
 
-		log.info("========>size="+list.size());
-		
-		Iterator<Authentication> lIt = list.iterator();
-		while (lIt.hasNext()) {
-			Authentication auth = lIt.next();
-			log.info("========>"+auth.getUserId());
+		if (var != null) {
+			if (list != null) {
+				req.setAttribute(var, list);
+			} else {
+				req.removeAttribute(var);
+			}
+		} else {
+			ctx.getOut().print(list);
 		}
 		
-//		// handle result
-//		if (var != null) {
-//			// put in variable
-//			if (list != null) {
-//				req.setAttribute(var, list);
-//			} else {
-//				req.removeAttribute(var);
-//			}
-//		} else {
-//			// write
-//			ctx.getOut().print(list);
-//		}
-		
+	}
+
+	public void setVar(String var) {
+		this.var = var;
 	}
 
 }
