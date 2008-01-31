5434ad9cc02aa32f1952d3eb634690dbc50274bd
wheijke <wheijke@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-01-31 12:59:17 +0000

    added constants
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@26863 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer_Modules/community/src/java/com/finalist/cmsc/community/forms/AbstractCommunityAction.java
+++ contributions/CMSContainer_Modules/community/src/java/com/finalist/cmsc/community/forms/AbstractCommunityAction.java
@@ -1,12 +1,12 @@
 /*
 
-This software is OSI Certified Open Source Software.
-OSI Certified is a certification mark of the Open Source Initiative.
+ This software is OSI Certified Open Source Software.
+ OSI Certified is a certification mark of the Open Source Initiative.
 
-The license (Mozilla version 1.0) can be read at the MMBase site.
-See http://www.MMBase.org/license
+ The license (Mozilla version 1.0) can be read at the MMBase site.
+ See http://www.MMBase.org/license
 
-*/
+ */
 package com.finalist.cmsc.community.forms;
 
 import org.springframework.web.struts.ActionSupport;
@@ -21,23 +21,27 @@ import com.finalist.cmsc.services.community.preferences.PreferenceService;
  */
 public class AbstractCommunityAction extends ActionSupport {
 
-    protected AuthenticationService getAuthenticationService() {
-        WebApplicationContext ctx = getWebApplicationContext();
-        return (AuthenticationService)ctx.getBean("authenticationService");
-    }
+	protected static final String ACTION_ADD = "add";
 
-    protected AuthorityService getAuthorityService() {
-        WebApplicationContext ctx = getWebApplicationContext();
-        return (AuthorityService)ctx.getBean("authorityService");
-    }
+	protected static final String ACTION_EDIT = "edit";
 
-    protected PersonService getPersonService() {
-        WebApplicationContext ctx = getWebApplicationContext();
-        return (PersonService)ctx.getBean("personService");
-    }
+	protected AuthenticationService getAuthenticationService() {
+		WebApplicationContext ctx = getWebApplicationContext();
+		return (AuthenticationService) ctx.getBean("authenticationService");
+	}
 
-    protected PreferenceService getPreferenceService() {
-        WebApplicationContext ctx = getWebApplicationContext();
-        return (PreferenceService)ctx.getBean("preferenceService");
-    }
+	protected AuthorityService getAuthorityService() {
+		WebApplicationContext ctx = getWebApplicationContext();
+		return (AuthorityService) ctx.getBean("authorityService");
+	}
+
+	protected PersonService getPersonService() {
+		WebApplicationContext ctx = getWebApplicationContext();
+		return (PersonService) ctx.getBean("personService");
+	}
+
+	protected PreferenceService getPreferenceService() {
+		WebApplicationContext ctx = getWebApplicationContext();
+		return (PreferenceService) ctx.getBean("preferenceService");
+	}
 }
