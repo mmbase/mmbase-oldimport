904add7ef326945914931fc6f3537a8620868441
wheijke <wheijke@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-01-31 13:48:49 +0000

    first working
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@26867 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer_Modules/community/src/java/com/finalist/cmsc/community/forms/UserAddAction.java
+++ contributions/CMSContainer_Modules/community/src/java/com/finalist/cmsc/community/forms/UserAddAction.java
@@ -1,31 +1,77 @@
 /*
 
-This software is OSI Certified Open Source Software.
-OSI Certified is a certification mark of the Open Source Initiative.
+ This software is OSI Certified Open Source Software.
+ OSI Certified is a certification mark of the Open Source Initiative.
 
-The license (Mozilla version 1.0) can be read at the MMBase site.
-See http://www.MMBase.org/license
+ The license (Mozilla version 1.0) can be read at the MMBase site.
+ See http://www.MMBase.org/license
 
-*/package com.finalist.cmsc.community.forms;
+ */package com.finalist.cmsc.community.forms;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
+import com.finalist.cmsc.services.community.person.PersonService;
+import com.finalist.cmsc.services.community.security.AuthenticationService;
+
 /**
+ * Add a Authentication and Person
+ * 
  * @author Remco Bos
+ * @author Wouter Heijke
  */
 public class UserAddAction extends AbstractCommunityAction {
 
-    public ActionForward execute(ActionMapping actionMapping, ActionForm actionForm, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
-        UserForm userForm = (UserForm)actionForm;
+	private static Log log = LogFactory.getLog(UserAddInitAction.class);
 
-        getAuthenticationService().createAuthentication(userForm.getEmail(), userForm.getPassword());
+	public ActionForward execute(ActionMapping actionMapping, ActionForm actionForm, HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse) throws Exception {
 
+		if (!isCancelled(httpServletRequest)) {
+			UserForm userForm = (UserForm) actionForm;
 
-        return actionMapping.findForward("success");
-    }
+			AuthenticationService as = getAuthenticationService();
+			PersonService ps = getPersonService();
+
+			if (userForm.getAction().equalsIgnoreCase(ACTION_ADD)) {
+				Long check1 = as.getAuthenticationIdForUserId(userForm.getEmail());
+				if (check1 == null) {
+					as.createAuthentication(userForm.getEmail(), userForm.getPassword());
+					Long check2 = as.getAuthenticationIdForUserId(userForm.getEmail());
+					if (check2 != null) {
+
+						ps.createPerson(userForm.getVoornaam(), userForm.getTussenVoegsels(), userForm.getAchterNaam(), userForm
+								.getEmail());
+					} else {
+						log.info("add check2 failed");
+					}
+				} else {
+					log.info("add check1 failed");
+				}
+			} else if (userForm.getAction().equalsIgnoreCase(ACTION_EDIT)) {
+				Long check1 = as.getAuthenticationIdForUserId(userForm.getEmail());
+				if (check1 == null) {
+					// as.updateAuthentication(userId, oldPassword, newPassword)
+
+				} else {
+					log.info("edit check1 failed");
+				}
+
+			} else {
+				log.info("action failed");
+			}
+		} else {
+			log.info("cancelled");
+		}
+
+		removeFromSession(httpServletRequest, actionForm);
+
+		return actionMapping.findForward(SUCCESS);
+	}
 }
--- contributions/CMSContainer_Modules/community/src/java/com/finalist/cmsc/community/forms/UserAddInitAction.java
+++ contributions/CMSContainer_Modules/community/src/java/com/finalist/cmsc/community/forms/UserAddInitAction.java
@@ -1,28 +1,66 @@
 /*
 
-This software is OSI Certified Open Source Software.
-OSI Certified is a certification mark of the Open Source Initiative.
+ This software is OSI Certified Open Source Software.
+ OSI Certified is a certification mark of the Open Source Initiative.
 
-The license (Mozilla version 1.0) can be read at the MMBase site.
-See http://www.MMBase.org/license
+ The license (Mozilla version 1.0) can be read at the MMBase site.
+ See http://www.MMBase.org/license
 
-*/package com.finalist.cmsc.community.forms;
+ */
+package com.finalist.cmsc.community.forms;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
+import com.finalist.cmsc.services.community.person.Person;
+import com.finalist.cmsc.services.community.person.PersonService;
+import com.finalist.cmsc.services.community.security.Authentication;
+import com.finalist.cmsc.services.community.security.AuthenticationService;
+
 /**
  * @author Remco Bos
+ * @author Wouter Heijke
  */
 public class UserAddInitAction extends AbstractCommunityAction {
+	private static Log log = LogFactory.getLog(UserAddInitAction.class);
 
-    public ActionForward execute(ActionMapping actionMapping, ActionForm actionForm, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
-        UserForm userForm = (UserForm)actionForm;
-        userForm.setAction("add");
-        return actionMapping.findForward("success");
-    }
+	public ActionForward execute(ActionMapping actionMapping, ActionForm actionForm, HttpServletRequest request,
+			HttpServletResponse httpServletResponse) throws Exception {
+
+		String id = request.getParameter(USERID);
+		UserForm userForm = (UserForm) actionForm;
+		if (id != null) {
+			userForm.setAction(ACTION_EDIT);
+
+			AuthenticationService as = getAuthenticationService();
+			Authentication auth = as.findAuthentication(id);
+			if (auth != null) {
+				userForm.setEmail(auth.getUserId());
+
+				PersonService ps = getPersonService();
+				Person person = ps.getPersonByUserId(id);
+				if (person != null) {
+					userForm.setVoornaam(person.getFirstName());
+					userForm.setTussenVoegsels(person.getInfix());
+					userForm.setAchterNaam(person.getLastName());
+				} else {
+					log.info("person failed");
+				}
+			} else {
+				log.info("auth failed");
+			}
+
+		} else {
+			// new
+			userForm.setAction(ACTION_ADD);
+		}
+
+		return actionMapping.findForward(SUCCESS);
+	}
 }
