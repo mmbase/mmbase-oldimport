6f3c3c5f78e37ede336cd08eaae605332e71c8a6
mguo <mguo@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-02-01 09:37:12 +0000

    CMSC-718 fixed.now isRelationEvent go over all MavigationgManager,if find a realtion,return true.
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@26872 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer/cmsc/sitemanagement/src/java/com/finalist/cmsc/services/sitemanagement/NavigationCacheEntryFactory.java
+++ contributions/CMSContainer/cmsc/sitemanagement/src/java/com/finalist/cmsc/services/sitemanagement/NavigationCacheEntryFactory.java
@@ -18,68 +18,72 @@ import com.finalist.cmsc.navigation.*;
 
 public class NavigationCacheEntryFactory extends MMBaseCacheEntryFactory {
 
-   public NavigationCacheEntryFactory() {
-      super(PagesUtil.PAGE);
-      for (NavigationItemManager manager : NavigationManager.getNavigationManagers()) {
-          if (!PagesUtil.isPageType(manager.getTreeManager())) {
-              registerListener(manager.getTreeManager());
-          }
-      }
-   }
-
-
-   @Override
-   protected Serializable loadEntry(Serializable key) throws Exception {
-      Node node = getNode(key);
-      NavigationItemManager manager = NavigationManager.getNavigationManager(node);
-      if (manager != null) {
-         return manager.loadNavigationItem((Integer) key, node);
-      }
-      return null;
-   }
-
-   @Override
-   protected boolean isRelationEvent(RelationEvent event, String nodeType) {
-       for (NavigationItemManager manager : NavigationManager.getNavigationManagers()) {
-           return super.isRelationEvent(event, manager.getTreeManager());
-       }
-       return false;
-   }
-
-
-   @Override
-   protected boolean isNodeEvent(NodeEvent event, String nodeType) {
-       return NavigationManager.getNavigationManager(event.getBuilderName()) != null;
-   }
-
-
-   @Override
-   public void notify(RelationEvent event) {
-      if (isRelationEvent(event)) {
-         Integer key = getKey(event);
-         if (key != null) {
-            switch (event.getType()) {
-               case Event.TYPE_CHANGE:
-                  refreshEntry(key);
-                  break;
-               case Event.TYPE_DELETE:
-                  // don't remove page entry when layout removed from page.
-                  if (PagesUtil.isPageType(event.getRelationSourceType()) 
-                          && !event.getRelationDestinationType().equals(PagesUtil.LAYOUT)) {
-                     deleteEntry(key);
-                  }
-                  break;
-               case Event.TYPE_NEW:
-                  refreshEntry(key);
-                  break;
-               case NodeEvent.TYPE_RELATION_CHANGE:
-                  deleteEntry(key);
-                  break;
-               default:
-                  break;
+    public NavigationCacheEntryFactory() {
+        super(PagesUtil.PAGE);
+        for (NavigationItemManager manager : NavigationManager.getNavigationManagers()) {
+            if (!PagesUtil.isPageType(manager.getTreeManager())) {
+                registerListener(manager.getTreeManager());
             }
-         }
-      }
-   }
+        }
+    }
+
+
+    @Override
+    protected Serializable loadEntry(Serializable key) throws Exception {
+        Node node = getNode(key);
+        NavigationItemManager manager = NavigationManager.getNavigationManager(node);
+        if (manager != null) {
+            return manager.loadNavigationItem((Integer) key, node);
+        }
+        return null;
+    }
+
+    @Override
+    protected boolean isRelationEvent(RelationEvent event, String nodeType) {
+        for (NavigationItemManager manager : NavigationManager.getNavigationManagers()) {
+
+            if (super.isRelationEvent(event, manager.getTreeManager())) {
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+
+
+    @Override
+    protected boolean isNodeEvent(NodeEvent event, String nodeType) {
+        return NavigationManager.getNavigationManager(event.getBuilderName()) != null;
+    }
+
+
+    @Override
+    public void notify(RelationEvent event) {
+        if (isRelationEvent(event)) {
+            Integer key = getKey(event);
+            if (key != null) {
+                switch (event.getType()) {
+                    case Event.TYPE_CHANGE:
+                        refreshEntry(key);
+                        break;
+                    case Event.TYPE_DELETE:
+                        // don't remove page entry when layout removed from page.
+                        if (PagesUtil.isPageType(event.getRelationSourceType()) && !event.getRelationDestinationType().equals(PagesUtil.LAYOUT)) {
+                            deleteEntry(key);
+                        }
+                        break;
+                    case Event.TYPE_NEW:
+                        refreshEntry(key);
+                        break;
+                    case NodeEvent.TYPE_RELATION_CHANGE:
+                        deleteEntry(key);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
 
 }
