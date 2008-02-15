fbf7e43a76a74ab8d8c996f621f881077f91f31a
nklasens <nklasens@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-02-15 09:41:36 +0000

    add check for parent dirs
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@27057 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer/scripts/buildall.bat
+++ contributions/CMSContainer/scripts/buildall.bat
@@ -1,2 +1,13 @@
 @ECHO OFF
-call build build cmsc ..\..\CMSContainer_Modules ..\..\CMSContainer_Portlets ..\..\%1
+
+set APPLICATION=%1
+IF exist "%APPLICATION%" GOTO okAppName
+set APPLICATION=..\..\%1
+IF exist "%APPLICATION%" GOTO okAppName
+echo Could not find the application with name %~2
+GOTO end
+
+:okAppName
+	call build build cmsc ..\..\CMSContainer_Modules ..\..\CMSContainer_Portlets
+	call build build %APPLICATION%
+:end
--- contributions/CMSContainer/scripts/cleanall.bat
+++ contributions/CMSContainer/scripts/cleanall.bat
@@ -2,4 +2,15 @@
 call clean ..\cmsc
 call clean ..\..\CMSContainer_Modules
 call clean ..\..\CMSContainer_Portlets
-call clean ..\..\%1
+
+set APPLICATION=%1
+IF exist "%APPLICATION%" GOTO okAppName
+set APPLICATION=..\..\%1
+IF exist "%APPLICATION%" GOTO okAppName
+echo Could not find the application with name %~2
+GOTO end
+
+:okAppName
+	call clean %APPLICATION%
+
+:end
