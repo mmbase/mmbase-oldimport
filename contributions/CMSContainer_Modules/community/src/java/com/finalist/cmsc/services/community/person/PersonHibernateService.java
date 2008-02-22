d0ae6e700d559c6884ba5d43022e48a346a292ae
wheijke <wheijke@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-02-22 12:39:37 +0000

    fixed a problem with deleting
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@27173 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer_Modules/community/src/java/com/finalist/cmsc/services/community/person/PersonHibernateService.java
+++ contributions/CMSContainer_Modules/community/src/java/com/finalist/cmsc/services/community/person/PersonHibernateService.java
@@ -1,16 +1,18 @@
 /*
 
-This software is OSI Certified Open Source Software.
-OSI Certified is a certification mark of the Open Source Initiative.
+ This software is OSI Certified Open Source Software.
+ OSI Certified is a certification mark of the Open Source Initiative.
 
-The license (Mozilla version 1.0) can be read at the MMBase site.
-See http://www.MMBase.org/license
+ The license (Mozilla version 1.0) can be read at the MMBase site.
+ See http://www.MMBase.org/license
 
-*/
+ */
 package com.finalist.cmsc.services.community.person;
 
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.hibernate.Criteria;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Required;
@@ -24,55 +26,60 @@ import com.finalist.cmsc.services.community.security.AuthenticationService;
  */
 public class PersonHibernateService extends HibernateService implements PersonService {
 
+	private static Log log = LogFactory.getLog(PersonHibernateService.class);
+
 	private AuthenticationService authenticationService;
-	
-    /** {@inheritDoc} */
-    @Transactional(readOnly = true)
-    public Person getPersonByUserId(String userId) {
-    	return findPersonByUserId(userId);
-    }
 
-    /** {@inheritDoc} */
-    @Transactional
-    public void createPerson(String firstName, String infix, String lastName, String userId) {
-    	Long authenticationId = authenticationService.getAuthenticationIdForUserId(userId);
-    	if (authenticationId != null) {
-        	Person person = new Person();
-            person.setFirstName(firstName);
-            person.setInfix(infix);
-            person.setLastName(lastName);
-            person.setAuthenticationId(authenticationId);
-            getSession().save(person);
-    	}
-    }
+	/** {@inheritDoc} */
+	@Transactional(readOnly = true)
+	public Person getPersonByUserId(String userId) {
+		return findPersonByUserId(userId);
+	}
 
-    /** {@inheritDoc} */
-    @Transactional
-    public void deletePersonByUserId(String userId) {
-        Person person = findPersonByUserId(userId);
-        getSession().delete(person);
-    }
+	/** {@inheritDoc} */
+	@Transactional
+	public void createPerson(String firstName, String infix, String lastName, String userId) {
+		Long authenticationId = authenticationService.getAuthenticationIdForUserId(userId);
+		if (authenticationId != null) {
+			Person person = new Person();
+			person.setFirstName(firstName);
+			person.setInfix(infix);
+			person.setLastName(lastName);
+			person.setAuthenticationId(authenticationId);
+			getSession().save(person);
+		}
+	}
 
-    private Person findPersonByUserId(String userId) {
-    	Long authenticationId = authenticationService.getAuthenticationIdForUserId(userId);
-    	Person person = null;
-    	if (authenticationId != null) {
-	    	Criteria criteria = getSession()
-	    		.createCriteria(Person.class)
-	    		.add(Restrictions.eq("authenticationId", authenticationId));
-	    	person = findPersonByCriteria(criteria);
-    	}
-        return person;
-    }
+	/** {@inheritDoc} */
+	@Transactional
+	public void deletePersonByUserId(String userId) {
+		if (userId != null) {
+			Person person = findPersonByUserId(userId);
+			if (person != null) {
+				getSession().delete(person);
+			}
+		}
+	}
 
-    @SuppressWarnings("unchecked")
-    private Person findPersonByCriteria(Criteria criteria) {
-        List personList = criteria.list();
-        return personList.size() == 1 ? (Person) personList.get(0) : null;
-    }
-    
-    @Required
-    public void setAuthenticationService(AuthenticationService authenticationService) {
-        this.authenticationService = authenticationService;
-    }
+	private Person findPersonByUserId(String userId) {
+		Long authenticationId = authenticationService.getAuthenticationIdForUserId(userId);
+		Person person = null;
+		if (authenticationId != null) {
+			Criteria criteria = getSession().createCriteria(Person.class).add(
+					Restrictions.eq("authenticationId", authenticationId));
+			person = findPersonByCriteria(criteria);
+		}
+		return person;
+	}
+
+	@SuppressWarnings("unchecked")
+	private Person findPersonByCriteria(Criteria criteria) {
+		List personList = criteria.list();
+		return personList.size() == 1 ? (Person) personList.get(0) : null;
+	}
+
+	@Required
+	public void setAuthenticationService(AuthenticationService authenticationService) {
+		this.authenticationService = authenticationService;
+	}
 }
