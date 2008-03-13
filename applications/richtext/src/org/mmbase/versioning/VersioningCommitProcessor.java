d12189cd79697ac7ede39685b0ac04c46deca68e
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-03-13 10:51:50 +0000

    only create new version if node actually changed
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@27475 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/richtext/src/org/mmbase/versioning/VersioningCommitProcessor.java
+++ applications/richtext/src/org/mmbase/versioning/VersioningCommitProcessor.java
@@ -9,7 +9,7 @@ import org.mmbase.util.logging.Logging;
  * This commitprocessor copies on every commit the complete node to a 'versioning' table.
  * @author Sander de Boer
  * @author Michiel Meeuwissen
- * @version $Id: VersioningCommitProcessor.java,v 1.3 2008-03-12 10:24:52 michiel Exp $
+ * @version $Id: VersioningCommitProcessor.java,v 1.4 2008-03-13 10:51:50 michiel Exp $
  * @since
  */
 
@@ -21,40 +21,41 @@ public class VersioningCommitProcessor implements CommitProcessor {
 
     public static final String VERSION_FIELD   = "version";
     public static final String OBJECT_FIELD    = "object";
-    public static final String TIMESTAMP_FIELD = "timestamp";
 
     public void commit(Node node, Field field) {
-        NodeManager wo = node.getNodeManager();
-        String versionBuilder = wo.getProperty("versionbuilder");
+        if (node.isChanged()) {
+            log.debug("Commiting " + node + "setting version in " + field);
+            NodeManager wo = node.getNodeManager();
+            String versionBuilder = wo.getProperty("versionbuilder");
 
-        if (versionBuilder == null || "".equals(versionBuilder)) {
-            versionBuilder = wo.getName() + "_versions";
-        }
-        NodeManager wv = node.getCloud().getNodeManager(versionBuilder);
-        log.debug("Found the version builder: '" + wv.getName() + "'");
+            if (versionBuilder == null || "".equals(versionBuilder)) {
+                versionBuilder = wo.getName() + "_versions";
+            }
+            NodeManager wv = node.getCloud().getNodeManager(versionBuilder);
+            log.debug("Found the version builder: '" + wv.getName() + "'");
 
-        //clone this version to the versions builder
-        Cloud cloud = node.getCloud();
-        Node version = wv.createNode();
+            //clone this version to the versions builder
+            Cloud cloud = node.getCloud();
 
-        //increase the version of the current node
-        int newVersionNo = node.getIntValue(field.getName()) + 1;
-        node.setIntValue(field.getName(), newVersionNo);
+            Node version = wv.createNode();
+            //increase the version of the current node
+            int newVersionNo = node.getIntValue(field.getName()) + 1;
+            node.setIntValue(field.getName(), newVersionNo);
 
-        cloneNode(node, version);
+            cloneNode(node, version);
 
-        if (node.getNodeManager().hasField("lastmodified")) {
-            version.setDateValue(TIMESTAMP_FIELD, node.getDateValue("lastmodified"));
-        }
-        version.setNodeValue(OBJECT_FIELD, node);
-        version.setIntValue(VERSION_FIELD, newVersionNo);
-        if (! node.isNew()) {
-            // shit..., node fields don't like new nodes.
-            version.commit();
-            // could solve it by in this case using the _old values_ of the node.
-            // But there are 2 bugs, which make this work around non-feasible:
+            version.setNodeValue(OBJECT_FIELD, node);
+            version.setIntValue(VERSION_FIELD, newVersionNo);
+            if (! node.isNew()) {
+                // shit..., node fields don't like new nodes.
+                version.commit();
+                // could solve it by in this case using the _old values_ of the node.
+                // But there are 2 bugs, which make this work around non-feasible:
             //  http://www.mmbase.org/jira/browse/MMB-1522.
             //  http://www.mmbase.org/jira/browse/MMB-1621 // This would also give a way to get  the 'old values'.
+            }
+        } else {
+            log.service("Node not changed");
         }
     }
 
