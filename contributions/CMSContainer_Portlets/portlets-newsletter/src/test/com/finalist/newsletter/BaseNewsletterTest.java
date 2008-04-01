d73ba3b674ff42eebf18e4f222f07bd065e3eaa3
mguo <mguo@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-04-01 09:41:25 +0000

    CMSC-725 builder and some test code.
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@27854 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ contributions/CMSContainer_Portlets/portlets-newsletter/src/test/com/finalist/newsletter/BaseNewsletterTest.java
@@ -0,0 +1,28 @@
+package com.finalist.newsletter;
+
+import junit.framework.TestCase;
+import org.mmbase.module.core.MMBaseContext;
+import org.mmbase.module.core.MMBase;
+import org.mmbase.module.tools.MMAdmin;
+import org.mmbase.module.tools.ApplicationInstaller;
+import org.mmbase.module.Module;
+import org.mmbase.bridge.Cloud;
+import net.sf.mmapps.modules.cloudprovider.CloudProvider;
+import net.sf.mmapps.modules.cloudprovider.CloudProviderFactory;
+
+public abstract class BaseNewsletterTest extends TestCase {
+
+    Cloud cloud;
+
+    protected void setUp() throws Exception {
+        MMBaseContext.init();
+        MMBase mmb = MMBase.getMMBase();
+
+        MMAdmin mmadmin = (MMAdmin) Module.getModule("mmadmin", true);
+        ApplicationInstaller installer = new ApplicationInstaller(mmb, mmadmin);
+        installer.installApplications();
+
+        CloudProvider provider = CloudProviderFactory.getCloudProvider();
+        cloud = provider.getCloud();
+    }
+}
