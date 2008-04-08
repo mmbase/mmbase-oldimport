83b40d684025c69cb1a51f4d3c5401aa1c6a68ef
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-04-08 08:27:56 +0000

    simplified, reindented
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@27941 91430c5f-d668-0410-9e7d-dd663366dcfd

--- CVSROOT/xmlvalidate.pl
+++ CVSROOT/xmlvalidate.pl
@@ -2,27 +2,30 @@
 $module_dir = shift @ARGV;
 
 foreach $file (@ARGV) {
-  if ($file =~ m/\.(xml|jspx|xsl|xslt|tagx|tld)$/) {
-    if (open FIL, "<$file") {
-      close FIL;
-      $files .= " $file";
-    } else {
-      print "Ignoring file '$file', cannot open it\n";
+    if ($file =~ m/\.(xml|jspx|xsl|xslt|tagx|tld)$/) {
+	if (open FIL, "<$file") {
+	    close FIL;
+	    $files .= " $file";
+	} else {
+	    print "Ignoring file '$file', cannot open it\n";
+	}
     }
-  }
 }
 
-print "Testing for XML DTD compliance:$files\n";
+print "Testing for XML compliance:$files\n";
 if ($files ne "") {
-  @output = `SGML_CATALOG_FILES=/usr/share/sgml/docbook/xmlcatalog:/usr/share/sgml/docbook/dtd/xml/4.1.2/catalog.xml /usr/bin/xmllint --noout $files 2>&1`;
-  for $line (@output) {
-    print STDERR $line;
-  }
-  if ($? != 0) {
-    exit(-1);
-  }
-  if ($#output > 0) {
-    exit(-1);
-  }
-  exit(0);
+    #@output = `SGML_CATALOG_FILES=/usr/share/sgml/docbook/xmlcatalog:/usr/share/sgml/docbook/dtd/xml/4.1.2/catalog.xml /usr/bin/xmllint --valid --noout $files 2>&1`;
+
+    @output = `/usr/bin/xmllint --noout $files 2>&1`;
+
+    for $line (@output) {
+	print STDERR $line;
+    }
+    if ($? != 0) {
+	exit(-1);
+    }
+    if ($#output > 0) {
+	exit(-1);
+    }
+    exit(0);
 }
