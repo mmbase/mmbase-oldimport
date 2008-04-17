7cbf2b419f323613bf9be56ca037fcb89c98b759
kevinshen <kevinshen@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-04-17 03:23:15 +0000

    CMSC-811
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@28155 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer_Portlets/portlets-newsletter/src/java/com/finalist/newsletter/forms/Schedule.java
+++ contributions/CMSContainer_Portlets/portlets-newsletter/src/java/com/finalist/newsletter/forms/Schedule.java
@@ -1,14 +1,11 @@
 package com.finalist.newsletter.forms;
 import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang.time.DateUtils;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
@@ -28,66 +25,67 @@ public class Schedule  extends MMBaseAction{
    public ActionForward execute(ActionMapping mapping, ActionForm form,
          HttpServletRequest request, HttpServletResponse response, Cloud cloud)
          throws Exception {
-      Map<String,Object> parameters = new HashMap<String,Object>();
+      Map<String,Object> requestParameters = new HashMap<String,Object>();
       AbstractSchedule schedule = null;
+      //type calendar type , 1 once ,2 daily ,3 weekly ,4 monthly
       String type = request.getParameter("type");
       String hour = request.getParameter("hour");
       String minute = request.getParameter("minute");
       if(type != null) {
-         parameters.put("hour", hour);
-         parameters.put("minute", minute);
+         requestParameters.put("hour", hour);
+         requestParameters.put("minute", minute);
          if(type.equals("1")) {
             String date = request.getParameter("date");
 
-            parameters.put("date", date);
+            requestParameters.put("date", date);
             schedule = new SingleSchedule();
          }
          else if(type.equals("2")) {
             String date = request.getParameter("date");
-            parameters.put("date", date);
+            requestParameters.put("date", date);
 
             String strategy = request.getParameter("strategy");
-            parameters.put("approach", strategy);
+            requestParameters.put("approach", strategy);
             
             if(strategy != null && strategy.equals("2")) {
                String interval = request.getParameter("interval");
-               parameters.put("interval", interval);
+               requestParameters.put("interval", interval);
             }
             schedule = new DailySchedule();
             
          }
          else if(type.equals("3")) {
             String interval = request.getParameter("interval");
-            parameters.put("interval", interval);
+            requestParameters.put("interval", interval);
             String[] weeks = request.getParameterValues("weeks");
-            parameters.put("weeks", getWeeks(weeks));
+            requestParameters.put("weeks", getWeeks(weeks));
             schedule = new WeeklySchedule();
          }
          else {
             String strategy = request.getParameter("strategy");
-            parameters.put("strategy", strategy);
+            requestParameters.put("strategy", strategy);
             
             if(strategy != null) {
                if(strategy.equals("0")) {
                   String day = request.getParameter("day");
-                  parameters.put("day", day); 
+                  requestParameters.put("day", day); 
                }
                else if (strategy.equals("1")) {
                   String whichweek = request.getParameter("whichweek");
-                  parameters.put("whichweek", whichweek); 
+                  requestParameters.put("whichweek", whichweek); 
                   
                   String week = request.getParameter("week");
-                  parameters.put("week", week);
+                  requestParameters.put("week", week);
                }
             }
             String[] months = request.getParameterValues("month");
-            parameters.put("month", getWeeks(months));
+            requestParameters.put("month", getWeeks(months));
             schedule = new MonthSchedule();
          }
         
          ScheduleService Service = new ScheduleService(schedule);
-         Service.setParameters(parameters);
-         String expression = Service.chansfer();
+         Service.setRequestParameters(requestParameters);
+         String expression = Service.transform();
          response.setContentType("text/xml");
          
          response.getWriter().print("<expression>"+expression+"</expression>");
@@ -100,11 +98,11 @@ public class Schedule  extends MMBaseAction{
     * @param args
     * @return
     */
-   private String getWeeks(String[] args) {
+   private String getWeeks(String[] weeks) {
       StringBuilder sb = new StringBuilder();
-      Arrays.sort(args);
-      for(int i = 0 ; i < args.length ; i++) {
-         sb.append(args[i]);
+      Arrays.sort(weeks);
+      for(int i = 0 ; i < weeks.length ; i++) {
+         sb.append(weeks[i]);
       }
       return sb.toString();
    }
