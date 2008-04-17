a57ad19ed5dbfe0ce1c44124b1af3d73a089a1ee
mguo <mguo@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-04-17 02:44:01 +0000

    CMSC-811 convert tools.
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@28151 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer_Portlets/portlets-newsletter/src/java/com/finalist/newsletter/util/POConvertUtils.java
+++ contributions/CMSContainer_Portlets/portlets-newsletter/src/java/com/finalist/newsletter/util/POConvertUtils.java
@@ -3,11 +3,17 @@ package com.finalist.newsletter.util;
 import com.finalist.cmsc.services.community.person.Person;
 import com.finalist.newsletter.domain.Subscription;
 import org.mmbase.bridge.Node;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.beanutils.BeanUtils;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
-public class POConvertUtils {
+public class POConvertUtils<T> {
+
    public static List<Subscription> convertSubscriptions(List<Node> nodes) {
       List<Subscription> subscriptions = new ArrayList<Subscription>();
       for (Node node : nodes) {
@@ -21,6 +27,34 @@ public class POConvertUtils {
       Person subscripber = CommunityModuleAdapter.getSubscriber(Integer.decode(node.getStringValue("subscriber")));
       subscription.setSubscriber(subscripber);
       return subscription;
+   }
+
+
+   public void convert(T target, Node node) throws IllegalAccessException, InvocationTargetException, InstantiationException {
+
+
+      Method[] targetFields = target.getClass().getDeclaredMethods();
+
+      for (Method method : targetFields) {
+         String name = method.getName();
+         if (null != getNodeValue(node, name)) {
+            BeanUtils.setProperty(target, getPropertieName(name), getNodeValue(node, name));
+         }
+      }
+   }
+
+   private Object getNodeValue(Node node, String name) {
+      String value = null;
+
+      if (name.startsWith("set") && node.getNodeManager().hasField(getPropertieName(name))) {
+         value = node.getStringValue(getPropertieName(name));
+      }
+
+      return value;
+   }
+
+   private String getPropertieName(String name) {
+      return name.substring(3, name.length()).toLowerCase();
    }
 
 }
