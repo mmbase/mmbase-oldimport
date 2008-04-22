12398720bec4dcdba5bc44d37170cdfc3eb23019
mguo <mguo@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-04-22 16:27:16 +0000

    function is moved to com.finalist.cmsc.newsletter.services.CommunityModuleAdapte
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@28272 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer_Portlets/portlets-newsletter/src/java/com/finalist/newsletter/services/CommunityModuleAdapter.java
+++ contributions/CMSContainer_Portlets/portlets-newsletter/src/java/com/finalist/newsletter/services/CommunityModuleAdapter.java
@@ -1,37 +1,46 @@
 package com.finalist.newsletter.services;
 //
-//import com.finalist.cmsc.services.community.person.Person;
-//import com.finalist.cmsc.services.community.person.PersonService;
-//import org.acegisecurity.context.SecurityContextHolder;
-//import org.acegisecurity.userdetails.UserDetails;
-//import org.springframework.web.context.WebApplicationContext;
-//import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.finalist.cmsc.services.community.person.Person;
+import com.finalist.cmsc.services.community.person.PersonService;
+import org.acegisecurity.context.SecurityContextHolder;
+import org.acegisecurity.context.SecurityContext;
+import org.acegisecurity.userdetails.UserDetails;
+import org.acegisecurity.Authentication;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.ServletContext;
 
 public class CommunityModuleAdapter {
    /**
-    * @param servletContext use RequestContextUtils.getWebApplicationContext(ServletRequest request) to get a WebApplicationContext
+    * @param servletContext get servletContext:
+    *In portlet : ((PortletContextImpl) this.getPortletContext()).getServletContext()
     * @return get current Person.
     */
-//   public static Person getCurrentUserId(ServletContext servletContext) {
-//
-//      WebApplicationContext ctx = WebApplicationContextUtils.getWebApplicationContext(servletContext);
-//
-//      Object obj = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//
-//      Person person = null;
-//
-//      if (obj instanceof UserDetails) {
-//         String username = ((UserDetails) obj).getUsername();
-//         PersonService personService = (PersonService) ctx.getBean("personService");
-//         person = personService.getPersonByUserId(username);
-//
-//         System.out.println("000000000000" + username);
-//      }
-//
-//      return person;
-//
-//   }
+   public static Person getCurrentUser(ServletContext servletContext) {
+
+      WebApplicationContext ctx = WebApplicationContextUtils.getWebApplicationContext(servletContext);
+
+      SecurityContext securityContext = SecurityContextHolder.getContext();
+      Authentication authentication = securityContext.getAuthentication();
+
+      Person person = null;
+
+      if (null == authentication) {
+         Object obj = authentication.getPrincipal();
+         if (obj instanceof UserDetails) {
+            String username = ((UserDetails) obj).getUsername();
+            PersonService personService = (PersonService) ctx.getBean("personService");
+            person = personService.getPersonByUserId(username);
+
+            System.out.println("000000000000" + username);
+         }
+      }
+
+
+      return person;
+
+   }
 
 }
