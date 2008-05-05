2d15f633076a2ec699b50e12d7feb9413629c479
nklasens <nklasens@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-05-05 15:33:08 +0000

    CMSC-864 utility to fix creationrels which are created in cmsc 1.1 wizard save
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@28487 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer/cmsc/maintenance/src/java/com/finalist/cmsc/maintenance/sql/SingleRelationDuplication.java
+++ contributions/CMSContainer/cmsc/maintenance/src/java/com/finalist/cmsc/maintenance/sql/SingleRelationDuplication.java
@@ -17,19 +17,23 @@ import org.mmbase.bridge.*;
 import com.finalist.cmsc.sql.SqlAction;
 
 
-public class CreationChannelDuplication extends SqlAction {
+public class SingleRelationDuplication extends SqlAction {
 
+    private String role;
+    private String searchDir;
     private String action;
     
-    public CreationChannelDuplication(String action) {
+    public SingleRelationDuplication(String role, String searchDir, String action) {
+        this.role = role;
+        this.searchDir = searchDir;
         this.action = action;
-     }
+    }
     
     @Override
     public String getSql() {
         return "select " + getFieldname("snumber") + ", " + getFieldname("dnumber") + ", " + getFieldname("rnumber")
                 + ", count(" + getFieldname("number") + ")" + 
-        		" from "+ getTable("creationrel") + 
+        		" from "+ getTable(role) + 
         		" group by " + getFieldname("snumber") + ", " + getFieldname("dnumber") + ", " + getFieldname("rnumber") + 
         		" having count(*) > 1;";
     }
@@ -47,7 +51,7 @@ public class CreationChannelDuplication extends SqlAction {
            if ("remove".equals(action)) {
               Node content = getCloud().getNode(nodeNumber);
               
-              RelationList list = content.getRelations("creationrel", null, "DESTINATION");
+              RelationList list = content.getRelations(role, null, searchDir);
               if (list.size() >= 2) {
                   for (int i = 1; i < list.size(); i++) {
                      Relation creationrel = list.getRelation(i);
