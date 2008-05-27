c7b696243e829c76b23950d3c56b72165b78077f
kevinshen <kevinshen@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-05-27 07:29:02 +0000

    CMSC-903,Newsletter statistics not works at all.add a new cron job class
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@28649 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ contributions/CMSContainer_Portlets/portlets-newsletter/src/java/com/finalist/newsletter/NewsletterSumLogsCronJob.java
@@ -0,0 +1,42 @@
+package com.finalist.newsletter;
+
+
+
+import java.util.List;
+import org.mmbase.applications.crontab.CronEntry;
+import org.mmbase.applications.crontab.CronJob;
+import org.mmbase.util.logging.Logger;
+import org.mmbase.util.logging.Logging;
+import com.finalist.newsletter.domain.StatisticResult;
+import com.finalist.newsletter.services.StatisticService;
+public class NewsletterSumLogsCronJob implements CronJob {
+
+	private static Logger log = Logging
+			.getLoggerInstance(NewsletterSumLogsCronJob.class.getName());
+
+	public void init(CronEntry cronEntry) {
+		log.info("Initializing NewsletterLogRecorder");
+	}
+
+	public void run() {
+		StatisticService service = (StatisticService) ApplicationContextFactory.getBean("statisticService");
+		List<StatisticResult> listRecorder = service.getLogs();
+		if (null != listRecorder) {
+			insetStatRecord(service, listRecorder);
+			listRecorder.clear();
+		}
+	}
+
+	private int insetStatRecord(StatisticService service,List<StatisticResult> listRecorder) {
+		if(null!=listRecorder){
+			return service.pushSumedLogs(listRecorder);			
+		}
+		else{
+		    return 0;	
+		}
+	}
+	public void stop() {
+		log.info("Stopping NewsletterLogRecorder");
+
+	}
+}
