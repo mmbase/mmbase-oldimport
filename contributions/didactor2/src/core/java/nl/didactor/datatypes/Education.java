9feaf7adb7d93c942164873525eb31fb1ae5f366
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-06-06 14:40:33 +0000

    create a data type that can be used for fields to point to the relevent education object, which can make things easier sometimes
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@28762 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ contributions/didactor2/src/core/java/nl/didactor/datatypes/Education.java
@@ -0,0 +1,48 @@
+package nl.didactor.datatypes;
+import org.mmbase.util.logging.Logger;
+import org.mmbase.util.logging.Logging;
+import org.mmbase.datatypes.*;
+import org.mmbase.util.*;
+import org.mmbase.bridge.*;
+import org.mmbase.core.event.*;
+import java.util.*;
+import java.util.regex.*;
+
+
+/**
+ * If you make an 'education' typed field, it will be autoamticly filled with the current user's education.
+ *
+ * @author Michiel Meeuwissen
+ * @version $Id: Education.java,v 1.1 2008-06-06 14:40:33 michiel Exp $
+ */
+public class Education extends NodeDataType  {
+    private static final Logger log = Logging.getLoggerInstance(Education.class);
+
+
+    public Education(String name) {
+        super(name);
+    }
+
+    @Override
+    public Object getDefaultValue(Locale locale, Cloud cloud, Field field) {
+        log.info("Getting default value for " + field, new Exception());
+        if (cloud == null) {
+            log.service("no cloud, returning null");
+            if (field == null) return null;
+            try {
+                cloud = field.getNodeManager().getCloud();
+            } catch (Exception e) {
+                log.warn(e.getMessage(), e);
+                return null;
+            }
+        }
+        Node user =  nl.didactor.security.Authentication.getCurrentUserNode(cloud);
+        Collection<Node> relatedEducations = (Collection<Node>) user.getFunctionValue("educations", null).get();
+        if (relatedEducations != null && relatedEducations.size() > 0) {
+            return relatedEducations.iterator().next();
+        } else {
+            return null;
+        }
+    }
+
+}
