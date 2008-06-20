73e1f53fe94849011fb8b6c68451c140ccff01bb
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-06-20 13:03:13 +0000

    code conventions only, still, this is a horrible class
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@28964 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/didactor2/src/core/java/nl/didactor/component/users/PeopleComparator.java
+++ contributions/didactor2/src/core/java/nl/didactor/component/users/PeopleComparator.java
@@ -2,24 +2,22 @@ package nl.didactor.component.users;
 
 import java.util.Comparator;
 
-public class PeopleComparator implements Comparator
-{
-   public PeopleComparator()
-   {
-   }
 
-   public int compare(Object ob1, Object ob2)
-   {
+/**
+ * lastname,firstname comparator for people
+ * @author azemskov
+ * @javadoc
+ */
+public class PeopleComparator implements Comparator {
+
+   public int compare(Object ob1, Object ob2)    {
       String[] arrstrObj1 = (String[]) ob1;
       String[] arrstrObj2 = (String[]) ob2;
 
       int iResult = arrstrObj1[1].compareTo(arrstrObj2[1]);
-      if (iResult != 0)
-      {
+      if (iResult != 0) {
          return iResult;
-      }
-      else
-      {
+      } else {
          return arrstrObj1[2].compareTo(arrstrObj2[2]);
       }
    }
