e5cb89dbd59f1fa674316ebfd3c7ff3e89a5daf4
nklasens <nklasens@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-06-21 12:43:39 +0000

    Cleaned it up a little
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@28979 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer_Portlets/portlets-guestbook/src/java/com/finalist/captcha/CaptchaServlet.java
+++ contributions/CMSContainer_Portlets/portlets-guestbook/src/java/com/finalist/captcha/CaptchaServlet.java
@@ -4,33 +4,28 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
 import com.octo.captcha.service.CaptchaServiceException;
 import com.sun.image.codec.jpeg.JPEGCodec;
 import com.sun.image.codec.jpeg.JPEGImageEncoder;
 
 public class CaptchaServlet extends HttpServlet {
-   /**
-    * 
-    */
+
    private static final long serialVersionUID = -8617461903432594139L;
 
 
+   @Override
    public void init(ServletConfig servletConfig) throws ServletException {
       super.init(servletConfig);
    }
 
 
+   @Override
    protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
-         throws ServletException, IOException {
+         throws IOException {
 
-      byte[] captchaChallengeAsJpeg = null;
       // the output stream to render the captcha image as jpeg into
       ByteArrayOutputStream jpegOutputStream = new ByteArrayOutputStream();
       try {
@@ -55,7 +50,7 @@ public class CaptchaServlet extends HttpServlet {
          return;
       }
 
-      captchaChallengeAsJpeg = jpegOutputStream.toByteArray();
+      byte[] captchaChallengeAsJpeg = jpegOutputStream.toByteArray();
 
       // flush it in the response
       httpServletResponse.setHeader("Cache-Control", "no-store");
