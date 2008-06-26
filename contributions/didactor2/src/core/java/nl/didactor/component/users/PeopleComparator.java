4380606c1c4ec6a265075470e32b9e4f14fbdf67
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-06-26 15:30:11 +0000

    rationalized a bit, though I still doubt whet the this was good for
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@29045 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/didactor2/src/core/java/nl/didactor/component/users/PeopleComparator.java
+++ contributions/didactor2/src/core/java/nl/didactor/component/users/PeopleComparator.java
@@ -1,6 +1,7 @@
 package nl.didactor.component.users;
 
 import java.util.Comparator;
+import org.mmbase.bridge.Node;
 
 
 /**
@@ -8,17 +9,15 @@ import java.util.Comparator;
  * @author azemskov
  * @javadoc
  */
-public class PeopleComparator implements Comparator {
+public class PeopleComparator implements Comparator<Node> {
 
-   public int compare(Object ob1, Object ob2)    {
-      String[] arrstrObj1 = (String[]) ob1;
-      String[] arrstrObj2 = (String[]) ob2;
+    public int compare(Node ob1, Node ob2)    {
 
-      int iResult = arrstrObj1[1].compareTo(arrstrObj2[1]);
-      if (iResult != 0) {
-         return iResult;
-      } else {
-         return arrstrObj1[2].compareTo(arrstrObj2[2]);
-      }
-   }
+        int iResult = ob1.getStringValue("firstname").compareTo(ob2.getStringValue("firstname"));
+        if (iResult != 0) {
+            return iResult;
+        } else {
+            return ob1.getStringValue("lastname").compareTo(ob2.getStringValue("lastname"));
+        }
+    }
 }
