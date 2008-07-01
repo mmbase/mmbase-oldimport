d22e0132677dffbf3e5338866dca21cf73479f70
avleeuwen <avleeuwen@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-07-01 08:21:31 +0000

    CMSC-993 - Forgot to format some of the files to the correct formatting
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@29127 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer/cmsc/taglib/src/java/com/finalist/cmsc/taglib/flash/ParamTag.java
+++ contributions/CMSContainer/cmsc/taglib/src/java/com/finalist/cmsc/taglib/flash/ParamTag.java
@@ -18,54 +18,54 @@ import javax.servlet.jsp.JspException;
  */
 public class ParamTag extends NameValueTag {
 
-	/**
-	 * Based on: <a
-	 * href="http://www.adobe.com/cfusion/knowledgebase/index.cfm?id=tn_12701"
-	 * >http://www.adobe.com/cfusion/knowledgebase/index.cfm?id=tn_12701</a>
-	 *
-	 * @see NameValueTag#initAllowedValues()
-	 *
-	 * @return the map with allowed values for all parameters, or
-	 *         <code>null</code> if no parameter checking is needed.
-	 */
-	@Override
-	protected Map<String, List<String>> initAllowedValues() {
-		Map<String, List<String>> map = new HashMap<String, List<String>>();
-		List<String> booleanList = Arrays.asList("true", "false");
+   /**
+    * Based on: <a
+    * href="http://www.adobe.com/cfusion/knowledgebase/index.cfm?id=tn_12701"
+    * >http://www.adobe.com/cfusion/knowledgebase/index.cfm?id=tn_12701</a>
+    *
+    * @see NameValueTag#initAllowedValues()
+    *
+    * @return the map with allowed values for all parameters, or
+    *         <code>null</code> if no parameter checking is needed.
+    */
+   @Override
+   protected Map<String, List<String>> initAllowedValues() {
+      Map<String, List<String>> map = new HashMap<String, List<String>>();
+      List<String> booleanList = Arrays.asList("true", "false");
 
-		map.put("play", booleanList);
-		map.put("loop", booleanList);
-		map.put("menu", booleanList);
-		map.put("quality", Arrays.asList("low", "autolow", "autohigh", "medium", "high", "best"));
-		map.put("scale", Arrays.asList("default", "noorder", "exactfit"));
-		map.put("salign", Arrays.asList("t", "r", "b", "l", "tl", "tb", "bl", "br"));
-		map.put("wmode", Arrays.asList("window", "opaque", "transparent"));
-		map.put("bgcolor", null);
-		map.put("base", null);
-		map.put("swliveconnect", null);
-		map.put("devicefont", booleanList);
-		map.put("allowscriptaccess", Arrays.asList("always", "never"));
-		map.put("seamlesstabbing", booleanList);
-		map.put("allowfullscreen", booleanList);
-		map.put("allownetworking", Arrays.asList("all", "internal", "none"));
+      map.put("play", booleanList);
+      map.put("loop", booleanList);
+      map.put("menu", booleanList);
+      map.put("quality", Arrays.asList("low", "autolow", "autohigh", "medium", "high", "best"));
+      map.put("scale", Arrays.asList("default", "noorder", "exactfit"));
+      map.put("salign", Arrays.asList("t", "r", "b", "l", "tl", "tb", "bl", "br"));
+      map.put("wmode", Arrays.asList("window", "opaque", "transparent"));
+      map.put("bgcolor", null);
+      map.put("base", null);
+      map.put("swliveconnect", null);
+      map.put("devicefont", booleanList);
+      map.put("allowscriptaccess", Arrays.asList("always", "never"));
+      map.put("seamlesstabbing", booleanList);
+      map.put("allowfullscreen", booleanList);
+      map.put("allownetworking", Arrays.asList("all", "internal", "none"));
 
-		return map;
-	}
+      return map;
+   }
 
-	/** {@inheritDoc} */
-	@Override
-	public void doTag() throws JspException, IOException {
-		super.doTag();
+   /** {@inheritDoc} */
+   @Override
+   public void doTag() throws JspException, IOException {
+      super.doTag();
 
-		FlashTag flashTag = (FlashTag) findAncestorWithClass(this, FlashTag.class);
-		if (flashTag == null) {
-			throw new JspException("A flashparam tag should be nested inside a flash tag!");
-		}
+      FlashTag flashTag = (FlashTag) findAncestorWithClass(this, FlashTag.class);
+      if (flashTag == null) {
+         throw new JspException("A flashparam tag should be nested inside a flash tag!");
+      }
 
-		if (!isAllowedValue(getName(), getValue())) {
-			throw new JspException(String.format("Invalid value '%s' for '%s'!", getValue(), getName()));
-		}
+      if (!isAllowedValue(getName(), getValue())) {
+         throw new JspException(String.format("Invalid value '%s' for '%s'!", getValue(), getName()));
+      }
 
-		flashTag.addParam(getName(), getValue());
-	}
+      flashTag.addParam(getName(), getValue());
+   }
 }
