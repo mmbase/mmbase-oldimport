d36dafb2d3b5c96aadfd97783b387668c6b3badc
nikkoyin <nikkoyin@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-07-15 12:10:20 +0000

    CMSC-947, Improve the newletter import/export into a generic community import/export,add uploadAction
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@29418 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ contributions/CMSContainer_Modules/community/src/java/com/finalist/cmsc/community/forms/ReferenceImportExportAction.java
@@ -0,0 +1,134 @@
+package com.finalist.cmsc.community.forms;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
+import org.apache.struts.action.ActionMessages;
+import org.apache.struts.actions.DispatchAction;
+import org.apache.struts.upload.FormFile;
+import com.finalist.cmsc.services.community.domain.CommunityExport;
+import com.finalist.cmsc.services.community.domain.PersonExportImportVO;
+import com.finalist.cmsc.services.community.person.Person;
+import com.finalist.cmsc.services.community.person.PersonService;
+import com.finalist.cmsc.services.community.preferences.Preference;
+import com.finalist.cmsc.services.community.security.Authentication;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+
+public class ReferenceImportExportAction extends DispatchAction {
+	private static Log log = LogFactory.getLog(ReferenceImportExportAction.class);
+	private PersonService personService;
+
+	
+	public ActionForward export(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
+		log.debug("Export Susbscriptions");
+		CommunityExport communityExport = new CommunityExport(personService.getPersonExportImportVO());
+		String xml = getXStream().toXML(communityExport);
+		byte[] bytes = xml.getBytes();
+		response.setContentType("text/xml");
+		response.setContentLength(bytes.length);
+		response.setHeader("Content-Disposition", "attachment; filename=subscriptions.xml");
+		response.setHeader("Cache-Control", "no-store");
+		response.flushBuffer();
+		OutputStream outStream = response.getOutputStream();
+		outStream.write(bytes);
+		outStream.flush();
+		return mapping.findForward(null);
+	}
+
+	public ActionForward importsubscription(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
+		ReferenceImportUploadForm myForm = (ReferenceImportUploadForm) form;
+		FormFile myFile = myForm.getDatafile();
+		byte[] fileData = myFile.getFileData();
+		String contentType = myFile.getContentType();
+		boolean isXML = "text/xml".equals(contentType);
+		String level = myForm.getLevel();
+		ActionMessages messages = new ActionMessages();
+		if (!isXML) {
+			messages.add("file", new ActionMessage("datafile.unsupport"));
+			saveMessages(request, messages);
+			return mapping.findForward("failed");
+		}
+		try {
+			importFromFile(fileData, level);
+		} catch (Exception e) {
+			log.error(e);
+			messages.add("file", new ActionMessage("datafile.invalid"));
+			saveMessages(request, messages);
+			return mapping.findForward("failed");
+		}
+
+		return mapping.findForward("success");
+	}
+
+	private void importFromFile(byte[] fileData, String level) throws Exception {
+		String xml = new String(fileData);
+		CommunityExport communityExport;
+		communityExport = (CommunityExport) getXStream().fromXML(xml);
+		List<PersonExportImportVO> xpersons = communityExport.getUsers();
+		if ("clean".equals(level)) {
+			batchCleanRecord();
+		}
+		for (PersonExportImportVO importPerson : xpersons) {
+			Authentication authentication=importPerson.getAuthentication();
+			if(null == authentication||null==authentication.getUserId()){
+				continue;
+			}
+			Person p = personService.getPersonByUserId(importPerson.getAuthentication().getUserId());
+			if(null!=p&&"over".equals(level)){
+				deleteRecord(p);
+				addNewRecord(importPerson);
+		    }
+			if(null==p){
+                // only add new users
+				addNewRecord(importPerson);
+		    }
+		}
+	}
+
+	private void batchCleanRecord() throws Exception {
+		personService.batchClean();
+	}
+
+	private void deleteRecord(Person dataPerson) {
+		Long id = dataPerson.getAuthenticationId();
+		personService.deleteRelationRecord(id);
+	}
+
+	private void addNewRecord(PersonExportImportVO xperson) {
+		personService.creatRelationRecord(xperson);
+	}
+	private XStream getXStream() {
+		XStream xstream = new XStream(new DomDriver());
+		xstream.alias("community-export", CommunityExport.class);
+		xstream.alias("user", PersonExportImportVO.class);
+		xstream.alias("authentication", Authentication.class);
+		xstream.alias("preference", Preference.class);
+		return xstream;
+	}
+
+	public static void setLog(Log log) {
+		ReferenceImportExportAction.log = log;
+	}
+
+	public void setPersonService(PersonService personService) {
+		this.personService = personService;
+		     
+	}
+
+	public static Log getLog() {
+		return log;
+	}
+
+	
+}
---
+++ contributions/CMSContainer_Modules/community/src/java/com/finalist/cmsc/community/forms/ReferenceImportUploadForm.java
@@ -0,0 +1,26 @@
+package com.finalist.cmsc.community.forms;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.upload.FormFile;
+
+public class ReferenceImportUploadForm extends ActionForm {
+   
+private FormFile datafile;
+private String level;
+
+   public String getLevel() {
+	return level;
+}
+
+public void setLevel(String level) {
+	this.level = level;
+}
+
+public FormFile getDatafile() {
+      return datafile;
+   }
+
+   public void setDatafile(FormFile datafile) {
+      this.datafile = datafile;
+   }
+}
