9189941bb8fe3536913c9b1649f2ff98344b070f
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-07-17 13:05:34 +0000

    added some test cases for addToResult method
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@29477 91430c5f-d668-0410-9e7d-dd663366dcfd

--- tests/bridge/org/mmbase/bridge/util/QueriesTest.java
+++ tests/bridge/org/mmbase/bridge/util/QueriesTest.java
@@ -37,5 +37,30 @@ public class QueriesTest extends BridgeTest {
 
     }
 
+    public void testAddToResultCreateOneRelation() {
+        Node mag =  getCloud().getNodeManager("mags").createNode();
+        mag.commit();
+        Node news =  getCloud().getNodeManager("news").createNode();
+        news.commit();
+        NodeQuery q = Queries.createRelatedNodesQuery(mag, getCloud().getNodeManager("news"), "posrel", "destination");
+        NodeList result = Queries.addToResult(q, news);
+        assertEquals(1, result.size());
+    }
+
+    public void testAddToResultCreateTwoRelations() {
+        Node mag =  getCloud().getNodeManager("mags").createNode();
+        mag.commit();
+        Node news =  getCloud().getNodeManager("news").createNode();
+        news.commit();
+        Node url =  getCloud().getNodeManager("urls").createNode();
+        url.commit();
+        NodeQuery q = Queries.createRelatedNodesQuery(mag, getCloud().getNodeManager("news"), "posrel", "destination");
+        Queries.addPath(q, "posrel,urls", "destination");
+        Queries.addStartNodes(q, "" + url.getNumber());
+        NodeList result = Queries.addToResult(q, news);
+        assertEquals(2, result.size());
+    }
+
+
 
 }
