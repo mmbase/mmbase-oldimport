21a47f04180bb91ac82b7ad10c185c9bfbbf4593
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-07-18 13:43:00 +0000

    *** empty log message ***
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@29549 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/didactor2/src/education/java/nl/didactor/education/Functions.java
+++ contributions/didactor2/src/education/java/nl/didactor/education/Functions.java
@@ -7,8 +7,9 @@ import org.mmbase.util.logging.*;
 import java.util.*;
 
 /**
- * Several functions on mmbase nodes which are used by didactor. * @author Michiel Meeuwissen
- * @version $Id: Functions.java,v 1.4 2008-02-05 15:05:55 michiel Exp $
+ * Several functions on mmbase nodes which are used by didactor.
+ * @author Michiel Meeuwissen
+ * @version $Id: Functions.java,v 1.5 2008-07-18 13:43:00 michiel Exp $
  */
 public class Functions {
     protected final static Logger log = Logging.getLoggerInstance(Functions.class);
@@ -20,7 +21,26 @@ public class Functions {
     }
 
 
+    /**
+     * returns a list of nodenumbers which follows the path in the cloud up, via learnobjects and posrel. This
+     * would mean that the last node in this path is the learobject directly related to the education node.
+     */
     public List<Integer> path() {
+        List<Integer> result = new ArrayList<Integer>();
+        result.add(node.getNumber());
+        NodeList parents = node.getRelatedNodes("learnobjects", "posrel", "source");
+        while (parents.size() > 0) {
+            assert parents.size() == 1 : "Node " + node.getNumber() + " has more than 1 posrel parents";
+            Node parent = parents.getNode(0);
+            if (result.contains(parent.getNumber())) break;
+            result.add(parent.getNumber());
+            parents = parent.getRelatedNodes("learnobjects", "posrel", "source");
+        }
+        return result;
+    }
+
+    /*
+    public String url() {
         List<Integer> result = new ArrayList<Integer>();
         result.add(node.getNumber());
         NodeList parents = node.getRelatedNodes("learnobjects", "posrel", "source");
@@ -34,6 +54,7 @@ public class Functions {
         return result;
     }
 
+    */
 
     /**
      * Used on nodes of type 'tests'
