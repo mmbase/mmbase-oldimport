5e27b218a208b0604e65bc9ded2330aae55709f6
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-07-26 19:38:32 +0000

    *** empty log message ***
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@29700 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/datatypes/processors/PosrelCommitProcessor.java
+++ src/org/mmbase/datatypes/processors/PosrelCommitProcessor.java
@@ -19,7 +19,7 @@ import org.mmbase.util.logging.*;
  * you left the value empty on commit.
  *
  * @author Michiel Meeuwissen
- * @version $Id: PosrelCommitProcessor.java,v 1.2 2008-02-29 14:47:07 michiel Exp $
+ * @version $Id: PosrelCommitProcessor.java,v 1.3 2008-07-26 19:38:32 michiel Exp $
  * @since MMBase-1.8.6
  */
 
@@ -36,16 +36,18 @@ public class PosrelCommitProcessor implements CommitProcessor {
         }
         if (node.getValue(field.getName()) == null || "".equals(node.getStringValue(field.getName()))) {
             Node source      = node.getNodeValue("snumber");
-            NodeQuery q = Queries.createNodeQuery(source);
-            String role = node.getNodeValue("rnumber").getStringValue("sname");
-            Step relationStep = q.addRelationStep(node.getCloud().getNodeManager("object"),
-                                                  role,
-                                                  "destination");
-            q.setNodeStep(relationStep);
-            Integer max = (Integer) Queries.max(q, q.getStepField(q.getNodeManager().getField(field.getName())));
-            log.debug("max now " + max);
-            if (max == null) max = 0;
-            node.setValueWithoutProcess(field.getName(), max + 1);
+            if (source != null) {
+                NodeQuery q = Queries.createNodeQuery(source);
+                String role = node.getNodeValue("rnumber").getStringValue("sname");
+                Step relationStep = q.addRelationStep(node.getCloud().getNodeManager("object"),
+                                                      role,
+                                                      "destination");
+                q.setNodeStep(relationStep);
+                Integer max = (Integer) Queries.max(q, q.getStepField(q.getNodeManager().getField(field.getName())));
+                log.debug("max now " + max);
+                if (max == null) max = 0;
+                node.setValueWithoutProcess(field.getName(), max + 1);
+            }
         }
     }
 
