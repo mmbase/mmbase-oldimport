75c5eb95928c90f7633324ed379abe9e92679cd0
lisachen <lisachen@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-07-28 14:49:13 +0000

    CMSC-990 merge add subscribers
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@29759 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer_Modules/community/src/java/com/finalist/cmsc/community/forms/SearchConditionalUserAction.java
+++ contributions/CMSContainer_Modules/community/src/java/com/finalist/cmsc/community/forms/SearchConditionalUserAction.java
@@ -25,76 +25,79 @@ import com.finalist.cmsc.paging.PagingUtils;
 import com.finalist.cmsc.services.community.person.Person;
 import com.finalist.cmsc.services.community.security.Authority;
 
-public class SearchConditionalUserAction extends AbstractCommunityAction{
-   
-   public ActionForward execute(ActionMapping actionMapping, ActionForm actionForm, HttpServletRequest request, HttpServletResponse response)
-    throws Exception{
-      
-      String groupName = request.getParameter("groupName");
-      SearchForm searchform = (SearchForm) actionForm;
-      HashMap map = new HashMap();
-      if(!StringUtil.isEmptyOrWhitespace(searchform.getFullName())){
-         map.put("fullname", searchform.getFullName());
-      }
-      if(!StringUtil.isEmptyOrWhitespace(searchform.getUserName())){
-         map.put("username", searchform.getUserName());
-      }
-      if(!StringUtil.isEmptyOrWhitespace(searchform.getemailAddr())){
-         map.put("email", searchform.getemailAddr());
-      }
-      if(!StringUtil.isEmptyOrWhitespace(searchform.getGroups())){
-         map.put("group", searchform.getGroups());
-      }
-      if(!StringUtil.isEmptyOrWhitespace(groupName)){
-         map.put("group", groupName);
-      }
-      if(!StringUtil.isEmptyOrWhitespace(groupName)){
-         map.put("group", groupName);
-         map.put("strict", "strict");
-      }
-      PagingStatusHolder holder = PagingUtils.getStatusHolder(request);
-      List<Person> persons;
-      int totalCount = 0;
-      if(map.size()>0){
-         persons = getPersonService().getAssociatedPersons(map,holder);
-         totalCount = getPersonService().getAssociatedPersonsNum(map,holder);
-      }else{
-         persons = getPersonService().getAllPeople(holder);
-         totalCount = getPersonService().countAllPersons();
-      }
-      request.setAttribute("personForShow", convertToVO(persons));
-      request.setAttribute("totalCount", totalCount);
-      if(!StringUtil.isEmptyOrWhitespace(groupName)){
-    	   request.setAttribute("groupName", groupName);
-           return actionMapping.findForward("group");
-        }
-      removeFromSession(request,searchform);
-      return actionMapping.findForward("success");
-   }
+public class SearchConditionalUserAction extends AbstractCommunityAction {
 
-   private List<PersonForShow> convertToVO(List<Person> persons) {
-      List<PersonForShow> perShow;
-      perShow = new ArrayList<PersonForShow>();
-      for(Person p:persons){
-         PersonForShow per = new PersonForShow();
-         per.setFullname(""+p.getFirstName()+" "+p.getLastName());
-         per.setEmail(p.getEmail());
-         per.setUsername(getAuthenticationService().getAuthenticationById(p.getAuthenticationId()).getUserId());
-         String groupsName = "";
+	public ActionForward execute(ActionMapping actionMapping, ActionForm actionForm, HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+
+		String groupName = request.getParameter("groupName");
+		SearchForm searchform = (SearchForm) actionForm;
+		HashMap map = new HashMap();
+		if (!StringUtil.isEmptyOrWhitespace(searchform.getFullName())) {
+			map.put("fullname", searchform.getFullName());
+		}
+		if (!StringUtil.isEmptyOrWhitespace(searchform.getUserName())) {
+			map.put("username", searchform.getUserName());
+		}
+		if (!StringUtil.isEmptyOrWhitespace(searchform.getemailAddr())) {
+			map.put("email", searchform.getemailAddr());
+		}
+		if (!StringUtil.isEmptyOrWhitespace(searchform.getGroups())) {
+			map.put("group", searchform.getGroups());
+		}
+		if (!StringUtil.isEmptyOrWhitespace(groupName)) {
+			map.put("group", groupName);
+		}
+		if (!StringUtil.isEmptyOrWhitespace(groupName)) {
+			map.put("group", groupName);
+			map.put("strict", "strict");
+		}
+		PagingStatusHolder holder = PagingUtils.getStatusHolder(request);
+		List<Person> persons;
+		int totalCount = 0;
+		if (map.size() > 0) {
+			persons = getPersonService().getAssociatedPersons(map, holder);
+			totalCount = getPersonService().getAssociatedPersonsNum(map, holder);
+		} else {
+			persons = getPersonService().getAllPeople(holder);
+			totalCount = getPersonService().countAllPersons();
+		}
+		request.setAttribute("personForShow", convertToVO(persons));
+		request.setAttribute("totalCount", totalCount);
+		request.setAttribute("newsletterId", request.getParameter("newsletterId"));
+		request.setAttribute("method", request.getParameter("method"));
+		if (!StringUtil.isEmptyOrWhitespace(groupName)) {
+			request.setAttribute("groupName", groupName);
+			return actionMapping.findForward("group");
+		}
+		removeFromSession(request, searchform);
+		ActionForward ret = actionMapping.findForward("success");
+		return ret;
+	}
+
+	private List<PersonForShow> convertToVO(List<Person> persons) {
+		List<PersonForShow> perShow;
+		perShow = new ArrayList<PersonForShow>();
+		for (Person p : persons) {
+			PersonForShow per = new PersonForShow();
+			per.setFullname("" + p.getFirstName() + " " + p.getLastName());
+			per.setEmail(p.getEmail());
+			per.setUsername(getAuthenticationService().getAuthenticationById(p.getAuthenticationId()).getUserId());
+			String groupsName = "";
          Set<String> authorityNames = getAuthorityService().getAuthorityNamesForUser(getAuthenticationService().getAuthenticationById(p.getAuthenticationId()).getUserId());
-         if(authorityNames.size()>=1) {
-            Iterator iter = authorityNames.iterator();
-            while(iter.hasNext()){
-               groupsName += iter.next()+", ";
-            }
-            groupsName = groupsName.substring(0, groupsName.length()-2);
-            per.setGroups(groupsName);
-         }else{
-            per.setGroups("");
-         }
-         per.setAuthId(p.getAuthenticationId());
-         perShow.add(per);
-      }
-      return perShow;
-   }
+			if (authorityNames.size() >= 1) {
+				Iterator iter = authorityNames.iterator();
+				while (iter.hasNext()) {
+					groupsName += iter.next() + ", ";
+				}
+				groupsName = groupsName.substring(0, groupsName.length() - 2);
+				per.setGroups(groupsName);
+			} else {
+				per.setGroups("");
+			}
+			per.setAuthId(p.getAuthenticationId());
+			perShow.add(per);
+		}
+		return perShow;
+	}
 }
--- contributions/CMSContainer_Modules/community/src/webapp/editors/community/listUsers.jsp
+++ contributions/CMSContainer_Modules/community/src/webapp/editors/community/listUsers.jsp
@@ -1,12 +1,9 @@
 <%@include file="globals.jsp" 
-%>
-<%@ taglib uri="http://finalist.com/cmsc" prefix="cmsc" 
-%>
-<%@ taglib prefix="edit" tagdir="/WEB-INF/tags/edit"
-%>
-<%@taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"
-%>
+%><%@ taglib uri="http://finalist.com/cmsc" prefix="cmsc" 
+%><%@ taglib prefix="edit" tagdir="/WEB-INF/tags/edit"
+%><%@taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
 <fmt:setBundle basename="cmsc-community" scope="request" />
+<mm:content type="text/html" encoding="UTF-8" expires="0">
 <cmscedit:head title="reactions.title">
 	<script type="text/javascript">
 	   function addToGroup(){
@@ -25,17 +22,26 @@
 	   }
 	</script>
 </cmscedit:head>
+<mm:cloud jspvar="cloud" rank="basic user" loginpage="../../login.jsp">
+<mm:import externid="newsletterId"/>
+<mm:import externid="method"/>
+<c:url var="addGroup" value="/editors/community/AddUserToGroupInit.do"/>
+<c:url var="subscribeUrl" value="/editors/newsletter/NewsletterSubscriptionAddRelAction.do?method=${method}"/>
 
 <edit:ui-tabs>
    <edit:ui-tab key="community.search.users" active="true"/>
+   <mm:notpresent referid="newsletterId" >
    <edit:ui-tab key="community.search.groups">
       ${pageContext.request.contextPath }/editors/community/searchConditionalGroupAction.do
    </edit:ui-tab>
+   </mm:notpresent>
 </edit:ui-tabs>
 
 <div class="editor">
    <div style="padding-left:10px;">
+      <mm:notpresent referid="newsletterId" >
       <p><a href="userAddInitAction.do" style=" padding-left:20px; background: url(<cmsc:staticurl page='/editors/gfx/icons/new.png'/>) left center no-repeat"><fmt:message key="view.new.user"/></a><p>
+      </mm:notpresent>
       <html:form action="/editors/community/SearchConditionalUser.do" method="post">
        <table border="0">
          <tbody >
@@ -68,11 +74,19 @@
 <div class="editor">
    <div class="ruler_green"><div>&nbsp;<fmt:message key="community.search.result"/>&nbsp;</div></div>
    <div class="body">
-      <form action="${pageContext.request.contextPath }/editors/community/AddUserToGroupInit.do" method="post">
-         <p><input type="submit" value="add To Group" name="submitButton" onclick="return addToGroup()"/></p>
+
+         <mm:notpresent referid="newsletterId" >
+            <form action="${addGroup}" method="post">
+               <input type="submit" value="add To Group" name="submitButton" onclick="return addToGroup()"/>
+         </mm:notpresent>
+         <mm:present referid="newsletterId">
+            <form action="${subscribeUrl}" method="post">
+            <input type="submit" value="Subscribe" name="subscribe"/>
+            <input type="hidden" name="newsletterId" value="${newsletterId}"/>
+         </mm:present>
          <edit:ui-table items="${personForShow}" var="person" size="${totalCount}" requestURI="/editors/community/SearchConditionalUser.do">
             <edit:ui-tcolumn title="">
-               <input type="checkbox" name="chk_" value="${person.authId}"/>&nbsp;
+               <input type="checkbox" name="chk_${person.authId}" value="${person.authId}"/>&nbsp;
                <a href="${pageContext.request.contextPath }/editors/community/userAddInitAction.do?authid=${person.authId}"><img src="<cmsc:staticurl page='/editors/gfx/icons/edit.png'/>" width="16" height="16" title="edit"></a>
                <a href="${pageContext.request.contextPath }/editors/community/deleteUserAction.do?authid=${person.authId}"><img src="<cmsc:staticurl page='/editors/gfx/icons/delete.png'/>" width="16" height="16" title="delete"></a>
             </edit:ui-tcolumn>
@@ -92,3 +106,5 @@
       </form>
    </div>
 </div>
+</mm:cloud>
+</mm:content>
