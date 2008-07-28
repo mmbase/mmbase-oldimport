937b1ad187f0e15ec4f38f28c639b9953106a752
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-07-28 16:13:17 +0000

    NodeManager names datatype
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@29777 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/datatypes/NodeManagerNamesDataType.java
@@ -0,0 +1,75 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.datatypes;
+
+import java.util.*;
+import org.mmbase.bridge.*;
+import org.mmbase.util.*;
+import org.mmbase.util.logging.Logger;
+import org.mmbase.util.logging.Logging;
+
+/**
+ * A StringDataType with can take all possible node manager as values.
+ * The default value can be indicated as a comma-seperated string, in which case the first existing
+ * nodemanager will be used as the actual node manager.
+ *
+ * @author Michiel Meeuwissen
+ * @version $Id: NodeManagerNamesDataType.java,v 1.1 2008-07-28 16:13:17 michiel Exp $
+ * @since MMBase-1.9
+ */
+public class NodeManagerNamesDataType extends StringDataType {
+
+    private static final Logger log = Logging.getLoggerInstance(NodeManagerNamesDataType.class);
+
+    private static final long serialVersionUID = 1L;
+
+
+    /*
+    protected String preCast(String value, Cloud cloud, Node node, Field field) {
+        if (value == null) return null;
+        D preCast =  enumerationRestriction.preCast(value, cloud);
+        return preCast;
+    }
+    */
+
+    public String getDefaultValue(Locale locale, Cloud cloud, Field field) {
+        if (defaultValue != null) {
+            for (String def : defaultValue.split("\\s*,\\s*")) {
+                log.info("Considereing " + def);
+                if (cloud.hasNodeManager(def)) return def;
+            }
+        }
+        return null;
+    }
+
+    public NodeManagerNamesDataType(String name) {
+        super(name);
+    }
+
+    public Iterator<Map.Entry<String, String>> getEnumerationValues(final Locale locale, final Cloud cloud, final Node node, final Field field) {
+        if (node == null && cloud == null) return null; // we don't know..
+        return new Iterator() {
+            List<NodeManager> list = node == null ? cloud.getNodeManagers() : node.getCloud().getNodeManagers();
+            Iterator<NodeManager> iterator = list.iterator();
+            public boolean hasNext() {
+                return iterator.hasNext();
+            }
+            public Map.Entry<String, String> next() {
+                NodeManager val = iterator.next();
+                return new Entry(val.getName(), val.getGUIName(NodeManager.GUI_PLURAL, locale));
+            }
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+
+
+}
