d1e3160add66b8dae6645bd55c135c624f6e7bf0
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-07-31 16:43:33 +0000

    framework related utilities
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@29936 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/framework/Utils.java
@@ -0,0 +1,55 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.framework;
+
+import java.io.*;
+import java.util.*;
+import org.w3c.dom.*;
+import org.xml.sax.*;
+import javax.xml.parsers.*;
+import org.mmbase.util.*;
+import org.mmbase.util.functions.*;
+import org.mmbase.util.logging.Logger;
+import org.mmbase.util.logging.Logging;
+
+/**
+ * @author Michiel Meeuwissen
+ * @version $Id: Utils.java,v 1.1 2008-07-31 16:43:33 michiel Exp $
+ * @since MMBase-1.9
+ */
+public abstract class Utils {
+
+    private Utils() {
+        // this class has no instances
+    }
+
+    public static Document renderToXml(Framework fw, Renderer renderer, Parameters blockParameters, Parameters frameworkParameters, Renderer.WindowState state,
+                                       Class<?> baseClass) throws FrameworkException {
+        boolean validation = true;
+        boolean xsd = false;
+        Writer w = new StringWriter();
+        fw.render(renderer, blockParameters, frameworkParameters, w, state);
+        InputSource source = new InputSource(new StringReader(w.toString()));
+        XMLEntityResolver resolver = new XMLEntityResolver(true, baseClass);
+        DocumentBuilder dbuilder = org.mmbase.util.xml.DocumentReader.getDocumentBuilder(validation, xsd,
+                                                                                         null/* default error handler */, resolver);
+        if(dbuilder == null) throw new RuntimeException("failure retrieving document builder");
+        try {
+            Document doc = dbuilder.parse(source);
+            return  doc;
+        } catch (IOException ioe) {
+            throw new FrameworkException(ioe);
+        } catch (SAXException se) {
+            throw new FrameworkException(se);
+        }
+
+    }
+
+}
