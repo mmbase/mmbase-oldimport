75132aa2ac2b1fb52908194ff91d337509ebfaa0
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-08-01 21:11:55 +0000

    Identify the future
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@30019 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/module/Module.java
+++ src/org/mmbase/module/Module.java
@@ -37,7 +37,7 @@ import org.mmbase.util.logging.Logger;
  * @author Rob Vermeulen (securitypart)
  * @author Pierre van Rooden
  *
- * @version $Id: Module.java,v 1.100 2008-07-31 13:46:03 michiel Exp $
+ * @version $Id: Module.java,v 1.101 2008-08-01 21:11:55 michiel Exp $
  */
 public abstract class Module extends DescribedFunctionProvider {
 
@@ -120,6 +120,7 @@ public abstract class Module extends DescribedFunctionProvider {
             },
             3600 - (now.get(Calendar.MINUTE) * 60 + now.get(Calendar.SECOND)),  // some effort to run exactly at hour
             3600, TimeUnit.SECONDS);
+        ThreadPools.identify(future, "maintainance probe for module " + (name == null ? this.getClass().getName() : name));
     }
     /**
      * @since MMBase-1.8
