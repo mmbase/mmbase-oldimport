9f2dc610eb4d23f63a31ecbbc97c873cfc37a24c
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-08-11 15:43:21 +0000

    some more tests
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@30268 91430c5f-d668-0410-9e7d-dd663366dcfd

--- tests/bridge/org/mmbase/bridge/DataTypesTest.java
+++ tests/bridge/org/mmbase/bridge/DataTypesTest.java
@@ -442,6 +442,23 @@ public class DataTypesTest extends BridgeTest {
         Field field = nodeManager.getField("float");
         DataType dt = field.getDataType();
         field.getDataType().validate("1,2");
+
+        Node newNode = nodeManager.createNode();
+        newNode.setValue("float", "1,2");
+        assertEquals(1.2, newNode.getFloatValue("float"));
+
+        newNode.setValue("float", "1.2");
+        assertEquals(1.2, newNode.getFloatValue("float"));
+
+        newNode.setValue("integer", "1e2");
+        assertEquals(100, newNode.getIntValue("integer"));
+
+        cloud.setLocale(Locale.US);
+        try {
+            field.getDataType().validate("1,2");
+            fail("1,2 is not a valid float value in locale US");
+        } catch (Exception e) {
+        }
     }
 
 }
