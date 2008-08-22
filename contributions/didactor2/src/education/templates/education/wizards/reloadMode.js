42a3cb792dba5da307108b8237fea8879915d20d
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-08-22 11:13:36 +0000

    *** empty log message ***
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@30482 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/didactor2/src/education/templates/education/wizards/reloadMode.js
+++ contributions/didactor2/src/education/templates/education/wizards/reloadMode.js
@@ -1,13 +1,11 @@
 function reloadMode() {
-    console.log("Reloading ${_}");
     var url = '${_}';
     $.ajax({async: true, url: url, type: "GET", dataType: "xml",
             complete: function(res, status){
 		if (status == "success") {
-		    console.log(res);
 		    document.getElementById('mode-${mode}').innerHTML = res.responseText;
-		    //restoreTree();
-		    //storeTree();
+		    restoreTree();
+		    storeTree();
 		}
 	    }
 	   });
