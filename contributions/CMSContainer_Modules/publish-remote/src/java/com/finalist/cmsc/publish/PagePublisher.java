b988ede3bfd62e321c85a7e5664106fc66cf7d9a
rschellhorn <rschellhorn@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-08-28 09:11:12 +0000

    Do not recursively publish pages and content underneath RSS feeds.
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@30556 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer_Modules/publish-remote/src/java/com/finalist/cmsc/publish/PagePublisher.java
+++ contributions/CMSContainer_Modules/publish-remote/src/java/com/finalist/cmsc/publish/PagePublisher.java
@@ -229,7 +229,10 @@ public class PagePublisher extends Publisher {
                                    }
                                }
                                else {
-                                   findPageNodes(childNode, nodes, publishDate);
+                                   boolean isNavigationType = NavigationUtil.getTreeManagers().containsKey(childNode.getNodeManager().getName());
+                                   if (!isNavigationType) {
+                                      findPageNodes(childNode, nodes, publishDate);
+                                   }
                                }
                            }
                        }
