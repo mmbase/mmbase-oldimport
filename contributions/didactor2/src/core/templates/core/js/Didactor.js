bc1fc4ad36dc20d5ee7f20a54f29395e29add8ae
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-08-28 16:32:17 +0000

    *** empty log message ***
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@30589 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/didactor2/src/core/templates/core/js/Didactor.js
+++ contributions/didactor2/src/core/templates/core/js/Didactor.js
@@ -1,20 +1,32 @@
+/**
+ * Didactor javascript object.
+ * Currently only arranged 'online reporting' and 'page stay reporting'.
+ * But more could perhaps be done here. E.g. content loading of /content/js could perhaps be generalized and migrated to here.
+ *
+ * One global variable 'didactor' is automaticly created, which can be be referenced (as long as the di:head tag is used).
+ * @since Didactor 2.3.0
+ * @author Michiel Meeuwissen
+ * @version $Id: Didactor.js,v 1.6 2008-08-28 16:32:17 michiel Exp $
+ */
 
 
 function Didactor() {
     this.onlineReporter = this.getSetting("Didactor-OnlineReporter");
     this.url            = this.getSetting("Didactor-URL");
     this.lastCheck      = new Date();
+    this.pageReporter   = this.getSetting("Didactor-PageReporter") == "true";
     var self = this;
     $.timer(500, function(timer) {
 	self.reportOnline();
-	timer.reset(self.getSetting("Didactor-PageReporter") == "true" ? 5000 : 1000 * 60 * 2);
+	timer.reset(self.pageReporter ? 5000 : 1000 * 60 * 2);
     });
-    $(window).bind("beforeunload", function() {
-	self.reportOnline(null, false);
-    });
+    if (this.pageReporter) {
+	$(window).bind("beforeunload", function() {
+	    self.reportOnline(null, false);
+	});
+    }
     this.content = $.query.get("learnobject");
     $.query.REMOVE("learnobject");
-
 }
 
 Didactor.prototype.getSetting = function(name) {
@@ -38,7 +50,9 @@ Didactor.prototype.reportOnline = function (timer, async) {
 }
 
 Didactor.prototype.setContent = function(c) {
-    reportOnline(null, false);
+    if (this.pageReporter) {
+	this.reportOnline(null, false);
+    }
     this.content = c;
 }
 
