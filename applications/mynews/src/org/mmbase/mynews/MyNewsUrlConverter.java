f66507ae70206ee5faf19486315bb3aa82b69b66
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-09-01 07:07:00 +0000

    extended from DirectoryUrlConverter
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@30613 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/mynews/src/org/mmbase/mynews/MyNewsUrlConverter.java
+++ applications/mynews/src/org/mmbase/mynews/MyNewsUrlConverter.java
@@ -16,7 +16,8 @@ import org.mmbase.bridge.*;
 import org.mmbase.bridge.util.Queries;
 import org.mmbase.storage.search.*;
 import org.mmbase.framework.*;
-import org.mmbase.framework.basic.UrlConverter;
+import org.mmbase.framework.basic.DirectoryUrlConverter;
+import org.mmbase.framework.basic.BasicFramework;
 import org.mmbase.util.functions.*;
 import org.mmbase.util.logging.*;
 
@@ -33,20 +34,20 @@ import org.mmbase.util.logging.*;
  *
  *
  * @author Michiel Meeuwissen
- * @version $Id: MyNewsUrlConverter.java,v 1.18 2008-07-09 17:54:46 michiel Exp $
+ * @version $Id: MyNewsUrlConverter.java,v 1.19 2008-09-01 07:07:00 michiel Exp $
  * @since MMBase-1.9
  */
-public class MyNewsUrlConverter implements UrlConverter {
+public class MyNewsUrlConverter extends DirectoryUrlConverter {
     private static final Logger log = Logging.getLoggerInstance(MyNewsUrlConverter.class);
 
     private static CharTransformer trans = new Identifier();
     private boolean useTitle = false;
     private int dateDepth  = 0;
-    private String  directory = "/magazine";
-    private final Framework framework;
 
-    public MyNewsUrlConverter(Framework fw) {
-        framework = fw;
+    public MyNewsUrlConverter(BasicFramework fw) {
+        super(fw);
+        setDirectory("/magazine/");
+        addComponent(ComponentRepository.getInstance().getComponent("mynews"));
     }
 
     public void setUseTitle(boolean t) {
@@ -55,158 +56,98 @@ public class MyNewsUrlConverter implements UrlConverter {
     public void setDateDepth(int d) {
         dateDepth = d;
     }
-    public void setDir(String d) {
-        directory = d;
-    }
 
 
-    public Parameter[] getParameterDefinition() {
-        return new Parameter[] {};
-    }
-
-    protected String getUrl(String path,
-                             Map<String, Object> parameters,
-                             Parameters frameworkParameters, boolean escapeAmps, boolean action) {
-        if (log.isDebugEnabled()) {
-            log.debug("" + path + parameters + frameworkParameters);
-        }
-        Block renderingBlock = framework.getRenderingBlock(frameworkParameters);
-        if (renderingBlock == null) {
-            log.debug("No current block found for parameters " + frameworkParameters);
-            return null;
-        } else {
-            HttpServletRequest request = frameworkParameters.get(Parameter.REQUEST);
-
-            Block block = framework.getBlock(frameworkParameters);
-
-            if (block != null && "mynews".equals(block.getComponent().getName())) {
-                block = renderingBlock.getComponent().getBlock(path);
-                log.debug("Found mynews block " + block);
-                Node n = (Node) parameters.get(Framework.N.getName());
-                StringBuilder b = new StringBuilder(directory);
-                if(block.getName().equals("article")) {
-                    b.append("/");
-                    if (dateDepth > 0) {
-                        Calendar cal = Calendar.getInstance();
-                        cal.setTime(n.getDateValue("date"));
-                        b.append(cal.get(Calendar.YEAR));
-                        b.append('/');
-                        if (dateDepth > 1) {
-                            b.append(cal.get(Calendar.MONTH) + 1);
-                            b.append('/');
-                            if (dateDepth > 2) {
-                                b.append(cal.get(Calendar.DAY_OF_MONTH));
-                                b.append('/');
-                            }
-                        }
-                    }
-
-                    if (useTitle) {
-                        b.append(trans.transform(n.getStringValue("title")));
-                    } else {
-                        b.append(n);
-                    }
-                }
-                return b.toString();
-            } else {
-                log.debug("No mynews block found");
-                return null;
-            }
-        }
-    }
-
-    public String getUrl(String path,
-                         Map<String, Object> parameters,
-                         Parameters frameworkParameters, boolean escapeAmps) {
-        return getUrl(path, parameters, frameworkParameters, escapeAmps, false);
-    }
-    public String getProcessUrl(String path,
+    protected String getNiceUrl(Block block,
                                 Map<String, Object> parameters,
-                                Parameters frameworkParameters, boolean escapeAmps) {
-        return getUrl(path, parameters, frameworkParameters, escapeAmps, true);
-    }
-
-    public String getInternalUrl(String page, Map<String, Object> params, Parameters frameworkParameters) throws FrameworkException {
-        HttpServletRequest request = frameworkParameters.get(Parameter.REQUEST);
-        if (page == null) throw new IllegalArgumentException();
-        if (page.startsWith(directory)) {
-            log.debug("Found a mynews url");
-            String sp = FrameworkFilter.getPath(request);
-            String[] path = sp.split("/");
-            if (log.isDebugEnabled()) {
-                log.debug("Going to filter " + Arrays.asList(path));
-            }
-            if (path.length >= 2) {
-                StringBuilder result = new StringBuilder("/mmbase/mynews/render.jspx");
-                assert path[0].equals("");
-                assert path[1].equals(directory.substring(1)) : path[1] + " " + directory;
-                if (path.length == 2) {
-                    // magazine mode.
-                    return result.toString();
-                } else {
-                    // article mode
-                    String id = path[path.length - 1];
-                    String n;
-                    if (useTitle) {
-                        Cloud cloud = ContextProvider.getDefaultCloudContext().getCloud("mmbase");
-                        NodeManager news = cloud.getNodeManager("news");
-                        NodeQuery q = news.createQuery();
-                        String like = id;//.replaceAll("_", "?");
-                        Queries.addConstraint(q, Queries.createConstraint(q, "title", Queries.getOperator("LIKE"), like));
-                        if (path.length > 3) {
-                            String[] date = new String[] {path[2], "01", "01"};
-                            String offset = "1 year";
-                            if (path.length > 4) {
-                                date[1] = path[3];
-                                offset = "1 month";
-                            }
-                            if (path.length > 5) {
-                                date[2] = path[4];
-                                offset = "1 day";
-                            }
-                            String ds = date[0] + '-' + date[1] + '-' + date[2];
-                            try {
-                                Constraint start = Queries.createConstraint(q, "date", Queries.getOperator("ge"), DynamicDate.getInstance(ds));
-                                Constraint end   = Queries.createConstraint(q, "date", Queries.getOperator("le"), DynamicDate.getInstance(ds + " + " + offset));
-                                Queries.addConstraint(q, start);
-                                Queries.addConstraint(q, end);
-                            } catch (org.mmbase.util.dateparser.ParseException pe) {
-                                throw new RuntimeException(pe);
-                            }
-                        }
-                        NodeList list = news.getList(q);
-                        Node node;
-                        if (list.size() > 0) {
-                            node = list.getNode(0);
-                        } else {
-                            if (cloud.hasNode(id)) {
-                                // alias/nodenumbers work too
-                                node = cloud.getNode(id);
-                            } else {
-                                throw new FrameworkException("" + q.toSql() + " gave no results");
-                            }
-                        }
-                        n = "" + node.getNumber();
-                    } else {
-                        // node was specified by number. Date spec can be ignored.
-                        n = id;
-                    }
-                    result.append("?block=article&n=" + n);
-                    return result.toString();
-                }
-            } else {
-                log.debug("path length " + path.length);
-                return null;
-            }
-        } else {
-            log.debug("Leaving unfiltered");
-            return null;
+                                Parameters frameworkParameters, boolean escapeAmps, boolean action) throws FrameworkException {
+        if (log.isDebugEnabled()) {
+            log.debug("" + block + parameters + frameworkParameters);
         }
+
+        log.debug("Found mynews block " + block);
+        Node n = (Node) parameters.get(Framework.N.getName());
+        StringBuilder b = new StringBuilder(directory);
+        if(block.getName().equals("article")) {
+            if (dateDepth > 0) {
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(n.getDateValue("date"));
+                b.append(cal.get(Calendar.YEAR));
+                b.append('/');
+                if (dateDepth > 1) {
+                    b.append(cal.get(Calendar.MONTH) + 1);
+                    b.append('/');
+                    if (dateDepth > 2) {
+                        b.append(cal.get(Calendar.DAY_OF_MONTH));
+                        b.append('/');
+                    }
+                }
+            }
+
+            if (useTitle) {
+                b.append(trans.transform(n.getStringValue("title")));
+            } else {
+                b.append(n);
+            }
+        }
+        return b.toString();
     }
 
-    public String toString() {
-        return directory;
-    }
 
+    public String getFilteredInternalUrl(List<String>  path, Map<String, Object> params, Parameters frameworkParameters) throws FrameworkException {
+        StringBuilder result = new StringBuilder("/mmbase/mynews/render.jspx");
+        if (path.size() > 0) {
+            // article mode
+            String id = path.get(path.size() - 1); // last element in the list identifies the article
+            String n;
+            if (useTitle) {
+                Cloud cloud = ContextProvider.getDefaultCloudContext().getCloud("mmbase");
+                NodeManager news = cloud.getNodeManager("news");
+                NodeQuery q = news.createQuery();
+                String like = id;
+                Queries.addConstraint(q, Queries.createConstraint(q, "title", Queries.getOperator("LIKE"), like));
+                if (path.size() > 1) {
+                    String[] date = new String[] {path.get(0), "01", "01"};
+                    String offset = "1 year";
+                    if (path.size() > 2) {
+                        date[1] = path.get(1);
+                        offset = "1 month";
+                    }
+                    if (path.size() > 3) {
+                        date[2] = path.get(2);
+                        offset = "1 day";
+                    }
+                    String ds = date[0] + '-' + date[1] + '-' + date[2];
+                    try {
+                        Constraint start = Queries.createConstraint(q, "date", Queries.getOperator("ge"), DynamicDate.getInstance(ds));
+                        Constraint end   = Queries.createConstraint(q, "date", Queries.getOperator("le"), DynamicDate.getInstance(ds + " + " + offset));
+                        Queries.addConstraint(q, start);
+                        Queries.addConstraint(q, end);
+                    } catch (org.mmbase.util.dateparser.ParseException pe) {
+                        throw new RuntimeException(pe);
+                    }
+                }
+                NodeList list = news.getList(q);
+                Node node;
+                if (list.size() > 0) {
+                    node = list.getNode(0);
+                } else {
+                    if (cloud.hasNode(id)) {
+                        // alias/nodenumbers work too
+                        node = cloud.getNode(id);
+                    } else {
+                        throw new FrameworkException("" + q.toSql() + " gave no results");
+                    }
+                }
+                n = "" + node.getNumber();
+            } else {
+                // node was specified by number. Date spec can be ignored.
+                n = id;
+            }
+            result.append("?block=article&n=" + n);
+        }
+
+        return result.toString();
+    }
 
 }
