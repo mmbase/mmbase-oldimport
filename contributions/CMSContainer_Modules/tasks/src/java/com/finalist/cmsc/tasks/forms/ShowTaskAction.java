41ddda0530e707447d8a028a96497a917b995050
nklasens <nklasens@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-09-03 09:58:58 +0000

    CMSC-900 Use commons.lang.StringUtils for string emptiness
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@30643 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer_Modules/tasks/src/java/com/finalist/cmsc/tasks/forms/ShowTaskAction.java
+++ contributions/CMSContainer_Modules/tasks/src/java/com/finalist/cmsc/tasks/forms/ShowTaskAction.java
@@ -3,8 +3,7 @@ package com.finalist.cmsc.tasks.forms;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import net.sf.mmapps.commons.util.StringUtil;
-
+import org.apache.commons.lang.StringUtils;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
@@ -21,7 +20,7 @@ public class ShowTaskAction extends MMBaseAction {
 		// TODO Auto-generated method stub
 		ShowTaskForm showTaskForm = (ShowTaskForm)form;
 		String taskShowType=showTaskForm.getTaskShowType();
-		if(!StringUtil.isEmptyOrWhitespace(taskShowType)){
+		if(StringUtils.isNotBlank(taskShowType)){
 			request.setAttribute("taskShowType", taskShowType);
 		}
 		return mapping.findForward("success");
