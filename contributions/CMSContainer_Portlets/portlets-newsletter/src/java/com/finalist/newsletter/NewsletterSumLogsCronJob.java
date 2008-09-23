ae9379130bccaf93094e5f487537dee5a245c1c6
nikkoyin <nikkoyin@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-09-23 07:41:58 +0000

    CMSC-899,Check and improve all lists according to guideline,delete tab character
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@30980 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer_Portlets/portlets-newsletter/src/java/com/finalist/newsletter/NewsletterSumLogsCronJob.java
+++ contributions/CMSContainer_Portlets/portlets-newsletter/src/java/com/finalist/newsletter/NewsletterSumLogsCronJob.java
@@ -1,7 +1,5 @@
 package com.finalist.newsletter;
 
-
-
 import java.util.List;
 
 import org.mmbase.applications.crontab.AbstractCronJob;
@@ -12,38 +10,50 @@ import org.mmbase.util.logging.Logging;
 import com.finalist.cmsc.services.community.ApplicationContextFactory;
 import com.finalist.newsletter.domain.StatisticResult;
 import com.finalist.newsletter.services.StatisticService;
+
+/**
+ * @author nikko
+ * 
+ */
 public class NewsletterSumLogsCronJob extends AbstractCronJob implements CronJob {
 
-	private static Logger log = Logging
-			.getLoggerInstance(NewsletterSumLogsCronJob.class.getName());
+   private static Logger log = Logging.getLoggerInstance(NewsletterSumLogsCronJob.class.getName());
 
-	@Override
+   /*
+    * @see org.mmbase.applications.crontab.AbstractCronJob#init()
+    */
+   @Override
    public void init() {
-		log.info("Initializing NewsletterLogRecorder");
-	}
+      log.info("Initializing NewsletterLogRecorder");
+   }
 
-	@Override
+   /*
+    * @see org.mmbase.applications.crontab.AbstractCronJob#run()
+    */
+   @Override
    public void run() {
-		StatisticService service = (StatisticService) ApplicationContextFactory.getBean("statisticService");
-		List<StatisticResult> listRecorder = service.getLogs();
-		if (null != listRecorder) {
-			insetStatRecord(service, listRecorder);
-			listRecorder.clear();
-		}
-	}
+      StatisticService service = (StatisticService) ApplicationContextFactory.getBean("statisticService");
+      List < StatisticResult > listRecorder = service.getLogs();
+      if (null != listRecorder) {
+         insetStatRecord(service, listRecorder);
+         listRecorder.clear();
+      }
+   }
 
-	private int insetStatRecord(StatisticService service,List<StatisticResult> listRecorder) {
-		if(null!=listRecorder){
-			return service.pushSumedLogs(listRecorder);
-		}
-		else{
-		    return 0;
-		}
-	}
+   private int insetStatRecord(StatisticService service, List < StatisticResult > listRecorder) {
+      if (null != listRecorder) {
+         return service.pushSumedLogs(listRecorder);
+      } else {
+         return 0;
+      }
+   }
 
-	@Override
+   /*
+    * @see org.mmbase.applications.crontab.AbstractCronJob#stop()
+    */
+   @Override
    public void stop() {
-		log.info("Stopping NewsletterLogRecorder");
+      log.info("Stopping NewsletterLogRecorder");
 
-	}
+   }
 }
