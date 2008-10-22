9e792d2636540463cb91bf2f632b32c33f29631e
nikkoyin <nikkoyin@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-10-22 05:35:01 +0000

    CMSC-1095, Newsletter Bounces list should be ordered,add paramater oder and direction
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@31424 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer_Portlets/portlets-newsletter/src/java/com/finalist/newsletter/forms/NewsletterBounceAction.java
+++ contributions/CMSContainer_Portlets/portlets-newsletter/src/java/com/finalist/newsletter/forms/NewsletterBounceAction.java
@@ -6,8 +6,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
@@ -19,27 +17,32 @@ import com.finalist.newsletter.util.NewsletterBounceUtil;
 
 public class NewsletterBounceAction extends DispatchAction {
 
-   public ActionForward list(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
-            throws Exception {
+   public ActionForward list(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+         HttpServletResponse response) throws Exception {
       int pageSize = 12;
       int offset = 0;
       if (StringUtils.isNotEmpty(PropertiesUtil.getProperty("repository.search.results.per.page"))) {
          pageSize = Integer.parseInt(PropertiesUtil.getProperty("repository.search.results.per.page"));
       }
       String strOffset = request.getParameter("offset");
+      String direction = request.getParameter("direction");
+      String order = request.getParameter("order");
       if (StringUtils.isNotEmpty(strOffset)) {
          offset = Integer.parseInt(strOffset);
       }
-      List<NewsletterBounce> bounces = NewsletterBounceUtil.getBounceRecord(offset * pageSize, pageSize);
+      List<NewsletterBounce> bounces = NewsletterBounceUtil.getBounceRecord(offset * pageSize, pageSize, order,
+            direction);
       int count = NewsletterBounceUtil.getTotalCount();
       request.setAttribute("resultList", bounces);
       request.setAttribute("resultCount", count);
       request.setAttribute("offset", offset);
+      request.setAttribute("direction", direction);
+      request.setAttribute("order", order);
       return mapping.findForward("success");
    }
 
-   public ActionForward getItem(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
-            throws Exception {
+   public ActionForward getItem(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+         HttpServletResponse response) throws Exception {
       String number = request.getParameter("objectnumber");
       NewsletterBounce bounce = NewsletterBounceUtil.getNewsletterBounce(Integer.parseInt(number));
       request.setAttribute("bounce", bounce);
