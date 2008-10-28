7bbe091edc518df927b1782d1fbf3248ff046d6d
fpunt <fpunt@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-10-28 13:36:26 +0000

    CMSC-1120 improved captcha pluginablity
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@31551 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer_Portlets/portlets-guestbook/src/java/com/finalist/captcha/CaptchaServiceSingleton.java
+++ contributions/CMSContainer_Portlets/portlets-guestbook/src/java/com/finalist/captcha/CaptchaServiceSingleton.java
@@ -1,11 +1,17 @@
 package com.finalist.captcha;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.finalist.cmsc.mmbase.PropertiesUtil;
+import com.octo.captcha.engine.image.ListImageCaptchaEngine;
 import com.octo.captcha.service.captchastore.FastHashMapCaptchaStore;
 import com.octo.captcha.service.image.DefaultManageableImageCaptchaService;
 import com.octo.captcha.service.image.ImageCaptchaService;
 
 public class CaptchaServiceSingleton {
 
+   private static Log log = LogFactory.getLog(CaptchaServiceSingleton.class);	
    private static ImageCaptchaService instance = initializeService();
 
 
@@ -15,7 +21,19 @@ public class CaptchaServiceSingleton {
 
 
    private static ImageCaptchaService initializeService() {
-      NumberListImageCaptchaEngine engine = new NumberListImageCaptchaEngine();
+	  String engineClass = PropertiesUtil.getProperty("guestbook.captcha.engineClass");
+	  ListImageCaptchaEngine engine = null; 
+      
+      if(engineClass == null || engineClass.length() == 0) {
+    	  engine = new NumberListImageCaptchaEngine();
+      }
+      else {
+    	  try {
+			engine = (ListImageCaptchaEngine) Class.forName(engineClass).newInstance();
+		  } catch (Exception e) {
+	        log.error("Could not instantiate engineClass for guestbook (see admin/properties)", e);
+		  }
+      }
       return new DefaultManageableImageCaptchaService(new FastHashMapCaptchaStore(), engine, 180, 100000, 75000);
    }
 }
