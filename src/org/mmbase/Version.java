9c37e3e932fd2f07f1ca91327e348a93bd1782f9
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-10-29 20:53:27 +0000

    will release 1.9.0very soon now
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@31576 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/Version.java
+++ src/org/mmbase/Version.java
@@ -17,7 +17,7 @@ import java.io.*;
  *
  * @author Daniel Ockeloen
  * @author Michiel Meeuwissen
- * @version $Id: Version.java,v 1.46 2008-06-27 09:37:34 michiel Exp $
+ * @version $Id: Version.java,v 1.47 2008-10-29 20:51:51 michiel Exp $
  */
 public class Version {
 
@@ -104,7 +104,7 @@ public class Version {
      * @since MMBase-1.6
      */
     public static boolean isRelease() {
-        return false;
+        return true;
     };
 
     /**
@@ -113,7 +113,7 @@ public class Version {
      * @since MMBase-1.7
      */
     public static String getReleaseStatus() {
-        return "";
+        return "final";
     };
 
     /**
