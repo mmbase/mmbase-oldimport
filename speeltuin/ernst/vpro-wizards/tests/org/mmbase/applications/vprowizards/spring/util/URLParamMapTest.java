03ed19ed30c0fe59b483653702624781053d173a
ernst <ernst@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-11-03 11:42:37 +0000

    chanted test back to junit 3, so maven actually understands it.
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@31650 91430c5f-d668-0410-9e7d-dd663366dcfd

--- speeltuin/ernst/vpro-wizards/tests/org/mmbase/applications/vprowizards/spring/util/URLParamMapTest.java
+++ speeltuin/ernst/vpro-wizards/tests/org/mmbase/applications/vprowizards/spring/util/URLParamMapTest.java
@@ -14,51 +14,42 @@ package org.mmbase.applications.vprowizards.spring.util;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Before;
-import org.junit.Test;
-
-import static org.junit.Assert.*;
+import junit.framework.TestCase;
 
 
 
-public class URLParamMapTest {
+public class URLParamMapTest extends TestCase{
     private URLParamMap testMap;
     private static final String url = "http://www.nogwat.com?een=zus&twee=zo";
 
-    @Before
     public void setUp(){
         testMap = new URLParamMap(url);
         
     }
     
-    @Test
     public void test_parse_url(){
         assertEquals(2, testMap.size());
         assertEquals(url, testMap.toString());
     }
 
-    @Test
     public void test_override_param_illegal(){
         testMap.addParam("een", "hallo", false);
         assertEquals(2, testMap.size());
         assertEquals(url, testMap.toString());
     }
     
-    @Test
     public void test_override_param_legal(){
         testMap.addParam("een", "vaarwel", true);
         assertEquals(2, testMap.size());
         assertEquals("http://www.nogwat.com?een=vaarwel&twee=zo", testMap.toString());
     }
     
-    @Test
     public void test_add_new_param(){
         testMap.addParam("drie", "hola", true);
         assertEquals(3, testMap.size());
         assertEquals(url + "&drie=hola", testMap.toString());
     }
     
-    @Test
     public void test_add_first_param(){
         testMap = new URLParamMap("www.blender.org");
         testMap.addParam("drie", "hola", true);
@@ -66,7 +57,6 @@ public class URLParamMapTest {
         assertEquals("www.blender.org?drie=hola", testMap.toString());
     }
     
-    @Test
     public void test_add_first_param_where_url_ends_with_questonmark(){
         testMap = new URLParamMap("www.blender.org?");
         testMap.addParam("drie", "hola", true);
@@ -74,7 +64,6 @@ public class URLParamMapTest {
         assertEquals("www.blender.org?drie=hola", testMap.toString());
     }
     
-    @Test
     public void test_output_is_input_when_no_params_added(){
         List<String>tests = new ArrayList<String>();
         tests.add("www.blender.org");
