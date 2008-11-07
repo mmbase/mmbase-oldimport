c7d3142ee65d6cd8915bf9e802e768d09b123b9e
evaguo <evaguo@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-11-07 02:55:12 +0000

    CMSC-1135(Improve the code.)
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@31764 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer_Portlets/portlets-newsletter/src/webapp/editors/newsletter/newsletter.js
+++ contributions/CMSContainer_Portlets/portlets-newsletter/src/webapp/editors/newsletter/newsletter.js
@@ -1,20 +1,20 @@
 function massDelete(confirmmessage) {
    if (confirmmessage) {
       if (confirm(confirmmessage)) {
-             var checkboxs = document.getElementsByTagName("input");
-             var objectnumbers = '';
-             for(i = 0; i < checkboxs.length; i++) {
-               if(checkboxs[i].type == 'checkbox' && checkboxs[i].name.indexOf('chk_') == 0 && checkboxs[i].checked) {
-                  objectnumbers += checkboxs[i].value+",";
-               }
-             }
-             if(objectnumbers == ''){
-               return ;
-             }
-             objectnumbers = objectnumbers.substr(0,objectnumbers.length - 1);
-             document.forms[0].deleteRequest.value = objectnumbers;
-             document.forms[0].method.value = "delete";
-             document.forms[0].submit();
+          var checkboxs = document.getElementsByTagName("input");
+          var objectnumbers = '';
+          for(i = 0; i < checkboxs.length; i++) {
+            if(checkboxs[i].type == 'checkbox' && checkboxs[i].name.indexOf('chk_') == 0 && checkboxs[i].checked) {
+               objectnumbers += checkboxs[i].value+",";
+            }
+          }
+          if(objectnumbers == ''){
+            return ;
+          }
+          objectnumbers = objectnumbers.substr(0,objectnumbers.length - 1);
+          document.forms[0].deleteRequest.value = objectnumbers;
+          document.forms[0].method.value = "delete";
+          document.forms[0].submit();
       }
    }   
 }
