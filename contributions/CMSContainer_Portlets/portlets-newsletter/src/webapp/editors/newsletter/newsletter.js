4372d7f06527346376b0f1c4582fdaed6252e22c
evaguo <evaguo@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-11-07 03:03:24 +0000

    CMSC-1135(Improve the code.)
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@31766 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer_Portlets/portlets-newsletter/src/webapp/editors/newsletter/newsletter.js
+++ contributions/CMSContainer_Portlets/portlets-newsletter/src/webapp/editors/newsletter/newsletter.js
@@ -1,20 +1,20 @@
 function massDelete(confirmmessage) {
    if (confirmmessage) {
       if (confirm(confirmmessage)) {
-          var checkboxs = document.getElementsByTagName("input");
-          var objectnumbers = '';
-          for(i = 0; i < checkboxs.length; i++) {
+         var checkboxs = document.getElementsByTagName("input");
+         var objectnumbers = '';
+         for(i = 0; i < checkboxs.length; i++) {
             if(checkboxs[i].type == 'checkbox' && checkboxs[i].name.indexOf('chk_') == 0 && checkboxs[i].checked) {
                objectnumbers += checkboxs[i].value+",";
             }
-          }
-          if(objectnumbers == ''){
+         }
+         if(objectnumbers == ''){
             return ;
-          }
-          objectnumbers = objectnumbers.substr(0,objectnumbers.length - 1);
-          document.forms[0].deleteRequest.value = objectnumbers;
-          document.forms[0].method.value = "delete";
-          document.forms[0].submit();
+         }
+         objectnumbers = objectnumbers.substr(0,objectnumbers.length - 1);
+         document.forms[0].deleteRequest.value = objectnumbers;
+         document.forms[0].method.value = "delete";
+         document.forms[0].submit();
       }
    }   
 }
@@ -58,24 +58,19 @@ function strDateTime(str){
    var reg = /^(\d{1,4})(-|\/)(\d{1,2})\2(\d{1,2})$/;
    var r = str.match(reg);
    if(r==null)return false;
-   var datenum = parseInt(r[4],10);
-   if(datenum>=1 && datenum<=9){
-      datenum = '0'+datenum;
-   }
-   var monthnum = parseInt(r[3],10);
-   if(monthnum >=1 && monthnum<=9){
-      monthnum = '0' +monthnum;
-   }
+   var datenum = formatDate(r[4]);
+   var monthnum = formatDate(r[3]);
    str = r[1]+r[2]+monthnum+r[2]+datenum;
    var d= new Date(r[1], r[3]-1,r[4]);
-   var dateRel = parseInt(d.getDate(),10);
-   if(dateRel >=1 && dateRel <=9){
-      dateRel = '0'+dateRel;
-   }
-   var monthRel = parseInt(d.getMonth()+1,10);
-   if(monthRel >=1 && monthRel <=9){
-      monthRel = '0' +monthRel;
-   }
+   var dateRel = formatDate(d.getDate());
+   var monthRel = formatDate(d.getMonth()+1);
    var newStr =d.getFullYear()+r[2]+monthRel+r[2]+dateRel;
    return newStr==str;
+}
+function formatDate(str){
+   var newdate = parseInt(str,10);
+   if(newdate >=1 && newdate <=9){
+      newdate = '0'+newdate;
+   }
+   return newdate;
 }
