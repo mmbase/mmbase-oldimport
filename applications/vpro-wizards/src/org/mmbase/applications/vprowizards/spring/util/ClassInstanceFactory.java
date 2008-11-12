d2a27a1bfab5f455d02e73aa0be11a0a84ee93d0
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-11-12 19:41:28 +0000

    didn't compile with java 6
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@31918 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/vpro-wizards/src/org/mmbase/applications/vprowizards/spring/util/ClassInstanceFactory.java
+++ applications/vpro-wizards/src/org/mmbase/applications/vprowizards/spring/util/ClassInstanceFactory.java
@@ -3,6 +3,12 @@ package org.mmbase.applications.vprowizards.spring.util;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Modifier;
 
+/**
+ * @javadoc
+ * @author Ernst Bunders
+ * @version $Id: ClassInstanceFactory.java,v 1.3 2008-11-12 19:41:28 michiel Exp $
+ */
+
 public class ClassInstanceFactory<T> {
 
     private Class<? extends T> clazz;
@@ -16,7 +22,7 @@ public class ClassInstanceFactory<T> {
         try {
             Class<? extends T> c = (Class<? extends T>) Class.forName(className);
             boolean pubNoarg = false;
-            for (Constructor<? extends T> m : c.getConstructors()) {
+            for (Constructor<?> m : c.getConstructors()) {
                 if (m.getParameterTypes().length == 0 && Modifier.isPublic(m.getModifiers())) {
                     pubNoarg = true;
                     this.clazz = c;
