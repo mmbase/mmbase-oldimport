6532b7079013d4f438e2c0a12f903231a06c3107
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-11-12 15:57:28 +0000

    test case for  MMB-1739
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@31902 91430c5f-d668-0410-9e7d-dd663366dcfd

--- tests/util/org/mmbase/util/ResourceLoaderTest.java
+++ tests/util/org/mmbase/util/ResourceLoaderTest.java
@@ -23,7 +23,7 @@ import junit.framework.TestCase;
  * </ul>
  *
  * @author Kees Jongenburger
- * @verion $Id: ResourceLoaderTest.java,v 1.7 2008-10-01 17:00:13 michiel Exp $
+ * @verion $Id: ResourceLoaderTest.java,v 1.8 2008-11-12 15:56:41 michiel Exp $
  */
 public class ResourceLoaderTest extends TestCase {
 
@@ -74,6 +74,11 @@ public class ResourceLoaderTest extends TestCase {
         List<URL> xmls2 = ResourceLoader.getConfigurationRoot().getChildResourceLoader("builders").getResourceList("properties.xml");
         assertEquals(xmls1, xmls2);
 
+    }
+
+    public void testWeightConfiguration() throws java.io.IOException {
+        URL u  = ResourceLoader.getConfigurationRoot().getChildResourceLoader("builders").getResource("core/object.xml");
+        assertTrue(u.toString(), u.toString().endsWith("/mmbase-test.jar!/org/mmbase/config/builders/core/object.xml"));
     }
 
 }
