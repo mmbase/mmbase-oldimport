f5bb98d52e0e6146f89c21530e44e74d9e90a42c
nightly <nightly@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-11-12 18:13:27 +0000

    *** empty log message ***
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@31915 91430c5f-d668-0410-9e7d-dd663366dcfd

--- speeltuin/nightly/cvs-stable.sh
+++ speeltuin/nightly/cvs-stable.sh
@@ -14,7 +14,7 @@ for i in 'applications/build.xml' 'contributions/build.xml' 'download.xml' ; do
 done
 echo "Build from ${revision} ${cvsversionoption} ${cvsversion} against java 1.4 are" > ${builddir}/README
 for i in 'src' 'maven-base' 'applications/app-base' 'documentation' 'tests' 'config' 'html' \
-    'applications/resources' 'applications/cloudsecurity' 'applications/mynews' 'application/xmlimporter' \
+    'applications/resources' 'applications/cloudsecurity' 'applications/mynews' 'applications/xmlimporter' \
     'applications/taglib' 'applications/editwizard' 'applications/dove' 'applications/cloudcontext' \
     'applications/rmmci' 'applications/vwms' 'applications/scan' 'applications/clustering' 'applications/oscache-cache' \
     'applications/media' 'applications/packaging' 'applications/community' 'applications/largeobjects' \
