c75724fd6b1e4ca083491a9a8b07c5e7fea0d1fb
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-11-13 09:34:33 +0000

    *** empty log message ***
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@31927 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/didactor2/src/core/java/nl/didactor/component/core/DidactorCore.java
+++ contributions/didactor2/src/core/java/nl/didactor/component/core/DidactorCore.java
@@ -166,7 +166,7 @@ public class DidactorCore extends Component {
         String owner = classrel.getStringValue("owner");
 
         MMObjectNode copybook = MMBase.getMMBase().getBuilder("copybooks").getNewNode(owner);
-        copybook.setValue("name", "copybook for " + person.getGUIIndicator());
+        copybook.setValue("name", "copybook for " + person.getFunctionValue("gui", null).toString());
         copybook.insert(owner);
 
         MMObjectNode relnode = MMBase.getMMBase().getInsRel().getNewNode(owner);
