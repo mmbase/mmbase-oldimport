b670c7b794f9859b9d6835ea84177d4f407a2e06
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-11-18 13:07:00 +0000

    *** empty log message ***
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@32042 91430c5f-d668-0410-9e7d-dd663366dcfd

--- tests/richtext/org/mmbase/richtext/VersioningTest.java
+++ tests/richtext/org/mmbase/richtext/VersioningTest.java
@@ -15,31 +15,51 @@ import org.mmbase.tests.*;
 /**
  *
  * @author Michiel Meeuwissen
- * @verion $Id: VersioningTest.java,v 1.1 2008-11-18 12:58:16 michiel Exp $
+ * @verion $Id: VersioningTest.java,v 1.2 2008-11-18 13:07:00 michiel Exp $
  */
 public class VersioningTest extends BridgeTest {
 
 
 
-    protected void testVersionField(Cloud c) {
+    protected Transaction getTransaction(Cloud c) {
+        return c.createTransaction("testtest");
+    }
+    protected Node testVersionField1(Cloud c) {
         NodeManager nm = c.getNodeManager("wiki_news");
         Node node = nm.createNode();
         assertEquals(0, node.getIntValue("version"));
         node.commit();
-        int nn = node.getNumber();
-        node = c.getNode(nn);
+        return node;
+    }
+    protected void testVersionField2(Cloud c, int nn) {
+        Node node = c.getNode(nn);
         node.setStringValue("title", "" + System.currentTimeMillis());
         node.commit();
         assertEquals(1, node.getIntValue("version"));
 
     }
 
+
+
     public void testVersionFieldCloud() {
-        testVersionField(getCloud());
+        Cloud c = getCloud();
+        Node n = testVersionField1(c);
+        n.commit();
+        int nn = n.getNumber();
+        assertTrue(nn > 0);
+        testVersionField2(c, nn);
     }
 
     public void testVersionFieldTransaction() {
-        testVersionField(getCloud().createTransaction("testtest"));
+        Transaction t = getTransaction(getCloud());
+        Node n = testVersionField1(t);
+        n.commit();
+        t.commit();
+        int nn = n.getNumber();
+        assertTrue(nn > 0);
+        t = getTransaction(getCloud());
+        testVersionField2(t, nn);
+
     }
 
 }
