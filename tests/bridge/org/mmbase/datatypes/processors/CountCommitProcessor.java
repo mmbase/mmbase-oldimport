fbe96cb6d2960551ff53753f4b89e1ff91d12694
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-11-25 16:02:02 +0000

    a test-datatype to test wether commit processors are called
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@32171 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ tests/bridge/org/mmbase/datatypes/processors/CountCommitProcessor.java
@@ -0,0 +1,36 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.datatypes.processors;
+
+import org.mmbase.bridge.*;
+
+/**
+ * This commit processor can be used to check how often a commitprocessor is called.
+ *
+ * @author Michiel Meeuwissen
+ * @version $Id: CountCommitProcessor.java,v 1.1 2008-11-25 16:02:02 michiel Exp $
+ * @since MMBase-1.9.1
+ */
+
+public class CountCommitProcessor implements CommitProcessor {
+
+    public static int count = 0;
+    public static int changed = 0;
+
+    public final void commit(Node node, Field field) {
+        count++;
+        if (node.isChanged()) {
+            changed++;
+            node.setIntValue(field.getName(), changed);
+        }
+    }
+}
+
+
