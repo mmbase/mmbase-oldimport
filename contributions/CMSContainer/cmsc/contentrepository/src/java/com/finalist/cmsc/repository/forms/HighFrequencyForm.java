c99d5e99795e6fb5a3368f061995d8d7e52c96b7
evaguo <evaguo@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-12-03 10:27:55 +0000

    CMSC-1126 (add form)
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@32334 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ contributions/CMSContainer/cmsc/contentrepository/src/java/com/finalist/cmsc/repository/forms/HighFrequencyForm.java
@@ -0,0 +1,24 @@
+package com.finalist.cmsc.repository.forms;
+
+import com.finalist.cmsc.struts.PagerForm;
+
+@SuppressWarnings("serial")
+public class HighFrequencyForm extends PagerForm {
+
+   private String channelid;
+
+   public String getChannelid() {
+      return channelid;
+   }
+
+   public void setChannelid(String channelid) {
+      this.channelid = channelid;
+
+   }
+
+   @override
+   public void reset() {
+      channelid = null;
+   }
+
+}
