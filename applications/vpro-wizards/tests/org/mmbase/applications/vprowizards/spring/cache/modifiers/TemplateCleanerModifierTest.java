d48d92384fbc4dcf397660e0cefc0c5847c6a8d5
ernst <ernst@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-12-08 15:59:26 +0000

    *** empty log message ***
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@32424 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ applications/vpro-wizards/tests/org/mmbase/applications/vprowizards/spring/cache/modifiers/TemplateCleanerModifierTest.java
@@ -0,0 +1,10 @@
+package org.mmbase.applications.vprowizards.spring.cache.modifiers;
+
+import junit.framework.TestCase;
+
+public class TemplateCleanerModifierTest extends TestCase {
+
+    public void test(){
+        //fail("implement");
+    }
+}
