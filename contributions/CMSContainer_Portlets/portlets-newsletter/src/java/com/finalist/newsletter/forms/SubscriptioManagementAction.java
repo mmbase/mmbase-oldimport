aa318955222cf881c1037d0bbd8d2840b54081ec
mfang <mfang@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-12-10 04:52:58 +0000

    CMSC-1126 using Assets in content(Move the ComparisonUtil.java back from Container)
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@32469 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer_Portlets/portlets-newsletter/src/java/com/finalist/newsletter/forms/SubscriptioManagementAction.java
+++ contributions/CMSContainer_Portlets/portlets-newsletter/src/java/com/finalist/newsletter/forms/SubscriptioManagementAction.java
@@ -2,7 +2,6 @@ package com.finalist.newsletter.forms;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
@@ -23,13 +22,13 @@ import org.springframework.web.struts.DispatchActionSupport;
 
 import com.finalist.cmsc.paging.PagingStatusHolder;
 import com.finalist.cmsc.paging.PagingUtils;
-import com.finalist.cmsc.util.ComparisonUtil;
 import com.finalist.cmsc.services.community.person.PersonService;
 import com.finalist.newsletter.domain.Newsletter;
 import com.finalist.newsletter.services.NewsletterPublicationService;
 import com.finalist.newsletter.services.NewsletterService;
 import com.finalist.newsletter.services.NewsletterSubscriptionServices;
 import com.finalist.newsletter.services.SubscriptionHibernateService;
+import com.finalist.newsletter.util.ComparisonUtil;
 
 
 /**
