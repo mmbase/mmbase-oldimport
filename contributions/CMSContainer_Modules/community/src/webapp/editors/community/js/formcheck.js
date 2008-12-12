4026f31182566d834022ee05cd78471937dc4135
nikkoyin <nikkoyin@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-12-12 06:22:47 +0000

    CMSC-949, Blocking users: user status enhancements,move javascript to jspf
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@32497 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer_Modules/community/src/webapp/editors/community/js/formcheck.js
+++ contributions/CMSContainer_Modules/community/src/webapp/editors/community/js/formcheck.js
@@ -23,25 +23,4 @@ function create() {
    document.forms[0].method.value = "addInit";
    document.forms[0].submit();
 }
-function selectState(url,ele){
-   surl=url+"&state="+ele.value
-   //	alert(surl);
-	window.location.href =surl;
-}
-function onState(){
-    var sels = document.getElementsByTagName("select");
-    for (i = 0; i < sels.length; i++) {
-        var sel = sels[i];
-        var options = sel.getElementsByTagName("option");
-        var v = options[0];
-        if ("" == v.value) {
-            options[0].parentNode.removeChild(options[0]);
-        }
-        for (j = 1; j < options.length; j++) {
-            if (v.value.toUpperCase() == options[j].value.toUpperCase()) {
-                options[j].selected=true;
-                options[0].parentNode.removeChild(options[0]);
-            }
-        }
-    }
-}
+
