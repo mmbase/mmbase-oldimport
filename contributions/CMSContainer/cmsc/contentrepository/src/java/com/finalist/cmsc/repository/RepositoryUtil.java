54a0a7cf9135eeb2d803fcd4a66bd790fd0dd7ae
evaguo <evaguo@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-12-22 06:13:48 +0000

    CMSC-1189(Modify the method which is used to get the Nodemanager.)
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@32645 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer/cmsc/contentrepository/src/java/com/finalist/cmsc/repository/RepositoryUtil.java
+++ contributions/CMSContainer/cmsc/contentrepository/src/java/com/finalist/cmsc/repository/RepositoryUtil.java
@@ -1329,23 +1329,15 @@ public final class RepositoryUtil {
    }
    public static int countLinkedElements(Node channel, List<String> contenttypes, String orderby, String direction, boolean useLifecycle, String archive, int offset, int maxNumber, int year, int month, int day,int maxDays){
       NodeQuery query = createLinkedContentQuery(channel, contenttypes, orderby, direction, useLifecycle, archive, offset, maxNumber, year, month, day);
-      String destinationManager = CONTENTELEMENT;
-      if (contenttypes != null && contenttypes.size() == 1) {
-         destinationManager = contenttypes.get(0);
-      }
       if(maxDays > 0){
-         SearchUtil.addDayConstraint(query, channel.getCloud().getNodeManager(destinationManager), ContentElementUtil.CREATIONDATE_FIELD, "-" + maxDays);
+         SearchUtil.addDayConstraint(query, query.getNodeManager(), ContentElementUtil.CREATIONDATE_FIELD, "-" + maxDays);
       }
       return Queries.count(query);
    }
    public static NodeList getLinkedElements(Node channel, List<String> contenttypes, String orderby, String direction, boolean useLifecycle, String archive, int offset, int maxNumber, int year, int month, int day, int maxDays) {
       NodeQuery query = createLinkedContentQuery(channel, contenttypes, orderby, direction, useLifecycle, archive, offset, maxNumber, year, month, day);
-      String destinationManager = CONTENTELEMENT;
-      if (contenttypes != null && contenttypes.size() == 1) {
-         destinationManager = contenttypes.get(0);
-      }
       if(maxDays > 0){
-         SearchUtil.addDayConstraint(query, channel.getCloud().getNodeManager(destinationManager), ContentElementUtil.CREATIONDATE_FIELD, "-" + maxDays);
+         SearchUtil.addDayConstraint(query, query.getNodeManager(), ContentElementUtil.CREATIONDATE_FIELD, "-" + maxDays);
       }
       return query.getNodeManager().getList(query);
    }
