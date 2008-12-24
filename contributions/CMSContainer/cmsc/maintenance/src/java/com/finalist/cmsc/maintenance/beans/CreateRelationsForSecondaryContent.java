f975212402beaa24c538ab269c82289219a5cdd5
jkoster <jkoster@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-12-24 14:16:58 +0000

    CMSC-1192 Assets: add SQL script to migrate all images/attachments/URLs to new assetstable: did some cleaning and improvements.
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@32712 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer/cmsc/maintenance/src/java/com/finalist/cmsc/maintenance/beans/CreateRelationsForSecondaryContent.java
+++ contributions/CMSContainer/cmsc/maintenance/src/java/com/finalist/cmsc/maintenance/beans/CreateRelationsForSecondaryContent.java
@@ -8,28 +8,32 @@ import org.mmbase.bridge.NodeQuery;
 import org.mmbase.bridge.Relation;
 
 import com.finalist.cmsc.mmbase.RelationUtil;
-import com.finalist.cmsc.navigation.ServerUtil;
 import com.finalist.cmsc.repository.RepositoryUtil;
 import com.finalist.cmsc.security.SecurityUtil;
 import com.finalist.cmsc.services.publish.Publish;
-import com.finalist.cmsc.services.workflow.Workflow;
+
+import org.mmbase.util.logging.Logger;
+import org.mmbase.util.logging.Logging;
+
 
 public class CreateRelationsForSecondaryContent {
 
-   private Cloud cloud ;
-   private Integer parentNumber ;
-   public CreateRelationsForSecondaryContent(Cloud cloud,Integer parentNumber) {
+   private Cloud cloud;
+   private Integer parentNumber;
+
+   /** MMbase logging system */
+   private static final Logger log = Logging.getLoggerInstance(CreateRelationsForSecondaryContent.class.getName());
+
+
+   public CreateRelationsForSecondaryContent(Cloud cloud, Integer parentNumber) {
       this.cloud = cloud;
       this.parentNumber = parentNumber;
    }
-   
-   public String execute() throws Exception {
-      createRelations();
-      return "success";
-   }
-   
-   private void createRelations()  throws Exception{
 
+   public String execute() throws Exception {
+      //Creating relations between assets and content channel
+      //Only process assets that don't have creationrels!
+      
       NodeManager assetManager = cloud.getNodeManager("assetelement");
       NodeManager ownerManager = cloud.getNodeManager("user");
       NodeQuery query = assetManager.createQuery();
@@ -37,45 +41,36 @@ public class CreateRelationsForSecondaryContent {
       Node root = null;
       if (parentNumber == null || parentNumber.intValue() < 1) {
          root = RepositoryUtil.getRootNode(cloud);
-      }
-      else {
+      } else {
          root = cloud.getNode(parentNumber);
       }
-      if(root == null) {
+      if (root == null || !RepositoryUtil.isChannel(root) ) {
          throw new Exception("Parent number must be refered to");
       }
       Node user = SecurityUtil.getUserNode(cloud);
       NodeList assets = query.getList();
       
-      for (int i = 0 ; i < assets.size() ; i++) {
+      log.info("Assets - Found total Assets: " + assets.size());
+      int counter = 0;
+      
+      for (int i = 0; i < assets.size(); i++) {
          Node asset = assets.getNode(i);
          if (!RepositoryUtil.hasCreationChannel(asset)) {
+            counter++;
             Relation relation = RelationUtil.createRelation(asset, root, "creationrel");
-            if(ServerUtil.isStaging() && !ServerUtil.isSingle()) {
-               if(Publish.isPublished(asset) && Publish.isPublishable(relation)) {
-                  Publish.publish(relation);
-               }
-               else {
-                  if (!Workflow.hasWorkflow(asset)) { 
-                     Workflow.create(asset, ""); 
-                  } 
-                  else { 
-                     Workflow.addUserToWorkflow(asset);
-                  }
-               }
-            }
+            Publish.publish(relation); // This method checks if it need to publish
+                                       // otherwise, it doesn't harm anyone
          }
+
          int owners = asset.countRelatedNodes(ownerManager, "ownerrel", "destination");
-         if (owners < 1) {  
+         if (owners < 1) {
             Relation relation = RelationUtil.createRelation(asset, user, "ownerrel");
-            if(ServerUtil.isStaging() && !ServerUtil.isSingle()) {
-               if(Publish.isPublished(asset) && Publish.isPublishable(relation)) {
-                  Publish.publish(relation);
-               }
-            }
+            Publish.publish(relation);
          }
       }
-      
+      log.info("Assets - Added relations to " + counter + " Asset elements.");
+  
+      return "success";
    }
 
 }
