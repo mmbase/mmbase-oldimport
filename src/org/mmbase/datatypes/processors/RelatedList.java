c2ca777a1e67ea0325163a1916a70e56c88f19db
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-12-30 12:38:59 +0000

    MMB-1762. This can be used as processers on virtual list fields (and it pretty useful then)
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@32769 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/datatypes/processors/RelatedList.java
@@ -0,0 +1,105 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.datatypes.processors;
+
+import org.mmbase.bridge.*;
+import org.mmbase.datatypes.*;
+import org.mmbase.util.*;
+import java.util.*;
+import org.mmbase.util.logging.*;
+
+/**
+ * The set- and get- processors implemented in this file can be used to make a virtual field which
+ * act as a list (or 'set')  field, but actually represent related nodes.
+ *
+ * In case just a selection of a limited number of other nodes is desired, this makes it easy to
+ * just create a multiple select for it.
+ *
+ * @author Michiel Meeuwissen
+ * @since MMBase-1.9.1
+ * @version $Id: RelatedList.java,v 1.1 2008-12-30 12:38:59 michiel Exp $
+ */
+
+public class RelatedList {
+
+    private static final Logger log = Logging.getLoggerInstance(RelatedList.class);
+
+    public abstract static class  AbstractProcessor implements Processor {
+
+        protected String role = "related";
+        protected String type = "object";
+        protected String searchDir = "destination";
+        public void setRole(String r) {
+            role = r;
+        }
+        public void setType(String t) {
+            type = t;
+        }
+        public void setSearchDir(String d) {
+            searchDir = d;
+        }
+    }
+
+    public static class Setter extends AbstractProcessor {
+
+        private static final long serialVersionUID = 1L;
+        public Object process(Node node, Field field, Object value) {
+            if (log.isDebugEnabled()) {
+                log.debug("Setting "  + value);
+            }
+            RelationList rl = node.getRelations(role, node.getCloud().getNodeManager(type), searchDir);
+
+            Cloud cloud = node.getCloud();
+            NodeList otherNodes   = cloud.getCloudContext().createNodeList();
+            NodeList relatedNodes = cloud.getCloudContext().createNodeList();
+            for (Object v : Casting.toList(value)) {
+                otherNodes.add(Casting.toNode(v, cloud));
+            }
+            relatedNodes.addAll(otherNodes);
+
+
+            for (Relation r : rl) {
+                Node source      = r.getSource();
+                Node destination = r.getDestination();
+                Node otherNode = source.equals(node) ? destination : source;
+                if (! relatedNodes.contains(otherNode)) {
+                    r.delete();
+                }
+                while(relatedNodes.remove(otherNode));
+            }
+            RelationManager rel = cloud.getRelationManager(node.getNodeManager(),
+                                                           cloud.getNodeManager(type),
+                                                           role);
+            for (Node otherNode : relatedNodes) {
+                if (node.isNew()) node.commit(); // Silly, but you cannot make relations to new nodes.
+                Relation newrel = node.createRelation(otherNode, rel);
+                newrel.commit();
+            }
+            return otherNodes;
+        }
+    }
+
+    public static class Getter extends AbstractProcessor {
+        private static final long serialVersionUID = 1L;
+
+        public Object process(Node node, Field field, Object value) {
+            if (log.isDebugEnabled()) {
+                log.debug("getting "  + node);
+            }
+            NodeList nl = node.getRelatedNodes(type, role, searchDir);
+            if (nl.size() == 0) {
+                return null;
+            } else {
+                return nl;
+            }
+        }
+    }
+
+}
