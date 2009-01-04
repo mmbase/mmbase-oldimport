d6e5b6b6af216bc19fbec7fbb01b67d8c426bd66
nklasens <nklasens@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-01-04 17:37:29 +0000

    stop when error occurred
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@32815 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer/scripts/buildall.bat
+++ contributions/CMSContainer/scripts/buildall.bat
@@ -9,5 +9,8 @@ GOTO end
 
 :okAppName
 	call build build cmsc ..\..\CMSContainer_Modules ..\..\CMSContainer_Portlets
+	:: stop when error occurred in loop
+	IF NOT "%ERRORLEVEL%" == "0" goto :EOF
+	
 	call build build %APPLICATION%
 :end
