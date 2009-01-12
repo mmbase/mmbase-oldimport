913d1f40bd1a5299ed64a4fb7b35d17a7281491e
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-01-12 21:10:52 +0000

    Added a renderer which can display more or less anything using an iframe.
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@33049 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/framework/IFrameRenderer.java
@@ -0,0 +1,77 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.framework;
+
+import java.util.*;
+import javax.servlet.http.*;
+import java.io.*;
+import org.mmbase.util.functions.*;
+import org.mmbase.util.logging.Logger;
+import org.mmbase.util.logging.Logging;
+
+/**
+ * Currently this generates a link to an editwizard list page.
+ *
+ * @todo Needs to produce a div, not an url.
+ *
+ * @author Michiel Meeuwissen
+ * @version $Id: IFrameRenderer.java,v 1.1 2009-01-12 21:10:52 michiel Exp $
+ * @since MMBase-1.9.1
+ */
+public class IFrameRenderer extends AbstractRenderer {
+    private static final Logger log = Logging.getLoggerInstance(IFrameRenderer.class);
+
+    protected final Map<String, Object> properties = new TreeMap<String, Object>();
+
+    private String url;
+
+    public IFrameRenderer(String t, Block parent) {
+        super(t, parent);
+    }
+
+    public void setProperty(String k, Object v) {
+        properties.put(k, v);
+    }
+
+    public void setUrl(String u) {
+        url = u;
+    }
+
+    protected String getStyleClass() {
+        return null;
+    }
+
+    protected String getIFrameUrl(Parameters blockParameters) {
+        HttpServletRequest request   = blockParameters.get(Parameter.REQUEST);
+        return org.mmbase.framework.basic.BasicUrlConverter.getUrl(url, properties, request, true);
+    }
+
+    public Parameter[] getParameters() {
+        return new Parameter[] {Parameter.RESPONSE, Parameter.REQUEST, Parameter.LOCALE};
+    }
+
+    /**
+     */
+    public void render(Parameters blockParameters, Writer w, RenderHints hints) throws FrameworkException {
+        try {
+            HttpServletRequest request   = blockParameters.get(Parameter.REQUEST);
+            HttpServletResponse response = blockParameters.get(Parameter.RESPONSE);
+            String sc = getStyleClass();
+            decorateIntro(hints, w, "iframe" + (sc == null ? "" : " " + sc));
+            String url = response.encodeUrl(request.getContextPath() + getIFrameUrl(blockParameters));
+            w.write("<iframe src=\"" + url + "\" />");
+            decorateOutro(hints, w);
+        } catch (IOException eio) {
+            throw new FrameworkException(eio.getMessage(), eio);
+        }
+    }
+
+
+}
