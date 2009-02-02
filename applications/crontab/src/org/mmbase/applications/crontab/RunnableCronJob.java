a9bdf220a6446ed55588d4afba2e90f8e4995293
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-02-02 11:01:44 +0000

    added a small feature to use runnables as cronjobs and sill be able to configure them
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@33315 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/crontab/src/org/mmbase/applications/crontab/RunnableCronJob.java
+++ applications/crontab/src/org/mmbase/applications/crontab/RunnableCronJob.java
@@ -7,14 +7,24 @@ See http://www.MMBase.org/license
  */
 package org.mmbase.applications.crontab;
 
+import java.util.*;
+import org.mmbase.util.logging.*;
+
+
 /**
  * Just wraps a Runnable into a CronJob.
  *
+ * Runnable can optionally be configured using 'bean' properties, where the key/values are read
+ * from the configuration string using {@link org.mmbase.util.StringSplitter#map}.
+ *
  * @author Michiel Meeuwissen
  * @since MMBase-1.8
+ * @version $Id: RunnableCronJob.java,v 1.3 2009-02-02 11:01:44 michiel Exp $
  */
 
 public class RunnableCronJob extends AbstractCronJob {
+
+    private static final Logger log = Logging.getLoggerInstance(RunnableCronJob.class);
 
     protected final Runnable runnable;
 
@@ -23,5 +33,18 @@ public class RunnableCronJob extends AbstractCronJob {
     }
     public void run() {
         runnable.run();
+    }
+
+    @Override protected void init() {
+        Map<String, String> config = org.mmbase.util.StringSplitter.map(cronEntry.getConfiguration());
+        for (Map.Entry<String, String> entry : config.entrySet()) {
+            try {
+                org.mmbase.util.xml.Instantiator.setProperty(entry.getKey(), runnable.getClass(), runnable, entry.getValue());
+                log.service("Set property " + entry + " on " + runnable);
+            } catch (Throwable t) {
+                log.error("For property " + entry + ": " + t.getMessage());
+            }
+        }
+
     }
 }
