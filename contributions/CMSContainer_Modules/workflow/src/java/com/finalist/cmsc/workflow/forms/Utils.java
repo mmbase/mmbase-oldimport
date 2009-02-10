830858d246b372d671b1614e5b0357ed0534a4d3
kevinshen <kevinshen@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-02-10 02:20:02 +0000

    CMSC-1287  Assets: sorting in Workflow window give stacktraces
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@33393 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer_Modules/workflow/src/java/com/finalist/cmsc/workflow/forms/Utils.java
+++ contributions/CMSContainer_Modules/workflow/src/java/com/finalist/cmsc/workflow/forms/Utils.java
@@ -2,21 +2,26 @@ package com.finalist.cmsc.workflow.forms;
 
 import javax.servlet.jsp.PageContext;
 
+import org.apache.commons.lang.StringUtils;
+
 public class Utils {
 
    public static String onClickandStyle(PageContext pageContext, String column) {
       String status = (String) pageContext.findAttribute("status");
       Boolean lastValue = (Boolean) pageContext.findAttribute("lastvalue");
       String orderby = (String) pageContext.findAttribute("orderby");
-
-      String template = "onclick=\"selectTab('%s','%s','%s')\" %s";
+      String workflowNodetype = (String) pageContext.findAttribute("workflowNodetype");
+      if (StringUtils.isEmpty(workflowNodetype)) {
+         workflowNodetype = "";
+      }
+      String template = "onclick=\"selectTab('%s','%s','%s','%s')\" %s";
 
       if ("undefined".equals(orderby)) {
-         return String.format(template, status, "lastmodifieddate", "true", "class=\"sortup\"").trim();
+         return String.format(template, status, workflowNodetype,"lastmodifieddate", "true", "class=\"sortup\"").trim();
       } else if (column.equals(orderby)) {
-         return String.format(template, status, column, lastValue, lastValue ? "class=\"sortup\"" : "class=\"sortdown\"");
+         return String.format(template, status, workflowNodetype,column, lastValue, lastValue ? "class=\"sortup\"" : "class=\"sortdown\"");
       } else {
-         return String.format(template, status, column, "false", "").trim();
+         return String.format(template, status, workflowNodetype,column, "false", "").trim();
       }
    }
 
