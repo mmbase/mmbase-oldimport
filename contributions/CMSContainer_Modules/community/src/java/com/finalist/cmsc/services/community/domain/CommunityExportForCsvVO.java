a4243643ccce31d3f3e5042a54ccd321a7bab5b7
evaguo <evaguo@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-02-23 12:51:38 +0000

    CMSC-1265 - Community: improve import and export options of community users
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@33625 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ contributions/CMSContainer_Modules/community/src/java/com/finalist/cmsc/services/community/domain/CommunityExportForCsvVO.java
@@ -0,0 +1,89 @@
+package com.finalist.cmsc.services.community.domain;
+
+public class CommunityExportForCsvVO {
+   private String authenticationId;
+   private String authenticationUserId;
+   private String authenticationPassword;
+   private String preferenceId;
+   private String preferenceModule;
+   private String preferenceKey;
+   private String preferenceValue;
+   private String firstName;
+   private String lastName;
+   private String infix;
+   private String email;
+   private String active;
+   public String getAuthenticationId() {
+      return authenticationId;
+   }
+   public void setAuthenticationId(String authenticationId) {
+      this.authenticationId = authenticationId;
+   }
+   public String getAuthenticationUserId() {
+      return authenticationUserId;
+   }
+   public void setAuthenticationUserId(String authenticationUserId) {
+      this.authenticationUserId = authenticationUserId;
+   }
+   public String getAuthenticationPassword() {
+      return authenticationPassword;
+   }
+   public void setAuthenticationPassword(String authenticationPassword) {
+      this.authenticationPassword = authenticationPassword;
+   }
+   public String getPreferenceId() {
+      return preferenceId;
+   }
+   public void setPreferenceId(String preferenceId) {
+      this.preferenceId = preferenceId;
+   }
+   public String getPreferenceModule() {
+      return preferenceModule;
+   }
+   public void setPreferenceModule(String preferenceModule) {
+      this.preferenceModule = preferenceModule;
+   }
+   public String getPreferenceKey() {
+      return preferenceKey;
+   }
+   public void setPreferenceKey(String preferenceKey) {
+      this.preferenceKey = preferenceKey;
+   }
+   public String getPreferenceValue() {
+      return preferenceValue;
+   }
+   public void setPreferenceValue(String preferenceValue) {
+      this.preferenceValue = preferenceValue;
+   }
+   public String getFirstName() {
+      return firstName;
+   }
+   public void setFirstName(String firstName) {
+      this.firstName = firstName;
+   }
+   public String getLastName() {
+      return lastName;
+   }
+   public void setLastName(String lastName) {
+      this.lastName = lastName;
+   }
+   public String getInfix() {
+      return infix;
+   }
+   public void setInfix(String infix) {
+      this.infix = infix;
+   }
+   public String getEmail() {
+      return email;
+   }
+   public void setEmail(String email) {
+      this.email = email;
+   }
+   public String getActive() {
+      return active;
+   }
+   public void setActive(String active) {
+      this.active = active;
+   }
+   
+}
