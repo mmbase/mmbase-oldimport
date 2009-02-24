1e92b2383decd25d91ccf91b8a15c11b8f374619
kevinshen <kevinshen@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-02-24 10:55:28 +0000

    CMSC-1298 Clone channel feature   strip the nodes that not in the current channel tree
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@33639 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ contributions/CMSContainer/cmsc/contentrepository/src/java/com/finalist/cmsc/repository/GetRichTextDocumentParameter.java
@@ -0,0 +1,9 @@
+package com.finalist.cmsc.repository;
+
+public class GetRichTextDocumentParameter {
+   public String in;
+
+   public GetRichTextDocumentParameter(String in) {
+      this.in = in;
+   }
+}
