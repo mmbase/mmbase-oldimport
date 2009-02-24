c907f1b579baed5ade044eb2a2ebc6dbd4647c69
kevinshen <kevinshen@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-02-24 03:37:36 +0000

    CMSC-1178  Newsletter: add mechanism to use restrictions in the newsletter edition workflow   add code to check if it is in single mode
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@33634 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer_Portlets/portlets-newsletter/src/java/com/finalist/newsletter/forms/NewsletterEditionFreeze.java
+++ contributions/CMSContainer_Portlets/portlets-newsletter/src/java/com/finalist/newsletter/forms/NewsletterEditionFreeze.java
@@ -7,14 +7,15 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.mmbase.bridge.Node;
+import org.mmbase.util.logging.Logger;
+import org.mmbase.util.logging.Logging;
 
 import com.finalist.cmsc.services.publish.Publish;
 import com.finalist.cmsc.services.workflow.Workflow;
 import com.finalist.cmsc.services.workflow.WorkflowException;
+import com.finalist.cmsc.util.ServerUtil;
 import com.finalist.newsletter.domain.EditionStatus;
 import com.finalist.newsletter.util.NewsletterPublicationUtil;
-import org.mmbase.util.logging.Logger;
-import org.mmbase.util.logging.Logging;
 
 public class NewsletterEditionFreeze extends NewsletterEditionAction{
    private static final String ERRORS = "errors";
@@ -23,26 +24,31 @@ public class NewsletterEditionFreeze extends NewsletterEditionAction{
    @Override
    protected void doSave(HttpServletRequest request, Node edition) throws Exception {
       if (!EditionStatus.FROZEN.value().equals(edition.getValue("process_status"))) {
-         if (Publish.isPublished(edition)) {
+         if(ServerUtil.isSingle()) {
             NewsletterPublicationUtil.freezeEdition(edition);
-         } else {
-            try {
-               Workflow.publish(edition);
-            }
-            catch (WorkflowException wfe) {
-               List<Node> errors = wfe.getErrors();
-               request.getSession().setAttribute(ERRORS, edition.getValue("title"));
-               for (Node errorNode : errors) {
-                  log.error(errorNode.getNodeManager().getName() + " " + errorNode.getNumber() + " ");
-               }        
-               if (Workflow.isAcceptedStepEnabled()) {
-                  Workflow.accept(edition, "");
+         }
+         else {
+            if (Publish.isPublished(edition)) {
+               NewsletterPublicationUtil.freezeEdition(edition);
+            } else {
+               try {
+                  Workflow.publish(edition);
                }
-               else {
-                  Workflow.finish(edition, "");
+               catch (WorkflowException wfe) {
+                  List<Node> errors = wfe.getErrors();
+                  request.getSession().setAttribute(ERRORS, edition.getValue("title"));
+                  for (Node errorNode : errors) {
+                     log.error(errorNode.getNodeManager().getName() + " " + errorNode.getNumber() + " ");
+                  }        
+                  if (Workflow.isAcceptedStepEnabled()) {
+                     Workflow.accept(edition, "");
+                  }
+                  else {
+                     Workflow.finish(edition, "");
+                  }
                }
+               request.getSession().setAttribute(NEEDAJAX, edition.getNumber());
             }
-            request.getSession().setAttribute(NEEDAJAX, edition.getNumber());
          }
       } 
    }
