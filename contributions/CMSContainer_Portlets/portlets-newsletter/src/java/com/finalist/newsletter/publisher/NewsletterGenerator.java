df65e9bbdb41f001302f11181c84d6d7d1019b75
jkoster <jkoster@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-03-04 17:21:32 +0000

    CMSC-1187 - Improve sending and retrieving HTML part. Better and faster code.
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@33747 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer_Portlets/portlets-newsletter/src/java/com/finalist/newsletter/publisher/NewsletterGenerator.java
+++ contributions/CMSContainer_Portlets/portlets-newsletter/src/java/com/finalist/newsletter/publisher/NewsletterGenerator.java
@@ -1,10 +1,11 @@
 package com.finalist.newsletter.publisher;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import javax.mail.MessagingException;
 
 import org.htmlparser.Parser;
 import org.htmlparser.util.ParserException;
@@ -19,19 +20,19 @@ public class NewsletterGenerator {
    private static Logger log = Logging.getLoggerInstance(NewsletterGenerator.class.getName());
 
 
-   public static String generate(String urlPath, String mimeType) throws MessagingException {
+   public static String generate(String urlPath, String mimeType) {
 
       log.debug("generate newsletter from url:" + urlPath);
 
       String inputString = "";
-      String errorInfo="please check the system live-path variable, ";
-      try {
+      String errorInfo = "please check the system live-path variable, ";
 
+      try {
          log.debug("Try to get content from URL:" + urlPath);
 
          URL url = new URL(urlPath);
          if (urlPath.indexOf("http") < 0) {
-            errorInfo += "the path does not start with http:// ";
+            errorInfo += "the path does not start with http: ";
          }
          HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
@@ -39,28 +40,31 @@ public class NewsletterGenerator {
          connection.setDoInput(true);
          connection.setRequestProperty("Content-Type", mimeType);
 
-         Reader reader = new InputStreamReader(connection.getInputStream());
+         BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
-         StringBuffer buffer = new StringBuffer();
+         StringBuilder buffer = new StringBuilder();
 
-         int c;
-         while ((c = reader.read()) != -1) {
-            char character = (char) c;
-            buffer.append("").append(character);
+         String line;
+         while ((line = in.readLine()) != null) {
+            buffer.append(line);
          }
-
-         reader.close();
+         in.close();
 
          inputString = buffer.toString().trim();
 
-         if ("text/plain".equals(mimeType)) {
+         if ("text/plain".equalsIgnoreCase(mimeType)) {
             inputString = getContentFromPage(inputString);
          }
          inputString = NewsletterUtil.calibrateRelativeURL(inputString);
 
          return (inputString);
-      } catch (Exception e) {
-         log.error("Error when try to get content from" + urlPath+errorInfo, e);
+         
+      } catch (FileNotFoundException e) {
+         log.error("Error when try to get content from:" + urlPath+errorInfo, e);
+         
+      } catch (IOException e) {
+         e.printStackTrace();
+         log.error("Error when reading input data from URL:" + urlPath+errorInfo, e);
       }
 
       return inputString;
@@ -82,7 +86,6 @@ public class NewsletterGenerator {
       inputString = inputString.replaceAll("(?m)^\\s*\r\n+", "").replaceAll("(?m)^\\s*\r+", "").replaceAll("(?m)^\\s*\n+", "");
       inputString = inputString.replaceAll("(?m)\r\n+", "").replaceAll("(?m)\r+", "").replaceAll("(?m)\n+", "");
       inputString = inputString.replaceAll("<br/>", "\r\n");
-
 
       return inputString;
    }
