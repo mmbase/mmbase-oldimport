4032117451fd21cf2a465a84150f8bf687c43eea
fpunt <fpunt@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-03-04 15:16:00 +0000

    CMSC-1304 Backfixed the stuff I fixed in the 1.5 branch
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@33742 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer_Modules/glossary/src/java/com/finalist/cmsc/module/glossary/Glossary.java
+++ contributions/CMSContainer_Modules/glossary/src/java/com/finalist/cmsc/module/glossary/Glossary.java
@@ -7,7 +7,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Glossary {
-   public static final String LINKPATTERN = "<span class=\"glossaryWord\" title=\"%s\" id=\"_glossary_%s\">%s</span>";
+   public static final String LINKPATTERN = "<dfn class=\"glossaryWord\" title=\"%2$s - %s\" id=\"_glossary_%2$s\">%2$s</dfn>";
    public static final String GLOSSARY = "glossary";
    private final Map<String, String> terms = new HashMap<String, String>();
    private static Glossary glossary = null;
@@ -20,7 +20,7 @@ public class Glossary {
       terms.remove(term);
    }
 
-
+   
    public String mark(String material) {
       Set<String> keyWords = terms.keySet();
 
@@ -34,7 +34,7 @@ public class Glossary {
             int end = matcher.end();
             String found = material.substring(start, end);
             if (!isInFormatedFragment(material, word, start)) {
-               String highlight = String.format(LINKPATTERN, terms.get(word), found, found);
+               String highlight = String.format(LINKPATTERN, terms.get(word), found);
                material = (new StringBuilder()).append(material.substring(0, start)).append(highlight).append(
                      material.substring(end, material.length())).toString();
                break;
@@ -46,20 +46,24 @@ public class Glossary {
    }
 
 
-   private boolean isInFormatedFragment(String material, String keywords, int keywordStartPosition) {
-      Pattern pattern = Pattern.compile(String.format("<[abh][^</]*%s[^>]*>", keywords));
-      Matcher matcher = pattern.matcher(material);
-
-      while (matcher.find()) {
-         int start = matcher.start();
-         int end = matcher.end();
-
-         if (keywordStartPosition + keywords.length() < end && keywordStartPosition > start)
-            return true;
+   private boolean isInFormatedFragment(String material, String keyword, int keywordStartPosition) {
+      String materialBefore = material.substring(0, keywordStartPosition);
+      
+      int openA = materialBefore.lastIndexOf("<a");
+      int closeA = materialBefore.lastIndexOf("/a>");
+      if(openA > closeA) {
+         return true;
       }
+
+      int openInput = materialBefore.lastIndexOf("<input");
+      int closeInput = materialBefore.lastIndexOf(">");
+      if(openInput > closeInput) {
+         return true;
+      }
+
       return false;
    }
-
+   
 
    public static synchronized Glossary instance() {
       if (null == glossary) {
