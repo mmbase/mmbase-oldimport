f70b296908540c118d698d43ee86f53d4755fd6f
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-03-25 21:42:04 +0000

    communicate the hints to EL
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@33976 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/framework/basic/BasicFramework.java
+++ src/org/mmbase/framework/basic/BasicFramework.java
@@ -33,7 +33,7 @@ import org.w3c.dom.NodeList;
  * are configured is the order in which they are processed.
  *
  * @author Michiel Meeuwissen
- * @version $Id: BasicFramework.java,v 1.44 2009-01-16 22:21:13 michiel Exp $
+ * @version $Id: BasicFramework.java,v 1.45 2009-03-25 21:42:04 michiel Exp $
  * @since MMBase-1.9
  */
 public class BasicFramework extends Framework {
@@ -222,6 +222,7 @@ public class BasicFramework extends Framework {
             state = new State(request);
         }
 
+        Object prevHints = request.getAttribute("org.mmbase.framework.hints");
         try {
 
             request.setAttribute(COMPONENT_CLASS_KEY, getComponentClass());
@@ -251,7 +252,9 @@ public class BasicFramework extends Framework {
             setBlockParametersForRender(state, blockParameters);
 
             RenderHints hints = new RenderHints(actualRenderer, windowState, state.getId(), getComponentClass());
+            request.setAttribute("org.mmbase.framework.hints", hints);
             actualRenderer.render(blockParameters, w, hints);
+            request.setAttribute("org.mmbase.framework.hints", hints);
         } catch (FrameworkException fe) {
             log.debug(fe);
             URI uri = renderer.getUri();
@@ -259,6 +262,8 @@ public class BasicFramework extends Framework {
             RenderHints hints = new RenderHints(error, windowState, state.getId(), getComponentClass());
             error.render(blockParameters, w, hints);
         } finally {
+            request.setAttribute("org.mmbase.framework.hints", prevHints);
+
             state.endBlock();
         }
     }
