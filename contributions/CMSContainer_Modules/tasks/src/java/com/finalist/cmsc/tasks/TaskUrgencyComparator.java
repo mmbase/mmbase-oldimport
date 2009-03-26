22b21a943e04c3e5aed2992bcdc82aab4bc9ccde
mfang <mfang@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-03-26 03:15:07 +0000

    CMSC-241 Show top 5 most urgent tasks on dashboard
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@33981 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer_Modules/tasks/src/java/com/finalist/cmsc/tasks/TaskUrgencyComparator.java
+++ contributions/CMSContainer_Modules/tasks/src/java/com/finalist/cmsc/tasks/TaskUrgencyComparator.java
@@ -11,7 +11,7 @@ import org.mmbase.bridge.Node;
 * 
  * @author Marco
  */
-public class TaskUrgencyComparator implements Comparator{
+public class TaskUrgencyComparator implements Comparator<Node>{
    
    private static final String STATUS = "status";
    private static final String DEADLINE = "deadline";
@@ -20,13 +20,13 @@ public class TaskUrgencyComparator implements Comparator{
    private static final String NOTIFIED = "task.status.notified";
    private static final String DONE = "task.status.done";
    
-   public int compare(Object o1, Object o2) {
-      String status1 = ((Node) o1).getStringValue(STATUS); 
-      String status2 = ((Node) o2).getStringValue(STATUS);
+   public int compare(Node o1, Node o2) {
+      String status1 = o1.getStringValue(STATUS); 
+      String status2 = o2.getStringValue(STATUS);
       if (status1.equals(INIT) && status2.equals(INIT) || 
          status1.equals(NOTIFIED) && status2.equals(NOTIFIED) || 
          status1.equals(DONE) && status2.equals(DONE)) {
-         return ((Node) o1).getStringValue(DEADLINE).compareTo(((Node) o2).getStringValue(DEADLINE));
+         return o1.getDateValue(DEADLINE).compareTo(o2.getDateValue(DEADLINE));
       }
       else if (status1.equals(INIT) && status2.equals(NOTIFIED) ||
              status1.equals(INIT) && status2.equals(DONE) ||
