a70c8a8fa2e4646e5f03a75899d68735a041dd0a
andre <andre@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-04-17 13:48:59 +0000

    *** empty log message ***
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@34289 91430c5f-d668-0410-9e7d-dd663366dcfd

--- speeltuin/andre/mmsite/src/org/mmbase/mmsite/SiteUrlConverter.java
+++ speeltuin/andre/mmsite/src/org/mmbase/mmsite/SiteUrlConverter.java
@@ -19,29 +19,39 @@ import org.mmbase.util.logging.*;
 /**
  * The UrlConverter that can filter and create urls for pages in the site application.
  * It can be used as a '/' (root) UrlConverter. Use 'excludedPaths' to list directories to
- * exclude that might get mixed up and are not mentioned in web.xml.
- * It presumes (pages) nodes with the fields 'path' and 'template'.
- *
- * @author Andre van Toly
- * @version $Id: SiteUrlConverter.java,v 1.2 2009-04-17 12:16:57 andre Exp $
+ * exclude that might get mixed up with this one and are not mentioned in web.xml.
+ * It presumes (pages) nodes with the fields 'path' and 'template'.<br />
+ * <br />
+ * &lt;urlconverter class="org.mmbase.mmsite.SiteUrlConverter"&gt;<br />
+ *   &lt;description xml:lang="en"&gt;UrlConverter for MMSite&lt;/description&gt;<br />
+ *   &lt;param name="directory"&gt;/&lt;/param&gt;<br />
+ *   &lt;param name="excludedPaths"&gt;mmbase,mmexamples&lt;/param&gt;<br />
+ *   &lt;param name="useExtension"&gt;true&lt;/param&gt;<br />
+ *   &lt;param name="extension"&gt;.html&lt;/param&gt;<br />
+ * &lt;/urlconverter&gt;
+ * 
+ * @author Andr&eacute; van Toly
+ * @version $Id: SiteUrlConverter.java,v 1.3 2009-04-17 13:48:59 andre Exp $
  * @since MMBase-1.9
  */
 public class SiteUrlConverter extends DirectoryUrlConverter {
     private static final Logger log = Logging.getLoggerInstance(SiteUrlConverter.class);
 
-    /* paths that should always be excluded (since this uc listens to all on '/') */
- 	protected static ArrayList<String> excludedPaths = new ArrayList(Arrays.asList(
- 	    "mmbase"
- 	));
-    protected static String extension = "";
+ 	protected static ArrayList<String> excludedPaths = new ArrayList();
+    protected static String extension = null;
     protected static boolean useExtension = false;
 
     public SiteUrlConverter(BasicFramework fw) {
         super(fw);
         setDirectory("/");
-        addComponent(ComponentRepository.getInstance().getComponent("site"));
+        addComponent(ComponentRepository.getInstance().getComponent("mmsite"));
     }
 
+    public void setExcludedPaths(String l) {
+        String[] sa = l.split(",");
+        excludedPaths = new ArrayList(Arrays.asList(sa));
+    }
+    
     public void setUseExtension(boolean t) {
         useExtension = t;
     }
