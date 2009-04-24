c75702bb204ac64ab2126bc7cffd78ec85adf92b
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-04-24 12:15:20 +0000

    Indentation (with 4 spaces, not 3). Javadoc. Using nio if copying between files
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@34408 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/util/IOUtil.java
+++ src/org/mmbase/util/IOUtil.java
@@ -10,66 +10,100 @@ See http://www.MMBase.org/license
 package org.mmbase.util;
 
 import java.io.*;
+import java.nio.channels.*;
+
+import org.mmbase.util.logging.*;
+
+/**
+ * Various utils to consisely and efficiently deal with streams
+ * @since MMBase-1.9.1
+ * @version $Id: IOUtil.java,v 1.3 2009-04-24 12:15:20 michiel Exp $
+ */
 
 public final class IOUtil {
 
-   private static final int DEFAULT_BUFFER_SIZE = 1024 * 4;
+    private static final Logger log = Logging.getLoggerInstance(IOUtil.class);
 
-   private IOUtil() {
-      // nothing
-   }
+    private static final int DEFAULT_BUFFER_SIZE = 1024 * 4;
 
-   /**
-    * Copy bytes from an <code>InputStream</code> to an <code>OutputStream</code>.
-    */
-   public static long copy(final InputStream input, final OutputStream output) throws IOException {
-      return copy(input, output, DEFAULT_BUFFER_SIZE);
-   }
+    private IOUtil() {
+        // nothing, only static methods.
+    }
 
-   /**
-    * Copy bytes from an <code>InputStream</code> to an <code>OutputStream</code>.
-    * 
-    * @param bufferSize
-    *           Size of internal buffer to use.
-    */
-   public static long copy(final InputStream input, final OutputStream output, final int bufferSize)
-         throws IOException {
-      long size = 0;
-      final byte[] buffer = new byte[bufferSize];
-      int n = 0;
-      while (-1 != (n = input.read(buffer))) {
-         output.write(buffer, 0, n);
-         size += n;
-      }
-      
-      return size;
-   }
+    /**
+     * Copy bytes from an <code>InputStream</code> to an <code>OutputStream</code>.
+     */
+    public static long copy(final InputStream input, final OutputStream output) throws IOException {
+        if (input instanceof FileInputStream && output instanceof FileOutputStream) {
+            log.debug("Streams are for files, using nio.");
+            return copy((FileInputStream) input, (FileOutputStream) output);
+        } else {
+            return copy(input, output, DEFAULT_BUFFER_SIZE);
+        }
+    }
 
-   /**
-    * Copy chars from a <code>Reader</code> to a <code>Writer</code>.
-    */
-   public static long copy(final Reader input, final Writer output) throws IOException {
-      return copy(input, output, DEFAULT_BUFFER_SIZE);
-   }
+    public static long copy(final FileInputStream input, final FileOutputStream output) throws IOException {
+        FileChannel source = null;
+        FileChannel destination = null;;
+        try {
+            source = input.getChannel();
+            destination = output.getChannel();
+            log.debug("Copying " + source.size() + " bytes");
+            destination.transferFrom(source, 0, source.size());
+            return source.size();
+        } finally {
+            if(source != null) {
+                source.close();
+            }
+            if(destination != null) {
+                destination.close();
+            }
+        }
+    }
 
-   /**
-    * Copy chars from a <code>Reader</code> to a <code>Writer</code>.
-    * 
-    * @param bufferSize
-    *           Size of internal buffer to use.
-    */
-   public static long copy(final Reader input, final Writer output, final int bufferSize)
-         throws IOException {
-      long size = 0;
-      final char[] buffer = new char[bufferSize];
-      int n = 0;
-      while (-1 != (n = input.read(buffer))) {
-         output.write(buffer, 0, n);
-         size += n;
-      }
-      output.flush();
-      
-      return size;
-   }
+
+    /**
+     * Copy bytes from an <code>InputStream</code> to an <code>OutputStream</code>.
+     *
+     * @param bufferSize
+     *           Size of internal buffer to use.
+     */
+    public static long copy(final InputStream input, final OutputStream output, final int bufferSize) throws IOException {
+        long size = 0;
+        final byte[] buffer = new byte[bufferSize];
+        int n = 0;
+        while (-1 != (n = input.read(buffer))) {
+            output.write(buffer, 0, n);
+            size += n;
+        }
+
+        return size;
+    }
+
+    /**
+     * Copy chars from a <code>Reader</code> to a <code>Writer</code>.
+     */
+    public static long copy(final Reader input, final Writer output) throws IOException {
+        return copy(input, output, DEFAULT_BUFFER_SIZE);
+    }
+
+    /**
+     * Copy chars from a <code>Reader</code> to a <code>Writer</code>.
+     *
+     * @param bufferSize
+     *           Size of internal buffer to use.
+     */
+    public static long copy(final Reader input, final Writer output, final int bufferSize) throws IOException {
+        long size = 0;
+        final char[] buffer = new char[bufferSize];
+        int n = 0;
+        while (-1 != (n = input.read(buffer))) {
+            output.write(buffer, 0, n);
+            size += n;
+        }
+        output.flush();
+
+        return size;
+    }
 
 }
