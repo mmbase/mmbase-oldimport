f5df3625f3b405beda0888901e290dbf9c8e88ee
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-04-28 14:06:54 +0000

    ConfirmPassword can now be a straigh-forward extension of CheckEqualityDataTye
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@34507 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/org/mmbase/datatypes/ConfirmPasswordDataType.java
+++ src/org/mmbase/datatypes/ConfirmPasswordDataType.java
@@ -20,98 +20,19 @@ import org.mmbase.util.logging.*;
  * only sense as a field of a node).
  *
  * @author Michiel Meeuwissen
- * @version $Id: ConfirmPasswordDataType.java,v 1.14 2007-06-21 07:32:31 pierre Exp $
+ * @version $Id: ConfirmPasswordDataType.java,v 1.15 2009-04-28 14:06:54 michiel Exp $
  * @since MMBase-1.8
  */
-public class ConfirmPasswordDataType extends StringDataType {
-    private static final Logger log = Logging.getLoggerInstance(ConfirmPasswordDataType.class);
-
-    private static final long serialVersionUID = 1L; // increase this if object serialization changes (which we shouldn't do!)
-
-    protected PasswordRestriction  passwordRestriction =  new PasswordRestriction("password");
-
+public class ConfirmPasswordDataType extends CheckEqualityDataType {
     /**
      * Constructor for string data type.
      * @param name the name of the data type
      */
     public ConfirmPasswordDataType(String name) {
         super(name);
+        setField("password");
+        setPassword(true);
     }
 
-    protected void inheritRestrictions(BasicDataType origin) {
-        super.inheritRestrictions(origin);
-        if (origin instanceof ConfirmPasswordDataType) {
-            ConfirmPasswordDataType dataType = (ConfirmPasswordDataType) origin;
-            passwordRestriction.inherit(dataType.passwordRestriction);
-        }
-    }
-    protected void cloneRestrictions(BasicDataType origin) {
-        super.cloneRestrictions(origin);
-        if (origin instanceof ConfirmPasswordDataType) {
-            ConfirmPasswordDataType dataType = (ConfirmPasswordDataType) origin;
-            passwordRestriction = new PasswordRestriction(dataType.passwordRestriction);
-        }
-    }
-
-    public int getEnforceStrength() {
-        return Math.max(super.getEnforceStrength(), passwordRestriction.getEnforceStrength());
-    }
-
-    protected Collection<LocalizedString> validateCastValue(Collection<LocalizedString> errors, Object castValue, Object value, Node node, Field field) {
-        errors = super.validateCastValue(errors, castValue, value, node, field);
-        errors = passwordRestriction.validate(errors, castValue, node, field);
-        return errors;
-    }
-    /**
-     * The field property is the name of the other password field that this fields 'confirms'. It default to 'password'.
-     * In datatype XML it can be set with the generic &lt;property name="field" value="..." /&gt;
-     */
-    public void setField(String field) {
-        passwordRestriction.setValue(field);
-    }
-    /**
-     * Returns the name of the field which is 'confirmed' by this datatype.
-     */
-    public String getField() {
-        edit();
-        return passwordRestriction.getField();
-    }
-
-    protected StringBuilder toStringBuilder() {
-        StringBuilder buf = super.toStringBuilder();
-        buf.append(" confirm(").append(passwordRestriction.getValue()).append(")");
-        return buf;
-    }
-
-    protected class PasswordRestriction extends AbstractRestriction {
-        PasswordRestriction(PasswordRestriction source) {
-            super(source);
-        }
-        PasswordRestriction(String field) {
-            super("confirmpassword", field);
-        }
-        protected final String getField() {
-            return (String) value;
-        }
-
-        protected boolean simpleValid(final Object v, final Node node, final Field field) {
-            if (node != null && field != null && v != null) {
-                if (! node.isChanged(getField())) return true;
-
-                Field passwordField = node.getNodeManager().getField(getField());
-                Processor setProcessor = passwordField.getDataType().getProcessor(PROCESS_SET);
-                Object processedValue = setProcessor.process(node, field, v);
-                String passwordValue = (String) node.getObjectValue(getField());
-                if (log.isDebugEnabled()) {
-                    log.debug("Password checking " + (node.isNew() ? "new" : "existing") + " node. Password field " + passwordField + " set-processor " + setProcessor);
-                    log.debug("Offered value '" + v + "' --> '" + processedValue);
-                    log.debug("Comparing '" + passwordValue + "' with '" + processedValue + "'(" + v + ")");
-                }
-                return passwordValue.equals(v) || passwordValue.equals(processedValue);
-            } else {
-                return true;
-            }
-        }
-    }
 
 }
