daf182b4cefd51f3d2e3cb0141551c3bfb887b79
nklasens <nklasens@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-04-29 13:14:22 +0000

    Catch Node not found exceptions
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@34542 91430c5f-d668-0410-9e7d-dd663366dcfd

--- contributions/CMSContainer_Modules/language-redirect/src/java/com/finalist/cmsc/languageredirect/LanguageRedirectUtil.java
+++ contributions/CMSContainer_Modules/language-redirect/src/java/com/finalist/cmsc/languageredirect/LanguageRedirectUtil.java
@@ -6,13 +6,7 @@ import net.sf.mmapps.modules.cloudprovider.CloudProviderFactory;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.mmbase.bridge.Cloud;
-import org.mmbase.bridge.Field;
-import org.mmbase.bridge.Node;
-import org.mmbase.bridge.NodeIterator;
-import org.mmbase.bridge.NodeList;
-import org.mmbase.bridge.NodeManager;
-import org.mmbase.bridge.NodeQuery;
+import org.mmbase.bridge.*;
 import org.mmbase.bridge.util.SearchUtil;
 import org.mmbase.storage.search.CompositeConstraint;
 import org.mmbase.storage.search.Constraint;
@@ -41,85 +35,90 @@ public class LanguageRedirectUtil {
          return null;
       }
       
-      // Get the node with the id given in the id parameter.
-      Node pageOrSiteNode = cloud.getNode(id);
-      NodeManager nodeManager = pageOrSiteNode.getNodeManager();
-      log.debug("The nodemanager of the current node's name is: " + nodeManager.getName());
+      try {
       
-      // Is the current Node a page or a site?    
-      if (PagesUtil.isPage(pageOrSiteNode)) {
-         log.debug("The current node is a page(id: " + pageOrSiteNode.getNumber() + ").");         
-         // Look for a page that has the same liname as the currentpage. Do this
-         // with a query on the database otherwise we would have to loop through
-         // the pages of the found sites to find the page ourselves.
-         Field liField = nodeManager.getField("liname");
-         NodeList pageList = getCorrespondingPages(pageOrSiteNode, liField, nodeManager);
-         while (pageList == null || pageList.size() == 0){           
-            /*
-             * If no page was found and the parent is not a site, get the parent
-             * of the current node and find the corresponding page for that
-             * site. If the parent is a site, go to the corresponding site
-             * instead.
-            */             
-            NodeList parentList = pageOrSiteNode.getRelatedNodes(nodeManager, NavigationUtil.NAVREL, "SOURCE");         
-            if (parentList.size() > 0) {
-               // The parentList should only contain one item since a page can
-               // always only have one source.
-               pageOrSiteNode = parentList.getNode(0);
-               if (SiteUtil.isSite(pageOrSiteNode)) {
-                  // The parent is a site, redirect to the site that corresponds
-                  // to the language given.
-                  return foreignSite.getStringValue(SiteUtil.FRAGMENT_FIELD);
+         // Get the node with the id given in the id parameter.
+         Node pageOrSiteNode = cloud.getNode(id);
+         NodeManager nodeManager = pageOrSiteNode.getNodeManager();
+         log.debug("The nodemanager of the current node's name is: " + nodeManager.getName());
+         
+         // Is the current Node a page or a site?    
+         if (PagesUtil.isPage(pageOrSiteNode)) {
+            log.debug("The current node is a page(id: " + pageOrSiteNode.getNumber() + ").");         
+            // Look for a page that has the same liname as the currentpage. Do this
+            // with a query on the database otherwise we would have to loop through
+            // the pages of the found sites to find the page ourselves.
+            Field liField = nodeManager.getField("liname");
+            NodeList pageList = getCorrespondingPages(pageOrSiteNode, liField, nodeManager);
+            while (pageList == null || pageList.size() == 0){           
+               /*
+                * If no page was found and the parent is not a site, get the parent
+                * of the current node and find the corresponding page for that
+                * site. If the parent is a site, go to the corresponding site
+                * instead.
+               */             
+               NodeList parentList = pageOrSiteNode.getRelatedNodes(nodeManager, NavigationUtil.NAVREL, "SOURCE");         
+               if (parentList.size() > 0) {
+                  // The parentList should only contain one item since a page can
+                  // always only have one source.
+                  pageOrSiteNode = parentList.getNode(0);
+                  if (SiteUtil.isSite(pageOrSiteNode)) {
+                     // The parent is a site, redirect to the site that corresponds
+                     // to the language given.
+                     return foreignSite.getStringValue(SiteUtil.FRAGMENT_FIELD);
+                  }
                }
-            }
-            else {
-               // There is no parent. 
-               log.error("Page with number: " + pageOrSiteNode.getFieldValue("number") + ", doesn't have a parent.");
-               return null;
-            }
+               else {
+                  // There is no parent. 
+                  log.error("Page with number: " + pageOrSiteNode.getFieldValue("number") + ", doesn't have a parent.");
+                  return null;
+               }
+               
+               pageList = getCorrespondingPages(pageOrSiteNode, liField, nodeManager);             
+               }
             
-            pageList = getCorrespondingPages(pageOrSiteNode, liField, nodeManager);             
+            /*
+             * We now have a list of pages that have the same liname as the page
+             * that was given as the id in the request or we have a list of pages
+             * that have the same liname as one of the parents of the page that was
+             * given as the id in the request. Find the site of this page and if it
+             * corresponds with the site found previously, redirect to that page.
+             */
+            if (pageList != null && pageList.size() > 0) {
+                LinkedHashMap<String, String> treeManagers = new LinkedHashMap<String, String>();
+                treeManagers.put(SiteUtil.SITE, SiteUtil.FRAGMENT_FIELD);
+                treeManagers.put(PagesUtil.PAGE, PagesUtil.FRAGMENT_FIELD);
+                
+                NodeIterator pageIt = pageList.nodeIterator();
+               while (pageIt.hasNext()) {
+                  Node curPage = (Node)pageIt.next();
+                  String path = TreeUtil.getPathToRootStringWithoutCache(curPage, treeManagers, NavigationUtil.NAVREL);
+                  if (path != null) {
+                      Node currentSite = NavigationUtil.getSiteFromPath(cloud, path);
+                      if (currentSite != null 
+                              && currentSite.getIntValue("number") == foreignSite.getIntValue("number") 
+                              && PagesUtil.isPage(curPage)) {
+                         return path;
+                      }
+                  }
+               }        
             }
-         
-         /*
-          * We now have a list of pages that have the same liname as the page
-          * that was given as the id in the request or we have a list of pages
-          * that have the same liname as one of the parents of the page that was
-          * given as the id in the request. Find the site of this page and if it
-          * corresponds with the site found previously, redirect to that page.
-          */
-         if (pageList != null && pageList.size() > 0) {
-             LinkedHashMap<String, String> treeManagers = new LinkedHashMap<String, String>();
-             treeManagers.put(SiteUtil.SITE, SiteUtil.FRAGMENT_FIELD);
-             treeManagers.put(PagesUtil.PAGE, PagesUtil.FRAGMENT_FIELD);
-             
-             NodeIterator pageIt = pageList.nodeIterator();
-            while (pageIt.hasNext()) {
-               Node curPage = (Node)pageIt.next();
-               String path = TreeUtil.getPathToRootStringWithoutCache(curPage, treeManagers, NavigationUtil.NAVREL);
-               if (path != null) {
-                   Node currentSite = NavigationUtil.getSiteFromPath(cloud, path);
-                   if (currentSite != null 
-                           && currentSite.getIntValue("number") == foreignSite.getIntValue("number") 
-                           && PagesUtil.isPage(curPage)) {
-                      return path;
-                   }
-               }
-            }        
+            // Return with an error. There was no site that had the same language
+            // as the given language parameter.
+            log.debug("None of the found pages belong to the site with langauge " + language + ".");
+            return foreignSite.getStringValue(SiteUtil.FRAGMENT_FIELD);
+            
          }
-         // Return with an error. There was no site that had the same language
-         // as the given language parameter.
-         log.debug("None of the found pages belong to the site with langauge " + language + ".");
-         return foreignSite.getStringValue(SiteUtil.FRAGMENT_FIELD);
-         
+         if (SiteUtil.isSite(pageOrSiteNode)) {
+            // We're in the root, the current node is a site, so go directly to the
+            // corresponding site in the given language.
+            log.debug("The current node is a site(id: " + pageOrSiteNode.getNumber() + ").");
+            return foreignSite.getStringValue(SiteUtil.FRAGMENT_FIELD);
+         }                 
       }
-      if (SiteUtil.isSite(pageOrSiteNode)) {
-         // We're in the root, the current node is a site, so go directly to the
-         // corresponding site in the given language.
-         log.debug("The current node is a site(id: " + pageOrSiteNode.getNumber() + ").");
-         return foreignSite.getStringValue(SiteUtil.FRAGMENT_FIELD);
-      }                 
-      
+      catch(NotFoundException nfe) {
+         log.debug("The node is not found (" + nfe.getMessage() + ").");
+      }
       return null;
    }
    
