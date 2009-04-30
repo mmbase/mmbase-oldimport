12ed0889ec2a623be0149ec5fd200c4b86c5d304
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-04-30 22:51:03 +0000

    will disable CVS tomorrow
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@34602 91430c5f-d668-0410-9e7d-dd663366dcfd

--- CVSROOT/deny.pl
+++ CVSROOT/deny.pl
@@ -1,3 +1,8 @@
 #!/usr/bin/perl
-print "You cannot check in in CVS any more. We migrated to Subversion. Please https://scm.mmbase.org.\n";
-exit(-1);
+$module_dir = shift @ARGV;
+if ($module_dir ne "CVSROOT") {
+    print "You cannot check in in CVS any more. We migrated to Subversion. Please use https://scm.mmbase.org.\n";
+    exit(-1);
+} else {
+    exit(0);
+}
