ef9a726fde991a6ea03487bc02e7bd0b56d9efac
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-04-30 14:16:08 +0000

    commented in the failing cases again. I know how to fix them now.
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@34565 91430c5f-d668-0410-9e7d-dd663366dcfd

--- tests/bridge/org/mmbase/bridge/ProcessorTest.java
+++ tests/bridge/org/mmbase/bridge/ProcessorTest.java
@@ -13,6 +13,7 @@ package org.mmbase.bridge;
 import org.mmbase.tests.*;
 import org.mmbase.datatypes.processors.*;
 import junit.framework.*;
+import org.mmbase.bridge.util.*;
 
 import org.mmbase.util.DynamicDate;
 import org.mmbase.util.logging.Logger;
@@ -20,9 +21,10 @@ import org.mmbase.util.logging.Logging;
 
 
 /**
+ * Testing wether the processors of datatypes behave as expected.
  *
  * @author Michiel Meeuwissen
- * @version $Id: ProcessorTest.java,v 1.5 2009-01-26 16:00:05 michiel Exp $
+ * @version $Id: ProcessorTest.java,v 1.6 2009-04-30 14:16:08 michiel Exp $
  * @since MMBase-1.9.1
   */
 public class ProcessorTest extends BridgeTest {
@@ -31,6 +33,7 @@ public class ProcessorTest extends BridgeTest {
     public ProcessorTest(String name) {
         super(name);
     }
+
 
     protected Node testCommitProcessorIsChanged1(Cloud c) {
         NodeManager nm = c.getNodeManager("mustbechanged");
@@ -49,12 +52,11 @@ public class ProcessorTest extends BridgeTest {
         Node n = c.getNode(nn);
         try {
             n.commit();
-            throw new AssertionFailedError("Should have thrown exception");
+            fail("Should have thrown exception");
         } catch(RuntimeException ru) {
             // ok
         }
     }
-
 
     public void testCommitProcessorIsChanged() {
         Cloud c = getCloud();
@@ -62,6 +64,7 @@ public class ProcessorTest extends BridgeTest {
         testCommitProcessorIsChanged2(c, nn);
         testCommitProcessorIsChanged3(c, nn);
     }
+
     public void testCommitProcessorIsChangedTransaction() {
         Cloud c = getCloud();
         Transaction t = c.getTransaction("aa");
@@ -74,8 +77,8 @@ public class ProcessorTest extends BridgeTest {
         t = c.getTransaction("cc");
         testCommitProcessorIsChanged3(t, nn);
         t.commit();
-
     }
+
 
     protected Node testAge(Cloud c) {
         NodeManager nm = c.getNodeManager("datatypes");
@@ -85,39 +88,44 @@ public class ProcessorTest extends BridgeTest {
         n = c.getNode(n.getNumber());
         assertEquals(DynamicDate.eval("2008-01-01"), n.getDateValue("birthdate"));
         n.setIntValue("age", 10);
-        /*
-        assertEquals(10, n.getIntValue("age")); /// TODO TODO FAILS FAILS
+        assertEquals(10, n.getIntValue("age"));
         n.commit();
         assertEquals(10, n.getIntValue("age"));
-        */
         return n;
     }
 
     public void testAge() {
-        org.mmbase.cache.CacheManager.getInstance().disable(".*");
-        testAge(getCloud());
-        org.mmbase.cache.CacheManager.getInstance().readConfiguration();
+        try {
+            org.mmbase.cache.CacheManager.getInstance().disable(".*");
+            testAge(getCloud());
+            org.mmbase.cache.CacheManager.getInstance().readConfiguration();
+        } catch (NoClassDefFoundError ncdfe) {
+            log.service("Probably using RMMCI, cannot disable caches then. " + ncdfe.getMessage());
+        }
     }
-
     public void testAgeTransaction() {
-        org.mmbase.cache.CacheManager.getInstance().disable(".*");
-        Transaction t = getCloud().getTransaction("bla");
-        Node n = testAge(t);
-        t.commit();
-        assertEquals(10, getCloud().getNode(n.getNumber()).getIntValue("age"));
-        org.mmbase.cache.CacheManager.getInstance().readConfiguration();
+        try {
+            org.mmbase.cache.CacheManager.getInstance().disable(".*");
+            Transaction t = getCloud().getTransaction("bla");
+            Node n = testAge(t);
+            t.commit();
+            assertEquals(10, getCloud().getNode(n.getNumber()).getIntValue("age"));
+            org.mmbase.cache.CacheManager.getInstance().readConfiguration();
+        } catch (NoClassDefFoundError ncdfe) {
+            log.service("Probably using RMMCI, cannot disable caches then. " + ncdfe.getMessage());
+        }
+
     }
 
-
-    protected int testCommitCount(Cloud c) {
-        NodeManager nm = c.getNodeManager("datatypes");
-        int ccbefore = CountCommitProcessor.count;
-        Node n = nm.createNode();
-        n.commit();
-        if (c.getCloudContext().getUri().equals(ContextProvider.DEFAULT_CLOUD_CONTEXT_NAME)) {
+    // Creates an commits a node, and checks if that increased the commit count
+    protected void testCommitCount(Cloud c) {
+        if (c.getCloudContext().getUri().equals(ContextProvider.DEFAULT_CLOUD_CONTEXT_NAME)) { // only test on local
+            NodeManager nm = c.getNodeManager("datatypes");
+            int ccbefore = CountCommitProcessor.count;
+            Node n = nm.createNode();
+            n.commit();
             assertEquals(ccbefore + 1, CountCommitProcessor.count);
         }
-        return n.getNumber();
     }
 
     public void testCommitCount() {
@@ -129,11 +137,11 @@ public class ProcessorTest extends BridgeTest {
         Transaction t = getCloud().getTransaction("commitcount");
         testCommitCount(t);
         t.commit();
-        if (getCloudContext().getUri().equals(ContextProvider.DEFAULT_CLOUD_CONTEXT_NAME)) {
-            // there is no point in calling a commit processor twice
-            assertEquals(ccbefore + 1, CountCommitProcessor.count);
+        if (getCloudContext().getUri().equals(ContextProvider.DEFAULT_CLOUD_CONTEXT_NAME)) { // only test on local
+            assertEquals(ccbefore + 1, CountCommitProcessor.count); // there is no point in calling a commit processor twice
         }
     }
+
     static int nn = 0;
     public void testCommitCountTransaction2() {
         Transaction t = getCloud().getTransaction("commitcount2");
@@ -144,12 +152,12 @@ public class ProcessorTest extends BridgeTest {
         t.commit(); // but only the transaction.
 
         nn = n.getNumber();
-        if (getCloudContext().getUri().equals(ContextProvider.DEFAULT_CLOUD_CONTEXT_NAME)) {
+        if (getCloudContext().getUri().equals(ContextProvider.DEFAULT_CLOUD_CONTEXT_NAME)) { // only test on local
             // commit processor must have been called.
-            // TODO TODY FAILS FAILS
-            //assertEquals(ccbefore + 1, CountCommitProcessor.count);
+            assertEquals(ccbefore + 1, CountCommitProcessor.count); // FAILS
         }
     }
+
 
     protected void testCommitCountOnChange(Cloud c, int nn) {
         int ccbefore = CountCommitProcessor.count;
@@ -165,11 +173,12 @@ public class ProcessorTest extends BridgeTest {
         int ccbefore = CountCommitProcessor.count;
         int changedbefore = CountCommitProcessor.changed;
         Node n = c.getNode(nn);
-        n.setStringValue("string", "foobar");
+        n.setStringValue("string", "foobar" + ccbefore);
+        assertTrue("Node is changed, but it reports that it isn't. Values" + new NodeMap(n) + " changed fields " + n.getChanged(), n.isChanged());
         n.commit();
         if (getCloudContext().getUri().equals(ContextProvider.DEFAULT_CLOUD_CONTEXT_NAME)) {
             assertEquals(ccbefore + 1, CountCommitProcessor.count);
-            assertEquals(changedbefore + 1, CountCommitProcessor.changed);
+            assertEquals(changedbefore + 1,  CountCommitProcessor.changed);
         }
     }
 
@@ -180,10 +189,8 @@ public class ProcessorTest extends BridgeTest {
 
     public void testCommitCountOnChangeTransaction() {
         testCommitCountOnChange(getCloud().getTransaction("commitcount3"), nn);
-        // testCommitCountOnChange2(getCloud().getTransaction("commitcount4"), nn); // TODO TODO
-        // FAILS FAILS
+        testCommitCountOnChange2(getCloud().getTransaction("commitcount4"), nn);
     }
-
 
 
 
