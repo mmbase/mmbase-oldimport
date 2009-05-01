c901b0a7220993a1ac907fd86be814ca9ccef2f9
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-05-01 12:30:51 +0000

    no point in maintaining CVSROOT in svn
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@34609 91430c5f-d668-0410-9e7d-dd663366dcfd

--- CVSROOT/deny.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl
-$module_dir = shift @ARGV;
-if ($module_dir ne "CVSROOT") {
-    print "You cannot check in in CVS any more. We migrated to Subversion. Please use https://scm.mmbase.org.\n";
-    exit(-1);
-} else {
-    exit(0);
-}
