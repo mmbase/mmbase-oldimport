8c073f78d85bc9e32edd36a205727545f1a4ca2a
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-05-22 12:20:18 +0000

    implemented a default for iso639 datatype (the current locale)
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@35388 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/org/mmbase/datatypes/LanguageDataType.java
@@ -0,0 +1,68 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.datatypes;
+
+import java.util.*;
+import org.mmbase.bridge.*;
+import org.mmbase.util.*;
+import org.mmbase.util.logging.Logger;
+import org.mmbase.util.logging.Logging;
+
+/**
+ * Returns the current language a a default value. For the rest it is like {@link StringDataType}.
+ * @author Michiel Meeuwissen
+ * @version $Id: NodeManagerNamesDataType.java 35348 2009-05-21 15:40:40Z michiel $
+ * @since MMBase-1.9.1
+ */
+public class LanguageDataType extends StringDataType {
+
+    private static final Logger log = Logging.getLoggerInstance(LanguageDataType.class);
+
+    private static final long serialVersionUID = 1L;
+
+    public LanguageDataType(String name) {
+        super(name);
+    }
+
+    protected static Locale getLocale(Cloud cloud, Field field) {
+        if (cloud != null) {
+            return cloud.getLocale();
+        } else if (field != null) {
+            return field.getNodeManager().getCloud().getLocale();
+        } else {
+            return org.mmbase.util.LocalizedString.getDefault();
+        }
+    }
+
+
+    @Override
+    public String getDefaultValue(Locale locale, Cloud cloud, Field field) {
+        if (locale != null) {
+            return locale.getLanguage();
+        } else {
+            return getLocale(cloud, field).getLanguage();
+        }
+    }
+
+    public static class Getter implements org.mmbase.datatypes.processors.Processor {
+
+        public Object process(Node node, Field field, Object value) {
+            if (log.isDebugEnabled()) {
+                log.debug("node " + node + " " + field + " "  + value);
+            }
+            if (value == null || "".equals(value)) {
+                return getLocale(node == null ? null : node.getCloud(), field).getLanguage();
+            } else {
+                return value;
+            }
+        }
+    }
+
+}
