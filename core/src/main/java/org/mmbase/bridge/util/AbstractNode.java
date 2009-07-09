31de8eadc4f78c9a191cb26a442e5a88debde75b
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-07-09 09:08:29 +0000

    clearer exception
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@36728 91430c5f-d668-0410-9e7d-dd663366dcfd

--- core/src/main/java/org/mmbase/bridge/util/AbstractNode.java
+++ core/src/main/java/org/mmbase/bridge/util/AbstractNode.java
@@ -662,14 +662,14 @@ public abstract class AbstractNode implements Node {
     }
 
     public void commit() {
-        throw new UnsupportedOperationException("Cannot edit virtual node");
+        throw new UnsupportedOperationException("Cannot edit node of type " + getClass().getName());
     }
 
     public void cancel() {
     }
 
     public void delete(boolean deleteRelations) {
-        throw new UnsupportedOperationException("Cannot edit virtual node");
+        throw new UnsupportedOperationException("Cannot edit node of type " + getClass().getName());
     }
 
     public void deleteRelations(String type) throws NotFoundException {
