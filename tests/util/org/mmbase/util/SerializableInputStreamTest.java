da1833af098e0feff9be31a13ba918f0e82e4dc0
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-08-08 21:22:54 +0000

    git-svn-id: https://scm.mmbase.org/mmbase/trunk@37619 91430c5f-d668-0410-9e7d-dd663366dcfd

--- tests/util/org/mmbase/util/SerializableInputStreamTest.java
+++ tests/util/org/mmbase/util/SerializableInputStreamTest.java
@@ -180,7 +180,7 @@ public class SerializableInputStreamTest extends TestCase {
         testReset(getDiskItemInstanceBig());
     }
 
-    public void testCopyConstructor() throws IOException {
+    public void testCopyConstructor() throws IOException, ClassNotFoundException  {
         SerializableInputStream is1 = getInputStreamInstance();
         SerializableInputStream is2 = new SerializableInputStream(is1);
         is1.finalize();
