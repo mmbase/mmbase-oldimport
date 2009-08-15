989fcd92359a7a6322d93b486622cd4275d3586a
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-08-15 08:32:50 +0000

    started on some test-cases for org.mmbase.bridge.util.Queries
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@37857 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ core/src/test/java/org/mmbase/bridge/util/QueriesTest.java
@@ -0,0 +1,60 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+
+package org.mmbase.bridge.util;
+
+import org.mmbase.bridge.*;
+import org.mmbase.bridge.dummy.*;
+import org.mmbase.datatypes.*;
+import java.util.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+/**
+ *
+ * @author Michiel Meeuwissen
+ * @version $Id: DummyTest.java 37837 2009-08-14 21:45:04Z michiel $
+ */
+public class QueriesTest  {
+
+    public CloudContext getCloudContext() {
+        return DummyCloudContext.getInstance();
+    }
+
+    @BeforeClass
+    public static void setup() throws Exception {
+        DummyCloudContext.getInstance().clear();
+        DummyCloudContext.getInstance().addCore();
+    }
+
+
+    @Test
+    public void nodeQuery1() {
+        Cloud cloud = getCloudContext().getCloud("mmbase");
+        NodeQuery q = cloud.getNodeManager("object").createQuery();
+    }
+
+    @Test
+    public void nodeQuery2() {
+        Cloud cloud = getCloudContext().getCloud("mmbase");
+        NodeQuery q = cloud.createNodeQuery();
+        q.addStep(cloud.getNodeManager("object"));
+    }
+
+    @Test
+    public void nodeQuery() {
+        Cloud cloud = getCloudContext().getCloud("mmbase");
+        Node node = cloud.getNodeManager("object").createNode();
+        node.commit();
+        Queries.createNodeQuery(node);
+    }
+
+}
