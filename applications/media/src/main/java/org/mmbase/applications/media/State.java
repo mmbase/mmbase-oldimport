5f66f3ea02490d882e809760b4c240f07b522cbe
andre <andre@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-08-28 19:46:23 +0000

    added an unsupported state to indicate that a media source can not be handled
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@38226 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/media/src/main/java/org/mmbase/applications/media/State.java
+++ applications/media/src/main/java/org/mmbase/applications/media/State.java
@@ -6,19 +6,27 @@ public enum State {
      */
     REQUEST(1),
     /**
-     * This means that the media source is currently being generated.
+     * The media source is currently being generated, typically being transcoded.
      */
     BUSY(2),
     /**
-     * This means that the media source is  generated and that is ready.
+     * The media source is generated, typically transcoded, and is ready.
      */
     DONE(3),
     /**
      * This means that the media source is an original.
      */
     SOURCE(4),
+    /**
+     * Generation of other media from this media source is not supported by this system, the source
+     * has an unknown or unsupported format or codec.
+     */
+    SOURCE_UNSUPPORTED(9),
     UNDEFINED(-1),
     REMOVED(10),
+    /**
+     * Transcoding of the media source is interrupted.
+     */
     INTERRUPTED(20);
 
     private final int value;
