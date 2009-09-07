8a488468cdd51a397a708545965427febf6ffff2
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-09-07 11:00:29 +0000

    testing the damn virtual fields
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@38461 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ applications/streams/src/test/java/org/mmbase/streams/MediaFragmentTest.java
@@ -0,0 +1,95 @@
+package org.mmbase.streams;
+
+import org.junit.*;
+import org.junit.runner.*;
+import org.junit.runners.*;
+import static org.junit.Assert.*;
+import static org.junit.Assume.*;
+import java.util.*;
+import java.io.*;
+import org.mmbase.bridge.*;
+import org.mmbase.datatypes.DataType;
+import static org.mmbase.datatypes.Constants.*;
+import org.mmbase.datatypes.processors.*;
+import org.mmbase.bridge.mock.*;
+import org.mmbase.streams.transcoders.*;
+import static org.mmbase.streams.transcoders.AnalyzerUtils.*;
+import org.mmbase.util.logging.*;
+
+import org.mmbase.servlet.FileServlet;
+
+
+/**
+ * @author Michiel Meeuwissen
+ */
+
+public class MediaFragmentTest {
+
+    private static final String REMOTE_URI = "rmi://127.0.0.1:1111/remotecontext";
+    private static Cloud remoteCloud;
+
+
+    private final static MockCloudContext cloudContext = new MockCloudContext();
+    private final static String FILE = "basic.mp4";
+    private static File  testFile;
+
+    public MediaFragmentTest() {
+    }
+
+
+    @AfterClass
+    public static void shutdown() {
+        System.out.println("Ready testing ");
+        org.mmbase.util.ThreadPools.shutdown();
+    }
+    @BeforeClass
+    public static void setUp() throws Exception {
+        try {
+            CloudContext c =  ContextProvider.getCloudContext(REMOTE_URI);
+            remoteCloud = c.getCloud("mmbase", "class", null);
+            System.out.println("Found remote cloud " + remoteCloud);
+        } catch (Exception e) {
+            System.err.println("Cannot get RemoteCloud. (" + e.getMessage() + "). Some tests will be skipped. (but reported as succes: see http://jira.codehaus.org/browse/SUREFIRE-542)");
+            System.err.println("You can start up a test-environment for remote tests: trunk/example-webapp$ mvn jetty:run");
+            remoteCloud = null;
+        }
+        testFile = new File("samples", FILE);
+
+    }
+
+    protected Cloud getCloud() {
+        if (remoteCloud != null) {
+            remoteCloud.setProperty(CreateCachesProcessor.NOT, "no implicit processesing please");
+            //remoteCloud.setProperty(BinaryCommitProcessor.NOT, "no implicit processesing please");
+            //remoteCloud.setProperty(org.mmbase.applications.media.FragmentTypeFixer.NOT, "no implicit processesing please");
+        }
+        return remoteCloud;
+    }
+
+
+
+
+    @Test
+    public void node() {
+        Cloud cloud = getCloud();
+        assumeNotNull(cloud);
+        NodeManager nm = cloud.getNodeManager("streamsources");
+        assumeNotNull(nm);
+        Node newSource = nm.createNode(); //
+        newSource.commit();
+        assertTrue(newSource.getNumber() > 0);
+
+        assertNull(newSource.getNodeValue("mediafragment"));
+        newSource.getStringValue("title");
+        assertNotNull(newSource.getNodeValue("mediafragment"));
+        newSource.setStringValue("title", "test test");
+        newSource.commit();
+
+        assertEquals("test test", newSource.getStringValue("title"));
+
+
+
+    }
+}
+
+
