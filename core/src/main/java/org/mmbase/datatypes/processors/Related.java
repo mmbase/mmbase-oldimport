0e799aff39812a00443b592bdda7b9eb1e540af0
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-09-07 09:48:36 +0000

    better not do it for new nodes
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@38458 91430c5f-d668-0410-9e7d-dd663366dcfd

--- core/src/main/java/org/mmbase/datatypes/processors/Related.java
+++ core/src/main/java/org/mmbase/datatypes/processors/Related.java
@@ -166,25 +166,27 @@ public class Related {
     public static class Creator extends AbstractProcessor {
         private static final long serialVersionUID = 1L;
         public Object process(final Node node, final Field field, final Object value) {
-            Node relatedNode = getRelatedNode(node, field);
-            if (relatedNode == null) {
-                log.service("No related node of type " + getRelatedCreateType(node) + " for node " + node.getNumber() + ". Implicitely creating now.");
-                Node newNode = getRelatedCreateType(node).createNode();
-                newNode.commit();
-                RelationManager rel = getRelationManager(node);
-                Relation newrel = node.createRelation(newNode, rel);
-                for (Map.Entry<String, String> entry : relationConstraints.entrySet()) {
-                    newrel.setStringValue(entry.getKey(), entry.getValue());
-                }
+            if (! node.isNew()) {
+                Node relatedNode = getRelatedNode(node, field);
+                if (relatedNode == null) {
+                    log.service("No related node of type " + getRelatedCreateType(node) + " for node " + node.getNumber() + ". Implicitely creating now.");
+                    Node newNode = getRelatedCreateType(node).createNode();
+                    newNode.commit();
+                    RelationManager rel = getRelationManager(node);
+                    Relation newrel = node.createRelation(newNode, rel);
+                    for (Map.Entry<String, String> entry : relationConstraints.entrySet()) {
+                        newrel.setStringValue(entry.getKey(), entry.getValue());
+                    }
 
-                newrel.commit();
-                if (node.getCloud() instanceof Transaction) {
-                    node.getCloud().setProperty(getKey(node, field),  newNode);
-                }
-                if (log.isDebugEnabled()) {
-                    log.debug("Created " + newrel);
-                    log.debug("Cloud: : " + node.getCloud().getProperties());
+                    newrel.commit();
+                    if (node.getCloud() instanceof Transaction) {
+                        node.getCloud().setProperty(getKey(node, field),  newNode);
+                    }
+                    if (log.isDebugEnabled()) {
+                        log.debug("Created " + newrel);
+                        log.debug("Cloud: : " + node.getCloud().getProperties());
 
+                    }
                 }
             }
             return value;
