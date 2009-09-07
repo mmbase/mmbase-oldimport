ac4df284b27ef168fced01116b9a64ac91e22c0d
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-09-07 06:21:44 +0000

    OIP-79
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@38448 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ applications/media/src/main/java/org/mmbase/applications/media/filters/NodeTypeSorter.java
@@ -0,0 +1,52 @@
+ /*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+ */
+
+package org.mmbase.applications.media.filters;
+
+import org.mmbase.applications.media.urlcomposers.URLComposer;
+
+import org.mmbase.module.core.MMObjectBuilder;
+import java.util.*;
+import org.mmbase.util.logging.*;
+
+/**
+ *
+ * @author  Michiel Meeuwissen
+ * @version $Id$
+ */
+public class NodeTypeSorter extends  PreferenceSorter {
+    private static final Logger log = Logging.getLoggerInstance(NodeTypeSorter.class);
+
+    private final List<String> nodeTypes = new ArrayList<String>();
+
+    public void setList(String nt) {
+        nodeTypes.addAll(org.mmbase.util.StringSplitter.split(nt));
+        Collections.reverse(nodeTypes);
+    }
+
+    public  NodeTypeSorter() {
+    }
+
+    protected int getPreference(MMObjectBuilder buil) {
+        return nodeTypes.indexOf(buil.getTableName());
+    }
+
+    protected int getPreference(URLComposer ri) {
+        MMObjectBuilder buil = ri.getSource().getBuilder();
+        int p = getPreference(buil);
+        if (p == -1 && buil.getParentBuilder() != null) {
+            buil = buil.getParentBuilder();
+            p = getPreference(buil);
+        }
+        return p;
+    }
+
+}
+
