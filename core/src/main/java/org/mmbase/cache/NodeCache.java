4982b0a5c409292f8b38fa0a7698d2b831eb8747
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-09-21 19:30:00 +0000

    MMB-411. Keeping weak references to all MMObjectNodes and use those if possible. This should make it more or less impossible to have 2 instances representing the same node in the database
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@38734 91430c5f-d668-0410-9e7d-dd663366dcfd

--- core/src/main/java/org/mmbase/cache/NodeCache.java
+++ core/src/main/java/org/mmbase/cache/NodeCache.java
@@ -11,6 +11,9 @@ package org.mmbase.cache;
 import org.mmbase.core.event.*;
 import org.mmbase.module.core.*;
 import org.mmbase.util.Casting;
+import java.util.*;
+import java.lang.ref.WeakReference;
+import java.lang.ref.ReferenceQueue;
 
 /**
  * A cache for MMObjectNodes.
@@ -32,6 +35,9 @@ public class NodeCache extends Cache<Integer, MMObjectNode> implements NodeEvent
         cache.putCache();
     }
 
+    private transient final ReferenceQueue<MMObjectNode> queue = new ReferenceQueue<MMObjectNode>();
+    private transient final Map<Integer, NodeReference> weak = new HashMap<Integer, NodeReference>();
+
     @Override
     public String getName() {
         return "Nodes";
@@ -49,10 +55,59 @@ public class NodeCache extends Cache<Integer, MMObjectNode> implements NodeEvent
         // node cache is registered as a Listener in MMBase.java.
     }
 
+
+    @Override
+    public  MMObjectNode get(Object key) {
+        synchronized(weak) {
+            purge();
+            MMObjectNode s = super.get(key);
+            if (s != null) {
+                return s;
+            } else {
+                NodeReference ref = weak.get(key);
+                if (ref != null) {
+                    return ref.get();
+                } else {
+                    return null;
+                }
+
+            }
+        }
+    }
+
+    @Override
+    public MMObjectNode put(Integer key, MMObjectNode value) {
+        synchronized(weak) {
+            purge();
+            weak.put(key, new NodeReference(value, key));
+            return super.put(key, value);
+        }
+    }
+
+
+    protected void purge() {
+        NodeReference ref = (NodeReference) queue.poll();
+        while (ref != null) {
+            weak.remove(ref.number);
+            ref = (NodeReference) queue.poll();
+        }
+    }
+
+
+
     public void notify(NodeEvent event) {
         int type = event.getType();
         if(type == Event.TYPE_DELETE || ((! event.isLocal()) && type == Event.TYPE_CHANGE)) {
             remove(event.getNodeNumber());
         }
+    }
+
+    private class NodeReference extends WeakReference<MMObjectNode> {
+        public final int number;
+        NodeReference(MMObjectNode n, int number) {
+            super(n, queue);
+            this.number = number;
+        }
+
     }
 }
