c63046c84f59a6dd17ff366c4cf060062f308404
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-09-28 16:56:37 +0000

    a listener that can delete 'protected' files on expiry of the session (the will be unavailable any way)
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@38890 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ applications/resources/src/main/java/org/mmbase/servlet/FileServletSessionListener.java
@@ -0,0 +1,59 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.servlet;
+
+import org.mmbase.module.core.*;
+import org.mmbase.datatypes.processors.FormatFileSize;
+import org.mmbase.util.transformers.*;
+import java.util.*;
+
+import javax.servlet.http.*;
+import javax.servlet.*;
+import java.util.regex.*;
+
+import java.io.*;
+import java.text.*;
+
+import org.mmbase.util.logging.*;
+
+
+/**
+
+ *
+ * @version $Id: FileServlet.java 38851 2009-09-25 08:28:12Z michiel $
+ * @author Michiel Meeuwissen
+ * @since  MMBase-1.9.2
+ */
+public class FileServletSessionListener implements HttpSessionListener {
+    private static final Logger log = Logging.getLoggerInstance(FileServletSessionListener.class);
+
+    public void sessionCreated(HttpSessionEvent se) {
+        // nothing to
+        log.info("Created " + se.getSession());
+    }
+    public void sessionDestroyed(HttpSessionEvent se) {
+        // delete files associated with this session
+        log.info("Destroyed " + se.getSession());
+        HttpSession session = se.getSession();
+        Set<File> protectedFiles = (Set<File>) session.getAttribute(FileServlet.PROTECTED_FILES);
+        if (protectedFiles != null) {
+            for (File f : protectedFiles) {
+
+                f.delete();
+                File sessionFile = FileServlet.getSessionFile(f);
+                sessionFile.delete();
+                log.info("Deleted " + f + " (" + sessionFile + ")");
+            }
+        }
+    }
+
+}
+
+
