5f29372ba63f98f5af9e1311e317096b9434941c
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-10-06 14:43:03 +0000

    junit 4, and added test-cases for XSS escaping
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@39004 91430c5f-d668-0410-9e7d-dd663366dcfd

--- core/src/test/java/org/mmbase/util/transformers/TagStripperTest.java
+++ core/src/test/java/org/mmbase/util/transformers/TagStripperTest.java
@@ -1,23 +1,36 @@
 package org.mmbase.util.transformers;
+
+import java.util.*;
+import java.io.*;
 import org.mmbase.util.*;
 import org.mmbase.util.functions.*;
 import java.util.*;
-import junit.framework.TestCase;
+import org.junit.*;
+import static org.junit.Assert.*;
 
 /**
  * @author Michiel Meeuwissen
  * @version $Id$
  */
-public class TagStripperTest  extends TestCase {
+public class TagStripperTest   {
 
-    private static TagStripperFactory factory = new TagStripperFactory();
-
-
+    private static final TagStripperFactory FACTORY = new TagStripperFactory();
 
 
-    public void testSimple() {
-        Parameters params = factory.createParameters();
-        CharTransformer stripper = factory.createTransformer(params);
+
+    protected CharTransformer getXSS() {
+        Parameters params = FACTORY.createParameters();
+        params.set(TagStripperFactory.TAGS, "XSS");
+        params.set(TagStripperFactory.ADD_BRS, false);
+        params.set(TagStripperFactory.ESCAPE_AMPS, true);
+        CharTransformer transformer = FACTORY.createTransformer(params);
+        return transformer;
+    }
+
+    @Test
+    public void simple() {
+        Parameters params = FACTORY.createParameters();
+        CharTransformer stripper = FACTORY.createTransformer(params);
 
         assertEquals("aaa", stripper.transform("<p>aaa</p>").trim());
         assertEquals("aaa", stripper.transform("<p>aaa\n</p>").trim());
@@ -28,4 +41,22 @@ public class TagStripperTest  extends TestCase {
 
 
     }
+
+
+    @Test
+    public void xss() {
+        CharTransformer xss = getXSS();
+        assertEquals("<p style=\"nanana\">allow this <b>and this</b></p>", xss.transform("<p style=\"nanana\">allow this <b>and this</b></p>").trim());
+        assertEquals("<p>allow this <b>and this</b></p>", xss.transform("<p onclick=\"nanana\">allow this <b>and this</b></p>").trim());
+        assertEquals("<p>allow this</p>", xss.transform("<p>allow this<script language='text/javascript'>bj aja </script>\n</p>").trim());
+        assertEquals("<p>allow this<a>foobar</a></p>", xss.transform("<p>allow this<a href=\"javascript:alert('hoi');\">foobar</a></p>").trim());
+
+
+        /*
+        assertEquals("<p style=\"nanana\">hoi hoi\n<br><table width=\"45\" height=99 border='1\"' font=\"bold\" styLe=\"\n\\\"one\">\nbla bla bla</table></p>",
+                     transformer.transform("<P sTyle=\"nanana\">hoi hoi\n<br><table WIDTH=\"45\" height=99 border='1\"' fONt=bold styLe=\"\none\">\nbla bla bla</table></p>").trim());
+        */
+
+    }
+
 }
