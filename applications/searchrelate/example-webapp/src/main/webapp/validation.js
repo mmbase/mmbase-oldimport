87f2d0e2012404f36a096c09887bead9f9b20ce3
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-10-12 09:19:48 +0000

    add min/max validation to all forms
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@39060 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/searchrelate/example-webapp/src/main/webapp/validation.js
+++ applications/searchrelate/example-webapp/src/main/webapp/validation.js
@@ -1,12 +1,13 @@
 
 $(document).ready(function() {
         $("form.mm_form").bind("mmsrValidateHook", function(ev, list, valid, reason) {
+                var formInvalid = $(ev.target).find("div.list.invalid");
                 if (valid) {
                     $(".info").text("Form is valid");
                 } else {
                     $(".info").text("Form is invalid: " + reason);
                 }
-                if (valid) {
+                if (formInvalid.length == 0) {
                     $("input[name=submit]").removeAttr("disabled");
                 } else {
                     $("input[name=submit]").attr("disabled", "disable");
