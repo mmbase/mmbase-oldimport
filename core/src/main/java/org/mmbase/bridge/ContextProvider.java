d9d127794f746958438b71bba799aecd06d2dc14
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-10-17 07:12:29 +0000

    javadoc
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@39182 91430c5f-d668-0410-9e7d-dd663366dcfd

--- core/src/main/java/org/mmbase/bridge/ContextProvider.java
+++ core/src/main/java/org/mmbase/bridge/ContextProvider.java
@@ -21,7 +21,41 @@ import org.mmbase.util.logging.*;
 
 
 /**
- * Main class to aquire CloudContexts
+ * This is the main factory to acquire certain {@link CloudContext}s. The most basic method is
+ * {@link #getCloudContext(String)}.
+ *
+ * The method {@link #getResolvers} can be used to inspect what kind of CloudContexts are currently
+ * available. Normally there is one named 'local' which represents the MMBase in the current
+ * application. CloudContexts are pluggable. An extra jar may also provide one or more CloudContext
+ * resolvers (e.g. the rmmci jar would do that).
+ *
+ * There is also a method {@link #getDefaultCloudContext()}. This returns
+ * <code>getCloudContext('local')</code>, unless a System property
+ * <code>mmbase.defaultcloudcontext</code> defines another URI (e.g. an rmmci implementation).
+ *
+ * If your code does not have any user interaction, you'll typically be able to start like this:
+ * <pre>
+    Cloud cloud = CloudContext.getDefaultCloudContext().getCloud("mmbase", "class", null);
+    // Now start doing things with the Cloud.
+ </pre>
+ * This acquires a cloud on the 'default context' authenticated with {@link
+ * org.mmbase.security.classsecurity.ClassAuthentication}. During testing the default cloud context
+ * can e.g. be a {@link org.mmbase.bridge.mock.MockCloudContext} or a {@link
+ * org.mmbase.bridge.RemoteCloudContext}.
+ *
+ * Though discouraged, it is sometimes also possible to directly access the wanted CloudContext
+ * implementation, and not use ContextProvider at all. It depends on that implementation how
+ * precisely that would go. E.g. if you're absolutely certain that your code must only work on the
+ * local MMBase, you may proceed like so:
+ <pre>
+    Cloud cloud = LocalContext.getCloudContext().getCloud("mmbase", "class", null);
+ </pre>
+ * Another example, which is acceptable in test cases:
+ <pre>
+    Cloud cloud = MockCloudContext.getInstance().getCloud("mmbase");
+ </pre>
+ *
+ *
  * @author Kees Jongenburger
  * @version $Id$
  * @since MMBase-1.5
@@ -79,6 +113,7 @@ public class ContextProvider {
      *
      * <ul>
      *   <li>local : will return a local context</li>
+     *   <li>mock://localhost : Will return a {@link org.mmbase.bridge.mock.MockCloudContext}.</li>
      *   <li>rmi://hostname:port/contextname : will return a remote context (only if the rmmci or rmmci-client jar is available)</li>
      *   <li>a null parameter: will return the default context. See {@link #getDefaultCloudContext} </li>
      * </ul>
@@ -102,6 +137,8 @@ public class ContextProvider {
     }
 
     /**
+     * The uri defined as default (using the 'mmbase.defaultcloudcontext' system property) or
+     * 'local'. The return value can be used in {@link #getCloudContext}, and is used in {@link #getDefaultCloudContext}.
      * @since MMBase-1.7
      * @return the name of the cloud context to be used as default
      **/
@@ -121,10 +158,14 @@ public class ContextProvider {
      }
 
     /**
+     * Returns the 'default' cloud context. This could be <code>getCloudContext('local')</code>, or
+     * <code>getCloudContext('rmi://127.0.0.1:1111/remotecontext</code> (as a fall back).
+     *
      * @since MMBase-1.7
      * @return the default cloud context This is the local cloud if mmbase is running or could be started (with mmbase.config system property),
-     *         Otherwise a default rmmci cloud, or specified with mmbase.defaultcloudtext.property
-     **/
+     *         Otherwise a default rmmci cloud, or specified with the mmbase.defaultcloudtext system
+     *         property
+     */
     public static CloudContext getDefaultCloudContext() {
         try {
             String uri = System.getProperty("mmbase.defaultcloudcontext");
@@ -164,19 +205,33 @@ public class ContextProvider {
 
 
     /**
-     * A Resolver resolves a URI-string to a CloudContext object.
+     * A Resolver resolves an URI-string (the argument of {@link #getCloudContext(String)} to a CloudContext object.
      */
     public static abstract class Resolver {
         protected final LocalizedString description = new LocalizedString(toString());
         {
             description.setBundle("org.mmbase.bridge.resources.contextproviders");
         }
-
+        /**
+         * Resolve an 'uri' string to an actual CloudContext instance.
+         * @return A CloudContext or <code>null</code> if this specific Resolver doesn't understand
+         * the uri.
+         */
         public abstract CloudContext resolve(String uri);
+
+        /**
+         * The syntax of the uri and goal of the resuling CloudContext may be explained in this
+         * description.
+         */
         public LocalizedString getDescription() {
             return description;
         }
     }
+
+
+    /**
+     * The resolver recognizing only the string 'local', which then returns {@link LocalContext#getCloudContext}.
+     */
 
     public static class LocalResolver extends Resolver {
         @Override
