d9f337bc17c7a8aca58be6b26dc75941bbb659e2
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-10-27 15:44:03 +0000

    various details
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@39389 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/searchrelate/example-webapp/src/main/webapp/example.js
+++ applications/searchrelate/example-webapp/src/main/webapp/example.js
@@ -1,15 +1,30 @@
 $(document).ready(function() {
-
-        { // prevent default of gui of images in search results
-
-            // TODO I doubt if this works in sub-items.
-            $("div.mm_related").bind("mmsrPaged",
-                                     function (e, status, relater) {
-                                         $(relater.div).find("a.mm_gui").removeAttr("onclick");
-                                         $(relater.div).find("a.mm_gui").click(function(ev) {
-                                                 ev.preventDefault();
-                                             });
-                                     }
-                                     );
+        {
+            // prevent default of gui of images in search results
+            $(document).bind("mmsrPaged",
+                             function (e, status, relater) {
+                                 $(relater.div).find("a.mm_gui").removeAttr("onclick");
+                                 $(relater.div).find("a.mm_gui").click(function(ev) {
+                                         ev.preventDefault();
+                                     });
+                             }
+                             );
         }
+
+        MMBaseSearcher.prototype.implicitsAreHidden();
+
+        {
+            $(document).bind("mmsrStartSave",
+                             function (e, list) {
+                                 //console.log("Starting save for " + list.rid);
+                             }
+                             );
+
+            $(document).bind("mmsrFinishedSave",
+                             function (e, list) {
+                                 //console.log("Finished saving for " + list.rid);
+                             }
+                             );
+        }
+
     });
