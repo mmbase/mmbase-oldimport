c7cd27e0db5530f7c30d654836d00359baa8dbfe
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-11-10 23:08:42 +0000

    simple ServletContextListener to set up MockCloudContext via web.xml
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@39624 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ core/src/main/java/org/mmbase/bridge/mock/MockSetup.java
@@ -0,0 +1,44 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+
+package org.mmbase.bridge.mock;
+
+import javax.servlet.*;
+
+/**
+ * This can be added to your web.xml:
+ <pre><![CDATA[
+  <listener>
+    <listener-class>org.mmbase.bridge.mock.MockSetup</listener-class>
+  </listener>
+]]></pre>
+ * This arranges the MockCloudContext to be (minimalisticly) set up, so that a mock bridge is available. E.g. to test taglib or so.
+ *
+ * @author  Michiel Meeuwissen
+ * @version $Id: MockCloud.java 39226 2009-10-19 20:00:46Z michiel $
+ * @since   MMBase-1.9.2
+ * @todo    EXPERIMENTAL
+ */
+
+public class MockSetup implements ServletContextListener {
+    public void	contextDestroyed(ServletContextEvent sce) {
+        MockCloudContext.getInstance().clear();
+
+    }
+    public void contextInitialized(ServletContextEvent sce) {
+        try {
+            MockCloudContext.getInstance().addCore();
+        } catch (java.io.IOException ioe) {
+            sce.getServletContext().log(ioe.getMessage(), ioe);
+        }
+    }
+
+}
+
