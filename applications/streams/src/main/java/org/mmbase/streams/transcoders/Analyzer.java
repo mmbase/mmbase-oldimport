007d7c8a6ab17acfe9d05abe076301c7e97cbfe0
andre <andre@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-11-25 14:05:18 +0000

    license and properties for svn
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@39936 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/streams/src/main/java/org/mmbase/streams/transcoders/Analyzer.java
+++ applications/streams/src/main/java/org/mmbase/streams/transcoders/Analyzer.java
@@ -1,12 +1,24 @@
 /*
 
-This software is OSI Certified Open Source Software.
-OSI Certified is a certification mark of the Open Source Initiative.
+This file is part of the MMBase Streams application, 
+which is part of MMBase - an open source content management system.
+    Copyright (C) 2009 André van Toly, Michiel Meeuwissen
 
-The license (Mozilla version 1.0) can be read at the MMBase site.
-See http://www.MMBase.org/license
+MMBase Streams is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MMBase Streams is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MMBase. If not, see <http://www.gnu.org/licenses/>.
 
 */
+
 package org.mmbase.streams.transcoders;
 
 import org.mmbase.util.logging.*;
@@ -19,10 +31,9 @@ import org.mmbase.bridge.*;
  * information about a {@link Transcoder} process, which appears in its' logging, in the 2 involved nodes.
  *
  * @author Michiel Meeuwissen
+ * @version $Id$
  */
-
 public interface Analyzer extends org.mmbase.util.PublicCloneable<Analyzer>, java.io.Serializable {
-
 
     void addThrowable(Throwable t);
 
@@ -30,7 +41,6 @@ public interface Analyzer extends org.mmbase.util.PublicCloneable<Analyzer>, jav
      * How many lines of logging should be offered for {@link #analyze}ing at the most.
      */
     int  getMaxLines();
-
 
     void addLogger(Logger l);
 
