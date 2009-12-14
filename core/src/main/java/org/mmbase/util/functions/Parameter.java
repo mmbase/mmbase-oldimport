ac43e9e176e4d2cd34652d6b23e8a7367a85597c
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-12-14 15:30:38 +0000

    Simplfied 'autoCast' using new 'castOrException'. Also fixes some of the (new) test-cases
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@40247 91430c5f-d668-0410-9e7d-dd663366dcfd

--- core/src/main/java/org/mmbase/util/functions/Parameter.java
+++ core/src/main/java/org/mmbase/util/functions/Parameter.java
@@ -354,16 +354,10 @@ public class Parameter<C> extends AbstractDescriptor implements java.io.Serializ
      * Tries to 'cast' an object for use with this parameter. E.g. if value is a String, but this
      * parameter is of type Integer, then the string can be parsed to Integer.
      * @param value The value to be filled in in this Parameter.
+     * @exception If no reasonable cast is possbible.
      */
     protected C autoCast(Object value) throws CastException {
-        Collection<LocalizedString> errors = dataType.castAndValidate(value, null, null);
-
-        if (errors.size() == 0) {
-            return dataType.cast(value, null, null);
-        } else {
-            throw new CastException("" + this, errors);
-        }
-
+        return dataType.castOrException(value, null, null);
     }
 
     /**
