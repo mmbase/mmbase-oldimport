cf4f81644b0c31f9f2ad5f6544ca42fab5e4fb1a
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-02-11 10:52:55 +0000

    small scopes are better
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@40960 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/vpro-wizards/src/main/java/org/mmbase/applications/vprowizards/spring/ReferrerResolver.java
+++ applications/vpro-wizards/src/main/java/org/mmbase/applications/vprowizards/spring/ReferrerResolver.java
@@ -6,7 +6,7 @@ OSI Certified is a certification mark of the Open Source Initiative.
 The license (Mozilla version 1.0) can be read at the MMBase site.
 See http://www.MMBase.org/license
 
-*/ 
+*/
 package org.mmbase.applications.vprowizards.spring;
 
 import java.util.*;
@@ -24,9 +24,9 @@ import org.springframework.web.servlet.view.RedirectView;
  * the result container. if there is an error it will return the value of the errorPage field. TODO: make sure the error
  * page exists and shows the errors well. only global errors should trigger the error page. field errors should be shown
  * in the editor.
- * 
+ *
  * @author ebunders
- * 
+ *
  */
 public class ReferrerResolver implements ModelAndViewResolver {
     private static Logger log = Logging.getLoggerInstance(ReferrerResolver.class);
@@ -34,19 +34,18 @@ public class ReferrerResolver implements ModelAndViewResolver {
 
     public ModelAndView getModelAndView(HttpServletRequest request, ResultContainer result) {
 
-        String callerPage;
         Map<String, Object> model = new HashMap<String, Object>();
         model.put("idmap", result.getIdMap());
         ModelAndView errorMandv = new ModelAndView(errorPage);
-        
+
 
         List<GlobalError> globalErrors = result.getGlobalErrors();
         if (result.hasGlobalErrors()) {
             errorMandv.addObject(GlobalError.MODEL_MAPPING_KEY, globalErrors);
             log.debug("request has global errors, so the return page is: "+errorPage);
             return errorMandv;
-        } 
-        
+        }
+
         if (result.hasFieldErrors()) {
             model.put(FieldError.MODEL_MAPPING_KEY, result.getFieldErrors());
             // Field errors are not displayed in the error page but in the referrer page (the form)
@@ -61,7 +60,7 @@ public class ReferrerResolver implements ModelAndViewResolver {
         // log.debug("object number " + newObject);
         // }
         // }
-        callerPage = request.getHeader("referer");
+        final String callerPage = request.getHeader("referer");
         if(callerPage == null){
             //this is an error for this view resolver
             globalErrors.add(new GlobalError("error.no.referrer.header", result.getLocale()));
@@ -70,7 +69,7 @@ public class ReferrerResolver implements ModelAndViewResolver {
             return errorMandv;
         }
         // add the node number of the new object to the referer url.
-        
+
 //        if (result.getNewObject().size() > 0) {
 //            if (log.isDebugEnabled()) {
 //                log.debug("new object created.");
@@ -79,14 +78,14 @@ public class ReferrerResolver implements ModelAndViewResolver {
 //            //newPage = newPage.substring(0, newPage.indexOf("?") + 1) + "nodenr=" + newNodeNr;
 //            String newPage = new URLParamMap(callerPage).addParam("nodenr", newNodeNr, true).toString();
 //        }
-        
+
         URLParamMap u = new URLParamMap(callerPage);
         if(result.getExtraParams().size() > 0){
             for(String param: result.getExtraParams().keySet()){
                 u.addParam(param, result.getExtraParams().get(param), true);
             }
         }
-        
+
         RedirectView redirectView = new RedirectView(u.toString());
         return new ModelAndView(redirectView, model);
     }
