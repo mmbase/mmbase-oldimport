576d3a435ae670118a3aafe812c8c4d3d37d1372
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-03-01 13:27:59 +0000

    spacing only
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@41205 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/vpro-wizards/src/main/java/org/mmbase/applications/vprowizards/spring/cache/FlushNameTemplateBean.java
+++ applications/vpro-wizards/src/main/java/org/mmbase/applications/vprowizards/spring/cache/FlushNameTemplateBean.java
@@ -16,35 +16,35 @@ import org.mmbase.util.logging.Logger;
 import org.mmbase.util.logging.Logging;
 
 /**
- * 
+ *
  * <pre>
  * This bean is used in the templates of the vpro-wizards. it is used to add node numbers to templates inside flush names.
- * It is actually a facade for the classes in {@link org.mmbase.applications.vprowizards.spring.cache.template}. 
- * 
+ * It is actually a facade for the classes in {@link org.mmbase.applications.vprowizards.spring.cache.template}.
+ *
  * So, what are templates?
- * 
+ *
  * Templates are a way to create dynamic cacheGroup names, where the dynamic bit is dependent on the node you are
- * currently editing, and are used by the list tag. 
+ * currently editing, and are used by the list tag.
  * You can use a flush name like, 'locations_[location]', where the 'location' between brackets is the builder name.
- *  
- * Inside the list tag the [builder name] part is then changed into [buildername:nodenumber] where node number is the 
- * number of each row (where the node type of the list matches the node type set in this bean). 
+ *
+ * Inside the list tag the [builder name] part is then changed into [buildername:nodenumber] where node number is the
+ * number of each row (where the node type of the list matches the node type set in this bean).
  * This way the flush name parameter will be different for each row in the list.
- * 
+ *
  * There is one variety, that you can use if you want to create a dynamic cache flush name for the parent node of the
  * nodes in a certain list. To do this you create a template like [this_type.relation_role.child_type]. Then the number of the
  * first node found with this path (where this_type matches the type set in this bean) will be inserted in the template.
- * 
+ *
  * The original template is not replaced with the node number, but the node number is appended to the template. This is
  * done because the template is handed to a (hierarchy of) jsp page(s), and is reused. with each reuse the previous node
  * numbers are stripped out of the template.
- * 
+ *
  * </pre>
- * 
+ *
  * TODO:/to test the 'extended' template (where the parent node is found we need either a cloud or a cloud mock object
- * 
+ *
  * @author ebunders
- * 
+ *
  */
 public class FlushNameTemplateBean {
 
