67544ae58b29ef5a7fe479182f393c3ff1b91fa8
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-03-19 17:56:00 +0000

    to fix   MMB-1936, factoring out core dependencies of ConstraintParser
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@41544 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ core/src/main/java/org/mmbase/module/core/CoreQueryContext.java
@@ -0,0 +1,34 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.module.core;
+
+import org.mmbase.bridge.Field;
+import org.mmbase.storage.search.QueryContext;
+import org.mmbase.storage.*;
+/**
+ *
+ * @author Michiel Meeuwissen
+ * @version $Id: ClusterNode.java 34900 2009-05-01 16:29:42Z michiel $
+ * @since MMBase-2.0
+ */
+public class CoreQueryContext implements QueryContext {
+
+    public static final QueryContext INSTANCE = new CoreQueryContext();
+
+    public  String getStorageIdentifier(String s) {
+        StorageManagerFactory<?> factory = MMBase.getMMBase().getStorageManagerFactory();
+        return (String) factory.getStorageIdentifier(s);
+    }
+    public  Field getField(String builder, String fieldName) {
+        return MMBase.getMMBase().getBuilder(builder).getField(fieldName);
+    }
+
+}
+
