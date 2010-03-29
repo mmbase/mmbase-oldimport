c2abf88d58896c7da7b69997f1e2f64625e467cf
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-03-29 08:28:02 +0000

    Merged maintaining also resolutions in the session, which makes it easier to do some things. E.g. temporary numbers can remain working. (merged -r41575 from 1.9)
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@41650 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/searchrelate/src/main/java/org/mmbase/searchrelate/Submitter.java
+++ applications/searchrelate/src/main/java/org/mmbase/searchrelate/Submitter.java
@@ -12,6 +12,8 @@ import java.util.concurrent.*;
 import org.mmbase.core.event.*;
 import org.mmbase.bridge.NodeQuery;
 import org.mmbase.bridge.util.Queries;
+import org.mmbase.storage.search.*;
+import javax.servlet.http.HttpSession;
 
 import org.mmbase.util.logging.Logger;
 import org.mmbase.util.logging.Logging;
@@ -22,7 +24,7 @@ import org.mmbase.util.logging.Logging;
  * In the first place this was for changes in order which were made in a transaction.  We do this afterwards because to sort a query result,
  * it is important that the query can be executed. Which is not possible when nodes are modified or new in a transaction.
  *
- *It will probably also be used to clean up other stuff from the session.
+ * It will probably also be used to clean up other stuff from the session.
  *
  * @author  Michiel Meeuwissen
  * @version $Id$
@@ -30,17 +32,20 @@ import org.mmbase.util.logging.Logging;
 public class Submitter implements TransactionEventListener {
     private static final Logger LOG = Logging.getLoggerInstance(Submitter.class);
 
+    private static final String KEY_RESOLUTIONS = Submitter.class.getName() + ".resolutions";
     private static final Map<String, Submitter> instances = new ConcurrentHashMap<String, Submitter>();
 
     private final String transactionName;
     private final Map<NodeQuery, List<Integer>> orders = new HashMap<NodeQuery, List<Integer>>();
     private final List<Runnable> endCallBacks = new ArrayList<Runnable>();
+    private HttpSession session;
+
 
     protected Submitter(String tn) {
         transactionName = tn;
     }
 
-    public static Submitter getInstance(String name) {
+    public static Submitter getInstance(String name, HttpSession session) {
         synchronized(instances) {
             Submitter os = instances.get(name);
             if (os == null) {
@@ -50,6 +55,9 @@ public class Submitter implements TransactionEventListener {
                 LOG.debug("Listening " + os);
             } else {
                 LOG.debug("Already an instance of " + name + " in " + instances);
+            }
+            if (session != null) {
+                os.session = session;
             }
             return os;
         }
@@ -92,6 +100,31 @@ public class Submitter implements TransactionEventListener {
             LOG.debug("" + e);
             if (e instanceof TransactionEvent.Resolve) {
                 TransactionEvent.Resolve resolve = (TransactionEvent.Resolve) e;
+                List<NodeQuery> keys = new ArrayList<NodeQuery>(orders.keySet());
+
+                if (session != null) {
+                    Map<Integer, Integer> resolutions = (Map<Integer, Integer>) session.getAttribute(KEY_RESOLUTIONS);
+                    if (resolutions == null) {
+                        resolutions =  new ConcurrentHashMap<Integer, Integer>();
+                        session.setAttribute(KEY_RESOLUTIONS, resolutions);
+                    }
+                    resolutions.putAll(resolve.getResolution());
+                    LOG.debug("put " + KEY_RESOLUTIONS + " -> " + resolutions + " in session");
+                } else {
+                    LOG.debug("No session in " + this);
+                }
+
+                // first fix queries which were using startNodes that are new.
+                for (NodeQuery q : keys) {
+                    NodeQuery toChange = (NodeQuery) Queries.fixQuery(q, resolve.getResolution());
+                    if (toChange != null) {
+                        List<Integer> value = orders.remove(q);
+                        orders.put(toChange, value);
+                        LOG.debug("Replaced " + q.toSql() + " with " + toChange.toSql());
+                    }
+                }
+
+                // The fix the new nodes in the the saved lists with orders.
                 for (Map.Entry<NodeQuery, List<Integer>> entry : orders.entrySet()) {
                     for (Map.Entry<Integer, Integer> resolution : resolve.getResolution().entrySet()) {
                         int index = entry.getValue().indexOf(resolution.getKey());
@@ -103,6 +136,8 @@ public class Submitter implements TransactionEventListener {
                 }
             }
             if (e instanceof TransactionEvent.Commit) {
+
+                // Commit the actual new order to the database too.
                 for (Map.Entry<NodeQuery, List<Integer>> entry : orders.entrySet()) {
                     try {
                         int changes = Queries.reorderResult(entry.getKey(), entry.getValue());
@@ -130,6 +165,8 @@ public class Submitter implements TransactionEventListener {
             LOG.debug("Ignoring " + e);
         }
     }
+
+
     @Override
     public String toString() {
         return "Submitter for " + transactionName;
