a1dd1dadc642b67fac877d1cc8e2e25b4d81b022
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-04-12 15:34:17 +0000

    was moved to bridge jar
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@41852 91430c5f-d668-0410-9e7d-dd663366dcfd

--- core/src/main/java/org/mmbase/util/functions/ExampleBean.java
+++ core/src/main/java/org/mmbase/util/functions/ExampleBean.java
@@ -1,6 +1,7 @@
 package org.mmbase.util.functions;
 
 import org.mmbase.module.core.*;
+import org.mmbase.module.core.NodeSearchQuery;
 import org.mmbase.storage.search.*;
 import org.mmbase.storage.search.implementation.*;
 import org.mmbase.bridge.*;
