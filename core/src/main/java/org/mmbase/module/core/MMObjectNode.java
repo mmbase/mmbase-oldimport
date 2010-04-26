e0e4956a7bb0ad1b33354b6d17c9e60056d8d810
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-04-26 13:29:03 +0000

    for virtual nodes it is pointless anyway
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@42042 91430c5f-d668-0410-9e7d-dd663366dcfd

--- core/src/main/java/org/mmbase/module/core/MMObjectNode.java
+++ core/src/main/java/org/mmbase/module/core/MMObjectNode.java
@@ -589,7 +589,13 @@ public class MMObjectNode implements org.mmbase.util.SizeMeasurable, java.io.Ser
             fieldValue = new SerializableInputStream((InputStream) fieldValue, getSize(fieldName));
         }
         if (fieldValue instanceof Node) {
-            fieldValue = Integer.valueOf(((Node) fieldValue).getNumber());
+            Node nodeValue = (Node) fieldValue;
+            if (! (nodeValue instanceof org.mmbase.bridge.implementation.VirtualNode)) {
+                int number = nodeValue.getNumber();
+                if (number != -1) { // -1 is very meaningless, and it will not be possible to make a sensible node of that again
+                    fieldValue = Integer.valueOf(number);
+                }
+            }
         }
         fieldValue = checkSerializable(fieldName, fieldValue);
         if (checkFieldExistance(fieldName)) {
