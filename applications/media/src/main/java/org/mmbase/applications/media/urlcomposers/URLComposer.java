f98c7fce40137d7c941e13b070161b0afb4d34db
andre <andre@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-06-08 09:30:35 +0000

    added audio codec
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@42443 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/media/src/main/java/org/mmbase/applications/media/urlcomposers/URLComposer.java
+++ applications/media/src/main/java/org/mmbase/applications/media/urlcomposers/URLComposer.java
@@ -10,14 +10,17 @@ See http://www.MMBase.org/license
 
 package org.mmbase.applications.media.urlcomposers;
 
-import java.util.*;
-
-import org.mmbase.applications.media.*;
 import org.mmbase.applications.media.builders.MediaProviders;
+import org.mmbase.applications.media.builders.MediaSources;
 import org.mmbase.module.core.MMObjectNode;
 import org.mmbase.util.HashCodeUtil;
 import org.mmbase.util.MimeType;
+import org.mmbase.applications.media.Format;
+import org.mmbase.applications.media.Codec;
+import org.mmbase.applications.media.State;
 import org.mmbase.util.images.Dimension;
+
+import java.util.*;
 
 /**
  * URLComposer is a wrapper/container class around an URL. It contains besides the
@@ -77,6 +80,20 @@ public class URLComposer  {
     public Codec getCodec() {
         return Codec.get(source.getIntValue("codec"));
     }
+    
+    /**
+     * Audio codec if its field is present, returns 'UNKNOWN' (-1) if no such field or codec is 
+     * unknown. Videosources have both codec and acodec (audio codec) fields, audiosources have 
+     * just one codec field.
+     */
+    public Codec getAcodec() {
+        if (source.getBuilder().hasField("acodec")) {
+            return Codec.get(source.getIntValue("acodec"));
+        } else {
+            return Codec.get(-1);
+        }
+    }
+    
     public int getBitrate() {
         return source.getIntValue("bitrate");
     }
