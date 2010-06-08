84ffbd844bc2d3a62502aa1931db3e34b67eb0da
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-06-08 09:57:31 +0000

    default owner
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@42449 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ bridge/src/main/java/org/mmbase/datatypes/processors/DefaultOwner.java
@@ -0,0 +1,45 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.datatypes.processors;
+
+import org.mmbase.bridge.*;
+import org.mmbase.bridge.util.*;
+import org.mmbase.util.logging.*;
+
+
+/**
+
+ * @author Michiel Meeuwissen
+ * @version $Id$
+ * @since MMBase-1.9.4
+ */
+
+public class DefaultOwner  implements Processor {
+    private static final Logger LOG = Logging.getLoggerInstance(DefaultOwner.class);
+
+    public Object process(Node node, Field field, Object value) {
+        LOG.debug("Getting default value for " + field);
+        final Cloud cloud;
+        if (node == null) {
+            cloud = CloudThreadLocal.currentCloud();
+        } else {
+            cloud = node.getCloud();
+        }
+        if (cloud == null) {
+            LOG.debug("No cloud  using " + value);
+            return value;
+        } else {
+            return cloud.getUser().getOwnerField();
+        }
+    }
+
+}
+
+
