91f4bd687ca8c5c76641a268a40d7e3138294fb5
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-07-02 07:00:10 +0000

    needs work, but will add stuff here
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@42791 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ applications/resources/src/test/java/org/mmbase/datatypes/processors/BinaryFileTest.java
@@ -0,0 +1,51 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.datatypes.processors;
+
+import java.io.*;
+import java.util.*;
+import org.mmbase.util.*;
+import org.mmbase.bridge.mock.*;
+import org.mmbase.bridge.*;
+import org.mmbase.datatypes.*;
+
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.junit.Assume.*;
+
+/**
+ *
+ * @author Michiel Meeuwissen
+ */
+
+public class BinaryFileTest {
+
+
+    @BeforeClass
+    public static void setup() {
+        DataTypes.initialize();
+        Map<String, DataType> map = new HashMap<String, DataType>();
+        map.put("number", Constants.DATATYPE_INTEGER);
+        map.put("file", DataTypes.getDataType("file"));
+        MockCloudContext.getInstance().addNodeManager("filetest", map);
+    }
+
+    @Test
+    public void set() {
+        NodeManager nm = MockCloudContext.getInstance().getCloud("mmbase").getNodeManager("filetest");
+        Node n = nm.createNode();
+        assertTrue(n.getNumber() < 0);
+
+    }
+
+
+
+}
