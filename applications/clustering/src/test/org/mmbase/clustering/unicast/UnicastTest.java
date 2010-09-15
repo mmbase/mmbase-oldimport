23aeda270c317c6711ed6a720b4e85d6a5765447
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-09-15 10:58:32 +0000

    git-svn-id: https://scm.mmbase.org/mmbase/trunk@43320 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ applications/clustering/src/test/org/mmbase/clustering/unicast/UnicastTest.java
@@ -0,0 +1,12 @@
+package org.mmbase.applications.clustering.unicast;
+import org.mmbase.util.*;
+import java.util.*;
+import org.junit.*;
+import org.junit.runner.*;
+import org.junit.runners.*;
+import static org.junit.Assert.*;
+
+public class UnicastTest {
+
+
+}
