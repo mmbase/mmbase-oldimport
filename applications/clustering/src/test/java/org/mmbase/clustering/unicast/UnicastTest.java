a52b59bca3b05793bfc2286dc8beaccab3cff797
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-09-15 11:36:57 +0000

    added a testcase for 'version 2' writing/reading
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@43323 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/clustering/src/test/java/org/mmbase/clustering/unicast/UnicastTest.java
+++ applications/clustering/src/test/java/org/mmbase/clustering/unicast/UnicastTest.java
@@ -1,12 +1,43 @@
-package org.mmbase.applications.clustering.unicast;
+package org.mmbase.clustering.unicast;
+import org.mmbase.clustering.Statistics;
 import org.mmbase.util.*;
 import java.util.*;
+import java.io.*;
 import org.junit.*;
-import org.junit.runner.*;
-import org.junit.runners.*;
 import static org.junit.Assert.*;
 
 public class UnicastTest {
 
+    @Test
+    public void basic() throws IOException {
+        ChangesReceiver receiver = new ChangesReceiver(null, -1, null, 2);
+        ChangesSender   sender   = new ChangesSender(new HashMap<String, String>(),
+                                                     -1, 100, null, new Statistics(), 2);
+
+        // not test if we can send
+        // make up some bytes
+        Random random = new Random(2);
+        List<byte[]> testSet = new ArrayList<byte[]>();
+
+        for (int i = 0; i < 50; i++) {
+            byte[] message = new byte[1 + i * 1000];
+            random.nextBytes(message);
+            testSet.add(message);
+        }
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        sender.writeVersion2(buffer, testSet);
+
+        LinkedList<byte[]> queue = new LinkedList<byte[]>();
+        InputStream in = new ByteArrayInputStream(buffer.toByteArray());
+        receiver.readStreamVersion2(in, queue);
+
+        assertEquals(testSet.size(), queue.size());
+        for (int i = 0 ; i < testSet.size(); i++) {
+            byte[] array1 = testSet.get(i);
+            byte[] array2 = queue.get(i);
+            assertTrue(Arrays.equals(array1, array2));
+        }
+
+    }
 
 }
