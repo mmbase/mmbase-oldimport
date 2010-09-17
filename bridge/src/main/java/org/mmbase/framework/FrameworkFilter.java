8db3581e702d4a8768f99101d4fdf8aef11f689b
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-09-17 11:44:04 +0000

    git-svn-id: https://scm.mmbase.org/mmbase/trunk@43390 91430c5f-d668-0410-9e7d-dd663366dcfd

--- bridge/src/main/java/org/mmbase/framework/FrameworkFilter.java
+++ bridge/src/main/java/org/mmbase/framework/FrameworkFilter.java
@@ -114,7 +114,6 @@ public class FrameworkFilter implements Filter { //, MMBaseStarter  {
         /* initialize MMBase if its not started yet */
         if (! MMBaseContext.isInitialized()) {
             MMBaseContext.init(ctx);
-            MMBaseContext.initHtmlRoot();
         }
 
         /*
