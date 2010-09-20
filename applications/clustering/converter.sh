060330df60fb473009b622008a0ec6cea83b2aa5
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-09-20 09:12:14 +0000

    added some comments only
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@43412 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/clustering/converter.sh
+++ applications/clustering/converter.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+# The clustering jar contains a class 'Converter' which can be used as a unicast/multicast bridge.
+# It is used at the VPRO to connect one 'out lying' server with unicast to the rest of there servers.
+#
+# This is an example script that only sets up the CLASSPATH correctly and calls this class. For an overview of possible
+# parameters call it with an unknown one, e.g. ./converter.sh help.
+
 
 export CLASSPATH=\
 ~/.m2/repository/org/mmbase/mmbase-utils/2.0-SNAPSHOT/mmbase-utils-2.0-SNAPSHOT.jar:\
 ~/.m2/repository/org/mmbase/mmbase-clustering/2.0-SNAPSHOT/mmbase-clustering-2.0-SNAPSHOT-classes.jar
 
-echo ${CLASSPATH}
+#echo ${CLASSPATH}
 
 java org.mmbase.clustering.Converter $@
