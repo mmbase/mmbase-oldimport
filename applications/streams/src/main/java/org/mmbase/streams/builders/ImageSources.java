aaf7241edec6043a52e82e7c74cc0d7864423602
andre <andre@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-10-18 09:06:30 +0000

    streams has gpl license
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@43665 91430c5f-d668-0410-9e7d-dd663366dcfd

--- applications/streams/src/main/java/org/mmbase/streams/builders/ImageSources.java
+++ applications/streams/src/main/java/org/mmbase/streams/builders/ImageSources.java
@@ -1,11 +1,23 @@
- /*
+/*
 
- This software is OSI Certified Open Source Software.
- OSI Certified is a certification mark of the Open Source Initiative.
+This file is part of the MMBase Streams application,
+which is part of MMBase - an open source content management system.
+    Copyright (C) 2009 André van Toly, Michiel Meeuwissen
 
- The license (Mozilla version 1.0) can be read at the MMBase site.
- See http://www.MMBase.org/license
-  */
+MMBase Streams is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MMBase Streams is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MMBase. If not, see <http://www.gnu.org/licenses/>.
+
+*/
 
 package org.mmbase.streams.builders;
 import org.mmbase.applications.media.builders.*;
