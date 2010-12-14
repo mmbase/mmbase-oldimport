ffc152f942b0789f59af71c74c9263034c384a3a
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-12-14 13:44:01 +0000

    test for AutodefiningParmaeters
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@43958 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ bridge/src/test/java/org/mmbase/util/functions/AutodefiningParametersTest.java
@@ -0,0 +1,54 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.util.functions;
+import org.mmbase.datatypes.*;
+import static org.junit.Assert.*;
+import org.junit.*;
+
+/**
+ *
+ * @author Michiel Meeuwissen
+ * @verion $Id: ParameterTest.java 39343 2009-10-26 21:04:47Z michiel $
+ */
+public class AutodefiningParametersTest {
+
+    @BeforeClass
+    public static void setup() throws Exception {
+        DataTypes.initialize();
+    }
+
+    @Test
+    public void basic() {
+        Parameters params = new AutodefiningParameters();
+        params.set("a", "A");
+        params.set("b", new Integer(2));
+
+        assertEquals("A", params.get("a"));
+        assertEquals(new Integer(2), params.get("b"));
+    }
+
+    @Test
+    public void index() {
+        Parameters params = new AutodefiningParameters();
+        params.set(1, "A");
+        params.set(2, new Integer(2));
+        assertEquals(3, params.size());
+
+        params.set("b", "B");
+        assertEquals(3, params.size());
+
+        assertEquals("B", params.get(0));
+        assertEquals("A", params.get(1));
+        assertEquals(new Integer(2), params.get(2));
+    }
+
+
+
+}
