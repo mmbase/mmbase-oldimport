31a1cc6cf5521e86b63dbe9166a06a4d0d1c56e4
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-12-14 13:42:27 +0000

    AutodefiningParameters didn't work quite well when useing set(int)
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@43956 91430c5f-d668-0410-9e7d-dd663366dcfd

--- bridge/src/main/java/org/mmbase/util/functions/AutodefiningParameters.java
+++ bridge/src/main/java/org/mmbase/util/functions/AutodefiningParameters.java
@@ -10,6 +10,8 @@ See http://www.MMBase.org/license
 
 package org.mmbase.util.functions;
 
+import java.util.*;
+
 /**
  * If there is no Parameter definition array available you could try it with this specialization, which does not need one.
  * You loose al checking on type and availability. It should only be used as a last fall back and accompanied by warnings.
@@ -22,6 +24,8 @@ package org.mmbase.util.functions;
 
 public class AutodefiningParameters extends Parameters {
     private static final long serialVersionUID = 0L;
+
+    private final Set<String> myParams = new TreeSet<String>();
     //private static Logger log = Logging.getLoggerInstance(AutodefiningParameters.class);
 
     public AutodefiningParameters(Parameter<?>[] base) {
@@ -37,15 +41,23 @@ public class AutodefiningParameters extends Parameters {
     }
 
     protected int define(Parameter param) {
-        Parameter<Object>[] newDef = new Parameter[definition.length + 1];
-        for (int i = 0; i < definition.length; i++) {
-            newDef[i] = definition[i];
+        if (myParams.size() > 0) {
+            String prevParam = myParams.iterator().next();
+            myParams.remove(prevParam);
+            int i = indexOfParameter(prevParam);
+            definition[i] = param;
+            return i;
+        } else {
+            Parameter<Object>[] newDef = new Parameter[definition.length + 1];
+            for (int i = 0; i < definition.length; i++) {
+                newDef[i] = definition[i];
+            }
+            newDef[newDef.length - 1] = param;
+            definition = newDef;
+            toIndex++;
+            patternLimit++;
+            return definition.length - 1;
         }
-        newDef[newDef.length - 1] = param;
-        definition = newDef;
-        toIndex++;
-        patternLimit++;
-        return definition.length - 1;
     }
 
     protected int define(String param) {
@@ -70,6 +82,23 @@ public class AutodefiningParameters extends Parameters {
         } else {
             return index;
         }
+    }
+
+    @Override
+    public Object set(int i, Object value) {
+        Set<String> newParams = new TreeSet<String>();
+        while (definition.length <= i) {
+            String argName = "arg" + definition.length;
+            while (! newParams.contains(argName) && super.indexOfParameter(argName) != -1) {
+                argName += "_";
+            }
+            define(argName);
+            if (definition.length <= i) {
+                newParams.add(argName);
+            }
+        }
+        myParams.addAll(newParams);
+        return super.set(i, value);
     }
 
     public static void main(String[] args) {
