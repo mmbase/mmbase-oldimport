a085142c49bc108161978e9f26cf3ca70cb26f0c
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-12-14 11:57:58 +0000

    modernized
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@43920 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ core/src/main/java/org/mmbase/package-info.java
@@ -0,0 +1,6 @@
+package org.mmbase;
+/**
+ * The implementation of <a href="http://www.mmbase.org">MMBase</a>, an open source content management system.
+ * @version $Id
+ * @since   MMBase-1.0
+ */
