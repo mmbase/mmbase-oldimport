7e1b1830521465ee6e84028647b9244360afc9d8
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2011-01-07 11:49:51 +0000

    @Override's only
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@44641 91430c5f-d668-0410-9e7d-dd663366dcfd

--- maven/maven-mmbase-plugin/src/main/java/org/mmbase/mojo/CreateIndexFiles.java
+++ maven/maven-mmbase-plugin/src/main/java/org/mmbase/mojo/CreateIndexFiles.java
@@ -21,74 +21,76 @@ import org.apache.maven.plugin.AbstractMojo;
  */
 public class CreateIndexFiles extends AbstractMojo {
 
-   /**
-    * Directory containing the classes and resource files that should be packaged into the JAR.
-    *
-    * @parameter expression="${project.build.outputDirectory}"
-    * @required
-    */
-   private File classesDirectory;
+    /**
+     * Directory containing the classes and resource files that should be packaged into the JAR.
+     *
+     * @parameter expression="${project.build.outputDirectory}"
+     * @required
+     */
+    private File classesDirectory;
 
-   private FilenameFilter dirs = new FilenameFilter() {
-      public boolean accept(File dir, String name) {
-         return new File(dir, name).isDirectory();
-      }
-   };
-
-   public void execute() {
-      if (classesDirectory != null && classesDirectory.exists()) {
-         File configDir = new File(classesDirectory, File.separator + "org" + File.separator
-                                   + "mmbase" + File.separator + "config");
-         if (configDir.exists()) {
-            createIndex(configDir);
-         }
-      }
-   }
-
-   protected void createIndex(File dir) {
-      if (dir.isDirectory()) {
-         File index = new File(dir, "INDEX");
-         if (index.exists()) {
-            index.delete();
-         }
-         writeIndex(dir, index);
-
-         String subdirs[] = dir.list(dirs);
-         for (int i = 0; i < subdirs.length; i++) {
-            createIndex(new File(dir, subdirs[i]));
-         }
-      }
-   }
-
-   private void writeIndex(File dir, File index) {
-      getLog().debug("Generating " + index);
-      BufferedWriter w = null;
-      try {
-         w = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(index), "UTF-8"));
-         String files[] = dir.list();
-         for (int i = 0; i < files.length; i++) {
-            File file = new File(dir, files[i]);
-            w.write(files[i]);
-            if (file.isDirectory()) {
-               w.write('/');
+    private FilenameFilter dirs = new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return new File(dir, name).isDirectory();
             }
-            w.newLine();
-         }
-      } catch (UnsupportedEncodingException e) {
-         getLog().error(e);
-      } catch (FileNotFoundException e) {
-         getLog().error(e);
-      } catch (IOException e) {
-         getLog().error(e);
-      } finally {
-         try {
-             if (w != null) {
-               w.close();
+        };
+
+    @Override
+    public void execute() {
+        if (classesDirectory != null && classesDirectory.exists()) {
+            File configDir = new File(classesDirectory, File.separator + "org" + File.separator
+                                      + "mmbase" + File.separator + "config");
+            if (configDir.exists()) {
+                createIndex(configDir);
             }
-         } catch (IOException e) {
-            getLog().debug("" + e.getMessage(), e);
-         }
-      }
-   }
+        }
+    }
+
+    protected void createIndex(File dir) {
+        if (dir.isDirectory()) {
+            File index = new File(dir, "INDEX");
+            if (index.exists()) {
+                index.delete();
+            }
+            writeIndex(dir, index);
+
+            String subdirs[] = dir.list(dirs);
+            for (int i = 0; i < subdirs.length; i++) {
+                createIndex(new File(dir, subdirs[i]));
+            }
+        }
+    }
+
+    private void writeIndex(File dir, File index) {
+        getLog().debug("Generating " + index);
+        BufferedWriter w = null;
+        try {
+            w = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(index), "UTF-8"));
+            String files[] = dir.list();
+            for (int i = 0; i < files.length; i++) {
+                File file = new File(dir, files[i]);
+                w.write(files[i]);
+                if (file.isDirectory()) {
+                    w.write('/');
+                }
+                w.newLine();
+            }
+        } catch (UnsupportedEncodingException e) {
+            getLog().error(e);
+        } catch (FileNotFoundException e) {
+            getLog().error(e);
+        } catch (IOException e) {
+            getLog().error(e);
+        } finally {
+            try {
+                if (w != null) {
+                    w.close();
+                }
+            } catch (IOException e) {
+                getLog().debug("" + e.getMessage(), e);
+            }
+        }
+    }
 
 }
