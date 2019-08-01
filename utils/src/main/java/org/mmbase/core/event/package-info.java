264eb52a58f6e5fc624a84489c8baf21d4fdd68d
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-08-01 13:59:50 +0200

    mmbase utils was move to its own git module

--- utils/src/main/java/org/mmbase/core/event/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * An event mechanism. It consists of {@link org.mmbase.core.event.Event}s, {@link org.mmbase.core.even.EventListener}s and {@link org.mmbase.core.even.EventBroker}s. And everything is  glued together with {@link org.mmbase.core.event.EventManager}
- *
- * @version $Id$
- * @since   MMBase-1.8
- * @todo    Move to org.mmbase.event
- */
-package org.mmbase.core.event;
